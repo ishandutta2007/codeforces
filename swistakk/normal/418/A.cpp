79e2c79b8b73034162206ffa71a972d9cc3714b5
Ishan <ishandutta2007@gmail.com>
2022-08-30 11:32:04 +0530

    swistakk

---
+++ swistakk/normal/418/A.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cstdio>
+#include <utility>
+#include <iomanip>
+#include <assert.h>
+#define MP make_pair
+#define PB push_back
+#define FOR(i, a, b) for(int i =(a); i <=(b); ++i)
+#define RE(i, n) FOR(i, 1, n)
+#define FORD(i, a, b) for(int i = (a); i >= (b); --i)
+#define REP(i, n) for(int i = 0;i <(n); ++i)
+#define VAR(v, i) __typeof(i) v=(i)
+#define FORE(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define PB push_back
+#define MP make_pair
+#ifdef LOCAL
+#define debug(x) {cerr <<#x <<" = " <<x <<"\n"; }
+#define debugv(x) {{cerr <<#x <<" = "; FORE(itt, (x)) cerr <<*itt <<", "; cerr <<"\n"; }}
+#else
+#define debug(x)
+#define debugv(x)
+#endif
+#define make(type, x) type x; cin>>x;
+#define make2(type, x, y) type x, y; cin>>x>>y;
+#define make3(type, x, y, z) type x, y, z; cin>>x>>y>>z;
+using namespace std;
+typedef long long ll;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+typedef vector<int> VI;
+typedef vector<ll> VLL;
+typedef vector<pair<int, int> > VPII;
+typedef vector<pair<ll, ll> > VPLL;
+
+template<class C> void mini(C&a4, C b4){a4=min(a4, b4); }
+template<class C> void maxi(C&a4, C b4){a4=max(a4, b4); }
+template<class T1, class T2>
+ostream& operator<< (ostream &out, pair<T1, T2> pair) { return out << "(" << pair.X << ", " << pair.Y << ")";}
+
+int main() {
+  // nie zapomnij o ll
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(10);
+  
+  make2(int, n, k);
+  if (k > (n - 1) / 2)  {
+      cout<<"-1\n";
+      return 0;
+  }
+  cout<<n*k<<endl;
+  for (int i = 1; i <= n; i++) {
+      for (int j = i + 1; j <= i + k; j++) {
+              cout<<i<<" "<<(j - 1) % n + 1<<"\n";
+          
+      }
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  // nie zapomnij o ll
+  return 0;
+}
