89081387c42680bd3627dfe0d26b72dd0058ddb4
Ishan <ishandutta2007@gmail.com>
2022-08-30 09:19:41 +0530

    swistakk

---
+++ swistakk/normal/993/C.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+#define MP make_pair
+#define PB push_back
+#define int long long
+#define st first
+#define nd second
+#define rd third
+#define FOR(i, a, b) for(int i =(a); i <=(b); ++i)
+#define RE(i, n) FOR(i, 1, n)
+#define FORD(i, a, b) for(int i = (a); i >= (b); --i)
+#define REP(i, n) for(int i = 0;i <(n); ++i)
+#define VAR(v, i) __typeof(i) v=(i)
+#define FORE(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+using namespace std;
+template<typename TH> void _dbg(const char* sdbg, TH h) { cerr<<sdbg<<"="<<h<<"\n"; }
+template<typename TH, typename... TA> void _dbg(const char* sdbg, TH h, TA... t) {
+  while(*sdbg != ',')cerr<<*sdbg++; cerr<<"="<<h<<","; _dbg(sdbg+1, t...);
+}
+#ifdef LOCAL
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#define debugv(x) {{cerr <<#x <<" = "; FORE(itt, (x)) cerr <<*itt <<", "; cerr <<"\n"; }}
+#else
+#define debug(...) (__VA_ARGS__)
+#define debugv(x)
+#define cerr if(0)cout
+#endif
+#define next ____next
+#define prev ____prev
+#define left ____left
+#define hash ____hash
+typedef long long ll;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL;
+typedef vector<pair<int, int> > VPII;
+typedef vector<pair<ll, ll> > VPLL;
+
+template<class C> void mini(C&a4, C b4){a4=min(a4, b4); }
+template<class C> void maxi(C&a4, C b4){a4=max(a4, b4); }
+template<class T1, class T2>
+ostream& operator<< (ostream &out, pair<T1, T2> pair) { return out << "(" << pair.first << ", " << pair.second << ")";}
+template<class A, class B, class C> struct Triple { A first; B second; C third;
+  bool operator<(const Triple& t) const { if (st != t.st) return st < t.st; if (nd != t.nd) return nd < t.nd; return rd < t.rd; } };
+template<class T> void ResizeVec(T&, vector<int>) {}
+template<class T> void ResizeVec(vector<T>& vec, vector<int> sz) {
+  vec.resize(sz[0]); sz.erase(sz.begin()); if (sz.empty()) { return; }
+  for (T& v : vec) { ResizeVec(v, sz); }
+}
+typedef Triple<int, int, int> TIII;
+template<class A, class B, class C>
+ostream& operator<< (ostream &out, Triple<A, B, C> t) { return out << "(" << t.st << ", " << t.nd << ", " << t.rd << ")"; }
+template<class T> ostream& operator<<(ostream& out, vector<T> vec) { out<<"("; for (auto& v: vec) out<<v<<", "; return out<<")"; }
+template<class T> ostream& operator<<(ostream& out, set<T> vec) { out<<"("; for (auto& v: vec) out<<v<<", "; return out<<")"; }
+template<class L, class R> ostream& operator<<(ostream& out, map<L, R> vec) { out<<"("; for (auto& v: vec) out<<v<<", "; return out<<")"; }
+
+typedef bitset<123> B;
+
+
+int32_t main() {
+
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(10);
+  cerr << fixed << setprecision(10);
+  cin.tie(0);
+  //double beg_clock = 1.0 * clock() / CLOCKS_PER_SEC;
+  
+  int n, m;
+  cin>>n>>m;
+  vector<int> left, right;
+  RE (i, n) {
+    int y;
+    cin>>y;
+    y += 10000;
+    left.PB(y);
+  }
+  RE (i, m) {
+    int y;
+    cin>>y;
+    y += 10000;
+    right.PB(y);
+  }
+  int M = 2e4 + 5;
+  vector<B> bitsets(2 * M);
+  REP (i, n) {
+    REP (j, m) {
+      bitsets[left[i] + right[j]][i] = 1;
+      bitsets[left[i] + right[j]][n + j] = 1;
+    }
+  }
+  int best = 0;
+  sort(ALL(bitsets), [&](B& a, B& b) { return a.count() > b.count(); });
+  REP (i, SZ(bitsets)) {
+    FOR (j, i + 1, SZ(bitsets) - 1) {
+      if ((int)(bitsets[i].count() + bitsets[j].count()) <= best) { break; }
+      maxi(best, (int)(bitsets[i] | bitsets[j]).count());
+    }
+  }
+  cout<<best<<endl;
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+}
---
+++ swistakk/normal/995/A.cpp
@@ -0,0 +1,191 @@
+#include <bits/stdc++.h>
+#define MP make_pair
+#define PB push_back
+#define int long long
+#define st first
+#define nd second
+#define rd third
+#define FOR(i, a, b) for(int i =(a); i <=(b); ++i)
+#define RE(i, n) FOR(i, 1, n)
+#define FORD(i, a, b) for(int i = (a); i >= (b); --i)
+#define REP(i, n) for(int i = 0;i <(n); ++i)
+#define VAR(v, i) __typeof(i) v=(i)
+#define FORE(i, c) for(VAR(i, (c).begin()); i != (c).end(); ++i)
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+using namespace std;
+template<typename TH> void _dbg(const char* sdbg, TH h) { cerr<<sdbg<<"="<<h<<"\n"; }
+template<typename TH, typename... TA> void _dbg(const char* sdbg, TH h, TA... t) {
+  while(*sdbg != ',')cerr<<*sdbg++; cerr<<"="<<h<<","; _dbg(sdbg+1, t...);
+}
+#ifdef LOCAL
+#define debug(...) _dbg(#__VA_ARGS__, __VA_ARGS__)
+#define debugv(x) {{cerr <<#x <<" = "; FORE(itt, (x)) cerr <<*itt <<", "; cerr <<"\n"; }}
+#else
+#define debug(...) (__VA_ARGS__)
+#define debugv(x)
+#define cerr if(0)cout
+#endif
+#define next ____next
+#define prev ____prev
+#define left ____left
+#define hash ____hash
+typedef long long ll;
+typedef long double LD;
+typedef pair<int, int> PII;
+typedef pair<ll, ll> PLL;
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+typedef vector<ll> VLL;
+typedef vector<pair<int, int> > VPII;
+typedef vector<pair<ll, ll> > VPLL;
+
+template<class C> void mini(C&a4, C b4){a4=min(a4, b4); }
+template<class C> void maxi(C&a4, C b4){a4=max(a4, b4); }
+template<class T1, class T2>
+ostream& operator<< (ostream &out, pair<T1, T2> pair) { return out << "(" << pair.first << ", " << pair.second << ")";}
+template<class A, class B, class C> struct Triple { A first; B second; C third;
+  bool operator<(const Triple& t) const { if (st != t.st) return st < t.st; if (nd != t.nd) return nd < t.nd; return rd < t.rd; } };
+template<class T> void ResizeVec(T&, vector<int>) {}
+template<class T> void ResizeVec(vector<T>& vec, vector<int> sz) {
+  vec.resize(sz[0]); sz.erase(sz.begin()); if (sz.empty()) { return; }
+  for (T& v : vec) { ResizeVec(v, sz); }
+}
+typedef Triple<int, int, int> TIII;
+template<class A, class B, class C>
+ostream& operator<< (ostream &out, Triple<A, B, C> t) { return out << "(" << t.st << ", " << t.nd << ", " << t.rd << ")"; }
+template<class T> ostream& operator<<(ostream& out, vector<T> vec) { out<<"("; for (auto& v: vec) out<<v<<", "; return out<<")"; }
+template<class T> ostream& operator<<(ostream& out, set<T> vec) { out<<"("; for (auto& v: vec) out<<v<<", "; return out<<")"; }
+template<class L, class R> ostream& operator<<(ostream& out, map<L, R> vec) { out<<"("; for (auto& v: vec) out<<v<<", "; return out<<")"; }
+
+const int N = 555;
+int fir[N];
+int sec[N];
+int rd[N];
+int fourth[N];
+int32_t main() {
+
+  ios_base::sync_with_stdio(0);
+  cout << fixed << setprecision(10);
+  cerr << fixed << setprecision(10);
+  cin.tie(0);
+  //double beg_clock = 1.0 * clock() / CLOCKS_PER_SEC;
+  
+  int n, k;
+  cin>>n>>k;
+  RE (i, n) {
+    cin>>fir[i];
+  }
+  RE (i, n) {
+    cin>>sec[i];
+  }
+  RE (i, n) {
+    cin>>rd[i];
+  }
+  RE (i, n) {
+    cin>>fourth[i];
+  }
+  VVI moves;
+  if (k == 2 * n) {
+    RE (i, n) {
+      if (fir[i] == sec[i]) {
+        moves.PB({sec[i], 1, i});
+        sec[i] = 0;
+      }
+      if (rd[i] == fourth[i]) {
+        moves.PB({rd[i], 4, i});
+        rd[i] = 0;
+      }
+    }
+    if (SZ(moves) == 0) {
+      cout<<"-1\n";
+      return 0;
+    }
+  }
+  VPII poses;
+  VI spaces;
+  VPII goal;
+  VI cur;
+  RE (i, n) {
+    poses.PB({2, i});
+    goal.PB({1, i});
+    spaces.PB(fir[i]);
+    cur.PB(sec[i]);
+  }
+  FORD (i, n, 1) {
+    goal.PB({4, i});
+    poses.PB({3, i});
+    spaces.PB(fourth[i]);
+    cur.PB(rd[i]);
+  }
+  REP (_, 2 * n) {
+    int fir_free = -1;
+    REP (i, 2 * n) {
+      if (cur[i] == 0) {
+        fir_free = i;
+        break;
+      }
+    }
+    FOR (i, fir_free + 1, 2 * n - 1) {
+      if (cur[i]) {
+        int r, c;
+        tie(r, c) = poses[i - 1];
+        moves.PB({cur[i], r, c});
+        cur[i - 1] = cur[i];
+        cur[i] = 0;
+      }
+    }
+    FOR (i, 0, fir_free - 1) {
+      if (cur[i]) {
+        int r, c;
+        int prv = i - 1;
+        if (i == 0) {
+          prv = 2 * n - 1;
+        }
+        tie(r, c) = poses[prv];
+        moves.PB({cur[i], r, c});
+        cur[prv] = cur[i];
+        cur[i] = 0;
+      }
+    }
+    REP (i, 2 * n) {
+      if (cur[i] == spaces[i] && cur[i]) {
+        int r, c;
+        tie(r, c) = goal[i];
+        moves.PB({cur[i], r, c});
+        cur[i] = 0;
+      }
+    }
+  }
+  cout<<SZ(moves)<<endl;
+  for (auto v : moves) {
+    cout<<v[0]<<" "<<v[1]<<" "<<v[2]<<"\n";
+  }
+    
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  return 0;
+}
