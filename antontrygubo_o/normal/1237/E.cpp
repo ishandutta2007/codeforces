f7bfc229037a405d871da93b314126e2fbecdef2
ishan dutta <ishandutta2007@gmail.com>
2022-09-02 20:47:27 +0530

    antontrygubo_o

---
+++ antontrygubo_o/normal/1237/E.cpp
@@ -0,0 +1,271 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+/*#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")*/
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define double long double
+#define ll long long
+#define ld long double
+#define mp make_pair
+#define what_is(x) cerr << #x << " is " << x << endl;
+
+typedef tree<
+        int,
+        null_type,
+        less<int>,
+        rb_tree_tag,
+        tree_order_statistics_node_update>
+        ordered_set;
+
+int p = 1e9 + 7;
+
+
+int mul(int a, int b) {
+    return (1LL * a * b) % p;
+}
+
+int add(int a, int b) {
+    int s = (a+b);
+    if (s>=p) s-=p;
+    return s;
+}
+
+int sub(int a, int b) {
+    int s = (a+p-b);
+    if (s>=p) s-=p;
+    return s;
+}
+
+int po(int a, int deg)
+{
+    if (deg==0) return 1;
+    if (deg%2==1) return mul(a, po(a, deg-1));
+    int t = po(a, deg/2);
+    return mul(t, t);
+}
+
+int inv(int n)
+{
+    return po(n, p-2);
+}
+
+
+vector<int> Z(vector<int> s)
+{
+    int n = s.size();
+    vector<int> z(n);
+    int L = 0, R = 0;
+    for (int i = 1; i < n; i++) {
+        if (i > R) {
+            L = R = i;
+            while (R < n && s[R-L] == s[R]) R++;
+            z[i] = R-L; R--;
+        } else {
+            int k = i-L;
+            if (z[k] < R-i+1) z[i] = z[k];
+            else {
+                L = i;
+                while (R < n && s[R-L] == s[R]) R++;
+                z[i] = R-L; R--;
+            }
+        }
+    }
+    return z;
+}
+
+mt19937 rnd(time(0));
+/*
+struct Line {
+    mutable ll k, m, p;
+    bool operator<(const Line& o) const { return k < o.k; }
+    bool operator<(ll x) const { return p < x; }
+};
+
+struct CHT : multiset<Line, less<>> {
+    // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    const ll inf = LLONG_MAX;
+    ll div(ll a, ll b) { // floored division
+        return a /b - ((a ^ b) < 0 && a % b); }
+    bool isect(iterator x, iterator y) {
+        if (y == end()) { x->p = inf; return false; }
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
+        return x->p >= y->p;
+    }
+    void add(ll k, ll m) {
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+        isect(x, erase(y));
+    }
+    ll query(ll x) {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.k * x + l.m;
+    }
+};
+*/
+
+
+
+/*
+typedef vector<int> VI;
+typedef vector<VI> VVI;
+
+const int INF = 1000000000;
+
+struct MaxFlow {
+  int N;
+  VVI cap, flow;
+  VI dad, Q;
+
+  MaxFlow(int N) :
+    N(N), cap(N, VI(N)), flow(N, VI(N)), dad(N), Q(N) {}
+
+  void AddEdge(int from, int to, int cap) {
+    this->cap[from][to] += cap;
+  }
+
+  int BlockingFlow(int s, int t) {
+    fill(dad.begin(), dad.end(), -1);
+    dad[s] = -2;
+
+    int head = 0, tail = 0;
+    Q[tail++] = s;
+    while (head < tail) {
+      int x = Q[head++];
+      for (int i = 0; i < N; i++) {
+        if (dad[i] == -1 && cap[x][i] - flow[x][i] > 0) {
+          dad[i] = x;
+          Q[tail++] = i;
+        }
+      }
+    }
+
+    if (dad[t] == -1) return 0;
+
+    int totflow = 0;
+    for (int i = 0; i < N; i++) {
+      if (dad[i] == -1) continue;
+      int amt = cap[i][t] - flow[i][t];
+      for (int j = i; amt && j != s; j = dad[j])
+        amt = min(amt, cap[dad[j]][j] - flow[dad[j]][j]);
+      if (amt == 0) continue;
+      flow[i][t] += amt;
+      flow[t][i] -= amt;
+      for (int j = i; j != s; j = dad[j]) {
+        flow[dad[j]][j] += amt;
+        flow[j][dad[j]] -= amt;
+      }
+      totflow += amt;
+    }
+
+    return totflow;
+  }
+
+  int GetMaxFlow(int source, int sink) {
+    int totflow = 0;
+    while (int flow = BlockingFlow(source, sink))
+      totflow += flow;
+    return totflow;
+  }
+};*/
+
+struct Sparse
+{
+    vector<vector<int>> t;
+
+    Sparse(vector<int> &a)
+    {
+        int n = a.size();
+        t = vector<vector<int>>(20, vector<int>(n));
+        for (int i = 0; i<n; i++) t[0][i] = a[i];
+        for (int it = 1; it<20; it++)
+        {
+            int N = 1<<it;
+            for (int i = 0; i+N<=n; i++)
+            {
+                t[it][i] = max(t[it-1][i], t[it-1][i+N/2]);
+            }
+        }
+    }
+
+    int max_on(int l, int r)
+    {
+        int i = 0;
+        while ((1<<(i+1))<=r-l+1) i++;
+        int N = 1<<i;
+        return max(t[i][l], t[i][r-N+1]);
+    }
+
+    int find_larger_left(int r, int x)
+    {
+        if (max_on(0, r)<x) return -1;
+        int L = 0;
+        int R = r;
+        while (R-L>1)
+        {
+            int mid = (L+R)/2;
+            if (max_on(mid, r)>=x) L = mid;
+            else R = mid;
+        }
+        return L;
+    }
+
+    int find_larger_right(int l, int x)
+    {
+        if (max_on(l, t[0].size()-1)<x) return t[0].size();
+        int L = l;
+        int R = t[0].size()-1;
+        while (R-L>1)
+        {
+            int mid = (L+R)/2;
+            if (max_on(l, mid)>=x) R = mid;
+            else L = mid;
+        }
+        return R;
+    }
+};
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    const int N = 25;
+
+    vector<int> l(N);
+    vector<int> r(N);
+    l[0] = 0;
+    r[0] = 0;
+    set<int> possible;
+    possible.insert(1);
+    possible.insert(2);
+    for (int i = 1; i<N; i++)
+    {
+        l[i] = r[i-1] + l[i-1] + 1;
+        r[i] = l[i];
+        if (r[i-1]%2==1) break;
+        if (l[i-1]%2==0) r[i]++;
+        possible.insert(l[i]+r[i]+1);
+        possible.insert(l[i]+r[i]+2);
+    }
+    /*for (auto it: possible) cout<<it<<' ';
+    cout<<endl;*/
+    int n;
+    cin>>n;
+    if (possible.find(n)==possible.end()) cout<<0;
+    else cout<<1;
+
+
+
+}
