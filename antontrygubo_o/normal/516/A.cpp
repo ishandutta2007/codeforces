e268f8654c737c728d6a27efc0894e85eec2ff01
ishan dutta <ishandutta2007@gmail.com>
2022-09-02 20:31:31 +0530

    antontrygubo_o

---
+++ antontrygubo_o/normal/516/A.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+#define mp make_pair
+
+const int p = 1000000007;
+
+
+int mul(int a, int b) {
+    return (1LL * a * b) % p;
+}
+
+int add(int a, int b) {
+    int s = (a+b);
+    if (s>=p) s-=p;
+    return s;
+}
+
+int sub(int a, int b) {
+    int s = (a+p-b);
+    if (s>=p) s-=p;
+    return s;
+}
+
+int po(int a, int deg)
+{
+    if (deg==0) return 1;
+    if (deg%2==1) return mul(a, po(a, deg-1));
+    int t = po(a, deg/2);
+    return mul(t, t);
+}
+
+int inv(int n)
+{
+    return po(n, p-2);
+}
+
+/*struct DSU {
+    vector<int> sz;
+    vector<int> parent;
+
+    void make_set(int v) {
+        parent[v] = v;
+        sz[v] = 1;
+    }
+
+    int find_set(int v) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (sz[a] < sz[b])
+                swap(a, b);
+            parent[b] = a;
+            sz[a] += sz[b];
+        }
+    }
+
+    DSU(int n) {
+        sz.resize(n);
+        parent.resize(n);
+        for (int i = 0; i < n; i++) make_set(i);
+    }
+};*/
+
+/*const int N = 1000000;
+
+vector<int> facs(N), invfacs(N);
+
+void init()
+{
+    facs[0] = 1;
+    for (int i = 1; i<N; i++) facs[i] = mul(facs[i-1], i);
+    invfacs[N-1] = inv(facs[N-1]);
+    for (int i = N-2; i>=0; i--) invfacs[i] = mul(invfacs[i+1], i+1);
+}
+
+int C(int n, int k)
+{
+    return mul(facs[n], mul(invfacs[k], invfacs[n-k]));
+}*/
+
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+    vector<int> a(10);
+    for (int i = 0; i<n; i++) a[s[i]-'0']++;
+    a[7]+=a[9];
+    a[2]+=a[9];
+    a[3]+=2*a[9];
+    a[9] = 0;
+
+    a[7]+=a[8];
+    a[2]+=3*a[8];
+    a[8] = 0;
+
+    a[5]+=a[6];
+    a[3]+=a[6];
+    a[6]= 0;
+
+    a[3]+=a[4];
+    a[2]+=2*a[4];
+    a[4]= 0;
+
+    for (int i = 9; i>=2; i--)
+    {
+        for (int j = 0; j<a[i]; j++) cout<<i;
+    }
+}
