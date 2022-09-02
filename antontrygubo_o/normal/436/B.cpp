79f16edfd19b4621a2f42f2d6cd8c5689db3f020
ishan dutta <ishandutta2007@gmail.com>
2022-09-02 21:02:38 +0530

    antontrygubo_o

---
+++ antontrygubo_o/normal/436/B.cpp
@@ -0,0 +1,124 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define _USE_MATH_DEFINES
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <bits/stdc++.h>
+/*#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")*/
+
+using namespace __gnu_pbds;
+using namespace std;
+
+#define ll long long
+#define ld long double
+#define mp make_pair
+ 
+typedef long double DOUBLE;
+typedef complex<DOUBLE> COMPLEX;
+typedef vector<DOUBLE> VD;
+typedef vector<COMPLEX> VC;
+
+typedef tree<
+int,
+null_type,
+less<int>,
+rb_tree_tag,
+tree_order_statistics_node_update>
+ordered_set;
+
+ 
+void print(auto &a)
+{
+    for (auto s: a) cout<<s<<' ';
+    cout<<endl;
+}
+ 
+void get(vector<auto> &a)
+{
+    for (int i = 0; i<(int)a.size(); i++) cin>>a[i];
+}
+ 
+const int p = 1e9 + 7;
+ 
+ 
+int mul(int a, int b) {
+    return (1LL * a%p * b%p) % p;
+}
+ 
+int add(int a, int b) {
+    int s = (a+b);
+    s = s%p;
+    if (s<0) s+=p; 
+    return s;
+}
+ 
+int sub(int a, int b) {
+    int s = (a-b);
+    s = s%p;
+    if (s<0) s+=p; 
+    return s;
+}
+ 
+int po(int a, int deg)
+{
+    if (deg==0) return 1;
+    if (deg%2==1) return mul(a, po(a, deg-1));
+    int t = po(a, deg/2);
+    return mul(t, t);
+}
+ 
+int inv(int n)
+{
+    return po(n, p-2);
+}
+ 
+vector<int> Z(vector<int> s)
+{
+int n = s.size();
+vector<int> z(n);
+int L = 0, R = 0;
+for (int i = 1; i < n; i++) {
+  if (i > R) {
+    L = R = i;
+    while (R < n && s[R-L] == s[R]) R++;
+    z[i] = R-L; R--;
+  } else {
+    int k = i-L;
+    if (z[k] < R-i+1) z[i] = z[k];
+    else {
+      L = i;
+      while (R < n && s[R-L] == s[R]) R++;
+      z[i] = R-L; R--;
+    }
+  }
+}
+return z;
+}
+
+mt19937 rnd(time(0));
+
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int n, m, x;
+    cin>>n>>m>>x;
+    vector<int> a(m);
+    string s;
+    for (int i = 0; i<n; i++)
+    {
+        cin>>s;
+        for (int j = 0; j<m; j++) if (s[j]!='.')
+        {
+            if (s[j]=='U') if (i%2==0) a[j]++;
+            if (s[j]=='D') continue;
+            if (s[j]=='L') if (j>=i) a[j-i]++;
+            if (s[j]=='R') if (j+i<m) a[j+i]++;
+        }
+    }
+    print(a);
+
+}
