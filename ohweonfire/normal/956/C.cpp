69376422c4a3d5d9affb9cd65a65f4a5525de3b5
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 22:55:20 +0530

    ohweonfire

---
+++ ohweonfire/normal/956/C.cpp
@@ -0,0 +1,66 @@
+// This amazing code is by Eric Sunli Chen.
+#include <algorithm>
+#include <bitset>
+#include <cmath>
+#include <cstring>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <cassert>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <string>
+#include <utility>
+#include <vector>
+using namespace std;
+template<typename T> bool get_int(T &x)
+{
+	char t=getchar();
+	bool neg=false;
+	x=0;
+	for(; (t>'9'||t<'0')&&t!='-'&&t!=EOF; t=getchar());
+	if(t=='-')neg=true,t=getchar();if(t==EOF)return false;
+	for(; t<='9'&&t>='0'; t=getchar())x=x*10+t-'0';
+	if(neg)x=-x;return true;
+}
+template<typename T> void print_int(T x)
+{
+	if(x<0)putchar('-'),x=-x;
+	short a[20]= {},sz=0;
+	while(x>0)a[sz++]=x%10,x/=10;
+	if(sz==0)putchar('0');
+	for(int i=sz-1; i>=0; i--)putchar('0'+a[i]);
+}
+#define ff first
+#define ss second
+#define pb push_back
+#define mp make_pair
+#define get1(a) get_int(a)
+#define get2(a,b) (get1(a)&&get1(b))
+#define get3(a,b,c) (get1(a)&&get2(b,c))
+#define printendl(a) print_int(a),puts("")
+typedef long long LL;
+typedef unsigned long long uLL;
+typedef pair<int,int> pii;
+const int inf=0x3f3f3f3f;
+const LL Linf=1ll<<61;
+const double pi=acos(-1.0);
+
+int n,a[100111],suf[100111];
+int main()
+{
+	get1(n);
+	for(int i=1;i<=n;i++)get1(a[i]);n--;for(int i=1;i<=n;i++)a[i]=a[i+1];
+	suf[n+1]=-inf;
+	for(int i=n;i>=1;i--)suf[i]=max(suf[i+1],a[i]-i);
+	int cur=1;LL ans=0;
+	for(int i=1;i<=n;i++)
+	{
+		cur=max(cur,a[i]);cur=max(cur,suf[i]+i+1);
+		ans+=cur-a[i]-1;
+	}
+	printendl(ans);
+	return 0;
+}
