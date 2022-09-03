845fae0017b0daf3211d0814ba075c02066e6e7f
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 09:42:47 +0530

    antontrygubo_o

---
+++ antontrygubo_o/normal/1434/C.cpp
@@ -0,0 +1,165 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#pragma GCC target ("avx2")
+#pragma GCC optimization ("O3")
+#pragma GCC optimization ("unroll-loops")
+
+using ll = long long;
+using ld = long double;
+
+
+
+#define mp make_pair
+/*
+const int p = 998244353;
+
+int mul(int a, int b) {
+    return (1LL * a * b) % p;
+}
+
+int add(int a, int b) {
+    int s = (a+b);
+    if (s>=p) s-=p;
+    return s;
+}
+
+int sub(int a, int b) {
+    int s = (a+p-b);
+    if (s>=p) s-=p;
+    return s;
+}
+
+int po(int a, int deg)
+{
+    if (deg==0) return 1;
+    if (deg%2==1) return mul(a, po(a, deg-1));
+    int t = po(a, deg/2);
+    return mul(t, t);
+}
+
+int inv(int n)
+{
+    return po(n, p-2);
+}*/
+
+
+mt19937 rnd(time(0));
+
+/*
+const int N = 1200000;
+
+vector<int> facs(N), invfacs(N);
+
+void init()
+{
+    facs[0] = 1;
+    for (int i = 1; i<N; i++) facs[i] = mul(facs[i-1], i);
+    invfacs[N-1] = inv(facs[N-1]);
+    for (int i = N-2; i>=0; i--) invfacs[i] = mul(invfacs[i+1], i+1);
+}
+
+int C(int n, int k)
+{
+    if (n<k) return 0;
+    if (n<0 || k<0) return 0;
+    return mul(facs[n], mul(invfacs[k], invfacs[n-k]));
+}
+
+*/
+
+
+/*struct DSU
+{
+    vector<int> sz;
+    vector<int> parent;
+
+    void make_set(int v) {
+        parent[v] = v;
+        sz[v] = 1;
+    }
+
+    int find_set(int v) {
+        if (v == parent[v])
+            return v;
+        return parent[v] = find_set(parent[v]);
+    }
+
+    void union_sets(int a, int b) {
+        a = find_set(a);
+        b = find_set(b);
+        if (a != b) {
+            if (sz[a] < sz[b])
+                swap(a, b);
+            parent[b] = a;
+            sz[a] += sz[b];
+        }
+    }
+
+    DSU (int n)
+    {
+        parent.resize(n);
+        sz.resize(n);
+        for (int i = 0; i<n; i++) make_set(i);
+    }
+};*/
+
+
+ll a, b, c, d;
+
+ll find(ll op)
+{
+    ll res = -(op+1)*a;
+    ll tm = op*d;
+
+    //cout<<"res: "<<res<<endl;
+
+    if (tm<=c) res+=( op*tm - d * op*(op-1)/2)*b;
+    else
+    {
+        ll free = (tm - c)/d;
+        //cout<<"free: "<<free<<endl;
+        res+=(free+1)*b*c;
+        res+=(tm*(op-free) - d * (op*(op+1)/2 - free*(free+1)/2))*b;
+    }
+    return -res;
+}
+
+void solve()
+{
+    cin>>a>>b>>c>>d;
+    if (a>b*c) {cout<<-1<<'\n'; return;}
+
+    //cout<<find(1)<<endl;
+
+    //for (int i = 0; i<=10; i++) cout<<i<<": "<<find(i)<<endl;
+
+    if (find(0)>=find(1)) {cout<<find(0)<<'\n'; return;}
+
+    int L = 0;
+    int R = 1e6 + 10;
+    while (R-L>1)
+    {
+        int mid = (L+R)/2;
+        if (find(mid)<find(mid+1)) L = mid;
+        else R = mid;
+    }
+    cout<<find(R)<<'\n';
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(nullptr);
+
+    int t;
+    cin>>t;
+    while (t--) solve();
+
+}
+
+/*
+1
+1 2 3 4
+ */
