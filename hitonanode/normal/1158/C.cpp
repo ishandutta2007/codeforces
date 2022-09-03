9de547db0e92e48d7c9e310894d53b56986ac5ab
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 17:03:23 +0530

    hitonanode

---
+++ hitonanode/normal/1158/C.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lint = long long int;
+using pint = pair<int, int>;
+using plint = pair<lint, lint>;
+struct fast_ios { fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; } fast_ios_;
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+template<typename T> istream &operator>>(istream &is, vector<T> &vec){ for (auto &v : vec) is >> v; return is; }
+template<typename T> ostream &operator<<(ostream &os, const vector<T> &vec){ os << "["; for (auto v : vec) os << v << ","; os << "]"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const deque<T> &vec){ os << "deq["; for (auto v : vec) os << v << ","; os << "]"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const set<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const unordered_set<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const multiset<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const unordered_multiset<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa){ os << "(" << pa.first << "," << pa.second << ")"; return os; }
+template<typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp){ os << "{"; for (auto v : mp) os << v.first << "=>" << v.second << ","; os << "}"; return os; }
+template<typename TK, typename TV> ostream &operator<<(ostream &os, const unordered_map<TK, TV> &mp){ os << "{"; for (auto v : mp) os << v.first << "=>" << v.second << ","; os << "}"; return os; }
+template<typename T> void ndarray(vector<T> &vec, int len) { vec.resize(len); }
+template<typename T, typename... Args> void ndarray(vector<T> &vec, int len, Args... args) { vec.resize(len); for (auto &v : vec) ndarray(v, args...); }
+template<typename T> bool mmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template<typename T> bool mmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template<typename T1, typename T2> pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first + r.first, l.second + r.second); }
+template<typename T1, typename T2> pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first - r.first, l.second - r.second); }
+#define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ") " << __FILE__ << endl;
+#define FI first
+#define SE second
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((lint)(x).size())
+#define POW2(n) (1LL << (n))
+
+int TC;
+
+vector<int> solve(int N, vector<int> &vec)
+{
+    vector<pint> ret(N);
+    REP(i, N) ret[i].first = -i, ret[i].second = i;
+    set<int> upd;
+    REP(i, N) if (vec[i] > 0)
+    {
+        if (upd.upper_bound(i) != upd.lower_bound(vec[i] - 1) and upd.upper_bound(i) != upd.end()) return vector<int>{-2};
+        upd.insert(vec[i] - 1);
+        if (vec[i] == N + 1) continue;
+        mmax(ret[vec[i] - 1].first, ret[i].first);
+    }
+
+    sort(ALL(ret));
+    vector<int> ans(N);
+    REP(i, N) ans[ret[i].second] = i;
+    return ans;
+}
+
+int main()
+{
+    cin >> TC;
+    REP(_, TC)
+    {
+        int N;
+        cin >> N;
+        vector<int> vec(N);
+        cin >> vec;
+        vector<int> ret = solve(N, vec);
+        for (auto v : ret) printf("%d ", v + 1);
+        puts("");
+    }
+}
