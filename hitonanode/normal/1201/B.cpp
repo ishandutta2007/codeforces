bd412bb1efeedad9a9e4af9275aa28d55195ea5a
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 16:51:36 +0530

    hitonanode

---
+++ hitonanode/normal/1201/B.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lint = long long int;
+using pint = pair<int, int>;
+using plint = pair<lint, lint>;
+struct fast_ios { fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; } fast_ios_;
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((lint)(x).size())
+#define POW2(n) (1LL << (n))
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+template<typename T> istream &operator>>(istream &is, vector<T> &vec){ for (auto &v : vec) is >> v; return is; }
+
+
+
+int main()
+{
+    int N;
+    cin >> N;
+    vector<lint> A(N);
+    cin >> A;
+    sort(ALL(A));
+    lint tot = accumulate(ALL(A), 0LL);
+    if (A.back() * 2 > tot or tot % 2) puts("NO");
+    else puts("YES");
+}
