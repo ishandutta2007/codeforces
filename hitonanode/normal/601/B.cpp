d37ff8b1c7b53dbd3dfdc6f9cb4f32694b91a535
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 10:51:18 +0530

    hitonanode

---
+++ hitonanode/normal/601/B.cpp
@@ -0,0 +1,76 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lint = long long;
+using pint = pair<int, int>;
+using plint = pair<lint, lint>;
+struct fast_ios { fast_ios(){ cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; } fast_ios_;
+#define ALL(x) (x).begin(), (x).end()
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+template <typename T> void ndarray(vector<T> &vec, int len) { vec.resize(len); }
+template <typename T, typename... Args> void ndarray(vector<T> &vec, int len, Args... args) { vec.resize(len); for (auto &v : vec) ndarray(v, args...); }
+template <typename V, typename T> void ndfill(V &x, const T &val) { x = val; }
+template <typename V, typename T> void ndfill(vector<V> &vec, const T &val) { for (auto &v : vec) ndfill(v, val); }
+template <typename T> bool chmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template <typename T> bool chmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template <typename T1, typename T2> pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first + r.first, l.second + r.second); }
+template <typename T1, typename T2> pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first - r.first, l.second - r.second); }
+template <typename T> vector<T> srtunq(vector<T> vec) { sort(vec.begin(), vec.end()), vec.erase(unique(vec.begin(), vec.end()), vec.end()); return vec; }
+template <typename T> istream &operator>>(istream &is, vector<T> &vec) { for (auto &v : vec) is >> v; return is; }
+template <typename T> ostream &operator<<(ostream &os, const vector<T> &vec) { os << '['; for (auto v : vec) os << v << ','; os << ']'; return os; }
+#if __cplusplus >= 201703L
+template <typename... T> istream &operator>>(istream &is, tuple<T...> &tpl) { std::apply([&is](auto &&... args) { ((is >> args), ...);}, tpl); return is; }
+template <typename... T> ostream &operator<<(ostream &os, const tuple<T...> &tpl) { std::apply([&os](auto &&... args) { ((os << args << ','), ...);}, tpl); return os; }
+#endif
+template <typename T> ostream &operator<<(ostream &os, const deque<T> &vec) { os << "deq["; for (auto v : vec) os << v << ','; os << ']'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const set<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const unordered_set<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T> ostream &operator<<(ostream &os, const unordered_multiset<T> &vec) { os << '{'; for (auto v : vec) os << v << ','; os << '}'; return os; }
+template <typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa) { os << '(' << pa.first << ',' << pa.second << ')'; return os; }
+template <typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+template <typename TK, typename TV> ostream &operator<<(ostream &os, const unordered_map<TK, TV> &mp) { os << '{'; for (auto v : mp) os << v.first << "=>" << v.second << ','; os << '}'; return os; }
+#ifdef HITONANODE_LOCAL
+#define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ") " << __FILE__ << endl
+#else
+#define dbg(x)
+#endif
+
+int main()
+{
+    int N, Q;
+    cin >> N >> Q;
+    vector<int> A(N);
+    cin >> A;
+    vector<pint> d2i;
+    REP(i, N - 1) d2i.emplace_back(abs(A[i + 1] - A[i]), i);
+    sort(d2i.rbegin(), d2i.rend());
+
+    vector<lint> ret(Q);
+    vector<int> L(Q), R(Q);
+    REP(q, Q)
+    {
+        cin >> L[q] >> R[q];
+        L[q]--, R[q]--;
+    }
+
+    set<int> se;
+    se.insert(-1);
+    se.insert(N - 1);
+    for (auto [d, i] : d2i)
+    {
+        se.insert(i);
+        int prv = *prev(se.lower_bound(i));
+        int nxt = *se.upper_bound(i);
+        dbg((vector<int>{d, i, prv, nxt}));
+        REP(q, Q) if (L[q] <= i and i < R[q])
+        {
+            lint nb_prv = i - max(prv, L[q] - 1);
+            lint nb_nxt = min(nxt, R[q]) - i;
+            ret[q] += nb_prv * nb_nxt * d;
+        }
+    }
+    for (auto x : ret) cout << x << '\n';
+}
