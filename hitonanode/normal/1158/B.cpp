cd34146d012c5910c016c4f05ea0c9ede50a3006
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 17:07:51 +0530

    hitonanode

---
+++ hitonanode/normal/1158/B.cpp
@@ -0,0 +1,95 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lint = long long int;
+using pint = pair<int, int>;
+using plint = pair<lint, lint>;
+struct fast_ios { fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; } fast_ios_;
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+template<typename T> istream &operator>>(istream &is, vector<T> &vec){ for (auto &v : vec) is >> v; return is; }
+template<typename T> ostream &operator<<(ostream &os, const vector<T> &vec){ os << "["; for (auto v : vec) os << v << ","; os << "]"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const deque<T> &vec){ os << "deq["; for (auto v : vec) os << v << ","; os << "]"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const set<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const unordered_set<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const multiset<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T> ostream &operator<<(ostream &os, const unordered_multiset<T> &vec){ os << "{"; for (auto v : vec) os << v << ","; os << "}"; return os; }
+template<typename T1, typename T2> ostream &operator<<(ostream &os, const pair<T1, T2> &pa){ os << "(" << pa.first << "," << pa.second << ")"; return os; }
+template<typename TK, typename TV> ostream &operator<<(ostream &os, const map<TK, TV> &mp){ os << "{"; for (auto v : mp) os << v.first << "=>" << v.second << ","; os << "}"; return os; }
+template<typename TK, typename TV> ostream &operator<<(ostream &os, const unordered_map<TK, TV> &mp){ os << "{"; for (auto v : mp) os << v.first << "=>" << v.second << ","; os << "}"; return os; }
+template<typename T> void ndarray(vector<T> &vec, int len) { vec.resize(len); }
+template<typename T, typename... Args> void ndarray(vector<T> &vec, int len, Args... args) { vec.resize(len); for (auto &v : vec) ndarray(v, args...); }
+template<typename T> bool mmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template<typename T> bool mmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template<typename T1, typename T2> pair<T1, T2> operator+(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first + r.first, l.second + r.second); }
+template<typename T1, typename T2> pair<T1, T2> operator-(const pair<T1, T2> &l, const pair<T1, T2> &r) { return make_pair(l.first - r.first, l.second - r.second); }
+#define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ") " << __FILE__ << endl;
+#define FI first
+#define SE second
+#define ALL(x) (x).begin(), (x).end()
+#define SZ(x) ((lint)(x).size())
+#define POW2(n) (1LL << (n))
+
+
+int solve(int N, int K)
+{
+    REP(S, 1LL << N)
+    {
+        bool flg = true;
+        FOR(k, 1, K)
+        {
+            map<int, int> ma;
+            REP(d, N + 1 - k)
+            {
+                ma[(S >> d) & (POW2(k) - 1)]++;
+            }
+            for (auto pa : ma) if (pa.second == 1) flg = false;
+            if (!flg) break;
+        }
+        if (!flg) continue;
+        map<int, int> ma;
+        REP(d, N + 1 - K) ma[(S >> d) & (POW2(K) - 1)]++;
+        for (auto pa : ma) if (pa.second == 1)
+        {
+            return S;
+        }
+    }
+}
+
+int main()
+{
+    int N, K;
+    cin >> N >> K;
+    if (N <= 16)
+    {
+        int S = solve(N, K);
+        string ret;
+        REP(d, N) ret += (char)('0' + ((S >> d) & 1));
+        cout << ret << endl;
+        return 0;
+    }
+    string S;
+    REP(i, N) S += "0";
+    int t = (N - K) / 2;
+
+    if (K <= 2)
+    {
+        REP(i, K) S[i] = '1';
+    }
+    else if ((t + 1) * 3 <= N)
+    {
+        int pos = t;
+        while (pos < N)
+        {
+            S[pos] = '1';
+            pos += t + 1;
+        }
+    }
+    else
+    {
+        S[K - 2] = S[2 * K - 3] = '1';
+    }
+    cout << S << endl;
+    return 0;
+}
