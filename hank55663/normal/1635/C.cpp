da9a6872b6fbc98a0e255bc80e1bc7ec3c8322df
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 01:07:16 +0530

    hank55663

---
+++ hank55663/normal/1635/C.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+#define pb push_back
+#define x first
+#define y second
+#define pdd pair<double,double>
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define LL long long
+#define ULL unsigned long long
+#define sqr(x) ((x)*(x))
+#define pi acos(-1)
+#define MEM(x) memset(x,0,sizeof(x))
+#define MEMS(x) memset(x,-1,sizeof(x))
+using namespace std;
+void solve(){
+    int n;
+    scanf("%d",&n);
+    int a[200005];
+    for(int i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int ok=1;
+    for(int i = 0;i<n;i++){
+        if(a[i]>a[i+1])ok=0;
+    }
+    if(ok){
+        printf("0\n");
+    }
+    else{
+        if(a[n-2]>a[n-1]){
+            printf("-1\n");
+            return;
+        }
+        if(a[n-1]<0){
+            printf("-1\n");
+            return;
+        }
+        printf("%d\n",n-2);
+        for(int i = 1;i<=n-2;i++){
+            printf("%d %d %d\n",i,n-1,n);
+        }
+    }
+}
+int main(){
+    int t=1;0000;
+    scanf("%d",&t);
+    for(int i = 1;i<=t;i++){
+        solve();
+    }
+    return 0;
+}
