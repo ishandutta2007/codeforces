13e4685a46b2eea54cb06e7c2e853f77e4113471
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 01:28:22 +0530

    hank55663

---
+++ hank55663/normal/1579/E2.cpp
@@ -0,0 +1,63 @@
+#include<bits/stdc++.h>
+#pragma optimize(Ofast)
+#define pb push_back
+#define x first
+#define y second
+#define pdd pair<double,double>
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define mp make_pair
+#define LL long long
+#define ULL unsigned long long
+#define sqr(x) ((x)*(x))
+#define pi acos(-1)
+#define MEM(x) memset(x,0,sizeof(x))
+#define MEMS(x) memset(x,-1,sizeof(x))
+using namespace std;
+#define MXN 1005
+int S[200005];
+void add(int x,int k){
+    for(int i = x;i<200005;i+=i&-i){
+        S[i]+=k;
+    }
+}
+int query(int x){
+    int res=0;
+    for(int i=x;i>0;i-=i&-i){
+        res+=S[i];
+    }
+    return res;
+}
+void solve(){
+    int n;
+    scanf("%d",&n);
+    int a[200005];
+    map<int,int> m;
+    for(int i = 0;i<n;i++){
+        scanf("%d",&a[i]);
+        m[a[i]];
+    }
+    int index=0;
+    for(auto &it:m)it.y=++index;
+    LL ans=0;
+    for(int i = 0;i<n;i++){
+        int aa=query(m[a[i]]-1),bb=i-query(m[a[i]]);
+        ans+=min(aa,bb);
+        add(m[a[i]],1);
+    }
+    for(int i = 0;i<n;i++)add(m[a[i]],-1);
+    printf("%lld\n",ans);
+}
+int main(){
+
+    int t=1;0000;
+    scanf("%d",&t);
+    for(int i = 1;i<=t;i++){
+        //cerr<<i<<endl;
+        solve();
+    }
+    return 0;
+}
+/*
+
+*/
