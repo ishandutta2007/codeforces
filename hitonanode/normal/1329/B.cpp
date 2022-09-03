70931357cae2e3a58a848a7ee266d2fc5e3b424d
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 16:14:25 +0530

    hitonanode

---
+++ hitonanode/normal/1329/B.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+using lint = long long int;
+struct fast_ios { fast_ios(){ cin.tie(0); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; } fast_ios_;
+#define ALL(x) (x).begin(), (x).end()
+#define FOR(i, begin, end) for(int i=(begin),i##_end_=(end);i<i##_end_;i++)
+#define IFOR(i, begin, end) for(int i=(end)-1,i##_begin_=(begin);i>=i##_begin_;i--)
+#define REP(i, n) FOR(i,0,n)
+#define IREP(i, n) IFOR(i,0,n)
+template<typename T> bool chmax(T &m, const T q) { if (m < q) {m = q; return true;} else return false; }
+template<typename T> bool chmin(T &m, const T q) { if (m > q) {m = q; return true;} else return false; }
+template<typename T> istream &operator>>(istream &is, vector<T> &vec){ for (auto &v : vec) is >> v; return is; }
+template<typename T> ostream &operator<<(ostream &os, const vector<T> &vec){ os << "["; for (auto v : vec) os << v << ","; os << "]"; return os; }
+#define dbg(x) cerr << #x << " = " << (x) << " (L" << __LINE__ << ") " << __FILE__ << endl;
+
+
+lint solve()
+{
+    lint D, M;
+    cin >> D >> M;
+    lint ret = 1;
+    int digit = 1;
+    while (true) {
+        lint hi = min((1LL << digit) - 1, D);
+        lint lo = 1 << (digit - 1);
+        lint nb = hi - lo + 1;
+        if (nb <= 0) break;
+        ret = ret * (nb + 1) % M;
+        digit++;
+    }
+    return (ret + M - 1) % M;
+}
+
+int main()
+{
+    int T;
+    cin >> T;
+    while (T--) cout << solve() << "\n";
+}
