5ba50feed18b3a7904a25551cce4266293293b1f
ishan dutta <ishandutta2007@gmail.com>
2022-09-03 17:16:33 +0530

    isaf27

---
+++ isaf27/normal/1552/B.cpp
@@ -0,0 +1,99 @@
+#pragma GCC optimize("Ofast")
+/*
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//defines
+typedef long long ll;
+typedef long double ld;
+#define TIME clock() * 1.0 / CLOCKS_PER_SEC
+#define y0 _y0
+#define kill _kill
+
+//permanent constants
+const ld pi = acos(-1.0);
+const int day[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+const int digarr[10] = {6, 2, 5, 5, 4, 5, 6, 3, 7, 6};
+const int dx[4] = {0, 1, 0, -1};
+const int dy[4] = {1, 0, -1, 0};
+const int dxo[8] = {-1, -1, -1, 0, 1, 1, 1, 0};
+const int dyo[8] = {-1, 0, 1, 1, 1, 0, -1, -1};
+const int alf = 26;
+const ld eps = 1e-10;
+const ll INF = (ll)(4e18 + 239);
+const int BIG = (int)(1e9 + 239);
+const int MOD = 1e9 + 7;
+const ll MOD2 = (ll)MOD * (ll)MOD;
+
+//random
+#ifdef ONPC
+mt19937_64 rnd(239);
+#else
+mt19937_64 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+//constants
+const int M = (int)(5e4 + 239);
+const int N = (int)(2e3 + 10);
+const int L = 21;
+const int T = (1 << 18) + 239;
+const int B = 510;
+const int X = 1e4 + 239;
+
+int n;
+int r[M][5];
+
+bool cmp(int i, int j) {
+    int cnt = 0;
+    for (int x = 0; x < 5; x++) {
+        cnt += (r[i][x] < r[j][x]);
+    }
+    return cnt >= 3;
+}
+
+int nmin(int i, int j) {
+    if (cmp(i, j)) {
+        return i;
+    }
+    return j;
+}
+
+void solve() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < 5; j++) {
+            cin >> r[i][j];
+            r[i][j]--;
+        }
+    }
+    int ans = 0;
+    for (int i = 1; i < n; i++) {
+        ans = nmin(ans, i);
+    }
+    for (int i = 0; i < n; i++) {
+        if (i != ans && cmp(i, ans)) {
+            cout << "-1\n";
+            return;
+        }
+    }
+    cout << ans + 1 << "\n";
+}
+
+int32_t main() {
+#ifdef ONPC
+    freopen("input", "r", stdin);
+#endif
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
