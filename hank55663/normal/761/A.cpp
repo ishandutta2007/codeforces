65da6b87f8ab09e4b261df2354962f2a03c55f56
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 04:23:33 +0530

    hank55663

---
+++ hank55663/normal/761/A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define LL long long
+#define mp make_pair
+#define pb push_back
+#define pii pair<int,int>
+#define pll pair<LL,LL>
+#define x first
+#define y second
+#define pi acos(-1)
+#define sqr(x) ((x)*(x))
+#define pdd pair<double,double>
+#define MEMS(x) memset(x,-1,sizeof(x))
+#define MEM(x) memset(x,0,sizeof(x))
+#define MXN 10005
+#define less Less
+int main(){
+    int a,b;
+    scanf("%d %d",&a,&b);
+    if(abs(a-b)<=1&&(a!=0||b!=0))printf("YES\n");
+    else printf("NO\n");
+}
