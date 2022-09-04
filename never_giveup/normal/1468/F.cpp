220e74651518d1269ab50f05149f6267f9df09f5
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 22:12:04 +0530

    never_giveup

---
+++ never_giveup/normal/1468/F.cpp
@@ -0,0 +1,91 @@
+/*
+    author:  Maksim1744
+    created: 25.12.2020 14:50:17
+*/
+
+#include "bits/stdc++.h"
+
+using namespace std;
+
+#define ll   long long
+#define ld   long double
+
+#define mp   make_pair
+#define pb   push_back
+#define eb   emplace_back
+
+#define sum(a)     ( accumulate ((a).begin(), (a).end(), 0ll))
+#define mine(a)    (*min_element((a).begin(), (a).end()))
+#define maxe(a)    (*max_element((a).begin(), (a).end()))
+#define mini(a)    ( min_element((a).begin(), (a).end()) - (a).begin())
+#define maxi(a)    ( max_element((a).begin(), (a).end()) - (a).begin())
+#define lowb(a, x) ( lower_bound((a).begin(), (a).end(), (x)) - (a).begin())
+#define uppb(a, x) ( upper_bound((a).begin(), (a).end(), (x)) - (a).begin())
+
+template<typename T>             vector<T>& operator--            (vector<T>& v){for (auto& i : v) --i;            return  v;}
+template<typename T>             vector<T>& operator++            (vector<T>& v){for (auto& i : v) ++i;            return  v;}
+template<typename T>             istream& operator>>(istream& is,  vector<T>& v){for (auto& i : v) is >> i;        return is;}
+template<typename T>             ostream& operator<<(ostream& os,  vector<T>& v){for (auto& i : v) os << i << ' '; return os;}
+template<typename T, typename U> istream& operator>>(istream& is, pair<T, U>& p){is >> p.first >> p.second;        return is;}
+template<typename T, typename U> ostream& operator<<(ostream& os, pair<T, U>& p){os << p.first << ' ' << p.second; return os;}
+template<typename T, typename U> pair<T,U> operator-(pair<T,U> a, pair<T,U> b){return mp(a.first-b.first, a.second-b.second);}
+template<typename T, typename U> pair<T,U> operator+(pair<T,U> a, pair<T,U> b){return mp(a.first+b.first, a.second+b.second);}
+template<typename T, typename U> void umin(T& a, U b){if (a > b) a = b;}
+template<typename T, typename U> void umax(T& a, U b){if (a < b) a = b;}
+
+#ifdef HOME
+#define TAG_LENGTH       25
+#define LR_LEFT          left
+#define LR_RIGHT         right
+#define LR_VALUE         value
+#define LR_SECOND_VALUE  mn
+#include "C:/C++ libs/print.cpp"
+#else
+#define showl            42;
+#define shows            42;
+#define show(...)        42;
+#define showm(...)       42;
+#define showt(...)       42;
+#define printTree(...)   42;
+#define printGraph(...)  42;
+#define printLRTree(...) 42;
+#define printMatrix(...) 42;
+#define printWGraph(...) 42;
+#define debug     if (false)
+#endif
+
+void test_case(int test) {
+    int n;
+    cin >> n;
+    map<pair<ll, ll>, ll> cnt;
+    for (int i = 0; i < n; ++i) {
+        ll a, b, c, d;
+        cin >> a >> b >> c >> d;
+        ll x = c - a;
+        ll y = d - b;
+        ll g = gcd(abs(x), abs(y));
+        x /= g;
+        y /= g;
+        cnt[{x, y}]++;
+    }
+    ll ans = 0;
+    for (auto [a, b] : cnt) {
+        pair<ll, ll> p{-a.first, -a.second};
+        if (cnt.count(p)) {
+            ans += b * cnt[p];
+        }
+    }
+    cout << ans / 2 << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+    int T;
+    cin >> T;
+    for (int test = 1; test <= T; ++test) {
+        test_case(test);
+    }
+
+    return 0;
+}
