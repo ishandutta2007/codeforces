84beeb73f928138e9557e088392cc1855af1b3dd
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 02:59:35 +0530

    noimi

---
+++ noimi/normal/1076/B.cpp
@@ -0,0 +1,75 @@
+#include "bits/stdc++.h"
+#define ll long long
+#define rep2(i,a,b) for(int i=a;i<=b;++i)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define tii tuple<int,int,int>
+#define pq priority_queue<int>
+#define pqg priority_queue<int,vector<int>,greater<int>>
+#define pb push_back
+#define edge(v,a,b) v[a].pb(b);v[b].pb(a);
+ll MOD=998244353;
+#define INF 1e9+7
+#define N 310000
+#define MAX_V 400010
+#define vec vector<int>
+using namespace std;
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        // http://xorshift.di.unimi.it/splitmix64.c
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
+#define umap unordered_map<ll,int,custom_hash>
+struct pair_hash
+{
+    template<class T1,class T2>
+    size_t operator() (const pair<T1,T2> &p)const{
+        return hash<T1>()(p.first)^hash<T2>()(p.second);
+    }
+};
+
+ll mycount=0;
+void calc(ll x){
+    if(x==0)return;
+    for(ll i=2;i*i<=x;i++){
+        if(x%i==0){
+            mycount+=(x/i-i+1);
+            cout<<x<<i;
+            return;
+            calc(i*(i-1));
+            return;
+        }
+    }
+    mycount++;
+    return;
+}
+
+main(){
+    ll n;
+    cin>>n;
+    if(n%2==0){
+        cout<<n/2;
+    }
+    else{
+        ll d=-1;
+        for(ll i=2;i*i<=n;i++){
+            if(n%i==0){
+                d=i;break;
+            }
+        }
+        if(d==-1)cout<<1;
+        else{
+            cout<<1+(n-d)/2;
+        }
+    }
+}
