c1a69b435e8792f615697e60a01a3701aa6b64af
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 02:56:03 +0530

    noimi

---
+++ noimi/normal/1096/B.cpp
@@ -0,0 +1,69 @@
+#include <stdio.h>  
+#include <algorithm>  
+#include <assert.h>
+#include <bitset>
+#include <cmath>  
+#include <complex>  
+#include <deque>  
+#include <functional>  
+#include <iostream>  
+#include <limits.h>  
+#include <map>  
+#include <math.h>  
+#include <queue>  
+#include <set>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <string>  
+#include <time.h>  
+#include <unordered_map>  
+#include <unordered_set>  
+#include <vector>  
+#define ll long long
+#define rep2(i,a,b) for(ll i=a;i<=b;++i)
+#define rep(i,n) for(int i=0;i<n;i++)
+#define rep3(i,a,b) for(int i=a;i>=b;i--)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define tii tuple<int,int,int>
+#define pq priority_queue<int>
+#define pqg priority_queue<int,vector<int>,greater<int>>
+#define pb push_back
+#define edge(v,a,b) v[a].pb(b);v[b].pb(a);
+#define MAX_V 400010
+#define vec vector<int>
+#define vecll vector<ll>
+#define vecpii vector<pii>
+#define endl "\n"
+#define ALL(c) (c).begin(),(c).end()
+using namespace std;
+int in() {int x;scanf("%d",&x);return x;}
+ll lin() {ll x;scanf("%lld",&x);return x;}
+template<typename A, size_t NNN, typename T>
+void Fill(A (&array)[NNN], const T &val){
+    fill( (T*)array, (T*)(array+NNN), val );
+}
+#define N 100100
+#define INF 1e9+7
+ll MOD=998244353;
+
+ll n;
+main(){
+    n=lin();
+    string s;cin>>s;
+    if(s[0]==s[n-1]){
+        ll l=0;
+        while(s[l]==s[0])l++;
+        ll r=0;
+        while(s[n-1-r]==s[n-1]){
+            r++;
+        }
+        cout<<((l+1)*(r+1)%MOD);
+    }
+    else{
+        int l=0,r=0;
+        while(s[l]==s[0])l++;
+        while(s[n-1-r]==s[n-1])r++;
+        cout<<l+r+1;
+    }
+}
