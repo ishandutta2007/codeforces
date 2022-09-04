deabcd6886d06d9b20bd953073e073fb110c623b
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 00:29:28 +0530

    noimi

---
+++ noimi/normal/403/B.cpp
@@ -0,0 +1,395 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+#pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep2(i, a, b) for(ll i = a; i <= b; ++i)
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for(ll i = a; i >= b; --i)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpll vector<pll>
+#define overload2(_1, _2, name, ...) name
+#define vec(type, name, ...) vector<type> name(__VA_ARGS__)
+#define VEC(type, name, size)                                                                                                                                  \
+    vector<type> name(size);                                                                                                                                   \
+    IN(name)
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define VV(type, name, h, w)                                                                                                                                   \
+    vector<vector<type>> name(h, vector<type>(w));                                                                                                             \
+    IN(name)
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...)                                                                                                                         \
+    vector<vector<vector<vector<type>>>> name(a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define mt make_tuple
+
+#define fi first
+#define se second
+#define all(c) begin(c), end(c)
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+using namespace std;
+constexpr pii dx4[4] = {pii{1, 0}, pii{0, 1}, pii{-1, 0}, pii{0, -1}};
+constexpr pii dx8[8] = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+const string YESNO[2] = {"NO", "YES"};
+const string YesNo[2] = {"No", "Yes"};
+const string yesno[2] = {"no", "yes"};
+void YES(bool t = 1) { cout << YESNO[t] << endl; }
+void Yes(bool t = 1) { cout << YesNo[t] << endl; }
+void yes(bool t = 1) { cout << yesno[t] << endl; }
+template <class T> using vc = vector<T>;
+template <class T> using vvc = vector<vc<T>>;
+template <class T> using vvvc = vector<vvc<T>>;
+template <class T> using vvvvc = vector<vvvc<T>>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define si(c) (int)(c).size()
+#define INT(...)                                                                                                                                               \
+    int __VA_ARGS__;                                                                                                                                           \
+    IN(__VA_ARGS__)
+#define LL(...)                                                                                                                                                \
+    ll __VA_ARGS__;                                                                                                                                            \
+    IN(__VA_ARGS__)
+#define STR(...)                                                                                                                                               \
+    string __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+#define CHR(...)                                                                                                                                               \
+    char __VA_ARGS__;                                                                                                                                          \
+    IN(__VA_ARGS__)
+#define DBL(...)                                                                                                                                               \
+    double __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &);
+template <class T> void scan(vector<T> &a) {
+    for(auto &i : a) scan(i);
+}
+template <class T> void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
+    scan(head);
+    IN(tail...);
+}
+template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
+template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
+vi iota(int n) {
+    vi a(n);
+    iota(all(a), 0);
+    return a;
+}
+template <typename T> vi iota(vector<T> &a, bool greater = false) {
+    vi res(a.size());
+    iota(all(res), 0);
+    sort(all(res), [&](int i, int j) {
+        if(greater) return a[i] > a[j];
+        return a[i] < a[j];
+    });
+    return res;
+}
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+template <class T> T POW(T x, int n) {
+    T res = 1;
+    for(; n; n >>= 1, x *= x)
+        if(n & 1) res *= x;
+    return res;
+}
+vector<pll> factor(ll x) {
+    vector<pll> ans;
+    for(ll i = 2; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.push_back({i, 1});
+            while((x /= i) % i == 0) ans.back().second++;
+        }
+    if(x != 1) ans.push_back({x, 1});
+    return ans;
+}
+template <class T> vector<T> divisor(T x) {
+    vector<T> ans;
+    for(T i = 1; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.pb(i);
+            if(i * i != x) ans.pb(x / i);
+        }
+    return ans;
+}
+template <typename T> void zip(vector<T> &x) {
+    vector<T> y = x;
+    sort(all(y));
+    for(int i = 0; i < x.size(); ++i) { x[i] = lb(y, x[i]); }
+}
+int topbit(signed t) { return t == 0 ? -1 : 31 - __builtin_clz(t); }
+int topbit(ll t) { return t == 0 ? -1 : 63 - __builtin_clzll(t); }
+int lowbit(signed a) { return a == 0 ? 32 : __builtin_ctz(a); }
+int lowbit(ll a) { return a == 0 ? 64 : __builtin_ctzll(a); }
+// int allbit(int n) { return (1 << n) - 1; }
+ll allbit(ll n) { return (1LL << n) - 1; }
+int popcount(signed t) { return __builtin_popcount(t); }
+int popcount(ll t) { return __builtin_popcountll(t); }
+bool ispow2(int i) { return i && (i & -i) == i; }
+
+int in() {
+    int x;
+    cin >> x;
+    return x;
+}
+ll lin() {
+    unsigned long long x;
+    cin >> x;
+    return x;
+}
+
+template <class T> pair<T, T> operator-(const pair<T, T> &x, const pair<T, T> &y) { return pair<T, T>(x.fi - y.fi, x.se - y.se); }
+template <class T> pair<T, T> operator+(const pair<T, T> &x, const pair<T, T> &y) { return pair<T, T>(x.fi + y.fi, x.se + y.se); }
+template <class T> ll operator*(const pair<T, T> &x, const pair<T, T> &y) { return (ll)x.fi * y.fi + (ll)x.se * y.se; }
+
+template <typename T> struct edge {
+    int from, to;
+    T cost;
+    int id;
+    edge(int to, T cost) : from(-1), to(to), cost(cost) {}
+    edge(int from, int to, T cost) : from(from), to(to), cost(cost) {}
+    edge(int from, int to, T cost, int id) : from(from), to(to), cost(cost), id(id) {}
+    edge &operator=(const int &x) {
+        to = x;
+        return *this;
+    }
+    operator int() const { return to; }
+};
+template <typename T> using Edges = vector<edge<T>>;
+
+using Tree = vector<vector<int>>;
+using Graph = vector<vector<int>>;
+template <class T> using Wgraph = vector<vector<edge<T>>>;
+Graph getG(int n, int m = -1, bool directed = false, int margin = 1) {
+    Tree res(n);
+    if(m == -1) m = n - 1;
+    while(m--) {
+        int a, b;
+        cin >> a >> b;
+        a -= margin, b -= margin;
+        res[a].emplace_back(b);
+        if(!directed) res[b].emplace_back(a);
+    }
+    return move(res);
+}
+template <class T> Wgraph<T> getWg(int n, int m = -1, bool directed = false, int margin = 1) {
+    Wgraph<T> res(n);
+    if(m == -1) m = n - 1;
+    while(m--) {
+        int a, b;
+        T c;
+        cin >> a >> b >> c;
+        a -= margin, b -= margin;
+        res[a].emplace_back(b, c);
+        if(!directed) res[b].emplace_back(a, c);
+    }
+    return move(res);
+}
+
+#define i128 __int128_t
+#define ull unsigned long long int
+#define TEST                                                                                                                                                   \
+    INT(testcases);                                                                                                                                            \
+    while(testcases--)
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v))
+            os << *it;
+        else
+            os << " " << *it;
+    }
+    return os;
+}
+template <class T, class S> ostream &operator<<(ostream &os, const pair<T, S> &p) {
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class S, class T> string to_string(pair<S, T> p) { return "(" + to_string(p.first) + "," + to_string(p.second) + ")"; }
+template <class A> string to_string(A v) {
+    if(v.empty()) return "{}";
+    string ret = "{";
+    for(auto &x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+string to_string(string s) { return "\"" + s + "\""; }
+
+void dump() { cerr << endl; }
+template <class Head, class... Tail> void dump(Head head, Tail... tail) {
+    cerr << to_string(head) << " ";
+    dump(tail...);
+}
+#define endl '\n'
+#ifdef _LOCAL
+#undef endl
+#define debug(x)                                                                                                                                               \
+    cout << #x << ": ";                                                                                                                                        \
+    dump(x)
+#else
+#define debug(x)
+#endif
+
+template <typename T> static constexpr T inf = numeric_limits<T>::max() / 2;
+struct Setup_io {
+    Setup_io() {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        cout << fixed << setprecision(15);
+    }
+} setup_io;
+#define drop(s) cout << #s << endl, exit(0)
+#pragma endregion
+
+namespace Factorize {
+struct Random {
+  private:
+    uint32_t a, b, c, d;
+
+  public:
+    Random(uint32_t seed = 517) { set_seed(seed); }
+    void set_seed(uint32_t seed) {
+        a = seed = 1812433253u * (seed ^ (seed >> 30));
+        b = seed = 1812433253u * (seed ^ (seed >> 30)) + 1;
+        c = seed = 1812433253u * (seed ^ (seed >> 30)) + 2;
+        d = seed = 1812433253u * (seed ^ (seed >> 30)) + 3;
+    }
+    uint32_t operator()() {
+        uint32_t t = (a ^ (a << 11));
+        a = b;
+        b = c;
+        c = d;
+        return d = (d ^ (d >> 19)) ^ (t ^ (t >> 8));
+    }
+
+    // [0,MAX)
+    int operator()(int MAX) { return (uint64_t) operator()() * MAX >> 32; }
+
+    // [L,R)
+    uint32_t operator()(int L, int R) { return L + operator()(R - L); }
+
+    // [0,1) 
+    double drnd() { return (double)operator()() / (1LL << 32); }
+
+    //  p  true 
+    bool prob(const double p) { return (uint32_t)(p * (1LL << 32)) > operator()(); }
+
+    bool coin() { return operator()() < (1 << 31); }
+};
+Random rnd;
+using i128 = __int128_t;
+ll get_factor(ll n) {
+    if(n == 3 or n == 5) return 0;
+    ll x, y;
+    ll iter = rnd(100);
+    while(true) {
+        ++iter;
+        x = iter % n;
+        y = (i128(iter) * i128(iter) + 1) % i128(n);
+        while(1) {
+            ll g = gcd(n, abs(x - y));
+            if(g == n) break;
+            if(g > 1) return g;
+            x = (i128(x) * i128(x) + 1) % i128(n);
+            y = (i128(y) * i128(y) + 1) % i128(n);
+            y = (i128(y) * i128(y) + 1) % i128(n);
+        }
+    }
+}
+ll modpow(ll a, ll n, ll mod) {
+    ll res = 1;
+    while(n) {
+        if(n & 1) res = i128(a) * i128(res) % i128(mod);
+        n >>= 1, a = i128(a) * i128(a) % i128(mod);
+    }
+    return res;
+}
+bool miller_rabin_test(ll p, ll s, ll d, ll n) {
+    ll x = modpow(p, d, n);
+    if(x == 1) return true;
+    ll y = n - 1;
+    rep(i, s) {
+        if(x == y) return true;
+        x = i128(x) * i128(x) % i128(n);
+    }
+    return false;
+}
+bool miller_rabin(ll n) {
+    if(n == 1) return false;
+    ll d = n - 1, s = 0;
+    while(~d & 1) d >>= 1, ++s;
+    if(n < 4759123141LL) {
+        for(auto p : {2, 7, 61}) {
+            if(p >= n) break;
+            if(!miller_rabin_test(p, s, d, n)) return false;
+        }
+    } else {
+        for(auto p : {2, 325, 9375, 28178, 450775, 9780504, 1795265022}) {
+            if(p >= n) break;
+            if(!miller_rabin_test(p, s, d, n)) return false;
+        }
+    }
+    return true;
+}
+
+map<ll, int> factorize(ll n) {
+    map<ll, int> res;
+    while(~n & 1) res[2]++, n >>= 1;
+    queue<ll> que;
+    que.emplace(n);
+    while(!que.empty()) {
+        ll now = que.front();
+        que.pop();
+        if(now == 1) continue;
+        if(miller_rabin(now)) {
+            res[now]++;
+            continue;
+        }
+        ll g = get_factor(now);
+        que.emplace(g);
+        que.emplace(now / g);
+    }
+    return res;
+}
+
+bool isPrime(ll n) { return miller_rabin(n); }
+} // namespace Factorize
+using namespace Factorize;
+
+int main() {
+    INT(n, m);
+    VEC(int, a, n);
+    VEC(int, b, m);
+    vi r(n + 1);
+    rep(i, n) r[i + 1] = gcd(r[i], a[i]);
+    int f = 1;
+
+    auto F = [&](int x) {
+        int c = 0;
+        for(auto e : b) {
+            while(x % e == 0) x /= e, c--;
+        }
+        for(auto [_, y] : factorize(x)) { c += y; }
+        return c;
+    };
+    rep3(j, n, 1) {
+        r[j] /= f;
+        int c = F(r[j]);
+        if(c <= 0) { f *= r[j]; }
+        a[j - 1] /= f;
+    }
+    int ans = 0;
+    rep(i, n) { ans += F(a[i]); }
+    cout << ans << endl;
+}
