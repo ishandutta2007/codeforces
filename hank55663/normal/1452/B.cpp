debdf701dd32519469cd6314798da693c502d194
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 10:58:54 +0530

    hank55663

---
+++ hank55663/normal/1452/B.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#define mp make_pair
+#define pb push_back
+#define pll pair<LL,LL>
+#define pii pair<int,int>
+#define y second
+#define x first
+#define LL long long 
+#define ULL unsigned long long 
+#define sqr(x) ((x)*(x))
+#define pi acos(-1)
+#define pdd pair<double,double>
+#define MEM(x) memset(x,0,sizeof(x))
+#define MEMS(x) memset(x,-1,sizeof(x))
+using namespace std;
+void solve(){
+    int n;
+    scanf("%d",&n);
+    int a[100005];
+    for(int i = 0;i<n;i++)scanf("%d",&a[i]);
+    sort(a,a+n);
+    LL sum=0;
+    for(int i = 0;i<n;i++)  sum+=a[i];
+    LL need=a[n-1]*1ll*(n-1);
+    if(sum>need){
+        printf("%lld\n",(n-1-(sum)%(n-1))%(n-1));
+    }
+    else{
+        printf("%lld\n",need-sum);
+    }
+}
+
+int main(){
+    int t=1;
+    scanf("%d",&t);
+    while(t--){
+        solve();
+    }
+}
