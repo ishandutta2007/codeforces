54a3e6756478568a35d2a2db264082a3860460f6
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 00:34:01 +0530

    noimi

---
+++ noimi/normal/434/D.cpp
@@ -0,0 +1,460 @@
+#pragma region Macros
+// #pragma GCC target("avx2")
+// #pragma GCC optimize("O3")
+// #pragma GCC optimize("unroll-loops")
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep2(i, a, b) for(ll i = a; i <= b; ++i)
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for(ll i = a; i >= b; --i)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpll vector<pll>
+#define overload2(_1, _2, name, ...) name
+#define vec(type, name, ...) vector<type> name(__VA_ARGS__)
+#define VEC(type, name, size)                                                                                                                                  \
+    vector<type> name(size);                                                                                                                                   \
+    IN(name)
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define VV(type, name, h, w)                                                                                                                                   \
+    vector<vector<type>> name(h, vector<type>(w));                                                                                                             \
+    IN(name)
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...)                                                                                                                         \
+    vector<vector<vector<vector<type>>>> name(a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define mt make_tuple
+
+#define fi first
+#define se second
+#define all(c) begin(c), end(c)
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+using namespace std;
+constexpr pii dx4[4] = {pii{1, 0}, pii{0, 1}, pii{-1, 0}, pii{0, -1}};
+constexpr pii dx8[8] = {{1, 0}, {1, 1}, {0, 1}, {-1, 1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
+const string YESNO[2] = {"NO", "YES"};
+const string YesNo[2] = {"No", "Yes"};
+const string yesno[2] = {"no", "yes"};
+void YES(bool t = 1) { cout << YESNO[t] << endl; }
+void Yes(bool t = 1) { cout << YesNo[t] << endl; }
+void yes(bool t = 1) { cout << yesno[t] << endl; }
+template <class T> using vc = vector<T>;
+template <class T> using vvc = vector<vc<T>>;
+template <class T> using vvvc = vector<vvc<T>>;
+template <class T> using vvvvc = vector<vvvc<T>>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define si(c) (int)(c).size()
+#define INT(...)                                                                                                                                               \
+    int __VA_ARGS__;                                                                                                                                           \
+    IN(__VA_ARGS__)
+#define LL(...)                                                                                                                                                \
+    ll __VA_ARGS__;                                                                                                                                            \
+    IN(__VA_ARGS__)
+#define STR(...)                                                                                                                                               \
+    string __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+#define CHR(...)                                                                                                                                               \
+    char __VA_ARGS__;                                                                                                                                          \
+    IN(__VA_ARGS__)
+#define DBL(...)                                                                                                                                               \
+    double __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &);
+template <class T> void scan(vector<T> &a) {
+    for(auto &i : a) scan(i);
+}
+template <class T> void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
+    scan(head);
+    IN(tail...);
+}
+template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
+template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
+vi iota(int n) {
+    vi a(n);
+    iota(all(a), 0);
+    return a;
+}
+template <typename T> vi iota(vector<T> &a, bool greater = false) {
+    vi res(a.size());
+    iota(all(res), 0);
+    sort(all(res), [&](int i, int j) {
+        if(greater) return a[i] > a[j];
+        return a[i] < a[j];
+    });
+    return res;
+}
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+template <class T> T POW(T x, int n) {
+    T res = 1;
+    for(; n; n >>= 1, x *= x)
+        if(n & 1) res *= x;
+    return res;
+}
+vector<pll> factor(ll x) {
+    vector<pll> ans;
+    for(ll i = 2; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.push_back({i, 1});
+            while((x /= i) % i == 0) ans.back().second++;
+        }
+    if(x != 1) ans.push_back({x, 1});
+    return ans;
+}
+template <class T> vector<T> divisor(T x) {
+    vector<T> ans;
+    for(T i = 1; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.pb(i);
+            if(i * i != x) ans.pb(x / i);
+        }
+    return ans;
+}
+template <typename T> void zip(vector<T> &x) {
+    vector<T> y = x;
+    sort(all(y));
+    for(int i = 0; i < x.size(); ++i) { x[i] = lb(y, x[i]); }
+}
+int topbit(signed t) { return t == 0 ? -1 : 31 - __builtin_clz(t); }
+int topbit(ll t) { return t == 0 ? -1 : 63 - __builtin_clzll(t); }
+int lowbit(signed a) { return a == 0 ? 32 : __builtin_ctz(a); }
+int lowbit(ll a) { return a == 0 ? 64 : __builtin_ctzll(a); }
+// int allbit(int n) { return (1 << n) - 1; }
+ll allbit(ll n) { return (1LL << n) - 1; }
+int popcount(signed t) { return __builtin_popcount(t); }
+int popcount(ll t) { return __builtin_popcountll(t); }
+bool ispow2(int i) { return i && (i & -i) == i; }
+
+int in() {
+    int x;
+    cin >> x;
+    return x;
+}
+ll lin() {
+    unsigned long long x;
+    cin >> x;
+    return x;
+}
+
+template <class T> pair<T, T> operator-(const pair<T, T> &x, const pair<T, T> &y) { return pair<T, T>(x.fi - y.fi, x.se - y.se); }
+template <class T> pair<T, T> operator+(const pair<T, T> &x, const pair<T, T> &y) { return pair<T, T>(x.fi + y.fi, x.se + y.se); }
+template <class T> ll operator*(const pair<T, T> &x, const pair<T, T> &y) { return (ll)x.fi * y.fi + (ll)x.se * y.se; }
+
+template <typename T> struct edge {
+    int from, to;
+    T cost;
+    int id;
+    edge(int to, T cost) : from(-1), to(to), cost(cost) {}
+    edge(int from, int to, T cost) : from(from), to(to), cost(cost) {}
+    edge(int from, int to, T cost, int id) : from(from), to(to), cost(cost), id(id) {}
+    edge &operator=(const int &x) {
+        to = x;
+        return *this;
+    }
+    operator int() const { return to; }
+};
+template <typename T> using Edges = vector<edge<T>>;
+
+using Tree = vector<vector<int>>;
+using Graph = vector<vector<int>>;
+template <class T> using Wgraph = vector<vector<edge<T>>>;
+Graph getG(int n, int m = -1, bool directed = false, int margin = 1) {
+    Tree res(n);
+    if(m == -1) m = n - 1;
+    while(m--) {
+        int a, b;
+        cin >> a >> b;
+        a -= margin, b -= margin;
+        res[a].emplace_back(b);
+        if(!directed) res[b].emplace_back(a);
+    }
+    return move(res);
+}
+template <class T> Wgraph<T> getWg(int n, int m = -1, bool directed = false, int margin = 1) {
+    Wgraph<T> res(n);
+    if(m == -1) m = n - 1;
+    while(m--) {
+        int a, b;
+        T c;
+        cin >> a >> b >> c;
+        a -= margin, b -= margin;
+        res[a].emplace_back(b, c);
+        if(!directed) res[b].emplace_back(a, c);
+    }
+    return move(res);
+}
+
+#define i128 __int128_t
+#define ull unsigned long long int
+#define TEST                                                                                                                                                   \
+    INT(testcases);                                                                                                                                            \
+    while(testcases--)
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v))
+            os << *it;
+        else
+            os << " " << *it;
+    }
+    return os;
+}
+template <class T, class S> ostream &operator<<(ostream &os, const pair<T, S> &p) {
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class S, class T> string to_string(pair<S, T> p) { return "(" + to_string(p.first) + "," + to_string(p.second) + ")"; }
+template <class A> string to_string(A v) {
+    if(v.empty()) return "{}";
+    string ret = "{";
+    for(auto &x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+string to_string(string s) { return "\"" + s + "\""; }
+
+void dump() { cerr << endl; }
+template <class Head, class... Tail> void dump(Head head, Tail... tail) {
+    cerr << to_string(head) << " ";
+    dump(tail...);
+}
+#define endl '\n'
+#ifdef _LOCAL
+#undef endl
+#define debug(x)                                                                                                                                               \
+    cout << #x << ": ";                                                                                                                                        \
+    dump(x)
+#else
+#define debug(x)
+#endif
+
+template <typename T> static constexpr T inf = numeric_limits<T>::max() / 2;
+struct Setup_io {
+    Setup_io() {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        cout << fixed << setprecision(15);
+    }
+} setup_io;
+#define drop(s) cout << #s << endl, exit(0)
+#pragma endregion
+
+
+#include <algorithm>
+
+#include <vector>
+
+namespace atcoder {
+
+namespace internal {
+
+template <class T> struct simple_queue {
+    std::vector<T> payload;
+    int pos = 0;
+    void reserve(int n) { payload.reserve(n); }
+    int size() const { return int(payload.size()) - pos; }
+    bool empty() const { return pos == int(payload.size()); }
+    void push(const T& t) { payload.push_back(t); }
+    T& front() { return payload[pos]; }
+    void clear() {
+        payload.clear();
+        pos = 0;
+    }
+    void pop() { pos++; }
+};
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+#include <cassert>
+#include <limits>
+#include <queue>
+#include <vector>
+
+namespace atcoder {
+
+template <class Cap> struct mf_graph {
+  public:
+    mf_graph() : _n(0) {}
+    mf_graph(int n) : _n(n), g(n) {}
+
+    int add_edge(int from, int to, Cap cap) {
+        assert(0 <= from && from < _n);
+        assert(0 <= to && to < _n);
+        assert(0 <= cap);
+        int m = int(pos.size());
+        pos.push_back({from, int(g[from].size())});
+        g[from].push_back(_edge{to, int(g[to].size()), cap});
+        g[to].push_back(_edge{from, int(g[from].size()) - 1, 0});
+        return m;
+    }
+
+    struct edge {
+        int from, to;
+        Cap cap, flow;
+    };
+
+    edge get_edge(int i) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        auto _e = g[pos[i].first][pos[i].second];
+        auto _re = g[_e.to][_e.rev];
+        return edge{pos[i].first, _e.to, _e.cap + _re.cap, _re.cap};
+    }
+    std::vector<edge> edges() {
+        int m = int(pos.size());
+        std::vector<edge> result;
+        for (int i = 0; i < m; i++) {
+            result.push_back(get_edge(i));
+        }
+        return result;
+    }
+    void change_edge(int i, Cap new_cap, Cap new_flow) {
+        int m = int(pos.size());
+        assert(0 <= i && i < m);
+        assert(0 <= new_flow && new_flow <= new_cap);
+        auto& _e = g[pos[i].first][pos[i].second];
+        auto& _re = g[_e.to][_e.rev];
+        _e.cap = new_cap - new_flow;
+        _re.cap = new_flow;
+    }
+
+    Cap flow(int s, int t) {
+        return flow(s, t, std::numeric_limits<Cap>::max());
+    }
+    Cap flow(int s, int t, Cap flow_limit) {
+        assert(0 <= s && s < _n);
+        assert(0 <= t && t < _n);
+
+        std::vector<int> level(_n), iter(_n);
+        internal::simple_queue<int> que;
+
+        auto bfs = [&]() {
+            std::fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            que.clear();
+            que.push(s);
+            while (!que.empty()) {
+                int v = que.front();
+                que.pop();
+                for (auto e : g[v]) {
+                    if (e.cap == 0 || level[e.to] >= 0) continue;
+                    level[e.to] = level[v] + 1;
+                    if (e.to == t) return;
+                    que.push(e.to);
+                }
+            }
+        };
+        auto dfs = [&](auto self, int v, Cap up) {
+            if (v == s) return up;
+            Cap res = 0;
+            int level_v = level[v];
+            for (int& i = iter[v]; i < int(g[v].size()); i++) {
+                _edge& e = g[v][i];
+                if (level_v <= level[e.to] || g[e.to][e.rev].cap == 0) continue;
+                Cap d =
+                    self(self, e.to, std::min(up - res, g[e.to][e.rev].cap));
+                if (d <= 0) continue;
+                g[v][i].cap += d;
+                g[e.to][e.rev].cap -= d;
+                res += d;
+                if (res == up) break;
+            }
+            return res;
+        };
+
+        Cap flow = 0;
+        while (flow < flow_limit) {
+            bfs();
+            if (level[t] == -1) break;
+            std::fill(iter.begin(), iter.end(), 0);
+            while (flow < flow_limit) {
+                Cap f = dfs(dfs, t, flow_limit - flow);
+                if (!f) break;
+                flow += f;
+            }
+        }
+        return flow;
+    }
+
+    std::vector<bool> min_cut(int s) {
+        std::vector<bool> visited(_n);
+        internal::simple_queue<int> que;
+        que.push(s);
+        while (!que.empty()) {
+            int p = que.front();
+            que.pop();
+            visited[p] = true;
+            for (auto e : g[p]) {
+                if (e.cap && !visited[e.to]) {
+                    visited[e.to] = true;
+                    que.push(e.to);
+                }
+            }
+        }
+        return visited;
+    }
+
+  private:
+    int _n;
+    struct _edge {
+        int to, rev;
+        Cap cap;
+    };
+    std::vector<std::pair<int, int>> pos;
+    std::vector<std::vector<_edge>> g;
+};
+
+}  // namespace atcoder
+
+
+int main() {
+    INT(n, m);
+    vll a(n), b(n), c(n);
+    rep(i, n) cin >> a[i] >> b[i] >> c[i];
+    auto f = [&](int i, int j) { return a[i] * j * j + b[i] * j + c[i]; };
+    vll l(n), r(n);
+    rep(i, n) cin >> l[i] >> r[i];
+    vi u(m), v(m), d(m);
+    rep(i, m) cin >> u[i] >> v[i] >> d[i], u[i]--, v[i]--;
+
+    atcoder::mf_graph<ll> flow(201 * n + 2);
+    int s = 201 * n, t = 201 * n + 1;
+    int INF = 1e6;
+    vi p(n);
+    int now = 0;
+    rep(i, n) {
+        p[i] = now;
+        flow.add_edge(s, now, inf<int>);
+        rep2(j, l[i], r[i]) {
+            flow.add_edge(now, now + 1, INF - f(i, j));
+            now++;
+        }
+        flow.add_edge(now, t, inf<int>);
+        now++;
+    }
+
+    rep(i, m) {
+        rep2(j, l[u[i]], r[u[i]]) {
+            int now = p[u[i]] + j - l[u[i]];
+            int nxt = j - d[i];
+            if(l[v[i]] <= nxt and nxt <= r[v[i]] + 1) flow.add_edge(now, p[v[i]] + nxt - l[v[i]], inf<int>);
+        }
+    }
+
+    cout << INF * n - flow.flow(s, t) << endl;
+}
