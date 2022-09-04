6125175519a1bd35d974ccd2103611c8234e8652
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 04:13:33 +0530

    geothermal

---
+++ geothermal/normal/1532/D.kt
@@ -0,0 +1,50 @@
+import java.io.PrintWriter
+import java.util.*
+
+@JvmField val INPUT = System.`in`
+@JvmField val OUTPUT = System.out
+
+@JvmField val _reader = INPUT.bufferedReader()
+fun readLine(): String? = _reader.readLine()
+fun readLn() = _reader.readLine()!!
+@JvmField var _tokenizer: StringTokenizer = StringTokenizer("")
+fun read(): String {
+    while (_tokenizer.hasMoreTokens().not()) _tokenizer = StringTokenizer(_reader.readLine() ?: return "", " ")
+    return _tokenizer.nextToken()
+}
+fun readInt() = read().toInt()
+fun readDouble() = read().toDouble()
+fun readLong() = read().toLong()
+fun readStrings(n: Int) = List(n) { read() }
+fun readLines(n: Int) = List(n) { readLn() }
+fun readInts(n: Int) = List(n) { read().toInt() }
+fun readIntArray(n: Int) = IntArray(n) { read().toInt() }
+fun readDoubles(n: Int) = List(n) { read().toDouble() }
+fun readDoubleArray(n: Int) = DoubleArray(n) { read().toDouble() }
+fun readLongs(n: Int) = List(n) { read().toLong() }
+fun readLongArray(n: Int) = LongArray(n) { read().toLong() }
+
+@JvmField val _writer = PrintWriter(OUTPUT, false)
+inline fun output(block: PrintWriter.() -> Unit) { _writer.apply(block).flush() }
+
+fun solve() {
+    var N = readInt()
+    var arr = readIntArray(N)
+    arr.sort()
+    var ans = 0
+    for (i in 0 until N-1 step 2) {
+        ans += arr[i+1] - arr[i]
+    }
+    println(ans)
+
+}
+
+fun main(args: Array<String>) {
+    var T = 1
+    //T = readInt()
+    while(T > 0) {
+        solve()
+        T--
+    }
+
+}
