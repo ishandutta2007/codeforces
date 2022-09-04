55cf24586c1a81747bd107984bab6079ea3864ca
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 01:52:38 +0530

    noimi

---
+++ noimi/normal/788/C.cpp
@@ -0,0 +1,245 @@
+#pragma region Macros
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep2(i,a,b) for(ll i=a;i<=b;++i)
+#define rep(i,n) for(ll i=0;i<n;++i)
+#define rep3(i,a,b) for(ll i=a;i>=b;--i)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define vec vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpll vector<pll>
+#define overload2(_1,_2,name,...) name
+#define vv(a,b) vector<vector<int>>(a,vector<int>(b))
+#define vv2(a,b,c) vector<vector<int>>(a,vector<int>(b,c))
+#define vvl(a,b) vector<vector<ll>>(a,vector<ll>(b))
+#define vvl2(a,b,c) vector<vector<ll>>(a,vector<ll>(b,c))
+#define vvv(a,b,c) vector<vv(b,c)>(a)
+#define vvv2(a,b,c,d) vector<vv(b,c,d)>(a)
+#define vvvl(a,b,c) vector<vvl(b,c)>(a)
+#define vvvl2(a,b,c,d) vector<vvl(b,c,d)>(a)
+#define fi first
+#define se second
+#define all(c) begin(c),end(c)
+#define ios ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(c,x) distance((c).begin(),lower_bound(all(c),(x)))
+#define ub(c,x) distance((c).begin(),upper_bound(all(c),(x)))
+using namespace std;
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+#define Size(c) (int)(c).size()
+#define INT(...) int __VA_ARGS__;IN(__VA_ARGS__)
+#define LL(...) ll __VA_ARGS__;IN(__VA_ARGS__)
+#define ULL(...) ull __VA_ARGS__;IN(__VA_ARGS__)
+#define STR(...) string __VA_ARGS__;IN(__VA_ARGS__)
+#define CHR(...) char __VA_ARGS__;IN(__VA_ARGS__)
+#define DBL(...) double __VA_ARGS__;IN(__VA_ARGS__)
+#define LD(...) ld __VA_ARGS__;IN(__VA_ARGS__)
+#define VEC(type,name,size) vector<type> name(size);IN(name)
+#define VV(type,name,h,w) vector<vector<type>>name(h,vector<type>(w));IN(name)
+int scan(){ return getchar(); }
+void scan(int& a){ cin>>a; }
+void scan(long long& a){ cin>>a; }
+void scan(char &a){cin>>a;}
+void scan(double &a){ cin>>a; }
+void scan(long double& a){ cin>>a; }
+void scan(char a[]){ scanf("%s", a); }
+void scan(string& a){ cin >> a; }
+template<class T> void scan(vector<T>&);
+template<class T, size_t size> void scan(array<T, size>&);
+template<class T, class L> void scan(pair<T, L>&);
+template<class T, size_t size> void scan(T(&)[size]);
+template<class T> void scan(vector<T>& a){ for(auto& i : a) scan(i); }
+template<class T> void scan(deque<T>& a){ for(auto& i : a) scan(i); }
+template<class T, size_t size> void scan(array<T, size>& a){ for(auto& i : a) scan(i); }
+template<class T, class L> void scan(pair<T, L>& p){ scan(p.first); scan(p.second); }
+template<class T, size_t size> void scan(T (&a)[size]){ for(auto& i : a) scan(i); }
+template<class T> void scan(T& a){ cin >> a; }
+void IN(){}
+template <class Head, class... Tail> void IN(Head& head, Tail&... tail){ scan(head); IN(tail...); }
+string stin() {string s;cin>>s;return s;}
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
+vi iota(int n){vi a(n);iota(all(a),0);return a;}
+template<class T> void UNIQUE(vector<T> &x){sort(all(x));x.erase(unique(all(x)),x.end());}
+int in() {int x;cin>>x;return x;}
+ll lin() {unsigned long long x;cin>>x;return x;}
+void print(){putchar(' ');}
+void print(bool a){cout<<a;}
+void print(int a){cout<<a;}
+void print(long long a){cout<<a;}
+void print(char a){cout<<a;}
+void print(string &a){cout<<a;}
+void print(double a){cout<<a;}
+template<class T> void print(const vector<T>&);
+template<class T, size_t size> void print(const array<T, size>&);
+template<class T, class L> void print(const pair<T, L>& p);
+template<class T, size_t size> void print(const T (&)[size]);
+template<class T> void print(const vector<T>& a){ if(a.empty()) return; print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } cout<<endl;}
+template<class T> void print(const deque<T>& a){ if(a.empty()) return; print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } }
+template<class T, size_t size> void print(const array<T, size>& a){ print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } }
+template<class T, class L> void print(const pair<T, L>& p){ cout<<'(';print(p.first); cout<<","; print(p.second);cout<<')'; }
+template<class T> void print(set<T> &x){for(auto e:x)print(e),cout<<" ";cout<<endl;}
+template<class T, size_t size> void print(const T (&a)[size]){ print(a[0]); for(auto i = a; ++i != end(a); ){ cout<<" "; print(*i); } }
+template<class T> void print(const T& a){ cout << a; }
+int out(){ putchar('\n'); return 0; }
+template<class T> int out(const T& t){ print(t); putchar('\n'); return 0; }
+template<class Head, class... Tail> int out(const Head& head, const Tail&... tail){ print(head); putchar(' '); out(tail...); return 0; }
+ll gcd(ll a, ll b){ while(b){ ll c = b; b = a % b; a = c; } return a; }
+ll lcm(ll a, ll b){ if(!a || !b) return 0; return a * b / gcd(a, b); }
+vector<pll> factor(ll x){ vector<pll> ans; for(ll i = 2; i * i <= x; i++) if(x % i == 0){ ans.push_back({i, 1}); while((x /= i) % i == 0) ans.back().second++; } if(x != 1) ans.push_back({x, 1}); return ans; }
+vector<int> divisor(int x){ vector<int> ans; for(int i=1;i*i<=x;i++)if(x%i==0){ans.pb(i);if(i*i!=x)ans.pb(x/i);} return ans;}
+int popcount(ll x){return __builtin_popcountll(x);}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+int rnd(int n){return uniform_int_distribution<int>(0, n-1)(rng);}
+#define endl '\n'
+
+#ifdef _LOCAL
+    #undef endl
+    #define debug(x) cout<<#x<<": ";print(x);cout<<endl;
+    void err(){}
+    template<class T> void err(const T& t){ print(t);  cout<<" ";}
+    template<class Head, class... Tail> void err(const Head& head, const Tail&... tail){ print(head); putchar(' '); out(tail...); }
+#else
+    #define debug(x)
+    template<class... T> void err(const T&...){}
+#endif
+
+struct Setup_io{ Setup_io(){ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);cout<<fixed<<setprecision(15);}}setup_io;
+#pragma endregion
+
+
+// ______________________
+
+
+namespace FastFourierTransform {
+  using real = double;
+
+  struct C {
+    real x, y;
+
+    C() : x(0), y(0) {}
+
+    C(real x, real y) : x(x), y(y) {}
+
+    inline C operator+(const C &c) const { return C(x + c.x, y + c.y); }
+
+    inline C operator-(const C &c) const { return C(x - c.x, y - c.y); }
+
+    inline C operator*(const C &c) const { return C(x * c.x - y * c.y, x * c.y + y * c.x); }
+
+    inline C conj() const { return C(x, -y); }
+  };
+
+  const real PI = acosl(-1);
+  int base = 1;
+  vector< C > rts = { {0, 0},
+                     {1, 0} };
+  vector< int > rev = {0, 1};
+
+
+  void ensure_base(int nbase) {
+    if(nbase <= base) return;
+    rev.resize(1 << nbase);
+    rts.resize(1 << nbase);
+    for(int i = 0; i < (1 << nbase); i++) {
+      rev[i] = (rev[i >> 1] >> 1) + ((i & 1) << (nbase - 1));
+    }
+    while(base < nbase) {
+      real angle = PI * 2.0 / (1 << (base + 1));
+      for(int i = 1 << (base - 1); i < (1 << base); i++) {
+        rts[i << 1] = rts[i];
+        real angle_i = angle * (2 * i + 1 - (1 << base));
+        rts[(i << 1) + 1] = C(cos(angle_i), sin(angle_i));
+      }
+      ++base;
+    }
+  }
+
+  void fft(vector< C > &a, int n) {
+    assert((n & (n - 1)) == 0);
+    int zeros = __builtin_ctz(n);
+    ensure_base(zeros);
+    int shift = base - zeros;
+    for(int i = 0; i < n; i++) {
+      if(i < (rev[i] >> shift)) {
+        swap(a[i], a[rev[i] >> shift]);
+      }
+    }
+    for(int k = 1; k < n; k <<= 1) {
+      for(int i = 0; i < n; i += 2 * k) {
+        for(int j = 0; j < k; j++) {
+          C z = a[i + j + k] * rts[j + k];
+          a[i + j + k] = a[i + j] - z;
+          a[i + j] = a[i + j] + z;
+        }
+      }
+    }
+  }
+
+  vector< int64_t > multiply(const vector< int > &a, const vector< int > &b) {
+    int need = (int) a.size() + (int) b.size() - 1;
+    int nbase = 1;
+    while((1 << nbase) < need) nbase++;
+    ensure_base(nbase);
+    int sz = 1 << nbase;
+    vector< C > fa(sz);
+    for(int i = 0; i < sz; i++) {
+      int x = (i < (int) a.size() ? a[i] : 0);
+      int y = (i < (int) b.size() ? b[i] : 0);
+      fa[i] = C(x, y);
+    }
+    fft(fa, sz);
+    C r(0, -0.25 / (sz >> 1)), s(0, 1), t(0.5, 0);
+    for(int i = 0; i <= (sz >> 1); i++) {
+      int j = (sz - i) & (sz - 1);
+      C z = (fa[j] * fa[j] - (fa[i] * fa[i]).conj()) * r;
+      fa[j] = (fa[i] * fa[i] - (fa[j] * fa[j]).conj()) * r;
+      fa[i] = z;
+    }
+    for(int i = 0; i < (sz >> 1); i++) {
+      C A0 = (fa[i] + fa[i + (sz >> 1)]) * t;
+      C A1 = (fa[i] - fa[i + (sz >> 1)]) * t * rts[(sz >> 1) + i];
+      fa[i] = A0 + A1 * s;
+    }
+    fft(fa, sz >> 1);
+    vector< int64_t > ret(need);
+    for(int i = 0; i < need; i++) {
+      ret[i] = llround(i & 1 ? fa[i >> 1].y : fa[i >> 1].x);
+    }
+    return ret;
+  }
+};
+
+signed main(){
+    INT(k,n);
+    const int T = 1000;
+    vector<int> a(T*2+1);
+    bool f,g;
+    f = g = false;
+    rep(i,n){
+        int x = in();
+        if(k-x<=0) f = true;
+        if(k-x>=0) g = true;
+        a[k-x + T] = 1;
+    }
+    if(!f or !g) {cout << -1 << endl; return 0;}
+    int cnt = 1;
+    vi c = a;
+    while(1){
+        if(c[T]){
+            cout<< cnt << endl;
+            return 0;
+        }
+        auto C = FastFourierTransform::multiply(c,a);
+        rep(i,T*2+1) c[i] = (C[i+T] ?1:0);
+        cnt++;
+    }
+
+}
