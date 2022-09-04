63a3533406acc884d36bcd9ff69396cafc8f08ea
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 04:21:56 +0530

    geothermal

---
+++ geothermal/normal/1528/A.cpp
@@ -0,0 +1,91 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001; 
+
+vector<vi> graph(MX);
+ll L[MX], R[MX];
+ll dp1[MX], dp2[MX];
+
+void dfs(int v, int p) {
+    dp1[v] = 0; dp2[v] = 0;
+    trav(a, graph[v]) {
+        if (a == p) continue;
+        dfs(a, v);
+        dp1[v] += max(abs(L[v]-L[a]) + dp1[a], abs(L[v]-R[a])+dp2[a]);
+        dp2[v] += max(abs(R[v]-L[a])+dp1[a], abs(R[v]-R[a])+dp2[a]);
+    }
+}
+
+void solve() {
+    int N; cin >> N;
+    F0R(i, N) graph[i].clear();
+    F0R(i, N) cin >> L[i] >> R[i];
+    F0R(i, N-1) {
+        int X, Y; cin >> X >> Y; X--; Y--;
+        graph[X].pb(Y); graph[Y].pb(X);
+    }
+    dfs(0, -1);
+
+    cout << max(dp1[0], dp2[0]) << nl;
+
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0); 
+    cin.exceptions(cin.failbit);
+
+    int T = 1;
+    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+	return 0;
+}
