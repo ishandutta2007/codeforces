787bc509fee0f0ae9f68d011d143d2c3c3b8d2d9
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 22:15:28 +0530

    never_giveup

---
+++ never_giveup/normal/1394/B.cpp
@@ -0,0 +1,450 @@
+/*
+                     .:*+=%@@@@@@=-.
+                 .:=@#@@@#@@#######%==*.
+              .-=####@######%*-.....:%##%.
+            .*@###########%+:--........-%@-
+          .*@##############@+--.........-:%-
+        .+##################@==%%%%=+*:----+.
+      .-@####################%++%@@@@@=+**%@@*
+      .%###################@%%@@@###@%+:--%@@%.
+     -@###################@%%%%*::*%++:-----=@+.
+    -#####################@%=++++++*:-------.-=:
+   .+####################@%++*::-:::--::*:::***=:
+   .@#####################%=++*::::-:::++*=##@@#@-
+  ..#####################@%%=++**:::::**+%@#@%%##-..
+   .%####################@@%=+++*+****::*=@######@.
+  .=######################@%%==+==++**+=@%@##@###+:...
+  -#######################@@@%%%===++=@@@%=++===*::--...
+  -########################@@@@@@@%==%%=++==@@:::::*:--.
+..:#########################@@@@@@%%======++++::-..:-.--...
+%#############################@###@%%@@%==%=%*----.--.::---.
+#############################################*-:*:-:---*---- .
+#############################################*--*--:---*---:-.
+#############################################+--::--::-*::-::.
+###########################################+:*-.---.---.:---*-..
+###########################################**:-----------------.
+##########################################@::**:--::::::--:::::-
+###########################################:--:*:::::::::**::*+*
+###########################################=:::***::::::**:::*+*
+############################@@@@@@#########@+****::::********+++
+############################@%%%%%@@@@@@@###%+***::::::::***+==+
+############################@%%%%%%%%%%%@####=+:::-::::-::*+=%%+
+#############################@%%%%%%%%%%@#####=::--------:*=%@%+
+%###########################@%%%%==%%%%%%@##@#=:------..-:+%@@%=
+----------------------------------------------
+--------------------------------------------
+----------------------------------------------
+--------------------------------------------
+----------------------------------------------
+
+         o###########oo
+      o##"          ""##o
+    o#"                "##
+  o#"                    "#o
+ #"  ##              ##   "##
+#"                          ##
+#  ###################       #
+#                            #
+#                            #
+#                            #
+#                            #
+#                            #
+#                            #
+#o                           #
+"#o                         ##
+ "#o                       ##
+  "#o                    o#"
+   "#o                  ##
+     "#o              o#"
+       "#ooo      ooo#######oo
+        ###############   "######o
+     o###""        "###o      # ###
+   o###o     oooo    ###    oo####"
+ o###**#     #**#   ############"
+ ""##""""""""""###########    #
+    # oooooooo#"#**     ##    #
+    # #       # # **    ##    #
+    #o#       #o#  *****###ooo#
+                        ##
+                        ##   o###o
+                        ## o##***##
+               o########## #***#**##o
+             o##"   ""###  #***##***#
+ o#######o  ###   oo####   ##**####*#
+o##"  ""#############""     ##****###
+##"         ##              ##*##*###
+##          ###              ##### ##
+##           ###              # ##  #
+##            ##                 #
+##             ##
+##             ###
+##              ###oo
+###              ""###
+ ###
+  ###
+*/
+
+#include <bits/stdc++.h>
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wunused-const-variable"
+#define popcnt(x) __builtin_popcount(x)
+
+#define fr first
+
+#define sc second
+
+#define m_p make_pair
+
+#define low_bo(a, x) lower_bound(a.begin(), a.end(), x) - a.begin()
+
+#define up_bo(a, x) upper_bound(a.begin(), a.end(), x) - a.begin()
+
+#define unique(a) a.resize(unique(a.begin(), a.end()) - a.begin())
+
+#define popcnt(x) __builtin_popcount(x)
+
+//#include <ext/pb_ds/assoc_container.hpp>
+
+//using namespace __gnu_pbds;
+
+//gp_hash_table<int, int> table;
+
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
+//#pragma GCC target("avx,tune=native")
+//float __attribute__((aligned(32)))
+
+/*char memory[(int)1e8];
+
+char memorypos;
+
+inline void * operator new(size_t n){
+    char * ret = memory + memorypos;
+    memorypos += n;
+    return (void *)ret;
+}
+
+inline void operator delete(void *){}
+*/
+
+using namespace std;
+
+typedef long long ll;
+
+typedef unsigned long long ull;
+
+typedef long double ld;
+
+typedef unsigned int uint;
+
+template <typename T>
+class Modular {
+public:
+    using Type = typename decay<decltype(T::value)>::type;
+
+    constexpr Modular() : value() {}
+    template <typename U>
+    Modular(const U& x) {
+        value = normalize(x);
+    }
+
+    static Type inverse(Type a, Type mod) {
+        Type b = mod, x = 0, y = 1;
+        while (a != 0) {
+            Type t = b / a;
+            b -= a * t;
+            x -= t * y;
+            swap(a, b);
+            swap(x, y);
+        }
+        if (x < 0)
+            x += mod;
+        return x;
+    }
+
+    template <typename U>
+    static Type normalize(const U& x) {
+        Type v;
+        if (-mod() <= x && x < mod()) v = static_cast<Type>(x);
+        else v = static_cast<Type>(x % mod());
+        if (v < 0) v += mod();
+        return v;
+    }
+
+    const Type& operator()() const { return value; }
+    template <typename U>
+    explicit operator U() const { return static_cast<U>(value); }
+    constexpr static Type mod() { return T::value; }
+
+    Modular& operator+=(const Modular& other) { if ((value += other.value) >= mod()) value -= mod(); return *this; }
+    Modular& operator-=(const Modular& other) { if ((value -= other.value) < 0) value += mod(); return *this; }
+    template <typename U> Modular& operator+=(const U& other) { return *this += Modular(other); }
+    template <typename U> Modular& operator-=(const U& other) { return *this -= Modular(other); }
+    Modular& operator++() { return *this += 1; }
+    Modular& operator--() { return *this -= 1; }
+    Modular operator++(int) { Modular result(*this); *this += 1; return result; }
+    Modular operator--(int) { Modular result(*this); *this -= 1; return result; }
+    Modular operator-() const { return Modular(-value); }
+
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int>::value, Modular>::type& operator*=(const Modular& rhs) {
+#ifdef _WIN32
+        uint64_t x = static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value);
+        uint32_t xh = static_cast<uint32_t>(x >> 32), xl = static_cast<uint32_t>(x), d, m;
+        asm(
+        "divl %4; \n\t"
+        : "=a" (d), "=d" (m)
+        : "d" (xh), "a" (xl), "r" (mod())
+        );
+        value = m;
+#else
+        value = normalize(static_cast<int64_t>(value) * static_cast<int64_t>(rhs.value));
+#endif
+        return *this;
+    }
+    template <typename U = T>
+    typename enable_if<is_same<typename Modular<U>::Type, int64_t>::value, Modular>::type& operator*=(const Modular& rhs) {
+        int64_t q = static_cast<int64_t>(static_cast<long double>(value) * rhs.value / mod());
+        value = normalize(value * rhs.value - q * mod());
+        return *this;
+    }
+    template <typename U = T>
+    typename enable_if<!is_integral<typename Modular<U>::Type>::value, Modular>::type& operator*=(const Modular& rhs) {
+        value = normalize(value * rhs.value);
+        return *this;
+    }
+
+    Modular& operator/=(const Modular& other) { return *this *= Modular(inverse(other.value, mod())); }
+
+    template <typename U>
+    friend const Modular<U>& abs(const Modular<U>& v) { return v; }
+
+    template <typename U>
+    friend bool operator==(const Modular<U>& lhs, const Modular<U>& rhs);
+
+    template <typename U>
+    friend bool operator<(const Modular<U>& lhs, const Modular<U>& rhs);
+
+    template <typename U>
+    friend std::istream& operator>>(std::istream& stream, Modular<U>& number);
+
+private:
+    Type value;
+};
+
+template <typename T> bool operator==(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value == rhs.value; }
+template <typename T, typename U> bool operator==(const Modular<T>& lhs, U rhs) { return lhs == Modular<T>(rhs); }
+template <typename T, typename U> bool operator==(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) == rhs; }
+
+template <typename T> bool operator!=(const Modular<T>& lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(const Modular<T>& lhs, U rhs) { return !(lhs == rhs); }
+template <typename T, typename U> bool operator!=(U lhs, const Modular<T>& rhs) { return !(lhs == rhs); }
+
+template <typename T> bool operator<(const Modular<T>& lhs, const Modular<T>& rhs) { return lhs.value < rhs.value; }
+
+template <typename T> Modular<T> operator+(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) += rhs; }
+template <typename T, typename U> Modular<T> operator+(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) += rhs; }
+
+template <typename T> Modular<T> operator-(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) -= rhs; }
+template <typename T, typename U> Modular<T> operator-(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) -= rhs; }
+
+template <typename T> Modular<T> operator*(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) *= rhs; }
+template <typename T, typename U> Modular<T> operator*(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) *= rhs; }
+
+template <typename T> Modular<T> operator/(const Modular<T>& lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(const Modular<T>& lhs, U rhs) { return Modular<T>(lhs) /= rhs; }
+template <typename T, typename U> Modular<T> operator/(U lhs, const Modular<T>& rhs) { return Modular<T>(lhs) /= rhs; }
+
+template<typename T, typename U>
+Modular<T> power(const Modular<T>& a, const U& b) {
+    assert(b >= 0);
+    Modular<T> x = a, res = 1;
+    U p = b;
+    while (p > 0) {
+        if (p & 1) res *= x;
+        x *= x;
+        p >>= 1;
+    }
+    return res;
+}
+
+template <typename T>
+bool IsZero(const Modular<T>& number) {
+    return number() == 0;
+}
+
+template <typename T>
+string to_string(const Modular<T>& number) {
+    return to_string(number());
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& stream, const Modular<T>& number) {
+    return stream << number();
+}
+
+template <typename T>
+std::istream& operator>>(std::istream& stream, Modular<T>& number) {
+    typename common_type<typename Modular<T>::Type, int64_t>::type x;
+    stream >> x;
+    number.value = Modular<T>::normalize(x);
+    return stream;
+}
+
+const int md = 1e9 + 7;
+using Mint = Modular<std::integral_constant<decay<decltype(md)>::type, md>>;
+
+ll sqr(ll x) {
+    return x * x;
+}
+
+int mysqrt(ll x) {
+    int l = 0, r = 1e9 + 1;
+    while (r - l > 1) {
+        int m = (l + r) / 2;
+        if (m * (ll) m <= x)
+            l = m;
+        else
+            r = m;
+    }
+    return l;
+}
+
+#ifdef ONPC
+mt19937 rnd(231);
+mt19937_64 rndll(231);
+#else
+mt19937 rnd(chrono::high_resolution_clock::now().time_since_epoch().count());
+    mt19937_64 rndll(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+
+template<typename T>
+T gcd(T a, T b) {
+    return a ? gcd(b % a, a) : b;
+}
+
+int gcdex(int a, int b, int &x, int &y) {
+    if (a == 0) {
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int ret = gcdex(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return ret;
+}
+
+void setmin(int &x, int y) {
+    x = min(x, y);
+}
+
+void setmax(int &x, int y) {
+    x = max(x, y);
+}
+
+void setmin(ll &x, ll y) {
+    x = min(x, y);
+}
+
+void setmax(ll &x, ll y) {
+    x = max(x, y);
+}
+
+const ll llinf = 4e18 + 100;
+
+const ld eps = 1e-9, PI = atan2(0, -1);
+
+const int maxn = 2e5 + 100, maxw = 1e4 + 100, inf = 1e9 + 100, sq = 450, LG = 18, mod = 1e9 + 933;
+
+int n, m, k;
+
+vector<int> e[maxn];
+
+vector<int> in[maxn];
+
+int pos[maxn];
+
+pair<int, pair<int, int>> edg[maxn];
+
+bool bad[10][10];
+
+int ans;
+
+int q[10][10], arr[10];
+
+bool bu[10][10][10][10];
+
+void go(int it = 1) {
+    if (it == k + 1) {
+        ans++;
+        return;
+    }
+    for (int t = 1; t <= it; t++)
+        if (!bad[it][t]) {
+            bool wo = 0;
+            for (int i = 1; i < it; i++)
+                if (bu[it][t][i][arr[i]]) {
+                    wo = 1;
+                    break;
+                }
+            if (!wo) {
+                arr[it] = t;
+                go(it + 1);
+            }
+        }
+}
+
+int main() {
+#ifdef ONPC
+    freopen("../a.in", "r", stdin);
+    freopen("../a.out", "w", stdout);
+#else
+    //freopen("a.in", "r", stdin);
+    //freopen("a.out", "w", stdout);
+#endif // ONPC
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n >> m >> k;
+    for (int i = 0; i < m; i++) {
+        int v, u, w;
+        cin >> v >> u >> w;
+        e[v - 1].push_back(i);
+        in[u - 1].push_back(i);
+        edg[i] = {w, {v - 1, u - 1}};
+    }
+    for (int i = 0; i < n; i++) {
+        sort(e[i].begin(), e[i].end(), [&](int x, int y) {return edg[x].fr < edg[y].fr;});
+        for (int j = 0; j < e[i].size(); j++) {
+            pos[e[i][j]] = j + 1;
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = 1; j <= k; j++)
+            for (int c = 1; c <= k; c++)
+                q[j][c] = 0;
+        for (int w : in[i]) {
+            q[e[edg[w].sc.fr].size()][pos[w]]++;
+        }
+        vector<pair<int, int>> g;
+        for (int j = 1; j <= k; j++)
+            for (int c = 1; c <= k; c++)
+                if (q[j][c] > 1)
+                    bad[j][c] = 1;
+                else if (q[j][c] == 1 && !bad[j][c])
+                    g.push_back({j, c});
+        sort(g.begin(), g.end());
+        for (int x = 0; x < g.size(); x++)
+            for (int y = x + 1; y < g.size(); y++)
+                bu[g[y].fr][g[y].sc][g[x].fr][g[x].sc] = 1;
+    }
+    go();
+    cout << ans << '\n';
+}
