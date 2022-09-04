2e7e04e09f30493229ee587f5bcc4cfe8d5c0eaf
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 22:28:54 +0530

    never_giveup

---
+++ never_giveup/normal/1056/C.cpp
@@ -0,0 +1,352 @@
+/*
+                     .:*+=%@@@@@@=-.
+                 .:=@#@@@#@@#######%==*.
+              .-=####@######%*-.....:%##%.
+            .*@###########%+:--........-%@-
+          .*@##############@+--.........-:%-
+        .+##################@==%%%%=+*:----+.
+      .-@####################%++%@@@@@=+**%@@*
+      .%###################@%%@@@###@%+:--%@@%.
+     -@###################@%%%%*::*%++:-----=@+.
+    -#####################@%=++++++*:-------.-=:
+   .+####################@%++*::-:::--::*:::***=:
+   .@#####################%=++*::::-:::++*=##@@#@-
+  ..#####################@%%=++**:::::**+%@#@%%##-..
+   .%####################@@%=+++*+****::*=@######@.
+  .=######################@%%==+==++**+=@%@##@###+:...
+  -#######################@@@%%%===++=@@@%=++===*::--...
+  -########################@@@@@@@%==%%=++==@@:::::*:--.
+..:#########################@@@@@@%%======++++::-..:-.--...
+%#############################@###@%%@@%==%=%*----.--.::---.
+#############################################*-:*:-:---*---- .
+#############################################*--*--:---*---:-.
+#############################################+--::--::-*::-::.
+###########################################+:*-.---.---.:---*-..
+###########################################**:-----------------.
+##########################################@::**:--::::::--:::::-
+###########################################:--:*:::::::::**::*+*
+###########################################=:::***::::::**:::*+*
+############################@@@@@@#########@+****::::********+++
+############################@%%%%%@@@@@@@###%+***::::::::***+==+
+############################@%%%%%%%%%%%@####=+:::-::::-::*+=%%+
+#############################@%%%%%%%%%%@#####=::--------:*=%@%+
+%###########################@%%%%==%%%%%%@##@#=:------..-:+%@@%=
+----------------------------------------------
+--------------------------------------------
+----------------------------------------------
+--------------------------------------------
+----------------------------------------------
+
+         o###########oo
+      o##"          ""##o
+    o#"                "##
+  o#"                    "#o
+ #"  ##              ##   "##
+#"                          ##
+#  ###################       #
+#                            #
+#                            #
+#                            #
+#                            #
+#                            #
+#                            #
+#o                           #
+"#o                         ##
+ "#o                       ##
+  "#o                    o#"
+   "#o                  ##
+     "#o              o#"
+       "#ooo      ooo#######oo
+        ###############   "######o
+     o###""        "###o      # ###
+   o###o     oooo    ###    oo####"
+ o###**#     #**#   ############"
+ ""##""""""""""###########    #
+    # oooooooo#"#**     ##    #
+    # #       # # **    ##    #
+    #o#       #o#  *****###ooo#
+                        ##
+                        ##   o###o
+                        ## o##***##
+               o########## #***#**##o
+             o##"   ""###  #***##***#
+ o#######o  ###   oo####   ##**####*#
+o##"  ""#############""     ##****###
+##"         ##              ##*##*###
+##          ###              ##### ##
+##           ###              # ##  #
+##            ##                 #
+##             ##
+##             ###
+##              ###oo
+###              ""###
+ ###
+  ###
+*/
+
+///YEAH IM THE BEST I'VE EVER WAS
+
+///SO HAPPY
+
+#include <bits/stdc++.h>
+
+#define popcnt(x) __builtin_popcount(x)
+
+#define fr first
+
+#define sc second
+
+#define m_p make_pair
+
+#define low_bo(a, x) lower_bound(a.begin(), a.end(), x) - a.begin()
+
+#define up_bo(a, x) upper_bound(a.begin(), a.end(), x) - a.begin()
+
+#define unique(a) a.resize(unique(a.begin(), a.end()) - a.begin())
+
+#define popcnt(x) __builtin_popcount(x)
+
+//#include <ext/pb_ds/assoc_container.hpp>
+
+//using namespace __gnu_pbds;
+
+//gp_hash_table<int, int> table;
+
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
+//#pragma GCC target("avx,tune=native")
+//float __attribute__((aligned(32)))
+
+/*char memory[(int)1e8];
+
+char memorypos;
+
+inline void * operator new(size_t n){
+    char * ret = memory + memorypos;
+    memorypos += n;
+    return (void *)ret;
+}
+
+inline void operator delete(void *){}
+*/
+
+using namespace std;
+
+typedef long long ll;
+
+typedef unsigned long long ull;
+
+typedef long double ld;
+
+typedef unsigned int uint;
+
+ll sqr(ll x){
+    return x * x;
+}
+
+int mysqrt(ll x){
+    int l = 0, r = 1e9 + 1;
+    while (r - l > 1){
+        int m = (l + r) / 2;
+        if (m * (ll)m <= x)
+            l = m;
+        else
+            r = m;
+    }
+    return l;
+}
+
+mt19937 rnd(1227);
+
+mt19937_64 rndll(12365);
+
+ll AR = 19, BR = 13, CR = 23, XR = 228, YR = 322, MODR = 1e9 + 993;
+
+ll myrand(){
+    ll ZR = (XR * AR + YR * BR + CR) % MODR;
+    XR = YR;
+    YR = ZR;
+    return ZR;
+}
+
+int gcd(int a, int b){
+    return a ? gcd(b % a, a) : b;
+}
+
+int gcdex(int a, int b, int &x, int &y){
+    if (a == 0){
+        x = 0;
+        y = 1;
+        return b;
+    }
+    int x1, y1;
+    int ret = gcdex(b % a, a, x1, y1);
+    x = y1 - (b / a) * x1;
+    y = x1;
+    return ret;
+}
+
+const int Mod = 998244353;
+
+int Bpow(int x, int y){
+    int ret = 1;
+    int w = x;
+    while (y){
+        if (y & 1)
+            ret = (ret * (ll)w) % Mod;
+        w = (w * (ll)w) % Mod;
+        y >>= 1;
+    }
+    return ret;
+}
+
+int Bdiv(int x){
+    int a, b;
+    gcdex(x, Mod, a, b);
+    if (a < 0)
+        a += Mod;
+    return a;
+}
+
+int Bdiv(int x, int y){
+    return (x * (ll)Bpow(y, Mod - 2)) % Mod;
+}
+
+inline int ad(int x, int y){
+    return x + y >= Mod ? x + y - Mod : x + y;
+}
+
+inline int dif(int x, int y){
+    return x - y < 0 ? x - y + Mod : x - y;
+}
+
+inline int mul(int x, int y){
+    return (x * (ll)y) % Mod;
+}
+
+int inv(int a){
+    int b = Mod, x = 0, y = 1;
+    while (a){
+        int t = b / a;
+        b -= a * t;
+        x -= t * y;
+        swap(a, b);
+        swap(x, y);
+    }
+    if (x < 0)
+        x += Mod;
+    return x;
+}
+
+void setmin(int &x, int y){
+    x = min(x, y);
+}
+
+void setmax(int &x, int y){
+    x = max(x, y);
+}
+
+void setmin(ll &x, ll y){
+    x = min(x, y);
+}
+
+void setmax(ll &x, ll y){
+    x = max(x, y);
+}
+
+const ll llinf = 4e18 + 100;
+
+const double eps = 1e-9;
+
+const int maxn = 1e5 + 100, maxw = 1e6 + 100, inf = 2e9 + 100, sq = 300, mod = 1e9 + 933, LG = 17;
+
+int a[maxn];
+
+int n, m;
+
+int q[maxn];
+
+bool vis[maxn];
+
+pair<int, int> b[maxn];
+
+int main()
+{
+    #ifdef ONPC
+    //ifstream cin("a.in");
+    //ofstream cout("a.out");
+    //freopen("a.in", "r", stdin);
+    //freopen("a.out", "w", stdout);
+    #else
+    //ifstream cin("gymnasts.in");
+    //ofstream cout("gymnasts.out");
+    //freopen("nextpartition.in", "r", stdin);
+    //freopen("nextpartition.out", "w", stdout);
+    #endif // ONPC
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int n, m;
+    cin >> n >> m;
+    for (int i = 0; i < 2 * n; i++)
+        cin >> a[i], q[i] = -1;
+    for (int i = 0; i < m; i++){
+        int v, u;
+        cin >> v >> u;
+        v--;
+        u--;
+        q[v] = u;
+        q[u] = v;
+    }
+    int t;
+    cin >> t;
+    int rem = 2 * n;
+    if (t == 2){
+        while (1){
+            if (rem == 0)
+                return 0;
+            int w;
+            cin >> w;
+            w--;
+            vis[w] = 1;
+            if (q[w] != -1){
+                vis[q[w]] = 1;
+                cout << q[w] + 1 << endl;
+                rem -= 2;
+            } else {
+                rem--;
+                break;
+            }
+        }
+    }
+    for (int i = 0; i < 2 * n; i++)
+    if (!vis[i] && q[i] != -1){
+        int v = i, u = q[i];
+        if (a[v] < a[u])
+            swap(v, u);
+        cout << v + 1 << endl;
+        cin >> u;
+        u--;
+        vis[v] = 1;
+        vis[u] = 1;
+        rem -= 2;
+    }
+    int sz = 0;
+    for (int i = 0; i < 2 * n; i++)
+    if (!vis[i])
+        b[sz++] = m_p(a[i], i);
+    sort(b, b + sz);
+    reverse(b, b + sz);
+    for (int i = 0; i < sz; i++)
+    if (!vis[b[i].sc]){
+        cout << b[i].sc + 1 << endl;
+        rem--;
+        if (rem == 0)
+            return 0;
+        int w;
+        cin >> w;
+        vis[w - 1] = 1;
+        rem--;
+    }
+}
