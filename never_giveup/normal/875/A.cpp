87973f2a45ed7751c5091219390dfddaa73930e9
ishan dutta <ishandutta2007@gmail.com>
2022-09-04 22:41:17 +0530

    never_giveup

---
+++ never_giveup/normal/875/A.cpp
@@ -0,0 +1,129 @@
+/*
+----------------------------------------------
+--------------------------------------------
+----------------------------------------------
+--------------------------------------------
+----------------------------------------------
+
+         o###########oo
+      o##"          ""##o
+    o#"                "##
+  o#"                    "#o
+ #"  ##              ##   "##
+#"                          ##
+#  ###################       #
+#                            #
+#                            #
+#                            #
+#                            #
+#                            #
+#                            #
+#o                           #
+"#o                         ##
+ "#o                       ##
+  "#o                    o#"
+   "#o                  ##
+     "#o              o#"
+       "#ooo      ooo#######oo
+        ###############   "######o
+     o###""        "###o      # ###
+   o###o     oooo    ###    oo####"
+ o###**#     #**#   ############"
+ ""##""""""""""###########    #
+    # oooooooo#"#**     ##    #
+    # #       # # **    ##    #
+    #o#       #o#  *****###ooo#
+                        ##
+                        ##   o###o
+                        ## o##***##
+               o########## #***#**##o
+             o##"   ""###  #***##***#
+ o#######o  ###   oo####   ##**####*#
+o##"  ""#############""     ##****###
+##"         ##              ##*##*###
+##          ###              ##### ##
+##           ###              # ##  #
+##            ##                 #
+##             ##
+##             ###
+##              ###oo
+###              ""###
+ ###
+  ###
+*/
+#include <bits/stdc++.h>
+//#pragma GCC optimize("O3")
+//#pragma GCC optimize("Ofast,no-stack-protector")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx")
+//#pragma GCC target("avx,tune=native")
+
+using namespace std;
+
+typedef long long ll;
+
+typedef long double ld;
+
+ll AR = 19, BR = 13, CR = 23, XR = 228, YR = 322, MOD = 1e9 + 993;
+
+ll myrand(){
+    ll ZR = (XR * AR + YR * BR + CR) % MOD;
+    XR = YR;
+    YR = ZR;
+    return ZR;
+}
+
+const int mod = 1e9 + 7;
+
+ll bpow(ll x, ll y){
+    if (y == 0)
+        return 1;
+    if (y == 1)
+        return x;
+    ll ret = bpow(x, y >> 1);
+    ret = (ret * ret) % mod;
+    if (y & 1)
+        ret = (ret * x) % mod;
+    return ret;
+}
+
+ll bdiv(ll x, ll y){
+    return (x * bpow(y, mod - 2)) % mod;
+}
+
+const ll llinf = 2e18 + 100;
+
+const int maxn = 1e5 + 100, inf = 1e9 + 100, sq = 300;
+
+int get(int x){
+    int r = 0;
+    while (x > 0)
+        r += x % 10, x /= 10;
+    return r;
+}
+
+int main()
+{
+    #ifdef ONPC
+    //ifstream cin("a.in");
+    //ofstream cout("a.out");
+    freopen("a.in", "r", stdin);
+    freopen("a.out", "w", stdout);
+    #else
+    //ifstream cin("a.in");
+    //ofstream cout("a.out");
+    //freopen("trap.in", "r", stdin);
+    //freopen("trap.out", "w", stdout);
+    #endif // ONPC
+    ios::sync_with_stdio(0);
+    int n;
+    cin >> n;
+    vector<int> answer;
+    for (int i = max(1, n - 100); i <= n; i++){
+        if (n == i + get(i)){
+            answer.push_back(i);
+        }
+    }
+    cout << answer.size() << '\n';
+    for (auto i : answer)
+        cout << i << '\n';
+}
