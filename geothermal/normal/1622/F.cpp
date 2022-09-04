e753120da9bb2e89ca01b7856edd56d1f30e6553
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 03:42:48 +0530

    geothermal

---
+++ geothermal/normal/1622/F.cpp
@@ -0,0 +1,160 @@
+#include "bits/stdc++.h"
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+ 
+using namespace std;
+ 
+typedef long long ll;
+typedef long double ld;
+typedef complex<ld> cd;
+ 
+typedef pair<int, int> pi;
+typedef pair<ll,ll> pl;
+typedef pair<ld,ld> pd;
+ 
+typedef vector<int> vi;
+typedef vector<ld> vd;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+ 
+#define FOR(i, a, b) for (int i=a; i<(b); i++)
+#define F0R(i, a) for (int i=0; i<(a); i++)
+#define FORd(i,a,b) for (int i = (b)-1; i >= a; i--)
+#define F0Rd(i,a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a,x) for (auto& a : x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+ 
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+#define all(x) x.begin(), x.end()
+#define ins insert
+
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+ 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ", "; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? ", " : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifdef DEBUG
+#define dbg(x...) cerr << "\e[91m"<<__func__<<":"<<__LINE__<<" [" << #x << "] = ["; _print(x); cerr << "\e[39m" << endl;
+#else
+#define dbg(x...)
+#endif
+
+
+const int MOD = 1000000007;
+const char nl = '\n';
+const int MX = 100001; 
+
+void solve() {
+
+    int N; cin >> N;
+    if (N == 1) {
+        cout << "1\n1\n"; return;
+    }
+    while (true) {
+        ll hsh[N+1];
+        F0R(i, N+1) {
+            hsh[i] = uniform_int_distribution<ll>(1, (1ll << 62) - 1)(rng);
+        }
+        vi facs[N+1];
+        FOR(i, 2, N+1) {
+            if (sz(facs[i])) continue;
+            for (ll j = i; j <= N; j *= i) {
+                for (ll k = j; k <= N; k += j) {
+                    facs[k].pb(i);
+                }
+            }
+        }
+
+        ll val[N+1]; val[1] = 0;
+        FOR(i, 2, N+1) {
+            val[i] = val[i-1];
+            trav(a, facs[i]) {
+                val[i] ^= hsh[a];
+            }
+        }
+
+        ll cur = 0;
+        FOR(i, 1, N+1) {
+            cur ^= val[i];
+        }
+
+        map<ll, int> vals;
+        FOR(i, 1, N+1) {
+            vals[val[i]] = i;
+        }
+
+        FOR(i, 1, N+1) {
+            if (cur == val[i]) {
+                cout << N-1 << nl;
+                FOR(j, 1, i) {
+                    cout << j << " ";
+                }
+                FOR(j, i+1, N+1) {
+                    cout << j << " ";
+                }
+                cout << nl; return;
+            }
+        }
+
+
+        FOR(i, 1, N+1) {
+            if (vals.count(val[i]^cur)) {
+                int X = vals[val[i]^cur];
+                cout << N-2 << nl;
+                FOR(j, 1, N+1) {
+                    if (j == X || j == i) continue;
+                    cout << j << " ";
+                }
+                cout << nl; return;
+            }
+        }
+        N--;
+    }
+
+
+}
+ 
+int main() {
+    cin.tie(0)->sync_with_stdio(0); 
+    cin.exceptions(cin.failbit);
+
+    int T = 1;
+//    cin >> T;
+    while(T--) {
+        solve();
+    }
+
+	return 0;
+}
