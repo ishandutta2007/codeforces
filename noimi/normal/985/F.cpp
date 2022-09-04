6e788335246fe4e9f9aca4208383011ebd9f6cc4
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 02:24:14 +0530

    noimi

---
+++ noimi/normal/985/F.cpp
@@ -0,0 +1,159 @@
+#pragma region Macros
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep2(i,a,b) for(ll i=a;i<=b;++i)
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rep3(i,a,b) for(ll i=a;i>=b;i--)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pq priority_queue
+#define pb push_back
+#define eb emplace_back
+#define vec vector<int>
+#define vecll vector<ll>
+#define vecpii vector<pii>
+#define vecpll vector<pll>
+#define vec2(a,b) vector<vec>(a,vec(b))
+#define vec2ll(a,b) vector<vecll>(a,vecll(b))
+#define vec3(a,b,c) vector<vector<vec>>(a,vec2(b,c))
+#define vec3ll(a,b,c) vector<vector<vecll>>(a,vec2ll(b,c))
+#define fi first
+#define se second
+#define endl "\n"
+#define all(c) begin(c),end(c)
+#define ios ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(c,x) distance(c.begin(),lower_bound(all(c),(x)))
+#define ub(c,x) distance(c.begin(),upper_bound(all(c),(x)))
+using namespace std;
+int in() {int x;cin>>x;return x;}
+ll lin() {ll x;cin>>x;return x;}
+string stin() {string s;cin>>s;return s;}
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
+vec iota(int n){vec a(n);iota(all(a),0);return a;}
+void print(){putchar(' ');}
+void print(bool a){cout<<a;}
+void print(int a){cout<<a;}
+void print(long long a){cout<<a;}
+void print(char a){cout<<a;}
+void print(string &a){cout<<a;}
+void print(double a){cout<<a;}
+template<class T> void print(const vector<T>&);
+template<class T, size_t size> void print(const array<T, size>&);
+template<class T, class L> void print(const pair<T, L>& p);
+template<class T, size_t size> void print(const T (&)[size]);
+template<class T> void print(const vector<T>& a){ if(a.empty()) return; print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } cout<<endl;}
+template<class T> void print(const deque<T>& a){ if(a.empty()) return; print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } }
+template<class T, size_t size> void print(const array<T, size>& a){ print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } }
+template<class T, class L> void print(const pair<T, L>& p){ cout<<'(';print(p.first); cout<<","; print(p.second);cout<<')'; }
+template<class T, size_t size> void print(const T (&a)[size]){ print(a[0]); for(auto i = a; ++i != end(a); ){ cout<<" "; print(*i); } }
+template<class T> void print(const T& a){ cout << a; }
+int out(){ putchar('\n'); return 0; }
+template<class T> int out(const T& t){ print(t); putchar('\n'); return 0; }
+template<class Head, class... Tail> int out(const Head& head, const Tail&... tail){ print(head); putchar(' '); out(tail...); return 0; }
+ll gcd(ll a, ll b){ while(b){ ll c = b; b = a % b; a = c; } return a; }
+ll lcm(ll a, ll b){ if(!a || !b) return 0; return a * b / gcd(a, b); }
+#define _GLIBCXX_DEBU
+#ifdef _MY_DEBUG
+    #undef endl
+    #define debug(x) cout<<#x<<": "<<x<<endl
+    void err(){}
+    template<class T> void err(const T& t){ print(t);  cout<<" ";}
+    template<class Head, class... Tail> void err(const Head& head, const Tail&... tail){ print(head); putchar(' '); out(tail...); }
+#else
+    #define debug(x)
+    template<class... T> void err(const T&...){}
+#endif
+#pragma endregion
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+template< unsigned mod >
+struct RollingHash {
+  vector< unsigned > hashed, power;
+
+  inline unsigned mul(unsigned a, unsigned b) const {
+    unsigned long long x = (unsigned long long) a * b;
+    unsigned xh = (unsigned) (x >> 32), xl = (unsigned) x, d, m;
+    asm("divl %4; \n\t" : "=a" (d), "=d" (m) : "d" (xh), "a" (xl), "r" (mod));
+    return m;
+  }
+
+  RollingHash(const string &s, unsigned base = 10007) {
+    int sz = (int) s.size();
+    hashed.assign(sz + 1, 0);
+    power.assign(sz + 1, 0);
+    power[0] = 1;
+    for(int i = 0; i < sz; i++) {
+      power[i + 1] = mul(power[i], base);
+      hashed[i + 1] = mul(hashed[i], base) + s[i];
+      if(hashed[i + 1] >= mod) hashed[i + 1] -= mod;
+    }
+  }
+
+  unsigned get(int l, int r) const {
+    unsigned ret = hashed[r] + mod - mul(hashed[l], power[r - l]);
+    if(ret >= mod) ret -= mod;
+    return ret;
+  }
+
+  unsigned connect(unsigned h1, int h2, int h2len) const {
+    unsigned ret = mul(h1, power[h2len]) + h2;
+    if(ret >= mod) ret -= mod;
+    return ret;
+  }
+
+  int LCP(const RollingHash< mod > &b, int l1, int r1, int l2, int r2) {
+    int len = min(r1 - l1, r2 - l2);
+    int low = -1, high = len + 1;
+    while(high - low > 1) {
+      int mid = (low + high) / 2;
+      if(get(l1, l1 + mid) == b.get(l2, l2 + mid)) low = mid;
+      else high = mid;
+    }
+    return (low);
+  }
+};
+
+using RH = RollingHash< 1000000007 >;
+using RH2 = RollingHash< 100000017 > ;
+
+signed main(){
+    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);cout<<fixed<<setprecision(15);
+    int n=in(),m=in();
+    int base1=rng()%100000;
+    int base2=rng()%100000;
+    vector<RH> rh;
+    vector<RH2> rh2;
+    string s;cin>>s;
+    rep(j,26){
+        string t;
+        rep(i,n){
+            if(s[i]=='a'+j)t.pb(1);
+            else t.pb(0);
+        }
+        RH ro(t,base1);
+        RH2 ro2(t,base2);
+        rh.pb(ro);
+        rh2.pb(ro2);
+    }
+        
+    rep(i,m){
+        int x=in(),y=in(),l=in();
+        vecll a,b;
+        vecll a2,b2;
+        rep(j,26){
+            a.pb(rh[j].get(x-1,x+l-1));
+            a2.pb(rh2[j].get(x-1,x+l-1));
+            b.pb(rh[j].get(y-1,y+l-1));
+            b2.pb(rh2[j].get(y-1,y+l-1));
+        }
+        sort(all(a));
+        sort(all(a2));
+        sort(all(b));
+        sort(all(b2));
+        cout<<(a==b and a2==b2 ? "YES\n":"NO\n");
+    }
+}
