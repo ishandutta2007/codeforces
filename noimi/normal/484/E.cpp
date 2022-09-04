d94b83474f364ecd820cd0d4a09ddb38190a9176
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 00:49:23 +0530

    noimi

---
+++ noimi/normal/484/E.cpp
@@ -0,0 +1,404 @@
+#pragma region Macros
+#pragma GCC optimize("O3")
+#pragma GCC target("avx2,avx")
+#pragma GCC optimize("unroll-loops")
+#pragma GCC target "sse4.2"
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep2(i, a, b) for(ll i = a; i <= b; ++i)
+#define rep(i, n) for(ll i = 0; i < n; ++i)
+#define rep3(i, a, b) for(ll i = a; i >= b; --i)
+#define pii pair<int, int>
+#define pll pair<ll, ll>
+#define pb push_back
+#define eb emplace_back
+#define vi vector<int>
+#define vll vector<ll>
+#define vpi vector<pii>
+#define vpll vector<pll>
+#define overload2(_1, _2, name, ...) name
+#define vec(type, name, ...) vector<type> name(__VA_ARGS__)
+#define VEC(type, name, size)                                                                                                                                  \
+    vector<type> name(size);                                                                                                                                   \
+    IN(name)
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define VV(type, name, h, w)                                                                                                                                   \
+    vector<vector<type>> name(h, vector<type>(w));                                                                                                             \
+    IN(name)
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...)                                                                                                                         \
+    vector<vector<vector<vector<type>>>> name(a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define fi first
+#define se second
+#define all(c) begin(c), end(c)
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+using namespace std;
+const string YESNO[2] = {"NO", "YES"};
+const string YesNo[2] = {"No", "Yes"};
+const string yesno[2] = {"no", "yes"};
+void YES(bool t = 1) { cout << YESNO[t] << endl; }
+void Yes(bool t = 1) { cout << YesNo[t] << endl; }
+void yes(bool t = 1) { cout << yesno[t] << endl; }
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+#define si(c) (int)(c).size()
+#define INT(...)                                                                                                                                               \
+    int __VA_ARGS__;                                                                                                                                           \
+    IN(__VA_ARGS__)
+#define LL(...)                                                                                                                                                \
+    ll __VA_ARGS__;                                                                                                                                            \
+    IN(__VA_ARGS__)
+#define STR(...)                                                                                                                                               \
+    string __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+#define CHR(...)                                                                                                                                               \
+    char __VA_ARGS__;                                                                                                                                          \
+    IN(__VA_ARGS__)
+#define DBL(...)                                                                                                                                               \
+    double __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &);
+template <class T> void scan(vector<T> &a) {
+    for(auto &i : a) scan(i);
+}
+template <class T> void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &... tail) {
+    scan(head);
+    IN(tail...);
+}
+template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
+template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
+vi iota(int n) {
+    vi a(n);
+    iota(all(a), 0);
+    return a;
+}
+template <typename T> vi iota(vector<T> &a, bool greater = false) {
+    vi res(a.size());
+    iota(all(res), 0);
+    sort(all(res), [&](int i, int j) {
+        if(greater) return a[i] > a[j];
+        return a[i] < a[j];
+    });
+    return res;
+}
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+template <class T> T POW(T x, int n) {
+    T res = 1;
+    for(; n; n >>= 1, x *= x)
+        if(n & 1) res *= x;
+    return res;
+}
+vector<pll> factor(ll x) {
+    vector<pll> ans;
+    for(ll i = 2; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.push_back({i, 1});
+            while((x /= i) % i == 0) ans.back().second++;
+        }
+    if(x != 1) ans.push_back({x, 1});
+    return ans;
+}
+template <class T> vector<T> divisor(T x) {
+    vector<T> ans;
+    for(T i = 1; i * i <= x; i++)
+        if(x % i == 0) {
+            ans.pb(i);
+            if(i * i != x) ans.pb(x / i);
+        }
+    return ans;
+}
+template <typename T> void zip(vector<T> &x) {
+    vector<T> y = x;
+    sort(all(y));
+    for(int i = 0; i < x.size(); ++i) { x[i] = lb(y, x[i]); }
+}
+int popcount(ll x) { return __builtin_popcountll(x); }
+int in() {
+    int x;
+    cin >> x;
+    return x;
+}
+ll lin() {
+    unsigned long long x;
+    cin >> x;
+    return x;
+}
+
+template <typename T> struct edge {
+    int from, to;
+    T cost;
+    int id;
+    edge(int to, T cost) : from(-1), to(to), cost(cost) {}
+    edge(int from, int to, T cost) : from(from), to(to), cost(cost) {}
+    edge(int from, int to, T cost, int id) : from(from), to(to), cost(cost), id(id) {}
+    edge &operator=(const int &x) {
+        to = x;
+        return *this;
+    }
+    operator int() const { return to; }
+};
+template <typename T> using Edges = vector<edge<T>>;
+
+using Tree = vector<vector<int>>;
+using Graph = vector<vector<int>>;
+template <class T> using Wgraph = vector<vector<edge<T>>>;
+Graph getG(int n, int m = -1, bool directed = false, int margin = 1) {
+    Tree res(n);
+    if(m == -1) m = n - 1;
+    while(m--) {
+        int a, b;
+        cin >> a >> b;
+        a -= margin, b -= margin;
+        res[a].emplace_back(b);
+        if(!directed) res[b].emplace_back(a);
+    }
+    return move(res);
+}
+template <class T> Wgraph<T> getWg(int n, int m = -1, bool directed = false, int margin = 1) {
+    Wgraph<T> res(n);
+    if(m == -1) m = n - 1;
+    while(m--) {
+        int a, b;
+        T c;
+        cin >> a >> b >> c;
+        a -= margin, b -= margin;
+        res[a].emplace_back(b, c);
+        if(!directed) res[b].emplace_back(a, c);
+    }
+    return move(res);
+}
+
+#define i128 __int128_t
+#define ull unsigned long long int
+#define TEST                                                                                                                                                   \
+    INT(testcases);                                                                                                                                            \
+    while(testcases--)
+template <class T> ostream &operator<<(ostream &os, const vector<T> &v) {
+    for(auto it = begin(v); it != end(v); ++it) {
+        if(it == begin(v))
+            os << *it;
+        else
+            os << " " << *it;
+    }
+    return os;
+}
+template <class T, class S> ostream &operator<<(ostream &os, const pair<T, S> &p) {
+    os << p.first << " " << p.second;
+    return os;
+}
+template <class S, class T> string to_string(pair<S, T> p) { return "(" + to_string(p.first) + "," + to_string(p.second) + ")"; }
+template <class A> string to_string(A v) {
+    if(v.empty()) return "{}";
+    string ret = "{";
+    for(auto &x : v) ret += to_string(x) + ",";
+    ret.back() = '}';
+    return ret;
+}
+
+void dump() { cerr << endl; }
+template <class Head, class... Tail> void dump(Head head, Tail... tail) {
+    cerr << to_string(head) << " ";
+    dump(tail...);
+}
+#define endl '\n'
+#ifdef _LOCAL
+#undef endl
+#define debug(x)                                                                                                                                               \
+    cout << #x << ": ";                                                                                                                                        \
+    dump(x)
+#else
+#define debug(x)
+#endif
+
+template <typename T> static constexpr T inf = numeric_limits<T>::max() / 2;
+struct Setup_io {
+    Setup_io() {
+        ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+        cout << fixed << setprecision(15);
+    }
+} setup_io;
+#define drop(s) cout << #s << endl, exit(0)
+#pragma endregion
+
+/**
+ * @brief Lazy-Segment-Tree()
+ * @docs docs/lazy-segment-tree.md
+ */
+template <typename Monoid, typename OperatorMonoid, typename F, typename G, typename H> struct LazySegmentTree {
+    int sz, height;
+    vector<Monoid> data;
+    vector<OperatorMonoid> lazy;
+    const F f;
+    const G g;
+    const H h;
+    const Monoid M1;
+    const OperatorMonoid OM0;
+
+    LazySegmentTree(int n, const F f, const G g, const H h, const Monoid &M1, const OperatorMonoid OM0) : f(f), g(g), h(h), M1(M1), OM0(OM0) {
+        sz = 1;
+        height = 0;
+        while(sz < n) sz <<= 1, height++;
+        data.assign(2 * sz, M1);
+        lazy.assign(2 * sz, OM0);
+    }
+
+    void set(int k, const Monoid &x) { data[k + sz] = x; }
+
+    void build() {
+        for(int k = sz - 1; k > 0; k--) { data[k] = f(data[2 * k + 0], data[2 * k + 1]); }
+    }
+
+    inline void propagate(int k) {
+        if(lazy[k] != OM0) {
+            lazy[2 * k + 0] = h(lazy[2 * k + 0], lazy[k]);
+            lazy[2 * k + 1] = h(lazy[2 * k + 1], lazy[k]);
+            data[k] = apply(k);
+            lazy[k] = OM0;
+        }
+    }
+
+    inline Monoid apply(int k) { return lazy[k] == OM0 ? data[k] : g(data[k], lazy[k]); }
+
+    inline void recalc(int k) {
+        while(k >>= 1) data[k] = f(apply(2 * k + 0), apply(2 * k + 1));
+    }
+
+    inline void thrust(int k) {
+        for(int i = height; i > 0; i--) propagate(k >> i);
+    }
+
+    void update(int a, int b, const OperatorMonoid &x) {
+        if(a >= b) return;
+        thrust(a += sz);
+        thrust(b += sz - 1);
+        for(int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+            if(l & 1) lazy[l] = h(lazy[l], x), ++l;
+            if(r & 1) --r, lazy[r] = h(lazy[r], x);
+        }
+        recalc(a);
+        recalc(b);
+    }
+
+    Monoid query(int a, int b) {
+        if(a >= b) return M1;
+        thrust(a += sz);
+        thrust(b += sz - 1);
+        Monoid L = M1, R = M1;
+        for(int l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+            if(l & 1) L = f(L, apply(l++));
+            if(r & 1) R = f(apply(--r), R);
+        }
+        return f(L, R);
+    }
+
+    Monoid operator[](const int &k) { return query(k, k + 1); }
+
+    template <typename C> int find_subtree(int a, const C &check, Monoid &M, bool type) {
+        while(a < sz) {
+            propagate(a);
+            Monoid nxt = type ? f(apply(2 * a + type), M) : f(M, apply(2 * a + type));
+            if(check(nxt))
+                a = 2 * a + type;
+            else
+                M = nxt, a = 2 * a + 1 - type;
+        }
+        return a - sz;
+    }
+
+    template <typename C> int find_first(int a, const C &check) {
+        Monoid L = M1;
+        if(a <= 0) {
+            if(check(f(L, apply(1)))) return find_subtree(1, check, L, false);
+            return -1;
+        }
+        thrust(a + sz);
+        int b = sz;
+        for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+            if(a & 1) {
+                Monoid nxt = f(L, apply(a));
+                if(check(nxt)) return find_subtree(a, check, L, false);
+                L = nxt;
+                ++a;
+            }
+        }
+        return -1;
+    }
+
+    template <typename C> int find_last(int b, const C &check) {
+        Monoid R = M1;
+        if(b >= sz) {
+            if(check(f(apply(1), R))) return find_subtree(1, check, R, true);
+            return -1;
+        }
+        thrust(b + sz - 1);
+        int a = sz;
+        for(b += sz; a < b; a >>= 1, b >>= 1) {
+            if(b & 1) {
+                Monoid nxt = f(apply(--b), R);
+                if(check(nxt)) return find_subtree(b, check, R, true);
+                R = nxt;
+            }
+        }
+        return -1;
+    }
+};
+
+template <typename Monoid, typename OperatorMonoid, typename F, typename G, typename H>
+LazySegmentTree<Monoid, OperatorMonoid, F, G, H> get_lazy_segment_tree(int N, const F &f, const G &g, const H &h, const Monoid &M1, const OperatorMonoid &OM0) {
+    return {N, f, g, h, M1, OM0};
+}
+
+template <typename T = int> auto _max = [](T x, T y) { return max(x, y); };
+
+int main() {
+    INT(n);
+    VEC(int, a, n);
+    INT(Q);
+    vi l(Q), r(Q), w(Q);
+    rep(i, Q) cin >> l[i] >> r[i] >> w[i], l[i]--;
+    vi ok(Q), ng(Q, inf<int>), mid(Q);
+    vpi v;
+    rep(i, n) v.eb(a[i], i);
+    sort(all(v), greater{});
+    while(1) {
+        bool ch = false;
+        rep(i, Q) {
+            ch |= (ng[i] - ok[i]) > 1;
+            mid[i] = (ok[i] + ng[i]) >> 1;
+        }
+        if(!ch) break;
+        auto id = iota(Q);
+        sort(all(id), [&](int i, int j) { return mid[i] > mid[j]; });
+        auto seg = get_lazy_segment_tree(n, _max<int>, plus<int>(), plus<int>(), 0, 0);
+        set<int> unused;
+        rep2(i, -1, n) unused.emplace(i);
+        int p = 0;
+        for(auto [h, i] : v) {
+            while(p < Q and mid[id[p]] > h) {
+                (seg.query(l[id[p]], r[id[p]] - w[id[p]] + 1) >= w[id[p]] ? ok[id[p]] : ng[id[p]]) = mid[id[p]];
+                p++;
+            }
+            unused.erase(i);
+            auto it = unused.lower_bound(i);
+            auto pit = prev(it);
+            int add = *it - i;
+            seg.update(*pit + 1, i + 1, add);
+        }
+        while(p < Q) {
+            (seg.query(l[id[p]], r[id[p]] - w[id[p]] + 1) >= w[id[p]] ? ok[id[p]] : ng[id[p]]) = mid[id[p]];
+            p++;
+        }
+    }
+    rep(i, Q) cout << ok[i] << endl;
+}
