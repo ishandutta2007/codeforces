3104ef5f50855b3af879f965c959572689b9f1ca
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 18:59:34 +0530

    fizzydavid

---
+++ fizzydavid/normal/409/D.cpp
@@ -0,0 +1,23 @@
+#include<algorithm>
+#include<cmath>
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<iostream>
+#include<map>
+#include<queue>
+#include<set>
+#include<stack>
+#include<string>
+#include<ctime>
+#include<utility>
+#include<vector>
+using namespace std; 
+const string ts="/1001010111001010";
+int main()
+{
+	int x;
+	cin>>x;
+	cout<<ts[x];
+ 	return 0;
+}
