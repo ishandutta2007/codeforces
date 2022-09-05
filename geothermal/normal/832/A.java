194d63974d231505a5ef504924dd46f0acce8c15
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 12:39:21 +0530

    geothermal

---
+++ geothermal/normal/832/A.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class StickGame832A {
+    public static void main(String[] args) throws IOException {
+        FastScanner in = new FastScanner();
+        long n = in.nextLong();
+        long k = in.nextLong();
+        
+        if (n % (2 * k) >= k) {
+            System.out.println("YES");
+        } else System.out.println("NO");
+    }
+
+    public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(String s) {
+            try {
+                br = new BufferedReader(new FileReader(s));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String nextToken() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            st = null;
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(nextToken());
+        }
+
+        long nextLong() {
+            return Long.parseLong(nextToken());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(nextToken());
+        }
+    }
+}
