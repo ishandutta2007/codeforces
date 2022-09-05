50e4e408f4632464d9aea828222805daefbda193
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 19:19:19 +0530

    cuizhuyefei

---
+++ cuizhuyefei/normal/1322/A.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define mp make_pair
+#define SZ(x) ((int)x.size())
+#define ALL(x) x.begin(),x.end()
+#define L(i,u) for (register int i=head[u]; i; i=nxt[i])
+#define rep(i,a,b) for (register int i=(a); i<=(b); i++)
+#define per(i,a,b) for (register int i=(a); i>=(b); i--)
+using namespace std;
+typedef long double ld;
+typedef long long ll;
+typedef unsigned int ui;
+typedef pair<int,int> Pii;
+typedef vector<int> Vi;
+template<class T> inline void read(T &x){
+	x=0; char c=getchar(); int f=1;
+	while (!isdigit(c)) {if (c=='-') f=-1; c=getchar();}
+	while (isdigit(c)) {x=x*10+c-'0'; c=getchar();} x*=f;
+}
+template<class T> T gcd(T a, T b){return !b?a:gcd(b,a%b);}
+template<class T> inline void umin(T &x, T y){x=x<y?x:y;}
+template<class T> inline void umax(T &x, T y){x=x>y?x:y;}
+mt19937 R(chrono::system_clock().now().time_since_epoch().count());
+const int N = 1766666;
+int a[N],r,n;char s[N];
+int main() {
+	read(n);scanf("%s",s+1);
+	rep(i,1,n)a[i]=a[i-1]+(s[i]=='('?1:-1);
+	if(a[n]){puts("-1");return 0;}
+	rep(i,1,n)if(a[i]<0){
+		int j=i;while(a[j]<0)j++;
+		r+=j-i+1;i=j;
+	}
+	cout<<r;
+	return 0;
+}
