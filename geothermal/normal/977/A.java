3eee4110bd5e4d2f423298f38a8d5a3522102d65
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 12:33:55 +0530

    geothermal

---
+++ geothermal/normal/977/A.java
@@ -0,0 +1,69 @@
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class A977 {
+    public static void main(String[] args) throws IOException {
+        FastScanner in = new FastScanner();
+        int n = in.nextInt(); int k = in.nextInt();
+        for (int i = 0; i < k; i++) {
+            if (n % 10 != 0) {
+                n--;
+            } else n = n / 10;
+        }
+        System.out.println(n);
+    }
+
+    public static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        public FastScanner(String s) {
+            try {
+                br = new BufferedReader(new FileReader(s));
+            } catch (FileNotFoundException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+
+        public FastScanner() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+
+        String nextToken() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        String nextLine() {
+            st = null;
+            try {
+                return br.readLine();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        int nextInt() {
+            return Integer.parseInt(nextToken());
+        }
+
+        long nextLong() {
+            return Long.parseLong(nextToken());
+        }
+
+        double nextDouble() {
+            return Double.parseDouble(nextToken());
+        }
+    }
+}
