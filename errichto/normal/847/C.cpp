7bb031337b25e3c2c957e3b582087353941afd1d
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 13:02:17 +0530

    errichto

---
+++ errichto/normal/847/C.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+	*this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+// debug & operator << (debug & dd, P p) { dd << "(" << p.x << ", " << p.y << ")"; return dd; }
+
+
+
+int main() {
+	long long n, k;
+	cin >> n >> k;
+	int open = 0;
+	string s;
+	for(int rep = 0; rep < n; ++rep) {
+		while(open > 0 && k < open) {
+			s += ')';
+			open--;
+		}
+		k -= open;
+		s += '(';
+		open++;
+		debug() << imie(rep) imie(open) imie(n) imie(s);
+	}
+	while(open--) {
+		s += ')';
+	}
+	if(k == 0) {
+		printf("%s\n", s.c_str());
+	}
+	else puts("Impossible");
+}
