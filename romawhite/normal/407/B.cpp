8a797ed9433ce1a92e414f1d47ec48d806f6df8e
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 15:18:32 +0530

    romawhite

---
+++ romawhite/normal/407/B.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(linker, "/stack:16777216")
+#include <string>
+#include <vector>
+#include <map>
+#include <list>
+#include <iterator>
+#include <set>
+#include <queue>
+#include <iostream>
+#include <sstream>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <memory.h>
+#include <cstdlib>
+#include <cstdio>
+#include <cctype>
+#include <algorithm>
+#include <utility>
+#include <fstream>
+#include <time.h>
+using namespace std;
+
+#define FOR(i, a, b) for(int i = (a); i < (b); ++i)
+#define RFOR(i, b, a) for(int i = (b) - 1; i >= (a); --i)
+#define REP(i, N) FOR(i, 0, N)
+#define RREP(i, N) RFOR(i, N, 0)
+#define FILL(A,value) memset(A,value,sizeof(A))
+#define ALL(V) V.begin(), V.end()
+#define SZ(V) (int)V.size()
+#define PB push_back
+#define MP make_pair
+#define y0 gfdssdfer
+#define y1 wetgwervbwqr
+
+typedef long long Int;
+
+const int INF = 1000000000;
+const int MOD = 1000000007;
+
+int res[1007];
+int dp[1007];
+int p[1007];
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    FOR(i,0,n){
+        cin >> p[i];
+        --p[i];
+    }
+    res[0] = 0;
+    int ans = 0;
+    FOR(i,0,n)
+    {
+        res[i + 1] = (2LL * res[i] - res[p[i]] + 2 + MOD) % MOD;
+    }
+
+
+    cout << res[n] << endl;
+}
