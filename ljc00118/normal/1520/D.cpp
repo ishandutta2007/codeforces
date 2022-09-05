6f157b1134dd322bb7154a92e0eef7f0926933d6
ishan dutta <ishandutta2007@gmail.com>
2022-09-06 02:17:54 +0530

    ljc00118

---
+++ ljc00118/normal/1520/D.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#define rep(i, a, b) for (int i = a; i <= b; i++)
+#define per(i, a, b) for (int i = a; i >= b; i--)
+using namespace std;
+
+typedef unsigned long long ull;
+typedef pair <int, int> pii;
+typedef long long ll;
+
+template <typename T>
+inline void read(T &f) {
+    f = 0; T fu = 1; char c = getchar();
+    while (c < '0' || c > '9') { if (c == '-') { fu = -1; } c = getchar(); }
+    while (c >= '0' && c <= '9') { f = (f << 3) + (f << 1) + (c & 15); c = getchar(); }
+    f *= fu;
+}
+
+template <typename T>
+void print(T x) {
+    if (x < 0) putchar('-'), x = -x;
+    if (x < 10) putchar(x + 48);
+    else print(x / 10), putchar(x % 10 + 48);
+}
+
+template <typename T>
+void print(T x, char t) {
+    print(x); putchar(t);
+}
+
+const int N = 2e5 + 5;
+
+unordered_map <int, int> cnt;
+int T, n; ll ans;
+
+int main() {
+    read(T);
+    while (T--) {
+        read(n);
+        ll ans = 0;
+        cnt.clear();
+        for (int i = 1; i <= n; i++) {
+            int x; read(x);
+            x -= i;
+            ans += cnt[x];
+            ++cnt[x];
+        }
+        print(ans, '\n');
+    }
+    return 0;
+}
