bb464b50695c423e127137a8f79b3af3cdfcafe6
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 13:26:18 +0530

    errichto

---
+++ errichto/normal/1033/B.cpp
@@ -0,0 +1,57 @@
+#include "bits/stdc++.h"
+using namespace std;
+#define sim template < class c
+#define ris return * this
+#define dor > debug & operator <<
+#define eni(x) sim > typename \
+  enable_if<sizeof dud<c>(0) x 1, debug&>::type operator<<(c i) {
+sim > struct rge { c b, e; };
+sim > rge<c> range(c i, c j) { return rge<c>{i, j}; }
+sim > auto dud(c* x) -> decltype(cerr << *x, 0);
+sim > char dud(...);
+struct debug {
+#ifdef LOCAL
+~debug() { cerr << endl; }
+eni(!=) cerr << boolalpha << i; ris; }
+eni(==) ris << range(begin(i), end(i)); }
+sim, class b dor(pair < b, c > d) {
+  ris << "(" << d.first << ", " << d.second << ")";
+}
+sim dor(rge<c> d) {
+  *this << "[";
+  for (auto it = d.b; it != d.e; ++it)
+    *this << ", " + 2 * (it == d.b) << *it;
+  ris << "]";
+}
+#else
+sim dor(const c&) { ris; }
+#endif
+};
+#define imie(...) " [" << #__VA_ARGS__ ": " << (__VA_ARGS__) << "] "
+
+typedef long long ll;
+
+void test_case() {
+	ll a, b;
+	cin >> a >> b;
+	if(a - b >= 2) {
+		puts("NO");
+		return;
+	}
+	for(ll i = 2; i * i <= a + b; ++i) {
+		if((a + b) % i == 0) {
+			debug() << imie(i);
+			puts("NO");
+			return;
+		}
+	}
+	puts("YES");
+}
+
+int main() {
+	int T;
+	cin >> T;
+	while(T--) {
+		test_case();
+	}
+}
