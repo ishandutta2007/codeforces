ee4c06e7c706f149dd16972e69a02308b89812ce
ishan dutta <ishandutta2007@gmail.com>
2022-09-05 20:17:05 +0530

    cuizhuyefei

---
+++ cuizhuyefei/normal/1028/A.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#define fi first
+#define se second
+#define pb push_back
+#define SZ(x) ((int)x.size())
+#define L(i,u) for (register int i=head[u]; i; i=nxt[i])
+#define rep(i,a,b) for (register int i=a; i<=b; i++)
+#define per(i,a,b) for (register int i=a; i>=b; i--)
+using namespace std;
+typedef long long ll;
+typedef unsigned int ui;
+typedef pair<int,int> Pii;
+typedef vector<int> Vi;
+inline void read(int &x) {
+	x=0; char c=getchar(); int f=1;
+	while (!isdigit(c)) {if (c=='-') f=-1; c=getchar();}
+	while (isdigit(c)) {x=x*10+c-'0'; c=getchar();} x*=f;
+}
+inline ui R() {
+	static ui seed=416;
+	return seed^=seed>>5,seed^=seed<<17,seed^=seed>>13;
+}
+const int N = 555;
+int n,m;char s[N][N];
+int cnt,x,y;
+int main() {
+	read(n);read(m);rep(i,1,n)scanf("%s",s[i]+1);
+	rep(i,1,n)rep(j,1,m)if(s[i][j]=='B')cnt++,x+=i,y+=j;
+	printf("%d %d",x/cnt,y/cnt);
+	return 0;
+}
