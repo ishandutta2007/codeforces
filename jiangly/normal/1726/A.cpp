329f65d1023ba761870621c41a87f02c15f329ae
ishan dutta <ishandutta2007@gmail.com>
2022-09-08 16:12:02 +0530

    jiangly

---
+++ jiangly/normal/1726/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using i64 = long long;
+
+void solve() {
+    int n;
+    std::cin >> n;
+    
+    std::vector<int> a(n);
+    for (int i = 0; i < n; i++) {
+        std::cin >> a[i];
+    }
+    
+    int ans = a[n - 1] - a[0];
+    for (int i = 0; i < n; i++) {
+        ans = std::max(ans, a[i] - a[(i + 1) % n]);
+    }
+    
+    for (int i = 0; i < n; i++) {
+        if (i > 0) {
+            ans = std::max(ans, a[i] - a[0]);
+        }
+        if (i < n - 1) {
+            ans = std::max(ans, a[n - 1] - a[i]);
+        }
+    }
+    std::cout << ans << "\n";
+}
+
+int main() {
+    std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    
+    int t;
+    std::cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+    
+    return 0;
+}
