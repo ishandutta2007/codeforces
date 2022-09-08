3ee5d65adebc683e14f915a88d2dd7cdf80d68fe
ishan dutta <ishandutta2007@gmail.com>
2022-09-08 20:20:18 +0530

    bigbag

---
+++ bigbag/normal/116/A.cpp
@@ -0,0 +1,30 @@
+#include <map>
+#include <set>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n, a, b, c, ans;
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    cin >> n;
+    while (n--) {
+        cin >> a >> b;
+        c -= a;
+        c += b;
+        ans = max(ans, c);
+    }
+    cout << ans << endl;
+    return 0;
+}
