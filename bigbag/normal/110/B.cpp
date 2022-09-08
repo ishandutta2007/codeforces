8d29c6c0d5755c5b2fef742f69e46909573bc6f6
ishan dutta <ishandutta2007@gmail.com>
2022-09-08 20:23:11 +0530

    bigbag

---
+++ bigbag/normal/110/B.cpp
@@ -0,0 +1,26 @@
+#include <map>
+#include <set>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+    for (int i = 0; i < n; ++i) {
+        printf("%c", 'a' + i % 4);
+    }
+    return 0;
+}
