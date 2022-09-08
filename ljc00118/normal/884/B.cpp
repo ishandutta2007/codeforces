56f42d36cf0f3fae1e1c84dcd0b5edb470e83400
ishan dutta <ishandutta2007@gmail.com>
2022-09-08 23:36:57 +0530

    ljc00118

---
+++ ljc00118/normal/884/B.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+#define CIOS ios::sync_with_stdio(false);
+#define rep(i, a, b) for(register int i = a; i <= b; i++)
+#define per(i, a, b) for(register int i = a; i >= b; i--)
+#define DEBUG(x) cerr << "DEBUG" << x << " >>> ";
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+
+template <typename T>
+inline void read(T &f) {
+	f = 0; T fu = 1; char c = getchar();
+	while (c < '0' || c > '9') { if (c == '-') fu = -1; c = getchar(); }
+	while (c >= '0' && c <= '9') { f = (f << 3) + (f << 1) + (c & 15); c = getchar(); }
+	f *= fu;
+}
+
+template <typename T>
+void print(T x) {
+	if (x < 0) putchar('-'), x = -x;
+	if (x < 10) putchar(x + 48);
+	else print(x / 10), putchar(x % 10 + 48);
+}
+
+template <typename T>
+void print(T x, char t) {
+	print(x); putchar(t);
+}
+
+int n, m, sum;
+
+int main() {
+	read(n); read(m); sum = n - 1;
+	for(register int i = 1; i <= n; i++) {
+		int a; read(a); sum += a;
+	}
+	if(sum == m) cout << "YES" << endl;
+	else cout << "NO" << endl;
+	return 0;
+}
