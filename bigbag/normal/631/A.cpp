b393ea4817572214cc93fd00c3c085f7d7721508
ishan dutta <ishandutta2007@gmail.com>
2022-09-08 19:41:52 +0530

    bigbag

---
+++ bigbag/normal/631/A.cpp
@@ -0,0 +1,39 @@
+#include <map>
+#include <set>
+#include <stack>
+#include <cmath>
+#include <queue>
+#include <vector>
+#include <cstdio>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+const int max_n = 1111, inf = 1111111111;
+
+int n, ans, a[2][max_n];
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    scanf("%d", &n);
+    for (int i = 0; i < 2; ++i) {
+        for (int j = 0; j < n; ++j) {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        int x = 0, y = 0;
+        for (int j = i; j < n; ++j) {
+            x |= a[0][j];
+            y |= a[1][j];
+            ans = max(ans, x + y);
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
