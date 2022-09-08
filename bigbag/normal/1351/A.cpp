44bee032e6548be0cac490f4c0b81daca50fe7e2
ishan dutta <ishandutta2007@gmail.com>
2022-09-08 17:44:11 +0530

    bigbag

---
+++ bigbag/normal/1351/A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t, a, b;
+
+int main() {
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    cin >> t;
+    while (t--) {
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+    return 0;
+}
