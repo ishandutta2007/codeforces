a7bd08c143a60c43aeb9ca9ec581e783b7097b4a
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 09:36:54 +0530

    eatmore

---
+++ eatmore/normal/1310/A.java
@@ -0,0 +1,125 @@
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class A {
+
+	static void sortBy(int a[], int n, int v[]) {
+		if (n == 0) {
+			return;
+		}
+		for (int i = 1; i < n; i++) {
+			int j = i;
+			int ca = a[i];
+			int cv = v[ca];
+			do {
+				int nj = (j - 1) >> 1;
+				int na = a[nj];
+				if (cv <= v[na]) {
+					break;
+				}
+				a[j] = na;
+				j = nj;
+			} while (j != 0);
+			a[j] = ca;
+		}
+		int ca = a[0];
+		for (int i = n - 1; i > 0; i--) {
+			int j = 0;
+			while ((j << 1) + 2 + Integer.MIN_VALUE < i + Integer.MIN_VALUE) {
+				j <<= 1;
+				j += (v[a[j + 2]] > v[a[j + 1]]) ? 2 : 1;
+			}
+			if ((j << 1) + 2 == i) {
+				j = (j << 1) + 1;
+			}
+			int na = a[i];
+			a[i] = ca;
+			ca = na;
+			int cv = v[ca];
+			while (j != 0 && v[a[j]] < cv) {
+				j = (j - 1) >> 1;
+			}
+			while (j != 0) {
+				na = a[j];
+				a[j] = ca;
+				ca = na;
+				j = (j - 1) >> 1;
+			}
+		}
+		a[0] = ca;
+	}
+
+	static void solve() throws Exception {
+		int n = scanInt();
+		int a[] = new int[n], t[] = new int[n], idx[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = scanInt();
+			idx[i] = i;
+		}
+		for (int i = 0; i < n; i++) {
+			t[i] = scanInt();
+		}
+		sortBy(idx, n, a);
+		PriorityQueue<Integer> ts = new PriorityQueue<>();
+		long tsum = 0;
+		int ca = 0;
+		long ans = 0;
+		for (int i: idx) {
+			while (ca < a[i] && !ts.isEmpty()) {
+				int v = ts.remove();
+				tsum += v;
+				ans += tsum;
+				++ca;
+			}
+			ca = a[i];
+			ts.add(-t[i]);
+			tsum += t[i];
+		}
+		while (!ts.isEmpty()) {
+			int v = ts.remove();
+			tsum += v;
+			ans += tsum;
+		}
+		out.print(ans);
+	}
+
+	static int scanInt() throws IOException {
+		return parseInt(scanString());
+	}
+
+	static long scanLong() throws IOException {
+		return parseLong(scanString());
+	}
+
+	static String scanString() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
+	}
+
+	static BufferedReader in;
+	static PrintWriter out;
+	static StringTokenizer tok;
+
+	public static void main(String[] args) {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			out = new PrintWriter(System.out);
+			solve();
+			in.close();
+			out.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			exit(1);
+		}
+	}
+}
