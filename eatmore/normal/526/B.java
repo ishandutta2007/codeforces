fca4c8caee3fe87f728493a3305625eb0405e6bf
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 10:16:41 +0530

    eatmore

---
+++ eatmore/normal/526/B.java
@@ -0,0 +1,68 @@
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B {
+	
+	static BufferedReader in;
+	static PrintWriter out;
+	static StringTokenizer tok;
+	
+	static void solve() throws Exception {
+		int n = nextInt();
+		long a[] = new long[(1 << (n + 1)) - 1];
+		for (int i = 1; i < a.length; i++) {
+			a[i] = nextInt();
+		}
+		long ans = 0;
+		for (int i = (1 << n) - 2; i >= 0; i--) {
+			long x = a[2 * i + 1];
+			long y = a[2 * i + 2];
+			ans += max(x, y) - min(x, y);
+			a[i] += max(x, y);
+		}
+		out.print(ans);
+	}
+	
+	static int nextInt() throws IOException {
+		return parseInt(next());
+	}
+
+	static long nextLong() throws IOException {
+		return parseLong(next());
+	}
+
+	static double nextDouble() throws IOException {
+		return parseDouble(next());
+	}
+
+	static String next() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
+	}
+
+	public static void main(String[] args) {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			out = new PrintWriter(new OutputStreamWriter(System.out));
+			solve();
+			in.close();
+			out.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			exit(1);
+		}
+	}
+}
