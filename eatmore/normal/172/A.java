87f0e15c83ff3b525c368d1d77d258ffa4d31bac
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 10:28:00 +0530

    eatmore

---
+++ eatmore/normal/172/A.java
@@ -0,0 +1,66 @@
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class A {
+	
+	static BufferedReader in;
+	static PrintWriter out;
+	static StringTokenizer tok;
+	
+	static void solve() throws Exception {
+		int n = nextInt();
+		String s = next();
+		int ans = s.length();
+		for (int i = 1; i < n; i++) {
+			String ss = next();
+			for (int j = 0; j < ans; j++) {
+				if (s.charAt(j) != ss.charAt(j)) {
+					ans = j;
+					break;
+				}
+			}
+		}
+		out.println(ans);
+	}
+	
+	static int nextInt() throws IOException {
+		return parseInt(next());
+	}
+
+	static long nextLong() throws IOException {
+		return parseLong(next());
+	}
+
+	static double nextDouble() throws IOException {
+		return parseDouble(next());
+	}
+
+	static String next() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
+	}
+
+	public static void main(String[] args) {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			out = new PrintWriter(new OutputStreamWriter(System.out));
+			solve();
+			in.close();
+			out.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			exit(1);
+		}
+	}
+}
