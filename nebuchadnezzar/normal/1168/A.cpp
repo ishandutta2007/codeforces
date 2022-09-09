7b72378cc044ad8fe39c49bdb4b49296ee2b87ad
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 20:58:56 +0530

    nebuchadnezzar

---
+++ nebuchadnezzar/normal/1168/A.cpp
@@ -0,0 +1,92 @@
+// Created by Nikolay Budin
+
+#ifdef LOCAL
+#  define _GLIBCXX_DEBUG
+#else
+#  define cerr __get_ce
+#endif
+#include <bits/stdc++.h>
+#define ff first
+#define ss second
+#define szof(x) ((int)x.size())
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+int const INF = (int)1e9 + 1e3;
+ll const INFL = (ll)1e18 + 1e6;
+#ifdef LOCAL
+	mt19937 tw(9450189);
+#else
+	mt19937 tw(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+uniform_int_distribution<ll> ll_distr;
+ll rnd(ll a, ll b) { return ll_distr(tw) % (b - a + 1) + a; }
+
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	vector<int> arr;
+	for (int i = 0; i < n; ++i) {
+		int num;
+		cin >> num;
+		arr.push_back(num);
+	}
+
+	int l = -1, r = m + 1;
+	while (r - l > 1) {
+		int mid = (l + r) / 2;
+		int last = 0;
+		bool fail = false;
+		for (int i = 0; i < n; ++i) {
+			int next = INF;
+			if (arr[i] + mid < m) {
+				if (arr[i] + mid >= last) {
+					next = min(next, max(last, arr[i]));
+				}
+			} else {
+				next = min(next, max(last, arr[i]));
+				if ((arr[i] + mid) % m >= last) {
+					next = min(next, max(last, 0));
+				}
+			}
+			if (next == INF) {
+				fail = true;
+				break;
+			}
+			last = next;
+		}
+		if (fail) {
+			l = mid;
+		} else {
+			r = mid;
+		}
+	}
+
+	cout << r << "\n";
+}
+
+
+int main() {
+#ifdef LOCAL
+	auto start_time = clock();
+	cerr << setprecision(3) << fixed;
+#endif
+	cout << setprecision(15) << fixed;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int test_count = 1;
+	// cin >> test_count;
+	for (int test = 1; test <= test_count; ++test) {
+		solve();
+	}
+	
+#ifdef LOCAL
+	auto end_time = clock();
+	cerr << "Execution time: " << (end_time - start_time) * (int)1e3 / CLOCKS_PER_SEC << " ms\n";
+#endif
+}
