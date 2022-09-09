713da509de892121b59455f10870b1dcdb3539fc
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 13:28:13 +0530

    kostroma

---
+++ kostroma/normal/292/A.cpp
@@ -0,0 +1,79 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <deque>
+#include <cmath>
+#include <ctime>
+#include <stack>
+#include <set>
+#include <map>
+#include <cassert>
+#include <memory.h>
+
+using namespace std;
+
+#define mp make_pair
+#define pb push_back
+#define all(a) a.begin(), a.end()
+
+typedef long long li;
+typedef long double ld;
+typedef vector<int> vi;
+typedef pair <int, int> pi;
+
+void solve();
+//void precalc();
+
+#define FILENAME "change me please"
+int main(){
+	string s = FILENAME;
+#ifdef room210
+	freopen("in.txt", "r", stdin);
+	//freopen("out.txt", "w", stdout);
+	//cout<<FILENAME<<endl;
+	//assert (s != "change me please");
+	clock_t start = clock();
+#else
+	//freopen(FILENAME ".in", "r", stdin);
+	//freopen(FILENAME".out", "w", stdout);
+#endif
+	//cout.sync_with_stdio(0);
+	int t = 1;
+	//precalc();
+	//cout << "done!\n";
+	//cin >> t;
+	//gen();
+	while (t--)
+		solve();
+	/*
+#ifdef room210
+	cout<<"\n\n\n"<<(clock() - start) / 1.0 / CLOCKS_PER_SEC<<"\n\n\n";
+#endif*/
+	return 0;
+}
+
+//#define int li
+
+int n;
+int t[100500], c[100500];
+
+void solve () {
+	cin >> n;
+	for (int i = 0; i < n; ++i)
+		cin >> t[i] >> c[i];
+	int maxSize = 0;
+	int curSize = 0;
+
+	int last = 0;
+	for (int i = 0; i < n; ++i) {
+		curSize = max(0, curSize - t[i] + last);
+		curSize += c[i];
+		maxSize = max(maxSize, curSize);
+		last = t[i];
+	}
+	cout << t[n - 1] + curSize << ' ' << maxSize << "\n";
+}
