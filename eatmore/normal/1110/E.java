4b8f363e5ba6c6768c5da8b9c2cba95cf9c29d9e
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 09:50:32 +0530

    eatmore

---
+++ eatmore/normal/1110/E.java
@@ -0,0 +1,115 @@
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class E {
+
+	static void solve() throws Exception {
+		int n = scanInt();
+		int a[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = scanInt();
+		}
+		int b[] = new int[n];
+		for (int i = 0; i < n; i++) {
+			b[i] = scanInt();
+		}
+		if (a[0] != b[0] || a[n - 1] != b[n - 1]) {
+			out.print("No");
+			return;
+		}
+		int da[] = new int[n - 1];
+		int db[] = new int[n - 1];
+		for (int i = 0; i < n - 1; i++) {
+			da[i] = a[i + 1] - a[i];
+			db[i] = b[i + 1] - b[i];
+		}
+		sort(da);
+		sort(db);
+		out.print(Arrays.equals(da, db) ? "Yes" : "No");
+	}
+
+	static void sort(int a[]) {
+		int n = a.length;
+		if (n == 0) {
+			return;
+		}
+		for (int i = 1; i < n; i++) {
+			int j = i;
+			int ca = a[i];
+			do {
+				int nj = (j - 1) >> 1;
+				int na = a[nj];
+				if (ca <= na) {
+					break;
+				}
+				a[j] = na;
+				j = nj;
+			} while (j != 0);
+			a[j] = ca;
+		}
+		int ca = a[0];
+		for (int i = n - 1; i > 0; i--) {
+			int j = 0;
+			while ((j << 1) + 2 + Integer.MIN_VALUE < i + Integer.MIN_VALUE) {
+				j <<= 1;
+				j += (a[j + 2] > a[j + 1]) ? 2 : 1;
+			}
+			if ((j << 1) + 2 == i) {
+				j = (j << 1) + 1;
+			}
+			int na = a[i];
+			a[i] = ca;
+			ca = na;
+			while (j != 0 && a[j] < ca) {
+				j = (j - 1) >> 1;
+			}
+			while (j != 0) {
+				na = a[j];
+				a[j] = ca;
+				ca = na;
+				j = (j - 1) >> 1;
+			}
+		}
+		a[0] = ca;
+	}
+
+	static int scanInt() throws IOException {
+		return parseInt(scanString());
+	}
+
+	static long scanLong() throws IOException {
+		return parseLong(scanString());
+	}
+
+	static String scanString() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
+	}
+
+	static BufferedReader in;
+	static PrintWriter out;
+	static StringTokenizer tok;
+
+	public static void main(String[] args) {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			out = new PrintWriter(System.out);
+			solve();
+			in.close();
+			out.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			exit(1);
+		}
+	}
+}
