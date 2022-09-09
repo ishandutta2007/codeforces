545619962a15f96ef3f3e58675afd1783ce0fc4b
ishan dutta <ishandutta2007@gmail.com>
2022-09-10 00:57:00 +0530

    dreamoon_love_aa

---
+++ dreamoon_love_aa/normal/1619/C.cpp
@@ -0,0 +1,109 @@
+/*{{{*/
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<iostream>
+#include<sstream>
+#include<set>
+#include<map>
+#include<queue>
+#include<bitset>
+#include<vector>
+#include<limits.h>
+#include<assert.h>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define FOR(I, A, B) for (int I = (A); I <= (B); ++I)
+#define FORS(I, S) for (int I = 0; S[I]; ++I)
+#define RS(X) scanf("%s", (X))
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define CASET int ___T; scanf("%d", &___T); for(int cs=1;cs<=___T;cs++)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<PII> VPII;
+typedef pair<LL,LL> PLL;
+typedef vector<PLL> VPLL;
+template<class T> void _R(T &x) { cin >> x; }
+void _R(int &x) { scanf("%d", &x); }
+void _R(int64_t &x) { scanf("%lld", &x); }
+void _R(double &x) { scanf("%lf", &x); }
+void _R(char &x) { scanf(" %c", &x); }
+void _R(char *x) { scanf("%s", x); }
+void R() {}
+template<class T, class... U> void R(T &head, U &... tail) { _R(head); R(tail...); }
+template<class T> void _W(const T &x) { cout << x; }
+void _W(const int &x) { printf("%d", x); }
+void _W(const int64_t &x) { printf("%lld", x); }
+void _W(const double &x) { printf("%.16f", x); }
+void _W(const char &x) { putchar(x); }
+void _W(const char *x) { printf("%s", x); }
+template<class T,class U> void _W(const pair<T,U> &x) {_W(x.F); putchar(' '); _W(x.S);}
+template<class T> void _W(const vector<T> &x) { for (auto i = x.begin(); i != x.end(); _W(*i++)) if (i != x.cbegin()) putchar(' '); }
+void W() {}
+template<class T, class... U> void W(const T &head, const U &... tail) { _W(head); putchar(sizeof...(tail) ? ' ' : '\n'); W(tail...); }
+#ifdef HOME
+ #define DEBUG(...) {printf("[DEBUG] ");W(__VA_ARGS__);}
+#else
+ #define DEBUG(...)
+#endif
+int MOD = 1e9+7;/*}}}*/
+void ADD(LL& x,LL v){x=(x+v)%MOD;if(x<0)x+=MOD;}
+
+const int SIZE = 1<<20;
+LL a;
+string s;
+void solve() {
+    string res;
+    while(a){
+        if(SZ(s)==0){
+            W(-1);
+            return;
+        }
+        int x=a%10;
+        a/=10;
+        int y=s.back()-'0'-x;
+        if(y<0)y+=10;
+        res.PB(y+'0');
+        s.pop_back();
+        if(x+y>=10&&(SZ(s)==0||s.back()!='1')){
+            W(-1);
+            return;
+        }
+        if(x+y>=10)s.pop_back();
+    }
+    while(SZ(s)){
+        res.PB(s.back());
+        s.pop_back();
+    }
+    while(SZ(res)>1&&res.back()=='0')res.pop_back();
+    reverse(ALL(res));
+    W(res);
+}
+void input() {
+    R(a, s);
+}
+int main(){
+    CASET {
+        input();
+        solve();
+    }
+    return 0;
+}
