e051da2f132c351f64cdf76c60124e39659d368c
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 20:16:17 +0530

    nebuchadnezzar

---
+++ nebuchadnezzar/normal/1594/A.cpp
@@ -0,0 +1,79 @@
+// Created by Nikolay Budin
+
+#ifdef DEBUG
+#  define _GLIBCXX_DEBUG
+#endif
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define ff first
+#define ss second
+#define szof(x) ((int)x.size())
+#ifndef LOCAL
+#  define cerr __get_ce
+#endif
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef unsigned long long ull;
+
+using namespace __gnu_pbds;
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template <typename K, typename V> using ordered_map = tree<K, V, less<K>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int const INF = (int)1e9 + 1e3;
+ll const INFL = (ll)1e18 + 1e6;
+#ifdef LOCAL
+	mt19937 tw(9450189);
+#else
+	mt19937 tw(chrono::high_resolution_clock::now().time_since_epoch().count());
+#endif
+uniform_int_distribution<ll> ll_distr;
+ll rnd(ll a, ll b) { return ll_distr(tw) % (b - a + 1) + a; }
+
+
+void solve() {
+	ll n;
+	cin >> n;
+	ll tmp = 1;
+	while (n % 2 == 0) {
+		n /= 2;
+		tmp *= 2;
+	}
+
+	ll k = n;
+
+	tmp *= 2;
+	// cerr << k << " " << tmp << endl;
+	ll r = (tmp + k - 1) / 2;
+	ll l;
+	if (k == 1) {
+		l = k - r;
+	} else {
+		l = tmp - r;
+	}
+	cout << l << " " << r << "\n";
+}
+
+
+int main() {
+#ifdef LOCAL
+	auto start_time = clock();
+	cerr << setprecision(3) << fixed;
+#endif
+	cout << setprecision(15) << fixed;
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int test_count = 1;
+	cin >> test_count;
+	for (int test = 1; test <= test_count; ++test) {
+		solve();
+	}
+
+#ifdef LOCAL
+	auto end_time = clock();
+	cerr << "Execution time: " << (end_time - start_time) * (int)1e3 / CLOCKS_PER_SEC << " ms\n";
+#endif
+}
