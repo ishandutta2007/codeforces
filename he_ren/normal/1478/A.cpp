41ec55b6a1ba19bb29abe79e8db735e202ed793b
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 15:08:32 +0530

    he_ren

---
+++ he_ren/normal/1478/A.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+const int MAXN = 1e2 + 5;
+
+int a[MAXN];
+
+void solve(void)
+{
+	int n;
+	scanf("%d",&n);
+	for(int i=1; i<=n; ++i) scanf("%d",&a[i]);
+	
+	int ans = 0, cur = 0;
+	for(int i=1; i<=n; ++i)
+	{
+		cur = a[i] == a[i-1]? cur+1: 1;
+		ans = max(ans, cur);
+	}
+	printf("%d\n",ans);
+}
+
+int main(void)
+{
+	int T;
+	scanf("%d",&T);
+	while(T--) solve();
+	return 0;
+}
