f2f77125f54d0e649dbd80402131ef106c3660fb
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 10:03:01 +0530

    eatmore

---
+++ eatmore/normal/859/B.java
@@ -0,0 +1,54 @@
+import static java.lang.Integer.parseInt;
+import static java.lang.Long.parseLong;
+import static java.lang.Math.min;
+import static java.lang.System.exit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B {
+
+	static void solve() throws Exception {
+		int n = scanInt();
+		int ans = 2 * n + 2;
+		for (int i = 2; i < n; i++) {
+			ans = min(ans, 2 * (i + (n + i - 1) / i));
+		}
+		out.print(ans);
+	}
+
+	static int scanInt() throws IOException {
+		return parseInt(scanString());
+	}
+
+	static long scanLong() throws IOException {
+		return parseLong(scanString());
+	}
+
+	static String scanString() throws IOException {
+		while (tok == null || !tok.hasMoreTokens()) {
+			tok = new StringTokenizer(in.readLine());
+		}
+		return tok.nextToken();
+	}
+
+	static BufferedReader in;
+	static PrintWriter out;
+	static StringTokenizer tok;
+
+	public static void main(String[] args) {
+		try {
+			in = new BufferedReader(new InputStreamReader(System.in));
+			out = new PrintWriter(System.out);
+			solve();
+			in.close();
+			out.close();
+		} catch (Throwable e) {
+			e.printStackTrace();
+			exit(1);
+		}
+	}
+}
