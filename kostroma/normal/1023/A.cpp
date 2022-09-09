f20bc8839de4f3b2102b9b608dbf0704899a8a3d
ishan dutta <ishandutta2007@gmail.com>
2022-09-09 12:28:14 +0530

    kostroma

---
+++ kostroma/normal/1023/A.cpp
@@ -0,0 +1,121 @@
+#pragma comment(linker, "/STACK:512000000")
+#define _CRT_SECURE_NO_WARNINGS
+//#include "testlib.h"
+#include <bits/stdc++.h>
+using namespace std;
+
+#define all(a) a.begin(), a.end()
+using li = long long;
+using ld = long double;
+void solve(bool);
+void precalc();
+clock_t start;
+int main() {
+#ifdef AIM
+  freopen("/home/alexandero/CLionProjects/ACM/input.txt", "r", stdin);
+  //freopen("/home/alexandero/CLionProjects/ACM/output.txt", "w", stdout);
+//freopen("out.txt", "w", stdout);
+#else
+  //freopen("input.txt", "r", stdin);
+//freopen("output.txt", "w", stdout);
+#endif
+  start = clock();
+  int t = 1;
+#ifndef AIM
+  cout.sync_with_stdio(0);
+  cin.tie(0);
+#endif
+  cout.precision(20);
+  cout << fixed;
+//cin >> t;
+  precalc();
+  while (t--) {
+    solve(true);
+  }
+  cout.flush();
+
+#ifdef AIM1
+  while (true) {
+solve(false);
+}
+#endif
+
+#ifdef AIM
+  cerr << "\n\n time: " << (clock() - start) / 1.0 / CLOCKS_PER_SEC << "\n\n";
+#endif
+  return 0;
+}
+
+//BE CAREFUL: IS INT REALLY INT?
+
+template<typename T>
+T binpow(T q, T w, T mod) {
+  if (!w)
+    return 1 % mod;
+  if (w & 1)
+    return q * 1LL * binpow(q, w - 1, mod) % mod;
+  return binpow(q * 1LL * q % mod, w / 2, mod);
+}
+
+template<typename T>
+T gcd(T q, T w) {
+  while (w) {
+    q %= w;
+    swap(q, w);
+  }
+  return q;
+}
+template<typename T>
+T lcm(T q, T w) {
+  return q / gcd(q, w) * w;
+}
+
+template <typename T>
+void make_unique(vector<T>& vec) {
+  sort(all(vec));
+  vec.erase(unique(all(vec)), vec.end());
+}
+
+template<typename T>
+void relax_min(T& cur, T val) {
+  cur = min(cur, val);
+}
+
+template<typename T>
+void relax_max(T& cur, T val) {
+  cur = max(cur, val);
+}
+
+void precalc() {
+
+}
+
+//#define int li
+//const li mod = 1000000007;
+//using ull = unsigned long long;
+
+void solve(__attribute__((unused)) bool read) {
+  int n, m;
+  cin >> n >> m;
+  string s, q;
+  cin >> s >> q;
+  int pos = 0;
+  while (pos < s.length() && s[pos] != '*') {
+    ++pos;
+  }
+  if (pos == s.length()) {
+    cout << (s == q ? "YES\n" : "NO\n");
+    return;
+  }
+  if (n - 1 > m) {
+    cout << "NO\n";
+    return;
+  }
+  int ren = n - pos - 1;
+  if (q.substr(0, pos) == s.substr(0, pos) && s.substr(pos + 1, ren) == q.substr(m - ren, ren)) {
+    cout << "YES\n";
+  } else {
+    cout << "NO\n";
+  }
+
+}
