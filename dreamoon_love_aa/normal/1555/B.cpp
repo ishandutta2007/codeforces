56a4e213cf322d33f918e91387392defcd3484f6
ishan dutta <ishandutta2007@gmail.com>
2022-09-10 01:15:54 +0530

    dreamoon_love_aa

---
+++ dreamoon_love_aa/normal/1555/B.cpp
@@ -0,0 +1,119 @@
+/*{{{*/
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<iostream>
+#include<sstream>
+#include<set>
+#include<map>
+#include<queue>
+#include<bitset>
+#include<vector>
+#include<limits.h>
+#include<assert.h>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define FOR(I, A, B) for (int I = (A); I <= (B); ++I)
+#define FORS(I, S) for (int I = 0; S[I]; ++I)
+#define RS(X) scanf("%s", (X))
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define CASET int ___T; scanf("%d", &___T); for(int cs=1;cs<=___T;cs++)
+#define MP make_pair
+#define PB emplace_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<PII> VPII;
+typedef pair<LL,LL> PLL;
+typedef vector<PLL> VPLL;
+template<class T> void _R(T &x) { cin >> x; }
+void _R(int &x) { scanf("%d", &x); }
+void _R(int64_t &x) { scanf("%lld", &x); }
+void _R(double &x) { scanf("%lf", &x); }
+void _R(char &x) { scanf(" %c", &x); }
+void _R(char *x) { scanf("%s", x); }
+void R() {}
+template<class T, class... U> void R(T &head, U &... tail) { _R(head); R(tail...); }
+template<class T> void _W(const T &x) { cout << x; }
+void _W(const int &x) { printf("%d", x); }
+void _W(const int64_t &x) { printf("%lld", x); }
+void _W(const double &x) { printf("%.16f", x); }
+void _W(const char &x) { putchar(x); }
+void _W(const char *x) { printf("%s", x); }
+template<class T,class U> void _W(const pair<T,U> &x) {_W(x.F); putchar(' '); _W(x.S);}
+template<class T> void _W(const vector<T> &x) { for (auto i = x.begin(); i != x.end(); _W(*i++)) if (i != x.cbegin()) putchar(' '); }
+void W() {}
+template<class T, class... U> void W(const T &head, const U &... tail) { _W(head); putchar(sizeof...(tail) ? ' ' : '\n'); W(tail...); }
+#ifdef HOME
+ #define DEBUG(...) {printf("[DEBUG] ");W(__VA_ARGS__);}
+#else
+ #define DEBUG(...)
+#endif
+int MOD = 1e9+7;/*}}}*/
+void ADD(LL& x,LL v){x=(x+v)%MOD;if(x<0)x+=MOD;}
+
+const int SIZE = 1<<20;
+const double EPS = 1e-12;
+const double PI = acos(-1);
+struct Point{
+    long long x,y;
+    Point (long long x = 0,long long y = 0) : x(x), y(y){ }
+    Point operator - (const Point& b) const { return Point(x - b.x, y - b.y); }
+    Point operator + (const Point& b) const { return Point(x + b.x, y + b.y); }
+    Point operator * (const long long v) const { return Point(x * v, y * v); }
+    long long operator * (const Point& b) const { return x * b.y - y * b.x; }
+    long long operator ^ (const Point& b) const { return x * b.x + y * b.y; }
+    bool operator < (const Point& b) const {
+        return x < b.x || (x == b.x && y < b.y);
+    }
+    double angle() const { return atan2(y, x); }
+    double dis() const{ return sqrt(x * x + y * y); }
+    void scan() { scanf("%lld%lld", &x, &y); }
+    void print() { printf("%lld %lld\n", x,  y); }
+}Rect,a[2],r,b[2];
+LL f(LL x){
+    return max(x,0LL);
+}
+void solve() {
+    if(r.x+a[1].x-a[0].x>Rect.x&&r.y+a[1].y-a[0].y>Rect.y){
+        W(-1);
+        return;
+    }
+    LL an=1e12;
+    if(r.x+a[1].x-a[0].x<=Rect.x){
+        an=min(an,f(r.x-a[0].x));
+        an=min(an,f(a[1].x-(Rect.x-r.x)));
+    }
+    if(r.y+a[1].y-a[0].y<=Rect.y){
+        an=min(an,f(r.y-a[0].y));
+        an=min(an,f(a[1].y-(Rect.y-r.y)));
+    }
+    W(an);
+}
+void input() {
+    Rect.scan();
+    REP(i,2)a[i].scan();
+    r.scan();
+}
+int main(){
+    CASET {
+        input();
+        solve();
+    }
+    return 0;
+}
