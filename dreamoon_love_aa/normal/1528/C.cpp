ecad55978cd540dceacde75ea7b170a329fa8c5f
ishan dutta <ishandutta2007@gmail.com>
2022-09-10 01:33:51 +0530

    dreamoon_love_aa

---
+++ dreamoon_love_aa/normal/1528/C.cpp
@@ -0,0 +1,143 @@
+/*{{{*/
+#include<cstdio>
+#include<cstdlib>
+#include<cstring>
+#include<cmath>
+#include<algorithm>
+#include<string>
+#include<iostream>
+#include<sstream>
+#include<set>
+#include<map>
+#include<queue>
+#include<bitset>
+#include<vector>
+#include<limits.h>
+#include<assert.h>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define FOR(I, A, B) for (int I = (A); I <= (B); ++I)
+#define FORS(I, S) for (int I = 0; S[I]; ++I)
+#define RS(X) scanf("%s", (X))
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define CASET int ___T; scanf("%d", &___T); for(int cs=1;cs<=___T;cs++)
+#define MP make_pair
+#define PB emplace_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<PII> VPII;
+typedef pair<LL,LL> PLL;
+typedef vector<PLL> VPLL;
+template<class T> void _R(T &x) { cin >> x; }
+void _R(int &x) { scanf("%d", &x); }
+void _R(int64_t &x) { scanf("%lld", &x); }
+void _R(double &x) { scanf("%lf", &x); }
+void _R(char &x) { scanf(" %c", &x); }
+void _R(char *x) { scanf("%s", x); }
+void R() {}
+template<class T, class... U> void R(T &head, U &... tail) { _R(head); R(tail...); }
+template<class T> void _W(const T &x) { cout << x; }
+void _W(const int &x) { printf("%d", x); }
+void _W(const int64_t &x) { printf("%lld", x); }
+void _W(const double &x) { printf("%.16f", x); }
+void _W(const char &x) { putchar(x); }
+void _W(const char *x) { printf("%s", x); }
+template<class T,class U> void _W(const pair<T,U> &x) {_W(x.F); putchar(' '); _W(x.S);}
+template<class T> void _W(const vector<T> &x) { for (auto i = x.begin(); i != x.end(); _W(*i++)) if (i != x.cbegin()) putchar(' '); }
+void W() {}
+template<class T, class... U> void W(const T &head, const U &... tail) { _W(head); putchar(sizeof...(tail) ? ' ' : '\n'); W(tail...); }
+#ifdef HOME
+ #define DEBUG(...) {printf("[DEBUG] ");W(__VA_ARGS__);}
+#else
+ #define DEBUG(...)
+#endif
+int MOD = 1e9+7;
+void ADD(LL& x,LL v){x=(x+v)%MOD;if(x<0)x+=MOD;}
+/*}}}*/
+const int SIZE = 1<<20;
+int n;
+int an;
+VI e[2][SIZE];
+int l[SIZE],r[SIZE],id;
+int from[SIZE];
+void dfs0(int x){
+    from[id]=x;
+    l[x]=r[x]=id++;
+    for(int y:e[1][x]){
+        dfs0(y);
+        r[x]=r[y];
+    }
+}
+set<int>AA;
+void dfs1(int x){
+    auto it=AA.lower_bound(l[x]);
+    int erase_id=-1;
+    bool ins=0;
+    if(it==AA.end()||*it>r[x]){
+        ins=1;
+        if(it!=AA.begin()){
+            int lt=*prev(it);
+            if(r[from[lt]]>=l[x]){
+                erase_id=lt;
+                AA.erase(prev(it));
+            }
+        }
+        AA.insert(l[x]);
+    }
+    //DEBUG(x,erase_id,(int)ins);
+    an=max(an,SZ(AA));
+    for(int y:e[0][x]){
+        dfs1(y);
+    }
+    if(erase_id!=-1){
+        AA.insert(erase_id);
+    }
+    if(ins){
+        AA.erase(l[x]);
+    }
+}
+void solve() {
+    an=1;
+    id=0;
+    dfs0(1);
+    dfs1(1);
+    W(an);
+    FOR(i,1,n){
+        VI t1,t2;
+        e[0][i].swap(t1);
+        e[1][i].swap(t2);
+    }
+}
+void input() {
+    R(n);
+    FOR(i,2,n){
+        int v;
+        R(v);
+        e[0][v].PB(i);
+    }
+    FOR(i,2,n){
+        int v;
+        R(v);
+        e[1][v].PB(i);
+    }
+}
+int main(){
+    CASET {
+        input();
+        solve();
+    }
+    return 0;
+}
