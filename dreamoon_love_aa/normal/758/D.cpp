fbc022d7ccdc3da3e2b9c5baf8965da0d0367936
ishan dutta <ishandutta2007@gmail.com>
2022-09-10 22:18:39 +0530

    dreamoon_love_aa

---
+++ dreamoon_love_aa/normal/758/D.cpp
@@ -0,0 +1,90 @@
+/*{{{*/
+#include <bits/stdc++.h>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define FOR(I, A, B) for (int I = (A); I <= (B); ++I)
+#define FORS(I, S) for (int I = 0; S[I]; ++I)
+#define RS(X) scanf("%s", (X))
+#define SORT_UNIQUE(c) (sort(c.begin(),c.end()), c.resize(distance(c.begin(),unique(c.begin(),c.end()))))
+#define GET_POS(c,x) (lower_bound(c.begin(),c.end(),x)-c.begin())
+#define CASET int ___T; scanf("%d", &___T); for(int cs=1;cs<=___T;cs++)
+#define MP make_pair
+#define PB emplace_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define F first
+#define S second
+using namespace std;
+typedef long long LL;
+typedef unsigned long long ULL;
+typedef long double LD;
+typedef pair<int,int> PII;
+typedef vector<int> VI;
+typedef vector<LL> VL;
+typedef vector<PII> VPII;
+typedef pair<LL,LL> PLL;
+typedef vector<PLL> VPLL;
+template<class T> void _R(T &x) { cin >> x; }
+void _R(int &x) { scanf("%d", &x); }
+void _R(int64_t &x) { scanf("%lld", &x); }
+void _R(double &x) { scanf("%lf", &x); }
+void _R(char &x) { scanf(" %c", &x); }
+void _R(char *x) { scanf("%s", x); }
+void R() {}
+template<class T, class... U> void R(T &head, U &... tail) { _R(head); R(tail...); }
+template<class T> void _W(const T &x) { cout << x; }
+void _W(const int &x) { printf("%d", x); }
+void _W(const int64_t &x) { printf("%lld", x); }
+void _W(const double &x) { printf("%.16f", x); }
+void _W(const char &x) { putchar(x); }
+void _W(const char *x) { printf("%s", x); }
+template<class T,class U> void _W(const pair<T,U> &x) {_W(x.F); putchar(' '); _W(x.S);}
+template<class T> void _W(const vector<T> &x) { for (auto i = x.begin(); i != x.end(); _W(*i++)) if (i != x.cbegin()) putchar(' '); }
+void W() {}
+template<class T, class... U> void W(const T &head, const U &... tail) { _W(head); putchar(sizeof...(tail) ? ' ' : '\n'); W(tail...); }
+#ifdef HOME
+ #define DEBUG(...) {printf("# ");printf(__VA_ARGS__);puts("");}
+#else
+ #define DEBUG(...)
+#endif
+int MOD = 1e9+7;
+void ADD(LL& x,LL v){x=(x+v)%MOD;if(x<0)x+=MOD;}
+/*}}}*/
+const int SIZE = 1e6+10;
+int main(){
+    LL n;
+    char s[70];
+    PII dp[70];
+    R(n);
+    RS(s+1);
+    int len=LEN(s+1);
+    dp[len+1]=MP(0,0);
+    for(int i=len;i>0;i--){
+        dp[i]=MP(1000,0);
+        LL now=0;
+        FOR(j,i,len){
+            now=now*10+s[j]-'0';
+            if(now<n){
+                if(dp[j+1].F+1<dp[i].F){
+                    dp[i]=MP(dp[j+1].F+1,j+1);
+                }
+            }
+            else break;
+            if(!now)break;
+        }
+    }
+    int now=1;
+    LL ret=0;
+    while(now<=len){
+        int nxt=dp[now].S;
+        LL v=0;
+        REPP(i,now,nxt)v=v*10+s[i]-'0';
+        ret=ret*n+v;
+        now=nxt;
+    }
+    W(ret);
+    return 0;
+}
