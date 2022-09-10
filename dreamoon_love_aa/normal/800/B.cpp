075d58eea6065425ab87e5393ce54d3d52473de5
ishan dutta <ishandutta2007@gmail.com>
2022-09-11 01:28:25 +0530

    dreamoon_love_aa

---
+++ dreamoon_love_aa/normal/800/B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#define SZ(X) ((int)(X).size())
+#define ALL(X) (X).begin(), (X).end()
+#define REP(I, N) for (int I = 0; I < (N); ++I)
+#define REPP(I, A, B) for (int I = (A); I < (B); ++I)
+#define RI(X) scanf("%d", &(X))
+#define RII(X, Y) scanf("%d%d", &(X), &(Y))
+#define RIII(X, Y, Z) scanf("%d%d%d", &(X), &(Y), &(Z))
+#define DRI(X) int (X); scanf("%d", &X)
+#define DRII(X, Y) int X, Y; scanf("%d%d", &X, &Y)
+#define DRIII(X, Y, Z) int X, Y, Z; scanf("%d%d%d", &X, &Y, &Z)
+#define RS(X) scanf("%s", (X))
+#define CASET int ___T, case_n = 1; scanf("%d ", &___T); while (___T-- > 0)
+#define MP make_pair
+#define PB push_back
+#define MS0(X) memset((X), 0, sizeof((X)))
+#define MS1(X) memset((X), -1, sizeof((X)))
+#define LEN(X) strlen(X)
+#define PII pair<int,int>
+#define VI vector<int>
+#define VPII vector<pair<int,int> >
+#define PLL pair<long long,long long>
+#define VPLL vector<pair<long long,long long> >
+#define F first
+#define S second
+typedef long long LL;
+using namespace std;
+const int MOD = 1e9+7;
+const int SIZE = 1e6+10;
+
+const double EPS=1e-12;
+const double PI=acos(-1);
+struct Point{
+    double x,y;
+    Point(double _x=0,double _y=0){x=_x;y=_y;}
+    Point operator-(const Point& b){return Point(x-b.x,y-b.y);}
+    Point operator+(const Point& b){return Point(x+b.x,y+b.y);}
+    Point operator*(const double v){return Point(x*v,y*v);}
+    double operator*(const Point& b){return x*b.y-y*b.x;}
+    double operator^(const Point& b){return x*b.x+y*b.y;}
+    bool operator<(const Point& b)const{
+        if(fabs(x-b.x)<EPS){
+            if(fabs(y-b.y)<EPS)return 0;
+            return y<b.y;
+        }
+        return x<b.x;
+    }
+    double angle(){return atan2(y,x);}
+    double dis(){return sqrt(x*x+y*y);}
+    Point rotate(double v){return Point(x*cos(v)-y*sin(v),x*sin(v)+y*cos(v));}
+    Point unit(){return (*this)*(1./dis());}
+    void scan(){scanf("%lf%lf",&x,&y);}
+    void print(){printf("%.9f %.9f\n",x,y);}
+}p[SIZE];
+int main(){
+    DRI(n);
+    REP(i,n){
+        p[i].scan();
+    }
+    p[n]=p[0];p[n+1]=p[1];
+    double an=1e100;
+    REP(i,n){
+        Point V=(p[i+2]-p[i]);
+        an=min(an,fabs(V*(p[i+1]-p[i]))/V.dis()*0.5);
+    }
+    printf("%.12f\n",an);
+    return 0;
+}
