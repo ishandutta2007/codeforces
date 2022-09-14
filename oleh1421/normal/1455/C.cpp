40168ff55d57af960c196a837802095f3f303776
ishan dutta <ishandutta2007@gmail.com>
2022-09-15 03:47:15 +0530

    oleh1421

---
+++ oleh1421/normal/1455/C.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+typedef long long ll;
+using namespace std;
+#define endl '\n'
+const int N=500010;
+void solve(){
+    int x,y;cin>>x>>y;
+    cout<<x-1<<" "<<y<<endl;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    int tt;cin>>tt;
+    while (tt--){
+        solve();
+    }
+
+    return 0;
+}
+//2 3 4 5
+//2 3 4 5 6 7 8 9 10 11 12 14
+//x y
