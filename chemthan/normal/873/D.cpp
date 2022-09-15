f56033b6e139e2a5b1fcb125e06e5f67abd04311
ishan dutta <ishandutta2007@gmail.com>
2022-09-15 09:14:06 +0530

    chemthan

---
+++ chemthan/normal/873/D.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ms(s, n) memset(s, n, sizeof(s))
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define FORd(i, a, b) for (int i = (a) - 1; i >= (b); i--)
+#define FORall(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define sz(a) int((a).size())
+#define present(t, x) (t.find(x) != t.end())
+#define all(a) (a).begin(), (a).end()
+#define uni(a) (a).erase(unique(all(a)), (a).end())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fi first
+#define se second
+#define prec(n) fixed<<setprecision(n)
+#define bit(n, i) (((n) >> (i)) & 1)
+#define bitcount(n) __builtin_popcountll(n)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vii;
+const int MOD = (int) 1e9 + 7;
+const int FFTMOD = 1007681537;
+const int INF = (int) 1e9;
+const ll LINF = (ll) 1e18;
+const ld PI = acos((ld) -1);
+const ld EPS = 1e-9;
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+inline ll fpow(ll n, ll k, int p = MOD) {ll r = 1; for (; k; k >>= 1) {if (k & 1) r = r * n % p; n = n * n % p;} return r;}
+template<class T> inline int chkmin(T& a, const T& val) {return val < a ? a = val, 1 : 0;}
+template<class T> inline int chkmax(T& a, const T& val) {return a < val ? a = val, 1 : 0;}
+inline ll isqrt(ll k) {ll r = sqrt(k) + 1; while (r * r > k) r--; return r;}
+inline ll icbrt(ll k) {ll r = cbrt(k) + 1; while (r * r * r > k) r--; return r;}
+inline void addmod(int& a, int val, int p = MOD) {if ((a = (a + val)) >= p) a -= p;}
+inline void submod(int& a, int val, int p = MOD) {if ((a = (a - val)) < 0) a += p;}
+inline int mult(int a, int b, int p = MOD) {return (ll) a * b % p;}
+inline int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
+inline int sign(ld x) {return x < -EPS ? -1 : x > +EPS;}
+inline int sign(ld x, ld y) {return sign(x - y);}
+#define db(x) cerr << #x << " = " << (x) << " ";
+#define endln cerr << "\n";
+
+const int maxn = 1e5 + 5;
+int n, k;
+int L[maxn << 2];
+int R[maxn << 2];
+int a[maxn];
+
+int tot;
+vi vals;
+void build(int p, int l, int r) {
+    tot++;
+    L[p] = l, R[p] = r;
+    if (l == r - 1) return;
+    vals.pb(p);
+    int m = l + r >> 1;
+    build(p << 1, l, m);
+    build(p << 1 | 1, m, r);
+}
+
+void solve() {
+    cin >> n >> k;
+    build(1, 0, n);
+    sort(all(vals), greater<int>());
+    FOR(i, 0, n) a[i] = n - i;
+    if (tot == k) {
+        FOR(i, 0, n) cout << a[i] << " \n"[i == n - 1];
+        return;
+    }
+    FOR(i, 0, sz(vals)) {
+        tot -= 2;
+        if (tot == k) {
+            FOR(j, 0, i + 1) {
+                int p = vals[j];
+                sort(a + p[L], a + p[R]);
+            }
+            FOR(j, 0, n) cout << a[j] << " \n"[j == n - 1];
+            return;
+        }
+    }
+    cout << -1 << "\n";
+}
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    int JUDGE_ONLINE = 1;
+    if (argc > 1) {
+        JUDGE_ONLINE = 0;
+        assert(freopen(argv[1], "r", stdin));
+    }
+    if (argc > 2) {
+        JUDGE_ONLINE = 0;
+        assert(freopen(argv[2], "w", stdout));
+    }
+    solve();
+    if (!JUDGE_ONLINE) {
+        cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    }
+    return 0;
+}
