96c13ec6b32648ea30059e30572bcfd1f63de884
ishan dutta <ishandutta2007@gmail.com>
2022-09-15 07:51:25 +0530

    chemthan

---
+++ chemthan/normal/280/A.cpp
@@ -0,0 +1,209 @@
+#include <bits/stdc++.h>
+using namespace std;
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+#define ms(s, n) memset(s, n, sizeof(s))
+#define FOR(i, a, b) for (int i = (a); i < (b); ++i)
+#define FORd(i, a, b) for (int i = (a) - 1; i >= (b); --i)
+#define FORall(it, a) for (__typeof((a).begin()) it = (a).begin(); it != (a).end(); it++)
+#define sz(a) int((a).size())
+#define present(t, x) (t.find(x) != t.end())
+#define all(a) (a).begin(), (a).end()
+#define uni(a) (a).erase(unique(all(a)), (a).end())
+#define pb push_back
+#define pf push_front
+#define mp make_pair
+#define fi first
+#define se second
+#define prec(n) fixed<<setprecision(n)
+#define bit(n, i) (((n) >> (i)) & 1)
+#define bitcount(n) __builtin_popcountll(n)
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vii;
+const int MOD = (int) 1e9 + 7;
+const int FFTMOD = 119 << 23 | 1;
+const int INF = (int) 1e9 + 23111992;
+const ll LINF = (ll) 1e18 + 23111992;
+const ld PI = acos((ld) -1);
+const ld EPS = 1e-9;
+inline ll gcd(ll a, ll b) {ll r; while (b) {r = a % b; a = b; b = r;} return a;}
+inline ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+inline ll fpow(ll n, ll k, int p = MOD) {ll r = 1; for (; k; k >>= 1) {if (k & 1) r = r * n % p; n = n * n % p;} return r;}
+template<class T> inline int chkmin(T& a, const T& val) {return val < a ? a = val, 1 : 0;}
+template<class T> inline int chkmax(T& a, const T& val) {return a < val ? a = val, 1 : 0;}
+inline ull isqrt(ull k) {ull r = sqrt(k) + 1; while (r * r > k) r--; return r;}
+inline ll icbrt(ll k) {ll r = cbrt(k) + 1; while (r * r * r > k) r--; return r;}
+inline void addmod(int& a, int val, int p = MOD) {if ((a = (a + val)) >= p) a -= p;}
+inline void submod(int& a, int val, int p = MOD) {if ((a = (a - val)) < 0) a += p;}
+inline int mult(int a, int b, int p = MOD) {return (ll) a * b % p;}
+inline int inv(int a, int p = MOD) {return fpow(a, p - 2, p);}
+inline int sign(ld x) {return x < -EPS ? -1 : x > +EPS;}
+inline int sign(ld x, ld y) {return sign(x - y);}
+mt19937 mt(chrono::high_resolution_clock::now().time_since_epoch().count());
+inline int mrand() {return abs((int) mt());}
+#define db(x) cerr << "[" << #x << ": " << (x) << "] ";
+#define endln cerr << "\n";
+
+#define RL double
+#define EPS 1e-9
+struct point_t {
+    RL x, y;
+    point_t() : x(0), y(0) {}
+    point_t(RL x, RL y) : x(x), y(y) {}
+    point_t(const point_t& p) : x(p.x), y(p.y) {}
+    int operator < (const point_t& rhs) const {return make_pair(y, x) < make_pair(rhs.y, rhs.x);}
+    int operator == (const point_t& rhs) const {return make_pair(y, x) == make_pair(rhs.y, rhs.x);}
+    point_t operator + (const point_t& p) const {return point_t(x + p.x, y + p.y);}
+    point_t operator - (const point_t& p) const {return point_t(x - p.x, y - p.y);}
+    point_t operator * (RL c) const {return point_t(x * c, y * c);}
+    point_t operator / (RL c) const {return point_t(x / c, y / c);}
+};
+RL cross(point_t p, point_t q) {return p.x * q.y - p.y * q.x;}
+RL area(point_t a, point_t b, point_t c) {return fabs(cross(a, b) + cross(b, c) + cross(c, a)) / 2;}
+RL area2(point_t a, point_t b, point_t c) {return cross(a, b) + cross(b, c) + cross(c, a);}
+RL dot(point_t p, point_t q) {return p.x * q.x + p.y * q.y;}
+RL dist(point_t p, point_t q) {return sqrt(dot(p - q, p - q));}
+RL dist2(point_t p, point_t q) {return dot(p - q, p - q);}
+point_t RotateCCW90(point_t p) {return point_t(-p.y, p.x);}
+point_t RotateCW90(point_t p) {return point_t(p.y, -p.x);}
+point_t RotateCCW(point_t p, RL t) {return point_t(p.x * cos(t) - p.y * sin(t), p.x * sin(t) + p.y * cos(t));}
+ostream& operator << (ostream& os, const point_t& p) {
+    os << "(" << p.x << "," << p.y << ")";
+    return os;
+}
+
+//Remove degenerate
+#define REMOVE_REDUNDANT
+#ifdef REMOVE_REDUNDANT
+bool between(const point_t& a, const point_t& b, const point_t& c) {
+    return (fabs(area2(a, b, c)) < EPS && (a.x - b.x) * (c.x - b.x) <= 0 && (a.y - b.y) * (c.y - b.y) <= 0);
+}
+#endif
+void ConvexHull(vector<point_t>& pts) {
+    sort(pts.begin(), pts.end());
+    pts.erase(unique(pts.begin(), pts.end()), pts.end());
+    vector<point_t> up, dn;
+    for (int i = 0; i < pts.size(); i++) {
+        while (up.size() > 1 && area2(up[up.size() - 2], up.back(), pts[i]) >= 0) up.pop_back();
+        while (dn.size() > 1 && area2(dn[dn.size() - 2], dn.back(), pts[i]) <= 0) dn.pop_back();
+        up.push_back(pts[i]);
+        dn.push_back(pts[i]);
+    }
+    pts = dn;
+    for (int i = up.size() - 2; i >= 1; i--) pts.push_back(up[i]);
+#ifdef REMOVE_REDUNDANT
+    if (pts.size() <= 2) return;
+    dn.clear();
+    dn.push_back(pts[0]);
+    dn.push_back(pts[1]);
+    for (int i = 2; i < pts.size(); i++) {
+        if (between(dn[dn.size() - 2], dn[dn.size() - 1], pts[i])) dn.pop_back();
+        dn.push_back(pts[i]);
+    }
+    if (dn.size() >= 3 && between(dn.back(), dn[0], dn[1])) {
+        dn[0] = dn.back();
+        dn.pop_back();
+    }
+    pts = dn;
+#endif
+}
+
+point_t ComputeLineIntersection(point_t a, point_t b, point_t c, point_t d) {
+    b = b - a; d = c - d; c = c - a;
+    return a + b * cross(c, d) / cross(b, d);
+}
+
+point_t ProjectPointSegment(point_t a, point_t b, point_t c) {
+    double r = dot(b - a, b - a);
+    if (fabs(r) < EPS) return a;
+    r = dot(c - a, b - a) / r;
+    if (r < 0) return a;
+    if (r > 1) return b;
+    return a + (b - a) * r;
+}
+
+//Determine if lines from a to b and c to d are parallel or collinear
+int LinesParallel(point_t a, point_t b, point_t c, point_t d) {
+    return fabs(cross(b - a, c - d)) < EPS;
+}
+int LinesCollinear(point_t a, point_t b, point_t c, point_t d) {
+    return LinesParallel(a, b, c, d) && fabs(cross(a - b, a - c)) < EPS && fabs(cross(c - d, c - a)) < EPS;
+}
+//Determine if line segment from a to b intersects with line segment from c to d
+int SegmentsIntersect(point_t a, point_t b, point_t c, point_t d) {
+    if (LinesCollinear(a, b, c, d)) {
+        if (dist2(a, c) < EPS || dist2(a, d) < EPS || dist2(b, c) < EPS || dist2(b, d) < EPS) return 1;
+        if (dot(c - a, c - b) > 0 && dot(d - a, d - b) > 0 && dot(c - b, d - b) > 0) return 0;
+        return 1;
+    }
+    if (cross(d - a, b - a) * cross(c - a, b - a) > 0) return 0;
+    if (cross(a - c, d - c) * cross(b - c, d - c) > 0) return 0;
+    return 1;
+}
+int PointInPolygonSlow(const vector<point_t>& p, point_t q) {
+    int c = 0;
+    for (int i = 0; i < p.size(); i++) {
+        int j = (i + 1) % p.size();
+        if ((p[i].y <= q.y && q.y < p[j].y || p[j].y <= q.y && q.y < p[i].y) && q.x < p[i].x + (p[j].x - p[i].x) * (q.y - p[i].y) / (p[j].y - p[i].y)) c = !c;
+    }
+    return c;
+}
+int PointOnPolygon(const vector<point_t>& p, point_t q) {
+    for (int i = 0; i < p.size(); i++) if (dist2(ProjectPointSegment(p[i], p[(i + 1) % p.size()], q), q) < EPS) return 1;
+    return 0;
+}
+
+void chemthan() {
+    double w, h, a; cin >> w >> h >> a;
+    vector<point_t> pts;
+    pts.pb(point_t(w / 2, h / 2));
+    pts.pb(point_t(w / 2, -h / 2));
+    pts.pb(point_t(-w / 2, h / 2));
+    pts.pb(point_t(-w / 2, -h / 2));
+    vector<point_t> npts = pts;
+    for (auto& q : npts) {
+        q = RotateCCW(q, a / 180 * PI);
+    }
+    ConvexHull(pts);
+    ConvexHull(npts);
+    vector<point_t> g;
+    FOR(i, 0, 4) {
+        FOR(j, 0, 4) {
+            int ii = (i + 1) % 4;
+            int jj = (j + 1) % 4;
+            if (SegmentsIntersect(pts[i], pts[ii], npts[j], npts[jj])) {
+                auto q = ComputeLineIntersection(pts[i], pts[ii], npts[j], npts[jj]);
+                if ((PointInPolygonSlow(pts, q) || PointOnPolygon(pts, q)) &&
+                        (PointInPolygonSlow(npts, q) || PointOnPolygon(npts, q))) {
+                    g.pb(q);
+                }
+            }
+        }
+    }
+    ConvexHull(g);
+    double res = 0;
+    FOR(i, 0, sz(g)) {
+        int ii = (i + 1) % sz(g);
+        res += cross(g[i], g[ii]);
+    }
+    res = abs(res) / 2;
+    cout << prec(9) << res << "\n";
+}
+
+int main(int argc, char* argv[]) {
+    ios_base::sync_with_stdio(0), cin.tie(0);
+    if (argc > 1) {
+        assert(freopen(argv[1], "r", stdin));
+    }
+    if (argc > 2) {
+        assert(freopen(argv[2], "wb", stdout));
+    }
+    chemthan();
+    cerr << "\nTime elapsed: " << 1000 * clock() / CLOCKS_PER_SEC << "ms\n";
+    return 0;
+}
