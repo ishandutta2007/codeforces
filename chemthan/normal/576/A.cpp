f926d443a6415ab1b7edb7e9dc08a66f8bb731a9
ishan dutta <ishandutta2007@gmail.com>
2022-09-15 11:47:20 +0530

    chemthan

---
+++ chemthan/normal/576/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ms(s, n) memset(s, n, sizeof(s))
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORd(i, a, b) for(int i = (a) - 1; i >= (b); i--)
+#define sz(a) int((a).size())
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<ll, ll> pi;
+typedef vector<ll> vi;
+typedef vector<pi> vii;
+ll gcd(ll a, ll b) {ll r; while (b != 0) {r = a % b; a = b; b = r;} return a;}
+ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+
+const int maxn = 1100;
+int n;
+vi ans;
+bool isprime[maxn];
+vi prime;
+
+void solve() {
+	scanf("%d", &n);
+	ms(isprime, 1);
+	for (int i = 2; i < maxn; i++) if (isprime[i]) {for (int j = i * i; j < maxn; j += i) isprime[j] = 0; prime.push_back(i);}
+	for (int i = 0; i < sz(prime) && prime[i] <= n; i++) {
+		int p = prime[i];
+		while (p <= n) {ans.push_back(p); p *= prime[i];}
+	}
+	printf("%d\n", sz(ans));
+	FOR(i, 0, sz(ans)) printf("%d ", ans[i]);
+}
+
+int main() {
+	//ios_base::sync_with_stdio(0); cin.tie(NULL);
+#ifdef _LOCAL_
+	freopen("in.txt", "r", stdin); freopen("out.txt", "w", stdout);
+#endif
+	solve();
+#ifdef _LOCAL_
+	printf("\nTime elapsed: %dms", 1000 * clock() / CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
