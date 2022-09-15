1e1d59b70f6bec058f85ff6b21429f4510eb7c25
ishan dutta <ishandutta2007@gmail.com>
2022-09-15 15:29:13 +0530

    sirshokoladina

---
+++ sirshokoladina/normal/1349/A.cpp
@@ -0,0 +1,170 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef SG
+	#include <debug.h>
+#else
+	template<typename T> struct outputer;
+	struct outputable {};
+	#define PRINT(...)
+	#define OUTPUT(...)
+	#define show(...)
+	#define debug(...)
+	#define deepen(...)
+	#define timer(...)
+	#define fbegin(...)
+	#define fend
+	#define pbegin(...)
+	#define pend
+#endif
+
+#define ARG4(_1,_2,_3,_4,...) _4
+
+#define forn3(i,l,r) for (int i = int(l); i < int(r); ++i)
+#define forn2(i,n) forn3 (i, 0, n)
+#define forn(...) ARG4(__VA_ARGS__, forn3, forn2) (__VA_ARGS__)
+
+#define ford3(i,l,r) for (int i = int(r) - 1; i >= int(l); --i)
+#define ford2(i,n) ford3 (i, 0, n)
+#define ford(...) ARG4(__VA_ARGS__, ford3, ford2) (__VA_ARGS__)
+
+#define ve vector
+#define pa pair
+#define tu tuple
+#define mp make_pair
+#define mt make_tuple
+#define pb emplace_back
+#define fs first
+#define sc second
+#define all(a) (a).begin(), (a).end()
+#define sz(a) ((int)(a).size())
+
+typedef long double ld;
+typedef int64_t ll;
+typedef uint64_t ull;
+typedef uint32_t ui;
+typedef uint16_t us;
+typedef uint8_t uc;
+typedef pa<int, int> pii;
+typedef pa<int, ll> pil;
+typedef pa<ll, int> pli;
+typedef pa<ll, ll> pll;
+typedef ve<int> vi;
+
+template<typename T> inline auto sqr (T x) -> decltype(x * x) {return x * x;}
+template<typename T1, typename T2> inline bool umx (T1& a, T2 b) {if (a < b) {a = b; return 1;} return 0;}
+template<typename T1, typename T2> inline bool umn (T1& a, T2 b) {if (b < a) {a = b; return 1;} return 0;}
+
+const int N = 100000;
+const int X = 200000;
+
+struct Input {
+	int n;
+	int a[N];
+	
+	bool read() {
+		if (!(cin >> n)) {
+			return 0;
+		}
+		forn (i, n) {
+			scanf("%d", &a[i]);
+		}
+		return 1;
+	}
+
+	void init(const Input &input) {
+		*this = input;
+	}
+};
+
+struct Data: Input {
+	ll ans;
+	
+	void write() {
+		cout << ans << endl;
+	}
+};
+
+
+namespace Main {
+	
+	struct Solution: Data {
+		vi v[X + 1];
+		
+		void solve() {
+			forn (i, n) {
+				int x = a[i];
+				for (int j = 2; j * j <= x; ++j) {
+					int k = 0;
+					while (x % j == 0) {
+						x /= j;
+						k++;
+					}
+					if (k) {
+						v[j].pb(k);
+					}
+				}
+				if (x > 1) {
+					v[x].pb(1);
+				}
+			}
+			ans = 1;
+			forn (i, X + 1) {
+				sort(all(v[i]));
+				int k = 0;
+				if (sz(v[i]) == n) {
+					k = v[i][1];
+				} else if (sz(v[i]) == n - 1) {
+					k = v[i][0];
+				} else {
+					k = 0;
+				}
+				if (sz(v[i])) {
+					debug(mt(i, v[i], k));
+				}
+				forn (j, k) {
+					ans *= i;
+				}
+			}
+		}
+		
+		void clear() {
+			*this = Solution();
+		}
+	};
+}
+
+
+Main::Solution sol;
+
+int main() {
+	cout.setf(ios::showpoint | ios::fixed);
+	cout.precision(20);
+
+	#ifdef SG
+		freopen((problemname + ".in").c_str(), "r", stdin);
+//		freopen((problemname + ".out").c_str(), "w", stdout);
+		while (sol.read()) {
+			sol.solve();
+			sol.write();
+			sol.clear();
+		}
+	#else
+		sol.read();
+		sol.solve();
+		sol.write();
+	#endif
+	
+	/*
+	int t;
+	cin >> t;
+	forn (i, t) {
+		sol.read();
+		sol.solve();
+		sol.write();
+		sol.clear();
+	}
+	*/
+	
+	return 0;
+}
