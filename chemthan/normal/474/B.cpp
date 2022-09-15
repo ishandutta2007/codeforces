752dd7c9e6db92eaaf36be2bb966980ca0ffe3c6
ishan dutta <ishandutta2007@gmail.com>
2022-09-15 11:44:45 +0530

    chemthan

---
+++ chemthan/normal/474/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ms(s, n) memset(s, n, sizeof(s))
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define FORd(i, a, b) for(int i = (a) - 1; i >= (b); i--)
+#define sz(a) int((a).size())
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pi;
+typedef vector<int> vi;
+typedef vector<pi> vii;
+ll gcd(ll a, ll b) {ll r; while (b != 0) {r = a % b; a = b; b = r;} return a;}
+ll lcm(ll a, ll b) {return a / gcd(a, b) * b;}
+
+const int maxn = 100010;
+int n;
+int a[maxn];
+
+void solve() {
+    scanf("%d", &n);
+    FOR(i, 0, n) scanf("%d", a + i);
+    FOR(i, 1, n) a[i] += a[i - 1];
+    int m; scanf("%d", &m);
+    while (m--) {
+        int x; scanf("%d", &x);
+        int k = lower_bound(a, a + n, x) - a;
+        printf("%d\n", k + 1);
+    }
+}
+
+int main() {
+    //ios_base::sync_with_stdio(0); cin.tie(NULL);
+#ifdef _LOCAL_
+	freopen("in.txt", "r", stdin); //freopen("out.txt", "w", stdout);
+#endif
+	solve();
+#ifdef _LOCAL_
+	//printf("\nTime elapsed: %dms", 1000 * clock() / CLOCKS_PER_SEC);
+#endif
+	return 0;
+}
