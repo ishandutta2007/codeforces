aa2266c863c52a6b1da6d7976c94562aa3871395
ishan dutta <ishandutta2007@gmail.com>
2022-09-20 01:08:10 +0530

    amoo_safar

---
+++ amoo_safar/normal/702/A.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define debug(x)  cerr << #x << " = " << x << endl
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<ll, ll> pll;
+
+const ld Pi = 3.14159265359;
+
+const ll MOD = (ll) 1e9 + 7;
+const ll MAXN = (ll) 1e6 + 10;
+const ll INF = (ll) 9223372036854775807;
+
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n, v , l, cnt = 1, ma = 1;
+    cin >> n >> l;
+    for(int i =1;i<n;i++){
+        cin >> v;
+        if(v > l)cnt ++;
+        else cnt = 1;
+        ma = max(ma, cnt);
+        l = v;
+    }
+    cout << ma;
+    return 0;
+}
+
+
+
+/*
+                                 ____              ,----..               ,----..
+   ,---,                       ,'  , `.           /   /   \             /   /   \
+  '  .' \                   ,-+-,.' _ |          /   .     :           /   .     :
+ /  ;    '.              ,-+-. ;   , ||         .   /   ;.  \         .   /   ;.  \
+:  :       \            ,--.'|'   |  ;|        .   ;   /  ` ;        .   ;   /  ` ;
+:  |   /\   \          |   |  ,', |  ':        ;   |  ; \ ; |        ;   |  ; \ ; |
+|  :  ' ;.   :         |   | /  | |  ||        |   :  | ; | '        |   :  | ; | '
+|  |  ;/  \   \        '   | :  | :  |,        .   |  ' ' ' :        .   |  ' ' ' :
+'  :  | \  \ ,'        ;   . |  ; |--'         '   ;  \; /  |        '   ;  \; /  |
+|  |  '  '--'          |   : |  | ,             \   \  ',  /          \   \  ',  /
+|  :  :                |   : '  |/               ;   :    /            ;   :    /
+|  | ,'                ;   | |`-'                 \   \ .'              \   \ .'
+`--''                  |   ;/                      `---`                 `---`
+                       '---'
+
+*/
