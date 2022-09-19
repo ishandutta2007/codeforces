79a5cd76e9bcc99c198bfa3ada9affa25214a5fe
ishan dutta <ishandutta2007@gmail.com>
2022-09-20 01:07:26 +0530

    amoo_safar

---
+++ amoo_safar/normal/507/B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define debug(x)  cerr << #x << " = " << x << endl
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<ll, ll> pll;
+
+const ld Pi = 3.14159265359;
+
+const ll MOD = (ll) 1e9 + 7;
+const ll MAXN = (ll) 1e5 + 10;
+const ll INF = (ll) 9223372036854775;
+const ld EPS = (ld) 1e-8;
+
+ll a[MAXN];
+ll b[MAXN];
+bool CMP(ll i, ll j){
+    return a[i] < a[j];
+}
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    //cout.precision(16);
+    ll r, x1, y1, x2, y2;
+    cin >> r >> x1 >> y1 >> x2 >> y2;
+    r *= 2;
+    ld dist = sqrt(pow(abs(x1-x2),2) + pow(abs(y1 - y2),2));
+    cout << ((ll)(dist - EPS + ((ld) r ))) / r;
+    return 0;
+}
+
+
+
+/*
+
+                                 ____              ,----..               ,----..
+   ,---,                       ,'  , `.           /   /   \             /   /   \
+  '  .' \                   ,-+-,.' _ |          /   .     :           /   .     :
+ /  ;    '.              ,-+-. ;   , ||         .   /   ;.  \         .   /   ;.  \
+:  :       \            ,--.'|'   |  ;|        .   ;   /  ` ;        .   ;   /  ` ;
+:  |   /\   \          |   |  ,', |  ':        ;   |  ; \ ; |        ;   |  ; \ ; |
+|  :  ' ;.   :         |   | /  | |  ||        |   :  | ; | '        |   :  | ; | '
+|  |  ;/  \   \        '   | :  | :  |,        .   |  ' ' ' :        .   |  ' ' ' :
+'  :  | \  \ ,'        ;   . |  ; |--'         '   ;  \; /  |        '   ;  \; /  |
+|  |  '  '--'          |   : |  | ,             \   \  ',  /          \   \  ',  /
+|  :  :                |   : '  |/               ;   :    /            ;   :    /
+|  | ,'                ;   | |`-'                 \   \ .'              \   \ .'
+`--''                  |   ;/                      `---`                 `---`
+                       '---'
+
+*/
