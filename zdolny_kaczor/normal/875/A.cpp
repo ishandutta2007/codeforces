151c3ea5916871995d041910864858557af93d95
ishan dutta <ishandutta2007@gmail.com>
2022-09-19 21:23:40 +0530

    zdolny_kaczor

---
+++ zdolny_kaczor/normal/875/A.cpp
@@ -0,0 +1,52 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define PII pair<int,int>
+#define f first
+#define s second
+#define VI vector<int>
+#define LL long long
+#define MP make_pair
+#define LD long double
+#define PB push_back
+#define EB emplace_back
+#define PLL pair <LL, LL>
+#define ALL(V) V.begin(),V.end()
+#define abs(x) max((x),-(x))
+#define PDD pair<LD,LD> 
+#define VPII vector< PII > 
+#define siz(V) ((int)V.size())
+#define FOR(x, b, e)  for(int x=b;x<=(e);x++)
+#define FORD(x, b, e) for(int x=b;x>=(e);x--)
+#define REP(x, n)     for(int x=0;x<(n);x++)
+#define mini(a,b) a=min(a,b)
+#define maxi(a,b) a=max(a,b)
+const bool debug = 
+#ifdef DEB
+true;
+#define debug(...) fprintf(stderr, __VA_ARGS__), fflush(stderr)
+#else
+false;
+#define debug(...)
+#define cerr if(0) cout
+#endif
+#define cc(x) x.f, x.s
+#define here() debug("LINE: %d\n", __LINE__)
+int sum(int x) {
+	if (x == 0) return 0;
+	else return x % 10 + sum(x / 10);
+}
+int main() {
+	VI ans;
+	int n;
+	scanf("%d", &n);
+	REP(i, 100) {
+		if (sum(n - i) == i)
+			ans.PB(n - i);
+	}
+	sort(ALL(ans));
+	printf("%d\n", siz(ans));
+	for (int c : ans) {
+		printf("%d ", c);
+	}
+	printf("\n");
+}
