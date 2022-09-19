a81b0e854010edea7af420f2e4338967253b9da3
ishan dutta <ishandutta2007@gmail.com>
2022-09-20 01:07:11 +0530

    amoo_safar

---
+++ amoo_safar/normal/501/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define debug(x)  cerr << #x << " = " << x << endl
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<ll, ll> pll;
+
+const ld Pi = 3.14159265359;
+
+const ll MOD = (ll) 1e9 + 7;
+const ll MAXN = (ll) 1e5 + 10;
+const ll INF = (ll) 9223372036854775;
+const ld EPS = (ld) 1e-8;
+
+
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll a,b,c,d;
+    cin >> a >> b >> c >> d;
+    ll p1 = max(3*a/10,a - (a/250)*c);
+    ll p2 = max(3*b/10,b - (b/250)*d);
+    if(p1 == p2)cout << "Tie";
+    if(p1 > p2)cout << "Misha";
+    if(p1 < p2)cout << "Vasya";
+    return 0;
+}
+
+
+
+/*
+
+                                 ____              ,----..               ,----..
+   ,---,                       ,'  , `.           /   /   \             /   /   \
+  '  .' \                   ,-+-,.' _ |          /   .     :           /   .     :
+ /  ;    '.              ,-+-. ;   , ||         .   /   ;.  \         .   /   ;.  \
+:  :       \            ,--.'|'   |  ;|        .   ;   /  ` ;        .   ;   /  ` ;
+:  |   /\   \          |   |  ,', |  ':        ;   |  ; \ ; |        ;   |  ; \ ; |
+|  :  ' ;.   :         |   | /  | |  ||        |   :  | ; | '        |   :  | ; | '
+|  |  ;/  \   \        '   | :  | :  |,        .   |  ' ' ' :        .   |  ' ' ' :
+'  :  | \  \ ,'        ;   . |  ; |--'         '   ;  \; /  |        '   ;  \; /  |
+|  |  '  '--'          |   : |  | ,             \   \  ',  /          \   \  ',  /
+|  :  :                |   : '  |/               ;   :    /            ;   :    /
+|  | ,'                ;   | |`-'                 \   \ .'              \   \ .'
+`--''                  |   ;/                      `---`                 `---`
+                       '---'
+
+*/
