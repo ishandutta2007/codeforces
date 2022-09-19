a87de9dece1386bc211b71c8c3deedf9e77fcc0f
ishan dutta <ishandutta2007@gmail.com>
2022-09-20 01:05:17 +0530

    amoo_safar

---
+++ amoo_safar/normal/1038/B.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+
+#define pb push_back
+#define mp make_pair
+#define F first
+#define S second
+#define debug(x)  cerr << #x << " = " << x << endl
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef string str;
+typedef pair<ll, ll> pll;
+
+const ld Pi = 3.14159265359;
+
+const ll MOD = (ll) 1e9 + 7;
+const ll MAXN = (ll) 3e5 + 10;
+const ll INF = (ll) 3e18;
+const ld EPS = (ld) 1e-8;
+
+ll a[30];
+
+int main(){
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    ll n;
+    cin >> n;
+    if(n < 3){
+        cout << "No";
+        return 0;
+    }
+    ll su = n*(n+1)/2;
+    for(int i = 2; i * i <= su; i++){
+        if(su%i == 0){
+            cout << "Yes" << endl;
+            cout << "1 " << i << endl;
+            cout << n-1 << " ";
+            for(int j =1; j <= n; j++){
+                if(j != i) cout << j << " ";
+            }
+            return 0;
+        }
+    }
+    cout << "No";
+    return 0;
+}
+
+
+
+/*
+
+                                 ____              ,----..               ,----..
+   ,---,                       ,'  , `.           /   /   \             /   /   \
+  '  .' \                   ,-+-,.' _ |          /   .     :           /   .     :
+ /  ;    '.              ,-+-. ;   , ||         .   /   ;.  \         .   /   ;.  \
+:  :       \            ,--.'|'   |  ;|        .   ;   /  ` ;        .   ;   /  ` ;
+:  |   /\   \          |   |  ,', |  ':        ;   |  ; \ ; |        ;   |  ; \ ; |
+|  :  ' ;.   :         |   | /  | |  ||        |   :  | ; | '        |   :  | ; | '
+|  |  ;/  \   \        '   | :  | :  |,        .   |  ' ' ' :        .   |  ' ' ' :
+'  :  | \  \ ,'        ;   . |  ; |--'         '   ;  \; /  |        '   ;  \; /  |
+|  |  '  '--'          |   : |  | ,             \   \  ',  /          \   \  ',  /
+|  :  :                |   : '  |/               ;   :    /            ;   :    /
+|  | ,'                ;   | |`-'                 \   \ .'              \   \ .'
+`--''                  |   ;/                      `---`                 `---`
+                       '---'
+
+*/
