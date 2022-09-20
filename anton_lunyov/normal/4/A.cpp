bc23be4728ed036d4f4a82417777e284402a7eb0
ishan dutta <ishandutta2007@gmail.com>
2022-09-20 15:04:24 +0530

    anton_lunyov

---
+++ anton_lunyov/normal/4/A.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <set>
+#include <map>
+#pragma comment(linker, "/STACK:16777216")
+using namespace std;
+
+#define bit(n) (1<<(n))
+#define inf 1000000000
+#define eps 1e-9
+#define PI 3.1415926535897932385
+#define pb push_back
+#define sz size()
+#define mp make_pair
+#define cl clear()
+#define all(a) a.begin(),a.end()
+#define fill(ar,val) memset(ar,val,sizeof ar)
+#define MIN(a,b) if(a>(b)) a=(b)
+#define MAX(a,b) if(a<(b)) a=(b)
+#define sqr(x) ((x)*(x))
+#define min(a,b) ((a)<(b)?(a):(b))
+#define max(a,b) ((a)>(b)?(a):(b))
+
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+//typedef long long LL;
+
+//clock_t start=clock();
+
+int main()
+{
+	//freopen("in.txt","r",stdin);
+	int n;
+	while(scanf("%d",&n)>0)
+		puts(n%2 || n==2?"NO":"YES");
+	//fprintf(stderr,"%d\n",clock()-start);
+	return 0;
+}
