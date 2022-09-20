afd1e33d5c50e1cd3ec057b5ca21ec5eb0ff7e7b
ishan dutta <ishandutta2007@gmail.com>
2022-09-20 15:08:49 +0530

    farhod_farmon

---
+++ farhod_farmon/normal/1608/A.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define fi first
+#define se second
+
+const int N = 30300;
+const int mod = 998244353;
+
+using namespace std;
+
+
+int main()
+{
+        #ifdef zxc
+                freopen("input.txt", "r", stdin);
+                freopen("output.txt", "w", stdout);
+        #endif
+        ios_base::sync_with_stdio(0);
+
+        int t;
+        cin >> t;
+        while(t--){
+                int n;
+                cin >> n;
+                for(int i = 1; i <= n; i++){
+                        cout << i + 1 << " ";
+                }
+                cout << "\n";
+        }
+}
