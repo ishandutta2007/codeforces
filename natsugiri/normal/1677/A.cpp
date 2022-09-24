c42d52815185da4b012e4e9de2ad869df6e11219
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 14:05:52 +0530

    natsugiri

---
+++ natsugiri/normal/1677/A.cpp
@@ -0,0 +1,92 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#pragma GCC optimize("unroll-loops")
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<string.h>
+
+#ifdef LOCAL
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define NDEBUG
+#define eprintf(...) do {} while (0)
+#endif
+#include<cassert>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+
+template<class T> inline void amin(T &x, const T &y) { if (y<x) x=y; }
+template<class T> inline void amax(T &x, const T &y) { if (x<y) x=y; }
+#define rprintf(fmt, begin, end) do { const auto end_rp = (end); auto it_rp = (begin); for (bool sp_rp=0; it_rp!=end_rp; ++it_rp) { if (sp_rp) putchar(' '); else sp_rp = true; printf(fmt, *it_rp); } putchar('\n'); } while(0)
+
+template<class T> struct Fenwick {
+    int n;
+    T* d;
+    Fenwick() : n(0), d(NULL) {}
+    Fenwick(int n_) : n(n_) {
+	d = new T[n_]();
+    }
+    Fenwick(const Fenwick &y) : n(y.n) {
+	d = new T[n];
+	memcpy(d, y.d, sizeof (T) * n);
+    }
+    ~Fenwick() {
+	delete[] d; d = NULL;
+	n = 0;
+    }
+    friend void swap(Fenwick &x, Fenwick &y) {
+	swap(x.n, y.n); swap(x.d, y.d);
+    }
+    Fenwick& operator=(Fenwick y) {
+	swap(*this, y);
+	return *this;
+    }
+    inline void add(int i, const T &x) {
+	for (; i<n; i|=i+1) d[i] += x;
+    }
+    inline T sum(int r) const {
+	T s = T();
+	for (; r; r&=r-1) s += d[r-1];
+	return s;
+    }
+    T sum(int l, int r) const {
+	return sum(r) - sum(l);
+    }
+};
+
+int N;
+int P[5011];
+
+void MAIN() {
+    scanf("%d", &N);
+    REP (i, N) scanf("%d", P+i);
+
+    LL ans = 0;
+    Fenwick<int> left(N+1);
+    for (int b=0; b<N; b++) {
+	Fenwick<int> right(N+1);
+	for (int c=N-1; b<c; c--) {
+	    ans += (LL)left.sum(P[c]) * right.sum(P[b]);
+	    right.add(P[c], 1);
+	}
+	left.add(P[b], 1);
+    }
+
+    printf("%lld\n", ans);
+}
+
+int main() {
+    int TC = 1;
+    scanf("%d", &TC);
+    REP (tc, TC) MAIN();
+    return 0;
+}
