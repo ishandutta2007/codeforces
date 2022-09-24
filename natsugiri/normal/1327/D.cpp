9beab853df3e4afeaec2a7754f0f89cc554de182
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 15:53:28 +0530

    natsugiri

---
+++ natsugiri/normal/1327/D.cpp
@@ -0,0 +1,80 @@
+#pragma GCC optimize ("O3")
+// #pragma GCC target ("avx")
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<string.h>
+
+#ifdef LOCAL
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define NDEBUG
+#define eprintf(...) do {} while (0)
+#endif
+#include<cassert>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+
+template<class T> inline void amin(T &x, const T &y) { if (y<x) x=y; }
+template<class T> inline void amax(T &x, const T &y) { if (x<y) x=y; }
+template<class Iter> void rprintf(const char *fmt, Iter begin, Iter end) {
+    for (bool sp=0; begin!=end; ++begin) { if (sp) putchar(' '); else sp = true; printf(fmt, *begin); }
+    putchar('\n');
+}
+
+int N;
+int P[200011];
+int C[200011];
+bool use[200011];
+
+void MAIN() {
+    scanf("%d", &N);
+    REP (i, N) scanf("%d", P+i), P[i]--;
+    REP (i, N) scanf("%d", C+i);
+    REP (i, N) use[i] = false;
+
+    int ans = N+N;
+
+    REP (i, N) if (!use[i]) {
+	int x = i;
+	VI g;
+	while (!use[x]) {
+	    use[x] = true;
+	    g.push_back(C[x]);
+	    x = P[x];
+	}
+
+	const int len = g.size();
+	for (int d=1; d<=len && d < ans; d++) if (len % d == 0) {
+	    REP (s, d) {
+		bool ok = true;
+		LL idx = s, nxt = idx + d;
+		REP (t, len / d) {
+		    if (g[idx % len] != g[nxt % len]) {
+			ok = false;
+			break;
+		    }
+		    idx += d;
+		    nxt += d;
+		}
+		if (ok) amin(ans, d);
+	    }
+	}
+    }
+    printf("%d\n", ans);
+}
+
+int main() {
+    int TC = 1;
+    scanf("%d", &TC);
+    REP (tc, TC) MAIN();
+    return 0;
+}
