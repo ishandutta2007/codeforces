cac0e49709bf8406f5abd4867710f3aa8dbdb3d1
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 20:54:29 +0530

    dorijanlendvaj

---
+++ dorijanlendvaj/normal/1468/L.cpp
@@ -0,0 +1,325 @@
+//DUEL
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define x first
+#define y second
+#define pii pair<int,int>
+#define pb push_back
+#define eb emplace_back
+#pragma GCC optimize("unroll-loops")
+#define shandom_ruffle(a, b) shuffle(a, b, rng)
+#define vi vector<int>
+#define vl vector<ll>
+#define popcnt __builtin_popcount
+#define popcntll __builtin_popcountll
+#define all(a) begin(a),end(a)
+
+using namespace std;
+using namespace __gnu_pbds;
+
+using ll=long long;
+using ull=unsigned long long;
+using ld=long double;
+int MOD=1000000007;
+int MOD2=998244353;
+vector<int> bases;
+const ll LLINF=1ll<<60;
+const char en='\n';
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+void yes() {cout<<"YES"<<en; exit(0);}
+void no() {cout<<"NO"<<en; exit(0);}
+inline int rund() {int x576363482791fuweh=rng();return abs(x576363482791fuweh)%RAND_MAX;}
+template<class T>
+void prVec(vector<T> w,bool fl=false)
+{
+	cout<<w.size()<<en;
+	for (int i=0;i<int(w.size())-1;++i) cout<<w[i]<<' ';
+	if (w.size()) cout<<w[w.size()-1]<<en;
+	if (fl) cout<<flush;
+}
+
+void M998()
+{
+	swap(MOD,MOD2);
+}
+
+ll raand()
+{
+	ll a=rund();
+	a*=RAND_MAX;
+	a+=rund();
+    return a;
+}
+
+#define rand raand
+
+ll raaand()
+{
+	return raand()*(MOD-7)+raand();
+}
+
+template<class T>
+vi compress(vector<T>&v)
+{
+	set<T> s;
+	for (auto a: v) s.insert(a);
+	vector<T> o(all(s));
+	vi nv;
+	for (int i=0;i<(int)v.size();++i) nv.pb(lower_bound(all(o),v[i])-o.begin());
+	return nv;
+}
+
+string to_upper(string a)
+{
+	for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+	return a;
+}
+
+string to_lower(string a)
+{
+	for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+	return a;
+}
+
+ll sti(string a,int base=10)
+{
+	ll k=0;
+	for (int i=0;i<(int)a.size();++i)
+	{
+		k*=base;
+		k+=a[i]-'0';
+	}
+	return k;
+}
+
+template<class T>
+void eras(vector<T>& a,T b)
+{
+	a.erase(find(a.begin(),a.end(),b));
+}
+
+string its(ll k,int base=10)
+{
+	if (k==0) return "0";
+	string a;
+	while (k)
+	{
+		a.push_back((k%base)+'0');
+		k/=base;
+	}
+	reverse(a.begin(),a.end());
+	return a;
+}
+
+ll min(ll a,int b)
+{
+	if (a<b) return a;
+	return b;
+}
+
+ll min(int a,ll b)
+{
+	if (a<b) return a;
+	return b;
+}
+
+ll max(ll a,int b)
+{
+	if (a>b) return a;
+	return b;
+}
+
+ll max(int a,ll b)
+{
+	if (a>b) return a;
+	return b;
+}
+
+ll gcd(ll a,ll b)
+{
+	if (b==0) return a;
+	return gcd(b,a%b);
+}
+
+ll lcm(ll a,ll b)
+{
+	return a/gcd(a,b)*b;
+}
+
+template<class T,class K>
+pair<T,K> mp(T a,K b)
+{
+	return make_pair(a,b);
+}
+
+inline int mult(ll a,ll b)
+{
+	return (a*b)%MOD;
+}
+
+inline int pot(int n,int k)
+{
+	if (k==0) return 1;
+	ll a=pot(n,k/2);
+	a=mult(a,a);
+	if (k%2) return mult(a,n);
+	else return a;
+}
+
+int divide(int a,int b)
+{
+	return mult(a,pot(b,MOD-2));
+}
+
+inline int sub(int a,int b)
+{
+	if (a-b>=0) return a-b;
+	return a-b+MOD;
+}
+
+inline int add(int a,int b)
+{
+	if (a+b>=MOD) return a+b-MOD;
+	return a+b;
+}
+
+bool prime(ll a)
+{
+	if (a==1) return 0;
+	for (int i=2;i<=round(sqrt(a));++i)
+	{
+		if (a%i==0) return 0;
+	}
+	return 1;
+}
+
+int dx[]={0,1,0,-1};
+int dy[]={1,0,-1,0};
+const int N=300010;
+
+ull modmul(ull a, ull b, ull M) {
+	ll ret = a * b - M * ull(1.L / M * a * b);
+	return ret + M * (ret < 0) - M * (ret >= (ll)M);
+}
+ull modpow(ull b, ull e, ull mod) {
+	ull ans = 1;
+	for (; e; b = modmul(b, b, mod), e /= 2)
+		if (e & 1) ans = modmul(ans, b, mod);
+	return ans;
+}
+
+bool isPrime(ull n) {
+	if (n < 2 || n % 6 % 4 != 1) return (n | 1) == 3;
+	ull A[] = {2, 325, 9375, 28178, 450775, 9780504, 1795265022},
+	    s = __builtin_ctzll(n-1), d = n >> s;
+	for (ull a : A) {   // ^ count trailing zeroes
+		ull p = modpow(a%n, d, n), i = s;
+		while (p != 1 && p != n - 1 && a % n && i--)
+			p = modmul(p, p, n);
+		if (p != n-1 && i != s) return 0;
+	}
+	return 1;
+}
+
+ull pollard(ull n) {
+	auto f = [n](ull x) { return modmul(x, x, n) + 1; };
+	ull x = 0, y = 0, t = 30, prd = 2, i = 1, q;
+	while (t++ % 40 || __gcd(prd, n) == 1) {
+		if (x == y) x = ++i, y = f(x);
+		if ((q = modmul(prd, max(x,y) - min(x,y), n))) prd = q;
+		x = f(x), y = f(f(y));
+	}
+	return __gcd(prd, n);
+}
+
+vector<ull> factor(ull n) {
+	if (n == 1) return {};
+	if (isPrime(n)) return {n};
+	ull x = pollard(n);
+	auto l = factor(x), r = factor(n / x);
+	l.insert(l.end(), all(r));
+	return l;
+}
+
+int n,k;
+ull h[N];
+vector<ull> re[N];
+map<ull,vector<ull>> ma;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	for (int i=0;i<10;++i) bases.push_back(rand()%(MOD-13893829*2)+13893829);
+	cin>>n>>k;
+	for (int i=0;i<n;++i)
+	{
+		cin>>h[i];
+		re[i]=factor(h[i]);
+		sort(all(re[i]));
+		if (re[i][0]==re[i].back())
+		{
+			ma[re[i][0]].pb(h[i]);
+		}
+	}
+	vector<ull> allo,res;
+	for (auto a: ma) if (a.y.size()>=2)
+	{
+		allo.pb(a.x);
+		res.pb(a.y[0]);
+		res.pb(a.y[1]);
+	}
+	set<ull> s1(all(allo)),s2(all(res));
+	if (res.size()>k)
+	{
+		if (k%2==0)
+		{
+			for (int i=0;i<k;++i) cout<<res[i]<<' ';
+			cout<<en;
+			exit(0);
+		}
+		for (int i=0;i<n;++i)
+		{
+			bool mog=1;
+			set<ull> s;
+			for (auto a: re[i])
+			{
+				if (ma[a].size()<2 || (ma[a].size()==2 && (ma[a][0]==h[i] || ma[a][1]==h[i]))) mog=0;
+				else s.insert(a);
+			}
+			if (mog && s.size()<=k/2)
+			{
+				int rem=k/2;
+				for (auto a: s)
+				{
+					int c=0;
+					for (auto z: ma[a]) if (z!=h[i] && c<2) cout<<z<<' ',++c;
+					--rem;
+				}
+				for (auto a: ma) if (a.y.size()>=2 && rem && s.find(a.x)==s.end())
+				{
+					cout<<a.y[0]<<' '<<a.y[1]<<' ';
+					--rem;
+				}
+				cout<<h[i]<<en;
+				exit(0);
+			}
+		}
+		cout<<0<<en;
+		exit(0);
+	}
+	for (int i=0;i<n;++i) if (s2.find(h[i])==s2.end())
+	{
+		bool ok=1;
+		for (auto a: re[i]) if (s1.find(a)==s1.end()) ok=0;
+		if (ok && res.size()<k) res.pb(h[i]);
+	}
+	if (res.size()<k)
+	{
+		cout<<0<<en;
+		exit(0);
+	}
+	for (auto a: res) cout<<a<<' ';
+	cout<<en;
+}
