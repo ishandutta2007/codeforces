7c6b4f455a0e5fea6a1e30643c1decda7fc2afd6
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 10:28:33 +0530

    353cerega

---
+++ 353cerega/normal/553/D.cpp
@@ -0,0 +1,377 @@
+#pragma GCC optimize("Ofast")
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = double;
+#define X first
+#define Y second
+
+const ll mod = 1000000007;
+
+ll gcd(ll a, ll b)
+{
+    if (a==0 or b==0) return a+b;
+    return gcd(b,a%b);
+}
+
+
+ll sum(ll a, ll b)
+{
+    a += b;
+    a %= mod;
+    return a;
+}
+
+ll sub(ll a, ll b)
+{
+    a += mod-b;
+    a %= mod;
+    return a;
+}
+
+inline ll mul(ll a, ll b)
+{
+    if (a*b<mod) return a*b;
+    return a*b%mod;
+}
+
+inline ll pew(ll a, ll b)
+{
+    ll res = 1;
+    while (b)
+    {
+        if (b&1) res = mul(res,a);
+        a = mul(a,a);
+        b /= 2;
+    }
+    return res;
+}
+
+
+
+/*const int MOD = 998244353;
+const int root = 31;
+const int root_1 = 128805723;
+const int root_pw = 1<<23;
+const int root = 31;
+const int MOD = 469762049;
+const int root_1 = 15658735;
+const int root_pw = 1<<26;*/
+
+
+/*void fft (vector<int> & a, bool invert) {
+    int n = (int) a.size();
+
+    for (int i=1, j=0; i<n; ++i) {
+        int bit = n >> 1;
+        for (; j>=bit; bit>>=1)
+            j -= bit;
+        j += bit;
+        if (i < j)
+            swap (a[i], a[j]);
+    }
+
+    for (int len=2; len<=n; len<<=1) {
+        int wlen = invert ? root_1 : root;
+        for (int i=len; i<root_pw; i<<=1)
+            wlen = int (wlen * 1ll * wlen % mod);
+        for (int i=0; i<n; i+=len) {
+            int w = 1;
+            for (int j=0; j<len/2; ++j) {
+                int u = a[i+j],  v = int (a[i+j+len/2] * 1ll * w % mod);
+                a[i+j] = u+v < mod ? u+v : u+v-mod;
+                a[i+j+len/2] = u-v >= 0 ? u-v : u-v+mod;
+                w = int (w * 1ll * wlen % mod);
+            }
+        }
+    }
+    if (invert) {
+        int nrev = pew(n, mod-2);
+        for (int i=0; i<n; ++i)
+            a[i] = int (a[i] * 1ll * nrev % mod);
+    }
+}*/
+
+
+int parent[600001];
+int rnk[600001];
+
+void make_set (int v) {
+    parent[v] = v;
+    rnk[v] = 0;
+}
+
+int find_set (int v) {
+    if (v == parent[v])
+        return v;
+    return parent[v] = find_set(parent[v]);
+}
+
+void union_sets (int a, int b) {
+    a = find_set(a);
+    b = find_set(b);
+    if (a != b) {
+        if (rnk[a] < rnk[b])
+            swap(a, b);
+        parent[b] = a;
+        if (rnk[a] == rnk[b])
+            ++rnk[a];
+    }
+}
+
+
+//ll t[1200000];
+//ll del[1200000];
+
+/*void build()
+{
+    for (int i=0;i<1200000;i++)
+    {
+        t[i] = 0;
+        //del[i] = 0;
+    }
+}*/
+
+/*void push(int v, int tl, int tr, int tm)
+{
+    t[2*v].X += del[v];
+    t[2*v+1].X += del[v];
+    del[2*v] += del[v];
+    del[2*v+1] += del[v];
+    del[v] = 0;
+}*/
+
+/*void update (int v, int tl, int tr, int l, int r, ll val) {
+    if (tl==l and tr==r)
+    {
+        //del[v] += val;
+
+        //t[v] += val*(tr-tl+1);
+        return;
+    }
+    int tm = (tl + tr) / 2;
+    //if (del[v]!=0) push(v,tl,tr,tm);
+    if (r<=tm)
+    {
+        update(2*v,tl,tm,l,r,val);
+        t[v] = t[2*v]+t[2*v+1];
+        return;
+    }
+    if (l>tm)
+    {
+        update(2*v+1,tm+1,tr,l,r,val);
+        t[v] = t[2*v]+t[2*v+1];
+        return;
+    }
+    update(2*v,tl,tm,l,tm,val);
+    update(2*v+1,tm+1,tr,tm+1,r,val);
+    t[v] = min(t[2*v],t[2*v+1]);
+}*/
+
+
+/*void update (int v, int tl, int tr, int pos, ll val) {
+    if (tl==tr)
+    {
+        //del[v] += val;
+        t[v] += val;
+        return;
+    }
+    int tm = (tl+tr)/2;
+    //push(v,tl,tr,tm);
+    if (pos<=tm)
+    {
+        update(2*v,tl,tm,pos,val);
+        t[v] = t[2*v]+t[2*v+1];
+        return;
+    }
+    else
+    {
+        update(2*v+1,tm+1,tr,pos,val);
+        t[v] = t[2*v]+t[2*v+1];
+        return;
+    }
+    //update(2*v,tl,tm,l,tm,val);
+    //update(2*v+1,tm+1,tr,tm+1,r,val);
+    //t[v] = min(t[2*v],t[2*v+1]);
+}
+
+
+
+ll get (int v, int tl, int tr, int l, int r) {
+    //if (del[v]!=0 and tl!=tr) push(v,tl,tr,tm);
+    if (tl==l and tr==r)
+    {
+        return t[v];
+    }
+    int tm = (tl+tr)/2;
+    //push(v,tl,tr,tm);
+    if (r<=tm) return get(2*v,tl,tm,l,r);
+    if (l>tm) return get(2*v+1,tm+1,tr,l,r);
+    return get(2*v,tl,tm,l,tm)+get(2*v+1,tm+1,tr,tm+1,r);
+}*/
+
+/*
+const ll base = 10;
+
+vector<ll> mul(vector<ll> a, vector<ll> b)
+{
+    vector<ll> res(a.size()+b.size());
+    for (int i=0;i<a.size();i++)
+    {
+        for (int j=0;j<b.size();j++)
+        {
+            res[i+j] += a[i]*b[j];
+        }
+    }
+    for (int i=0;i<res.size();i++)
+    {
+        if (res[i]>base)
+        {
+            res[i+1] += res[i]/base;
+            res[i] %= base;
+        }
+    }
+    for (int i=res.size()-1;i>0;i--)
+    {
+        if (res[i]==0)
+        {
+            res.pop_back();
+        }
+        else
+        {
+            break;
+        }
+    }
+    return res;
+}
+
+vector<ll> sum(vector<ll> a, vector<ll> b)
+{
+    if (a.size()<b.size())
+    {
+        swap(a,b);
+    }
+    vector<ll> res(a.size()+b.size());
+    for (int i=0;i<b.size();i++)
+    {
+        res[i] += a[i]+b[i];
+    }
+    for (int i=b.size();i<a.size();i++)
+    {
+        res[i] = a[i];
+    }
+    for (int i=0;i<res.size();i++)
+    {
+        if (res[i]>=base)
+        {
+            res[i+1] += res[i]/base;
+            res[i] %= base;
+        }
+    }
+    for (int i=res.size()-1;i>0;i--)
+    {
+        if (res[i]==0)
+        {
+            res.pop_back();
+        }
+        else
+        {
+            break;
+        }
+    }
+    return res;
+}
+
+vector<ll> sub(vector<ll> a, vector<ll> b)
+{
+    vector<ll> res(a.size()+b.size());
+    for (int i=0;i<b.size();i++)
+    {
+        res[i] += a[i]-b[i];
+    }
+    for (int i=b.size();i<a.size();i++)
+    {
+        res[i] = a[i];
+    }
+    for (int i=0;i<res.size();i++)
+    {
+        if (res[i]<0)
+        {
+            res[i+1]--;
+            res[i] += base;
+        }
+    }
+    for (int i=res.size()-1;i>0;i--)
+    {
+        if (res[i]==0)
+        {
+            res.pop_back();
+        }
+        else
+        {
+            break;
+        }
+    }
+    return res;
+}*/
+
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    int n, m, k;
+    cin >> n >> m >> k;
+    vector<int> lul(k);
+    for (int i=0;i<k;i++)
+    {
+        cin >> lul[i];
+    }
+    sort(lul.begin(),lul.end());
+    vector<vector<int>> g(n+1);
+    vector<int> d(n+1);
+    for (int i=0;i<m;i++)
+    {
+        int a, b;
+        cin >> a >> b;
+        g[a].push_back(b);
+        g[b].push_back(a);
+        d[a]++;
+        d[b]++;
+    }
+    ld L = -0.0001;
+    ld R = 1.0001;
+    vector<int> ans;
+    for (int z=0;z<70;z++)
+    {
+        ld M = (L+R)*0.5;
+        vector<int> q = lul;
+        vector<int> cur(n+1);
+        vector<int> was(n+1);
+        for (int i=0;i<k;i++) was[lul[i]] = 1;
+        for (int st=0;st<q.size();st++)
+        {
+            int u = q[st];
+            for (int v: g[u])
+            {
+                if (was[v]==1) continue;
+                cur[v]++;
+                if (d[v]*M<cur[v])
+                {
+                    was[v] = 1;
+                    q.push_back(v);
+                }
+            }
+        }
+        if (q.size()<n)
+        {
+            ans.clear();
+            for (int i=1;i<=n;i++) if (was[i]==0) ans.push_back(i);
+            R = M;
+        } else L = M;
+    }
+    cout << ans.size() << endl;
+    for (int i=0;i<ans.size();i++) cout << ans[i] << " ";
+}
