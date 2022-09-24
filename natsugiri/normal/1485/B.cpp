2d3954e13f5c90b94cfa5b01042998a8d775636c
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 15:02:38 +0530

    natsugiri

---
+++ natsugiri/normal/1485/B.cpp
@@ -0,0 +1,57 @@
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<string.h>
+
+#ifdef LOCAL
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define NDEBUG
+#define eprintf(...) do {} while (0)
+#endif
+#include<cassert>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+
+template<class T> inline void amin(T &x, const T &y) { if (y<x) x=y; }
+template<class T> inline void amax(T &x, const T &y) { if (x<y) x=y; }
+#define rprintf(fmt, begin, end) do { const auto end_rp = (end); auto it_rp = (begin); for (bool sp_rp=0; it_rp!=end_rp; ++it_rp) { if (sp_rp) putchar(' '); else sp_rp = true; printf(fmt, *it_rp); } putchar('\n'); } while(0)
+
+int N;
+int Q;
+LL K;
+LL A[100011];
+
+void MAIN() {
+    scanf("%d%d%lld", &N, &Q, &K);
+    REP (i, N) scanf("%lld", A+i);
+
+    REP ($, Q) {
+	int l, r;
+	scanf("%d%d", &l, &r);
+	l--;
+
+	LL ans = 0;
+	ans += A[l] - 1;
+	ans += K - A[r-1];
+	ans += ((A[r-1] - A[l] + 1) - (r-l)) * 2;
+	printf("%lld\n", ans);
+    }
+}
+
+int main() {
+    int TC = 1;
+//    scanf("%d", &TC);
+    REP (tc, TC) MAIN();
+    return 0;
+}
