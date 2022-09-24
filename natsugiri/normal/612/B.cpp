df2d5256e9b6e30229aca343ed853e365d2d9a21
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 18:11:52 +0530

    natsugiri

---
+++ natsugiri/normal/612/B.cpp
@@ -0,0 +1,37 @@
+#include<cstdio>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<cstring>
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+#define eprintf(s...) fprintf(stderr, s)
+
+template<class T> inline void amin(T &a, const T &b) { if (b<a) a=b; }
+template<class T> inline void amax(T &a, const T &b) { if (a<b) a=b; }
+
+
+int N, A[200011], P[200011];
+
+int main() {
+    scanf("%d", &N);
+    REP (i, N) {
+	scanf("%d", A+i);
+	A[i]--;
+	P[A[i]] = i;
+    }
+
+    LL ans = 0;
+    REP (i, N-1)
+	ans += abs(P[i+1] - P[i]);
+
+    printf("%lld\n", ans);
+    
+    return 0;
+}
