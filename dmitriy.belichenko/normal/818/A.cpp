3c2799401daefa8be2412a988f386646bb34437b
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 12:02:52 +0530

    dmitriy.belichenko

---
+++ dmitriy.belichenko/normal/818/A.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<math.h>
+#include<set>
+#include<algorithm>
+#include<vector>
+#include<map>
+#include<iomanip>
+#include<deque>
+using namespace std;
+long long inf = 1LL << 62;
+static long parseans(long j, long l)
+{
+    string k = "";
+    long ps = 0;
+    for (long long i = (long long) j; i <= l; ++i)
+    {
+        ps *= 10;
+        ps += k[i] - '0';
+    }
+    return ps;
+}
+long long n , s;
+
+int cur = 1;
+int cnt = 0;
+static long gcd(long a, long b)
+{
+    if (b == 0)
+    {
+      return a;
+    }
+    else
+    {
+        return gcd(b, a % b);
+    }
+}
+
+static bool pri(long long k)
+{
+    if (k == 1LL) return false;
+    for (long long i = 2; i * i <= k; i++)
+    {
+        
+        if (k % i == 0) return false;
+    }
+    return true;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    cout.precision(20);
+    long long n , k;
+    cin >> n >> k;
+    cout << (n/2)/(k + 1) << " " << (n/2)/(k+1)*k << " " << n - (n/2)/(k + 1)- (n/2)/(k+1)*k;
+}
