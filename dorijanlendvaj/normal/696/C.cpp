bdcbaf76884fd43d2690b9bb902e6b7b264f3bd6
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 21:57:34 +0530

    dorijanlendvaj

---
+++ dorijanlendvaj/normal/696/C.cpp
@@ -0,0 +1,230 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#define x first
+#define y second
+#define pii pair<int,int>
+#define pb push_back
+#define eb emplace_back
+#pragma GCC optimize("unroll-loops")
+#define shandom_ruffle(a, b) shuffle(a, b, rng)
+#define vi vector<int>
+#define vl vector<ll>
+#define popcnt __builtin_popcount
+#define popcntll __builtin_popcountll
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef long long int ll;
+typedef unsigned long long int ull;
+int MOD=1000000007;
+int MOD2=998244353;
+vector<int> bases;
+const ll LLINF=1ll<<60;
+const char en='\n';
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+void yes() {cout<<"YES"<<en; exit(0);}
+void no() {cout<<"NO"<<en; exit(0);}
+inline int rund() {int x576363482791fuweh=rng();return abs(x576363482791fuweh)%RAND_MAX;}
+template<class T>
+void prVec(vector<T> w)
+{
+	cout<<w.size()<<endl;
+	for (int i=0;i<int(w.size())-1;++i) cout<<w[i]<<' ';
+	if (w.size()) cout<<w[w.size()-1]<<endl;
+}
+
+void M998()
+{
+	swap(MOD,MOD2);
+}
+
+ll raand()
+{
+	ll a=rund();
+	a*=RAND_MAX;
+	a+=rund();
+    return a;
+}
+
+#define rand raand
+
+ll raaand()
+{
+	return raand()*(MOD-7)+raand();
+}
+
+string to_upper(string a)
+{
+	for (int i=0;i<(int)a.size();++i) if (a[i]>='a' && a[i]<='z') a[i]-='a'-'A';
+	return a;
+}
+
+string to_lower(string a)
+{
+	for (int i=0;i<(int)a.size();++i) if (a[i]>='A' && a[i]<='Z') a[i]+='a'-'A';
+	return a;
+}
+
+ll sti(string a,int base=10)
+{
+	ll k=0;
+	for (int i=0;i<(int)a.size();++i)
+	{
+		k*=base;
+		k+=a[i]-'0';
+	}
+	return k;
+}
+
+string its(ll k,int base=10)
+{
+	if (k==0) return "0";
+	string a;
+	while (k)
+	{
+		a.push_back((k%base)+'0');
+		k/=base;
+	}
+	reverse(a.begin(),a.end());
+	return a;
+}
+
+ll min(ll a,int b)
+{
+	if (a<b) return a;
+	return b;
+}
+
+ll min(int a,ll b)
+{
+	if (a<b) return a;
+	return b;
+}
+
+ll max(ll a,int b)
+{
+	if (a>b) return a;
+	return b;
+}
+
+ll max(int a,ll b)
+{
+	if (a>b) return a;
+	return b;
+}
+
+ll gcd(ll a,ll b)
+{
+	if (b==0) return a;
+	return gcd(b,a%b);
+}
+
+ll lcm(ll a,ll b)
+{
+	return a/gcd(a,b)*b;
+}
+
+template<class T,class K>
+pair<T,K> mp(T a,K b)
+{
+	return make_pair(a,b);
+}
+
+inline int mult(ll a,ll b)
+{
+	return (a*b)%MOD;
+}
+
+inline int pot(int n,ll k)
+{
+	if (k==0) return 1;
+	ll a=pot(n,k/2);
+	a=mult(a,a);
+	if (k%2) return mult(a,n);
+	else return a;
+}
+
+int divide(int a,int b)
+{
+	return mult(a,pot(b,MOD-2));
+}
+
+inline int sub(int a,int b)
+{
+	if (a-b>=0) return a-b;
+	return a-b+MOD;
+}
+
+inline int add(int a,int b)
+{
+	if (a+b>=MOD) return a+b-MOD;
+	return a+b;
+}
+
+bool prime(ll a)
+{
+	if (a==1) return 0;
+	for (int i=2;i<=round(sqrt(a));++i)
+	{
+		if (a%i==0) return 0;
+	}
+	return 1;
+}
+
+const int N=300010,M2=(MOD+1)/2;
+int n;
+ll k[N];
+array<int,9> ma;
+
+void print(array<int,9> x)
+{
+	for (int i=0;i<9;++i)
+	{
+		cout<<x[i]<<' ';
+		if (i%3==2) cout<<en;
+	}
+	cout<<en;
+}
+
+
+array<int,9> mul(array<int,9> a,array<int,9> b)
+{
+	array<int,9> x={0,0,0,0,0,0,0,0,0};
+	for (int i=0;i<3;++i) for (int j=0;j<3;++j) x[i*3+j]=(a[i*3]*1ll*b[j]+a[i*3+1]*1ll*b[3+j]+a[i*3+2]*1ll*b[6+j])%MOD;
+	return x;
+}
+
+array<int,9> ex(ll am)
+{
+	if (am==0) return {1,0,0,0,1,0,0,0,1};
+	array<int,9> mat=ex(am/2);
+	array<int,9> res=mul(mat,mat);
+	if (am%2) res=mul(res,ma);
+	return res;
+}
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	for (int i=0;i<10;++i) bases.push_back(rand()%(MOD-13893829)+13893829);
+	cin>>n;
+	array<int,9> ans={M2,M2,0,M2,0,M2,0,M2,M2};
+	int dem=2;
+	for (int i=0;i<n;++i)
+	{
+		cin>>k[i];
+		ma=ans;
+		ans=ex(k[i]);
+		dem=pot(dem,k[i]);
+	}
+	dem=divide(dem,2);
+	if (ans[4]==0)
+	{
+		cout<<"0/1";
+		exit(0);
+	}
+	cout<<mult(ans[4],dem)<<'/'<<dem<<endl;
+}
