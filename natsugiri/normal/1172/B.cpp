0cfdae88e7a41ed432b6a7fc80ab61d6ae6f89f0
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 16:18:46 +0530

    natsugiri

---
+++ natsugiri/normal/1172/B.cpp
@@ -0,0 +1,116 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<string.h>
+
+#ifdef LOCAL
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define NDEBUG
+#define eprintf(...) do {} while (0)
+#endif
+#include<cassert>
+
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+
+template<class T> inline void amin(T &x, const T &y) { if (y<x) x=y; }
+template<class T> inline void amax(T &x, const T &y) { if (x<y) x=y; }
+template<class Iter> void rprintf(const char *fmt, Iter begin, Iter end) {
+    for (bool sp=0; begin!=end; ++begin) { if (sp) putchar(' '); else sp = true; printf(fmt, *begin); }
+    putchar('\n');
+}
+
+template<unsigned MOD_> struct ModInt {
+    static const unsigned MOD = MOD_;
+    unsigned x;
+    void undef() { x = (unsigned)-1; }
+    bool isnan() const { return x == (unsigned)-1; }
+    inline int geti() const { return (int)x; }
+    ModInt() { x = 0; }
+    ModInt(const ModInt &y) { x = y.x; }
+    ModInt(int y) { if (y<0 || (int)MOD<=y) y %= (int)MOD; if (y<0) y += MOD; x=y; }
+    ModInt(unsigned y) { if (MOD<=y) x = y % MOD; else x = y; }
+    ModInt(long long y) { if (y<0 || MOD<=y) y %= MOD; if (y<0) y += MOD; x=y; }
+    ModInt(unsigned long long y) { if (MOD<=y) x = y % MOD; else x = y; }
+    ModInt &operator+=(const ModInt y) { if ((x += y.x) >= MOD) x -= MOD; return *this; }
+    ModInt &operator-=(const ModInt y) { if ((x -= y.x) & (1u<<31)) x += MOD; return *this; }
+    ModInt &operator*=(const ModInt y) { x = (unsigned long long)x * y.x % MOD; return *this; }
+    ModInt &operator/=(const ModInt y) { x = (unsigned long long)x * y.inv().x % MOD; return *this; }
+    ModInt operator-() const { return (x ? MOD-x: 0); }
+
+    ModInt inv() const { return pow(MOD-2); }
+    ModInt pow(long long y) const {
+	ModInt b = *this, r = 1;
+	if (y < 0) { b = b.inv(); y = -y; }
+	for (; y; y>>=1) {
+	    if (y&1) r *= b;
+	    b *= b;
+	}
+	return r;
+    }
+
+    ModInt extgcd() const {
+	unsigned a = MOD, b = x; int u = 0, v = 1;
+	while (b) {
+	    int t = a / b;
+	    a -= t * b; swap(a, b);
+	    u -= t * v; swap(u, v);
+	}
+	if (u < 0) u += MOD;
+	return ModInt(u);
+    }
+
+    friend ModInt operator+(ModInt x, const ModInt y) { return x += y; }
+    friend ModInt operator-(ModInt x, const ModInt y) { return x -= y; }
+    friend ModInt operator*(ModInt x, const ModInt y) { return x *= y; }
+    friend ModInt operator/(ModInt x, const ModInt y) { return x *= y.inv(); }
+    friend bool operator<(const ModInt x, const ModInt y) { return x.x < y.x; }
+    friend bool operator==(const ModInt x, const ModInt y) { return x.x == y.x; }
+    friend bool operator!=(const ModInt x, const ModInt y) { return x.x != y.x; }
+};
+
+const LL MOD = 998244353;
+typedef ModInt<MOD> Mint;
+
+const int MAXN = 200011;
+
+int N;
+VI G[MAXN];
+
+int par[MAXN];
+int ch[MAXN];
+Mint fact[MAXN];
+
+void MAIN() {
+    scanf("%d", &N);
+    REP (i, N-1) {
+	int x, y;
+	scanf("%d%d", &x, &y);
+	x--; y--;
+	G[x].push_back(y);
+	G[y].push_back(x);
+    }
+
+    fact[0] = 1;
+    REP (i, MAXN-1) fact[i+1] = fact[i] * (i+1);
+
+    Mint ans = N;
+    REP (i, N) ans *= fact[G[i].size()];
+
+    printf("%d\n", ans.geti());
+}
+
+int main() {
+    int TC = 1;
+//    scanf("%d", &TC);
+    REP (tc, TC) MAIN();
+    return 0;
+}
