c3683caf7906ff9e471fce110c72dd09330f5fa9
ishan dutta <ishandutta2007@gmail.com>
2022-09-24 17:33:51 +0530

    natsugiri

---
+++ natsugiri/normal/776/B.cpp
@@ -0,0 +1,64 @@
+#include<stdio.h>
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<string.h>
+using namespace std;
+
+typedef long long LL;
+typedef vector<int> VI;
+
+#define REP(i,n) for(int i=0, i##_len=(n); i<i##_len; ++i)
+#define EACH(i,c) for(__typeof((c).begin()) i=(c).begin(),i##_end=(c).end();i!=i##_end;++i)
+#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+
+template<class T> inline void amin(T &x, const T &y) { if (y<x) x=y; }
+template<class T> inline void amax(T &x, const T &y) { if (x<y) x=y; }
+template<class Iter> void rprintf(const char *fmt, Iter begin, Iter end) {
+    for (bool sp=0; begin!=end; ++begin) { if (sp) putchar(' '); else sp = true; printf(fmt, *begin); }
+    putchar('\n');
+}
+
+/*
+const int prime[26]={2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 0};
+const int SZ=25;
+*/
+
+const int MAX = 10000000;
+int min_factor[MAX+1];
+vector<int>prime;
+void make_prime() {
+    for (int i=2; i<=MAX; i+=2) min_factor[i] = 2;
+    for (int i=3; i<=MAX; i+=3) if (!min_factor[i]) min_factor[i] = 3;
+    for (int i=5, d=2; i*i<=MAX; ) {
+	if (!min_factor[i]) {
+	    min_factor[i] = i;
+	    for (int j=i*i; j<=MAX; j+=i)
+		if (!min_factor[j]) min_factor[j] = i;
+	}
+	i += d; d = 6 - d;
+    }
+    for (int i=2; i<=MAX; i++) {
+	if (min_factor[i]==0) min_factor[i] = i;
+	if (min_factor[i]==i) prime.push_back(i);
+    }
+}
+
+int N;
+int A[100111];
+
+int main() {
+    make_prime();
+    scanf("%d", &N);
+
+    if (N <= 2) puts("1"); // 2 3
+    else puts("2"); // 2 3 4 ..
+
+    for (int i=2; i<=N+1; i++) 
+	printf("%c%c", min_factor[i] == i? '1' : '2', i==N+1?'\n':' ');
+
+
+
+    return 0;
+}
