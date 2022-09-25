234af89649525d0f5334365a65edb06d6b86c101
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 01:53:02 +0530

    errorgorn

---
+++ errorgorn/normal/1153/C.cpp
@@ -0,0 +1,94 @@
+//
+//
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+//#define endl '\n'
+#define debug(x) cout << #x << " is " << x << endl
+
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int n;
+string s;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	cin>>n;
+	cin>>s;
+	
+	if (s.front()==')' || s.back()=='(' || n%2==1){
+		cout<<":("<<endl;
+		return 0;
+	}
+	
+	s.front()='(',s.back()=')';
+	
+	int sum=0;
+	int miss=0;
+	
+	for (auto &it:s){
+		if (it=='(') sum++;
+		else if (it==')') sum--;
+		else miss++;
+	}
+	
+	if (miss<sum){
+		cout<<":("<<endl;
+		return 0;
+	}
+	
+	int c=0;
+	s.pob();
+	for (auto &it:s){
+		if (it=='?'){
+			if (sum<miss){
+				it='(';
+				sum++;
+			}
+			else{
+				it=')';
+				sum--;
+			}
+		
+			miss--;
+		}
+		
+		if (it=='(') c++;
+		else c--;
+		
+		if (c<=0){
+			cout<<":("<<endl;
+			return 0;
+		}
+	}
+	
+	cout<<s<<")"<<endl;
+}
