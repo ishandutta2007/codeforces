b33e4bf08aaadcbe3921c5ec9336107dbcfb3c1f
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 00:53:16 +0530

    errorgorn

---
+++ errorgorn/normal/1686/A.cpp
@@ -0,0 +1,71 @@
+
+// Problem: A. Everything Everywhere All But One
+// Contest: Codeforces - antontrygubO_o + dario2994
+// URL: https://codeforces.com/gym/378507/problem/A
+// Memory Limit: 256 MB
+// Time Limit: 1000 ms
+// Powered by CP Editor (https://github.com/cpeditor/cpeditor)
+
+//
+//
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define int long long
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << ": " << x << endl
+
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int n;
+int arr[55];
+
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	int TC;
+	cin>>TC;
+	while (TC--){
+		cin>>n;
+		rep(x,0,n) cin>>arr[x];
+		
+		int tot=0;
+		rep(x,0,n) tot+=arr[x];
+		
+		if (tot%n==0) tot/=n;
+		else tot=-1;
+		
+		bool ok=false;
+		rep(x,0,n) if (arr[x]==tot) ok=true;
+		
+		if (ok) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	}
+}
