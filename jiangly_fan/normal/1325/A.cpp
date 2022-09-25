2b1d2c7cc2db3e551b7d5b796cf2d51048b9cc16
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 23:02:56 +0530

    jiangly_fan

---
+++ jiangly_fan/normal/1325/A.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+using LL = long long;
+constexpr int maxn = -1;
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    int t;
+    for(cin >> t; t; t -= 1){
+        int x;
+        cin >> x;
+        cout << x - 1 << " " << 1 << "\n";
+    }
+    return 0;
+}
