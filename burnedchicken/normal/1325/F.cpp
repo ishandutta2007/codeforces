6bc22be02bf76832b4ba6e22d7b842be0cfaf3d3
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 21:05:10 +0530

    burnedchicken

---
+++ burnedchicken/normal/1325/F.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+#define int long long
+#define ull unsigned long long
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<((int)(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=((int)(a)); i--)
+#define all(a) a.begin(),a.end()
+#define pii pair<int,int>
+#define pb push_back
+#define mp make_pair
+//#define inf 2000000000
+#define inf 8000000000000000000
+#define pi 3.14159265358979323846264338328
+#define eps 1e-9
+#define sz(a) ((int)a.size())
+#define pow2(x) (1ll<<(x))
+#define ceiling(a,b) (((a)+(b)-1)/(b))
+#define print0(a) cout << (a) << ' '
+#define print1(a) cout << (a) << '\n'
+#define print2(a,b) cout << (a) << ' ',print1(b)
+#define print3(a,b,c) cout << (a) << ' ',print2(b,c)
+#define print4(a,b,c,d) cout << (a) << ' ',print3(b,c,d)
+#define ykh mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#ifdef i_am_noob
+#define debug(a) print2(#a,a)
+#else
+#define debug(a)
+#endif
+#define ordered_set tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+const int Mod=1000000007,Mod2=998244353;
+const int MOD=Mod;
+
+inline int add(int a, int b){return (a+b>=MOD?a+b-MOD:a+b);}
+inline int sub(int a, int b){return (a+MOD-b>=MOD?a-b:a+MOD-b);}
+int Pow(int a, int b){
+	if(!b) return 1;
+	int tmp=Pow(a*a%MOD,b>>1);
+	return (b&1?tmp*a%MOD:tmp);
+}
+inline int inverse(int n){return Pow(n,MOD-2);}
+
+const int maxn=100005;
+//i_am_noob
+int n,m,u,v,k,dep[maxn],fa[maxn],maxx=-inf,maxid;
+vector<int> adj[maxn],vec[355];
+bool vis[maxn];
+
+void dfs(int u, int pa){
+    dep[u]=(pa==-1?0:dep[pa]+1);
+    fa[u]=pa;
+    vec[dep[u]%k].pb(u);
+    vis[u]=true;
+    for(auto v: adj[u]){
+        if(vis[v]){
+            if(dep[u]-dep[v]>=k){
+                print1(2),print1(dep[u]-dep[v]+1);
+                int tmp=u;
+                while(tmp!=v){
+                    print0(tmp+1);
+                    tmp=fa[tmp];
+                }
+                print0(v+1);
+                exit(0);
+            }
+            continue;
+        }
+        dfs(v,u);
+    }
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0),cin.tie(0);
+    cin >> n >> m;
+    rep(m){
+        cin >> u >> v;
+        u--,v--;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+    k=sqrt(n)+eps;
+    dfs(0,-1);
+    rep(k) if(sz(vec[i])>maxx) maxx=sz(vec[i]),maxid=i;
+    print1(1);
+    rep(sz(vec[maxid])){
+        if(i*i>=n) break;
+        print0(vec[maxid][i]+1);
+    }
+    return 0;
+}
