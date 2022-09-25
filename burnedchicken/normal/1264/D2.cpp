843ea7e5e4924d4f853941449ad36a1179f3b844
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 20:41:56 +0530

    burnedchicken

---
+++ burnedchicken/normal/1264/D2.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+#include <bits/extc++.h>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define int long long
+#define ull unsigned long long
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<((int)(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=((int)(a)); i--)
+#define all(a) a.begin(),a.end()
+#define pii pair<int,int>
+#define pb push_back
+//#define inf 1010000000
+#define inf 4000000000000000000
+#define eps 1e-9
+#define sz(a) ((int)a.size())
+#define pow2(x) (1ll<<(x))
+#define ceiling(a,b) (((a)+(b)-1)/(b))
+#define print0(a) cout << (a) << ' '
+#define ykh mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#define ordered_set tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+#ifdef i_am_noob
+#define bug(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", _do(__VA_ARGS__)
+template<typename T> void _do(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void _do(T && x, S&&...y) {cerr << x << ", "; _do(y...);}
+#else
+#define bug(...) 826
+#endif
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+const int Mod=1000000007,Mod2=998244353;
+const int MOD=Mod2;
+const int maxn=1000005;
+//i_am_noob
+template <int mod>
+struct Modint{
+    int val;
+    Modint(int _val=0){val=(_val%mod+mod)%mod;}
+    operator int() const {return (*this).val;}
+    Modint operator +(const Modint& o) const {return val+o.val>=mod?Modint(val+o.val-mod):Modint(val+o.val);}
+    Modint operator -() const {return val?Modint(mod-val):*this;}
+    Modint operator -(const Modint& o) const {return val-o.val<0?Modint(val-o.val+mod):Modint(val-o.val);}
+    Modint operator *(const Modint& o) const {return Modint(val*o.val%mod);}
+    Modint operator +=(const Modint& o){*this=(*this)+o;return *this;}
+    Modint operator -=(const Modint& o){*this=(*this)-o;return *this;}
+    Modint operator *=(const Modint& o){*this=(*this)*o;return *this;}
+    Modint Pow(int b) const {
+        Modint tmp(val),ret(1);
+        while(b){
+            if(b&1) ret*=tmp;
+            b>>=1;tmp*=tmp;
+        }
+        return ret;
+    }
+    Modint Pow(const Modint& a, int b) const {return a.Pow(b);}
+    inline Modint inv() const {return (*this).Pow(mod-2);}
+    Modint operator /(const Modint& o) const {return *this*o.inv();}
+    bool operator ==(const Modint& o) const {return val==o.val;}
+};
+#define modint Modint<MOD>
+int n,cntl,cntr,cnt1,cnt2;
+modint fact[maxn],invfact[maxn],ans;
+string str;
+
+inline modint C(int n, int m){return n<0||m<0||m>n?modint(0):fact[n]*invfact[m]*invfact[n-m];}
+
+signed main(){
+    ios_base::sync_with_stdio(0),cin.tie(0);
+    #ifdef i_am_noob
+    freopen("input1.txt","r",stdin);
+    freopen("output1.txt","w",stdout);
+    #endif
+    cin >> str;
+    n=sz(str);
+    fact[0]=invfact[0]=invfact[1]=1;
+    rep2(i,1,maxn) fact[i]=fact[i-1]*modint(i);
+    rep2(i,2,maxn) invfact[i]=invfact[MOD%i]*modint(MOD-MOD/i);
+    rep2(i,2,maxn) invfact[i]*=invfact[i-1];
+    rep(n) if(str[i]==')') cntr++;
+    rep(n) if(str[i]=='?') cnt2++;
+    rep(n){
+        if(str[i]=='(') cntl++;
+        else if(str[i]==')') cntr--;
+        else cnt1++,cnt2--;
+        ans+=modint(cntl)*C(cnt1+cnt2,cnt2-cntl+cntr)+modint(cnt1)*C(cnt1+cnt2-1,cnt2-cntl+cntr-1);
+    }
+    print(ans);
+    return 0;
+}
