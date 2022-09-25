460a15d801bf79f8a365e5fc8ad46722fd8822d4
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 21:43:10 +0530

    burnedchicken

---
+++ burnedchicken/normal/1304/B.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<(int(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=(int(a)); i--)
+#define all(a) a.begin(),a.end()
+#define forall(a,type) for(type::iterator it=a.begin(); it!=a.end(); it++)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define sz(a) ((int)a.size())
+#define pow2(x) (1ll<<(x))
+#define print0(a) cout << (a) << ' '
+#define print1(a) cout << (a) << '\n'
+#define print2(a,b) cout << (a) << ' ',print1(b)
+#define print3(a,b,c) cout << (a) << ' ',print2(b,c)
+#define print4(a,b,c,d) cout << (a) << ' ',print3(b,c,d)
+#define printvec(a) rep(a.size()) print0(a[i])
+#define debug(a) print2(#a,a)
+ 
+using namespace std;
+ 
+const ll Mod=1000000007,Mod2=998244353;
+const ll MOD=Mod2;
+ 
+inline ll add(ll a, ll b){return (a+b>=MOD?a+b-MOD:a+b);}
+inline ll sub(ll a, ll b){return (a+MOD-b>=MOD?a-b:a+MOD-b);}
+inline ll Pow(ll a, ll b){
+	if(!b) return 1;
+	ll tmp=Pow(a*a%MOD,b>>1);
+	return (b&1?tmp*a%MOD:tmp);
+}
+inline ll inverse(ll n){return Pow(n,MOD-2);}
+ll dgts2(ll n){return (n?dgts2(n>>1)+1:0);}
+ll dgts(ll n, ll k){return (n?dgts(n/k,k)+1:0);}
+inline ll v2(ll n){return __builtin_ctzll(n);}
+ 
+const int maxn=105;
+//i_am_noob
+int n,m;
+string str[maxn],str1;
+map<string,int> ma;
+map<int,int> ma1;
+list<int> lst;
+bool visited[maxn]={false};
+ 
+int main(){
+	ios_base::sync_with_stdio(0),cin.tie(0);
+	cin >> n >> m;
+	rep(n) cin >> str[i];
+	rep(n) ma[str[i]]=i;
+	rep(n){
+		reverse(all(str[i]));
+		str1=str[i];
+		reverse(all(str[i]));
+		if(ma.count(str1)) ma1[i]=ma[str1];
+	}
+	rep(n){
+		if(ma1.count(i)){
+			if(ma1[i]==i){
+				lst.pb(i);
+				break;
+			}
+		}
+	}
+	rep(n){
+		if(visited[i]) continue;
+		if(ma1.count(i)){
+			if(ma1[i]!=i){
+				lst.push_front(i);
+				lst.pb(ma1[i]);
+				visited[i]=true;
+				visited[ma1[i]]=true;
+			}
+		}
+	}
+	print1(sz(lst)*m);
+	while(!lst.empty()){
+		cout << str[lst.front()];
+		lst.pop_front();
+	}
+	return 0;
+}
