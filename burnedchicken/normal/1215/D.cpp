9c1fe69b00c0a1aa5062ab83c93572d8ef116fcf
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 21:44:39 +0530

    burnedchicken

---
+++ burnedchicken/normal/1215/D.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<(int(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=(int(a)); i--)
+#define all(a) a.begin(),a.end()
+#define forall(a,type) for(type::iterator it=a.begin(); it!=a.end(); it++)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define pow2(x) (1ll<<(x))
+#define print0(a) cout << (a) << ' '
+#define print1(a) cout << (a) << '\n'
+#define print2(a,b) cout << (a) << ' ',print1(b)
+#define print3(a,b,c) cout << (a) << ' ',print2(b,c)
+#define print4(a,b,c,d) cout << (a) << ' ',print3(b,c,d)
+#define printvec(a) rep(a.size()) print0(a[i])
+#define debug(a) print2(#a,a)
+
+using namespace std;
+
+const ll Mod=1000000007,Mod2=998244353;
+const ll MOD=Mod2;
+
+ll add(ll a, ll b){return (a+b>=MOD?a+b-MOD:a+b);}
+ll sub(ll a, ll b){return (a+MOD-b>=MOD?a-b:a+MOD-b);}
+ll Pow(ll a, ll b){
+	if(!b) return 1;
+	ll tmp=Pow(a*a%MOD,b>>1);
+	return (b&1?tmp*a%MOD:tmp);
+}
+ll inverse(ll n){return Pow(n,MOD-2);}
+ll dgts2(ll n){return (n?dgts2(n>>1)+1:0);}
+ll dgts(ll n, ll k){return (n?dgts(n/k,k)+1:0);}
+ll v2(ll n){return ((n&1)?0:v2(n>>1)+1);}
+
+const int maxn=200005;
+//i_am_noob
+int n,cnt1=0,cnt2=0,dif=0;
+string str;
+
+int main(){
+	ios_base::sync_with_stdio(0),cin.tie(0);
+	cin >> n >> str;
+	rep(n>>1){
+		if(str[i]=='?') cnt1++;
+		else dif+=str[i]-'0';
+	}
+	rep(n>>1){
+		if(str[(n>>1)+i]=='?') cnt2++;
+		else dif-=str[(n>>1)+i]-'0';
+	}
+	if(dif==9*(cnt2-cnt1>>1)) print1("Bicarp");
+	else print1("Monocarp");
+	return 0;
+}
