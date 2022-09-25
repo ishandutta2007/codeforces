5c14e8cff5dcc2eb36550503124dce3964cf112f
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 01:20:04 +0530

    errorgorn

---
+++ errorgorn/normal/1591/D.cpp
@@ -0,0 +1,93 @@
+// Super Idol
+//    
+//  
+//    
+//  105C
+// 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << ": " << x << endl
+
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int n;
+int arr[500005];
+int fen[500005];
+int cnt[500005];
+
+void upd(int i,int j){
+	while (i<500005){
+		fen[i]+=j;
+		i+=i&-i;
+	}
+}
+
+int query(int i){
+	int res=0;
+	while (i){
+		res+=fen[i];
+		i-=i&-i;
+	}
+	return res;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	int TC;
+	cin>>TC;
+	while (TC--){
+		cin>>n;
+		rep(x,0,n) cin>>arr[x];
+		
+		rep(x,1,n+1) fen[x]=cnt[x]=0;
+		
+		rep(x,0,n) cnt[arr[x]]++;
+		
+		bool can=false;
+		rep(x,1,n+1) if (cnt[x]>=2) can=true;
+		
+		if (can){
+			cout<<"YES"<<endl;
+			continue;
+		}
+		
+		ll cnt=0;
+		rep(x,0,n){
+			cnt+=x-query(arr[x]);
+			upd(arr[x],1);
+		}
+		
+		if (cnt%2==0) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	}
+}
