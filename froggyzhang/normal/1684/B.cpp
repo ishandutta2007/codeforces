8baf167b0be7b5b9f281445ac9da5f268f11ab90
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 03:28:54 +0530

    froggyzhang

---
+++ froggyzhang/normal/1684/B.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int a,b,c;
+void Solve(){
+	cin>>a>>b>>c;
+	cout<<a+100000000LL*b<<' '<<b<<' '<<a+100000000LL*b+c<<'\n';
+}
+int main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int T;
+	cin>>T;
+	while(T--)Solve();
+	return 0;
+}
