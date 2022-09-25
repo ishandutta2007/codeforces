07e32fed883a8a2b3cb9ed1ac5d1b2be54dfa10f
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 01:12:00 +0530

    errorgorn

---
+++ errorgorn/normal/1646/E.cpp
@@ -0,0 +1,73 @@
+//
+//
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << ": " << x << endl
+
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+ll n,m;
+
+ll cnt[22];
+
+bool has[1000005];
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	cin>>n>>m;
+	
+	ll curr=0;
+	rep(x,1,22){
+		for (ll y=x;y<=x*m;y+=x){
+			bool ok=true;
+			rep(z,1,x) if (y%z==0 && y/z<=m){
+				ok=false;
+				break;
+			}
+			if (ok) curr++;
+		}
+		cnt[x]=curr;
+	}
+	
+	ll ans=1;
+	rep(x,2,n+1) if (!has[x]){
+		ll num=0;
+		for (ll y=x;y<=n;y*=x){
+			has[y]=true;
+			num++;
+		}
+		ans+=cnt[num];
+	}
+	
+	cout<<ans<<endl;
+}
