1c1e6f8905672f9cdc87b10313d3aa2642ec2e06
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 02:40:45 +0530

    errorgorn

---
+++ errorgorn/normal/1343/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << " is " << x << endl;
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+ll MAX(ll a){return a;}
+ll MIN(ll a){return a;}
+template<typename... Args>
+ll MAX(ll a,Args... args){return max(a,MAX(args...));}
+template<typename... Args>
+ll MIN(ll a,Args... args){return min(a,MIN(args...));}
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int TC;
+	cin>>TC;
+	while (TC--){
+		int n;
+		cin>>n;
+		
+		int curr=1;
+		rep(x,1,30){
+			curr+=1<<x;
+			
+			if (n%curr==0){
+				cout<<n/curr<<endl;
+				goto _end;
+			}
+		}
+		
+		_end:;
+	}
+}
