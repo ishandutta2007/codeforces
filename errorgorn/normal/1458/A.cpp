867071da44946274c689118c8df5cec8f74a7ef0
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 02:05:47 +0530

    errorgorn

---
+++ errorgorn/normal/1458/A.cpp
@@ -0,0 +1,52 @@
+//
+//
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << " is " << x << endl
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int n,m;
+ll arr[200005];
+ll brr[200005];
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	cin>>n>>m;
+	rep(x,0,n) cin>>arr[x];
+	rep(x,0,m) cin>>brr[x];
+	
+	sort(arr,arr+n);
+	
+	rep(x,1,n) arr[x]-=arr[0];
+	
+	ll g=0;
+	rep(x,1,n) g=__gcd(g,arr[x]);
+	//cout<<g<<endl;
+	
+	rep(x,0,m) cout<<__gcd(g,brr[x]+arr[0])<<" ";
+	
+}
