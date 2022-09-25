0a4c4199d98d277a40ecc3777ebc0292b28f85ea
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 21:43:41 +0530

    burnedchicken

---
+++ burnedchicken/normal/1282/C.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+ 
+#define ll long long
+#define ull unsigned long long
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<(int(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=(int(a)); i--)
+#define all(a) a.begin(),a.end()
+#define forall(a,type) for(type::iterator it=a.begin(); it!=a.end(); it++)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pb push_back
+#define mp make_pair
+#define pow2(x) (1ll<<(x))
+#define print0(a) cout << (a) << ' '
+#define print1(a) cout << (a) << '\n'
+#define print2(a,b) cout << (a) << ' ',print1(b)
+#define print3(a,b,c) cout << (a) << ' ',print2(b,c)
+#define print4(a,b,c,d) cout << (a) << ' ',print3(b,c,d)
+#define printvec(a) rep(a.size()) print0(a[i])
+#define debug(a) print2(#a,a)
+ 
+using namespace std;
+ 
+const ll Mod=1000000007,Mod2=998244353;
+const ll MOD=Mod2;
+ 
+ll add(ll a, ll b){return (a+b>=MOD?a+b-MOD:a+b);}
+ll sub(ll a, ll b){return (a+MOD-b>=MOD?a-b:a+MOD-b);}
+ll Pow(ll a, ll b){
+	if(!b) return 1;
+	ll tmp=Pow(a*a%MOD,b>>1);
+	return (b&1?tmp*a%MOD:tmp);
+}
+ll inverse(ll n){return Pow(n,MOD-2);}
+ll dgts2(ll n){return (n?dgts2(n>>1)+1:0);}
+ll dgts(ll n, ll k){return (n?dgts(n/k,k)+1:0);}
+ll v2(ll n){return ((n&1)?0:v2(n>>1)+1);}
+ 
+const int maxn=200005;
+//i_am_noob
+ll m,n,t,a,b,x,y,sum,ans,cnt1,cnt2;
+pll p[maxn];
+bool ok;
+ 
+int main(){
+	ios_base::sync_with_stdio(0),cin.tie(0);
+	cin >> m;
+	while(m--){
+		cin >> n >> t >> a >> b;
+		cnt1=cnt2=0;
+		rep(n){
+			cin >> x;
+			p[i].second=x?b:a;
+			x?cnt2++:cnt1++;
+		}
+		rep(n) cin >> p[i].first;
+		sort(p,p+n);
+		ok=false;
+		sum=0;
+		rep(n){
+			sum+=p[i].second;
+			if(sum>t) break;
+			if(i==n-1){
+				print1(n);
+				ok=true;
+			}
+		}
+		if(ok) continue;
+		sum=ans=0;
+		rep(n){
+			x=p[i].first-1-sum;
+			if(x>=0){
+				y=i+(x<cnt1*a?x/a:cnt1+(x-cnt1*a)/b);
+				ans=max(ans,y);
+			}
+			sum+=p[i].second;
+			p[i].second==a?cnt1--:cnt2--;
+			if(sum>t) break;
+		}
+		print1(ans);
+	}
+	return 0;
+}
