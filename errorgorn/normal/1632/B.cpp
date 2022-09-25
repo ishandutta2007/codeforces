568ae28c9e5028594a40a5e8c113f0128dd85924
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 01:16:16 +0530

    errorgorn

---
+++ errorgorn/normal/1632/B.cpp
@@ -0,0 +1,58 @@
+// Super Idol
+//    
+//  
+//    
+//  105C
+// 
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define int long long
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << ": " << x << endl
+
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	int TC;
+	cin>>TC;
+	while (TC--){
+		int n;
+		cin>>n;
+		
+		int p2=1;
+		while (p2*2<n) p2*=2;
+		
+		rep(x,p2,0) cout<<x<<" ";
+		rep(x,p2,n) cout<<x<<" "; cout<<endl;
+	}
+}
