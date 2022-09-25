2c30baed2d8afa35dd495e84ede31475238c085b
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 21:24:16 +0530

    burnedchicken

---
+++ burnedchicken/normal/1209/E2.cpp
@@ -0,0 +1,87 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+//#define int long long
+#define ull unsigned long long
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<((int)(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=((int)(a)); i--)
+#define all(a) a.begin(),a.end()
+#define pii pair<int,int>
+#define pb push_back
+#define mp make_pair
+#define eps 1e-9
+#define sz(a) ((int)a.size())
+#define pow2(x) (1ll<<(x))
+#define ceiling(a,b) (((a)+(b)-1)/(b))
+#define print0(a) cout << (a) << ' '
+#define print1(a) cout << (a) << '\n'
+#define print2(a,b) cout << (a) << ' ',print1(b)
+#define print3(a,b,c) cout << (a) << ' ',print2(b,c)
+#define print4(a,b,c,d) cout << (a) << ' ',print3(b,c,d)
+#ifdef i_am_noob
+#define debug(a) print2(#a,a)
+#else
+#define debug(a) 
+#endif
+#define ordered_set tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+const int Mod=1000000007,Mod2=998244353;
+const int MOD=Mod2;
+ 
+inline int add(int a, int b){return (a+b>=MOD?a+b-MOD:a+b);}
+inline int sub(int a, int b){return (a+MOD-b>=MOD?a-b:a+MOD-b);}
+int Pow(int a, int b){
+	if(!b) return 1;
+	int tmp=Pow(a*a%MOD,b>>1);
+	return (b&1?tmp*a%MOD:tmp);
+}
+inline int inverse(int n){return Pow(n,MOD-2);}
+ 
+const int maxn=505;
+//i_am_noob
+int t,n,m,a[12][2005],b[2005],maxx[12][1<<12],dp[12][1<<12],x,mask,ans;
+ 
+bool comp(int x, int y){
+	int tmp1=-2e9,tmp2=-2e9;
+	rep(n) tmp1=max(tmp1,a[i][x]);
+	rep(n) tmp2=max(tmp2,a[i][y]);
+	return tmp1>tmp2;
+}
+ 
+signed main(){
+	ios_base::sync_with_stdio(0),cin.tie(0);
+	cin >> t;
+	while(t--){
+		cin >> n >> m;
+		rep(n) rep1(j,m) cin >> a[i][j];
+		rep(m) b[i]=i;
+		sort(b,b+m,comp);
+		m=min(m,n);
+		rep(m) rep1(j,1<<n){
+			maxx[i][j]=-2e9;
+			rep1(k,n){
+				x=0;
+				rep1(l,n) if(j&pow2(l)) x+=a[(k+l)%n][b[i]];
+				maxx[i][j]=max(maxx[i][j],x);
+			}
+		}
+		rep1(j,1<<n) dp[0][j]=maxx[0][j];
+		rep2(i,1,m) rep1(j,1<<n){
+			dp[i][j]=dp[i-1][j];
+			mask=j;
+			while(mask>0){
+				dp[i][j]=max(dp[i][j],maxx[i][mask]+dp[i-1][j-mask]);
+				mask=j&(mask-1);
+			}
+		}
+		print1(dp[m-1][(1<<n)-1]);
+	}
+	return 0;
+}
