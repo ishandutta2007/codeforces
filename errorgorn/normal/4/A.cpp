e32c2a2e6fd6ec5cf88146e78b88b33c0b23c01c
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 02:26:01 +0530

    errorgorn

---
+++ errorgorn/normal/4/A.cpp
@@ -0,0 +1,143 @@
+//
+//
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << " is " << x << endl;
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+ll MAX(ll a){return a;}
+ll MIN(ll a){return a;}
+template<typename... Args>
+ll MAX(ll a,Args... args){return max(a,MAX(args...));}
+template<typename... Args>
+ll MIN(ll a,Args... args){return min(a,MIN(args...));}
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int n;
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	cin>>n;
+	
+	if (n==1) cout<<"NO";
+	if (n==2) cout<<"NO";
+	if (n==3) cout<<"NO";
+	if (n==4) cout<<"YES";
+	if (n==5) cout<<"NO";
+	if (n==6) cout<<"YES";
+	if (n==7) cout<<"NO";
+	if (n==8) cout<<"YES";
+	if (n==9) cout<<"NO";
+	if (n==10) cout<<"YES";
+	if (n==11) cout<<"NO";
+	if (n==12) cout<<"YES";
+	if (n==13) cout<<"NO";
+	if (n==14) cout<<"YES";
+	if (n==15) cout<<"NO";
+	if (n==16) cout<<"YES";
+	if (n==17) cout<<"NO";
+	if (n==18) cout<<"YES";
+	if (n==19) cout<<"NO";
+	if (n==20) cout<<"YES";
+	if (n==21) cout<<"NO";
+	if (n==22) cout<<"YES";
+	if (n==23) cout<<"NO";
+	if (n==24) cout<<"YES";
+	if (n==25) cout<<"NO";
+	if (n==26) cout<<"YES";
+	if (n==27) cout<<"NO";
+	if (n==28) cout<<"YES";
+	if (n==29) cout<<"NO";
+	if (n==30) cout<<"YES";
+	if (n==31) cout<<"NO";
+	if (n==32) cout<<"YES";
+	if (n==33) cout<<"NO";
+	if (n==34) cout<<"YES";
+	if (n==35) cout<<"NO";
+	if (n==36) cout<<"YES";
+	if (n==37) cout<<"NO";
+	if (n==38) cout<<"YES";
+	if (n==39) cout<<"NO";
+	if (n==40) cout<<"YES";
+	if (n==41) cout<<"NO";
+	if (n==42) cout<<"YES";
+	if (n==43) cout<<"NO";
+	if (n==44) cout<<"YES";
+	if (n==45) cout<<"NO";
+	if (n==46) cout<<"YES";
+	if (n==47) cout<<"NO";
+	if (n==48) cout<<"YES";
+	if (n==49) cout<<"NO";
+	if (n==50) cout<<"YES";
+	if (n==51) cout<<"NO";
+	if (n==52) cout<<"YES";
+	if (n==53) cout<<"NO";
+	if (n==54) cout<<"YES";
+	if (n==55) cout<<"NO";
+	if (n==56) cout<<"YES";
+	if (n==57) cout<<"NO";
+	if (n==58) cout<<"YES";
+	if (n==59) cout<<"NO";
+	if (n==60) cout<<"YES";
+	if (n==61) cout<<"NO";
+	if (n==62) cout<<"YES";
+	if (n==63) cout<<"NO";
+	if (n==64) cout<<"YES";
+	if (n==65) cout<<"NO";
+	if (n==66) cout<<"YES";
+	if (n==67) cout<<"NO";
+	if (n==68) cout<<"YES";
+	if (n==69) cout<<"NO";
+	if (n==70) cout<<"YES";
+	if (n==71) cout<<"NO";
+	if (n==72) cout<<"YES";
+	if (n==73) cout<<"NO";
+	if (n==74) cout<<"YES";
+	if (n==75) cout<<"NO";
+	if (n==76) cout<<"YES";
+	if (n==77) cout<<"NO";
+	if (n==78) cout<<"YES";
+	if (n==79) cout<<"NO";
+	if (n==80) cout<<"YES";
+	if (n==81) cout<<"NO";
+	if (n==82) cout<<"YES";
+	if (n==83) cout<<"NO";
+	if (n==84) cout<<"YES";
+	if (n==85) cout<<"NO";
+	if (n==86) cout<<"YES";
+	if (n==87) cout<<"NO";
+	if (n==88) cout<<"YES";
+	if (n==89) cout<<"NO";
+	if (n==90) cout<<"YES";
+	if (n==91) cout<<"NO";
+	if (n==92) cout<<"YES";
+	if (n==93) cout<<"NO";
+	if (n==94) cout<<"YES";
+	if (n==95) cout<<"NO";
+	if (n==96) cout<<"YES";
+	if (n==97) cout<<"NO";
+	if (n==98) cout<<"YES";
+	if (n==99) cout<<"NO";
+	if (n==100) cout<<"YES";
+}
