305014ae9771abe851d6f9111615746e21401028
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 06:17:57 +0530

    izban

---
+++ izban/normal/270/B.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <map> 
+#include <math.h>
+#include <vector>
+#include <algorithm>
+#include <cstdio>
+#include <set>
+#include <queue>
+#include <sstream>
+#include <deque>
+#include <memory.h>
+
+
+using namespace std;
+
+#define ll long long
+#pragma comment(linker, "/STACK:64000000")
+
+const int maxn = 1 << 17;
+const int inf = 1000000007;
+const int mod = 1000000007;
+
+
+int a[maxn];
+
+int main() {
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+    int n;
+	while (cin >> n) {
+		int ans = 0;
+		for (int i = 1; i <= n; i++) {
+			cin >> a[i];
+			if (a[i - 1] > a[i])
+				ans = i - 1;
+		}
+		cout << ans << endl;
+	}
+
+	return 0;
+}
