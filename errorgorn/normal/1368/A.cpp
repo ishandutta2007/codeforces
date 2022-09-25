4c927554d54201e893f57992ec281b687c68d577
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 02:34:53 +0530

    errorgorn

---
+++ errorgorn/normal/1368/A.cpp
@@ -0,0 +1,77 @@
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << " is " << x << endl;
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+ll MAX(ll a){return a;}
+ll MIN(ll a){return a;}
+template<typename... Args>
+ll MAX(ll a,Args... args){return max(a,MAX(args...));}
+template<typename... Args>
+ll MIN(ll a,Args... args){return min(a,MIN(args...));}
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+string s,t;
+
+vector<ii> memo[10005];
+int oa[10005];
+int ob[10005];
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	int TC;
+	cin>>TC;
+	while (TC--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		
+		int ans=0;
+		
+		while (true){
+			if (a>b) swap(a,b);
+			if (b>c) break;
+			
+			a+=b;
+			
+			ans++;
+		}
+		
+		cout<<ans<<endl;
+	}
+}
