cf3e8ce617551f1ab71724a180dd95e6b2f6ad8c
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 07:02:08 +0530

    zemen

---
+++ zemen/normal/1544/A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using ld = long double;
+
+signed main() {
+#ifdef LOCAL
+  assert(freopen("a.in", "r", stdin));
+#endif
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int tn;
+  cin >> tn;
+  for (int i = 0; i < tn; ++i) {
+    string s;
+    cin >> s;
+    cout << *max_element(s.begin(), s.end()) << '\n';
+  }
+}
