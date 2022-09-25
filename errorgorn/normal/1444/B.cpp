7a81d98695e9f04563a4f3d85b1bef16de52d4d5
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 02:10:49 +0530

    errorgorn

---
+++ errorgorn/normal/1444/B.cpp
@@ -0,0 +1,67 @@
+//
+//
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/rope>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace __gnu_cxx;
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << " is " << x << endl
+
+#define rep(x,start,end) for(auto x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+const int MOD=998244353;
+
+int n;
+int arr[300005];
+
+ll qexp(ll b,ll p,int m){
+    ll res=1;
+    while (p){
+        if (p&1) res=(res*b)%m;
+        b=(b*b)%m;
+        p>>=1;
+    }
+    return res;
+}
+
+ll fix(ll i){
+	i%=MOD;
+	if (i<0) i+=MOD;
+	return i;
+}
+
+int main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	cin>>n;
+	rep(x,0,2*n) cin>>arr[x];
+	sort(arr,arr+2*n);
+	
+	ll total=0;
+	rep(x,0,n) total=fix(total-arr[x]);
+	rep(x,n,2*n) total=fix(total+arr[x]);
+	
+	rep(x,1,n+1) total=fix(total*qexp(x,MOD-2,MOD));
+	rep(x,n+1,2*n+1) total=fix(total*x);
+	
+	cout<<total<<endl;
+}
