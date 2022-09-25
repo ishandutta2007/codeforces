74720d19ba803cd526f132d3af1d07f87d6c463c
ishan dutta <ishandutta2007@gmail.com>
2022-09-25 19:50:32 +0530

    burnedchicken

---
+++ burnedchicken/normal/1394/D.cpp
@@ -0,0 +1,166 @@
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("Ofast,unroll-loops")
+
+#define ll long long
+#define int ll
+#define ull unsigned ll
+#define ld long double
+#define rep(a) rep1(i,a)
+#define rep1(i,a) rep2(i,0,a)
+#define rep2(i,b,a) for(int i=(b); i<((int)(a)); i++)
+#define rep3(i,b,a) for(int i=(b); i>=((int)(a)); i--)
+#define chkmin(a,b) (a=min(a,b))
+#define chkmax(a,b) (a=max(a,b))
+#define all(a) a.begin(),a.end()
+#define pii pair<int,int>
+#define pb push_back
+//#define inf 1010000000
+#define inf 4000000000000000000
+#define eps 1e-9
+#define sz(a) ((int)a.size())
+#define pow2(x) (1ll<<(x))
+#define ceiling(a,b) (((a)+(b)-1)/(b))
+#define print0(a) cout << (a) << ' '
+#define ykh mt19937 rng(chrono::steady_clock::now().time_since_epoch().count())
+#ifdef i_am_noob
+#define bug(...) cerr << "#" << __LINE__ << ' ' << #__VA_ARGS__ << "- ", _do(__VA_ARGS__)
+template<typename T> void _do(vector<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void _do(set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void _do(unordered_set<T> x){for(auto i: x) cerr << i << ' ';cerr << "\n";}
+template<typename T> void _do(T && x) {cerr << x << endl;}
+template<typename T, typename ...S> void _do(T && x, S&&...y) {cerr << x << ", "; _do(y...);}
+#else
+#define bug(...) 777771449
+#endif
+template<typename T> void print(T && x) {cout << x << "\n";}
+template<typename T, typename... S> void print(T && x, S&&... y) {cout << x << ' ';print(y...);}
+
+const int Mod=1000000007,Mod2=998244353;
+const int MOD=Mod;
+template <int mod>
+struct Modint{
+    int val;
+    Modint(int _val=0){val=_val%mod;if(val<0) val+=mod;}
+    Modint operator +(const Modint& o) const {
+        Modint res;
+        res.val=val+o.val;
+        if(res.val>=mod) res.val-=mod;
+        return res;
+    }
+    Modint operator +(const int& o) const {return Modint(val+o);}
+    Modint operator -() const {
+        Modint res;
+        res.val=-val;
+        if(res.val<0) res.val+=mod;
+        return res;
+    }
+    Modint operator -(const Modint& o) const {
+        Modint res;
+        res.val=val-o.val;
+        if(res.val<0) res.val+=mod;
+        return res;
+    }
+    Modint operator -(const int& o) const {return Modint(val-o);}
+    Modint operator *(const Modint& o) const {return Modint(val*o.val);}
+    Modint operator *(const int& o) const {return Modint(val*(o%mod));}
+    Modint operator +=(const Modint& o){*this=(*this)+o;return *this;}
+    Modint operator -=(const Modint& o){*this=(*this)-o;return *this;}
+    Modint operator *=(const Modint& o){*this=(*this)*o;return *this;}
+    Modint Pow(int b) const {
+        Modint tmp(val),ret(1);
+        while(b){
+            if(b&1) ret*=tmp;
+            b>>=1;tmp*=tmp;
+        }
+        return ret;
+    }
+    Modint Pow(const Modint& a, int b) const {return a.Pow(b);}
+    inline Modint inv() const {return (*this).Pow(mod-2);}
+    Modint operator /(const Modint& o) const {return *this*o.inv();}
+    Modint operator /(const int& o) const {return *this*Modint(o).inv();}
+    bool operator ==(const Modint& o) const {return val==o.val;}
+};
+template<int mod>
+ostream& operator << (ostream& o, Modint<mod> x){return o << x.val;}
+template<int mod>
+Modint<mod> operator +(const int& x, const Modint<mod>& y){return Modint<mod>(x+y.val);}
+template<int mod>
+Modint<mod> operator -(const int& x, const Modint<mod>& y){return Modint<mod>(x-y.val);}
+template<int mod>
+Modint<mod> operator *(const int& x, const Modint<mod>& y){return Modint<mod>(x%mod*y.val);}
+#define modint Modint<MOD>
+vector<modint> inv,fac,invfac;
+void init_comb(int N){
+    inv.resize(N),fac.resize(N),invfac.resize(N);
+    inv[1]=1,fac[0]=1,invfac[0]=1;
+    rep2(i,2,N) inv[i]=inv[MOD%i]*(MOD-MOD/i);
+    rep2(i,1,N) fac[i]=fac[i-1]*i;
+    rep2(i,1,N) invfac[i]=invfac[i-1]*inv[i];
+}
+inline modint C(int n, int m){return m>n||m<0?modint(0):fac[n]*invfac[m]*invfac[n-m];}
+inline modint H(int n, int m){return C(n+m-1,n);}
+const int maxn=200005,maxm=100005,maxk=7777714;
+
+//i_am_noob
+//#define wiwihorz  
+int n,a[maxn],b[maxn],child[maxn];
+vector<vector<int>> adj,dp;
+
+void dfs(int u, int par){
+    for(auto v: adj[u]) if(v!=par) dfs(v,u);
+    vector<int> c0(child[u],1e13),c1(child[u],1e13);//0: out 1: in
+    for(auto& i: dp[u]) i=1e13;
+    int id=0;
+    for(auto v: adj[u]) if(v!=par){
+        rep(sz(dp[v])) chkmin(c0[id],dp[v][i]+max(i,child[v]-i+1)*a[v]);
+        rep(sz(dp[v])) chkmin(c1[id],dp[v][i]+max(i+1,child[v]-i)*a[v]);
+        if(b[u]>b[v]) c0[id]=1e13;
+        if(b[u]<b[v]) c1[id]=1e13;
+        id++;
+    }
+    int sum=accumulate(all(c1),0ll);
+    dp[u][0]=sum;
+    vector<int> vec;
+    rep(child[u]) vec.pb(c0[i]-c1[i]);
+    sort(all(vec));
+    rep2(i,1,child[u]+1) dp[u][i]=dp[u][i-1]+vec[i-1];
+}
+
+void orzck(){
+    adj.resize(maxn),dp.resize(maxn);
+    cin >> n;
+    rep(n) cin >> a[i];
+    rep(n) cin >> b[i];
+    rep(n-1){
+        int u,v;
+        cin >> u >> v;
+        u--,v--;
+        adj[u].pb(v),adj[v].pb(u);
+    }
+    rep(n) child[i]=sz(adj[i]);
+    rep2(i,1,n) child[i]--;
+    rep(n) dp[i].resize(child[i]+1);
+    dfs(0,-1);
+    int res=inf;
+    rep(child[0]+1) chkmin(res,dp[0][i]+max(i,child[0]-i)*a[0]);
+    print(res);
+}
+
+signed main(){
+    ios_base::sync_with_stdio(0),cin.tie(0);
+    // #ifdef i_am_noob
+    // freopen("input1.txt","r",stdin);
+    // freopen("output1.txt","w",stdout);
+    // freopen("output2.txt","w",stderr);
+    // #endif
+    cout << fixed << setprecision(15);
+    int t;
+    #ifdef wiwihorz
+    cin >> t;
+    #else
+    t=1;
+    #endif
+    while(t--) orzck();
+    return 0;
+}
