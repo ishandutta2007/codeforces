abc52306ad9260aa53a17fd4934363b1b77c1253
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 00:52:43 +0530

    errorgorn

---
+++ errorgorn/normal/1691/B.cpp
@@ -0,0 +1,64 @@
+// Super Idol
+//    
+//  
+//    
+//  105C
+// 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define ll long long
+#define ii pair<ll,ll>
+#define iii pair<ii,ll>
+#define fi first
+#define se second
+#define endl '\n'
+#define debug(x) cout << #x << ": " << x << endl
+
+#define pub push_back
+#define pob pop_back
+#define puf push_front
+#define pof pop_front
+#define lb lower_bound
+#define ub upper_bound
+
+#define rep(x,start,end) for(int x=(start)-((start)>(end));x!=(end)-((start)>(end));((start)<(end)?x++:x--))
+#define all(x) (x).begin(),(x).end()
+#define sz(x) (int)(x).size()
+
+#define indexed_set tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update>
+//change less to less_equal for non distinct pbds, but erase will bug
+
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+
+int n;
+int arr[100005];
+int ans[100005];
+
+signed main(){
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cin.exceptions(ios::badbit | ios::failbit);
+	
+	int TC;
+	cin>>TC;
+	while (TC--){
+		cin>>n;
+		rep(x,1,n+1) cin>>arr[x];
+		
+		map<int,vector<int> > m;
+		rep(x,1,n+1) m[arr[x]].pub(x);
+		
+		bool bad=false;
+		for (auto [a,b]:m){
+			if (sz(b)==1) bad=true;
+			rep(x,0,sz(b)) ans[b[x]]=b[(x+1)%sz(b)];
+		}
+		
+		if (bad) cout<<"-1"<<endl;
+		else rep(x,1,n+1) cout<<ans[x]<<" "; cout<<endl;
+	}
+}
