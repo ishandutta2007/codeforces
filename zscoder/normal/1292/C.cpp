b64353f1cb9675fbc9411c8a098604def6ea66c8
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 09:18:28 +0530

    zscoder

---
+++ zscoder/normal/1292/C.cpp
@@ -0,0 +1,358 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define fbo find_by_order
+#define ook order_of_key
+ 
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef long double ld; 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+vi adj[3333];
+ll dp[3333][3333];
+
+struct Tree
+{
+	struct data
+	{
+		ll w;
+	};
+	
+	struct node
+	{
+		int p; //parent
+		ll w; //modify for different problems
+	};
+	
+	struct edge
+	{
+		int v; data dat;
+	};
+	
+	vector<vector<edge> > adj;
+	int n;
+	
+	Tree(int _n)
+	{
+		adj.resize(_n);
+		n = _n;
+	}
+	
+	vi level;
+	vi depth;
+	vi h;
+	vi euler;
+	vi firstocc;
+	vector<vi> rmqtable;
+	vi subsize;
+	vi par;
+	vi start; vi en;
+	vector<vector<node> > st;
+	
+	void addedge(int u, int v)
+	{
+		edge tmp; tmp.v = v;
+		adj[u].pb(tmp);
+		tmp.v = u;
+		adj[v].pb(tmp);
+	}
+	
+	void reset(int _n)
+	{
+		adj.clear();
+		level.clear();
+		depth.clear();
+		euler.clear();
+		rmqtable.clear();
+		subsize.clear();
+		start.clear();
+		en.clear();
+		st.clear();
+		firstocc.clear();
+		adj.resize(_n);
+		n = _n;
+	}
+	
+	void dfssub(int u, int p)
+	{
+		subsize[u] = 1;
+		for(int i = 0; i < adj[u].size(); i++)
+		{
+			int v = adj[u][i].v;
+			if(v == p) continue;
+			dfssub(v, u);
+			subsize[u] += subsize[v];
+		}
+	}
+	
+	void calcsub()
+	{
+		subsize.resize(n);
+		dfssub(0, -1);
+	}
+	
+	int timer;
+	
+	void dfsstartend(int u, int p)
+	{
+		start[u] = ++timer;
+		if(p == -1) h[u] = 0;
+		else h[u] = h[p] + 1;
+		for(int i = 0; i < adj[u].size(); i++)
+		{
+			int v = adj[u][i].v;
+			if(v == p) continue;
+			dfsstartend(v, u);
+		}
+		en[u] = ++timer;
+	}
+	
+	void calcstartend()
+	{
+		timer = 0;
+		start.resize(n); en.resize(n); h.resize(n);
+		dfsstartend(0, -1);
+	}
+	
+	int eulercnt;
+	
+	void dfseuler(int u, int p)
+	{
+		euler[eulercnt] = u; eulercnt++;
+		if(p == -1) {depth[u] = 0;}
+		else {depth[u] = depth[p] + 1;}
+		firstocc[u] = eulercnt-1;
+		for(int i = 0; i < adj[u].size(); i++)
+		{
+			int v = adj[u][i].v;
+			if(v == p) continue ;
+			dfseuler(v, u);
+			euler[eulercnt] = u; eulercnt++;
+		}
+	}
+	
+	void calceuler()
+	{
+		eulercnt = 0;
+		level.assign(2*n+1, 0);
+		euler.assign(2*n+1, 0);
+		depth.assign(n, 0);
+		firstocc.resize(n);
+		dfseuler(0, -1);
+	}
+
+	void filllevel()
+	{
+		int LG = 0;
+		while((1<<LG) <= n*2) LG++;
+		rmqtable.resize(LG);
+		for(int i = 0; i < LG; i++) rmqtable[i].resize(eulercnt);
+		for(int i = 0; i < eulercnt; i++)
+		{
+			level[i] = depth[euler[i]];
+		}
+		level[eulercnt] = 1000000000;
+		for(int j = 0; j < LG; j++)
+		{
+			for(int i = 0; i < eulercnt; i++)
+			{
+				rmqtable[j][i] = eulercnt;
+				if(i + (1<<j) - 1 < eulercnt)
+				{
+					if(j == 0)
+					{
+						rmqtable[j][i] = i;
+					}
+					else
+					{
+						if(level[rmqtable[j - 1][i]] < level[rmqtable[j-1][i + (1<<(j-1))]])
+						{
+							rmqtable[j][i] = rmqtable[j-1][i];
+						}
+						else
+						{
+							rmqtable[j][i] = rmqtable[j-1][i + (1<<(j-1))];
+						}
+					}
+				}
+			}
+		}
+	}
+
+	int rmq(int l, int r)
+	{
+		int k = 31 - __builtin_clz(r-l);
+		//cout << l << ' ' << r << ' ' << rmqtable[l][k] << ' ' << rmqtable[r - (1<<k) + 1][k] << endl;
+		if(level[rmqtable[k][l]] < level[rmqtable[k][r - (1<<k) + 1]])
+		{
+			return rmqtable[k][l];
+		}
+		else
+		{
+			return rmqtable[k][r - (1<<k) + 1];
+		}
+	}
+
+	int lcaeuler(int u, int v)
+	{
+		if(firstocc[u] > firstocc[v]) swap(u, v);
+		//cerr << firstocc[u] << ' ' << firstocc[v] << ' ' << rmq(firstocc[u], firstocc[v]) << ' ' << euler[rmq(firstocc[u], firstocc[v])] << endl;
+		return euler[rmq(firstocc[u], firstocc[v])];
+	}
+	
+	bool insub(int u, int v) //is u in the subtree of v?
+	{
+		if(start[v] <= start[u] && en[u] <= en[v]) return true;
+		return false;
+	}
+	
+	void dfspar(int u, int p)
+	{
+		//cerr << u << ' ' << p << '\n';
+		st[0][u].p = p;
+		if(p == -1) h[u] = 0;
+		else h[u] = h[p] + 1;
+		for(int i = 0; i < adj[u].size(); i++)
+		{
+			int v = adj[u][i].v;
+			if(v == p) continue;
+			dfspar(v, u);
+		}
+	}
+	
+	int LOG;
+	
+	void calcpar()
+	{
+		h.resize(n);
+		int LG = 0; LOG = 0;
+		while((1<<LG) <= n) {LG++; LOG++;}
+		st.resize(LG);
+		for(int i = 0; i < LG; i++)
+		{
+			st[i].resize(n);
+		}
+		dfspar(0, -1);
+		//cerr << "HER" << ' ' << LG << endl;
+		for(int i = 1; i < LG; i++)
+		{
+			for(int j = 0; j < n; j++)
+			{
+				if(st[i-1][j].p == -1) st[i][j].p = -1;
+				else st[i][j].p = st[i-1][st[i-1][j].p].p;
+			}
+		}
+	}
+	
+	int getpar(int u, ll k)
+	{
+		for(int i = LOG - 1; i >= 0; i--)
+		{
+			if(k&(1<<i))
+			{
+				u = st[i][u].p;
+			}
+		}
+		return u;
+	}
+	
+	int lca(int u, int v)
+	{
+		if(h[u] > h[v]) swap(u, v);
+		for(int i = LOG - 1; i >= 0; i--)
+		{
+			if(st[i][v].p != -1 && h[st[i][v].p] >= h[u])
+			{
+				v = st[i][v].p;
+			}
+		}
+		if(u == v) return u;
+		for(int i = LOG - 1; i >= 0; i--)
+		{
+			if(st[i][v].p != -1 && st[i][v].p != st[i][u].p)
+			{
+				u = st[i][u].p;
+				v = st[i][v].p;
+			}
+		}
+		return st[0][u].p;
+	}
+
+	int distance(int u, int v)
+	{
+		int lc = lca(u, v);
+		return (h[u]+h[v]-2*h[lc]);
+	}
+};
+
+vector<ii> V[3001];
+int lc[3001][3001];
+int par[3333];
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int n; cin>>n;
+	Tree T(n);
+	for(int i=0;i<n-1;i++)
+	{
+		int u,v; cin>>u>>v; u--; v--;
+		adj[u].pb(v); adj[v].pb(u);
+		T.addedge(u,v);
+	}
+	T.calcpar();
+	for(int i=0;i<n;i++)
+	{
+		for(int j=i+1;j<n;j++)
+		{
+			lc[i][j]=T.lca(i,j);
+			int d = T.h[i]+T.h[j]-2*T.h[lc[i][j]];
+			V[d].pb({i,j});
+		}
+	}	
+	par[0]=-1;
+	for(int i=1;i<n;i++)
+	{
+		par[i]=T.st[0][i].p;
+	}
+	ll mx=0;
+	T.calcsub();
+	for(int i=0;i<n;i++)
+	{
+		dp[i][i]=0;
+	}
+	for(int i=1;i<=n;i++)
+	{
+		for(ii x:V[i])
+		{
+			int u=x.fi; int v=x.se; int l=lc[u][v];
+			if(l==u)
+			{
+				int tp = T.getpar(v,T.h[v]-T.h[u]-1);
+				dp[u][v]=max(dp[u][par[v]],dp[tp][v])+T.subsize[v]*(n-T.subsize[tp]);
+			}
+			else if(l==v)
+			{
+				swap(u,v);
+				int tp = T.getpar(v,T.h[v]-T.h[u]-1);
+				dp[u][v]=max(dp[u][par[v]],dp[tp][v])+T.subsize[v]*(n-T.subsize[tp]);
+			}
+			else
+			{
+				dp[u][v]=max(dp[u][par[v]],dp[par[u]][v])+T.subsize[u]*T.subsize[v];
+			}
+			dp[v][u]=dp[u][v];
+			mx=max(mx,dp[u][v]);
+		}
+	}
+	cout<<mx<<'\n';
+}
