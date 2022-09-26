6960356588126d9aa69a4b02bc3bdabb275d31c3
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 09:19:24 +0530

    zscoder

---
+++ zscoder/normal/1268/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define fbo find_by_order
+#define ook order_of_key
+ 
+typedef long long ll;
+typedef pair<int,int> ii;
+typedef vector<int> vi;
+typedef long double ld; 
+typedef tree<ii, null_type, less<ii>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	int n; cin>>n;
+	ll cnt[2] = {0,0};
+	for(int i=0;i<n;i++)
+	{
+		ll x; cin>>x;
+		cnt[i&1]+=(x+1)/2;
+		cnt[(i+1)&1]+=x/2;
+	}
+	cout<<min(cnt[0],cnt[1])<<'\n';
+}
