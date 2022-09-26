51835b1de880fe4530460ae7393d6dec254638b6
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 14:18:19 +0530

    hazyknight

---
+++ hazyknight/normal/1081/A.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <iostream>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <map>
+#include <iomanip>
+#include <assert.h>
+
+using namespace std;
+
+int n;
+
+int main()
+{
+	cin >> n;
+	if (n == 2)
+		cout << 2 << endl;
+	else
+		cout << 1 << endl;
+	return 0;
+}
