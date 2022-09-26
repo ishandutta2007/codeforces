23bf2edbff5a77b3d398f38b3bf650dc0b8ea7a0
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 11:32:51 +0530

    maripium

---
+++ maripium/normal/1556/F.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+template <int MOD_> struct modnum {
+   static constexpr int MOD = MOD_;
+   static_assert(MOD_ > 0, "MOD must be positive");
+ 
+private:
+   using ll = long long;
+ 
+   int v;
+ 
+   static int minv(int a, int m) {
+      a %= m;
+      assert(a);
+      return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
+   }
+ 
+public:
+ 
+   modnum() : v(0) {}
+   modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+   explicit operator int() const { return v; }
+   friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+   friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+ 
+   friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+   friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+ 
+   modnum inv() const {
+      modnum res;
+      res.v = minv(v, MOD);
+      return res;
+   }
+   friend modnum inv(const modnum& m) { return m.inv(); }
+   modnum neg() const {
+      modnum res;
+      res.v = v ? MOD-v : 0;
+      return res;
+   }
+   friend modnum neg(const modnum& m) { return m.neg(); }
+ 
+   modnum operator- () const {
+      return neg();
+   }
+   modnum operator+ () const {
+      return modnum(*this);
+   }
+ 
+   modnum& operator ++ () {
+      v ++;
+      if (v == MOD) v = 0;
+      return *this;
+   }
+   modnum& operator -- () {
+      if (v == 0) v = MOD;
+      v --;
+      return *this;
+   }
+   modnum& operator += (const modnum& o) {
+      v -= MOD-o.v;
+      v = (v < 0) ? v + MOD : v;
+      return *this;
+   }
+   modnum& operator -= (const modnum& o) {
+      v -= o.v;
+      v = (v < 0) ? v + MOD : v;
+      return *this;
+   }
+   modnum& operator *= (const modnum& o) {
+      v = int(ll(v) * ll(o.v) % MOD);
+      return *this;
+   }
+   modnum& operator /= (const modnum& o) {
+      return *this *= o.inv();
+   }
+ 
+   friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+   friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+   friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+   friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+   friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+   friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+};
+ 
+using num = modnum<1000000007>;
+
+int main() {
+   ios_base::sync_with_stdio(false); cin.tie(nullptr);
+   int N; cin >> N;
+   vector<int> A(N); for (int &a : A) cin >> a;
+   vector<vector<num>> win(N, vector<num>(N));
+   for (int i = 0; i < N; ++i) {
+      for (int j = 0; j < N; ++j) win[i][j] = num(A[i]) / num(A[i] + A[j]);
+   }
+   vector<num> dp(1 << N);
+   for (int m = 1; m < 1 << N; ++m) {
+      dp[m] = 1;
+      for (int m0 = m & (m - 1); m0; m0 = (m0 - 1) & m) {
+         int m1 = m ^ m0;
+         num prob = dp[m0];
+         for (int i = 0; i < N; ++i) if (m0 >> i & 1) {
+            for (int j = 0; j < N; ++j) if (m1 >> j & 1) {
+               prob *= win[i][j];
+            }
+         }
+         dp[m] -= prob;
+      } 
+   }
+
+   num ans = 0;
+   for (int m = 1; m < 1 << N; ++m) {
+      num prob = dp[m];
+      for (int i = 0; i < N; ++i) if (m >> i & 1) {
+         for (int j = 0; j < N; ++j) if (!(m >> j & 1)) {
+            prob *= win[i][j];
+         }
+      }
+      ans += prob * __builtin_popcount(m);
+   }
+   cout << ans << '\n';
+   return 0;
+}
