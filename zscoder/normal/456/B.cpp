21c24343b08adc2a542a825c4bf439b6e807f709
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 10:31:02 +0530

    zscoder

---
+++ zscoder/normal/456/B.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define fbo find_by_order
+#define ook order_of_key
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<int> vi;
+typedef long double ld; 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef set<int>::iterator sit;
+typedef map<int,int>::iterator mit;
+typedef vector<int>::iterator vit;
+
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	string s; cin >> s;
+	char c = s[s.length() - 1];
+	char d = '0';
+	if(s.length()>=2) d = s[s.length()-2];
+	int v = (d-'0')*10+(c-'0');
+	if(v%4==0) cout << 4;
+	else cout << 0;
+}
