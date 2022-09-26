f440ab7d049a488ab69c7f082794617e729b019c
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 06:30:30 +0530

    froggyzhang

---
+++ froggyzhang/normal/1197/A.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+using namespace std;
+#define N 100010
+typedef long long ll;
+inline ll read(){
+	ll x=0,f=1;
+	char c=getchar();
+	while(c<'0'||c>'9'){
+		if(c=='-')f=-1;
+		c=getchar();
+	}
+	while(c>='0'&&c<='9'){
+		x=(x<<1)+(x<<3)+c-'0';
+		c=getchar();
+	}
+	return x*f;
+}
+int T,n,a[N];
+int Solve(){
+	n=read();
+	for(int i=1;i<=n;++i){
+		a[i]=read();
+	}
+	sort(a+1,a+n+1);
+	return min(n-2,a[n-1]-1);
+}
+int main(){
+	T=read();
+	while(T--){
+		printf("%d\n",Solve());
+	}
+	return 0;
+}
