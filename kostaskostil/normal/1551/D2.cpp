9f8a7fad8c38b75d1781b8e1b7f501550a593ffc
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 07:50:05 +0530

    kostaskostil

---
+++ kostaskostil/normal/1551/D2.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("O3")
+
+#define pb push_back
+#define fi first
+#define se second
+
+using namespace std;
+
+main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+    int _;
+    cin>>_;
+    while (_--)
+    {
+        int n, m, k;
+        cin>>n>>m>>k;
+        bool s = false;
+        if (n%2 != 0)
+        {
+            swap(n, m);
+            k = n*m/2 - k;
+            s = true;
+        }
+
+        vector<vector<char> > v(n, vector<char>(m, 0));
+        for (int i=0; i < n; i+=2)
+            for (int j=0; j<m; j++)
+                v[i][j] = v[i+1][j] = 'a' + 5 * (i%5) + (j % 5);
+        for (int i = 0; i < n; i+=2)
+            for (int j=0; j+1 < m; j+=2)
+                if (k>=2)
+        {
+            k-=2;
+            swap(v[i][j+1], v[i+1][j]);
+        }
+        if (k==0)
+        {
+            cout<<"YES\n";
+            if (s)
+                swap(n, m);
+            for (int i = 0; i<n; i++, cout<<"\n")
+            for (int j=0; j<m; j++)
+            {
+                if (s) cout<<v[j][i];
+                else cout<<v[i][j];
+            }
+        }
+        else
+            cout<<"NO\n";
+    }
+}
