5c94978c152597b337fc903a0225e71ffbb3e459
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 06:40:14 +0530

    froggyzhang

---
+++ froggyzhang/normal/1425/A.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+
+#ifndef AT_HOME
+#define getchar() IO::myGetchar()
+#define putchar(x) IO::myPutchar(x)
+#endif
+
+namespace IO {
+	static const int IN_BUF = 1 << 23, OUT_BUF = 1 << 23;
+
+	inline char myGetchar() {
+		static char buf[IN_BUF], *ps = buf, *pt = buf;
+		if (ps == pt) {
+			ps = buf, pt = buf + fread(buf, 1, IN_BUF, stdin);
+		}
+		return ps == pt ? EOF : *ps++;
+	}
+
+	template<typename T>
+	inline bool read(T &x) {
+		bool op = 0;
+		char ch = getchar();
+		x = 0;
+		for (; !isdigit(ch) && ch != EOF; ch = getchar()) {
+			op ^= (ch == '-');
+		}
+		if (ch == EOF) {
+			return false;
+		}
+		for (; isdigit(ch); ch = getchar()) {
+			x = x * 10 + (ch ^ '0');
+		}
+		if (op) {
+			x = -x;
+		}
+		return true;
+	}
+
+	inline int readStr(char *s) {
+		int n = 0;
+		char ch = getchar();
+		for (; isspace(ch) && ch != EOF; ch = getchar())
+			;
+		for (; !isspace(ch) && ch != EOF; ch = getchar()) {
+			s[n++] = ch;
+		}
+		s[n] = '\0';
+		return n;
+	}
+
+	inline void myPutchar(char x) {
+		static char pbuf[OUT_BUF], *pp = pbuf;
+		struct _flusher {
+			~_flusher() {
+				fwrite(pbuf, 1, pp - pbuf, stdout);
+			}
+		};
+		static _flusher outputFlusher;
+		if (pp == pbuf + OUT_BUF) {
+			fwrite(pbuf, 1, OUT_BUF, stdout);
+			pp = pbuf;
+		}
+		*pp++ = x;
+	}
+
+	template<typename T>
+	inline void print_(T x) {
+		if (x == 0) {
+			putchar('0');
+			return;
+		}
+		static int num[40];
+		if (x < 0) {
+			putchar('-');
+			x = -x;
+		}
+		for (*num = 0; x; x /= 10) {
+			num[++*num] = x % 10;
+		}
+		while (*num){
+			putchar(num[*num] ^ '0');
+			--*num;
+		}
+	}
+
+	template<typename T>
+	inline void print(T x, char ch = '\n') {
+		print_(x);
+		putchar(ch);
+	}
+
+	inline void printStr_(const char *s, int n = -1) {
+		if (n == -1) {
+			n = strlen(s);
+		}
+		for (int i = 0; i < n; ++i) {
+			putchar(s[i]);
+		}
+	}
+
+	inline void printStr(const char *s, int n = -1, char ch = '\n') {
+		printStr_(s, n);
+		putchar(ch);
+	}
+}
+using namespace IO;
+
+int f[105];
+
+void init() {
+	f[0] = 0;
+	for (int i = 1; i <= 100; ++i) {
+		if (i & 1) {
+			f[i] = i - f[i - 1];
+		} else {
+			f[i] = std::max(i - f[i - 1], i - f[i / 2]);
+		}
+	}
+}
+
+long long n;
+
+long long F(long long n) {
+	if (n <= 100) {
+		return f[n];
+	}
+	if (n & 1) {
+		return n - F(n - 1);
+	}
+	if (n >> 1 & 1) {
+		return n - F(n / 2);
+	} else {
+		return n - F(n - 1);
+	}
+}
+
+void solve() {
+	read(n);
+	print(F(n));
+}
+
+int main() {
+	init();
+	int T = 1;
+	read(T);
+	while (T--) {
+		solve();
+	}
+}
