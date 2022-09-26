a82e4bffae6e4d25ed5bb201d2e6439bf0229f66
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 10:39:11 +0530

    zscoder

---
+++ zscoder/normal/313/B.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define fbo find_by_order
+#define ook order_of_key
+
+typedef long long ll;
+typedef pair<ll,ll> ii;
+typedef vector<int> vi;
+typedef long double ld; 
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef set<int>::iterator sit;
+typedef map<int,int>::iterator mit;
+typedef vector<int>::iterator vit;
+
+int dp[1000001];
+int main()
+{
+	ios_base::sync_with_stdio(0); cin.tie(0);
+	string s; cin >> s;
+	int n = s.length();
+	for(int i = 1; i <= n; i++)
+	{
+		if(s[i] == s[i-1]) dp[i] = 1;
+		dp[i] += dp[i-1];
+	}
+	int m; cin >> m;
+	for(int i = 0; i < m; i++)
+	{
+		int l, r; cin >> l >> r;
+		l--; r--;
+		cout << dp[r] - dp[l] << '\n';
+	}
+}
