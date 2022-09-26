10ace5636ec6e55881b1ef2623b0e1e353516d82
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 11:03:26 +0530

    zscoder

---
+++ zscoder/normal/617/A.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int, int> ii;
+typedef vector<vi> vvi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+const int MOD = 1e9 + 7;
+const ll INF = ll(1e18);
+
+#define pb push_back
+
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//freopen(".txt", "r", stdin);
+	int x;
+	cin >> x;
+	cout << (x+4)/5;
+	return 0;
+}
+
+/*
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//freopen(".txt", "r", stdin);
+	
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//freopen(".txt", "r", stdin);
+	
+	return 0;
+}
+*/
+/*
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//freopen(".txt", "r", stdin);
+	
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//freopen(".txt", "r", stdin);
+	
+	return 0;
+}
+*/
+
+/*
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	//freopen(".txt", "r", stdin);
+	
+	return 0;
+}
+*/
