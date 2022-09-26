3be13e0bb3338c8d029973fb11431830b2b856a5
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 06:34:17 +0530

    froggyzhang

---
+++ froggyzhang/normal/1437/A.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<vector>
+#include<algorithm>
+using namespace std;
+typedef long long ll;
+inline ll read(){
+	ll x=0,f=1;
+	char c=getchar();
+	while(c<'0'||c>'9'){
+		if(c=='-')f=-1;
+		c=getchar();
+	}
+	while(c>='0'&&c<='9'){
+		x=(x<<1)+(x<<3)+c-'0';
+		c=getchar();
+	}
+	return x*f;
+}
+int T,l,r;
+int main(){
+	T=read();
+	while(T--){
+		l=read(),r=read();
+		printf(2*l>=r+1?"YES\n":"NO\n");
+	}
+	return 0;
+}
