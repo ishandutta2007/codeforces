c65e08bf65300688b21f3bcf989892f99b8bf72a
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 05:59:45 +0530

    froggyzhang

---
+++ froggyzhang/normal/1505/A.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+inline ll read(){
+	ll x=0,f=1;
+	char c=getchar();
+	while(c<'0'||c>'9'){
+		if(c=='-')f=-1;
+		c=getchar();
+	}
+	while(c>='0'&&c<='9'){
+		x=(x<<1)+(x<<3)+c-'0';
+		c=getchar();
+	}
+	return x*f;
+}
+string s;
+int main(){
+	while(cin>>s){
+		if(s[0]=='r'){
+			puts("NO");
+			fflush(stdout);
+		}	
+	}
+	return 0;
+}
