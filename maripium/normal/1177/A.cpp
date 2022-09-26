915f921af678b000d68cec700eef32a105d229c6
ishan dutta <ishandutta2007@gmail.com>
2022-09-26 12:20:02 +0530

    maripium

---
+++ maripium/normal/1177/A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+   long long n; cin >> n;
+   long long d = 1, num = 9;
+   while (true) {
+      if (num * d >= n) break;
+      n -= num * d;
+      d++;
+      num *= 10;
+   }
+   long long cur = 1;
+   for (int i = 0; i < d - 1; ++i) cur *= 10;
+   cur += (n - 1) / d;
+   n -= (n - 1) / d * d;   
+   deque<int> dq;
+   while (cur) {
+      dq.push_front(cur % 10);
+      cur /= 10;
+   }
+   cout << dq[n - 1] << '\n';
+}
