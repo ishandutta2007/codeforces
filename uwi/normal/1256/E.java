72dfe921e0b134abd07efbbad9cf17b3be4a53ac
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 18:22:50 +0530

    uwi

---
+++ uwi/normal/1256/E.java
@@ -0,0 +1,227 @@
+//package round598;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class E {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni();
+		int[] a = na(n);
+		long[] ai = new long[n];
+		for(int i = 0;i < n;i++){
+			ai[i] = (long)a[i]<<32|i;
+		}
+		ai = radixSort(ai);
+		
+		long[] dp = new long[n+1];
+		Arrays.fill(dp, Long.MIN_VALUE / 10);
+		dp[0] = 0;
+		long[] dpmax = new long[n+1];
+		Arrays.fill(dpmax, -1);
+		int[] args = new int[n+1];
+		int[] argsmax = new int[n+1];
+		Arrays.fill(args, -1);
+		Arrays.fill(argsmax, -1);
+		dpmax[0] = 0;
+		argsmax[0] = 0;
+		
+		long def = (ai[n-1]>>>32) - (ai[0]>>>32);
+		for(int i = 0;i < n;i++){
+			if(i-2 >= 0){
+				dp[i+1] = dpmax[i-2];
+				args[i+1] = argsmax[i-2];
+			}
+			if(i < n-1){
+				long v = dp[i+1] + (ai[i+1]>>>32) - (ai[i]>>>32);
+				dpmax[i+1] = Math.max(dpmax[i], v);
+				if(v > dpmax[i]){
+					argsmax[i+1] = i+1;
+				}else{
+					argsmax[i+1] = argsmax[i];
+				}
+			}
+		}
+		int clus = 1;
+		int[] cs = new int[n];
+		Arrays.fill(cs, -1);
+		int cur = n;
+		while(cur > 0){
+			for(int i = args[cur];i < cur;i++){
+				cs[(int)ai[i]] = clus;
+			}
+			clus++;
+			cur = args[cur];
+		}
+		
+		out.println(def-dp[n] + " " + (clus-1));
+		for(int v : cs){
+			out.print(v + " ");
+		}
+		out.println();
+//		tr(dp);
+//		tr(dpmax);
+//		tr(args);
+//		tr(argsmax);
+//		tr(def - dp[n]);
+		
+	}
+	
+	
+	public static long[] radixSort(long[] f){ return radixSort(f, f.length); }
+	public static long[] radixSort(long[] f, int n)
+	{
+		long[] to = new long[n];
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]>>>16&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]>>>16&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]>>>32&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]>>>32&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		{
+			int[] b = new int[65537];
+			for(int i = 0;i < n;i++)b[1+(int)(f[i]>>>48&0xffff)]++;
+			for(int i = 1;i <= 65536;i++)b[i]+=b[i-1];
+			for(int i = 0;i < n;i++)to[b[(int)(f[i]>>>48&0xffff)]++] = f[i];
+			long[] d = f; f = to;to = d;
+		}
+		return f;
+	}
+
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new E().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
