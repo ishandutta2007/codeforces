f703f96893ffb2bb83a9f5e97d07c1e7bdd1ee9d
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 19:37:51 +0530

    uwi

---
+++ uwi/normal/940/E.java
@@ -0,0 +1,257 @@
+//package round466;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class E {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni(), K = ni();
+		int[] a = na(n);
+		SegmentTreeRMQ st = new SegmentTreeRMQ(a);
+		long[] dp = new long[n+1];
+		Arrays.fill(dp, Long.MAX_VALUE / 10);
+		dp[0] = 0;
+		long[] cum = new long[n+1];
+		for(int i = 0;i < n;i++)cum[i+1] = cum[i] + a[i];
+		for(int i = 1;i <= n;i++){
+			dp[i] = dp[i-1] + a[i-1];
+			if(i >= K){
+				dp[i] = Math.min(dp[i], dp[i-K] + (cum[i] - cum[i-K] - st.minx(i-K, i)));
+			}
+		}
+		out.println(dp[n]);
+	}
+	
+	public static class SegmentTreeRMQ {
+		public int M, H, N;
+		public int[] st;
+		
+		public SegmentTreeRMQ(int n)
+		{
+			N = n;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new int[M];
+			Arrays.fill(st, 0, M, Integer.MAX_VALUE);
+		}
+		
+		public SegmentTreeRMQ(int[] a)
+		{
+			N = a.length;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new int[M];
+			for(int i = 0;i < N;i++){
+				st[H+i] = a[i];
+			}
+			Arrays.fill(st, H+N, M, Integer.MAX_VALUE);
+			for(int i = H-1;i >= 1;i--)propagate(i);
+		}
+		
+		public void update(int pos, int x)
+		{
+			st[H+pos] = x;
+			for(int i = (H+pos)>>>1;i >= 1;i >>>= 1)propagate(i);
+		}
+		
+		private void propagate(int i)
+		{
+			st[i] = Math.min(st[2*i], st[2*i+1]);
+		}
+		
+		public int minx(int l, int r){
+			int min = Integer.MAX_VALUE;
+			if(l >= r)return min;
+			while(l != 0){
+				int f = l&-l;
+				if(l+f > r)break;
+				int v = st[(H+l)/f];
+				if(v < min)min = v;
+				l += f;
+			}
+			
+			while(l < r){
+				int f = r&-r;
+				int v = st[(H+r)/f-1];
+				if(v < min)min = v;
+				r -= f;
+			}
+			return min;
+		}
+		
+		public int min(int l, int r){ return l >= r ? 0 : min(l, r, 0, H, 1);}
+		
+		private int min(int l, int r, int cl, int cr, int cur)
+		{
+			if(l <= cl && cr <= r){
+				return st[cur];
+			}else{
+				int mid = cl+cr>>>1;
+				int ret = Integer.MAX_VALUE;
+				if(cl < r && l < mid){
+					ret = Math.min(ret, min(l, r, cl, mid, 2*cur));
+				}
+				if(mid < r && l < cr){
+					ret = Math.min(ret, min(l, r, mid, cr, 2*cur+1));
+				}
+				return ret;
+			}
+		}
+		
+		public int firstle(int l, int v) {
+			int cur = H+l;
+			while(true){
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur;
+					}else{
+						return cur-H;
+					}
+				}else{
+					cur++;
+					if((cur&cur-1) == 0)return -1;
+					if((cur&1)==0)cur>>>=1;
+				}
+			}
+		}
+		
+		public int lastle(int l, int v) {
+			int cur = H+l;
+			while(true){
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur+1;
+					}else{
+						return cur-H;
+					}
+				}else{
+					if((cur&cur-1) == 0)return -1;
+					cur--;
+					if((cur&1)==1)cur>>>=1;
+				}
+			}
+		}
+	}
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new E().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
