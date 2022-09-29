ccdb83067ba25fefab9a327939607cd0d06d97ac
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 17:31:50 +0530

    uwi

---
+++ uwi/normal/1428/F.java
@@ -0,0 +1,185 @@
+//package raif;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class F {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni();
+		char[] s = ns(n);
+
+		int[] lens = new int[n];
+		int[] fs = new int[n];
+		int sp = 0;
+		int rep = 0;
+		long S = 0;
+		long ans = 0;
+		for(char c : s){
+			if(c == '0'){
+				while(rep > 0){
+					lens[sp] = rep;
+					fs[sp] = 1;
+					sp++;
+					S += rep;
+					rep--;
+				}
+				assert rep == 0;
+
+				ans += S;
+				if(sp-1 >= 0 && lens[sp-1] == 0){
+					fs[sp-1]++;
+				}else{
+					lens[sp] = 0;
+					fs[sp] = 1;
+					sp++;
+				}
+			}else{
+				rep++;
+				int f = 0;
+				while(sp-1 >= 0 && lens[sp-1] <= rep){
+					f += fs[sp-1];
+					S -= (long)fs[sp-1] * lens[sp-1];
+					sp--;
+				}
+				lens[sp] = rep;
+				fs[sp] = f;
+				sp++;
+				S += (long)fs[sp-1] * lens[sp-1];
+				ans += S + (long)rep*(rep+1)/2;
+			}
+		}
+		out.println(ans);
+	}
+
+	void run() throws Exception
+	{
+//		int n = 500000, m = 99999;
+//		Random gen = new Random();
+//		StringBuilder sb = new StringBuilder();
+//		sb.append(n + " ");
+//		for (int i = 0; i < n; i++) {
+//			sb.append(1);//gen.nextInt(2));
+//		}
+//		INPUT = sb.toString();
+
+
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new F().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
