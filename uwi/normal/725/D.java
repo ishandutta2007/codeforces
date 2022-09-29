48a939d3239b9d1a8488540ac6c460ab11eaf166
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 20:43:53 +0530

    uwi

---
+++ uwi/normal/725/D.java
@@ -0,0 +1,221 @@
+//package canada2016;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+
+public class D {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni();
+		long[][] ts = new long[n-1][];
+		long myt = nl();
+		nl();
+		for(int i = 0;i < n-1;i++){
+			ts[i] = new long[]{nl(), nl(), -1};
+		}
+		Arrays.sort(ts, new Comparator<long[]>() {
+			public int compare(long[] a, long[] b) {
+				return Long.compare(a[1]-a[0], b[1]-b[0]);
+			}
+		});
+		for(int i = 0;i < n-1;i++)ts[i][2] = i;
+		Arrays.sort(ts, new Comparator<long[]>() {
+			public int compare(long[] a, long[] b) {
+				return Long.compare(a[0], b[0]);
+			}
+		});
+		
+		long[] fs = new long[n+3];
+		int[] fn = new int[n+3];
+		
+		int rank = 1;
+		int best = n + 100;
+		
+		if(myt >= ts[n-2][0]){
+			out.println(1);
+			return;
+		}
+		for(int i = n-2;i >= 0;i--){
+			long lower = i > 0 ? ts[i-1][0] : 0;
+			addFenwick(fs, (int)ts[i][2], ts[i][1]-ts[i][0]+1);
+			addFenwick(fn, (int)ts[i][2], 1);
+			rank++;
+			if(myt >= lower){
+				long can = myt - lower;
+				int ind = firstGEFenwick(fs, can+1);
+				int num = sumFenwick(fn, ind-1);
+				best = Math.min(best, rank-num);
+			}
+		}
+		out.println(best);
+	}
+	
+	public static long sumFenwick(long[] ft, int i)
+	{
+		long sum = 0;
+		for(i++;i > 0;i -= i&-i)sum += ft[i];
+		return sum;
+	}
+	
+	static long big = 7000000000000000000L;
+	
+	public static void addFenwick(long[] ft, int i, long v)
+	{
+		if(v == 0)return;
+		int n = ft.length;
+		for(i++;i < n;i += i&-i){
+			ft[i] += v;
+			if(ft[i] > big){
+				ft[i] = big;
+			}
+		}
+	}
+	
+	public static int firstGEFenwick(long[] ft, long v)
+	{
+		int i = 0, n = ft.length;
+		for(int b = Integer.highestOneBit(n);b != 0;b >>= 1){
+			if((i|b) < n && ft[i|b] < v){
+				i |= b;
+				v -= ft[i];
+			}
+		}
+		return i;
+	}
+	
+	public static int sumFenwick(int[] ft, int i)
+	{
+		int sum = 0;
+		for(i++;i > 0;i -= i&-i)sum += ft[i];
+		return sum;
+	}
+	
+	public static void addFenwick(int[] ft, int i, int v)
+	{
+		if(v == 0 || i < 0)return;
+		int n = ft.length;
+		for(i++;i < n;i += i&-i)ft[i] += v;
+	}
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new D().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	private int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
