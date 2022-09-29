0a05117d1b3db31b741deadc5502cb5d5e88331d
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 18:55:13 +0530

    uwi

---
+++ uwi/normal/1110/D.java
@@ -0,0 +1,172 @@
+//package global001;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class D {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni(), m = ni();
+		int[] f = new int[m+1];
+		for(int i = 0;i < n;i++)f[ni()]++;
+		int[][] dp = new int[6][6];
+		for(int i = 0;i < 6;i++)Arrays.fill(dp[i], -9999999);
+		dp[0][0] = 0;
+		for(int i = 0;i <= m;i++){
+			int[][] ndp = new int[6][6];
+			for(int j = 0;j < 6;j++)Arrays.fill(ndp[j], -9999999);
+			for(int j = 0;j < 6;j++){
+				for(int k = 0;k < 6;k++){
+					if(dp[j][k] < 0)continue;
+					for(int l = 0;l <= j && l <= k && l <= f[i];l++){
+						int u = (f[i]-l) % 3;
+						ndp[k-l][u] = Math.max(ndp[k-l][u], (f[i]-l)/3 + l + dp[j][k]);
+						u += 3;
+						if(u <= f[i]){
+							ndp[k-l][u] = Math.max(ndp[k-l][u], (f[i]-l)/3-1 + l + dp[j][k]);
+						}
+					}
+				}
+			}
+			dp = ndp;
+		}
+		long ans = 0;
+		for(int i = 0;i < 6;i++){
+			for(int j = 0;j < 6;j++){
+				ans = Math.max(ans, dp[i][j]);
+			}
+		}
+		out.println(ans);
+	}
+	
+	void run() throws Exception
+	{
+//		int n = 1000000, m = 99999;
+//		Random gen = new Random();
+//		StringBuilder sb = new StringBuilder();
+//		sb.append(n + " ");
+//		sb.append(m + " ");
+//		for (int i = 0; i < n; i++) {
+//			sb.append(gen.nextInt(m)+1 + " ");
+//		}
+//		INPUT = sb.toString();
+
+		
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new D().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
