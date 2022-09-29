3f875c2c99688946a0172ab570d75410ca12c52f
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 18:54:56 +0530

    uwi

---
+++ uwi/normal/1110/F.java
@@ -0,0 +1,469 @@
+//package global001;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.Random;
+
+public class F {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni(), Q = ni();
+		int[] from = new int[n - 1];
+		int[] to = new int[n - 1];
+		int[] ws = new int[n-1];
+		for (int i = 0; i < n-1; i++) {
+			from[i] = i+1;
+			to[i] = ni() - 1;
+			ws[i] = ni();
+		}
+		int[][][] g = packWU(n, from, to, ws);
+//		int[][] pg = packU(n, from, to);
+//		Random gen = new Random();
+//		for(int[] row : pg){
+//			Arrays.sort(shuffle(row, gen));
+//		}
+		int[][] pars = parents(g, 0);
+		int[] par = pars[0], dep = pars[2], pw = pars[4];
+		int[][] qs = new int[Q][];
+		for(int i = 0;i < Q;i++){
+			qs[i] = new int[]{ni()-1, ni()-1, ni()-1, i};
+		}
+		long[] dw = new long[n];
+		for(int i = 1;i < n;i++){
+			dw[i] = dw[par[i]] + pw[i];
+		}
+		long[] ins = Arrays.copyOf(dw, n);
+		for(int i = 0;i < n;i++){
+			if(g[i].length != 1){
+				ins[i] = Long.MAX_VALUE / 10;
+			}
+		}
+		
+		int[] right = new int[n];
+		Arrays.fill(right, n);
+		for(int i = 0;i < n-1;i++){
+			int x = i;
+			while(x != par[i+1]){
+				right[x] = i+1;
+				x = par[x];
+			}
+		}
+		
+		Arrays.sort(qs, new Comparator<int[]>() {
+			public int compare(int[] a, int[] b) {
+				return a[0] - b[0];
+			}
+		});
+		StarrySkyTreeL sst = new StarrySkyTreeL(ins);
+		int p = 0;
+		long[] anss = new long[Q];
+		for(int i = 0;i < n;i++){
+			while(p < Q && qs[p][0] == i){
+				anss[qs[p][3]] = sst.min(qs[p][1], qs[p][2]+1) + dw[qs[p][0]];
+				p++;
+			}
+			// i -> i+1
+			if(i < n-1){
+				int x = i;
+				while(x != par[i+1]){
+					sst.add(x, right[x], 2L*pw[x]);
+					x = par[x];
+				}
+				sst.add(i+1, right[i+1], -2L*pw[i+1]);
+			}
+		}
+		for(long v : anss){
+			out.println(v);
+		}
+	}
+	
+	public static int[] shuffle(int[] a, Random gen){ for(int i = 0, n = a.length;i < n;i++){ int ind = gen.nextInt(n-i)+i; int d = a[i]; a[i] = a[ind]; a[ind] = d; } return a; }
+
+	
+	static int[][] packU(int n, int[] from, int[] to) {
+		int[][] g = new int[n][];
+		int[] p = new int[n];
+		for (int f : from)
+			p[f]++;
+		for (int t : to)
+			p[t]++;
+		for (int i = 0; i < n; i++)
+			g[i] = new int[p[i]];
+		for (int i = 0; i < from.length; i++) {
+			g[from[i]][--p[from[i]]] = to[i];
+			g[to[i]][--p[to[i]]] = from[i];
+		}
+		return g;
+	}
+	
+	public static class StarrySkyTreeL {
+		public int M, H, N;
+		public long[] st;
+		public long[] plus;
+		public long I = Long.MAX_VALUE/4; // I+plus<long
+		
+		public StarrySkyTreeL(int n)
+		{
+			N = n;
+			M = Integer.highestOneBit(Math.max(n-1, 1))<<2;
+			H = M>>>1;
+			st = new long[M];
+			plus = new long[H];
+		}
+		
+		public StarrySkyTreeL(long[] a)
+		{
+			N = a.length;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new long[M];
+			for(int i = 0;i < N;i++){
+				st[H+i] = a[i];
+			}
+			plus = new long[H];
+			Arrays.fill(st, H+N, M, I);
+			for(int i = H-1;i >= 1;i--)propagate(i);
+		}
+		
+		private void propagate(int i)
+		{
+			st[i] = Math.min(st[2*i], st[2*i+1]) + plus[i];
+		}
+		
+		public void add(int l, int r, long v){ if(l < r)add(l, r, v, 0, H, 1); }
+		
+		private void add(int l, int r, long v, int cl, int cr, int cur)
+		{
+			if(l <= cl && cr <= r){
+				if(cur >= H){
+					st[cur] += v;
+				}else{
+					plus[cur] += v;
+					propagate(cur);
+				}
+			}else{
+				int mid = cl+cr>>>1;
+				if(cl < r && l < mid){
+					add(l, r, v, cl, mid, 2*cur);
+				}
+				if(mid < r && l < cr){
+					add(l, r, v, mid, cr, 2*cur+1);
+				}
+				propagate(cur);
+			}
+		}
+		
+		public long min(int l, int r){ return l >= r ? I : min(l, r, 0, H, 1);}
+		
+		private long min(int l, int r, int cl, int cr, int cur)
+		{
+			if(l <= cl && cr <= r){
+				return st[cur];
+			}else{
+				int mid = cl+cr>>>1;
+				long ret = I;
+				if(cl < r && l < mid){
+					ret = Math.min(ret, min(l, r, cl, mid, 2*cur));
+				}
+				if(mid < r && l < cr){
+					ret = Math.min(ret, min(l, r, mid, cr, 2*cur+1));
+				}
+				return ret + plus[cur];
+			}
+		}
+		
+		public void fall(int i)
+		{
+			if(i < H){
+				if(2*i < H){
+					plus[2*i] += plus[i];
+					plus[2*i+1] += plus[i];
+				}
+				st[2*i] += plus[i];
+				st[2*i+1] += plus[i];
+				plus[i] = 0;
+			}
+		}
+		
+		public int firstle(int l, long v) {
+			int cur = H+l;
+			for(int i = 1, j = Integer.numberOfTrailingZeros(H)-1;i <= cur;j--){
+				fall(i);
+				i = i*2|cur>>>j&1;
+			}
+			while(true){
+				fall(cur);
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur;
+					}else{
+						return cur-H;
+					}
+				}else{
+					cur++;
+					if((cur&cur-1) == 0)return -1;
+					cur = cur>>>Integer.numberOfTrailingZeros(cur);
+				}
+			}
+		}
+		
+		public int lastle(int l, long v) {
+			int cur = H+l;
+			for(int i = 1, j = Integer.numberOfTrailingZeros(H)-1;i <= cur;j--){
+				fall(i);
+				i = i*2|cur>>>j&1;
+			}
+			while(true){
+				fall(cur);
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur+1;
+					}else{
+						return cur-H;
+					}
+				}else{
+					if((cur&cur-1) == 0)return -1;
+					cur = cur>>>Integer.numberOfTrailingZeros(cur);
+					cur--;
+				}
+			}
+		}
+		
+		public void addx(int l, int r, long v){
+			if(l >= r)return;
+			while(l != 0){
+				int f = l&-l;
+				if(l+f > r)break;
+				if(f == 1){
+					st[(H+l)/f] += v;
+				}else{
+					plus[(H+l)/f] += v;
+				}
+				l += f;
+			}
+			
+			while(l < r){
+				int f = r&-r;
+				if(f == 1){
+					st[(H+r)/f-1] += v;
+				}else{
+					plus[(H+r)/f-1] += v;
+				}
+				r -= f;
+			}
+		}
+		
+		public long minx(int l, int r){
+			long lmin = I;
+			if(l >= r)return lmin;
+			if(l != 0){
+				for(int d = 0;H>>>d > 0;d++){
+					if(d > 0){
+						int id = (H+l-1>>>d);
+						lmin += plus[id];
+					}
+					if(l<<~d<0 && l+(1<<d) <= r){
+						long v = st[H+l>>>d];
+						if(v < lmin)lmin = v;
+						l += 1<<d;
+					}
+				}
+			}
+			long rmin = I;
+			for(int d = 0;H>>>d > 0;d++){
+				if(d > 0 && r < H)rmin += plus[H+r>>>d];
+				if(r<<~d<0 && l < r){
+					long v = st[(H+r>>>d)-1];
+					if(v < rmin)rmin = v;
+					r -= 1<<d;
+				}
+			}
+			long min = Math.min(lmin, rmin);
+			return min;
+		}
+		
+		public long[] toArray() { return toArray(1, 0, H, new long[H]); }
+		
+		private long[] toArray(int cur, int l, int r, long[] ret)
+		{
+			if(r-l == 1){
+				ret[cur-H] = st[cur];
+			}else{
+				toArray(2*cur, l, l+r>>>1, ret);
+				toArray(2*cur+1, l+r>>>1, r, ret);
+				for(int i = l;i < r;i++)ret[i] += plus[cur];
+			}
+			return ret;
+		}
+	}
+
+	
+	public static int[][] parents(int[][][] g, int root) {
+		int n = g.length;
+		int[] par = new int[n];
+		Arrays.fill(par, -1);
+		int[] dw = new int[n];
+		int[] pw = new int[n];
+		int[] dep = new int[n];
+
+		int[] q = new int[n];
+		q[0] = root;
+		for (int p = 0, r = 1; p < r; p++) {
+			int cur = q[p];
+			for (int[] nex : g[cur]) {
+				if (par[cur] != nex[0]) {
+					q[r++] = nex[0];
+					par[nex[0]] = cur;
+					dep[nex[0]] = dep[cur] + 1;
+					dw[nex[0]] = dw[cur] + nex[1];
+					pw[nex[0]] = nex[1];
+				}
+			}
+		}
+		return new int[][] { par, q, dep, dw, pw };
+	}
+
+	
+	public static int[][][] packWU(int n, int[] from, int[] to, int[] w) {
+		int[][][] g = new int[n][][];
+		int[] p = new int[n];
+		for (int f : from)
+			p[f]++;
+		for (int t : to)
+			p[t]++;
+		for (int i = 0; i < n; i++)
+			g[i] = new int[p[i]][2];
+		for (int i = 0; i < from.length; i++) {
+			--p[from[i]];
+			g[from[i]][p[from[i]]][0] = to[i];
+			g[from[i]][p[from[i]]][1] = w[i];
+			--p[to[i]];
+			g[to[i]][p[to[i]]][0] = from[i];
+			g[to[i]][p[to[i]]][1] = w[i];
+		}
+		return g;
+	}
+
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new F().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
