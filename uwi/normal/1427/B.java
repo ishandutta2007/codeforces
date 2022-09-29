bb16720f755a08137aa01ab7c07e2b4ae6095360
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 17:32:52 +0530

    uwi

---
+++ uwi/normal/1427/B.java
@@ -0,0 +1,186 @@
+//package global11;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class B {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		for(int T = ni();T > 0;T--)go();
+	}
+
+	void go()
+	{
+		int n = ni(), K = ni();
+		char[] s = ns(n);
+		int[][] uc = uniqcount(s);
+		if(uc.length == 1 && uc[0][0] == 'L'){
+			out.println(K == 0 ? 0 : (K-1)*2 + 1);
+			return;
+		}
+
+		int l = 0;
+		for(char c : s){
+			if(c == 'L')l++;
+		}
+		K = Math.min(K, l);
+
+		int[] three = new int[n];
+		int p = 0;
+		for(int i = 1;i < uc.length-1;i++){
+			if(uc[i][0] == 'L'){
+				three[p++] = uc[i][1];
+			}
+		}
+		Arrays.sort(three, 0, p);
+		int ans = 0;
+		for(int[] u : uc){
+			if(u[0] == 'W'){
+				ans += (u[1]-1)*2 + 1;
+			}
+		}
+
+		ans += 2*K;
+		for(int i = 0;i < p;i++){
+			int use = three[i];
+			if(K >= use){
+				K -= use;
+				ans++;
+			}
+		}
+		out.println(ans);
+	}
+
+	public static int[][] uniqcount(char[] a)
+	{
+		int n = a.length;
+		int p = 0;
+		int[][] b = new int[n][];
+		for(int i = 0;i < n;i++){
+			if(i == 0 || a[i] != a[i-1])b[p++] = new int[]{a[i], 0};
+			b[p-1][1]++;
+		}
+		return Arrays.copyOf(b, p);
+	}
+
+
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new B().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
