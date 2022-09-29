7559f727c5b61e868e2becfcacfc397aab735330
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 17:09:27 +0530

    uwi

---
+++ uwi/normal/985/G.java
@@ -0,0 +1,553 @@
+//package ecr44;
+
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+import java.util.Queue;
+
+public class G {
+	InputStream is;
+	FastWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni(), m = ni();
+		long A = nl(), B = nl(), C = nl();
+		int[] from = new int[m];
+		int[] to = new int[m];
+		for(int i = 0;i < m;i++){
+		    from[i] = ni();
+		    to[i] = ni();
+		    if(from[i] > to[i]){
+		    	int d = from[i]; from[i] = to[i]; to[i] = d;
+		    }
+		}
+		int[][] g = packU(n, from, to);
+
+		// 0
+		long ans = 0;
+		for(int i = 0;i < n;i++){
+			ans += (long)(n-i-1)*(n-i-2)/2*i*A;
+			ans += (long)(n-i-1)*i*i*B;
+			ans += (long)i*(i-1)/2*i*C;
+		}
+
+		// 1
+		for(int i = 0;i < m;i++){
+			ans -= (long)(n-1-to[i]) * (A*from[i]+B*to[i]) + S(to[i]+1, n) * C;
+			ans -= (long)(to[i]-from[i]-1) * (A*from[i]+C*to[i]) + S(from[i]+1, to[i]) * B;
+			ans -= (long)(from[i]) * (B*from[i]+C*to[i]) + S(0, from[i]) * A;
+		}
+
+		// 2
+		for(int i = 0;i < n;i++){
+			Arrays.sort(g[i]);
+			long[] cum = new long[g[i].length+1];
+			for(int j = 0;j < g[i].length;j++){
+				cum[j+1] = cum[j] + g[i][j];
+			}
+			long larger = 0;
+			int nlarger = 0;
+			long smaller = 0;
+			int nsmaller = 0;
+			for(int e : g[i]){
+				if(e > i){
+					larger += e;
+					nlarger++;
+				}else{
+					smaller += e;
+					nsmaller++;
+				}
+			}
+			for(int j = 0;j < g[i].length;j++){
+				int tar = g[i][j];
+				if(tar < i){
+					ans += (long)nlarger * (A*tar + B*i) + larger * C;
+					ans += (long)(nsmaller - j - 1) * (A*tar + C*i) + (smaller - cum[j+1]) * B;
+				}else{
+					// B
+					ans += (long)(g[i].length-j-1) * (A*i + B*tar) + (cum[g[i].length] - cum[j+1]) * C;
+				}
+			}
+		}
+
+		// 3
+		{
+			int[] temp = new int[n];
+			int[][] dg = new int[n][];
+			for(int i = 0;i < n;i++){
+				int od = 0;
+				for(int e : g[i]){
+					if(g[i].length < g[e].length || g[i].length == g[e].length && i < e){
+						temp[od++] = e;
+					}
+				}
+				dg[i] = Arrays.copyOf(temp, od);
+				Arrays.sort(dg[i]);
+			}
+			int[] lt = new int[3];
+			for(int i = 0;i < n;i++){
+				for(int e : dg[i]){
+					for(int p = 0, q = 0;p < dg[i].length && q < dg[e].length;){
+						if(dg[i][p] < dg[e][q]){
+							p++;
+						}else if(dg[i][p] > dg[e][q]){
+							q++;
+						}else{
+							// i,e,dg[i][p]
+							lt[0] = i; lt[1] = e; lt[2] = dg[i][p];
+							Arrays.sort(lt);
+							ans -= lt[0]*A + lt[1]*B + lt[2]*C;
+							p++; q++;
+						}
+					}
+				}
+			}
+		}
+		out.println(Long.toUnsignedString(ans));
+	}
+
+	long S(long l, long r)
+	{
+		return (r-1)*r/2 - (l-1)*l/2;
+	}
+
+	public static int[][] packU(int n, int[] from, int[] to) {
+		return packU(n, from, to, from.length);
+	}
+
+	public static int[][] packU(int n, int[] from, int[] to, int sup) {
+		int[][] g = new int[n][];
+		int[] p = new int[n];
+		for (int i = 0; i < sup; i++) p[from[i]]++;
+		for (int i = 0; i < sup; i++) p[to[i]]++;
+		for (int i = 0; i < n; i++) g[i] = new int[p[i]];
+		for (int i = 0; i < sup; i++) {
+			g[from[i]][--p[from[i]]] = to[i];
+			g[to[i]][--p[to[i]]] = from[i];
+		}
+		return g;
+	}
+
+
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new FastWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new G().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+
+	private long[] nal(int n)
+	{
+		long[] a = new long[n];
+		for(int i = 0;i < n;i++)a[i] = nl();
+		return a;
+	}
+
+	private char[][] nm(int n, int m) {
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+
+	private int[][] nmi(int n, int m) {
+		int[][] map = new int[n][];
+		for(int i = 0;i < n;i++)map[i] = na(m);
+		return map;
+	}
+
+	private int ni() { return (int)nl(); }
+
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+
+	public static class FastWriter
+	{
+		private static final int BUF_SIZE = 1<<13;
+		private final byte[] buf = new byte[BUF_SIZE];
+		private final OutputStream out;
+		private int ptr = 0;
+
+		private FastWriter(){out = null;}
+
+		public FastWriter(OutputStream os)
+		{
+			this.out = os;
+		}
+
+		public FastWriter(String path)
+		{
+			try {
+				this.out = new FileOutputStream(path);
+			} catch (FileNotFoundException e) {
+				throw new RuntimeException("FastWriter");
+			}
+		}
+
+		public FastWriter write(byte b)
+		{
+			buf[ptr++] = b;
+			if(ptr == BUF_SIZE)innerflush();
+			return this;
+		}
+
+		public FastWriter write(char c)
+		{
+			return write((byte)c);
+		}
+
+		public FastWriter write(char[] s)
+		{
+			for(char c : s){
+				buf[ptr++] = (byte)c;
+				if(ptr == BUF_SIZE)innerflush();
+			}
+			return this;
+		}
+
+		public FastWriter write(String s)
+		{
+			s.chars().forEach(c -> {
+				buf[ptr++] = (byte)c;
+				if(ptr == BUF_SIZE)innerflush();
+			});
+			return this;
+		}
+
+		private static int countDigits(int l) {
+			if (l >= 1000000000) return 10;
+			if (l >= 100000000) return 9;
+			if (l >= 10000000) return 8;
+			if (l >= 1000000) return 7;
+			if (l >= 100000) return 6;
+			if (l >= 10000) return 5;
+			if (l >= 1000) return 4;
+			if (l >= 100) return 3;
+			if (l >= 10) return 2;
+			return 1;
+		}
+
+		public FastWriter write(int x)
+		{
+			if(x == Integer.MIN_VALUE){
+				return write((long)x);
+			}
+			if(ptr + 12 >= BUF_SIZE)innerflush();
+			if(x < 0){
+				write((byte)'-');
+				x = -x;
+			}
+			int d = countDigits(x);
+			for(int i = ptr + d - 1;i >= ptr;i--){
+				buf[i] = (byte)('0'+x%10);
+				x /= 10;
+			}
+			ptr += d;
+			return this;
+		}
+
+		private static int countDigits(long l) {
+			if (l >= 1000000000000000000L) return 19;
+			if (l >= 100000000000000000L) return 18;
+			if (l >= 10000000000000000L) return 17;
+			if (l >= 1000000000000000L) return 16;
+			if (l >= 100000000000000L) return 15;
+			if (l >= 10000000000000L) return 14;
+			if (l >= 1000000000000L) return 13;
+			if (l >= 100000000000L) return 12;
+			if (l >= 10000000000L) return 11;
+			if (l >= 1000000000L) return 10;
+			if (l >= 100000000L) return 9;
+			if (l >= 10000000L) return 8;
+			if (l >= 1000000L) return 7;
+			if (l >= 100000L) return 6;
+			if (l >= 10000L) return 5;
+			if (l >= 1000L) return 4;
+			if (l >= 100L) return 3;
+			if (l >= 10L) return 2;
+			return 1;
+		}
+
+		public FastWriter write(long x)
+		{
+			if(x == Long.MIN_VALUE){
+				return write("" + x);
+			}
+			if(ptr + 21 >= BUF_SIZE)innerflush();
+			if(x < 0){
+				write((byte)'-');
+				x = -x;
+			}
+			int d = countDigits(x);
+			for(int i = ptr + d - 1;i >= ptr;i--){
+				buf[i] = (byte)('0'+x%10);
+				x /= 10;
+			}
+			ptr += d;
+			return this;
+		}
+
+		public FastWriter write(double x, int precision)
+		{
+			if(x < 0){
+				write('-');
+				x = -x;
+			}
+			x += Math.pow(10, -precision)/2;
+			//		if(x < 0){ x = 0; }
+			write((long)x).write(".");
+			x -= (long)x;
+			for(int i = 0;i < precision;i++){
+				x *= 10;
+				write((char)('0'+(int)x));
+				x -= (int)x;
+			}
+			return this;
+		}
+
+		public FastWriter writeln(char c){
+			return write(c).writeln();
+		}
+
+		public FastWriter writeln(int x){
+			return write(x).writeln();
+		}
+
+		public FastWriter writeln(long x){
+			return write(x).writeln();
+		}
+
+		public FastWriter writeln(double x, int precision){
+			return write(x, precision).writeln();
+		}
+
+		public FastWriter write(int... xs)
+		{
+			boolean first = true;
+			for(int x : xs) {
+				if (!first) write(' ');
+				first = false;
+				write(x);
+			}
+			return this;
+		}
+
+		public FastWriter write(long... xs)
+		{
+			boolean first = true;
+			for(long x : xs) {
+				if (!first) write(' ');
+				first = false;
+				write(x);
+			}
+			return this;
+		}
+
+		public FastWriter writeln()
+		{
+			return write((byte)'\n');
+		}
+
+		public FastWriter writeln(int... xs)
+		{
+			return write(xs).writeln();
+		}
+
+		public FastWriter writeln(long... xs)
+		{
+			return write(xs).writeln();
+		}
+
+		public FastWriter writeln(char[] line)
+		{
+			return write(line).writeln();
+		}
+
+		public FastWriter writeln(char[]... map)
+		{
+			for(char[] line : map)write(line).writeln();
+			return this;
+		}
+
+		public FastWriter writeln(String s)
+		{
+			return write(s).writeln();
+		}
+
+		private void innerflush()
+		{
+			try {
+				out.write(buf, 0, ptr);
+				ptr = 0;
+			} catch (IOException e) {
+				throw new RuntimeException("innerflush");
+			}
+		}
+
+		public void flush()
+		{
+			innerflush();
+			try {
+				out.flush();
+			} catch (IOException e) {
+				throw new RuntimeException("flush");
+			}
+		}
+
+		public FastWriter print(byte b) { return write(b); }
+		public FastWriter print(char c) { return write(c); }
+		public FastWriter print(char[] s) { return write(s); }
+		public FastWriter print(String s) { return write(s); }
+		public FastWriter print(int x) { return write(x); }
+		public FastWriter print(long x) { return write(x); }
+		public FastWriter print(double x, int precision) { return write(x, precision); }
+		public FastWriter println(char c){ return writeln(c); }
+		public FastWriter println(int x){ return writeln(x); }
+		public FastWriter println(long x){ return writeln(x); }
+		public FastWriter println(double x, int precision){ return writeln(x, precision); }
+		public FastWriter print(int... xs) { return write(xs); }
+		public FastWriter print(long... xs) { return write(xs); }
+		public FastWriter println(int... xs) { return writeln(xs); }
+		public FastWriter println(long... xs) { return writeln(xs); }
+		public FastWriter println(char[] line) { return writeln(line); }
+		public FastWriter println(char[]... map) { return writeln(map); }
+		public FastWriter println(String s) { return writeln(s); }
+		public FastWriter println() { return writeln(); }
+	}
+
+	public void trnz(int... o)
+	{
+		for(int i = 0;i < o.length;i++)if(o[i] != 0)System.out.print(i+":"+o[i]+" ");
+		System.out.println();
+	}
+
+	// print ids which are 1
+	public void trt(long... o)
+	{
+		Queue<Integer> stands = new ArrayDeque<>();
+		for(int i = 0;i < o.length;i++){
+			for(long x = o[i];x != 0;x &= x-1)stands.add(i<<6|Long.numberOfTrailingZeros(x));
+		}
+		System.out.println(stands);
+	}
+
+	public void tf(boolean... r)
+	{
+		for(boolean x : r)System.out.print(x?'#':'.');
+		System.out.println();
+	}
+
+	public void tf(boolean[]... b)
+	{
+		for(boolean[] r : b) {
+			for(boolean x : r)System.out.print(x?'#':'.');
+			System.out.println();
+		}
+		System.out.println();
+	}
+
+	public void tf(long[]... b)
+	{
+		if(INPUT.length() != 0) {
+			for (long[] r : b) {
+				for (long x : r) {
+					for (int i = 0; i < 64; i++) {
+						System.out.print(x << ~i < 0 ? '#' : '.');
+					}
+				}
+				System.out.println();
+			}
+			System.out.println();
+		}
+	}
+
+	public void tf(long... b)
+	{
+		if(INPUT.length() != 0) {
+			for (long x : b) {
+				for (int i = 0; i < 64; i++) {
+					System.out.print(x << ~i < 0 ? '#' : '.');
+				}
+			}
+			System.out.println();
+		}
+	}
+
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
