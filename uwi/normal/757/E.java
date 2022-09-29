e1f9c59ddc496cb7087f9740f2aafa24231a7cd5
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 20:35:52 +0530

    uwi

---
+++ uwi/normal/757/E.java
@@ -0,0 +1,222 @@
+//package round391;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class E2 {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	public static int[][] enumFIF(int n, int mod) {
+		int[] f = new int[n + 1];
+		int[] invf = new int[n + 1];
+		f[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			f[i] = (int) ((long) f[i - 1] * i % mod);
+		}
+		long a = f[n];
+		long b = mod;
+		long p = 1, q = 0;
+		while (b > 0) {
+			long c = a / b;
+			long d;
+			d = a;
+			a = b;
+			b = d % b;
+			d = p;
+			p = q;
+			q = d - c * q;
+		}
+		invf[n] = (int) (p < 0 ? p + mod : p);
+		for (int i = n - 1; i >= 0; i--) {
+			invf[i] = (int) ((long) invf[i + 1] * (i + 1) % mod);
+		}
+		return new int[][] { f, invf };
+	}
+
+	
+	public static long C(int n, int r, int mod, int[][] fif) {
+		if (n < 0 || r < 0 || r > n)
+			return 0;
+		return (long) fif[0][n] * fif[1][r] % mod * fif[1][n - r] % mod;
+	}
+
+	
+	void solve()
+	{
+		int[] lpf = enumLowestPrimeFactors(1000005);
+		int mod = 1000000007;
+		int[][] fif = enumFIF(1100005, mod);
+		for(int Q = ni();Q > 0;Q--){
+			int r = ni(), n = ni();
+			int pre = -1;
+			long ret = 1;
+			int e = 0;
+			for(int j = n;j != 1;j /= lpf[j]){
+				if(pre != lpf[j]){
+					if(pre != -1){
+						ret = ret * (C(r+e-1, e, mod, fif) + C(r+e-1, r, mod, fif)*2L)%mod;
+					}
+					pre = lpf[j];
+					e = 1;
+				}else{
+					e++;
+				}
+			}
+			if(e > 0){
+				ret = ret * (C(r+e-1, e, mod, fif) + C(r+e-1, r, mod, fif)*2L)%mod;
+			}
+			assert ret >= 0;
+			
+			out.println(ret);
+		}
+	}
+	
+	public static int[] enumLowestPrimeFactors(int n) {
+		int tot = 0;
+		int[] lpf = new int[n + 1];
+		int u = n + 32;
+		double lu = Math.log(u);
+		int[] primes = new int[(int) (u / lu + u / lu / lu * 1.5)];
+		for (int i = 2; i <= n; i++)
+			lpf[i] = i;
+		for (int p = 2; p <= n; p++) {
+			if (lpf[p] == p)
+				primes[tot++] = p;
+			int tmp;
+			for (int i = 0; i < tot && primes[i] <= lpf[p] && (tmp = primes[i] * p) <= n; i++) {
+				lpf[tmp] = primes[i];
+			}
+		}
+		return lpf;
+	}
+
+	
+	void run() throws Exception
+	{
+//		int n = 1000000, m = 99999;
+//		Random gen = new Random();
+//		StringBuilder sb = new StringBuilder();
+//		sb.append(n + " ");
+//		for (int i = 0; i < n; i++) {
+//			sb.append(gen.nextInt(1000001) + " ");
+//			sb.append(2*3*5*7*11*13*17 + " ");
+//		}
+//		INPUT = sb.toString();
+
+		
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new E2().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	private int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
