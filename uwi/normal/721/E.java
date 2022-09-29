7a544dbbb2bc1ae3d2dead4ed384e94254c28b6a
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 20:49:18 +0530

    uwi

---
+++ uwi/normal/721/E.java
@@ -0,0 +1,271 @@
+//package round374;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class E2 {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int L = ni(), n = ni(), P = ni(), t = ni();
+		long[][] rs = new long[n][];
+		for(int i = 0;i < n;i++){
+			rs[i] = new long[]{ni(), ni()};
+		}
+		long[] reachs = new long[n+1];
+		SegmentTreeRMQL st = new SegmentTreeRMQL(n+1);
+		reachs[0] = -t;
+		st.update(0, 0);
+		for(int i = 0;i < n;i++){
+			long[] r = rs[i];
+			int id = Arrays.binarySearch(reachs, 0, i+1, r[0]-t);
+			if(id < 0)id = -id-2;
+			long max = -st.minx(0, id+1) + (r[1]-r[0])/P;
+			long nr = (r[1]-r[0])/P*P + r[0];
+			int nid = id+1;
+			if(nid < i+1 && r[1]-P >= reachs[nid]+t){
+				long val = ((r[1]-(reachs[nid]+t))/P - st.minx(nid, nid+1));
+				long cannr = reachs[nid]+t+((r[1]-(reachs[nid]+t))/P*P);
+				if(val > max || val == max && cannr < nr){
+					max = val;
+					nr = cannr;
+				}
+			}
+			
+			reachs[i+1] = nr;
+			st.update(i+1, -max);
+		}
+		
+		out.println(-st.st[1]);
+	}
+	
+	public static class SegmentTreeRMQL {
+		public int M, H, N;
+		public long[] st;
+		
+		public SegmentTreeRMQL(int n)
+		{
+			N = n;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new long[M];
+			Arrays.fill(st, 0, M, Long.MAX_VALUE);
+		}
+		
+		public SegmentTreeRMQL(long[] a)
+		{
+			N = a.length;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new long[M];
+			for(int i = 0;i < N;i++){
+				st[H+i] = a[i];
+			}
+			Arrays.fill(st, H+N, M, Long.MAX_VALUE);
+			for(int i = H-1;i >= 1;i--)propagate(i);
+		}
+		
+		public void update(int pos, long x)
+		{
+			st[H+pos] = x;
+			for(int i = (H+pos)>>>1;i >= 1;i >>>= 1)propagate(i);
+		}
+		
+		private void propagate(int i)
+		{
+			st[i] = Math.min(st[2*i], st[2*i+1]);
+		}
+		
+		public long minx(int l, int r){
+			if(l >= r)return 0L;
+			long min = Long.MAX_VALUE;
+			while(l != 0){
+				int f = l&-l;
+				if(l+f > r)break;
+				long v = st[(H+l)/f];
+				if(v < min)min = v;
+				l += f;
+			}
+			
+			while(l < r){
+				int f = r&-r;
+				long v = st[(H+r)/f-1];
+				if(v < min)min = v;
+				r -= f;
+			}
+			return min;
+		}
+		
+		public long min(int l, int r){ return l >= r ? 0 : min(l, r, 0, H, 1);}
+		
+		private long min(int l, int r, int cl, int cr, int cur)
+		{
+			if(l <= cl && cr <= r){
+				return st[cur];
+			}else{
+				int mid = cl+cr>>>1;
+				long ret = Long.MAX_VALUE;
+				if(cl < r && l < mid){
+					ret = Math.min(ret, min(l, r, cl, mid, 2*cur));
+				}
+				if(mid < r && l < cr){
+					ret = Math.min(ret, min(l, r, mid, cr, 2*cur+1));
+				}
+				return ret;
+			}
+		}
+		
+		public int firstle(int l, long v) {
+			int cur = H+l;
+			while(true){
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur;
+					}else{
+						return cur-H;
+					}
+				}else{
+					cur++;
+					if((cur&cur-1) == 0)return -1;
+					if((cur&1)==0)cur>>>=1;
+				}
+			}
+		}
+		
+		public int lastle(int l, long v) {
+			int cur = H+l;
+			while(true){
+				if(st[cur] <= v){
+					if(cur < H){
+						cur = 2*cur+1;
+					}else{
+						return cur-H;
+					}
+				}else{
+					if((cur&cur-1) == 0)return -1;
+					cur--;
+					if((cur&1)==1)cur>>>=1;
+				}
+			}
+		}
+	}
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new E2().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	private int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
