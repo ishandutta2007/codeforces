cc4c1a9a00680aad434a1e4d0dd4f3ea22907426
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 18:01:56 +0530

    uwi

---
+++ uwi/normal/1342/E.java
@@ -0,0 +1,198 @@
+//package ecr086;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class E {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int mod = 998244353;
+		int n = ni();
+		long K = nl();
+		if(K >= n){
+			out.println(0);
+			return;
+		}
+		int[][] fif = enumFIF(250000, mod);
+		if(K == 0){
+			out.println(fif[0][n]);
+			return;
+		}
+		
+		long ans = 0;
+		for(int i = (int)(n-K), s = 1;i >= 1;i--, s = mod - s){
+			ans += C(n, i, mod, fif) * pow(i, n, mod) % mod * s % mod * C((int)(n-i), (int)K, mod, fif);
+			ans %= mod;
+		}
+		ans = ans * 2 % mod;
+		out.println(ans);
+	}
+	
+	public static long pow(long a, long n, long mod) {
+		//		a %= mod;
+		long ret = 1;
+		int x = 63 - Long.numberOfLeadingZeros(n);
+		for (; x >= 0; x--) {
+			ret = ret * ret % mod;
+			if (n << 63 - x < 0)
+				ret = ret * a % mod;
+		}
+		return ret;
+	}
+
+	
+	public static long C(int n, int r, int mod, int[][] fif) {
+		if (n < 0 || r < 0 || r > n)
+			return 0;
+		return (long) fif[0][n] * fif[1][r] % mod * fif[1][n - r] % mod;
+	}
+
+	
+	public static int[][] enumFIF(int n, int mod) {
+		int[] f = new int[n + 1];
+		int[] invf = new int[n + 1];
+		f[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			f[i] = (int) ((long) f[i - 1] * i % mod);
+		}
+		long a = f[n];
+		long b = mod;
+		long p = 1, q = 0;
+		while (b > 0) {
+			long c = a / b;
+			long d;
+			d = a;
+			a = b;
+			b = d % b;
+			d = p;
+			p = q;
+			q = d - c * q;
+		}
+		invf[n] = (int) (p < 0 ? p + mod : p);
+		for (int i = n - 1; i >= 0; i--) {
+			invf[i] = (int) ((long) invf[i + 1] * (i + 1) % mod);
+		}
+		return new int[][] { f, invf };
+	}
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new E().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
