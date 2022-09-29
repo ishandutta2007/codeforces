5563965dd767fd6736b69b17d33f3912036d7dc2
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 20:13:34 +0530

    uwi

---
+++ uwi/normal/815/D.java
@@ -0,0 +1,313 @@
+//package round419;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.InputMismatchException;
+import java.util.List;
+
+public class D {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni(), P = ni(), Q = ni(), R = ni();
+		int[][] es = new int[3*n][];
+		int p = 0;
+		for(int i = 0;i < n;i++){
+			int x = ni(), y = ni(), z = ni();
+			es[p++] = new int[]{z, P, y};
+			es[p++] = new int[]{z, x, Q};
+			es[p++] = new int[]{R, x, y};
+		}
+		Arrays.sort(es, new Comparator<int[]>() {
+			public int compare(int[] a, int[] b) {
+				return -(a[0] - b[0]);
+			}
+		});
+		
+		LST lst = new LST(P+3);
+		long[] hs = new long[P+3];
+		Arrays.fill(hs, -1);
+		hs[0] = 1<<22;
+		hs[1] = 0;
+		lst.set(0);
+		lst.set(1);
+		
+		long V = (long)P*Q*R;
+		long pz = R;
+		long S = 0;
+		for(int[] e : es){
+			V -= (pz-e[0]) * S;
+			pz = e[0];
+			
+			int x = e[1], y = e[2];
+			int pr = lst.prev(x);
+//			tr(e, S, V, pr, lst, hs, y);
+			if(y <= hs[pr])continue;
+			
+			if(!lst.get(x+1)){
+				hs[x+1] = hs[pr];
+				lst.set(x+1);
+			}
+			
+			int px = x+1;
+			while(true){
+				pr = lst.prev(x);
+				if(hs[pr] < y){
+					S -= (px-pr)*hs[pr];
+					lst.unset(pr);
+					hs[pr] = -1;
+					px = pr;
+				}else{
+					break;
+				}
+			}
+			hs[px] = y;
+			lst.set(px);
+			S += (long)(x+1-px) * y;
+		}
+		V -= (pz-0) * S;
+		out.println(V);
+	}
+	
+	public static class LST {
+		public long[][] set;
+		public int n;
+//		public int size;
+		
+		public LST(int n) {
+			this.n = n;
+			int d = 1;
+			for(int m = n;m > 1;m>>>=6, d++);
+			
+			set = new long[d][];
+			for(int i = 0, m = n>>>6;i < d;i++, m>>>=6){
+				set[i] = new long[m+1];
+			}
+//			size = 0;
+		}
+		
+		// [0,r)
+		public LST setRange(int r)
+		{
+			for(int i = 0;i < set.length;i++, r=r+63>>>6){
+				for(int j = 0;j < r>>>6;j++){
+					set[i][j] = -1L;
+				}
+				if((r&63) != 0)set[i][r>>>6] |= (1L<<r)-1;
+			}
+			return this;
+		}
+		
+		// [0,r)
+		public LST unsetRange(int r)
+		{
+			if(r >= 0){
+				for(int i = 0;i < set.length;i++, r=r+63>>>6){
+					for(int j = 0;j < r+63>>>6;j++){
+						set[i][j] = 0;
+					}
+					if((r&63) != 0)set[i][r>>>6] &= ~((1L<<r)-1);
+				}
+			}
+			return this;
+		}
+		
+		public LST set(int pos)
+		{
+			if(pos >= 0 && pos < n){
+//				if(!get(pos))size++;
+				for(int i = 0;i < set.length;i++, pos>>>=6){
+					set[i][pos>>>6] |= 1L<<pos;
+				}
+			}
+			return this;
+		}
+		
+		public LST unset(int pos)
+		{
+			if(pos >= 0 && pos < n){
+//				if(get(pos))size--;
+				for(int i = 0;i < set.length && (i == 0 || set[i-1][pos] == 0L);i++, pos>>>=6){
+					set[i][pos>>>6] &= ~(1L<<pos);
+				}
+			}
+			return this;
+		}
+		
+		public boolean get(int pos)
+		{
+			return pos >= 0 && pos < n && set[0][pos>>>6]<<~pos<0;
+		}
+		
+		public int prev(int pos)
+		{
+			for(int i = 0;i < set.length && pos >= 0;i++, pos>>>=6, pos--){
+				int pre = prev(set[i][pos>>>6], pos&63);
+				if(pre != -1){
+					pos = pos>>>6<<6|pre;
+					while(i > 0)pos = pos<<6|63-Long.numberOfLeadingZeros(set[--i][pos]);
+					return pos;
+				}
+			}
+			return -1;
+		}
+		
+		public int next(int pos)
+		{
+			for(int i = 0;i < set.length && pos>>>6 < set[i].length;i++, pos>>>=6, pos++){
+				int nex = next(set[i][pos>>>6], pos&63);
+				if(nex != -1){
+					pos = pos>>>6<<6|nex;
+					while(i > 0)pos = pos<<6|Long.numberOfTrailingZeros(set[--i][pos]);
+					return pos;
+				}
+			}
+			return -1;
+		}
+		
+		private static int prev(long set, int n)
+		{
+			long h = Long.highestOneBit(set<<~n);
+			if(h == 0L)return -1;
+			return Long.numberOfTrailingZeros(h)-(63-n);
+		}
+		
+		private static int next(long set, int n)
+		{
+			long h = Long.lowestOneBit(set>>>n);
+			if(h == 0L)return -1;
+			return Long.numberOfTrailingZeros(h)+n;
+		}
+		
+		@Override
+		public String toString()
+		{
+			List<Integer> list = new ArrayList<Integer>();
+			for(int pos = next(0);pos != -1;pos = next(pos+1)){
+				list.add(pos);
+			}
+			return list.toString();
+		}
+	}
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new D().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
