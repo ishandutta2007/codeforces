78f8336abe0f28aad14b7857ee0c011498dd5544
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 20:35:21 +0530

    uwi

---
+++ uwi/normal/755/D.java
@@ -0,0 +1,181 @@
+//package eightvc.e;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.InputMismatchException;
+
+public class D {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni(), K = ni();
+		if(2*K >= n){
+			K = n-K;
+		}
+		int[] ft = new int[n+3];
+		long ret = 1;
+		int cur = 0;
+		for(int i = 0;i < n;i++){
+			int nex = cur+K;
+			// [cur-K+1,cur+K-1]
+			int pre = cur-K+1;
+			if(nex < n){
+				if(pre >= 0){
+					ret += sumFenwick(ft, nex-1) - sumFenwick(ft, pre-1);
+				}else{
+					ret += sumFenwick(ft, nex-1) + sumFenwick(ft, n-1) - sumFenwick(ft, pre+n-1);
+				}
+			}else{
+				ret += sumFenwick(ft, n-1) - sumFenwick(ft, pre-1) + sumFenwick(ft, nex-n-1);
+			}
+			ret++;
+			addFenwick(ft, cur, 1);
+			while(nex >= n)nex -= n;
+			cur = nex;
+			out.print(ret + " ");
+		}
+	}
+	
+	public static int[] restoreFenwick(int[] ft)
+	{
+		int n = ft.length-1;
+		int[] ret = new int[n];
+		for(int i = 0;i < n;i++)ret[i] = sumFenwick(ft, i);
+		for(int i = n-1;i >= 1;i--)ret[i] -= ret[i-1];
+		return ret;
+	}
+	
+	public static int sumFenwick(int[] ft, int i)
+	{
+		assert i >= -1;
+		int sum = 0;
+		for(i++;i > 0;i -= i&-i)sum += ft[i];
+		return sum;
+	}
+	
+	public static void addFenwick(int[] ft, int i, int v)
+	{
+		if(v == 0 || i < 0)return;
+		int n = ft.length;
+		for(i++;i < n;i += i&-i)ft[i] += v;
+	}
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new D().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	public int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
