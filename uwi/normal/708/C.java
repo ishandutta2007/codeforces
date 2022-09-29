0286102156c90c7a4e643b7676e086ae80111b82
ishan dutta <ishandutta2007@gmail.com>
2022-09-29 20:53:05 +0530

    uwi

---
+++ uwi/normal/708/C.java
@@ -0,0 +1,483 @@
+//package aim.r3;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.InputMismatchException;
+
+public class C {
+	InputStream is;
+	PrintWriter out;
+	String INPUT = "";
+	
+	void solve()
+	{
+		int n = ni();
+		int[] from = new int[n - 1];
+		int[] to = new int[n - 1];
+		for (int i = 0; i < n - 1; i++) {
+			from[i] = ni() - 1;
+			to[i] = ni() - 1;
+		}
+		int[][] g = packU(n, from, to);
+		int[] ok = new int[n];
+		Arrays.fill(ok, 1);
+		int root = 0;
+		int[][] pars = parents3(g, root);
+		int[] par = pars[0], dep = pars[2];
+		int[][] rs = makeRights(g, par, root);
+		int[] ord = rs[0], iord = rs[1], right = rs[2];
+		int[] des = new int[n];
+		for(int i = 0;i < n;i++){
+			des[i] = right[i] - i + 1;
+		}
+		SegmentTreeRMQPos st = new SegmentTreeRMQPos(des);
+		for(int i = n-1;i >= 1;i--){
+			int cur = ord[i];
+			if(des[i] >= n/2+1){
+				// par[cur] is yabai
+				while(true){
+					int min = st.minx(i, right[i]+1);
+					if(min >= n/2+1){
+						ok[par[cur]] = 0;
+						break;
+					}else if(des[i]-min >= n/2+1){
+						st.update(st.minpos, 99999999);
+					}else{
+						break;
+					}
+				}
+			}
+		}
+		for(int i = 0;i < n;i++){
+			int up = n-des[i];
+			if(up >= n/2+1){
+				ok[ord[i]] = 0;
+			}
+		}
+		dfs(0, par, g, new int[n+3], n, des, iord, ok);
+		dfs2(0, par, g, new int[n+3], n, des, iord, ok);
+		for(int i = 0;i < n;i++){
+			out.print(ok[i] + " ");
+		}
+		out.println();
+	}
+	
+	void dfs(int cur, int[] par, int[][] g, int[] ft, int n, int[] des, int[] iord, int[] ok)
+	{
+		int up = n-des[iord[cur]];
+		if(up >= n/2+1){
+			if(sumFenwick(ft, n/2) - sumFenwick(ft, up-n/2-1) > 0){
+				ok[cur] = 1;
+			}
+		}
+		addFenwick(ft, up, 1);
+		for(int i = 0;i < g[cur].length;i++){
+			int nex = g[cur][i];
+			if(nex == par[cur])continue;
+			dfs(nex, par, g, ft, n, des, iord, ok);
+		}
+		addFenwick(ft, up, -1);
+		addFenwick(ft, des[iord[cur]], 1);
+	}
+	
+	void dfs2(int cur, int[] par, int[][] g, int[] ft, int n, int[] des, int[] iord, int[] ok)
+	{
+		int up = n-des[iord[cur]];
+		if(up >= n/2+1){
+			if(sumFenwick(ft, n/2) - sumFenwick(ft, up-n/2-1) > 0){
+				ok[cur] = 1;
+			}
+		}
+		addFenwick(ft, up, 1);
+		for(int i = g[cur].length-1;i >= 0;i--){
+			int nex = g[cur][i];
+			if(nex == par[cur])continue;
+			dfs2(nex, par, g, ft, n, des, iord, ok);
+		}
+		addFenwick(ft, up, -1);
+		addFenwick(ft, des[iord[cur]], 1);
+	}
+	
+	public static int sumFenwick(int[] ft, int i) {
+		int sum = 0;
+		for (i++; i > 0; i -= i & -i)
+			sum += ft[i];
+		return sum;
+	}
+
+	public static void addFenwick(int[] ft, int i, int v) {
+		if (v == 0 || i < 0)
+			return;
+		int n = ft.length;
+		for (i++; i < n; i += i & -i)
+			ft[i] += v;
+	}
+
+	public static int findGFenwick(int[] ft, int v) {
+		int i = 0;
+		int n = ft.length;
+		for (int b = Integer.highestOneBit(n); b != 0 && i < n; b >>= 1) {
+			if (i + b < n) {
+				int t = i + b;
+				if (v >= ft[t]) {
+					i = t;
+					v -= ft[t];
+				}
+			}
+		}
+		return v != 0 ? -(i + 1) : i - 1;
+	}
+
+	public static int valFenwick(int[] ft, int i) {
+		return sumFenwick(ft, i) - sumFenwick(ft, i - 1);
+	}
+
+	public static int[] restoreFenwick(int[] ft) {
+		int n = ft.length - 1;
+		int[] ret = new int[n];
+		for (int i = 0; i < n; i++)
+			ret[i] = sumFenwick(ft, i);
+		for (int i = n - 1; i >= 1; i--)
+			ret[i] -= ret[i - 1];
+		return ret;
+	}
+
+	public static int before(int[] ft, int x) {
+		int u = sumFenwick(ft, x - 1);
+		if (u == 0)
+			return -1;
+		return findGFenwick(ft, u - 1) + 1;
+	}
+
+	public static int after(int[] ft, int x) {
+		int u = sumFenwick(ft, x);
+		int f = findGFenwick(ft, u);
+		if (f + 1 >= ft.length - 1)
+			return -1;
+		return f + 1;
+	}
+
+	public static int[] buildFenwick(int[] a) {
+		int n = a.length;
+		int[] ft = new int[n + 1];
+		System.arraycopy(a, 0, ft, 1, n);
+		for (int k = 2, h = 1; k <= n; k *= 2, h *= 2) {
+			for (int i = k; i <= n; i += k) {
+				ft[i] += ft[i - h];
+			}
+		}
+		return ft;
+	}
+
+	public static int[] buildFenwick(int n, int v) {
+		int[] ft = new int[n + 1];
+		Arrays.fill(ft, 1, n + 1, v);
+		for (int k = 2, h = 1; k <= n; k *= 2, h *= 2) {
+			for (int i = k; i <= n; i += k) {
+				ft[i] += ft[i - h];
+			}
+		}
+		return ft;
+	}
+
+	public static class SegmentTreeRMQPos {
+		public int M, H, N;
+		public int[] st;
+		public int[] pos;
+		
+		public SegmentTreeRMQPos(int n)
+		{
+			N = n;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new int[M];
+			pos = new int[M];
+			for(int i = 0;i < N;i++)pos[H+i] = i;
+			Arrays.fill(st, 0, M, Integer.MAX_VALUE);
+			for(int i = H-1;i >= 1;i--)propagate(i);
+		}
+		
+		public SegmentTreeRMQPos(int[] a)
+		{
+			N = a.length;
+			M = Integer.highestOneBit(Math.max(N-1, 1))<<2;
+			H = M>>>1;
+			st = new int[M];
+			pos = new int[M];
+			for(int i = 0;i < N;i++){
+				st[H+i] = a[i];
+				pos[H+i] = i;
+			}
+			Arrays.fill(st, H+N, M, Integer.MAX_VALUE);
+			for(int i = H-1;i >= 1;i--)propagate(i);
+		}
+		
+		public void update(int pos, int x)
+		{
+			st[H+pos] = x;
+			for(int i = (H+pos)>>>1;i >= 1;i >>>= 1)propagate(i);
+		}
+		
+		private void propagate(int i)
+		{
+			if(st[2*i] <= st[2*i+1]){
+				st[i] = st[2*i];
+				pos[i] = pos[2*i];
+			}else{
+				st[i] = st[2*i+1];
+				pos[i] = pos[2*i+1];
+			}
+		}
+		
+		public int minpos;
+		public int minval;
+		
+		public int minx(int l, int r){
+			if(l >= r)return Integer.MAX_VALUE;
+			minval = Integer.MAX_VALUE;
+			minpos = -1;
+			while(l != 0){
+				int f = l&-l;
+				if(l+f > r)break;
+				int v = st[(H+l)/f];
+				if(v < minval){
+					minval = v;
+					minpos = pos[(H+l)/f];
+				}
+				l += f;
+			}
+			
+			while(l < r){
+				int f = r&-r;
+				int v = st[(H+r)/f-1];
+				if(v < minval){
+					minval = v;
+					minpos = pos[(H+r)/f-1];
+				}
+				r -= f;
+			}
+			return minval;
+		}
+		
+		public int min(int l, int r){ 
+			minpos = -1;
+			minval = Integer.MAX_VALUE;
+			min(l, r, 0, H, 1);
+			return minval;
+		}
+		
+		private void min(int l, int r, int cl, int cr, int cur)
+		{
+			if(l <= cl && cr <= r){
+				if(st[cur] < minval){
+					minval = st[cur];
+					minpos = pos[cur];
+				}
+			}else{
+				int mid = cl+cr>>>1;
+				if(cl < r && l < mid)min(l, r, cl, mid, 2*cur);
+				if(mid < r && l < cr)min(l, r, mid, cr, 2*cur+1);
+			}
+		}
+	}
+	
+	public static int[] sortByPreorder(int[][] g, int root){
+		int n = g.length;
+		int[] stack = new int[n];
+		int[] ord = new int[n];
+		BitSet ved = new BitSet();
+		stack[0] = root;
+		int p = 1;
+		int r = 0;
+		ved.set(root);
+		while(p > 0){
+			int cur = stack[p-1];
+			ord[r++] = cur;
+			p--;
+			for(int e : g[cur]){
+				if(!ved.get(e)){
+					stack[p++] = e;
+					ved.set(e);
+				}
+			}
+		}
+		return ord;
+	}
+	
+	public static int[][] makeRights(int[][] g, int[] par, int root)
+	{
+		int n = g.length;
+		int[] ord = sortByPreorder(g, root);
+		int[] iord = new int[n];
+		for(int i = 0;i < n;i++)iord[ord[i]] = i;
+		
+		int[] right = new int[n];
+		for(int i = n-1;i >= 0;i--){
+			int v = i;
+			for(int e : g[ord[i]]){
+				if(e != par[ord[i]]){
+					v = Math.max(v, right[iord[e]]);
+				}
+			}
+			right[i] = v;
+		}
+		return new int[][]{ord, iord, right};
+	}
+
+
+	public static int[][] parents3(int[][] g, int root) {
+		int n = g.length;
+		int[] par = new int[n];
+		Arrays.fill(par, -1);
+
+		int[] depth = new int[n];
+		depth[0] = 0;
+
+		int[] q = new int[n];
+		q[0] = root;
+		for (int p = 0, r = 1; p < r; p++) {
+			int cur = q[p];
+			for (int nex : g[cur]) {
+				if (par[cur] != nex) {
+					q[r++] = nex;
+					par[nex] = cur;
+					depth[nex] = depth[cur] + 1;
+				}
+			}
+		}
+		return new int[][] { par, q, depth };
+	}
+
+	static int[][] packU(int n, int[] from, int[] to) {
+		int[][] g = new int[n][];
+		int[] p = new int[n];
+		for (int f : from)
+			p[f]++;
+		for (int t : to)
+			p[t]++;
+		for (int i = 0; i < n; i++)
+			g[i] = new int[p[i]];
+		for (int i = 0; i < from.length; i++) {
+			g[from[i]][--p[from[i]]] = to[i];
+			g[to[i]][--p[to[i]]] = from[i];
+		}
+		return g;
+	}
+
+	
+	void run() throws Exception
+	{
+		is = oj ? System.in : new ByteArrayInputStream(INPUT.getBytes());
+		out = new PrintWriter(System.out);
+		
+		long s = System.currentTimeMillis();
+		solve();
+		out.flush();
+		tr(System.currentTimeMillis()-s+"ms");
+	}
+	
+	public static void main(String[] args) throws Exception { new C().run(); }
+	
+	private byte[] inbuf = new byte[1024];
+	private int lenbuf = 0, ptrbuf = 0;
+	
+	private int readByte()
+	{
+		if(lenbuf == -1)throw new InputMismatchException();
+		if(ptrbuf >= lenbuf){
+			ptrbuf = 0;
+			try { lenbuf = is.read(inbuf); } catch (IOException e) { throw new InputMismatchException(); }
+			if(lenbuf <= 0)return -1;
+		}
+		return inbuf[ptrbuf++];
+	}
+	
+	private boolean isSpaceChar(int c) { return !(c >= 33 && c <= 126); }
+	private int skip() { int b; while((b = readByte()) != -1 && isSpaceChar(b)); return b; }
+	
+	private double nd() { return Double.parseDouble(ns()); }
+	private char nc() { return (char)skip(); }
+	
+	private String ns()
+	{
+		int b = skip();
+		StringBuilder sb = new StringBuilder();
+		while(!(isSpaceChar(b))){ // when nextLine, (isSpaceChar(b) && b != ' ')
+			sb.appendCodePoint(b);
+			b = readByte();
+		}
+		return sb.toString();
+	}
+	
+	private char[] ns(int n)
+	{
+		char[] buf = new char[n];
+		int b = skip(), p = 0;
+		while(p < n && !(isSpaceChar(b))){
+			buf[p++] = (char)b;
+			b = readByte();
+		}
+		return n == p ? buf : Arrays.copyOf(buf, p);
+	}
+	
+	private char[][] nm(int n, int m)
+	{
+		char[][] map = new char[n][];
+		for(int i = 0;i < n;i++)map[i] = ns(m);
+		return map;
+	}
+	
+	private int[] na(int n)
+	{
+		int[] a = new int[n];
+		for(int i = 0;i < n;i++)a[i] = ni();
+		return a;
+	}
+	
+	private int ni()
+	{
+		int num = 0, b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private long nl()
+	{
+		long num = 0;
+		int b;
+		boolean minus = false;
+		while((b = readByte()) != -1 && !((b >= '0' && b <= '9') || b == '-'));
+		if(b == '-'){
+			minus = true;
+			b = readByte();
+		}
+		
+		while(true){
+			if(b >= '0' && b <= '9'){
+				num = num * 10 + (b - '0');
+			}else{
+				return minus ? -num : num;
+			}
+			b = readByte();
+		}
+	}
+	
+	private boolean oj = System.getProperty("ONLINE_JUDGE") != null;
+	private void tr(Object... o) { if(!oj)System.out.println(Arrays.deepToString(o)); }
+}
