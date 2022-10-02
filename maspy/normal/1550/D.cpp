543ee96e753eccf4078bee59e5750d1b177f8aa6
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 21:37:27 +0530

    maspy

---
+++ maspy/normal/1550/D.cpp
@@ -0,0 +1,374 @@
+#line 1 "main.cpp"
+#include <bits/stdc++.h>
+#line 3 "/home/maspy/library/my_template.hpp"
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define int ll
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define FOR(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define FOR3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define FOR_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+#define FORIN(x, A) for (auto&& x : A)
+#define all(x) x.begin(), x.end()
+
+#define elif else if
+
+#define popcnt __builtin_popcount
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define VEC vector
+
+template <typename T>
+struct Matrix {
+  ll H, W;
+  vector<vector<T>> data;
+  pi shape;
+  Matrix(ll H, ll W) : H(H), W(W), data(H, vector<T>(W)), shape({H,W}) {}
+  void fill(T x) {
+    FOR(i, H) { FOR(j, W) data[i][j] = x; }
+  }
+  vector<T>& operator[](ll i) { return data[i]; }
+  bool operator==(Matrix<T>& other){
+    if(H != other.H || W != other.W) return false;
+    FOR(i, H) FOR(j, W){
+      if(data[i][j] != other[i][j]) return false;
+    }
+    return true;
+  }
+  Matrix rot(){
+    auto B = Matrix(W,H);
+    FOR(i, W){
+      FOR(j, H){
+        B[i][j] = (*this)[j][W-1-i];
+      }
+    }
+    return B;
+  }
+};
+
+
+// [
+ll in() {
+  ll i;
+  cin >> i;
+  return i;
+}
+
+pi in2() {
+  ll a, b;
+  cin >> a >> b;
+  return {a, b};
+}
+
+tuple<ll, ll, ll> in3() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  return {a, b, c};
+}
+
+tuple<ll, ll, ll, ll> in4() {
+  ll a, b, c, d;
+  cin >> a >> b >> c >> d;
+  return {a, b, c, d};
+}
+
+vi vin(int n) {
+  vi A(n);
+  for (auto&& x : A) {
+    cin >> x;
+  }
+  return A;
+}
+
+string sin() {
+  string s;
+  cin >> s;
+  return s;
+}
+
+vi s_to_vi(string S, char first_char='a'){
+  vi A(S.size());
+  FOR(i, S.size()){
+    A[i] = S[i] - first_char;
+  }
+  return A;
+}
+
+Matrix<ll> matrixin(int H, int W){
+  Matrix<ll> A(H, W);
+  FOR(i, H){
+    FOR(j, W){
+      A[i][j] = in();
+    }
+  }
+  return A;
+}
+
+Matrix<char> cmatrixin(int H, int W){
+  Matrix<char> A(H, W);
+  FOR(i, H){
+    string S = sin();
+    FOR(j, W){
+      A[i][j] = S[j];
+    }
+  }
+  return A;
+}
+
+// ]
+
+// [
+template <typename T, typename U>
+ostream& operator<<(ostream& os, pair<T, U>& A) {
+  os << A.fi << " " << A.se;
+  return os;
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+
+
+void print() {
+  cout << "\n";
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+template <typename T>
+string join(vector<T>& v){
+  ostringstream os;
+  for(auto&& x : v){
+    os << x;
+  }
+  return os.str();
+}
+
+void YES() {print("YES");}
+void NO() {print("NO");}
+void Yes() {print("Yes");}
+void No() {print("No");}
+// ]
+
+template <typename T>
+ll sum(T A) {
+  return accumulate(all(A), 0LL);
+}
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+vi bin_count(vi& A, ll size) {
+  vi C(size);
+  for (auto&& x : A) {
+    ++C[x];
+  }
+  return C;
+}
+
+template <typename T>
+vi argsort(vector<T>& A){
+  vi ids(A.size());
+  iota(all(ids), 0);
+  sort(all(ids), [&](ll i, ll j){return mp(A[i],i)<=mp(A[j],j);});
+  return ids;
+}
+
+template <typename T>
+int argmax(vector<T>& A){
+  return max_element(all(A)) - A.begin();
+}
+
+
+template <typename T>
+ostream& operator<<(ostream& os, Matrix<T>& A) {
+  FOR(i, A.H){
+    if(i) os << "\n";
+    os << A[i];
+  }
+  return os;
+}
+
+template <class T, class U>
+bool chmax(T& a, const U& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T, class U>
+bool chmin(T& a, const U& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+#line 3 "/home/maspy/library/mod/modint.hpp"
+
+template <std::uint_fast64_t Modulus> class modint {
+  long long val;
+  public:
+    static const uint_fast64_t mod = Modulus;
+    constexpr modint(long long x = 0) noexcept : val(x % Modulus) {}
+    constexpr modint operator+(const modint rhs) const noexcept {return modint(*this) += rhs;}
+    constexpr modint operator-(const modint rhs) const noexcept {return modint(*this) -= rhs;}
+    constexpr modint operator*(const modint rhs) const noexcept {return modint(*this) *= rhs;}
+    constexpr modint operator/(const modint rhs) const noexcept {return modint(*this) /= rhs;}
+    constexpr modint &operator+=(const modint rhs) noexcept {
+      val += rhs.val;
+      if (val >= Modulus) val -= Modulus;
+      return *this;
+    }
+    constexpr modint &operator-=(const modint rhs) noexcept {
+      if (val < rhs.val) val += Modulus;
+      val -= rhs.val;
+      return *this;
+    }
+    constexpr modint &operator*=(const modint rhs) noexcept {
+      val = val * rhs.val % Modulus;
+      return *this;
+    }
+    constexpr modint &operator/=(modint rhs) noexcept {
+      auto exp = Modulus - 2;
+      while (exp) {
+        if (exp % 2) {
+          *this *= rhs;
+        }
+        rhs *= rhs;
+        exp /= 2;
+      }
+      return *this;
+    }
+    friend ostream &operator<<(ostream &os, const modint &p) {
+      return os << p.val;
+    }
+};
+
+template< typename T >
+struct ModCalc {
+  vector<T> _fact = {1, 1};
+  vector<T> _fact_inv = {1, 1};
+  vector<T> _inv = {0, 1};
+  
+  T pow(T a, int n){
+    T x(1);
+    while(n) {
+      if(n & 1) x *= a;
+      a *= a;
+      n >>= 1;
+    }
+    return x;
+  }
+  void expand(int n){
+    while(_fact.size() <= n){
+      auto i = _fact.size();
+      _fact.eb(_fact[i-1] * i);
+      auto q = T::mod / i, r = T::mod % i;
+      _inv.eb(_inv[r] * (T::mod-q));
+      _fact_inv.eb(_fact_inv[i-1] * _inv[i]);
+    }
+  }
+
+  T fact(int n){
+    if(n >= _fact.size()) expand(n);
+    return _fact[n];
+  }
+
+  T fact_inv(int n){
+    if(n >= _fact.size()) expand(n);
+    return _fact_inv[n];
+  }
+  
+  T inv(int n){
+    if(n >= _fact.size()) expand(n);
+    return _inv[n];
+  }
+  
+  T C(int n, int k, bool large=false){
+    assert(n >= 0);
+    if (k < 0 || n < k) return 0;
+    if (!large) return fact(n) * fact_inv(k) * fact_inv(n-k);
+    T x(1);
+    FOR(i, k){
+      x *= n - i;
+      x *= inv(i + 1);
+    }
+    return x;
+  }
+};
+
+using modint107 = modint<1'000'000'007>;
+using modint998 = modint<998'244'353>;
+#line 4 "main.cpp"
+
+using mint = modint107;
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  ModCalc<mint> MD;
+
+  auto T = in();
+  FOR(_, T) {
+    auto [N, L, R] = in3();
+    // 
+    --L;
+    --R;
+    L = -L;
+    R = R - N + 1;
+    if (L > R) swap(L, R);
+    vi left_cnt;
+    left_cnt.eb(N / 2);
+    if (N & 1) left_cnt.eb(N / 2 + 1);
+    mint ANS;
+    ANS = 0;
+    FORIN(l, left_cnt) {
+      // k <= L
+      mint c = MD.C(N, l);
+      ANS += c * L;
+      FOR3(k, L + 1, L + N) {
+        ll l_cnt = N - (k - L);
+        // R + i >= k  0 <= i < N
+        ll r_cnt = max(0LL, N - max(0LL, k - R));
+        if (l_cnt < l || r_cnt < N - l) continue;
+        ll must_l = N - r_cnt;
+        ll must_r = N - l_cnt;
+        ll both = N - must_l - must_r;
+        mint x = MD.C(both, l - must_l);
+        ANS += x;
+      }
+    }
+    print(ANS);
+  }
+  return 0;
+}
