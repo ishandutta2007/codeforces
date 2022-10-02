1659e4c524353b2ebf16bb9af189c7f2a5b39cba
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:01:31 +0530

    maspy

---
+++ maspy/normal/1095/F.cpp
@@ -0,0 +1,305 @@
+#line 1 "main.cpp"
+#include <bits/stdc++.h>
+#line 3 "/home/maspy/library/my_template.hpp"
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define int ll
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define FOR(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define FOR3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define FOR_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+#define all(x) x.begin(), x.end()
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define VEC vector
+
+template <typename T>
+struct Grid {
+  ll H, W;
+  vector<vector<T>> data;
+  pi shape;
+  Grid(ll H, ll W) : H(H), W(W), data(H, vector<T>(W)), shape({H,W}) {}
+  void fill(T x) {
+    FOR(i, H) { FOR(j, W) data[i][j] = x; }
+  }
+  vector<T>& operator[](ll i) { return data[i]; }
+  bool operator==(Grid<T>& other){
+    if(H != other.H || W != other.W) return false;
+    FOR(i, H) FOR(j, W){
+      if(data[i][j] != other[i][j]) return false;
+    }
+    return true;
+  }
+  Grid rot(){
+    auto B = Grid(W,H);
+    FOR(i, W){
+      FOR(j, H){
+        B[i][j] = (*this)[j][W-1-i];
+      }
+    }
+    return B;
+  }
+};
+
+
+// [
+ll in() {
+  ll i;
+  cin >> i;
+  return i;
+}
+
+pi in2(int off = 0) {
+  ll a, b;
+  cin >> a >> b;
+  return {a + off, b + off};
+}
+
+tuple<ll, ll, ll> in3() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  return {a, b, c};
+}
+
+tuple<ll, ll, ll, ll> in4() {
+  ll a, b, c, d;
+  cin >> a >> b >> c >> d;
+  return {a, b, c, d};
+}
+
+vi vin(int n, int off = 0) {
+  vi A(n);
+  for (auto&& x : A) {
+    cin >> x;
+    x += off;
+  }
+  return A;
+}
+
+string sin() {
+  string s;
+  cin >> s;
+  return s;
+}
+
+Grid<ll> gridin(int H, int W){
+  Grid<ll> A(H, W);
+  FOR(i, H){
+    FOR(j, W){
+      A[i][j] = in();
+    }
+  }
+  return A;
+}
+
+Grid<char> cgridin(int H, int W){
+  Grid<char> A(H, W);
+  FOR(i, H){
+    string S = sin();
+    FOR(j, W){
+      A[i][j] = S[j];
+    }
+  }
+  return A;
+}
+
+// ]
+
+// [
+template <typename T, typename U>
+ostream& operator<<(ostream& os, pair<T, U>& A) {
+  os << A.fi << " " << A.se;
+  return os;
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+
+
+void print() {
+  cout << "\n";
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+void YES() {print("YES");}
+void NO() {print("NO");}
+void Yes() {print("Yes");}
+void No() {print("No");}
+// ]
+
+template <typename T>
+ll sum(T A) {
+  return accumulate(all(A), 0);
+}
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+vi bin_count(vi& A, ll size) {
+  vi C(size);
+  for (auto&& x : A) {
+    ++C[x];
+  }
+  return C;
+}
+
+template <typename T>
+int argmax(vector<T>& A){
+  return max_element(all(A)) - A.begin();
+}
+
+
+
+template <typename T>
+ostream& operator<<(ostream& os, Grid<T>& A) {
+  FOR(i, A.H){
+    if(i) os << "\n";
+    os << A[i];
+  }
+  return os;
+}
+
+template <class T, class U>
+bool chmax(T& a, const U& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T, class U>
+bool chmin(T& a, const U& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+#line 3 "/home/maspy/library/graph/base.hpp"
+
+template <typename T>
+struct Edge {
+  ll frm, to;
+  T cost;
+  ll id;
+  Edge(ll a, ll b, T c, ll d) : frm(a), to(b), cost(c), id(d) {}
+};
+
+template <typename T>
+struct Graph {
+  ll N, M;
+  using edge_t = Edge<T>;
+  vector<edge_t> edges;
+  vector<vector<edge_t>> G;
+  bool directed;
+  Graph(ll N, bool bl = false) : N(N), M(0), G(N), directed(bl) {}
+
+  void add_edge(ll frm, ll to, T cost = 1) {
+    auto e = edge_t(frm, to, cost, M);
+    edges.eb(e);
+    G[frm].eb(e);
+    if (!directed) {
+      auto e_rev = edge_t(to, frm, cost, M);
+      G[to].eb(e_rev);
+    }
+    ++M;
+  }
+
+  void print(bool detail=false) {
+    FOR(v, N) {
+      cout << v << " :";
+      for (auto e : G[v]) {
+        if(detail) cout << " (" << e.frm << "," << e.to << "," << e.cost << "," << e.id << ")";
+        else cout << " " << e.to;
+      }
+      cout << "\n";
+    }
+  }
+
+  vector<edge_t>& operator[](ll v) { return G[v]; }
+};
+#line 4 "main.cpp"
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  auto [N, M] = in2();
+  auto A = vin(N);
+  Graph<ll> G(N);
+  FOR(_, M) {
+    auto [a, b, c] = in3();
+    --a;
+    --b;
+    G.add_edge(a, b, c);
+  }
+
+  /*
+  
+    prique, (-wt, to)
+    prique, (-wt, to)
+  */
+  priority_queue<pi> que_1;
+  priority_queue<pi> que_2;
+  FOR3(v, 1, N) { que_1.push(mp(-A[v], v)); }
+  for (auto&& e : G[0]) {
+    que_2.push(mp(-e.cost, e.to));
+  }
+  ll min_v = A[0];
+  vector<bool> done(N);
+  done[0] = true;
+  const ll INF = 1LL << 60;
+  ll ans = 0;
+  FOR(_, N - 1) {
+    while (!que_1.empty() && done[que_1.top().se]) que_1.pop();
+    while (!que_2.empty() && done[que_2.top().se]) que_2.pop();
+    auto cost_1 = (que_1.empty() ? INF : -que_1.top().fi + min_v);
+    auto cost_2 = (que_2.empty() ? INF : -que_2.top().fi);
+    ll to, cost;
+    if (cost_1 < cost_2) {
+      to = que_1.top().se;
+      cost = cost_1;
+    } else {
+      to = que_2.top().se;
+      cost = cost_2;
+    }
+    done[to] = true;
+    ans += cost;
+    chmin(min_v, A[to]);
+    for (auto&& e : G[to]) {
+      que_2.push(mp(-e.cost, e.to));
+    }
+  }
+  print(ans);
+
+  return 0;
+}
