eded010f3b5404d479c0ffefd256ac20d04e9c72
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 01:47:56 +0530

    egor.lifar

---
+++ egor.lifar/normal/575/D.cpp
@@ -0,0 +1,25 @@
+#include <cstdio>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <map>
+#include <vector>
+#include <queue>
+#include <set>
+
+
+using namespace std;
+
+
+int main() {
+	//freopen("bubble.txt", "w", stdout);
+	cout << 2000 << endl;
+	for (int i = 1; i <= 1000; i++) {
+		cout << i << ' ' << 1 << ' ' << i << ' ' << 2 << endl;
+	}
+	for (int i = 1000; i >= 1; i--) {
+		cout << i << ' ' << 1 << ' ' << i << ' ' << 2 << endl;
+	}
+	return 0;
+}
