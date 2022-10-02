98dbcf3c5e3c7c18fa43b1f6e1ffb780f1094529
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:41:26 +0530

    egor.lifar

---
+++ egor.lifar/normal/1599/F.cpp
@@ -0,0 +1,577 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+using ll = long long;
+using db = long double; // or double, if TL is tight
+using str = string; // yay python! 
+
+using pi = pair<int,int>;
+using pl = pair<ll,ll>;
+using pd = pair<db,db>;
+
+using vi = vector<int>;
+using vb = vector<bool>;
+using vl = vector<ll>;
+using vd = vector<db>;
+using vs = vector<str>;
+using vpi = vector<pi>;
+using vpl = vector<pl>;
+using vpd = vector<pd>;
+
+#define tcT template<class T
+#define tcTU tcT, class U
+// ^ lol this makes everything look weird but I'll try it
+tcT> using V = vector<T>; 
+tcT, size_t SZ> using AR = array<T,SZ>; 
+tcT> using PR = pair<T,T>;
+
+// pairs
+#define mp make_pair
+#define f first
+#define s second
+
+// vectors
+// oops size(x), rbegin(x), rend(x) need C++17
+#define sz(x) int((x).size())
+#define bg(x) begin(x)
+#define all(x) bg(x), end(x)
+#define rall(x) x.rbegin(), x.rend() 
+#define sor(x) sort(all(x)) 
+#define rsz resize
+#define ins insert 
+#define ft front()
+#define bk back()
+#define pb push_back
+#define eb emplace_back 
+#define pf push_front
+#define rtn return
+
+#define lb lower_bound
+#define ub upper_bound
+tcT> int lwb(V<T>& a, const T& b) { return int(lb(all(a),b)-bg(a)); }
+tcT> int upb(V<T>& a, const T& b) { return int(ub(all(a),b)-bg(a)); }
+
+// loops
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define rep(a) F0R(_,a)
+#define each(a,x) for (auto& a: x)
+
+const int MOD = 1e9+7; // 998244353;
+const int MX = 2e5+5;
+const ll BIG = 1e18; // not too close to LLONG_MAX
+const db PI = acos((db)-1);
+const int dx[4]{1,0,-1,0}, dy[4]{0,1,0,-1}; // for every grid problem!!
+mt19937 rng(127); 
+template<class T> using pqg = priority_queue<T,vector<T>,greater<T>>;
+
+// bitwise ops
+// also see https://gcc.gnu.org/onlinedocs/gcc/Other-Builtins.html
+constexpr int pct(int x) { return __builtin_popcount(x); } // # of bits set
+constexpr int bits(int x) { // assert(x >= 0); // make C++11 compatible until USACO updates ...
+    return x == 0 ? 0 : 31-__builtin_clz(x); } // floor(log2(x)) 
+constexpr int p2(int x) { return 1<<x; }
+constexpr int msk2(int x) { return p2(x)-1; }
+
+ll cdiv(ll a, ll b) { return a/b+((a^b)>0&&a%b); } // divide a by b rounded up
+ll fdiv(ll a, ll b) { return a/b-((a^b)<0&&a%b); } // divide a by b rounded down
+
+tcT> bool ckmin(T& a, const T& b) {
+    return b < a ? a = b, 1 : 0; } // set a = min(a,b)
+tcT> bool ckmax(T& a, const T& b) {
+    return a < b ? a = b, 1 : 0; }
+
+tcTU> T fstTrue(T lo, T hi, U f) {
+    hi ++; assert(lo <= hi); // assuming f is increasing
+    while (lo < hi) { // find first index such that f is true 
+        T mid = lo+(hi-lo)/2;
+        f(mid) ? hi = mid : lo = mid+1; 
+    } 
+    return lo;
+}
+tcTU> T lstTrue(T lo, T hi, U f) {
+    lo --; assert(lo <= hi); // assuming f is decreasing
+    while (lo < hi) { // find first index such that f is true 
+        T mid = lo+(hi-lo+1)/2;
+        f(mid) ? lo = mid : hi = mid-1;
+    } 
+    return lo;
+}
+tcT> void remDup(vector<T>& v) { // sort and remove duplicates
+    sort(all(v)); v.erase(unique(all(v)),end(v)); }
+tcTU> void erase(T& t, const U& u) { // don't erase
+    auto it = t.find(u); assert(it != end(t));
+    t.erase(it); } // element that doesn't exist from (multi)set
+
+#define tcTUU tcT, class ...U
+
+inline namespace Helpers {
+    //////////// is_iterable
+    // https://stackoverflow.com/questions/13830158/check-if-a-variable-type-is-iterable
+    // this gets used only when we can call begin() and end() on that type
+    tcT, class = void> struct is_iterable : false_type {};
+    tcT> struct is_iterable<T, void_t<decltype(begin(declval<T>())),
+                                      decltype(end(declval<T>()))
+                                     >
+                           > : true_type {};
+    tcT> constexpr bool is_iterable_v = is_iterable<T>::value;
+
+    //////////// is_readable
+    tcT, class = void> struct is_readable : false_type {};
+    tcT> struct is_readable<T,
+            typename std::enable_if_t<
+                is_same_v<decltype(cin >> declval<T&>()), istream&>
+            >
+        > : true_type {};
+    tcT> constexpr bool is_readable_v = is_readable<T>::value;
+
+    //////////// is_printable
+    // // https://nafe.es/posts/2020-02-29-is-printable/
+    tcT, class = void> struct is_printable : false_type {};
+    tcT> struct is_printable<T,
+            typename std::enable_if_t<
+                is_same_v<decltype(cout << declval<T>()), ostream&>
+            >
+        > : true_type {};
+    tcT> constexpr bool is_printable_v = is_printable<T>::value;
+}
+
+inline namespace Input {
+    tcT> constexpr bool needs_input_v = !is_readable_v<T> && is_iterable_v<T>;
+    tcTUU> void re(T& t, U&... u);
+    tcTU> void re(pair<T,U>& p); // pairs
+
+    // re: read
+    tcT> typename enable_if<is_readable_v<T>,void>::type re(T& x) { cin >> x; } // default
+    tcT> void re(complex<T>& c) { T a,b; re(a,b); c = {a,b}; } // complex
+    tcT> typename enable_if<needs_input_v<T>,void>::type re(T& i); // ex. vectors, arrays
+    tcTU> void re(pair<T,U>& p) { re(p.f,p.s); }
+    tcT> typename enable_if<needs_input_v<T>,void>::type re(T& i) {
+        each(x,i) re(x); }
+    tcTUU> void re(T& t, U&... u) { re(t); re(u...); } // read multiple
+
+    // rv: resize and read vectors
+    void rv(size_t) {}
+    tcTUU> void rv(size_t N, V<T>& t, U&... u);
+    template<class...U> void rv(size_t, size_t N2, U&... u);
+    tcTUU> void rv(size_t N, V<T>& t, U&... u) {
+        t.rsz(N); re(t);
+        rv(N,u...); }
+    template<class...U> void rv(size_t, size_t N2, U&... u) {
+        rv(N2,u...); }
+
+    // dumb shortcuts to read in ints
+    void decrement() {} // subtract one from each
+    tcTUU> void decrement(T& t, U&... u) { --t; decrement(u...); }
+    #define ints(...) int __VA_ARGS__; re(__VA_ARGS__);
+    #define int1(...) ints(__VA_ARGS__); decrement(__VA_ARGS__);
+}
+
+inline namespace ToString {
+    tcT> constexpr bool needs_output_v = !is_printable_v<T> && is_iterable_v<T>;
+
+    // ts: string representation to print
+    tcT> typename enable_if<is_printable_v<T>,str>::type ts(T v) {
+        stringstream ss; ss << fixed << setprecision(15) << v;
+        return ss.str(); } // default
+    tcT> str bit_vec(T t) { // bit vector to string
+        str res = "{"; F0R(i,sz(t)) res += ts(t[i]);
+        res += "}"; return res; }
+    str ts(V<bool> v) { return bit_vec(v); }
+    template<size_t SZ> str ts(bitset<SZ> b) { return bit_vec(b); } // bit vector
+    tcTU> str ts(pair<T,U> p); // pairs
+    tcT> typename enable_if<needs_output_v<T>,str>::type ts(T v); // vectors, arrays
+    tcTU> str ts(pair<T,U> p) { return "("+ts(p.f)+", "+ts(p.s)+")"; }
+    tcT> typename enable_if<is_iterable_v<T>,str>::type ts_sep(T v, str sep) {
+        // convert container to string w/ separator sep
+        bool fst = 1; str res = "";
+        for (const auto& x: v) {
+            if (!fst) res += sep;
+            fst = 0; res += ts(x);
+        }
+        return res;
+    }
+    tcT> typename enable_if<needs_output_v<T>,str>::type ts(T v) {
+        return "{"+ts_sep(v,", ")+"}"; }
+
+    // for nested DS
+    template<int, class T> typename enable_if<!needs_output_v<T>,vs>::type 
+      ts_lev(const T& v) { return {ts(v)}; }
+    template<int lev, class T> typename enable_if<needs_output_v<T>,vs>::type 
+      ts_lev(const T& v) {
+        if (lev == 0 || !sz(v)) return {ts(v)};
+        vs res;
+        for (const auto& t: v) {
+            if (sz(res)) res.bk += ",";
+            vs tmp = ts_lev<lev-1>(t);
+            res.ins(end(res),all(tmp));
+        }
+        F0R(i,sz(res)) {
+            str bef = " "; if (i == 0) bef = "{";
+            res[i] = bef+res[i];
+        }
+        res.bk += "}";
+        return res;
+    }
+}
+
+inline namespace Output {
+    template<class T> void pr_sep(ostream& os, str, const T& t) { os << ts(t); }
+    template<class T, class... U> void pr_sep(ostream& os, str sep, const T& t, const U&... u) {
+        pr_sep(os,sep,t); os << sep; pr_sep(os,sep,u...); }
+    // print w/ no spaces
+    template<class ...T> void pr(const T&... t) { pr_sep(cout,"",t...); } 
+    // print w/ spaces, end with newline
+    void ps() { cout << "\n"; }
+    template<class ...T> void ps(const T&... t) { pr_sep(cout," ",t...); ps(); } 
+    // debug to cerr
+    template<class ...T> void dbg_out(const T&... t) {
+        pr_sep(cerr," | ",t...); cerr << endl; }
+    void loc_info(int line, str names) {
+        cerr << "Line(" << line << ") -> [" << names << "]: "; }
+    template<int lev, class T> void dbgl_out(const T& t) {
+        cerr << "\n\n" << ts_sep(ts_lev<lev>(t),"\n") << "\n" << endl; }
+    #ifdef LOCAL
+        #define dbg(...) loc_info(__LINE__,#__VA_ARGS__), dbg_out(__VA_ARGS__)
+        #define dbgl(lev,x) loc_info(__LINE__,#x), dbgl_out<lev>(x)
+    #else // don't actually submit with this
+        #define dbg(...) 0
+        #define dbgl(lev,x) 0
+    #endif
+
+    const clock_t beg = clock();
+    #define dbg_time() dbg((db)(clock()-beg)/CLOCKS_PER_SEC)
+}
+
+inline namespace FileIO {
+    void setIn(str s)  { freopen(s.c_str(),"r",stdin); }
+    void setOut(str s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(str s = "") {
+        cin.tie(0)->sync_with_stdio(0); // unsync C / C++ I/O streams
+        // cin.exceptions(cin.failbit);
+        // throws exception when do smth illegal
+        // ex. try to read letter into int
+        if (sz(s)) setIn(s+".in"), setOut(s+".out"); // for old USACO
+    }
+}
+
+
+
+/**
+ * Description: modular arithmetic operations 
+ * Source: 
+    * KACTL
+    * https://codeforces.com/blog/entry/63903
+    * https://codeforces.com/contest/1261/submission/65632855 (tourist)
+    * https://codeforces.com/contest/1264/submission/66344993 (ksun)
+    * also see https://github.com/ecnerwala/cp-book/blob/master/src/modnum.hpp (ecnerwal)
+ * Verification: 
+    * https://open.kattis.com/problems/modulararithmetic
+ */
+
+template<int MOD, int RT> struct mint {
+    static const int mod = MOD;
+    static constexpr mint rt() { return RT; } // primitive root for FFT
+    int v; explicit operator int() const { return v; } // explicit -> don't silently convert to int
+    mint() { v = 0; }
+    mint(ll _v) { v = int((-MOD < _v && _v < MOD) ? _v : _v % MOD);
+        if (v < 0) v += MOD; }
+    friend bool operator==(const mint& a, const mint& b) { 
+        return a.v == b.v; }
+    friend bool operator!=(const mint& a, const mint& b) { 
+        return !(a == b); }
+    friend bool operator<(const mint& a, const mint& b) { 
+        return a.v < b.v; }
+    friend void re(mint& a) { ll x; re(x); a = mint(x); }
+    friend str ts(mint a) { return ts(a.v); }
+   
+    mint& operator+=(const mint& m) { 
+        if ((v += m.v) >= MOD) v -= MOD; 
+        return *this; }
+    mint& operator-=(const mint& m) { 
+        if ((v -= m.v) < 0) v += MOD; 
+        return *this; }
+    mint& operator*=(const mint& m) { 
+        v = int((ll)v*m.v%MOD); return *this; }
+    mint& operator/=(const mint& m) { return (*this) *= inv(m); }
+    friend mint pow(mint a, ll p) {
+        mint ans = 1; assert(p >= 0);
+        for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+        return ans; }
+    friend mint inv(const mint& a) { assert(a.v != 0); 
+        return pow(a,MOD-2); }
+        
+    mint operator-() const { return mint(-v); }
+    mint& operator++() { return *this += 1; }
+    mint& operator--() { return *this -= 1; }
+    friend mint operator+(mint a, const mint& b) { return a += b; }
+    friend mint operator-(mint a, const mint& b) { return a -= b; }
+    friend mint operator*(mint a, const mint& b) { return a *= b; }
+    friend mint operator/(mint a, const mint& b) { return a /= b; }
+};
+
+typedef mint<MOD,5> mi; // 5 is primitive root for both common mods
+typedef vector<mi> vmi;
+typedef pair<mi,mi> pmi;
+typedef vector<pmi> vpmi;
+
+vector<vmi> scmb; // small combinations
+void genComb(int SZ) {
+    scmb.assign(SZ,vmi(SZ)); scmb[0][0] = 1;
+    FOR(i,1,SZ) F0R(j,i+1) 
+        scmb[i][j] = scmb[i-1][j]+(j?scmb[i-1][j-1]:0);
+}
+// const int BLOCK_SIZE = 1000005;
+// const int DIFF_SIZE = 45;
+
+/**
+ * Description: pre-compute factorial mod inverses,
+     * assumes $MOD$ is prime and $SZ < MOD$.
+ * Time: O(SZ)
+ * Source: KACTL
+ * Verification: https://dmoj.ca/problem/tle17c4p5
+ */
+
+vmi invs, fac, ifac;
+void genFac(int SZ) {
+    invs.rsz(SZ), fac.rsz(SZ), ifac.rsz(SZ); 
+    invs[1] = fac[0] = ifac[0] = 1; 
+    FOR(i,2,SZ) invs[i] = mi(-(ll)MOD/i*(int)invs[MOD%i]);
+    FOR(i,1,SZ) fac[i] = fac[i-1]*i, ifac[i] = ifac[i-1]*invs[i];
+}
+mi comb(int a, int b) {
+    if (a < b || b < 0) return 0;
+    return fac[a]*ifac[b]*ifac[a-b]; }
+
+
+    /**
+     * Description: range sum queries and point updates for $D$ dimensions
+     * Source: https://codeforces.com/blog/entry/64914
+     * Verification: SPOJ matsum
+     * Usage: \texttt{BIT<int,10,10>} gives 2D BIT
+     * Time: O((\log N)^D)
+     */
+
+    template <class T, int ...Ns> struct BIT {
+        T val = 0; void upd(T v) { val += v; }
+        T query() { return val; }
+    };
+    template <class T, int N, int... Ns> struct BIT<T, N, Ns...> {
+        BIT<T,Ns...> bit[N+1];
+        template<typename... Args> void upd(int pos, Args... args) { assert(pos > 0);
+            for (; pos<=N; pos+=pos&-pos) bit[pos].upd(args...); }
+        template<typename... Args> T sum(int r, Args... args) {
+                if(r < 1) return mi(0);
+            T res=0; for (;r;r-=r&-r) res += bit[r].query(args...); 
+            return res; }
+        template<typename... Args> T query(int l, int r, Args... 
+            args) { return sum(r,args...)-sum(l-1,args...); }
+    }; 
+    template<class T, int N> int get_kth(const BIT<T,N>& bit, T des) { 
+        assert(des > 0);
+        int ind = 0;
+        for (int i = 1<<bits(N); i; i /= 2)
+            if (ind+i <= N && bit.bit[ind+i].val < des)
+                des -= bit.bit[ind += i].val;
+        assert(ind < N); return ind+1;
+    }
+
+
+const int mx = 200005;
+const int POWS = 10;
+
+struct Eff{
+
+int N, Q;
+int A[mx];
+mi actual_sums[mx][POWS+5]; //using 2D sums
+mi predicted_sums[mx][POWS+5]; //using arithmetic
+
+
+int L[mx];
+int R[mx];
+mi D[mx];
+
+map<int, int> m;
+int point_to[mx]{};
+BIT<mi, 200005> bt[POWS+5];
+
+// mi getPowSum(mi a, mi d, mi c, int k){ //a^k + (a+d)^k + (a+2d)^k + ... (a+(c-1)d)^k
+//     mi res = mi(0);
+//     for(int i = 0; i < c.v; i++){
+//         res+=pow(a+d*i, k);
+//     }
+//     return res;
+// }
+
+mi pow_sum[mx][POWS+5];
+
+void initPowSum(){
+    for(int i = 0; i < mx; i++){
+        mi cur = mi(1);
+        for(int j = 0; j <= POWS; j++){
+            pow_sum[i][j]+=cur;
+            cur*=mi(i);
+        }
+    }
+
+    for(int j = 0; j <= POWS; j++){
+        for(int i = 1; i < mx; i++){
+            pow_sum[i][j]+=pow_sum[i-1][j];
+        }
+    }
+}
+
+mi getPowSum(mi a, mi d, mi c, int k){ //a^k + (a+d)^k + (a+2d)^k + ... (a+(c-1)d)^k
+    mi res = mi(0);
+    assert(0 <= c.v-1 && c.v-1 < mx);
+
+    vmi apows = vmi(k+1, mi(1));
+    vmi dpows = vmi(k+1, mi(1));
+
+    for(int i = 1; i <= k; i++){
+        apows[i] = apows[i-1]*a;
+    }
+    for(int i = 1; i <= k; i++){
+        dpows[i] = dpows[i-1]*d;
+    }
+
+    for(int i = 0; i <= k; i++){
+        // if(a.v == 6 && d.v == 2 && c.v == 3 && k == 2){
+        //     dbg(i);
+        //     dbg(pow_sum[c.v-1][i]);
+        // }
+        
+        res+=apows[k-i]*dpows[i]*pow_sum[c.v-1][i]*comb(k, i);
+    }
+
+
+    // if(a.v == 6 && d.v == 2 && c.v == 3 && k == 2){
+    //     dbg(a, d, c, k, res);
+    // }
+    
+
+    return res;
+}
+
+void solve(){
+    genFac(1000005);
+    initPowSum();
+    cin >> N >> Q;
+    for(int i = 1; i <= N; i++){
+        cin >> A[i]; //city values
+    }
+
+
+    for(int i = 1; i <= Q; i++){
+        int _D;
+        cin >> L[i] >> R[i] >> _D;
+        D[i] = mi(_D);
+    }
+
+    for(int i = 1; i <= N; i++){
+        if(!m.count(A[i])){
+            m[A[i]] = 0;
+        }
+        point_to[i] = m[A[i]];
+        m[A[i]] = i;
+    }
+
+    vpi updates; //y, index
+    for(int i = 1; i <= N; i++){
+        updates.pb(mp(point_to[i], i));
+    }
+
+    vpi queries; //y value of query, index of query
+    for(int i = 1; i <= Q; i++){
+        queries.pb(mp(L[i]-1, i));
+    }
+
+    sort(all(updates));
+    sort(all(queries));
+
+
+
+    int updates_ind = 0;
+    for(auto q: queries){
+        while(updates_ind < sz(updates)){
+            if(updates[updates_ind].f <= q.f){
+                int u_ind = updates[updates_ind].s; //array index
+                for(int j = 0; j <= POWS; j++){
+                    bt[j].upd(u_ind, pow(mi(A[u_ind]), j));
+                }
+                
+                updates_ind++;
+            }
+            else{
+                break;
+            }
+        }
+
+        int q_ind = q.s;
+        for(int j = 0; j <= POWS; j++){
+            actual_sums[q_ind][j] = bt[j].query(L[q_ind], R[q_ind]);
+        }
+    }
+
+    for(int i = 1; i <= Q; i++){
+        // for(int j = 0; j <= 10; j++){
+        //     dbg(i, j, actual_sums[i][j]);
+        // }
+
+        
+        //compute a^k + (a+d)^k + (a+2d)^k + ... (a+(c-1)d)^k
+        mi d = D[i]; //difference
+        mi c = actual_sums[i][0]; 
+
+        mi a = (mi(2)*actual_sums[i][1]/c-d*(c-mi(1)))/mi(2); // we need to find this
+        
+        for(int k = 2; k <= POWS; k++){
+            predicted_sums[i][k] = getPowSum(a, d, c, k);
+        }
+    }
+
+    for(int i = 1; i <= Q; i++){
+        bool works = 1;
+        for(int j = 2; j <= POWS; j++){
+            if(actual_sums[i][j] != predicted_sums[i][j]){
+                // dbg(i, j, actual_sums[i][j], predicted_sums[i][j]);
+                works = 0;
+                break;
+            }
+        }
+
+        if(works){
+            ps("Yes");
+        }
+        else{
+            ps("No");
+        }
+    }
+
+
+
+}
+
+
+};
+
+int main() {
+    setIO();
+    Eff e;
+    e.solve();
+    
+
+
+    // you should actually read the stuff at the bottom
+}
+
+/* stuff you should look for
+    * int overflow, array bounds
+    * special cases (n=1?)
+    * do smth instead of nothing and stay organized
+    * WRITE STUFF DOWN
+    * DON'T GET STUCK ON ONE APPROACH
+*/
