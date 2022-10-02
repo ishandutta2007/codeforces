4fd3ba0be0b32d84e7e841139dedf9fb2e372347
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 21:42:27 +0530

    maspy

---
+++ maspy/normal/1324/F.cpp
@@ -0,0 +1,405 @@
+#line 1 "main.cpp"
+#include <bits/stdc++.h>
+#line 3 "/home/maspy/library/my_template.hpp"
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define int ll
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define FOR(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define FOR3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define FOR_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+#define FORIN(x, A) for (auto&& x : A)
+#define all(x) x.begin(), x.end()
+
+#define elif else if
+
+#define popcnt __builtin_popcount
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+#define VEC vector
+
+template <typename T>
+struct Matrix {
+  ll H, W;
+  vector<vector<T>> data;
+  pi shape;
+  Matrix(ll H, ll W) : H(H), W(W), data(H, vector<T>(W)), shape({H,W}) {}
+  void fill(T x) {
+    FOR(i, H) { FOR(j, W) data[i][j] = x; }
+  }
+  vector<T>& operator[](ll i) { return data[i]; }
+  bool operator==(Matrix<T>& other){
+    if(H != other.H || W != other.W) return false;
+    FOR(i, H) FOR(j, W){
+      if(data[i][j] != other[i][j]) return false;
+    }
+    return true;
+  }
+  Matrix rot(){
+    auto B = Matrix(W,H);
+    FOR(i, W){
+      FOR(j, H){
+        B[i][j] = (*this)[j][W-1-i];
+      }
+    }
+    return B;
+  }
+};
+
+
+// [
+ll in() {
+  ll i;
+  cin >> i;
+  return i;
+}
+
+pi in2() {
+  ll a, b;
+  cin >> a >> b;
+  return {a, b};
+}
+
+tuple<ll, ll, ll> in3() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  return {a, b, c};
+}
+
+tuple<ll, ll, ll, ll> in4() {
+  ll a, b, c, d;
+  cin >> a >> b >> c >> d;
+  return {a, b, c, d};
+}
+
+vi vin(int n) {
+  vi A(n);
+  for (auto&& x : A) {
+    cin >> x;
+  }
+  return A;
+}
+
+string sin() {
+  string s;
+  cin >> s;
+  return s;
+}
+
+vi s_to_vi(string S, char first_char='a'){
+  vi A(S.size());
+  FOR(i, S.size()){
+    A[i] = S[i] - first_char;
+  }
+  return A;
+}
+
+Matrix<ll> matrixin(int H, int W){
+  Matrix<ll> A(H, W);
+  FOR(i, H){
+    FOR(j, W){
+      A[i][j] = in();
+    }
+  }
+  return A;
+}
+
+Matrix<char> cmatrixin(int H, int W){
+  Matrix<char> A(H, W);
+  FOR(i, H){
+    string S = sin();
+    FOR(j, W){
+      A[i][j] = S[j];
+    }
+  }
+  return A;
+}
+
+// ]
+
+// [
+template <typename T, typename U>
+ostream& operator<<(ostream& os, pair<T, U>& A) {
+  os << A.fi << " " << A.se;
+  return os;
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+
+
+void print() {
+  cout << "\n";
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+template <typename T>
+string join(vector<T>& v){
+  ostringstream os;
+  for(auto&& x : v){
+    os << x;
+  }
+  return os.str();
+}
+
+void YES() {print("YES");}
+void NO() {print("NO");}
+void Yes() {print("Yes");}
+void No() {print("No");}
+// ]
+
+template <typename T>
+ll sum(T A) {
+  return accumulate(all(A), 0LL);
+}
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+vi bin_count(vi& A, ll size) {
+  vi C(size);
+  for (auto&& x : A) {
+    ++C[x];
+  }
+  return C;
+}
+
+template <typename T>
+vi argsort(vector<T>& A){
+  vi ids(A.size());
+  iota(all(ids), 0);
+  sort(all(ids), [&](ll i, ll j){return mp(A[i],i)<=mp(A[j],j);});
+  return ids;
+}
+
+template <typename T>
+int argmax(vector<T>& A){
+  return max_element(all(A)) - A.begin();
+}
+
+
+template <typename T>
+ostream& operator<<(ostream& os, Matrix<T>& A) {
+  FOR(i, A.H){
+    if(i) os << "\n";
+    os << A[i];
+  }
+  return os;
+}
+
+template <class T, class U>
+bool chmax(T& a, const U& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T, class U>
+bool chmin(T& a, const U& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+#line 3 "/home/maspy/library/graph/base.hpp"
+
+template <typename T>
+struct Edge {
+  int frm, to;
+  T cost;
+  int id;
+  Edge(int a, int b, T c, int d) : frm(a), to(b), cost(c), id(d) {}
+};
+
+template <typename T>
+struct Graph {
+  int N, M;
+  using edge_t = Edge<T>;
+  vector<edge_t> edges;
+  vector<vector<edge_t>> G;
+  bool directed;
+  Graph(int N, bool bl = false) : N(N), M(0), G(N), directed(bl) {}
+
+  void add_edge(int frm, int to, T cost = 1) {
+    auto e = edge_t(frm, to, cost, M);
+    edges.eb(e);
+    G[frm].eb(e);
+    if (!directed) {
+      auto e_rev = edge_t(to, frm, cost, M);
+      G[to].eb(e_rev);
+    }
+    ++M;
+  }
+
+  void print(bool detail=false) {
+    FOR(v, N) {
+      cout << v << " :";
+      for (auto e : G[v]) {
+        if(detail) cout << " (" << e.frm << "," << e.to << "," << e.cost << "," << e.id << ")";
+        else cout << " " << e.to;
+      }
+      cout << "\n";
+    }
+  }
+
+  vector<edge_t>& operator[](int v) { return G[v]; }
+};
+#line 3 "/home/maspy/library/graph/treedfs.hpp"
+
+template <class T>
+struct TreeDfs {
+  T& G;
+  vi V, LID, RID, par, VR, depth;
+  Matrix<ll> ancestors;
+  vector<vi> ch;
+  ll root;
+
+  TreeDfs(T& G, int root = 0) : G(G), V(G.N), LID(G.N), RID(G.N), par(G.N), root(root), ch(G.N), ancestors(20, G.N), depth(G.N) {
+    ll n = 0;
+    auto dfs = [&](auto self, auto v) -> void {
+      LID[v] = n;
+      V[n] = v;
+      ++n;
+      for (auto&& e : G[v]) {
+        if (e.to != par[e.frm]) {
+          ch[e.frm].eb(e.to);
+          par[e.to] = e.frm;
+          depth[e.to] = depth[e.frm] + 1;
+          self(self, e.to);
+        }
+      }
+      RID[v] = n;
+    };
+    par[0] = -1;
+    dfs(dfs, root);
+    VR = V;
+    reverse(all(VR));
+    // ancestors 
+    ancestors[0] = par;
+    FOR3(k, 1, 20){
+      FOR(v, G.N){
+        ll w = ancestors[k-1][v];
+        ancestors[k][v] = (w == -1 ? -1 : ancestors[k-1][w]);
+      }
+    }
+  }
+
+  ll LCA(ll a, ll b){
+    if(depth[a] > depth[b]) swap(a, b);
+    auto n = depth[b] - depth[a];
+    FOR(k, 20){
+      if(n & 1<<k) b = ancestors[k][b];
+    }
+    if(a==b) return a;
+    FOR_R(k, 20){
+      if(ancestors[k][a] != ancestors[k][b]){
+        a = ancestors[k][a];
+        b = ancestors[k][b];
+      }
+    }
+    return par[a];
+  }
+};
+#line 5 "main.cpp"
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+  auto N = in();
+  auto A = vin(N);
+  Graph<ll> G(N);
+  FOR(_, N - 1) {
+    auto [a, b] = in2();
+    G.add_edge(--a, --b);
+  }
+  TreeDfs dfs(G, 0);
+
+  using node_data = ll;
+  using edge_data = node_data;
+  // 
+  edge_data e_unit(0);
+  // 
+  auto f_ev = [&](ll v, edge_data& x) -> node_data { return max(0LL, x) + (A[v] == 1 ? 1 : -1); };
+  // 
+  auto f_ve = [&](ll v, node_data& x) -> edge_data { return max(0LL, x); };
+  // 
+  auto mul_e = [&](edge_data& x, edge_data& y) -> edge_data { return x + y; };
+
+  auto rerooting = [&]() {
+    VEC<node_data> dp1_v(N);
+    VEC<edge_data> dp1_e(N);
+    for (auto&& v : dfs.VR) {
+      auto x = e_unit;
+      for (auto&& e : G[v]) {
+        auto w = e.to;
+        if (dfs.par[w] == v) {
+          x = mul_e(x, dp1_e[w]);
+        }
+      }
+      dp1_v[v] = f_ev(v, x);
+      dp1_e[v] = f_ve(v, dp1_v[v]);
+    }
+
+    VEC<node_data> dp(N);
+    VEC<edge_data> dp2_e(N);
+    dp2_e[0] = e_unit;
+
+    for (auto&& v : dfs.V) {
+      auto& ch = dfs.ch[v];
+      auto n = ch.size();
+      VEC<edge_data> cum_l(n + 1);
+      VEC<edge_data> cum_r(n + 1);
+      cum_l[0] = e_unit;
+      cum_r[n] = e_unit;
+      FOR(i, n) { cum_l[i + 1] = mul_e(cum_l[i], dp1_e[ch[i]]); }
+      FOR_R(i, n) { cum_r[i] = mul_e(cum_r[i + 1], dp1_e[ch[i]]); }
+      FOR(i, n) {
+        auto x = mul_e(cum_l[i], cum_r[i + 1]);
+        x = mul_e(dp2_e[v], x);
+        auto y = f_ev(v, x);
+        dp2_e[ch[i]] = f_ve(v, y);
+      }
+      auto x = dp2_e[v];
+      x = mul_e(x, cum_r[0]);
+      x = f_ev(v, x);
+      dp[v] = x;
+    }
+    return dp;
+  };
+
+  auto dp = rerooting();
+  print(dp);
+
+  return 0;
+}
