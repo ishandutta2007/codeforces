47fc7b913fdbe62441349fc6f678e19272ce04d0
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 23:19:37 +0530

    egor.lifar

---
+++ egor.lifar/normal/1344/A.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+using namespace std;
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; }
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define left left228
+#define right right228
+#define rank rank228
+#define y1 y1228
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define files(FILENAME) read(FILENAME), write(FILENAME)
+#define pb push_back
+#define mp make_pair
+using ll = long long;
+using ld = long double;
+const string FILENAME = "input";
+const int MAXN = 200228;
+
+
+int n;
+int a[MAXN];
+
+
+void solve() {
+	cin >> n;
+	set<int> s;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		int t = (a[i] >= 0 ? a[i] % n: n - abs(a[i] % n)) % n;
+		t += i;
+		t %= n;
+		s.insert(t);
+	}	
+	if (sz(s) == n) {
+		cout << "YES\n";
+	} else {
+		cout << "NO\n";
+	}
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    //read(FILENAME);
+    int t;
+    cin >> t;
+    while (t--) {
+    	solve();
+    }
+}
