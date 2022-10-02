ceaf4f994f12b17b31b273dcb945e86029af48a2
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:10:10 +0530

    maspy

---
+++ maspy/normal/977/C.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+#define int ll
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define FOR(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define FOR3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define FOR_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+#define all(x) x.begin(), x.end()
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+
+template <typename T>
+void print(T x) {
+  cout << x << "\n";
+}
+
+template <typename A, typename B>
+void print(A x, B y, string sep = " ") {
+  cout << x << sep << y << "\n";
+}
+
+template <typename A, typename B, typename C>
+void print(A x, B y, C z, string sep = " ") {
+  cout << x << sep << y << sep << z << "\n";
+}
+
+template <typename T>
+void printseq(T begin, T end, string sep = " ") {
+  if (begin != end) {
+    cout << *(begin++);
+    for (auto it = begin; it != end; it++) {
+      cout << sep << (*it);
+    }
+  }
+  cout << "\n";
+}
+
+template <typename T>
+void printall(T A, string sep = " ") {
+  printseq(all(A), sep);
+}
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+ll read() {
+  ll i;
+  cin >> i;
+  return i;
+}
+
+pi read2() {
+  ll a, b;
+  cin >> a >> b;
+  return {a, b};
+}
+
+tuple<ll, ll, ll> read3() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  return {a, b, c};
+}
+
+vi readvi(int n, int off = 0) {
+  vi v(n);
+  for (auto&& x : v) {
+    x = read() + off;
+  }
+  return v;
+}
+
+string readstr() {
+  string s;
+  cin >> s;
+  return s;
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll N = read();
+  ll K = read();
+  auto A = readvi(N);
+
+  auto cnt = [&](ll x) { return count_if(all(A), [&](ll y) { return y <= x; }); };
+
+  ll ng = 0, ok = 1 << 30;
+  while (ng + 1 < ok) {
+    ll x = (ng + ok) / 2;
+    if (cnt(x) >= K) {
+      ok = x;
+    } else {
+      ng = x;
+    }
+  }
+  if (cnt(ok) == K) {
+    print(ok);
+  } else {
+    print(-1);
+  }
+  return 0;
+}
