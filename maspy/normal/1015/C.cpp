7b993c3b56b51838000f40aa6a52b5fad1eeaaec
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:05:20 +0530

    maspy

---
+++ maspy/normal/1015/C.cpp
@@ -0,0 +1,165 @@
+#line 1 "main.cpp"
+#include <bits/stdc++.h>
+#line 3 "/home/maspy/library/my_template.hpp"
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+#define int ll
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define FOR(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define FOR3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define FOR_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+#define all(x) x.begin(), x.end()
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+
+// [
+ll in() {
+  ll i;
+  cin >> i;
+  return i;
+}
+
+pi in2(int off = 0) {
+  ll a, b;
+  cin >> a >> b;
+  return {a + off, b + off};
+}
+
+tuple<ll, ll, ll> in3() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  return {a, b, c};
+}
+
+vi vin(int n, int off = 0) {
+  vi A(n);
+  for (auto&& x : A) {
+    cin >> x;
+    x += off;
+  }
+  return A;
+}
+
+string sin() {
+  string s;
+  cin >> s;
+  return s;
+}
+// ]
+
+// [
+template <typename T, typename U>
+ostream& operator<<(ostream& os, pair<T, U>& A) {
+  os << A.fi << " " << A.se;
+  return os;
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (int i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+
+void print() {
+  cout << "\n";
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+// ]
+
+template <typename T>
+ll sum(T A) {
+  return accumulate(all(A), 0);
+}
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+vi bin_count(vi& A, ll size) {
+  vi C(size);
+  for (auto&& x : A) {
+    ++C[x];
+  }
+  return C;
+}
+
+template <typename T>
+struct vec2d {
+  ll H, W;
+  vector<vector<T>> data;
+  vec2d(ll H, ll W) : H(H), W(W), data(H, vector<T>(W)) {}
+  void fill(T x) {
+    FOR(i, H) { FOR(j, W) data[i][j] = x; }
+  }
+  void print() { FOR(i, H) print_itr(all(data[i])); }
+  vector<T>& operator[](ll i) { return data[i]; }
+  pi shape() {return mp(H, W);}
+};
+
+template <class T, class U>
+bool chmax(T& a, const U& b) {
+  if (a < b) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+template <class T, class U>
+bool chmin(T& a, const U& b) {
+  if (b < a) {
+    a = b;
+    return 1;
+  }
+  return 0;
+}
+#line 3 "main.cpp"
+
+template <typename T1, typename T2, typename T3>
+void solve() {
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  auto [N, M] = in2();
+  ll now = 0;
+  vi A(N);
+  FOR(i, N) {
+    auto [a, b] = in2();
+    now += a;
+    A[i] = a - b;
+  }
+  sort(all(A));
+  reverse(all(A));
+  A = cumsum(A);
+  auto n = lower_bound(all(A), now - M) - A.begin();
+  if (n == N + 1) n = -1;
+  print(n);
+  return 0;
+}
