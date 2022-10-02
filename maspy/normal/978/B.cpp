2470cfb5626edf262fa8cef2df9d67c23083af5c
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:09:29 +0530

    maspy

---
+++ maspy/normal/978/B.cpp
@@ -0,0 +1,111 @@
+#line 1 "main.cpp"
+#include <bits/stdc++.h>
+#line 3 "/home/maspy/library/my_template.hpp"
+
+using namespace std;
+
+using ll = long long;
+#define int ll
+using pi = pair<int, int>;
+using vi = vector<int>;
+
+#define FOR(i, n) for (int i = 0; (i) < (int)(n); ++(i))
+#define FOR3(i, m, n) for (int i = (m); (i) < (int)(n); ++(i))
+#define FOR_R(i, n) for (int i = (int)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (int i = (int)(n)-1; (i) >= (int)(m); --(i))
+#define all(x) x.begin(), x.end()
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+
+template <typename T>
+void print(T x) {
+  cout << x << "\n";
+}
+
+template <typename A, typename B>
+void print(A x, B y, string sep = " ") {
+  cout << x << sep << y << "\n";
+}
+
+template <typename A, typename B, typename C>
+void print(A x, B y, C z, string sep = " ") {
+  cout << x << sep << y << sep << z << "\n";
+}
+
+template <typename T>
+void print_seq(T begin, T end, string sep = " ") {
+  if (begin != end) {
+    cout << *(begin++);
+    for (auto it = begin; it != end; it++) {
+      cout << sep << (*it);
+    }
+  }
+  cout << "\n";
+}
+
+template <typename T>
+void print_all(T A, string sep = " ") {
+  print_seq(all(A), sep);
+}
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+ll read() {
+  ll i;
+  cin >> i;
+  return i;
+}
+
+pi read2() {
+  ll a, b;
+  cin >> a >> b;
+  return {a, b};
+}
+
+tuple<ll, ll, ll> read3() {
+  ll a, b, c;
+  cin >> a >> b >> c;
+  return {a, b, c};
+}
+
+vi read_vec(int n, int off = 0) {
+  vi A(n);
+  for (auto&& x : A) {
+    cin >> x;
+    x += off;
+  }
+  return A;
+}
+
+string read_str() {
+  string s;
+  cin >> s;
+  return s;
+}
+#line 3 "main.cpp"
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+
+  ll N = read();
+  string S = read_str();
+  vector<char> stack;
+  for (auto& s : S) {
+    if (stack.size() >= 2 && stack[stack.size() - 1] == 'x' && stack[stack.size() - 2] == 'x' && s == 'x')
+      stack.pop_back();
+    stack.eb(s);
+  }
+  print(N - stack.size());
+
+  return 0;
+}
