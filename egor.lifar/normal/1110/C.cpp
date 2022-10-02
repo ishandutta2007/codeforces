0ae98b540b7381dd8ccf84ba21e2dc16373ed690
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 23:53:51 +0530

    egor.lifar

---
+++ egor.lifar/normal/1110/C.cpp
@@ -0,0 +1,135 @@
+
+/*
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+*/
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+#include <array>
+#include <iomanip>
+		
+using namespace std;
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; } 
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
+template<typename T, typename U> inline istream &operator>> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) { if (_v.empty()) { return _out; } _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) { _in >> _i; } return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define left left228
+#define right right228
+#define rank rank228
+#define y1 y1228                                                         
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define files(FILENAME) read(FILENAME), write(FILENAME)
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+#define len(c) sz(c)
+const string FILENAME = "input";
+
+
+
+int gcd(int a, int b) {
+	return (a == 0 ? b: gcd(b % a, a));
+}
+
+
+int ans[33554432];
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	//read(FILENAME);
+	int q;
+	cin >> q;
+	ans[3] = 1;
+ans[7] = 1;
+ans[15] = 5;
+ans[31] = 1;
+ans[63] = 21;
+ans[127] = 1;
+ans[255] = 85;
+ans[511] = 73;
+ans[1023] = 341;
+ans[2047] = 89;
+ans[4095] = 1365;
+ans[8191] = 1;
+ans[16383] = 5461;
+ans[32767] = 4681;
+ans[65535] = 21845;
+ans[131071] = 1;
+ans[262143] = 87381;
+ans[524287] = 1;
+ans[1048575] = 349525;
+ans[2097151] = 299593;
+ans[4194303] = 1398101;
+ans[8388607] = 178481;
+ans[16777215] = 5592405;
+ans[33554431] = 1082401;
+	//q = 1000;
+	for (int i = 0; i < q; i++) {
+		int a;
+		cin >> a;
+	//	a = i + 2;
+		int cnt = 1;
+		while (cnt <= a) {
+			cnt *= 2;
+		}
+		if (a == cnt - 1) {
+			//cout << f << ' ' << a << endl;
+			//assert(ans[a] == f);
+			cout << ans[a] << '\n';
+		} else {
+			//assert(f == cnt - 1);
+			cout << cnt - 1 << '\n';
+		}
+	}
+}
