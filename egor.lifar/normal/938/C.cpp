e6264da2be0508d0f9332b08a4d6f039bd6859a8
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 00:27:27 +0530

    egor.lifar

---
+++ egor.lifar/normal/938/C.cpp
@@ -0,0 +1,110 @@
+/*
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ */
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+    
+     
+     
+using namespace std;
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; } 
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
+template<typename T, typename U> inline istream &operator>> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) { if (_v.empty()) { return _out; } _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) { _in >> _i; } return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define left left228
+#define right right228
+#define next next228
+#define rank rank228
+#define prev prev228
+#define y1 y1228                                                         
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define files(FILENAME) read(FILENAME), write(FILENAME)
+#define pb push_back
+#define x first
+#define y second
+const string FILENAME = "input";
+const int MAXN = 100001;
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ //   read(FILENAME);
+    int tt;
+    cin >> tt;
+    for (int it = 0; it < tt; it++) {
+        int x;
+        cin >> x;
+        if (x == 0) {
+            cout << 1 << ' ' << 1 << '\n';
+            continue;
+        }
+        bool was = false;
+        for (int t = 1; t <= 100000; t++) {
+            int l = 1;
+            int r = t;
+            while (l != r) {
+                int mid = (l + r) >> 1;
+                long long cnt = 1LL * t * t - 1LL * (t / mid) * (t / mid);
+                if (cnt < x) {
+                    l = mid + 1;
+                } else {
+                    r = mid;
+                }
+            }
+            long long cnt = 1LL * t * t - 1LL * (t / l) * (t / l);
+            if (cnt == x) {
+                was = true;
+                cout << t << ' ' << l << '\n';
+                break;
+            }
+        }
+        if (!was) {
+            cout << -1 << '\n';        
+        }
+    }
+}
