93b6a98b1f91a1e12102f7c4e2290ea70cb7bb2c
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 00:29:38 +0530

    egor.lifar

---
+++ egor.lifar/normal/915/E.cpp
@@ -0,0 +1,255 @@
+/*
+  !
+
+
+
+
+
+
+
+
+
+
+*/
+/*
+@@@@@@@@@@@@@@@@PP!!__......::ii@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@NNGG@@MM..............MM$$oo@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@UU--..::00@@II..........GGGG..^^NN@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@UU..........FF@@##,,......IINN....,,WW@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@WW..............--##NNll....++@@''....::NN@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@^^............--::JJ@@@@dd....NNrr......++@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@LL....--rrooqqDDNN@@@@@@@@@@WWcc00JJ........UU@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@EEGGWW@@NNDD@@@@@@@@@@@@@@@@@@@@@@VV......^^@@NN%%AANN@@@@@@@@@@##%%%%qq%%NN@@@@@@@@003333AAwwwwWW@@@@NNwwdd%%%%##@@@@@@DDSSddGGGGGGGGddWW@@@@ddGG@@@@@@@@UUAANN@@@@@@GGAAGGGGmmNN@@@@@@@@UUddGGGGGGGG
+@@DDqqffrr..^^@@@@@@@@@@@@@@@@@@@@@@OO......NN@@''''..22@@@@@@@@WW............EE@@@@@@ll..........GG@@@@PP..........::NN@@::..............ee@@%%....NN@@@@NN....%%@@@@##............&&@@@@WW..........,,
+ff..........QQ@@@@@@@@@@@@@@@@@@@@@@NN....RR@@;;..!!..))@@@@@@@@GG....$$##!!....NN@@@@,,..ff00####@@@@@@))..rrWWdd....77@@OOUUZZ....oo##AA@@@@JJ..!!@@@@@@AA....NN@@@@55..''88##--..!!@@@@AA....GG88GGWW
+::........oo@@@@@@@@@@@@@@@@@@@@@@@@@@--==@@EE..!!qq..!!@@@@@@@@cc..//@@@@oo....NN@@MM....00@@@@@@@@@@@@,,..$$@@@@''..JJ@@@@@@OO..''@@@@@@@@@@!!..ss@@@@@@TT..;;@@@@@@ll..II@@@@rr..;;@@@@ll..rr@@@@@@@@
+--......~~@@@@@@@@@@@@@@@@@@@@@@@@@@@@FF@@NN....NN@@..''NN@@@@@@,,..JJNNEE....++@@@@44....,,::::00@@@@MM....ooww;;..!!NN@@@@@@cc..rr@@@@@@@@NN....RR@@@@@@::..FF@@@@@@__..))SS//..,,QQ@@@@::....!!::11@@
+........DD@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@;;..~~NN##....MM@@@@00....--......++NN@@@@ll....,,''::NN@@@@ZZ..'',,....nn@@@@@@@@@@::..**@@@@@@@@%%....NN@@@@00....##@@@@DD....::....((@@@@@@WW....''''..JJ@@
+......SSUU))@@@@@@@@@@@@@@@@@@@@@@@@@@@@oo........''....GG@@@@SS....wwAAqqWW@@@@@@@@,,..SS@@@@@@@@@@@@cc..))NN!!..ii@@@@@@@@NN....UU@@@@@@@@aa..--@@@@@@SS....@@@@@@VV..,,NN))..^^@@@@@@qq..''@@@@@@@@@@
+''..;;@@,,''@@@@@@@@@@@@@@@@@@@@@@@@@@QQ....11PP**FF,,..oo@@@@((..==@@@@@@@@@@@@@@MM....55NNMMWW@@@@@@__..55@@SS....NN@@@@@@dd....NN@@@@@@@@OO....oo00dd''..ss@@@@@@//..((@@00....##@@@@cc..!!NNNN00@@@@
+!!..WWPP....NN@@@@@@@@@@@@@@@@@@@@@@@@::..;;@@@@@@@@//..++@@@@''..**@@@@@@@@@@@@@@ee..........!!@@@@##....QQ@@00....GG@@@@@@((..^^@@@@@@@@@@@@ii..........((@@@@@@NN....44@@@@''..LL@@@@__..........AA@@
+CCee00......OO@@@@@@@@@@@@@@@@@@@@@@NNGGqq@@@@@@@@@@0055UU@@@@RRAANN@@@@@@@@@@@@@@MMaawwaaaaFFMM@@@@NNPPdd@@@@@@RRVV00@@@@@@UUZZDD@@@@@@@@@@@@@@UU2211oo##@@@@@@@@@@AA55NN@@@@88PP##@@@@qqZZeeFFoo55@@@@
+@@NN::......aa@@@@@@@@@@@@@@@@@@@@@@@@@@@@QQ**ee@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NN@@@@@@@@@@@@@@NN@@@@@@@@@@NNNN@@@@@@@@@@@@@@@@@@@@NNNNNN@@@@@@@@@@@@@@@@@@@@@@NNNN@@@@@@@@@@@@@@@@NNNN@@@@@@@@@@@@@@@@
+@@EE........ssUU^^WW@@@@@@@@@@NNmmee((::......aa@@@@NN22NN@@@@@@@@WWccNN@@@@@@VVddooqq@@@@@@SSddAA%%@@@@@@ooqqZZdd@@@@@@@@LLOO@@@@@@GGccFFNN@@@@OOVV$$PP@@@@@@5544AAFF@@@@QQAA@@@@&&LLGGOO@@@@%%55dd%%@@
+@@NN........;;WW....ww@@@@((................;;@@@@@@@@LL@@@@@@@@@@aa~~55@@@@@@**%%CC22@@@@OO**@@@@IIWW@@NNssUUoooo@@@@@@%%//11@@@@@@@@ee00@@@@@@ff@@@@EESS@@@@**GGRRiiNN@@##aa@@@@qqLLMMWW@@@@GGFF&&UU@@
+@@@@UU......--NN!!....++NNOO''..............WW@@@@@@@@))00@@@@@@00//ddccMM@@@@LLddSS**@@@@8822@@@@ffNN@@NNII##PPaa@@@@@@//AAiiRR@@@@@@CC##@@@@@@iiNN@@ooRR@@@@22ddRRff@@@@##22@@@@3377##MM@@@@OODDSSii@@
+@@@@@@qq......MM((......--UU@@**..........##@@@@@@@@@@FFeemm@@@@##%%@@UU88@@@@33GGGGRR@@@@@@44qqwwRR@@@@@@OO@@@@GG@@@@WWEE@@WWdd@@@@@@%%NN@@@@@@OOPP%%EE@@@@@@$$@@@@RRNN@@NNRR@@@@##ZZmm$$@@@@UUddmmSS@@
+@@@@@@@@QQ::..qqPP..........11@@MM__..::00@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@NNNN@@@@
+@@@@@@@@@@@@**GG%%..............DDNNGG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+@@@@@@@@@@@@@@@@NNcc::......::==GG@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+*/
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+    
+     
+     
+using namespace std;
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; }
+/** Interface */
+     
+inline int readChar();
+template <class T = int> inline T readInt(); 
+template <class T> inline void writeInt( T x, char end = 0 );
+inline void writeChar( int x ); 
+inline void writeWord( const char *s );
+     
+/** Read */
+     
+static const int buf_size = 4096;
+     
+inline int getChar() {
+    static char buf[buf_size];
+    static int len = 0, pos = 0;
+    if (pos == len) {
+        pos = 0, len = fread(buf, 1, buf_size, stdin);
+    }
+    if (pos == len) {
+        return -1;
+    }
+    return buf[pos++];
+}
+     
+inline int readChar() {
+    int c = getChar();
+    while (c <= 32) {
+        c = getChar();
+    }
+    return c;
+}
+     
+template <class T>
+inline T readInt() {
+    int s = 1, c = readChar();
+    T x = 0;
+    if (c == '-')
+        s = -1, c = getChar();
+    while ('0' <= c && c <= '9')
+        x = x * 10 + c - '0', c = getChar();
+    return s == 1 ? x : -x;
+}
+     
+/** Write */
+     
+static int write_pos = 0;
+static char write_buf[buf_size];
+     
+inline void writeChar( int x ) {
+    if (write_pos == buf_size)
+        fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
+    write_buf[write_pos++] = x;
+}
+     
+template <class T> 
+inline void writeInt( T x, char end ) {
+    if (x < 0)
+        writeChar('-'), x = -x;
+     
+    char s[24];
+    int n = 0;
+    while (x || !n)
+        s[n++] = '0' + x % 10, x /= 10;
+    while (n--)
+        writeChar(s[n]);
+    if (end)
+        writeChar(end);
+}
+     
+inline void writeWord( const char *s ) {     while (*s)
+writeChar(*s++); }
+     
+struct Flusher {
+    ~Flusher() {
+        if (write_pos)
+            fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
+    }
+} flusher;   
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define left left228
+#define right right228
+#define next next228
+#define rank rank228
+#define prev prev228
+#define y1 y1228                                                         
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define files(FILENAME) read(FILENAME), write(FILENAME)
+#define pb push_back
+const string FILENAME = "input";
+const int MAXN = 1111111;
+
+
+int n, q;
+int mod[MAXN * 4];
+int d[MAXN * 4];
+vector<int> vx;
+
+
+void build(int v, int vl, int vr) {
+    if (vl + 1 == vr) {
+        mod[v] = 0;
+        d[v] = vx[vl + 1] - vx[vl];
+        return;
+    }
+    int m = (vl + vr) >> 1;
+    mod[v] = 0;
+    build(v * 2 + 1, vl, m);
+    build(v * 2 + 2, m, vr);
+    d[v] = vx[vr] - vx[vl];
+}
+
+
+void push(int v, int vl, int vr) {
+    if (!mod[v]) {
+        return;
+    }
+    int m = (vl + vr) >> 1;
+    if (mod[v] == 1) {
+        mod[v * 2 + 1] = 1;
+        d[v * 2 + 1] = 0;
+        mod[v * 2 + 2] = 1;
+        d[v * 2 + 2] = 0;
+        mod[v] = 0;
+    } else {
+        mod[v * 2 + 1] = 2;
+        d[v * 2 + 1] = vx[m] - vx[vl];
+        mod[v * 2 + 2] = 2;
+        d[v * 2 + 2] = vx[vr] - vx[m];
+        mod[v] = 0;
+    }
+}
+
+
+void add(int v, int vl, int vr, int l, int r, int k) {
+    if (r <= vl || vr <= l) {
+        return;
+    }
+    if (l <= vl && vr <= r) {
+        mod[v] = k;
+        if (k == 1) {
+            d[v] = 0;
+        } else {
+            d[v] = vx[vr] - vx[vl];
+        }
+        return;
+    }
+    push(v, vl, vr);
+    int m = (vl + vr) >> 1;
+    add(v * 2 + 1, vl, m, l, r, k);
+    add(v * 2 + 2, m, vr, l, r, k);
+    d[v] = d[v * 2 + 1] + d[v * 2 + 2];
+}
+
+
+int lq[MAXN];
+int rq[MAXN];
+int k[MAXN];
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    //read(FILENAME);
+    cin >> n >> q;
+    for (int i = 0; i < q; i++) {
+        cin >> lq[i] >> rq[i] >> k[i];
+        vx.push_back(lq[i]);
+        vx.push_back(rq[i] + 1);
+    }
+    vx.push_back(1);
+    vx.push_back(n + 1);
+    sort(vx.begin(), vx.end());
+    sort(all(vx));
+    vx.resize(distance(vx.begin(), unique(all(vx))));
+    int sz = sz(vx) - 1;
+    build(0, 0, sz);
+    for (int i = 0; i < q; i++) {
+        lq[i] = lower_bound(all(vx), lq[i]) - vx.begin();
+        rq[i] = lower_bound(all(vx), rq[i] + 1) - vx.begin();
+        add(0, 0, sz, lq[i], rq[i], k[i]);
+        cout << d[0] << '\n';
+    }
+    return 0;
+}
