05d736e0de04faddef731852ee54df8655b927c8
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:54:17 +0530

    egor.lifar

---
+++ egor.lifar/normal/1508/B.cpp
@@ -0,0 +1,154 @@
+/*
+KAMUI!
+ 
+ 
+
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+*/
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+ 
+using namespace std;
+template<typename T1, typename T2> inline void chkmin(T1 &a, T2 b) {if (a > b) a = b;}
+template<typename T1, typename T2> inline void chkmax(T1 &a, T2 b) {if (a < b) a = b;}
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (int)(c).size()
+#define left left228
+#define right right228
+#define y1 y1228
+#define mp make_pair
+#define pb push_back
+#define y2 y2228
+#define rank rank228
+using ll = long long;
+using ld = long double; 
+const string FILENAME = "input";
+const int Mod = 1000000007;
+
+
+int sum(int a, int b) {
+    return (a + b >= Mod ? a + b - Mod: a + b);
+}
+
+
+int mul(int a, int b) {
+    return ((ll)a * b) % Mod;
+}
+
+
+int powm(int a, int b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) {
+            res = mul(res, a);
+        }
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int inv(int a) {
+    return powm(a, Mod - 2);
+}
+
+
+
+const int MAXN = 100228;
+
+
+int n;
+ll k;
+ll dp[MAXN];
+
+
+void solve() {
+    cin >> n >> k;
+    dp[0] = 1;
+    dp[1] = 1;
+    dp[2] = 2;
+    ll sum = 4;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = sum;
+        chkmin(dp[i], k + 1);
+        sum += dp[i];
+        chkmin(sum, k + 1);
+    }
+    if (k > dp[n]) {
+        cout << -1 << '\n';
+        return;
+    }
+    int cur = 1;
+    while (cur <= n) { 
+        int t = n - cur;
+        int cur1 = cur;
+        while (k > dp[t]) {
+            k -= dp[t];
+            cur1++;
+            t--;
+        }
+        for (int i = cur1; i >= cur; i--) {
+            cout << i << ' ';
+        }
+        cur = cur1 + 1;
+    }
+    cout << '\n';
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   // read(FILENAME);
+    int t;
+    cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
