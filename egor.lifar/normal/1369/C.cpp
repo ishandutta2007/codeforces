a4f2d8cbfca122b00f5d2e06b17416cade29e957
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 23:15:24 +0530

    egor.lifar

---
+++ egor.lifar/normal/1369/C.cpp
@@ -0,0 +1,121 @@
+ /*
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ */
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+ 
+ 
+using namespace std;
+template<typename T1, typename T2> inline void chkmin(T1 &a, T2 b) {if (a > b) a = b;}
+template<typename T1, typename T2> inline void chkmax(T1 &a, T2 b) {if (a < b) a = b;}
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (int)(c).size()
+#define left left228
+#define right right228
+#define y1 y1228
+#define mp make_pair
+#define pb push_back
+#define y2 y2228
+#define rank rank228
+using ll = long long;
+using ld = long double;
+const string FILENAME = "input";
+const int MAXN = 200228;
+ 
+ 
+int n, k;
+int a[MAXN];
+int w[MAXN];
+ 
+ 
+void solve() {
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+	}
+	sort(a, a + n);
+	for (int i = 0; i < k; i++) {
+		cin >> w[i];
+		w[i]--;
+	}
+	sort(w, w + k);
+	ll ans = 0;
+	int it = 0;
+	for (int i = n - 1; i >= n - k; i--) {
+		ans += a[i];
+		if (w[it] == 0) {
+			ans += a[i];
+		}
+		it++;
+	}
+	int uk = n - k - 1;;
+	for (int i = 0; i < k; i++) {
+		if (!w[i]) {
+			continue;
+		}
+		
+		for (int j = 0; j < w[i] - 1; j++) {
+			uk--;
+		}
+		ans += a[uk];
+		uk--;
+	}
+	cout << ans << '\n';
+}
+ 
+ 
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+ //	read(FILENAME);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;	 	
+}
