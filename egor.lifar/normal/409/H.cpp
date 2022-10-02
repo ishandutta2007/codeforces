a85baab884f5ebe1dda5e75fe2b2c5f4573c5e09
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 02:44:27 +0530

    egor.lifar

---
+++ egor.lifar/normal/409/H.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <string.h>
+#include <sstream>
+#include <queue>
+#include <map>
+#include <set>
+#include <assert.h>
+
+
+using namespace std;
+
+
+int a, b;
+
+////////////
+int main(){
+	cin >> a >> b;
+	cout << a + b << endl;
+    return 0;   
+}
