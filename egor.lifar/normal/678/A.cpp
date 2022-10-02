9d9dd3189df32b44185c1a561313d14f0a36354a
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 01:28:57 +0530

    egor.lifar

---
+++ egor.lifar/normal/678/A.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <cassert>
+
+
+
+using namespace std;
+
+
+int n, k;
+
+
+int main() {
+    cin >> n >> k;
+    int t = (n + k) / k * k;
+    cout << t << endl;
+    return 0;   
+}
