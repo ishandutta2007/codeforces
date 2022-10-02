607b755992d7ba7d71aad702605d17f27b1215eb
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 00:59:45 +0530

    egor.lifar

---
+++ egor.lifar/normal/778/B.cpp
@@ -0,0 +1,205 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+ 
+  
+  
+using namespace std;
+  
+  
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; }
+/** Interface */
+  
+inline int readChar();
+template <class T = int> inline T readInt(); 
+template <class T> inline void writeInt( T x, char end = 0 );
+inline void writeChar( int x ); 
+inline void writeWord( const char *s );
+  
+/** Read */
+  
+static const int buf_size = 4096;
+  
+inline int getChar() {
+    static char buf[buf_size];
+    static int len = 0, pos = 0;
+    if (pos == len) {
+        pos = 0, len = fread(buf, 1, buf_size, stdin);
+    }
+    if (pos == len) {
+        return -1;
+    }
+    return buf[pos++];
+}
+  
+inline int readChar() {
+    int c = getChar();
+    while (c <= 32) {
+        c = getChar();
+    }
+    return c;
+}
+  
+template <class T>
+inline T readInt() {
+    int s = 1, c = readChar();
+    T x = 0;
+    if (c == '-')
+        s = -1, c = getChar();
+    while ('0' <= c && c <= '9')
+        x = x * 10 + c - '0', c = getChar();
+    return s == 1 ? x : -x;
+}
+  
+/** Write */
+  
+static int write_pos = 0;
+static char write_buf[buf_size];
+  
+inline void writeChar( int x ) {
+    if (write_pos == buf_size)
+        fwrite(write_buf, 1, buf_size, stdout), write_pos = 0;
+    write_buf[write_pos++] = x;
+}
+  
+template <class T> 
+inline void writeInt( T x, char end ) {
+    if (x < 0)
+        writeChar('-'), x = -x;
+  
+    char s[24];
+    int n = 0;
+    while (x || !n)
+        s[n++] = '0' + x % 10, x /= 10;
+    while (n--)
+        writeChar(s[n]);
+    if (end)
+        writeChar(end);
+}
+  
+inline void writeWord( const char *s ) {     while (*s)
+writeChar(*s++); }
+  
+struct Flusher {
+    ~Flusher() {
+        if (write_pos)
+            fwrite(write_buf, 1, write_pos, stdout), write_pos = 0;
+    }
+} flusher;
+
+
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+const int MAXN = 5001;
+
+
+int n, m;
+string name[MAXN];
+int t[MAXN];
+string a[MAXN], b[MAXN];
+map<string, int> where;
+int ind[MAXN], ind1[MAXN];
+int z[MAXN];
+
+
+inline bool bits(string &a) {
+    if (sz(a) != m) {
+        return 0;
+    }
+    for (auto c: a) {
+        if (c < '0' || c > '1') {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> name[i];
+        where[name[i]] = i;
+        string tr;
+        cin >> tr;
+        cin >> a[i];
+        if (bits(a[i])) {
+            t[i] = 0;
+            continue;
+        }
+        string oper;
+        cin >> oper;
+        if (oper == "OR") {
+            t[i] = 1;
+        } else {
+            if (oper == "AND") {
+                t[i] = 2;
+            } else {
+                t[i] = 3;
+            }
+        }
+        cin >> b[i];
+        if (a[i] == "?") {
+            ind[i] = -1;
+        } else {
+            ind[i] = where[a[i]];
+        }
+        if (b[i] == "?") {
+            ind1[i] = -1;
+        } else {
+            ind1[i] = where[b[i]];
+        }
+    }    
+    string ans1, ans2;
+    for (int i = 0; i < m; i++) {
+        vector<pair<int, int> > v;
+        for (int s = 0; s < 2; s++) {
+            int sum = 0;
+            for (int j = 0; j < n; j++) {
+                if (!t[j]) {
+                    sum += a[j][i] - '0';
+                    z[j] = a[j][i] - '0';
+                } else {
+                    int f = (ind[j] == -1 ? s: z[ind[j]]);
+                    int f1 = (ind1[j] == -1 ? s: z[ind1[j]]);
+                    if (t[j] == 1) {
+                        sum += f | f1;
+                        z[j] = f | f1;
+                    } else {
+                        if (t[j] == 2) {
+                            sum += f & f1;
+                            z[j] = f & f1;
+                        } else {
+                            sum += f ^ f1;
+                            z[j] = f ^ f1;
+                        }
+                    }
+                }
+            }
+            v.push_back(make_pair(sum, s));
+        }
+        sort(all(v));
+        ans1 += v[0].second + '0';
+        ans2 += (v[1].first == v[0].first ? v[0].second + '0': v[1].second + '0');
+    }
+    cout << ans1 << endl << ans2 << endl;
+    return 0;
+}
