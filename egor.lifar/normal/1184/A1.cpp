f960c9d0df4e56a5ddfc33a8a68e38196d10b32f
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 23:42:45 +0530

    egor.lifar

---
+++ egor.lifar/normal/1184/A1.cpp
@@ -0,0 +1,75 @@
+/*
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+ 
+ 
+using namespace std;
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; }
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define left left228
+#define right right228
+#define next next228
+#define rank rank228
+#define prev prev228
+#define y1 y1228
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define files(FILENAME) read(FILENAME), write(FILENAME)
+#define pb push_back
+const string FILENAME = "input";
+
+
+
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	//read(FILENAME);
+	long long r;
+	cin >> r;
+	if (r & 1 && r >= 5) {
+		cout << 1 << ' ' << (r - 2) / 2 << '\n';
+	} else {
+		cout << "NO\n";
+	}
+	return 0;
+}
