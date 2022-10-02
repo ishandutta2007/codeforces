fb80882133fe2f7e67a2e37348325cf3d6adaacb
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 00:01:55 +0530

    egor.lifar

---
+++ egor.lifar/normal/1043/E.cpp
@@ -0,0 +1,123 @@
+/*
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+ */
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+     
+      
+using namespace std;
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; } 
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
+template<typename T, typename U> inline istream &operator>> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) { if (_v.empty()) { return _out; } _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) { _in >> _i; } return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+#define sz(c) (int)(c).size()
+#define all(c) (c).begin(), (c).end()
+#define rall(c) (c).rbegin(), (c).rend()
+#define left left228
+#define right right228
+#define next next228
+#define rank rank228
+#define prev prev228
+#define y1 y1228                                                         
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define files(FILENAME) read(FILENAME), write(FILENAME)
+#define pb push_back
+const string FILENAME = "input";
+const int MAXN = 300228;
+
+
+int n, m;
+int x[MAXN], y[MAXN];
+vector<int> g[MAXN];
+long long sum[MAXN];
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);    
+   // read(FILENAME);
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> y[i];
+    }
+    for (int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        a--, b--;
+        g[a].pb(b);
+        g[b].pb(a);
+    }
+   // x[i] + y[j] <= y[i] + x[j];
+   // x[i] - y[i] < x[j] - y[j];
+    vector<pair<int, int> > st;
+    for (int i = 0; i < n; i++) {
+        st.pb({x[i] - y[i], i});
+    }
+    sort(all(st));
+    long long cur = 0;
+    for (int id = 0; id < n; id++) {
+        int i = st[id].second;
+        sum[i] += 1LL * y[i] * id;
+        sum[i] += cur;
+        //y[i] 
+        cur += x[i];
+    }
+    cur = 0;
+    for (int id = n - 1; id >= 0; id-- ){
+        int i = st[id].second;
+        sum[i] += 1LL * x[i] * (n - id - 1);
+        sum[i] += cur;
+        cur += y[i];
+    }
+    for (int i= 0 ; i < n; i++) {
+        for (auto j: g[i]) {
+            sum[i] -= min(x[i] + y[j], x[j] + y[i]);
+        }
+        cout << sum[i] << ' ';
+    }
+    cout << '\n';
+    return 0;
+}
