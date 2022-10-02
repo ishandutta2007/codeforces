cbecb071a13d2c189909947ae35b35c451c568ff
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 22:49:18 +0530

    egor.lifar

---
+++ egor.lifar/normal/1553/B.cpp
@@ -0,0 +1,139 @@
+/*
+KAMUI!
+ 
+ 
+
+ 
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+*/
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+ 
+using namespace std;
+template<typename T1, typename T2> inline void chkmin(T1 &a, T2 b) {if (a > b) a = b;}
+template<typename T1, typename T2> inline void chkmax(T1 &a, T2 b) {if (a < b) a = b;}
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (int)(c).size()
+#define left left228
+#define right right228
+#define y1 y1228
+#define mp make_pair
+#define pb push_back
+#define y2 y2228
+#define rank rank228
+using ll = long long;
+using ld = long double; 
+const string FILENAME = "input";
+const int Mod = 1000000007;
+
+
+int sum(int a, int b) {
+    return (a + b >= Mod ? a + b - Mod: a + b);
+}
+
+
+int mul(int a, int b) {
+    return ((ll)a * b) % Mod;
+}
+
+
+int powm(int a, int b) {
+    int res = 1;
+    while (b) {
+        if (b & 1) {
+            res = mul(res, a);
+        }
+        a = mul(a, a);
+        b >>= 1;
+    }
+    return res;
+}
+
+
+int inv(int a) {
+    return powm(a, Mod - 2);
+}
+
+
+
+void solve() {
+    string s, t;
+    cin >> s >> t;
+    for (int i = 0; i < sz(s); i++) {
+        string cur;
+        for (int j = i; j < sz(s); j++) {
+            cur += s[j];
+            string cur1 = cur;
+            if (sz(cur1) < sz(t)) {
+                for (int k = j - 1; k >= 0; k--) {
+                    cur1 += s[k];
+                    if (sz(cur1) == sz(t)) {
+                        break;
+                    }
+                }
+            }
+            if (sz(cur1) == sz(t) && cur1 == t) {
+                cout << "YES\n";
+                return;
+            }
+        }
+    }
+    cout << "NO\n";
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+   // read(FILENAME);
+    int t;
+    cin >> t;
+   // t = 1;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
