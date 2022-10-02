5e1683a400ac778c888c9462347d4d8985d25c17
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 04:15:13 +0530

    majk

---
+++ majk/normal/340/E.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+typedef long long ll;
+
+template <unsigned int N> class Field {
+    typedef unsigned int ui;
+    typedef unsigned long long ull;
+	inline ui pow(ui a, ui p){ui r=1,e=a;while(p){if(p&1){r=((ull)r*e)%N;}e=((ull)e*e)%N;p>>=1;}return r;}
+	/*extended GCD(slow):ll t=0,nt=1,r=N,nr=a;while(nr){ll q=r/nr;t-=q*nt;swap(t,nt);r-=q*nr;swap(r,nr);}assert(r<=1);return(t<0)?t+N:t;*/
+	inline ui inv(ui a){return pow(a,N-2);}
+public:
+    inline Field(int x = 0) : v(x) {}
+	inline Field<N> pow(int p){return (*this)^p; }
+	inline Field<N> operator^(int p){return {(int)pow(v,(ui)p)};}
+    inline Field<N>&operator+=(const Field<N>&o) {if ((ll)v+o.v >= N) v += o.v - N; else v += o.v; return *this; }
+    inline Field<N>&operator-=(const Field<N>&o) {if (v<o.v) v -= o.v-N; else v-=o.v; return *this; }
+    inline Field<N>&operator*=(const Field<N>&o) {v=(ull)v*o.v % N; return *this; }
+    inline Field<N>&operator/=(const Field<N>&o) { return *this*=inv(o.v); }
+    inline Field<N> operator+(const Field<N>&o) const {Field<N>r{*this};return r+=o;}
+    inline Field<N> operator-(const Field<N>&o) const {Field<N>r{*this};return r-=o;}
+    inline Field<N> operator*(const Field<N>&o) const {Field<N>r{*this};return r*=o;}
+    inline Field<N> operator/(const Field<N>&o) const {Field<N>r{*this};return r/=o;}
+    inline Field<N> operator-() {if(v) return {(int)(N-v)}; else return {0};};
+    inline Field<N>& operator++() { ++v; if (v==N) v=0; return *this; }
+    inline Field<N> operator++(int) { Field<N>r{*this}; ++*this; return r; }
+    inline Field<N>& operator--() { --v; if (v==-1) v=N-1; return *this; }
+    inline Field<N> operator--(int) { Field<N>r{*this}; --*this; return r; }
+    inline bool operator==(const Field<N>&o) const { return o.v==v; }
+	inline bool operator!=(const Field<N>&o) const { return o.v!=v; }
+	inline explicit operator ui() const { return v; }
+	inline static vector<Field<N>>fact(int t){vector<Field<N>>F(t+1,1);for(int i=2;i<=t;++i){F[i]=F[i-1]*i;}return F;}
+	inline static vector<Field<N>>invfact(int t){vector<Field<N>>F(t+1,1);Field<N> X{1};for(int i=2;i<=t;++i){X=X*i;}F[t]=1/X;for(int i=t-1;i>=2;--i){F[i]=F[i+1]*(i+1);}return F;}
+private: ui v;
+};
+
+template<unsigned int N>istream &operator>>(std::istream&is,Field<N>&f){unsigned int v;is>>v;f=v;return is;}
+template<unsigned int N>ostream &operator<<(std::ostream&os,const Field<N>&f){return os<<(unsigned int)f;}
+template<unsigned int N>Field<N> operator+(int i,const Field<N>&f){return Field<N>(i)+f;}
+template<unsigned int N>Field<N> operator-(int i,const Field<N>&f){return Field<N>(i)-f;}
+template<unsigned int N>Field<N> operator*(int i,const Field<N>&f){return Field<N>(i)*f;}
+template<unsigned int N>Field<N> operator/(int i,const Field<N>&f){return Field<N>(i)/f;}
+typedef Field<1000000007> FieldMod;
+
+int main() {
+    int N; cin >> N;
+    auto F = FieldMod::fact(N);
+    auto I = FieldMod::invfact(N);
+    vector<int> A(N), B(N, -1);
+    for (int &a:A) cin >> a;
+    for (int i = 0; i < N; ++i) {
+        if (A[i] != -1) B[A[i]-1] = i;
+    }
+    int x = 0, y = 0;
+    for (int i = 0; i < N; ++i) {
+        if (B[i] == -1 && A[i] == -1) x++;
+        if (B[i] == -1) y++;
+    }
+
+    FieldMod ans = 0;
+    for (int i = 0; i <= x; ++i) {
+        FieldMod cur = F[x] * I[i] * I[x-i] * F[y-i];
+        ans += i%2?-cur:cur;
+    }
+    cout << ans << endl;
+}
