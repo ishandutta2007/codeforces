e666dd260e02bc2893b0d827c9d3a83397df0716
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 00:18:01 +0530

    egor.lifar

---
+++ egor.lifar/normal/925/B.cpp
@@ -0,0 +1,119 @@
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <deque>
+#include <cassert>
+#include <iomanip>      
+#define pb push_back
+#define x first
+#define y second
+#define mp make_pair
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+using namespace std;
+  
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; } 
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
+template<typename T, typename U> inline istream &operator>> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) { if (_v.empty()) { return _out; } _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) { _in >> _i; } return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+  
+typedef long long base;
+typedef pair<base, base> point;
+
+const string FILENAME = "input";
+
+const double PI = acos(-1);
+
+int n;
+long long A, B;
+vector<int> v, res;
+
+bool cmp(int i, int j) {
+	return v[i] < v[j];
+}
+
+int main() {
+	srand(time(0));
+	//read(FILENAME);
+	cin >> n >> A >> B;
+	v.resize(n);
+	res.resize(n);
+	cin >> v;
+	for (int i = 0; i < n; ++i)
+		res[i] = i;
+	sort(res.begin(), res.end(), cmp);
+	for (int t = 0; t < 2; ++t) {
+		for (int i = 0; i < n; ++i) {
+			int need = (A + v[res[i]] - 1) / v[res[i]];
+			if (i + need >= n) continue;
+			long long C = 1LL * v[res[i + need]] * (n - i - need);
+			if (C >= B) {
+				vector<int> ans[2];
+				for (int j = i; j < i + need; ++j) {
+					ans[0].pb(res[j] + 1);
+				}	
+				for (int j = i + need; j < n; ++j) {
+					ans[1].pb(res[j] + 1);
+				}
+				if (t) swap(ans[0], ans[1]);
+				cout << "Yes" << endl;
+				cout << ans[0].size() << ' ' << ans[1].size() << '\n';
+				cout << ans[0] << '\n' << ans[1] << '\n';
+				return 0;
+			}
+		}
+		swap(A, B);
+	}
+	cout << "No" << endl;
+}
