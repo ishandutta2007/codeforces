6d63ed8dc6e405261d5871b6b29015625923486f
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 21:18:30 +0530

    maspy

---
+++ maspy/normal/616/F.cpp
@@ -0,0 +1,433 @@
+#line 1 "main.cpp"
+#include <bits/stdc++.h>
+#line 3 "/home/maspy/library/my_template.hpp"
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using pi = pair<ll, ll>;
+using vi = vector<ll>;
+template <class T> using vc = vector<T>;
+template <class T> using vvc = vector<vc<T>>;
+template <class T> using vvvc = vector<vvc<T>>;
+template <class T> using vvvvc = vector<vvvc<T>>;
+template <class T> using pq = priority_queue<T>;
+template <class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+
+#define vec(type, name, ...) vector<type> name(__VA_ARGS__)
+#define VEC(type, name, size)                                                                                                                                  \
+    vector<type> name(size);                                                                                                                                   \
+    IN(name)
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define VV(type, name, h, w)                                                                                                                                   \
+    vector<vector<type>> name(h, vector<type>(w));                                                                                                             \
+    IN(name)
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...)                                                                                                                         \
+    vector<vector<vector<vector<type>>>> name(a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+
+#define FOR(i, n) for (ll i = 0; (i) < (ll)(n); ++(i))
+#define FOR3(i, m, n) for (ll i = (m); (i) < (ll)(n); ++(i))
+#define FOR_R(i, n) for (ll i = (ll)(n)-1; (i) >= 0; --(i))
+#define FOR3_R(i, m, n) for (ll i = (ll)(n)-1; (i) >= (ll)(m); --(i))
+#define FORIN(x, A) for (auto&& x : A)
+#define all(x) x.begin(), x.end()
+
+#define elif else if
+
+#define popcnt __builtin_popcount
+
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define fi first
+#define se second
+
+#define SUM(v) accumulate(all(v), 0LL)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+
+#define LB(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define UB(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+
+template <class T> T ceil(T x, T y) {
+    assert(y >= 1);
+    return (x > 0 ? (x + y - 1) / y : x / y);
+}
+
+template <class T> T floor(T x, T y) {
+    assert(y >= 1);
+    return (x > 0 ? x / y : (x - y + 1) / y);
+}
+
+#define INT(...)                                                                                                                                               \
+  int __VA_ARGS__;                                                                                                                                           \
+  IN(__VA_ARGS__)
+#define LL(...)                                                                                                                                                \
+  ll __VA_ARGS__;                                                                                                                                            \
+  IN(__VA_ARGS__)
+#define STR(...)                                                                                                                                               \
+  string __VA_ARGS__;                                                                                                                                        \
+  IN(__VA_ARGS__)
+#define CHR(...)                                                                                                                                               \
+  char __VA_ARGS__;                                                                                                                                          \
+  IN(__VA_ARGS__)
+#define DBL(...)                                                                                                                                               \
+  double __VA_ARGS__;                                                                                                                                        \
+  IN(__VA_ARGS__)
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &a) {for(auto &i : a) scan(i);}
+template <class T> void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
+  scan(head);
+  IN(tail...);
+}
+
+vi s_to_vi(string S, char first_char='a'){
+  vi A(S.size());
+  FOR(i, S.size()){
+    A[i] = S[i] - first_char;
+  }
+  return A;
+}
+
+template <typename T, typename U>
+ostream& operator<<(ostream& os, const pair<T, U>& A) {
+  os << A.fi << " " << A.se;
+  return os;
+}
+
+template <typename T>
+ostream& operator<<(ostream& os, const vector<T>& A) {
+  for (size_t i = 0; i < A.size(); i++) {
+    if(i) os << " ";
+    os << A[i];
+  }
+  return os;
+}
+
+void print() {
+  cout << "\n";
+}
+
+template <class Head, class... Tail>
+void print(Head&& head, Tail&&... tail) {
+  cout << head;
+  if (sizeof...(Tail)) cout << " ";
+  print(forward<Tail>(tail)...);
+}
+
+const string YESNO[2] = {"NO", "YES"};
+const string YesNo[2] = {"No", "Yes"};
+const string yesno[2] = {"no", "yes"};
+void YES(bool t = 1) { cout << YESNO[t] << endl; }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { cout << YesNo[t] << endl; }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { cout << yesno[t] << endl; }
+void no(bool t = 1) { yes(!t); }
+
+template <typename T>
+vector<T> cumsum(vector<T> A) {
+  ll N = A.size();
+  vector<T> B(N + 1);
+  B[0] = T(0);
+  FOR(i, N) { B[i + 1] = B[i] + A[i]; }
+  return B;
+}
+
+vi bin_count(vi& A, ll size) {
+  vi C(size);
+  for (auto&& x : A) {
+    ++C[x];
+  }
+  return C;
+}
+
+template <typename T>
+vi argsort(vector<T>& A){
+  vi ids(A.size());
+  iota(all(ids), 0);
+  sort(all(ids), [&](ll i, ll j){return mp(A[i],i)<=mp(A[j],j);});
+  return ids;
+}
+
+
+template <class T, class S> inline bool chmax(T &a, const S &b) { return (a < b ? a = b, 1 : 0); }
+template <class T, class S> inline bool chmin(T &a, const S &b) { return (a > b ? a = b, 1 : 0); }
+#line 3 "/home/maspy/library/ds/unionfind.hpp"
+
+struct UnionFind {
+  int num;
+  int comp;
+  vi v_sz, e_sz, par;
+  UnionFind(int n) : num(n), comp(n), v_sz(n, 1), e_sz(n, 0), par(n) { iota(par.begin(), par.end(), 0); }
+  int find(int x) {
+    while (par[x] != x) {
+      par[x] = par[par[x]];
+      x = par[x];
+    }
+    return x;
+  }
+
+  int operator[](int x) {
+    return find(x);
+  }
+
+
+  bool merge(ll x, ll y) {
+    x = find(x);
+    y = find(y);
+    if (x == y) {
+      e_sz[x]++;
+      return false;
+    }
+    comp--;
+    if (v_sz[x] < v_sz[y]) swap(x, y);
+    e_sz[x] += e_sz[y] + 1;
+    v_sz[x] += v_sz[y];
+    par[y] = x;
+    return true;
+  }
+
+  vi find_all() {
+    vi A(num);
+    FOR(i, num) A[i] = find(i);
+    return A;
+  }
+};
+#line 2 "/home/maspy/library/string/suffixarray.hpp"
+using namespace std;
+
+struct SuffixArray {
+  vector<int> SA;
+  vector<int> ISA;
+  vector<int> LCP;
+
+  SuffixArray(string& s, const char first = 'a', const char last = 'z') {
+    SA = calc_suffix_array(s, first, last);
+    calc_LCP(s);
+  }
+
+  SuffixArray(vector<int>& s) {
+    SA = calc_suffix_array(s);
+    calc_LCP(s);
+  }
+
+  void induced_sort(const std::vector<int>& vect, int val_range, std::vector<int>& SA, const std::vector<bool>& sl,
+                    const std::vector<int>& lms_idx) {
+    std::vector<int> l(val_range, 0), r(val_range, 0);
+    for (int c : vect) {
+      if (c + 1 < val_range) ++l[c + 1];
+      ++r[c];
+    }
+    std::partial_sum(l.begin(), l.end(), l.begin());
+    std::partial_sum(r.begin(), r.end(), r.begin());
+    std::fill(SA.begin(), SA.end(), -1);
+    for (int i = (int)lms_idx.size() - 1; i >= 0; --i) SA[--r[vect[lms_idx[i]]]] = lms_idx[i];
+    for (int i : SA)
+      if (i >= 1 && sl[i - 1]) SA[l[vect[i - 1]]++] = i - 1;
+    std::fill(r.begin(), r.end(), 0);
+    for (int c : vect) ++r[c];
+    std::partial_sum(r.begin(), r.end(), r.begin());
+    for (int k = (int)SA.size() - 1, i = SA[k]; k >= 1; --k, i = SA[k])
+      if (i >= 1 && !sl[i - 1]) {
+        SA[--r[vect[i - 1]]] = i - 1;
+      }
+  }
+
+  std::vector<int> SA_IS(const std::vector<int>& vect, int val_range) {
+    const int n = vect.size();
+    std::vector<int> SA(n), lms_idx;
+    std::vector<bool> sl(n);
+    sl[n - 1] = false;
+    for (int i = n - 2; i >= 0; --i) {
+      sl[i] = (vect[i] > vect[i + 1] || (vect[i] == vect[i + 1] && sl[i + 1]));
+      if (sl[i] && !sl[i + 1]) lms_idx.push_back(i + 1);
+    }
+    std::reverse(lms_idx.begin(), lms_idx.end());
+    induced_sort(vect, val_range, SA, sl, lms_idx);
+    std::vector<int> new_lms_idx(lms_idx.size()), lms_vec(lms_idx.size());
+    for (int i = 0, k = 0; i < n; ++i)
+      if (!sl[SA[i]] && SA[i] >= 1 && sl[SA[i] - 1]) {
+        new_lms_idx[k++] = SA[i];
+      }
+    int cur = 0;
+    SA[n - 1] = cur;
+    for (size_t k = 1; k < new_lms_idx.size(); ++k) {
+      int i = new_lms_idx[k - 1], j = new_lms_idx[k];
+      if (vect[i] != vect[j]) {
+        SA[j] = ++cur;
+        continue;
+      }
+      bool flag = false;
+      for (int a = i + 1, b = j + 1;; ++a, ++b) {
+        if (vect[a] != vect[b]) {
+          flag = true;
+          break;
+        }
+        if ((!sl[a] && sl[a - 1]) || (!sl[b] && sl[b - 1])) {
+          flag = !((!sl[a] && sl[a - 1]) && (!sl[b] && sl[b - 1]));
+          break;
+        }
+      }
+      SA[j] = (flag ? ++cur : cur);
+    }
+    for (size_t i = 0; i < lms_idx.size(); ++i) lms_vec[i] = SA[lms_idx[i]];
+    if (cur + 1 < (int)lms_idx.size()) {
+      auto lms_SA = SA_IS(lms_vec, cur + 1);
+      for (size_t i = 0; i < lms_idx.size(); ++i) {
+        new_lms_idx[i] = lms_idx[lms_SA[i]];
+      }
+    }
+    induced_sort(vect, val_range, SA, sl, new_lms_idx);
+    return SA;
+  }
+
+  std::vector<int> calc_suffix_array(const std::string& s, const char first = 'a', const char last = 'z') {
+    std::vector<int> vect(s.size() + 1);
+    std::copy(std::begin(s), std::end(s), std::begin(vect));
+    for (auto& x : vect) x -= (int)first - 1;
+    vect.back() = 0;
+    auto ret = SA_IS(vect, (int)last - (int)first + 2);
+    ret.erase(ret.begin());
+    return ret;
+  }
+
+  std::vector<int> calc_suffix_array(const vector<int>& s) {
+    vector<int> ss = s;
+    sort(ss.begin(), ss.end());
+    ss.erase(unique(ss.begin(), ss.end()), ss.end());
+
+    std::vector<int> vect(s.size() + 1);
+    std::copy(std::begin(s), std::end(s), std::begin(vect));
+    for (auto& x : vect) x = lower_bound(ss.begin(), ss.end(), x) - ss.begin() + 1;
+    vect.back() = 0;
+    auto ret = SA_IS(vect, *max_element(vect.begin(), vect.end()) + 2);
+    ret.erase(ret.begin());
+    return ret;
+  }
+
+  void calc_LCP(const std::string& s) {
+    int n = s.size(), k = 0;
+    ISA.resize(n);
+    LCP.resize(n);
+    for (int i = 0; i < n; i++) ISA[SA[i]] = i;
+    for (int i = 0; i < n; i++, k ? k-- : 0) {
+      if (ISA[i] == n - 1) {
+        k = 0;
+        continue;
+      }
+      int j = SA[ISA[i] + 1];
+      while (i + k < n && j + k < n && s[i + k] == s[j + k]) k++;
+      LCP[ISA[i]] = k;
+    }
+    LCP.resize(n - 1);
+  }
+
+  void calc_LCP(const vector<int>& s) {
+    int n = s.size(), k = 0;
+    ISA.resize(n);
+    LCP.resize(n);
+    for (int i = 0; i < n; i++) ISA[SA[i]] = i;
+    for (int i = 0; i < n; i++, k ? k-- : 0) {
+      if (ISA[i] == n - 1) {
+        k = 0;
+        continue;
+      }
+      int j = SA[ISA[i] + 1];
+      while (i + k < n && j + k < n && s[i + k] == s[j + k]) k++;
+      LCP[ISA[i]] = k;
+    }
+    LCP.resize(n - 1);
+  }
+};
+#line 5 "main.cpp"
+
+void solve() {
+  INT(N);
+  VEC(string, S, N);
+  VEC(ll, A, N);
+
+  string T;
+  FOR(i, N) { T += S[i] + '~'; }
+  SuffixArray sa(T, 'a', '~');
+  auto SA = sa.SA;
+  auto ISA = sa.ISA;
+  auto LCP = sa.LCP;
+
+  vi LID(N);
+  vi RID(N);
+  FOR(i, N) {
+    if (i) LID[i] = RID[i - 1] + 1;
+    RID[i] = LID[i] + S[i].size();
+  }
+
+  vi LEN(T.size());
+  vi COST(T.size());
+
+  FOR(i, N) {
+    FOR(j, S[i].size()) {
+      int n = ISA[LID[i] + j];
+      LEN[n] = S[i].size() - j;
+      COST[n] = A[i];
+    }
+  }
+
+  FOR(i, T.size() - 1) {
+    chmin(LCP[i], LEN[i]);
+    chmin(LCP[i], LEN[i + 1]);
+  }
+
+  ll ANS = 0;
+  // 
+  FOR(i, T.size()) {
+    if (i > 0 && LCP[i - 1] == LEN[i]) continue;
+    if (i < LCP.size() && LCP[i] == LEN[i]) continue;
+    chmax(ANS, LEN[i] * COST[i]);
+  }
+
+  UnionFind uf(T.size());
+
+  vvc<ll> sz_to_i(MAX(LCP) + 1);
+  FOR(i, LCP.size()) sz_to_i[LCP[i]].eb(i);
+  FOR_R(sz, MAX(LCP) + 1) {
+    if (sz == 0) break;
+    vi I;
+    FORIN(i, sz_to_i[sz]) {
+      auto a = uf[i], b = uf[i + 1];
+      uf.merge(a, b);
+      if (uf[a] == a) {
+        COST[a] += COST[b];
+        COST[b] = 0;
+        I.eb(a);
+      } else {
+        COST[b] += COST[a];
+        COST[a] = 0;
+        I.eb(b);
+      }
+    }
+    FORIN(i, I) {
+      if (uf[i] == i) chmax(ANS, COST[i] * sz);
+    }
+  }
+  print(ANS);
+}
+
+signed main() {
+  cin.tie(nullptr);
+  ios::sync_with_stdio(false);
+  cout << setprecision(15);
+
+  solve();
+
+  return 0;
+}
