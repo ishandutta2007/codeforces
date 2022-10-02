615cc8c5a2e0d0a98f45c7e7f172765307a19bb6
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 23:05:36 +0530

    egor.lifar

---
+++ egor.lifar/normal/1428/A.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+
+using namespace std;
+template<typename T1, typename T2> inline void chkmin(T1 &a, T2 b) {if (a > b) a = b;}
+template<typename T1, typename T2> inline void chkmax(T1 &a, T2 b) {if (a < b) a = b;}
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (int)(c).size()
+#define left left228
+#define right right228
+#define y1 y1228
+#define mp make_pair
+#define pb push_back
+#define y2 y2228
+#define rank rank228
+using ll = long long;
+using ld = long double; 
+const string FILENAME = "input";
+
+
+void solve() {
+	ll x1, y1, x2, y2;
+	cin >> x1 >> y1 >> x2 >> y2;
+	if (x1 == x2) {
+		cout << abs(y1 - y2) << '\n';
+		return;
+	}
+	if (y1 == y2) {
+		cout << abs(x1 - x2) << '\n';
+		return;
+	}
+	cout << abs(x1 - x2) + abs(y1 - y2) + 2 << '\n';
+}
+
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	//read(FILENAME);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+}
