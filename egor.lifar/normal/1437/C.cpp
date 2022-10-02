4a13cbc6d5b8ce0ba597874eaa901412dec964df
ishan dutta <ishandutta2007@gmail.com>
2022-10-02 23:03:29 +0530

    egor.lifar

---
+++ egor.lifar/normal/1437/C.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+ 
+using namespace std;
+template<typename T1, typename T2> inline void chkmin(T1 &a, T2 b) {if (a > b) a = b;}
+template<typename T1, typename T2> inline void chkmax(T1 &a, T2 b) {if (a < b) a = b;}
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (int)(c).size()
+#define left left228
+#define right right228
+#define y1 y1228
+#define mp make_pair
+#define pb push_back
+#define y2 y2228
+#define rank rank228
+using ll = long long;
+using ld = long double; 
+const string FILENAME = "input";
+const int MAXN = 405;
+
+
+int n;
+int t[MAXN];
+int dp[MAXN][MAXN];
+
+
+void solve() {
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> t[i];
+	}
+	sort(t, t + n);
+	for (int i = 1; i <= 2 * n + 1; i++) {
+		for (int j = 0; j <= n; j++) {
+			dp[i][j] = 1e9;
+		}
+	}
+	dp[1][0] = 0;
+	int res = 1e9;
+	for (int i = 1; i <= 2 * n + 1; i++) {
+		for (int j = 0; j < n; j++) {
+			chkmin(dp[i + 1][j], dp[i][j]);
+			chkmin(dp[i + 1][j + 1], dp[i][j] + abs(t[j] - i));
+		}
+		chkmin(res, dp[i][n]);
+	}
+	cout << res << '\n';
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+//	read(FILENAME);
+	int t;
+	cin >> t;
+	while (t--) {
+		solve();
+	}
+	return 0;
+}
