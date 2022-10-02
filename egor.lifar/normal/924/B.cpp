68037cfb95a4666b39d0a262b1a435bdea426902
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 00:21:28 +0530

    egor.lifar

---
+++ egor.lifar/normal/924/B.cpp
@@ -0,0 +1,103 @@
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+// 
+//#pragma GCC target("sse4,tune=native")
+//#pragma GCC optimize("O3")
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <deque>
+#include <cassert>
+#include <iomanip>      
+#define pb push_back
+#define x first
+#define y second
+#define mp make_pair
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+using namespace std;
+  
+template<typename T1, typename T2>inline void chkmin(T1 &x, T2 y) { if (x > y) x = y; }
+template<typename T1, typename T2>inline void chkmax(T1 &x, T2 y) { if (x < y) x = y; } 
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const pair<T, U> &_p) { _out << _p.first << ' ' << _p.second; return _out; }
+template<typename T, typename U> inline istream &operator>> (istream &_in, pair<T, U> &_p) { _in >> _p.first >> _p.second; return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const vector<T> &_v) { if (_v.empty()) { return _out; } _out << _v.front(); for (auto _it = ++_v.begin(); _it != _v.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline istream &operator>> (istream &_in, vector<T> &_v) { for (auto &_i : _v) { _in >> _i; } return _in; }
+template<typename T> inline ostream &operator<< (ostream &_out, const set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_set<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T> inline ostream &operator<< (ostream &_out, const unordered_multiset<T> &_s) { if (_s.empty()) { return _out; } _out << *_s.begin(); for (auto _it = ++_s.begin(); _it != _s.end(); ++_it) { _out << ' ' << *_it; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+template<typename T, typename U> inline ostream &operator<< (ostream &_out, const unordered_map<T, U> &_m) { if (_m.empty()) { return _out; } _out << '(' << _m.begin()->first << ": " << _m.begin()->second << ')'; for (auto _it = ++_m.begin(); _it != _m.end(); ++_it) { _out << ", (" << _it->first << ": " << _it->second << ')'; } return _out; }
+  
+typedef long long base;
+typedef pair<int, int> point;
+
+const string FILENAME = "input";
+
+long double ans = -1e18;
+
+int n, w;
+vector<int> a;
+
+int main() {
+	srand(time(0));
+	ios::sync_with_stdio(false);
+	cin >> n >> w;
+	a.resize(n);
+	for (int i = 0; i < n; ++i) {
+		cin >> a[i];
+	}
+	for (int i = 0; i + 1 < n; ++i) {
+		int q = upper_bound(a.begin(), a.end(), a[i] + w) - a.begin() - 1;
+		if (q - i < 2) continue;
+		long double A = a[q], B = a[i + 1], C = a[i];
+		chkmax(ans, (A - B) / (A - C));
+	}
+	if (ans < 0) {
+		cout << -1 << endl;
+	} else {
+		cout.precision(18);
+		cout << fixed << ans << endl;
+	}
+}
