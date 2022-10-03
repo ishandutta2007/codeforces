e62578fb24be7c368176a6ccaedb9867f47eb44d
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:36:40 +0530

    alishahali1382

---
+++ alishahali1382/normal/1167/C.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 500010, LOG=20;
+
+struct DSU{
+	int par[MAXN];
+	int sz[MAXN];
+	DSU(){ for (int i=0; i<MAXN; i++) par[i]=i, sz[i]=1;}
+	int get(int x){
+		if (par[x]==x) return x;
+		return par[x]=get(par[x]);
+	}
+	void join(int x, int y){
+		//debug2(x, y)
+		x=get(x);
+		y=get(y);
+		if (x==y) return ;
+		sz[x]+=sz[y];
+		par[y]=x;
+	}
+} dsu;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>m;
+	while (m--){
+		cin>>k;
+		if (!k) continue ;
+		cin>>x;
+		for (int i=1; i<k; i++) cin>>y, dsu.join(x, y);
+	}
+	for (int i=1; i<=n; i++) cout<<dsu.sz[dsu.get(i)]<<' ';
+	
+	return 0;
+}
+/*
+      ___           ___                            ___           ___           ___           ___           ___           ___
+     /\  \         /\__\      ___                 /\  \         /\__\         /\  \         /\__\         /\  \         /\__\      ___
+    /::\  \       /:/  /     /\  \               /::\  \       /:/  /        /::\  \       /:/  /        /::\  \       /:/  /     /\  \
+   /:/\:\  \     /:/  /      \:\  \             /:/\ \  \     /:/__/        /:/\:\  \     /:/__/        /:/\:\  \     /:/  /      \:\  \
+  /::\~\:\  \   /:/  /       /::\__\           _\:\~\ \  \   /::\  \ ___   /::\~\:\  \   /::\  \ ___   /::\~\:\  \   /:/  /       /::\__\
+ /:/\:\ \:\__\ /:/__/     __/:/\/__/          /\ \:\ \ \__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/__/     __/:/\/__/
+ \/__\:\/:/  / \:\  \    /\/:/  /             \:\ \:\ \/__/ \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \:\  \    /\/:/  /
+      \::/  /   \:\  \   \::/__/               \:\ \:\__\        \::/  /       \::/  /       \::/  /       \::/  /   \:\  \   \::/__/
+      /:/  /     \:\  \   \:\__\                \:\/:/  /        /:/  /        /:/  /        /:/  /        /:/  /     \:\  \   \:\__\
+     /:/  /       \:\__\   \/__/                 \::/  /        /:/  /        /:/  /        /:/  /        /:/  /       \:\__\   \/__/
+     \/__/         \/__/                          \/__/         \/__/         \/__/         \/__/         \/__/         \/__/
+*/
