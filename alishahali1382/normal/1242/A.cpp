4834e7efec81451ca3b1483775e33a9a008f20be
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 07:53:01 +0530

    alishahali1382

---
+++ alishahali1382/normal/1242/A.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O2")
+#pragma GCC optimize ("unroll-loops")
+//#pragma GCC optimize("no-stack-protector,fast-math")
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 100010, LOG=20;
+
+ll n, m, k, u, v, x, y, t, a, b, ans;
+vector<ll> vec;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n;
+	if (n==1) kill(1)
+	ll tmp=n;
+	for (ll i=2; i*i<=tmp; i++) if (tmp%i==0){
+		vec.pb(i);
+		while (tmp%i==0) tmp/=i;
+	}
+	if (tmp>1) vec.pb(tmp);
+	
+	if (vec.size()==1) kill(vec.back())
+	kill(1)
+	
+	return 0;
+}
