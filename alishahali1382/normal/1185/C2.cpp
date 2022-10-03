d894698819446b0d712f9f410050f923bd28490d
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:25:13 +0530

    alishahali1382

---
+++ alishahali1382/normal/1185/C2.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 200010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+int A[MAXN];
+int cnt[101];
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>m;
+	for (int i=1; i<=n; i++){
+		cin>>A[i];
+		ans=0;
+		int sum=m-A[i];
+		for (int j=1; j<=100; j++) if (cnt[j]){
+			if (sum>=cnt[j]*j){
+				sum-=cnt[j]*j;
+				ans+=cnt[j];
+				continue ;
+			}
+			int tmp=max(0, sum/j-5);
+			while ((tmp+1)*j<=sum) tmp++;
+			ans+=tmp;
+			sum-=tmp*j;
+			break ;
+		}
+		cout<<i-1-ans<<' ';
+		cnt[A[i]]++;
+	}
+	
+	
+	return 0;
+}
