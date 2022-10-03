49684724451081c36e7d5d4eef8cc516cc5abbdb
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 10:33:46 +0530

    alishahali1382

---
+++ alishahali1382/normal/112/A.py
@@ -0,0 +1,5 @@
+s=input().lower()
+t=input().lower()
+if s==t: print(0)
+elif s>t: print(1)
+else: print(-1)
---
+++ alishahali1382/normal/96/A.py
@@ -0,0 +1,5 @@
+s=input()
+if "1111111" in s or "0000000" in s:
+    print("YES")
+else:
+    print("NO")
