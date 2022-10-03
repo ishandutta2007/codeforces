e14db9fecc5e1aa7b95d51a02cce5af621a72d0e
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 06:57:54 +0530

    alishahali1382

---
+++ alishahali1382/normal/1552/B.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O2,unroll-loops")
+//#pragma GCC optimize("no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) {cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;}
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define SZ(x) ((int)x.size())
+#define kill(x) return cout<<x<<'\n', 0;
+
+const int inf=1000000010;
+const ll INF=1000000000000001000LL;
+const int mod=1000000007;
+const int MAXN=300010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+int A[MAXN][5], B[MAXN];
+string S, T;
+
+bool cmp(int i, int j){
+	int t=0;
+	for (int x=0; x<5; x++) t+=(A[i][x]>A[j][x]);
+	return (t<=2);
+}
+
+int Solve(){
+	cin>>n;
+	for (int i=1; i<=n; i++) for (int j=0; j<5; j++) cin>>A[i][j];
+	iota(B+1, B+n+1, 1);
+	sort(B+1, B+n+1, cmp);
+	for (int i=2; i<=n; i++) if (!cmp(B[1], B[i])) kill(-1)
+	kill(B[1])
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int T;
+	cin>>T;
+	while (T--) Solve();
+	
+	return 0;
+}
