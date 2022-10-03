0ad0c4e7b6e4b53b1f0ae0b8db48139bca3b9652
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 10:11:22 +0530

    alishahali1382

---
+++ alishahali1382/normal/1077/E.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<x<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<x.first<<", "<<x.second<<"}"<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const int mod = 1000000007;
+const int MAXN = 200010;
+
+int n, ans, x, b, t, k, s, cnt;
+int A[MAXN];
+int mx[MAXN];
+
+map<int, int> mp;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n;
+	for (int i=1; i<=n; i++){
+		cin>>x;
+		mp[x]++;
+	}
+	for (pii p:mp) A[cnt++]=p.second;
+	mp.clear();
+	n=cnt;
+	sort(A, A+n);
+	//for (int i=0; i<n; i++) cerr<<A[i]<<' ';cerr<<endl;
+	ans=*max_element(A, A+n);
+	for (int i=1; i<=A[0]; i++) mx[i]=i;
+	
+	for (int i=1; i<n; i++){
+		for (int j=A[i]; j; j--){
+			if (j%2==1){
+				mx[j]=max(mx[j], j);
+				continue ;
+			}
+			int tmp=j/2;
+			mx[j]=max(mx[j], mx[tmp]+j);
+		}
+	} 
+	
+	cout<<(*max_element(mx, mx+MAXN))<<endl;
+	
+	return 0;
+}
+/*
+                                                                                                            __                    .----.                    
+          .---.                                                                     .---.              ...-'  |`. ..-'''-.       / .--. \        .-''-.     
+          |   |.--.                       .                     .                   |   |.--.          |      |  |\.-'''\ \     ' '    ' '     .' .-.  )    
+          |   ||__|                     .'|                   .'|                   |   ||__|          ....   |  |       | |    \ \    / /    / .'  / /     
+          |   |.--.                    <  |                  <  |                   |   |.--.            -|   |  |    __/ /      `.`'--.'    (_/   / /      
+    __    |   ||  |                     | |             __    | |             __    |   ||  |             |   |  |   |_  '.      / `'-. `.        / /       
+ .:--.'.  |   ||  |                 _   | | .'''-.   .:--.'.  | | .'''-.   .:--.'.  |   ||  |          ...'   `--'      `.  \   ' /    `. \      / /        
+/ |   \ | |   ||  |               .' |  | |/.'''. \ / |   \ | | |/.'''. \ / |   \ | |   ||  |          |         |`.      \ '. / /       \ '    . '         
+`" __ | | |   ||  |              .   | /|  /    | | `" __ | | |  /    | | `" __ | | |   ||  |          ` --------\ |       , || |         | |  / /    _.-') 
+ .'.''| | |   ||__|            .'.'| |//| |     | |  .'.''| | | |     | |  .'.''| | |   ||__|           `---------'        | || |         | |.' '  _.'.-''  
+/ /   | |_'---'              .'.'.-'  / | |     | | / /   | |_| |     | | / /   | |_'---'                                 / ,' \ \       / //  /.-'_.'      
+\ \._,\ '/                   .'   \_.'  | '.    | '.\ \._,\ '/| '.    | '.\ \._,\ '/                              -....--'  /   `.'-...-'.'/    _.'         
+ `--'  `"                               '---'   '---'`--'  `" '---'   '---'`--'  `"                               `.. __..-'       `-...-'( _.-'            
+ */
