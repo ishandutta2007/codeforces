5695e951bdf47d429992e4ad49871d04c9e5f314
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 07:02:35 +0530

    alishahali1382

---
+++ alishahali1382/normal/1515/B.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O2,unroll-loops")
+//#pragma GCC optimize("no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) {cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;}
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const int inf=1000000010;
+const ll INF=1000000000000001000LL;
+const int mod=1000000007;
+const int MAXN=300010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+int A[MAXN];
+
+int Solve(){
+	cin>>n;
+	if (n&1) kill("NO")
+	n>>=1;
+	int y=round(sqrt(n));
+	if (y*y==n) kill("YES")
+	if (n&1) kill("NO")
+	n>>=1;
+	y=round(sqrt(n));
+	if (y*y==n) kill("YES")
+	kill("NO")
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int T=1;
+	cin>>T;
+	while (T--) Solve();
+	
+	return 0;
+}
