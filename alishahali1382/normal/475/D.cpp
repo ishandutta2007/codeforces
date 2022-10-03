cdf6c3e7107eda34fd1292105c665f71078d12be
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:15:33 +0530

    alishahali1382

---
+++ alishahali1382/normal/475/D.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("Ofast")
+#pragma GCC optimize ("unroll-loops")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 100010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b;
+int A[MAXN];
+map<int, ll> mp[MAXN];
+map<int, ll> ans;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n;
+	for (int i=1; i<=n; i++){
+		cin>>A[i];
+		for (pii p:mp[i-1]) mp[i][__gcd(p.first, A[i])]+=p.second;
+		mp[i][A[i]]++;
+		for (pii p:mp[i]) ans[p.first]+=p.second;
+	}
+	cin>>m;
+	while (m--) cin>>x, cout<<ans[x]<<'\n';
+	
+	return 0;
+}
