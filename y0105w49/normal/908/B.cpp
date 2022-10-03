78f13ef70583c93cd852fc0b3d125e885f9da0d0
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 05:34:42 +0530

    y0105w49

---
+++ y0105w49/normal/908/B.cpp
@@ -0,0 +1,136 @@
+// ayy
+// ' lamo
+// SUBLIME HAX
+	#include <bits/stdc++.h>
+	using namespace std;
+	template<class T,class U>
+	ostream &operator<<(ostream &os,const pair<T,U> &x) {
+		return os<<"("<<x.first<<","<<x.second<<")";
+	}
+	namespace dbg_ns {
+		template<typename C>
+		struct is_iterable {
+			template<class T> static long check(...);
+			template<class T> static char check(int,
+				typename T::const_iterator = C().end());
+			enum {
+				value = sizeof(check<C>(0)) == sizeof(char),
+				neg_value = sizeof(check<C>(0)) != sizeof(char)
+			};
+		};
+		template<class T> ostream &_out_str(ostream &os,const T &x) {
+			return os<<'"'<<x<<'"';
+		}
+		template<class T> ostream &_dbg2_5(ostream &,const T &);
+		template<bool B,typename T=void> using eit=typename enable_if<B,T>::type;
+		template<class T>
+		inline ostream &_dbg3(ostream &os,eit<is_iterable<T>::neg_value,const T> &x) {
+			return os<<x;
+		}
+		template<class T>
+		inline ostream &_dbg3(ostream &os,eit<is_iterable<T>::value,const T> &V) {
+			os<<"{";
+			bool ff=0;
+			for(const auto &E:V) _dbg2_5(ff?os<<",":os,E), ff=1;
+			return os<<"}";
+		}
+		template<>
+		inline ostream &_dbg3<string>(ostream &os,const string &x) {
+			return _out_str(os,x);
+		}
+		template<>
+		inline ostream &_dbg3<const char *>(ostream &os,const char *const &x) {
+			return _out_str(os,x);
+		}
+		template<class T> inline ostream &_dbg2_5(ostream &os,const T &x) {
+			return _dbg3<T>(os,x);
+		}
+		template<class T,typename... Args> ostream &_dbg2(ostream &os,vector<string>::iterator nm,const T &x,Args&&... args);
+		inline ostream &_dbg2(ostream &os,vector<string>::iterator) { return os; }
+		template<typename... Args>
+		inline ostream &_dbg2(ostream &os,vector<string>::iterator nm,const char *x,Args&&... args) {
+			return _dbg2(_dbg3<const char *>(os<<"  ",x),nm+1,args...);
+		}
+		template<class T,typename... Args>
+		inline ostream &_dbg2(ostream &os,vector<string>::iterator nm,const T &x,Args&&... args) {
+			return _dbg2(_dbg3<T>(os<<"  "<<*nm<<"=",x),nm+1,args...);
+		}
+		vector<string> split(string s) {
+			vector<string> Z;
+			string z="";
+			s+=',';
+			int dep=0;
+			for(char c:s) {
+				if(c==',' && !dep) Z.push_back(z),z="";
+				else z+=c;
+				if(c=='(') ++dep;
+				if(c==')') --dep;
+			}
+			return Z;
+		}
+		template<typename... Args> inline ostream &_dbg1(int ln,const string &nm,Args&&... args) {
+			auto nms=split(nm);
+			return _dbg2(cerr<<"L"<<ln<<":",nms.begin(),args...)<<endl;
+		}
+	}
+	#define dbg(...) dbg_ns::_dbg1(__LINE__,#__VA_ARGS__,__VA_ARGS__)
+	#define sz(x) (int(x.size()))
+	#define eprintf(...) fprintf(stderr,__VA_ARGS__)
+	#define fi first
+	#define se second
+	#define pb push_back
+// END SUBLIME HAX
+// #include <bits/extc++.h>
+// using namespace __gnu_pbds;
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld; //CARE
+typedef complex<ld> pt;
+const ld eps=(ld)1e-8;
+const ld tau=2*(ld)acosl(-1);
+const int inf=1e9+99;
+const ll linf=1e18+99;
+const int P=1e9+7;
+
+
+
+const int dx[]={1,0,-1,0};
+const int dy[]={0,1,0,-1,0};
+
+const int N=64;
+int n,m;
+char g[N][N];
+int p[4];
+
+
+
+
+int32_t main() {
+	scanf("%d%d",&n,&m);
+	for(int i=1;i<=n;i++) scanf("%s",g[i]+1);
+	int sx=-1,sy=-1,tx=-1,ty=-1;
+	for(int i=1;i<=n;i++) for(int j=1;j<=m;j++) {
+		if(g[i][j]=='S') sx=i, sy=j;
+		if(g[i][j]=='E') tx=i, ty=j;
+	}
+	string S; cin>>S;
+	S+='$';
+
+	for(int k=0;k<4;k++) p[k]=k;
+	int Z=0;
+	do {
+		int x=sx,y=sy;
+		for(char c:S) {
+			if(x==tx && y==ty) goto win;
+			if(!x || !y || x>n || y>m) break;
+			if(g[x][y]=='#') break;
+			if(c=='$') break;
+			int k=p[c-'0'];
+			x+=dx[k],y+=dy[k];
+		}
+		continue;
+		win:;
+		++Z;
+	} while(next_permutation(p,p+4));
+	printf("%d\n",Z);
+}
