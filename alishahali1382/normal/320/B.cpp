fe7d867461aa9a8aab7f3903d138df6a985fab90
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 10:10:22 +0530

    alishahali1382

---
+++ alishahali1382/normal/320/B.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const int mod = 1000000007;
+const int MAXN = 110;
+
+int n, m, q, ans, a, b, c, typ;
+pii A[MAXN];
+bool mark[MAXN];
+vector<int> G[MAXN];
+
+bool dfs(int node, int x){
+	if (node==x) return true;
+	mark[node]=true;
+	for (int v:G[node]){
+		if (mark[v]) continue ;
+		if (dfs(v, x)) return true;
+	}
+	return false;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>q;
+	while (q--){
+		cin>>typ>>a>>b;
+		if (typ==2){
+			memset(mark, 0, sizeof(mark));
+			if (dfs(a, b)) cout<<"YES"<<endl;
+			else cout<<"NO"<<endl;
+			continue ;
+		}
+		A[++n]={a, b};
+		for (int i=1; i<n; i++){
+			int c=A[i].first;
+			int d=A[i].second;
+			if ((c<a && a<d)||(c<b && b<d)) G[n].pb(i);
+			if ((a<c && c<b)||(a<d && d<b)) G[i].pb(n);
+		}
+	}
+	
+	return 0;
+}
+/*
+                                                                                                            __                    .----.                    
+          .---.                                                                     .---.              ...-'  |`. ..-'''-.       / .--. \        .-''-.     
+          |   |.--.                       .                     .                   |   |.--.          |      |  |\.-'''\ \     ' '    ' '     .' .-.  )    
+          |   ||__|                     .'|                   .'|                   |   ||__|          ....   |  |       | |    \ \    / /    / .'  / /     
+          |   |.--.                    <  |                  <  |                   |   |.--.            -|   |  |    __/ /      `.`'--.'    (_/   / /      
+    __    |   ||  |                     | |             __    | |             __    |   ||  |             |   |  |   |_  '.      / `'-. `.        / /       
+ .:--.'.  |   ||  |                 _   | | .'''-.   .:--.'.  | | .'''-.   .:--.'.  |   ||  |          ...'   `--'      `.  \   ' /    `. \      / /        
+/ |   \ | |   ||  |               .' |  | |/.'''. \ / |   \ | | |/.'''. \ / |   \ | |   ||  |          |         |`.      \ '. / /       \ '    . '         
+`" __ | | |   ||  |              .   | /|  /    | | `" __ | | |  /    | | `" __ | | |   ||  |          ` --------\ |       , || |         | |  / /    _.-') 
+ .'.''| | |   ||__|            .'.'| |//| |     | |  .'.''| | | |     | |  .'.''| | |   ||__|           `---------'        | || |         | |.' '  _.'.-''  
+/ /   | |_'---'              .'.'.-'  / | |     | | / /   | |_| |     | | / /   | |_'---'                                 / ,' \ \       / //  /.-'_.'      
+\ \._,\ '/                   .'   \_.'  | '.    | '.\ \._,\ '/| '.    | '.\ \._,\ '/                              -....--'  /   `.'-...-'.'/    _.'         
+ `--'  `"                               '---'   '---'`--'  `" '---'   '---'`--'  `"                               `.. __..-'       `-...-'( _.-'            
+ */
