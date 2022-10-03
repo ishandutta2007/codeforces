e3f648bf1ca1ab2b3f238e0fd1df2fbe26382888
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 07:24:24 +0530

    alishahali1382

---
+++ alishahali1382/normal/1369/D.cpp
@@ -0,0 +1,53 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O2,unroll-loops")
+//#pragma GCC optimize("no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) {cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;}
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod=1000000007;
+const int MAXN=100010, LOG=20;
+
+ll n, m, k, u, v, x, y, t, a, b, ans;
+
+ll powmod(ll a, ll b){
+	ll res=1;
+	for (; b; b>>=1, a=a*a%mod) if (b&1) res=res*a%mod;
+	return res;
+}
+
+int Solve(){
+	cin>>n;
+	if (n<3) kill(0)
+	n-=3;
+	ans=(powmod(8, n/3+1)-1)*powmod(7, mod-2)%mod;
+	ans=ans*powmod(2, n%3+1)%mod;
+	if (n%6==0 || n%6==2) ans++;
+	if (n%6==1) ans--;
+	cout<<ans*4*powmod(3, mod-2)%mod<<'\n';
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>t;
+	while (t--) Solve();
+	
+	return 0;
+}
