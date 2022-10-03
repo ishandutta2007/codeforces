08e28be251bcd6db5db2124098df3215346749e0
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 13:38:05 +0530

    rubikun

---
+++ rubikun/normal/1172/B.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(),(x).end()
+const int mod=998244353,MAX=200005,INF=1<<30;
+vector<int> G[MAX];
+
+ll inv[MAX],fac[MAX],finv[MAX];
+
+void make(){
+    
+    fac[0]=fac[1]=1;
+    finv[0]=finv[1]=1;
+    inv[1]=1;
+    
+    for(int i=2;i<MAX;i++){
+        inv[i]=mod-inv[mod%i]*(mod/i)%mod;
+        fac[i]=fac[i-1]*(ll)i%mod;
+        finv[i]=finv[i-1]*inv[i]%mod;
+    }
+}
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    make();
+    
+    int N;cin>>N;
+    for(int i=0;i<N-1;i++){
+        int a,b;cin>>a>>b;
+        a--;b--;
+        G[a].push_back(b);
+        G[b].push_back(a);
+    }
+    ll ans=fac[G[0].size()]*ll(N)%mod;
+    for(int i=1;i<N;i++){
+        ans*=fac[G[i].size()];
+        ans%=mod;
+    }
+    cout<<ans<<endl;
+}
