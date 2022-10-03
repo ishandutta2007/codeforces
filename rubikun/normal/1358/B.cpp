f236bb77377970f1c4196608c01bf634b40ac426
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 13:26:09 +0530

    rubikun

---
+++ rubikun/normal/1358/B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define si(x) int(x.size())
+const int mod=1000000007,MAX=200005,INF=1<<30;
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int Q;cin>>Q;
+    while(Q--){
+        int N;cin>>N;
+        vector<int> A(N+1);
+        for(int i=0;i<N;i++) cin>>A[i];
+        A[N]=0;
+        sort(all(A));
+        
+        for(int i=N;i>=0;i--){
+            if(A[i]<=i){
+                cout<<i+1<<"\n";
+                break;
+            }
+        }
+        
+    }
+}
