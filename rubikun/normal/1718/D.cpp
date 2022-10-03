606018a170a5523826ededdfb2e6dc86c292efa7
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 11:10:37 +0530

    rubikun

---
+++ rubikun/normal/1718/D.cpp
@@ -0,0 +1,693 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define si(x) int(x.size())
+const int mod=998244353,MAX=300005,INF=1<<30;
+
+// BIT   
+
+// from: https://gist.github.com/yosupo06/ddd51afb727600fd95d9d8ad6c3c80c9
+// (based on AtCoder STL)
+
+#include <algorithm>
+#include <array>
+#ifdef _MSC_VER
+#include <intrin.h>
+#endif
+namespace atcoder {
+    namespace internal {
+        int ceil_pow2(int n) {
+            int x = 0;
+            while ((1U << x) < (unsigned int)(n)) x++;
+            return x;
+        }
+        int bsf(unsigned int n) {
+#ifdef _MSC_VER
+            unsigned long index;
+            _BitScanForward(&index, n);
+            return index;
+#else
+            return __builtin_ctz(n);
+#endif
+        }
+    }  // namespace internal
+    
+}  // namespace atcoder
+
+#include <cassert>
+#include <numeric>
+#include <type_traits>
+
+namespace atcoder {
+    
+    namespace internal {
+        
+#ifndef _MSC_VER
+        template <class T>
+        using is_signed_int128 =
+        typename std::conditional<std::is_same<T, __int128_t>::value ||
+        std::is_same<T, __int128>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using is_unsigned_int128 =
+        typename std::conditional<std::is_same<T, __uint128_t>::value ||
+        std::is_same<T, unsigned __int128>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using make_unsigned_int128 =
+        typename std::conditional<std::is_same<T, __int128_t>::value,
+        __uint128_t,
+        unsigned __int128>;
+        
+        template <class T>
+        using is_integral = typename std::conditional<std::is_integral<T>::value ||
+        is_signed_int128<T>::value ||
+        is_unsigned_int128<T>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using is_signed_int = typename std::conditional<(is_integral<T>::value &&
+                                                         std::is_signed<T>::value) ||
+        is_signed_int128<T>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using is_unsigned_int =
+        typename std::conditional<(is_integral<T>::value &&
+                                   std::is_unsigned<T>::value) ||
+        is_unsigned_int128<T>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using to_unsigned = typename std::conditional<
+        is_signed_int128<T>::value,
+        make_unsigned_int128<T>,
+        typename std::conditional<std::is_signed<T>::value,
+        std::make_unsigned<T>,
+        std::common_type<T>>::type>::type;
+        
+#else
+        
+        template <class T> using is_integral = typename std::is_integral<T>;
+        
+        template <class T>
+        using is_signed_int =
+        typename std::conditional<is_integral<T>::value && std::is_signed<T>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using is_unsigned_int =
+        typename std::conditional<is_integral<T>::value &&
+        std::is_unsigned<T>::value,
+        std::true_type,
+        std::false_type>::type;
+        
+        template <class T>
+        using to_unsigned = typename std::conditional<is_signed_int<T>::value,
+        std::make_unsigned<T>,
+        std::common_type<T>>::type;
+        
+#endif
+        
+        template <class T>
+        using is_signed_int_t = std::enable_if_t<is_signed_int<T>::value>;
+        
+        template <class T>
+        using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
+        
+        template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
+        
+    }  // namespace internal
+    
+}  // namespace atcoder
+
+#include <cassert>
+#include <vector>
+
+namespace atcoder {
+    
+    template <class T> struct fenwick_tree {
+        using U = internal::to_unsigned_t<T>;
+        
+    public:
+        fenwick_tree() : _n(0) {}
+        fenwick_tree(int n) : _n(n), data(n) {}
+        
+        void add(int p, T x) {
+            assert(0 <= p && p < _n);
+            p++;
+            while (p <= _n) {
+                data[p - 1] += U(x);
+                p += p & -p;
+            }
+        }
+        
+        T sum(int l, int r) {
+            assert(0 <= l && l <= r && r <= _n);
+            return sum(r) - sum(l);
+        }
+        
+    private:
+        int _n;
+        std::vector<U> data;
+        
+        U sum(int r) {
+            U s = 0;
+            while (r > 0) {
+                s += data[r - 1];
+                r -= r & -r;
+            }
+            return s;
+        }
+    };
+    
+}  // namespace atcoder
+
+
+#include <algorithm>
+#include <cassert>
+#include <iostream>
+#include <vector>
+namespace atcoder {
+    
+    template <class S,
+    S (*op)(S, S),
+    S (*e)(),
+    class F,
+    S (*mapping)(F, S),
+    F (*composition)(F, F),
+    F (*id)()>
+    struct lazy_segtree {
+    public:
+        lazy_segtree() : lazy_segtree(0) {}
+        lazy_segtree(int n) : lazy_segtree(std::vector<S>(n, e())) {}
+        lazy_segtree(const std::vector<S>& v) : _n(int(v.size())) {
+            log = internal::ceil_pow2(_n);
+            size = 1 << log;
+            d = std::vector<S>(2 * size, e());
+            lz = std::vector<F>(size, id());
+            for (int i = 0; i < _n; i++) d[size + i] = v[i];
+            for (int i = size - 1; i >= 1; i--) {
+                update(i);
+            }
+        }
+        
+        void set(int p, S x) {
+            assert(0 <= p && p < _n);
+            p += size;
+            for (int i = log; i >= 1; i--) push(p >> i);
+            d[p] = x;
+            for (int i = 1; i <= log; i++) update(p >> i);
+        }
+        
+        S get(int p) {
+            assert(0 <= p && p < _n);
+            p += size;
+            for (int i = log; i >= 1; i--) push(p >> i);
+            return d[p];
+        }
+        
+        S prod(int l, int r) {
+            assert(0 <= l && l <= r && r <= _n);
+            if (l == r) return e();
+            
+            l += size;
+            r += size;
+            
+            for (int i = log; i >= 1; i--) {
+                if (((l >> i) << i) != l) push(l >> i);
+                if (((r >> i) << i) != r) push(r >> i);
+            }
+            
+            S sml = e(), smr = e();
+            while (l < r) {
+                if (l & 1) sml = op(sml, d[l++]);
+                if (r & 1) smr = op(d[--r], smr);
+                l >>= 1;
+                r >>= 1;
+            }
+            
+            return op(sml, smr);
+        }
+        
+        S all_prod() { return d[1]; }
+        
+        void apply(int p, F f) {
+            assert(0 <= p && p < _n);
+            p += size;
+            for (int i = log; i >= 1; i--) push(p >> i);
+            d[p] = mapping(f, d[p]);
+            for (int i = 1; i <= log; i++) update(p >> i);
+        }
+        void apply(int l, int r, F f) {
+            assert(0 <= l && l <= r && r <= _n);
+            if (l == r) return;
+            
+            l += size;
+            r += size;
+            
+            for (int i = log; i >= 1; i--) {
+                if (((l >> i) << i) != l) push(l >> i);
+                if (((r >> i) << i) != r) push((r - 1) >> i);
+            }
+            
+            {
+                int l2 = l, r2 = r;
+                while (l < r) {
+                    if (l & 1) all_apply(l++, f);
+                    if (r & 1) all_apply(--r, f);
+                    l >>= 1;
+                    r >>= 1;
+                }
+                l = l2;
+                r = r2;
+            }
+            
+            for (int i = 1; i <= log; i++) {
+                if (((l >> i) << i) != l) update(l >> i);
+                if (((r >> i) << i) != r) update((r - 1) >> i);
+            }
+        }
+        
+        template <bool (*g)(S)> int max_right(int l) {
+            return max_right(l, [](S x) { return g(x); });
+        }
+        template <class G> int max_right(int l, G g) {
+            assert(0 <= l && l <= _n);
+            assert(g(e()));
+            if (l == _n) return _n;
+            l += size;
+            for (int i = log; i >= 1; i--) push(l >> i);
+            S sm = e();
+            do {
+                while (l % 2 == 0) l >>= 1;
+                if (!g(op(sm, d[l]))) {
+                    while (l < size) {
+                        push(l);
+                        l = (2 * l);
+                        if (g(op(sm, d[l]))) {
+                            sm = op(sm, d[l]);
+                            l++;
+                        }
+                    }
+                    return l - size;
+                }
+                sm = op(sm, d[l]);
+                l++;
+            } while ((l & -l) != l);
+            return _n;
+        }
+        
+        template <bool (*g)(S)> int min_left(int r) {
+            return min_left(r, [](S x) { return g(x); });
+        }
+        template <class G> int min_left(int r, G g) {
+            assert(0 <= r && r <= _n);
+            assert(g(e()));
+            if (r == 0) return 0;
+            r += size;
+            for (int i = log; i >= 1; i--) push((r - 1) >> i);
+            S sm = e();
+            do {
+                r--;
+                while (r > 1 && (r % 2)) r >>= 1;
+                if (!g(op(d[r], sm))) {
+                    while (r < size) {
+                        push(r);
+                        r = (2 * r + 1);
+                        if (g(op(d[r], sm))) {
+                            sm = op(d[r], sm);
+                            r--;
+                        }
+                    }
+                    return r + 1 - size;
+                }
+                sm = op(d[r], sm);
+            } while ((r & -r) != r);
+            return 0;
+        }
+        
+    private:
+        int _n, size, log;
+        std::vector<S> d;
+        std::vector<F> lz;
+        
+        void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+        void all_apply(int k, F f) {
+            d[k] = mapping(f, d[k]);
+            if (k < size) lz[k] = composition(f, lz[k]);
+        }
+        void push(int k) {
+            all_apply(2 * k, lz[k]);
+            all_apply(2 * k + 1, lz[k]);
+            lz[k] = id();
+        }
+    };
+    
+}  // namespace atcoder
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace atcoder {
+    
+    template <class S, S (*op)(S, S), S (*e)()> struct segtree {
+    public:
+        segtree() : segtree(0) {}
+        segtree(int n) : segtree(std::vector<S>(n, e())) {}
+        segtree(const std::vector<S>& v) : _n(int(v.size())) {
+            log = internal::ceil_pow2(_n);
+            size = 1 << log;
+            d = std::vector<S>(2 * size, e());
+            for (int i = 0; i < _n; i++) d[size + i] = v[i];
+            for (int i = size - 1; i >= 1; i--) {
+                update(i);
+            }
+        }
+        
+        void set(int p, S x) {
+            assert(0 <= p && p < _n);
+            p += size;
+            d[p] = x;
+            for (int i = 1; i <= log; i++) update(p >> i);
+        }
+        
+        S get(int p) {
+            assert(0 <= p && p < _n);
+            return d[p + size];
+        }
+        
+        S prod(int l, int r) {
+            assert(0 <= l && l <= r && r <= _n);
+            S sml = e(), smr = e();
+            l += size;
+            r += size;
+            
+            while (l < r) {
+                if (l & 1) sml = op(sml, d[l++]);
+                if (r & 1) smr = op(d[--r], smr);
+                l >>= 1;
+                r >>= 1;
+            }
+            return op(sml, smr);
+        }
+        
+        S all_prod() { return d[1]; }
+        
+        template <bool (*f)(S)> int max_right(int l) {
+            return max_right(l, [](S x) { return f(x); });
+        }
+        template <class F> int max_right(int l, F f) {
+            assert(0 <= l && l <= _n);
+            assert(f(e()));
+            if (l == _n) return _n;
+            l += size;
+            S sm = e();
+            do {
+                while (l % 2 == 0) l >>= 1;
+                if (!f(op(sm, d[l]))) {
+                    while (l < size) {
+                        l = (2 * l);
+                        if (f(op(sm, d[l]))) {
+                            sm = op(sm, d[l]);
+                            l++;
+                        }
+                    }
+                    return l - size;
+                }
+                sm = op(sm, d[l]);
+                l++;
+            } while ((l & -l) != l);
+            return _n;
+        }
+        
+        template <bool (*f)(S)> int min_left(int r) {
+            return min_left(r, [](S x) { return f(x); });
+        }
+        template <class F> int min_left(int r, F f) {
+            assert(0 <= r && r <= _n);
+            assert(f(e()));
+            if (r == 0) return 0;
+            r += size;
+            S sm = e();
+            do {
+                r--;
+                while (r > 1 && (r % 2)) r >>= 1;
+                if (!f(op(d[r], sm))) {
+                    while (r < size) {
+                        r = (2 * r + 1);
+                        if (f(op(d[r], sm))) {
+                            sm = op(d[r], sm);
+                            r--;
+                        }
+                    }
+                    return r + 1 - size;
+                }
+                sm = op(d[r], sm);
+            } while ((r & -r) != r);
+            return 0;
+        }
+        
+    private:
+        int _n, size, log;
+        std::vector<S> d;
+        
+        void update(int k) { d[k] = op(d[2 * k], d[2 * k + 1]); }
+    };
+    
+}  // namespace atcoder
+
+using T=pair<int,int>;
+
+T f(T a,T b){
+    return max(a,b);
+}
+
+T ti(){
+    return mp(-1,-1);
+}
+
+atcoder::segtree<T,f,ti> seg;
+
+vector<int> G[MAX];
+vector<int> A,B;
+int ma[MAX],mi[MAX];
+bool ok=true;
+
+void maketree(int l,int r,int p){
+    if(l+1==r){
+        if(p!=-1) G[p].push_back(l);
+        return;
+    }
+    if(l==r) return;
+    
+    auto m=seg.prod(l,r).se;
+    
+    if(p!=-1) G[p].push_back(m);
+    
+    maketree(l,m,m);
+    maketree(m+1,r,m);
+}
+
+void DFS(int u){
+    if(B[u]){
+        if(ma[u]<B[u]){
+            ok=false;
+        }
+        ma[u]=B[u];
+    }
+    for(int to:G[u]){
+        ma[to]=ma[u]-1;
+        DFS(to);
+        chmax(mi[u],mi[to]+1);
+    }
+    if(B[u]){
+        if(mi[u]>B[u]){
+            ok=false;
+        }
+        mi[u]=B[u];
+    }
+}
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int QQ;cin>>QQ;
+    while(QQ--){
+        int N,Q;cin>>N>>Q;
+        ok=true;
+        for(int i=0;i<N;i++) G[i].clear();
+        A.resize(N);
+        for(int i=0;i<N;i++){
+            cin>>A[i];
+        }
+        vector<T> def(N);
+        for(int i=0;i<N;i++) def[i]=mp(A[i],i);
+        seg=atcoder::segtree<T,f,ti>(def);
+        maketree(0,N,-1);
+        
+        B.resize(N);
+        for(int i=0;i<N;i++){
+            cin>>B[i];
+            ma[i]=INF;
+            mi[i]=-INF;
+        }
+        int ro=seg.all_prod().se;
+        DFS(ro);
+        
+        int K=0;
+        for(int i=0;i<N;i++){
+            if(B[i]==0) K++;
+        }
+        
+        vector<int> S(K-1);
+        for(int i=0;i<K-1;i++){
+            cin>>S[i];
+        }
+        sort(all(S));
+        
+        vector<int> use;
+        for(int i=0;i<N;i++){
+            use.push_back(mi[i]);
+            use.push_back(ma[i]);
+        }
+        for(int a:S) use.push_back(a);
+        
+        vector<int> que(Q);
+        for(int q=0;q<Q;q++){
+            cin>>que[q];
+            use.push_back(que[q]);
+        }
+        sort(all(use));
+        use.erase(unique(all(use)),use.end());
+        
+        if(!ok){
+            for(int q=0;q<Q;q++) cout<<"NO\n";
+            continue;
+        }
+        
+        if(K==N){
+            for(int q=0;q<Q;q++) cout<<"YES\n";
+            continue;
+        }
+        
+        vector<pair<int,int>> lim;
+        
+        for(int i=0;i<N;i++){
+            if(B[i]==0){
+                int l=lower_bound(all(use),mi[i])-use.begin();
+                int r=lower_bound(all(use),ma[i])-use.begin();
+                lim.push_back(mp(l,r));
+            }
+        }
+        
+        vector<vector<int>> wh(si(use));
+        
+        for(auto [l,r]:lim){
+            wh[l].push_back(r);
+        }
+        
+        for(int i=0;i<si(use);i++){
+            sort(all(wh[i]));
+        }
+        
+        vector<int> aru(si(use));
+        for(int i=0;i<si(S);i++){
+            S[i]=lower_bound(all(use),S[i])-use.begin();
+            aru[S[i]]=true;
+        }
+        
+        for(int q=0;q<Q;q++){
+            que[q]=lower_bound(all(use),que[q])-use.begin();
+        }
+        
+        {
+            priority_queue<int,vector<int>,greater<int>> PQ;
+            int need=INF,kara=-1;
+            for(int j=0;j<si(use);j++){
+                for(int r:wh[j]){
+                    PQ.push(r);
+                }
+                if(aru[j]){
+                    if(PQ.empty()){
+                        need=-1;
+                        break;
+                    }else{
+                        if(PQ.top()<j){
+                            need=PQ.top();
+                            break;
+                        }else{
+                            PQ.pop();
+                        }
+                    }
+                }
+            }
+            
+            if(need==INF){
+                need=PQ.top();
+            }
+            
+            if(need==-1){
+                for(int q=0;q<Q;q++) cout<<"NO\n";
+                continue;
+            }
+            
+            int left=-1,right=need+1;
+            while(right-left>1){
+                int mid=(left+right)/2;
+                aru[mid]++;
+                
+                priority_queue<int,vector<int>,greater<int>> PQ;
+                bool ok=true;
+                for(int j=0;j<si(use);j++){
+                    for(int r:wh[j]){
+                        PQ.push(r);
+                    }
+                    for(int t=0;t<aru[j];t++){
+                        if(PQ.empty()){
+                            ok=false;
+                            break;
+                        }else{
+                            if(PQ.top()<j){
+                                ok=false;
+                                break;
+                            }else{
+                                PQ.pop();
+                            }
+                        }
+                    }
+                    if(!ok) break;
+                }
+                
+                if(ok) right=mid;
+                else left=mid;
+                
+                aru[mid]--;
+            }
+            
+            for(int q=0;q<Q;q++){
+                if(right<=que[q]&&que[q]<=need) cout<<"YES\n";
+                else cout<<"NO\n";
+            }
+        }
+    }
+}
