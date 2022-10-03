7d5361c4058da1e18af3863fae68504d1a25227d
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 06:40:58 +0530

    aoligei

---
+++ aoligei/normal/1467/D.cpp
@@ -0,0 +1,114 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <bitset>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+#define itn int
+#define nit int
+#define ll long long
+#define ms multiset
+#define F(i,a,b) for(register int i=a,i##end=b;i<=i##end;++i)
+#define UF(i,a,b) for(register int i=a,i##end=b;i>=i##end;--i)
+#define re register
+#define ri re int
+#define il inline
+#define pii pair<int,int>
+#define mod 1000000007ll
+#define cp complex<double>
+//#pra gma G CC opti mize(3)
+using namespace std;
+using std::bitset;
+//using namespace __gnu_pbds;
+const double Pi=acos(-1);
+namespace fastIO {
+	template<class T>
+	inline void read(T &x) {
+		x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		if(fu)x=-x;
+	}
+	inline int read() {
+		int x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		return fu?-x:x;
+	}
+	char _n_u_m_[40];
+	template<class T>
+	inline void write(T x ) {
+		if(x==0){
+			putchar('0');
+			return;
+		}
+		T tmp = x > 0 ? x : -x ;
+		if( x < 0 ) putchar('-') ;
+		register int cnt = 0 ;
+		while( tmp > 0 ) {
+			_n_u_m_[ cnt ++ ] = tmp % 10 + '0' ;
+			tmp /= 10 ;
+		}
+		while( cnt > 0 ) putchar(_n_u_m_[ -- cnt ]) ;
+	}
+	template<class T>
+	inline void write(T x ,char ch) {
+		write(x);
+		putchar(ch);
+	}
+}
+using namespace fastIO;
+ll n,k,q,a[5002],v[5002],t[5002][5002],dp[5002][5002],ans[5002],now,temp;
+int main() {
+	cin>>n>>k>>q;k++;
+	F(i,1,n)read(a[i]);
+	F(i,1,n)t[k][i]=dp[1][i]=1;
+	F(i,2,k){
+		F(j,1,n){
+			dp[i][j]=dp[i-1][j-1]+dp[i-1][j+1];
+			dp[i][j]%=mod;
+		}
+	}
+	F(i,1,k){
+		F(j,1,n){
+			ans[j]+=dp[i][j]*dp[k-i+1][j];
+			ans[j]%=mod;
+		}
+	}F(i,1,n)now+=a[i]*ans[i],now%=mod;
+	while(q--){
+		ri p=read();read(temp);
+		now+=temp*ans[p];
+		now-=a[p]*ans[p];
+		a[p]=temp;
+		now%=mod;
+		if(now<0)now+=mod;write(now,'\n');
+	}
+	return 0;
+}
