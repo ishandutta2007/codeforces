45915167cc88ce658bb57fe1b846ca8b07ee01de
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 05:25:00 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/1197/A.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define debug(...) fprintf(stderr,__VA_ARGS__)
+#define MN 300005
+#define MM 600005
+#define ll long long
+#define mod 998244353
+int n,m,q,k,t;
+int a[MN],b[MN],f[MN],g[MN];
+int h[MN],nxt[MM],to[MM],tot;
+inline void ins(int x,int y){nxt[++tot]=h[x];to[tot]=y;h[x]=tot;}
+int main(){
+	scanf("%d",&t);
+	while(t--){
+		scanf("%d",&n);
+		F(i,1,n)scanf("%d",a+i);
+		sort(a+1,a+n+1);
+		printf("%d\n",std::min(n-2,a[n-1]-1));
+	}
+	return 0;
+}
