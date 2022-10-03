8a11e2de18d7b10249987e5a359714feeb33b29b
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 06:40:49 +0530

    aoligei

---
+++ aoligei/normal/1467/C.cpp
@@ -0,0 +1,110 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <bitset>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+#define itn int
+#define nit int
+#define ll long long
+#define ms multiset
+#define F(i,a,b) for(register int i=a,i##end=b;i<=i##end;++i)
+#define UF(i,a,b) for(register int i=a,i##end=b;i>=i##end;--i)
+#define re register
+#define ri re int
+#define il inline
+#define pii pair<int,int>
+#define cp complex<double>
+//#pra gma G CC opti mize(3)
+using namespace std;
+using std::bitset;
+//using namespace __gnu_pbds;
+const double Pi=acos(-1);
+namespace fastIO {
+	template<class T>
+	inline void read(T &x) {
+		x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		if(fu)x=-x;
+	}
+	inline int read() {
+		int x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		return fu?-x:x;
+	}
+	char _n_u_m_[40];
+	template<class T>
+	inline void write(T x ) {
+		if(x==0){
+			putchar('0');
+			return;
+		}
+		T tmp = x > 0 ? x : -x ;
+		if( x < 0 ) putchar('-') ;
+		register int cnt = 0 ;
+		while( tmp > 0 ) {
+			_n_u_m_[ cnt ++ ] = tmp % 10 + '0' ;
+			tmp /= 10 ;
+		}
+		while( cnt > 0 ) putchar(_n_u_m_[ -- cnt ]) ;
+	}
+	template<class T>
+	inline void write(T x ,char ch) {
+		write(x);
+		putchar(ch);
+	}
+}
+using namespace fastIO;
+ll mmin=LLONG_MAX,temp,tot,qwq,t1,t2,t3;
+int main() {
+ri n1=read(),n2=read(),n3=read();t1=t2=t3=LLONG_MAX;
+qwq=0;
+F(i,1,n1){
+	read(temp);t1=min(t1,temp);
+	tot+=temp;
+	qwq+=temp;
+}mmin=min(mmin,qwq);
+qwq=0;
+F(i,1,n2){
+	read(temp);t2=min(t2,temp);
+	tot+=temp;
+	qwq+=temp;
+}mmin=min(mmin,qwq);
+qwq=0;
+F(i,1,n3){
+	read(temp);t3=min(t3,temp);
+	tot+=temp;
+	qwq+=temp;
+}mmin=min(mmin,qwq);t1<<=1,t2<<=1,t3<<=1;
+cout<<max(max(tot-t1-t3,tot-t2-t3),max(tot-2*mmin,tot-t1-t2));
+	return 0;
+}
