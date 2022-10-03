80f9ccb800daf15b560e51ea6daa282c83428afb
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 10:18:57 +0530

    alishahali1382

---
+++ alishahali1382/normal/553/A.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const int mod = 1000000007;
+const int MAXN = 100010;
+
+ll n, m, l, x, s, k, ans=1;
+ll F[1000001];
+ll I[1000001];
+
+ll powmod(ll a, ll b){
+	if (!b) return 1;
+	if (b%2) return (powmod(a*a%mod, b>>1)*a)%mod;
+	return powmod(a*a%mod, b>>1);
+}
+
+ll C(ll n, ll r){
+	if (n<0 || r<0 || r>n) return 0;
+	return (F[n]*I[r]%mod*I[n-r])%mod;
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	F[0]=1;
+	for (int i=1; i<=1000000; i++) F[i]=(F[i-1]*i)%mod;
+	I[0]=1;
+	for (int i=1; i<=1000000; i++) I[i]=(I[i-1]*powmod(i, mod-2))%mod;
+	
+	cin>>n>>s;
+	for (int i=1; i<n; i++){
+		//cerr<<s<<' '<<ans<<endl;
+		cin>>x;
+		ans=(ans*C(s+x-1, x-1))%mod;
+		s+=x;
+	}
+	cout<<ans<<endl;
+	return 0;
+}
+/*
+          .---.                                                                     .---.              ...-'  |`. ..-'''-.       / .--. \        .-''-.     
+          |   |.--.                       .                     .                   |   |.--.          |      |  |\.-'''\ \     ' '    ' '     .' .-.  )    
+          |   ||__|                     .'|                   .'|                   |   ||__|          ....   |  |       | |    \ \    / /    / .'  / /     
+          |   |.--.                    <  |                  <  |                   |   |.--.            -|   |  |    __/ /      `.`'--.'    (_/   / /      
+    __    |   ||  |                     | |             __    | |             __    |   ||  |             |   |  |   |_  '.      / `'-. `.        / /       
+ .:--.'.  |   ||  |                 _   | | .'''-.   .:--.'.  | | .'''-.   .:--.'.  |   ||  |          ...'   `--'      `.  \   ' /    `. \      / /        
+/ |   \ | |   ||  |               .' |  | |/.'''. \ / |   \ | | |/.'''. \ / |   \ | |   ||  |          |         |`.      \ '. / /       \ '    . '         
+`" __ | | |   ||  |              .   | /|  /    | | `" __ | | |  /    | | `" __ | | |   ||  |          ` --------\ |       , || |         | |  / /    _.-') 
+ .'.''| | |   ||__|            .'.'| |//| |     | |  .'.''| | | |     | |  .'.''| | |   ||__|           `---------'        | || |         | |.' '  _.'.-''  
+/ /   | |_'---'              .'.'.-'  / | |     | | / /   | |_| |     | | / /   | |_'---'                                 / ,' \ \       / //  /.-'_.'      
+\ \._,\ '/                   .'   \_.'  | '.    | '.\ \._,\ '/| '.    | '.\ \._,\ '/                              -....--'  /   `.'-...-'.'/    _.'         
+ `--'  `"                               '---'   '---'`--'  `" '---'   '---'`--'  `"                               `.. __..-'       `-...-'( _.-'            
+ */
