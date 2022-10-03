b58a3def54d44dd270961441dafe8cd61a8b728c
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 10:22:06 +0530

    alishahali1382

---
+++ alishahali1382/normal/858/A.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+
+const ld eps=1e-10;
+const int inf=1000000010;
+const int mod = 1000 * 1000 * 1000 + 7;
+const int MAXN = 100010;
+
+ll n, k, x=1;
+
+int main(){
+	ios_base::sync_with_stdio(false);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>k;
+	for (int i=0; i<k; i++) x*=10;
+	cout<<x*n/__gcd(x, n)<<endl;
+	return 0;
+}
