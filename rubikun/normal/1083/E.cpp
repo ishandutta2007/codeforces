554cf14ebb2f04992a32e8a54f189fb6299ec408
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 12:54:48 +0530

    rubikun

---
+++ rubikun/normal/1083/E.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define si(x) int(x.size())
+const int mod=1000000007,MAX=1000005;
+const ll INF=1LL<<60;
+
+/**
+ * Author: Simon Lindholm
+ * Date: 2017-04-20
+ * License: CC0
+ * Source: own work
+ * Description: Container where you can add lines of the form kx+m, and query maximum values at points x.
+ *  Useful for dynamic programming (``convex hull trick'').
+ * Time: O(\log N)
+ * Status: stress-tested
+ */
+#pragma once
+
+struct Line {
+    mutable ll k, m, p;
+    bool operator<(const Line& o) const { return k < o.k; }
+    bool operator<(ll x) const { return p < x; }
+};
+
+struct LineContainer : multiset<Line, less<>> {
+    // (for doubles, use inf = 1/.0, div(a,b) = a/b)
+    static const ll inf = LLONG_MAX;
+    ll div(ll a, ll b) { // floored division
+        return a / b - ((a ^ b) < 0 && a % b); }
+    bool isect(iterator x, iterator y) {
+        if (y == end()) return x->p = inf, 0;
+        if (x->k == y->k) x->p = x->m > y->m ? inf : -inf;
+        else x->p = div(y->m - x->m, x->k - y->k);
+        return x->p >= y->p;
+    }
+    void add(ll k, ll m) {
+        auto z = insert({k, m, 0}), y = z++, x = y;
+        while (isect(y, z)) z = erase(z);
+        if (x != begin() && isect(--x, y)) isect(x, y = erase(y));
+        while ((y = x) != begin() && (--x)->p >= y->p)
+            isect(x, erase(y));
+    }
+    ll query(ll x) {
+        assert(!empty());
+        auto l = *lower_bound(x);
+        return l.k * x + l.m;
+    }
+};
+
+ll dp[MAX];
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int N;cin>>N;
+    
+    struct dat{
+        ll x;
+        ll y;
+        ll cost;
+    };
+    
+    vector<dat> S(N);
+    
+    for(int i=0;i<N;i++) cin>>S[i].x>>S[i].y>>S[i].cost;
+    
+    sort(all(S),[](auto a,auto b){
+        return a.x<b.x;
+    });
+    
+    dp[0]=0;
+    LineContainer cht;
+    cht.add(0,0);
+    
+    for(int i=0;i<N;i++){
+        ll ma=cht.query(S[i].y);
+        dp[i+1]=ma+S[i].x*S[i].y-S[i].cost;
+        cht.add(-S[i].x,dp[i+1]);
+    }
+    
+    ll ans=0;
+    
+    for(int i=0;i<=N;i++) chmax(ans,dp[i]);
+    
+    cout<<ans<<endl;
+    
+}
