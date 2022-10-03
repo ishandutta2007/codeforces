887b04b3e6265aaddf0a00cecaad4e98043a5758
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 15:11:41 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/1539/A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define debug(...) fprintf(stderr,__VA_ARGS__)
+#define Debug debug("Passing [%s] in LINE %d\n",__FUNCTION__,__LINE__)
+#define MN 300005
+#define MM 600005
+#define ll long long
+#define mod 998244353
+#define inf 0x3f3f3f3f
+#define infll 0x3f3f3f3f3f3f3f3f
+typedef pair<int,int> pii;
+#define pb push_back
+#define mkp make_pair
+#define fi first
+#define se second
+int main(){int tests=1;scanf("%d",&tests);
+while(tests--){
+	int n,x,t;
+	scanf("%d%d%d",&n,&x,&t);
+	int d=t/x;
+	if(n>d)
+		printf("%lld\n",(ll)d*n-(ll)d*(d+1)/2);
+	else
+		printf("%lld\n",(ll)n*(n-1)/2);
+}	return 0;
+}
