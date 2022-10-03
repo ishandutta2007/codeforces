7b1d8336af322e9bf20138654cd6e8e51f302861
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:19:58 +0530

    alishahali1382

---
+++ alishahali1382/normal/839/A.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 100010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, cur;
+int A[MAXN];
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>k;
+	for (int i=1; i<=n; i++){
+		cin>>x;
+		cur+=x;
+		k-=min(cur, 8);
+		cur-=min(cur, 8);
+		if (k<=0) kill(i)
+	}
+	kill(-1)
+	
+	return 0;
+}
