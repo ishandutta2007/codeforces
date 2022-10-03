ae0e16934d1f01efa34c1149a50edb9bfe75d781
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:19:04 +0530

    alishahali1382

---
+++ alishahali1382/normal/1190/C.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 100010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+int A[MAXN];
+int ps[MAXN];
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>k;
+	for (int i=1; i<=n; i++){
+		char ch;
+		cin>>ch;
+		A[i]=ch-'0';
+		ps[i]=ps[i-1]+A[i];
+	}
+	for (int i=1; i+k-1<=n; i++){
+		int l=i, r=l+k-1;
+		int cl=ps[l-1], cr=ps[n]-ps[r];
+		if (cl+cr==0 || cl+cr==n-k) kill("tokitsukaze")
+	}
+	if (k==n-1) kill("quailty")
+	if (2*k<n) kill("once again")
+	
+	bool f=1;
+	for (int i=1; i+k-1<=n; i++){
+		int l=i, r=l+k-1;
+		int cl=ps[l-1], cr=ps[n]-ps[r];
+		if ((cl==0 || cr==0) && (cl==l-1 || cr==n-r)) continue ;
+		f=0;
+	}
+	
+	if (f) kill("quailty")
+	kill("once again")
+	
+	return 0;
+}
