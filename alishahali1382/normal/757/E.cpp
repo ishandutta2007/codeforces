35b458044ba3028fa8c0022cd2590447824718bd
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:32:47 +0530

    alishahali1382

---
+++ alishahali1382/normal/757/E.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 1000110, LOG=20;
+
+ll powmod(ll a, ll b){
+	if (!b) return 1;
+	if (b&1) return a*powmod(a*a%mod, b>>1)%mod;
+	return powmod(a*a%mod, b>>1);
+}
+
+ll n, m, k, q, x, y, t, a, b;
+int sieve[MAXN];
+ll F[MAXN];
+ll I[MAXN];
+
+ll C(ll n, ll r){
+	if (r<0 || r>n) return 0;
+	return (F[n]*I[r]%mod)*I[n-r]%mod;
+}
+
+void solve(){
+	cin>>k>>n;
+	ll ans=1;
+	while (n>1){
+		ll p=sieve[n], t=0;
+		while (n%p==0) n/=p, t++;
+		ans=ans*(C(t+k, k) + C(t+k-1, k))%mod;
+	}
+	cout<<ans<<'\n';
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	F[0]=1;
+	for (int i=1; i<MAXN; i++) F[i]=F[i-1]*i%mod;
+	I[MAXN-1]=powmod(F[MAXN-1], mod-2);
+	for (int i=MAXN-1; i; i--) I[i-1]=I[i]*i%mod;
+	for (ll i=2; i<MAXN; i++) if (!sieve[i]){
+		sieve[i]=i;
+		for (ll j=i*i; j<MAXN; j+=i) sieve[j]=i;
+	}
+	cin>>q;
+	while (q--) solve();
+	
+	return 0;
+}
+/*
+      ___           ___                            ___           ___           ___           ___           ___           ___
+     /\  \         /\__\      ___                 /\  \         /\__\         /\  \         /\__\         /\  \         /\__\      ___
+    /::\  \       /:/  /     /\  \               /::\  \       /:/  /        /::\  \       /:/  /        /::\  \       /:/  /     /\  \
+   /:/\:\  \     /:/  /      \:\  \             /:/\ \  \     /:/__/        /:/\:\  \     /:/__/        /:/\:\  \     /:/  /      \:\  \
+  /::\~\:\  \   /:/  /       /::\__\           _\:\~\ \  \   /::\  \ ___   /::\~\:\  \   /::\  \ ___   /::\~\:\  \   /:/  /       /::\__\
+ /:/\:\ \:\__\ /:/__/     __/:/\/__/          /\ \:\ \ \__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/__/     __/:/\/__/
+ \/__\:\/:/  / \:\  \    /\/:/  /             \:\ \:\ \/__/ \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \:\  \    /\/:/  /
+      \::/  /   \:\  \   \::/__/               \:\ \:\__\        \::/  /       \::/  /       \::/  /       \::/  /   \:\  \   \::/__/
+      /:/  /     \:\  \   \:\__\                \:\/:/  /        /:/  /        /:/  /        /:/  /        /:/  /     \:\  \   \:\__\
+     /:/  /       \:\__\   \/__/                 \::/  /        /:/  /        /:/  /        /:/  /        /:/  /       \:\__\   \/__/
+     \/__/         \/__/                          \/__/         \/__/         \/__/         \/__/         \/__/         \/__/
+*/
