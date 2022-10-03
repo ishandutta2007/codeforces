b85ce507b04ad99354b13b3bc5119ab617128164
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 11:51:14 +0530

    rubikun

---
+++ rubikun/normal/755/C.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define si(x) int(x.size())
+const int mod=1000000007,MAX=1000005;
+const ll INF=1LL<<60;
+
+struct UF{
+    int n;
+    vector<int> par,size,edge;
+    
+    void init(int n_){
+        n=n_;
+        par.assign(n,-1);
+        size.assign(n,1);
+        edge.assign(n,0);
+        
+        for(int i=0;i<n;i++){
+            par[i]=i;
+        }
+    }
+    
+    int root(int a){
+        if(par[a]==a) return a;
+        else return par[a]=root(par[a]);
+    }
+    
+    void unite(int a,int b){
+        edge[root(a)]++;
+        if(root(a)!=root(b)){
+            size[root(a)]+=size[root(b)];
+            edge[root(a)]+=edge[root(b)];
+            par[root(b)]=root(a);
+        }
+    }
+    
+    bool check(int a,int b){
+        return root(a)==root(b);
+    }
+};
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int N;cin>>N;
+    UF uf;uf.init(N);
+    for(int i=0;i<N;i++){
+        int x;cin>>x;x--;
+        uf.unite(x,i);
+    }
+    int ans=0;
+    for(int i=0;i<N;i++){
+        if(uf.root(i)==i) ans++;
+    }
+    cout<<ans<<endl;
+}
