76c670154a4cec60328e7306fc08eb785a82844b
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 06:34:06 +0530

    aoligei

---
+++ aoligei/normal/1566/D2.cpp
@@ -0,0 +1,118 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <bitset>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+#define itn int
+#define nit int
+#define ll long long
+#define ms multiset
+#define F(i,a,b) for(register int i=a,i##end=b;i<=i##end;++i)
+#define UF(i,a,b) for(register int i=a,i##end=b;i>=i##end;--i)
+#define re register
+#define ri re int
+#define il inline
+#define pii pair<int,int>
+#define cp complex<double>
+#define vi vector<int>
+#define ull unsigned long long
+using namespace std;
+using std::bitset;
+//using namespace __gnu_pbds;
+const double Pi=acos(-1);
+namespace fastIO {
+	template<class T>
+	inline void read(T &x) {
+		x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		if(fu)x=-x;
+	}
+	inline int read() {
+		int x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		return fu?-x:x;
+	}
+	template<class T,class... Args>
+	inline void read(T& t,Args&... args) {
+		read(t);
+		read(args...);
+	}
+	char _n_u_m_[40];
+	template<class T>
+	inline void write(T x) {
+		if(x==0){
+			putchar('0');
+			return;
+		}
+		T tmp = x > 0 ? x : -x ;
+		if( x < 0 ) putchar('-') ;
+		register int cnt = 0 ;
+		while( tmp > 0 ) {
+			_n_u_m_[ cnt ++ ] = tmp % 10 + '0' ;
+			tmp /= 10 ;
+		}
+		while( cnt > 0 ) putchar(_n_u_m_[ -- cnt ]) ;
+	}
+	template<class T>
+	inline void write(T x ,char ch) {
+		write(x);
+		putchar(ch);
+	}
+}
+using namespace fastIO;
+pii a[100002],p[100002];
+int cnt,m,n,A[100002];
+int vis[302][302];
+int main() {
+	F(iakioi,1,read()){
+		read(n);read(m);
+		F(i,1,n*m)a[i]=make_pair(read(),i);
+		memset(vis,0,sizeof(vis));
+		sort(a+1,a+n*m+1);
+		int nx=1,ny=1;
+		F(i,1,n*m){
+			A[a[i].second]=a[i].first;
+			p[a[i].second]=make_pair(nx,ny);
+			++ny;
+			if(ny>m)++nx,ny=1;
+		}
+		int ans=0;
+		A[0]=INT_MAX;
+		F(i,1,n*m){//cerr<<p[i].first<<' '<<p[i].second<<endl;
+			F(j,1,p[i].second)ans+=(A[vis[p[i].first][j]]<A[i]);
+			vis[p[i].first][p[i].second]=i;
+		}
+		write(ans,'\n');
+	}
+	return 0;
+}
