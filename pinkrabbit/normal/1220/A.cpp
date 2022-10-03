acba81277c44fc1e147c2b30191a8019767ddc09
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 05:17:55 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/1220/A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define debug(...) fprintf(stderr,__VA_ARGS__)
+#define Debug debug("Passing [%s] in LINE %d\n",__FUNCTION__,__LINE__)
+#define MN 300005
+#define MM 600005
+#define ll long long
+#define mod 998244353
+typedef pair<int,int> pii;
+#define pb push_back
+#define mp make_pair
+#define fi first
+#define se second
+int n,m,q,k;
+int a[MN],b[MN],f[MN],g[MN];
+int h[MN],nxt[MM],to[MM],tot;
+inline void ins(int x,int y){nxt[++tot]=h[x];to[tot]=y;h[x]=tot;}
+char s[MN];
+int main(){
+	scanf("%*d%s",s+1),n=strlen(s+1);
+	int a=0,b=0;
+	F(i,1,n){if(s[i]=='z')++a;if(s[i]=='n')++b;}
+	while(b--)printf("1 ");
+	while(a--)printf("0 ");
+	return 0;
+}
