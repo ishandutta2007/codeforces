ea8818959e1423d000d8395ade755bff10aa1b75
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:31:39 +0530

    alishahali1382

---
+++ alishahali1382/normal/570/D.cpp
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 500010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b;
+int P[MAXN];
+bool ans[MAXN];
+int H[MAXN];
+vector<pii> query[MAXN];
+vector<int> deq[MAXN];
+char ch[MAXN];
+
+void merge(vector<int> &d1, vector<int> &d2){
+	if (d1.size()<d2.size()) d1.swap(d2);
+	for (int i=0; i<d2.size(); i++) d1[d1.size()-1-i]^=d2[d2.size()-1-i];
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>m;
+	for (int i=2; i<=n; i++) cin>>P[i], H[i]=H[P[i]]+1;
+	for (int i=1; i<=n; i++) cin>>ch[i];
+	for (int i=1; i<=m; i++){
+		cin>>v>>x;
+		query[v].pb({x-H[v]-1, i});
+	}
+	for (int i=n; i; i--){
+		deq[i].pb(1<<(ch[i]-'a'));
+		for (pii p:query[i]) ans[p.second]=(p.first<0 || deq[i].size()<=p.first || __builtin_popcount(deq[i][deq[i].size()-1-p.first])<=1);
+		merge(deq[P[i]], deq[i]);
+	}
+	
+	for (int i=1; i<=m; i++) cout<<(ans[i]?"Yes":"No")<<'\n';
+	
+	return 0;
+}
+/*
+      ___           ___                            ___           ___           ___           ___           ___           ___
+     /\  \         /\__\      ___                 /\  \         /\__\         /\  \         /\__\         /\  \         /\__\      ___
+    /::\  \       /:/  /     /\  \               /::\  \       /:/  /        /::\  \       /:/  /        /::\  \       /:/  /     /\  \
+   /:/\:\  \     /:/  /      \:\  \             /:/\ \  \     /:/__/        /:/\:\  \     /:/__/        /:/\:\  \     /:/  /      \:\  \
+  /::\~\:\  \   /:/  /       /::\__\           _\:\~\ \  \   /::\  \ ___   /::\~\:\  \   /::\  \ ___   /::\~\:\  \   /:/  /       /::\__\
+ /:/\:\ \:\__\ /:/__/     __/:/\/__/          /\ \:\ \ \__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/__/     __/:/\/__/
+ \/__\:\/:/  / \:\  \    /\/:/  /             \:\ \:\ \/__/ \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \:\  \    /\/:/  /
+      \::/  /   \:\  \   \::/__/               \:\ \:\__\        \::/  /       \::/  /       \::/  /       \::/  /   \:\  \   \::/__/
+      /:/  /     \:\  \   \:\__\                \:\/:/  /        /:/  /        /:/  /        /:/  /        /:/  /     \:\  \   \:\__\
+     /:/  /       \:\__\   \/__/                 \::/  /        /:/  /        /:/  /        /:/  /        /:/  /       \:\__\   \/__/
+     \/__/         \/__/                          \/__/         \/__/         \/__/         \/__/         \/__/         \/__/
+*/
