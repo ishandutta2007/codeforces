6a4116796d436ad81bb6eb5246e21f62c9cbbb93
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 15:03:35 +0530

    rubikun

---
+++ rubikun/normal/1194/A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(),(x).end()
+const ll mod=998244353,MAX=300005,INF=1<<30;
+
+int main(){
+    
+    //std::ifstream in("text.txt");
+    //std::cin.rdbuf(in.rdbuf());
+    
+    int T;cin>>T;
+    for(int i=0;i<T;i++){
+        ll a,b;cin>>a>>b;
+        cout<<2*b<<endl;
+    }
+}
