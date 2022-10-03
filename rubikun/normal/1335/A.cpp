552a2863a86900231d7ae55ca85eeb221f1c052c
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 13:33:33 +0530

    rubikun

---
+++ rubikun/normal/1335/A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define all(x) (x).begin(),(x).end()
+const int mod=1000000007,MAX=200003,INF=1<<30;
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int Q;cin>>Q;
+    while(Q--){
+        int N;cin>>N;
+        if(N%2==0) cout<<N/2-1<<"\n";
+        else cout<<N/2<<"\n";
+    }
+}
