87210b5ded77b3a80f5e2e8e5ab2f876ca961afc
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:40:06 +0530

    alishahali1382

---
+++ alishahali1382/normal/1155/A.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 1000010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+string s;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>s;
+	for (int i=1; i<n; i++) if (s[i-1]>s[i]){
+		cout<<"YES\n";
+		cout<<i<<' '<<i+1<<'\n';
+		return 0;
+	}
+	kill("NO")
+	
+	return 0;
+}
+/*
+      ___           ___                            ___           ___           ___           ___           ___           ___
+     /\  \         /\__\      ___                 /\  \         /\__\         /\  \         /\__\         /\  \         /\__\      ___
+    /::\  \       /:/  /     /\  \               /::\  \       /:/  /        /::\  \       /:/  /        /::\  \       /:/  /     /\  \
+   /:/\:\  \     /:/  /      \:\  \             /:/\ \  \     /:/__/        /:/\:\  \     /:/__/        /:/\:\  \     /:/  /      \:\  \
+  /::\~\:\  \   /:/  /       /::\__\           _\:\~\ \  \   /::\  \ ___   /::\~\:\  \   /::\  \ ___   /::\~\:\  \   /:/  /       /::\__\
+ /:/\:\ \:\__\ /:/__/     __/:/\/__/          /\ \:\ \ \__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/__/     __/:/\/__/
+ \/__\:\/:/  / \:\  \    /\/:/  /             \:\ \:\ \/__/ \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \:\  \    /\/:/  /
+      \::/  /   \:\  \   \::/__/               \:\ \:\__\        \::/  /       \::/  /       \::/  /       \::/  /   \:\  \   \::/__/
+      /:/  /     \:\  \   \:\__\                \:\/:/  /        /:/  /        /:/  /        /:/  /        /:/  /     \:\  \   \:\__\
+     /:/  /       \:\__\   \/__/                 \::/  /        /:/  /        /:/  /        /:/  /        /:/  /       \:\__\   \/__/
+     \/__/         \/__/                          \/__/         \/__/         \/__/         \/__/         \/__/         \/__/
+*/
