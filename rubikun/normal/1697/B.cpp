d1aec4023a43196e082aeba8bf8b121114ac0006
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 11:21:38 +0530

    rubikun

---
+++ rubikun/normal/1697/B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+template<class T>bool chmax(T &a, const T &b) { if (a<b) { a=b; return true; } return false; }
+template<class T>bool chmin(T &a, const T &b) { if (b<a) { a=b; return true; } return false; }
+#define all(x) (x).begin(),(x).end()
+#define fi first
+#define se second
+#define mp make_pair
+#define si(x) int(x.size())
+const int mod=998244353,MAX=300005,INF=1<<30;
+
+int main(){
+    
+    std::ifstream in("text.txt");
+    std::cin.rdbuf(in.rdbuf());
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    
+    int N,Q;cin>>N>>Q;
+    vector<ll> A(N);
+    for(int i=0;i<N;i++) cin>>A[i];
+    sort(all(A));
+    vector<ll> rui(N+1);
+    for(int i=1;i<=N;i++){
+        rui[i]=rui[i-1]+A[i-1];
+    }
+    
+    while(Q--){
+        int x,y;cin>>x>>y;
+        cout<<rui[N-(x-y)]-rui[N-x]<<"\n";
+    }
+}
