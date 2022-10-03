02364d409a0712e4b90d215021280b278139c863
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 06:37:48 +0530

    aoligei

---
+++ aoligei/normal/1495/A.cpp
@@ -0,0 +1,107 @@
+//#include <bits/stdc++.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cmath>
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <map>
+#include <set>
+#include <bitset>
+#include <complex>
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+#define itn int
+#define nit int
+#define ll long long
+#define ms multiset
+#define F(i,a,b) for(register int i=a,i##end=b;i<=i##end;++i)
+#define UF(i,a,b) for(register int i=a,i##end=b;i>=i##end;--i)
+#define re register
+#define ri re int
+#define il inline
+#define pii pair<int,int>
+#define cp complex<double>
+//#pra gma G CC opti mize(3)
+using namespace std;
+using std::bitset;
+//using namespace __gnu_pbds;
+const double Pi=acos(-1);
+namespace fastIO {
+	template<class T>
+	inline void read(T &x) {
+		x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		if(fu)x=-x;
+	}
+	inline int read() {
+		int x=0;
+		bool fu=0;
+		char ch=0;
+		while(ch>'9'||ch<'0') {
+			ch=getchar();
+			if(ch=='-')fu=1;
+		}
+		while(ch<='9'&&ch>='0') x=(x*10-48+ch),ch=getchar();
+		return fu?-x:x;
+	}
+	char _n_u_m_[40];
+	template<class T>
+	inline void write(T x ) {
+		if(x==0){
+			putchar('0');
+			return;
+		}
+		T tmp = x > 0 ? x : -x ;
+		if( x < 0 ) putchar('-') ;
+		register int cnt = 0 ;
+		while( tmp > 0 ) {
+			_n_u_m_[ cnt ++ ] = tmp % 10 + '0' ;
+			tmp /= 10 ;
+		}
+		while( cnt > 0 ) putchar(_n_u_m_[ -- cnt ]) ;
+	}
+	template<class T>
+	inline void write(T x ,char ch) {
+		write(x);
+		putchar(ch);
+	}
+}
+using namespace fastIO;
+int n,cnt1,cnt2;
+long double a[100002],b[100002],t1,t2,ans,ans2;
+int main() {
+F(qwq,1,read()){
+	read(n);cnt1=cnt2=0;ans=0;
+	F(i,1,n+n){
+		read(t1);read(t2);
+		if(t1==0)a[++cnt1]=labs(t2);
+		else b[++cnt2]=labs(t1);
+	}sort(a+1,a+n+1);sort(b+1,b+n+1);
+	F(i,1,n){
+		ans+=sqrt(a[i]*a[i]+b[i]*b[i]);
+	}
+	ans2=0;
+	F(i,1,n){
+		ans2+=sqrt(a[n-i+1]*a[n-i+1]+b[i]*b[i]);
+	}
+	cout<<fixed<<setprecision(20)<<min(ans,ans2)<<endl;
+}
+	return 0;
+}
