f01ad81ba9b38bcdd17b52bb1ea59b9e179f145c
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:16:40 +0530

    alishahali1382

---
+++ alishahali1382/normal/1197/C.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 300010, LOG=20;
+
+ll n, m, k, u, v, x, y, t, a, b, ans;
+ll A[MAXN];
+vector<ll> B;
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>k;
+	for (int i=1; i<=n; i++) cin>>A[i];
+	for (int i=1; i<n; i++) B.pb(A[i+1]-A[i]);
+	sort(all(B));
+	reverse(all(B));
+	ans=A[n]-A[1];
+	k--;
+	for (int i=0; i<k; i++) ans-=B[i];
+	cout<<ans<<'\n';
+	
+	return 0;
+}
