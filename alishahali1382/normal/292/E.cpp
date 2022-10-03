231d00789e23839f056dacfe4b53354fb8a28a7d
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 08:34:58 +0530

    alishahali1382

---
+++ alishahali1382/normal/292/E.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+#if defined(__GNUC__)
+#pragma GCC optimize ("Ofast")
+#endif
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define kill(x) return cout<<x<<'\n', 0;
+
+const ld eps=1e-7;
+const int inf=1000000010;
+const ll INF=10000000000000010LL;
+const int mod = 1000000007;
+const int MAXN = 100010, LOG=20;
+
+int n, m, typ, k, x, y, t, a, b;
+int A[MAXN];
+int B[MAXN];
+int seg[4*MAXN];
+int lazy[4*MAXN];
+bool mark[4*MAXN];
+
+void shift(int id){
+	if (!mark[id]) return ;
+	lazy[id<<1]=lazy[id<<1 | 1]=lazy[id];
+	mark[id<<1]=mark[id<<1 | 1]=1;
+	mark[id]=0;
+}
+
+void update(int id, int tl, int tr, int l, int r, int val){
+	if (tr<l || r<tl) return ;
+	if (l<=tl && tr<=r){
+		lazy[id]=val;
+		mark[id]=1;
+		return ;
+	}
+	shift(id);
+	int mid=(tl+tr)>>1;
+	update(id<<1, tl, mid, l, r, val);
+	update(id<<1 | 1, mid+1, tr, l, r, val);
+}
+
+int get(int id, int tl, int tr, int x){
+	if (tl==tr && tl==x) return lazy[id];
+	shift(id);
+	int mid=(tl+tr)>>1;
+	if (x<=mid) return get(id<<1, tl, mid, x);
+	return get(id<<1 | 1, mid+1, tr, x);
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	cin>>n>>m;
+	for (int i=1; i<=n; i++) cin>>A[i];
+	for (int i=1; i<=n; i++) cin>>B[i];
+	
+	update(1, 1, n, 1, n, inf);
+	
+	while (m--){
+		cin>>typ;
+		if (typ==2){
+			cin>>x;
+			int ans=get(1, 1, n, x);
+			if (ans==inf) cout<<B[x]<<'\n';
+			else cout<<A[ans+x]<<'\n';
+			continue ;
+		}
+		cin>>x>>y>>k;
+		update(1, 1, n, y, y+k-1, x-y);
+	}
+	
+	return 0;
+}
+/*
+      ___           ___                            ___           ___           ___           ___           ___           ___
+     /\  \         /\__\      ___                 /\  \         /\__\         /\  \         /\__\         /\  \         /\__\      ___
+    /::\  \       /:/  /     /\  \               /::\  \       /:/  /        /::\  \       /:/  /        /::\  \       /:/  /     /\  \
+   /:/\:\  \     /:/  /      \:\  \             /:/\ \  \     /:/__/        /:/\:\  \     /:/__/        /:/\:\  \     /:/  /      \:\  \
+  /::\~\:\  \   /:/  /       /::\__\           _\:\~\ \  \   /::\  \ ___   /::\~\:\  \   /::\  \ ___   /::\~\:\  \   /:/  /       /::\__\
+ /:/\:\ \:\__\ /:/__/     __/:/\/__/          /\ \:\ \ \__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/\:\  /\__\ /:/\:\ \:\__\ /:/__/     __/:/\/__/
+ \/__\:\/:/  / \:\  \    /\/:/  /             \:\ \:\ \/__/ \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \/__\:\/:/  / \:\  \    /\/:/  /
+      \::/  /   \:\  \   \::/__/               \:\ \:\__\        \::/  /       \::/  /       \::/  /       \::/  /   \:\  \   \::/__/
+      /:/  /     \:\  \   \:\__\                \:\/:/  /        /:/  /        /:/  /        /:/  /        /:/  /     \:\  \   \:\__\
+     /:/  /       \:\__\   \/__/                 \::/  /        /:/  /        /:/  /        /:/  /        /:/  /       \:\__\   \/__/
+     \/__/         \/__/                          \/__/         \/__/         \/__/         \/__/         \/__/         \/__/
+*/
