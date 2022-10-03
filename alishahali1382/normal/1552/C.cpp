b4e29f273f9b8003acf5e51e7b257d927be088bf
ishan dutta <ishandutta2007@gmail.com>
2022-10-03 06:57:38 +0530

    alishahali1382

---
+++ alishahali1382/normal/1552/C.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O2,unroll-loops")
+//#pragma GCC optimize("no-stack-protector,fast-math")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef pair<pii, int> piii;
+typedef pair<ll, ll> pll;
+#define debug(x) cerr<<#x<<'='<<(x)<<endl;
+#define debugp(x) cerr<<#x<<"= {"<<(x.first)<<", "<<(x.second)<<"}"<<endl;
+#define debug2(x, y) cerr<<"{"<<#x<<", "<<#y<<"} = {"<<(x)<<", "<<(y)<<"}"<<endl;
+#define debugv(v) {cerr<<#v<<" : ";for (auto x:v) cerr<<x<<' ';cerr<<endl;}
+#define all(x) x.begin(), x.end()
+#define pb push_back
+#define SZ(x) ((int)x.size())
+#define kill(x) return cout<<x<<'\n', 0;
+
+const int inf=1000000010;
+const ll INF=1000000000000001000LL;
+const int mod=1000000007;
+const int MAXN=300010, LOG=20;
+
+int n, m, k, u, v, x, y, t, a, b, ans;
+int A[MAXN], B[MAXN], mark[MAXN];
+vector<pii> vec;
+vector<int> X;
+
+int Solve(){
+	cin>>n>>k;
+	X.clear();
+	vec.clear();
+	ans=0;
+	for (int i=1; i<=2*n; i++) mark[i]=0;
+	while (k--){
+		cin>>x>>y;
+		if (x>y) swap(x, y);
+		mark[x]=mark[y]=1;
+		vec.pb({x, y});
+	}
+	for (int i=1; i<=2*n; i++) if (!mark[i]) X.pb(i);
+	k=SZ(X)/2;
+	for (int i=0; i<k; i++) vec.pb({X[i], X[i+k]});
+	sort(all(vec));
+	for (int i=0; i<n; i++) for (int j=i+1; j<n; j++){
+		if (vec[j].first<vec[i].second && vec[i].second<vec[j].second) ans++;
+	}
+	kill(ans)
+}
+
+int main(){
+	ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int T;
+	cin>>T;
+	while (T--) Solve();
+	
+	return 0;
+}
