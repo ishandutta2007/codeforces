a8422a9a1c605e48f764055228a21d1cbc2f6811
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 05:57:42 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/990/A.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define dF2(i,a,b) for(int i=a;i>(b);--i)
+#define eF(i,u) for(int i=h[u];i;i=nxt[i])
+#define ll long long
+#define ld double
+typedef pair<int,int> pii;
+inline int Gcd(int X,int Y){return Y?Gcd(Y,X%Y):X;}
+inline ll Gcd(ll X,ll Y){return Y?Gcd(Y,X%Y):X;}
+inline int Pow(int base,ll exp,int _mod){if(!(base%=_mod))return 0;int _ans=1;for(;exp;exp>>=1,base=(ll)base*base%_mod)exp&1?_ans=(ll)_ans*base%_mod:0;return _ans;}
+inline ll Pow(ll base,ll exp,ll _mod){if(!(base%=_mod))return 0;ll _ans=1;for(;exp;exp>>=1,base=base*base%_mod)exp&1?_ans=_ans*base%_mod:0;return _ans;}
+const int INF=0x3f3f3f3f;
+
+ll n,m,a,b;
+
+int h[100001],nxt[200001],to[200001],tot;
+inline void ins(int x,int y){nxt[++tot]=h[x];to[tot]=y;h[x]=tot;}
+
+int main(){
+	scanf("%lld%lld%lld%lld",&n,&m,&a,&b);
+	ll k=n/m*m;
+	printf("%lld",min((n-k)*b,(k+m-n)*a));
+	return 0;
+}
---
+++ pinkrabbit/normal/990/B.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define dF2(i,a,b) for(int i=a;i>(b);--i)
+#define eF(i,u) for(int i=h[u];i;i=nxt[i])
+#define ll long long
+#define ld double
+typedef pair<int,int> pii;
+inline int Gcd(int X,int Y){return Y?Gcd(Y,X%Y):X;}
+inline ll Gcd(ll X,ll Y){return Y?Gcd(Y,X%Y):X;}
+inline int Pow(int base,ll exp,int _mod){if(!(base%=_mod))return 0;int _ans=1;for(;exp;exp>>=1,base=(ll)base*base%_mod)exp&1?_ans=(ll)_ans*base%_mod:0;return _ans;}
+inline ll Pow(ll base,ll exp,ll _mod){if(!(base%=_mod))return 0;ll _ans=1;for(;exp;exp>>=1,base=base*base%_mod)exp&1?_ans=_ans*base%_mod:0;return _ans;}
+const int INF=0x3f3f3f3f;
+
+int n,k;
+int a[200002],b[100001];
+map<int,int> c;
+
+int h[100001],nxt[200001],to[200001],tot;
+inline void ins(int x,int y){nxt[++tot]=h[x];to[tot]=y;h[x]=tot;}
+
+int main(){
+	scanf("%d%d",&n,&k);
+	F(i,1,n) scanf("%d",a+i), c[a[i]]++;
+	sort(a+1,a+n+1); a[n+1]=INF;
+	int A=0;
+	F(i,2,n+1) if(a[i]-a[i-1]>k) A+=c[a[i-1]];
+	printf("%d",A);
+	return 0;
+}
