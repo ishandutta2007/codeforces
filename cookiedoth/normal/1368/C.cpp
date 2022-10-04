6cfee3efe26cdbf55abf4133b2bd702b777e08c5
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:52:00 +0530

    cookiedoth

---
+++ cookiedoth/normal/1368/C.cpp
@@ -0,0 +1,108 @@
+/*
+
+Code for problem C by cookiedoth
+Generated 18 Jun 2020 at 05.54 PM
+
+
+   ,##.                   ,==.
+ ,#    #.                 \ o ',
+#        #     _     _     \    \
+#        #    (_)   (_)    /    ; 
+ `#    #'                 /   .'  
+   `##'                   "=="
+
+o_O
+-_-
+z_z
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#include <random>
+#include <cstring>
+#include <numeric>
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define sz(a) (int)a.size()
+
+using namespace std;
+
+template<class T> int chkmax(T &a, T b) {
+	if (b > a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class T> int chkmin(T &a, T b) {
+	if (b < a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class iterator> void output(iterator begin, iterator end, ostream& out = cerr) {
+	while (begin != end) {
+		out << (*begin) << " ";
+		begin++;
+	}
+	out << endl;
+}
+
+template<class T> void output(T x, ostream& out = cerr) {
+	output(x.begin(), x.end(), out);
+}
+
+void fast_io() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+signed main() {
+	fast_io();
+	int n;
+	cin >> n;
+	set<pair<int, int> > ans;
+	int t = 0;
+	for (int i = 0; i < n + 1; ++i) {
+		for (int x = 0; x < 3; ++x) {
+			for (int y = 0; y < 3; ++y) {
+				if (x == 1 && y == 1) {
+					continue;
+				}
+				ans.insert({t + x, t + y});
+			}
+		}
+		t += 2;
+	}
+	cout << ans.size() << endl;
+	for (auto pp : ans) {
+		cout << pp.first << " " << pp.second << '\n';
+	}
+}
