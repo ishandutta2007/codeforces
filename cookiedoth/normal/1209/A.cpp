216c8ea08750a900bfe9631278007e6d11dc42b2
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 07:20:49 +0530

    cookiedoth

---
+++ cookiedoth/normal/1209/A.cpp
@@ -0,0 +1,114 @@
+/*
+
+Code for problem A by cookiedoth
+Generated 14 Sep 2019 at 04.01 P
+
+
+_____________
+_____________
+_____________
+_____________ ______
+_____________
+_____________
+___________ ________
+
+-_-
+~_^
+=_=
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#include <random>
+#include <cstring>
+#include <numeric>
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define sz(a) (int)a.size()
+
+using namespace std;
+
+template<class T> int chkmax(T &a, T b) {
+	if (b > a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class T> int chkmin(T &a, T b) {
+	if (b < a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class iterator> void output(iterator begin, iterator end, ostream& out = cerr) {
+	while (begin != end) {
+		out << (*begin) << " ";
+		begin++;
+	}
+	out << endl;
+}
+
+template<class T> void output(T x, ostream& out = cerr) {
+	output(x.begin(), x.end(), out);
+}
+
+void fast_io() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+const int mx = 110;
+int n, cnt[mx];
+
+signed main() {
+	fast_io();
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		int val;
+		cin >> val;
+		cnt[val]++;
+	}
+	int ans = 0;
+	for (int i = 1; i < mx; ++i) {
+		if (cnt[i]) {
+			int ok = 0;
+			for (int j = 1; j < i; ++j) {
+				if (i % j == 0 && cnt[j]) {
+					ok = 1;
+					break;
+				}
+			}
+			if (ok == 0) {
+				ans++;
+			}
+		}
+	}
+	cout << ans << endl;
+}
