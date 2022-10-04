d02e7ef158c313acccf8b4f7ed170da71353a18d
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 07:34:36 +0530

    cookiedoth

---
+++ cookiedoth/normal/977/A.cpp
@@ -0,0 +1,48 @@
+/*
+   ,##.                   ,==.
+ ,#    #.                 \ o ',
+#        #     _     _     \    \
+#        #    (_)   (_)    /    ; 
+ `#    #'                 /   .'  
+   `##'                   "=="
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#define ll long long
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+
+using namespace std;
+
+const ll MOD = 1e9 + 7;
+
+int main() {
+	int n, k;
+	cin >> n >> k;
+	for (int i = 0; i < k; ++i) {
+		if (n % 10 == 0)
+			n /= 10;
+		else
+			n -= 1;
+	}
+	cout << n << endl;
+}
