6292878e993599db290ca96dbb509438e2e6a431
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 05:42:17 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/1062/D.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define dF2(i,a,b) for(int i=a;i>(b);--i)
+#define eF(i,u) for(int i=h[u];i;i=nxt[i])
+#define SZ(x) ((int)(x).size())
+#define prts(x) return puts(x),0
+#define pirtf(x) return printf("%d\n",x),0
+#define re return
+typedef long long LL;
+typedef double ld;
+typedef pair<int,int> pii;
+inline int Gcd(int X,int Y){return Y?Gcd(Y,X%Y):X;}
+inline LL Gcd(LL X,LL Y){return Y?Gcd(Y,X%Y):X;}
+inline int Pow(int B,LL E,int M){if(!(B%=M))return 0;int A=1;for(;E;E>>=1,B=(LL)B*B%M)E&1?A=(LL)A*B%M:0;return A;}
+inline LL Pow(LL B,LL E,LL M){if(!(B%=M))return 0;LL A=1;for(;E;E>>=1,B=B*B%M)E&1?A=A*B%M:0;return A;}
+const int INF=0x3f3f3f3f;
+
+map<int,int> mp;
+set<int> st;
+
+int n,q,m,k;
+int a[300005],b[300005];
+int f[300005],g[300005];
+int M[3005][3005];
+char str[300005];
+
+int h[300005],nxt[600005],to[600005],tot;
+inline void ins(int x,int y){nxt[++tot]=h[x];to[tot]=y;h[x]=tot;}
+
+int main(){
+	LL Ans=0;
+	scanf("%d",&n);
+	F(i,2,n){
+		int P=n/i;
+		Ans+=(LL)P*(P+1)/2-1;
+	}
+	printf("%lld\n",4*Ans);
+	return 0;
+}
