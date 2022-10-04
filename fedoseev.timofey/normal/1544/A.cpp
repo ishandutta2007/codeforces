3cd4fab108e3359cce1a53cd088a52e54745b83a
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:36:16 +0530

    fedoseev.timofey

---
+++ fedoseev.timofey/normal/1544/A.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <random>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+#include <bitset>
+#include <chrono>
+ 
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+#endif
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int mx = 0;
+    while (n > 0) {
+      mx = max(mx, n % 10);
+      n /= 10;
+    }
+    cout << mx << '\n';
+  } 
+}
