6d838ada9fe28d8f91116a825fd19564db2fd631
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 15:06:33 +0530

    fedoseev.timofey

---
+++ fedoseev.timofey/normal/1331/D.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <random>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+#endif
+  string s;
+  cin >> s;
+  cout << (s.back() - '0') % 2 << '\n';
+}
