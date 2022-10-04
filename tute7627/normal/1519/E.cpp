d54e33980d770544498c29b0537410a383837422
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 11:11:22 +0530

    tute7627

---
+++ tute7627/normal/1519/E.cpp
@@ -0,0 +1,302 @@
+//#define _GLIBCXX_DEBUG
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define lfs cout<<fixed<<setprecision(10)
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define fi first
+#define se second
+#define MP make_pair
+#define MT make_tuple
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (ll)(m) - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+const ll MOD1 = 1e9+7;
+const ll MOD9 = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T> using PQ = priority_queue<T>;
+template<typename T> using QP = priority_queue<T,vector<T>,greater<T>>;
+template<typename T1, typename T2>bool chmin(T1 &a,T2 b){if(a>b){a=b;return true;}else return false;}
+template<typename T1, typename T2>bool chmax(T1 &a,T2 b){if(a<b){a=b;return true;}else return false;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T1,typename T2,typename T3>void anss(T1 x,T2 y,T3 z){ans(x!=y,x,z);};  
+template<typename T>void debug(const T &v,ll h,ll w,string sv=" "){for(ll i=0;i<h;i++){cout<<v[i][0];for(ll j=1;j<w;j++)cout<<sv<<v[i][j];cout<<endl;}};
+template<typename T>void debug(const T &v,ll n,string sv=" "){if(n!=0)cout<<v[0];for(ll i=1;i<n;i++)cout<<sv<<v[i];cout<<endl;};
+template<typename T>vector<vector<T>>vec(ll x, ll y, T w){vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,-1,0,0,1,1,-1,-1};vector<ll>dy={0,0,1,-1,1,-1,1,-1};
+template<typename T>vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>auto make_v(size_t a,Ts... ts){return vector<decltype(make_v(ts...))>(a,make_v(ts...));}
+template<typename T1, typename T2>ostream &operator<<(ostream &os, const pair<T1, T2>&p){return os << p.first << " " << p.second;}
+template<typename T>ostream &operator<<(ostream &os, const vector<T> &v){for(auto &z:v)os << z << " ";cout<<"|"; return os;}
+template<typename T>void rearrange(vector<int>&ord, vector<T>&v){
+  auto tmp = v;
+  for(int i=0;i<tmp.size();i++)v[i] = tmp[ord[i]];
+}
+template<typename Head, typename... Tail>void rearrange(vector<int>&ord,Head&& head, Tail&&... tail){
+  rearrange(ord, head);
+  rearrange(ord, tail...);
+}
+template<typename T> vector<int> ascend(const vector<T>&v){
+  vector<int>ord(v.size());iota(ord.begin(),ord.end(),0);
+  sort(ord.begin(),ord.end(),[&](int i,int j){return v[i]<v[j];});
+  return ord;
+}
+template<typename T> vector<int> descend(const vector<T>&v){
+  vector<int>ord(v.size());iota(ord.begin(),ord.end(),0);
+  sort(ord.begin(),ord.end(),[&](int i,int j){return v[i]>v[j];});
+  return ord;
+}
+ll FLOOR(ll n,ll div){return n>=0?n/div:(n-div+1)/div;}
+ll CEIL(ll n,ll div){return n>=0?(n+div-1)/div:n/div;}
+template<typename T>T min(const vector<T>&v){return *min_element(v.begin(),v.end());}
+template<typename T>T max(const vector<T>&v){return *max_element(v.begin(),v.end());}
+template<typename T>T reverse(const T &v){return T(v.rbegin(),v.rend());};
+//mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+int popcount(ll x){return __builtin_popcountll(x);};
+int poplow(ll x){return __builtin_ctzll(x);};
+int pophigh(ll x){return 63 - __builtin_clzll(x);};
+template<typename T>T poll(queue<T> &q){auto ret=q.front();q.pop();return ret;};
+template<typename T>T poll(priority_queue<T> &q){auto ret=q.top();q.pop();return ret;};
+template<typename T>T poll(QP<T> &q){auto ret=q.top();q.pop();return ret;};
+template<typename T>T poll(stack<T> &s){auto ret=s.top();s.pop();return ret;};
+template< typename T = int >
+struct edge {
+  int to;
+  T cost;
+  int id;
+  edge():id(-1){};
+  edge(int to, T cost = 1, int id = -1):to(to), cost(cost), id(id){}
+  operator int() const { return to; }
+};
+
+template<typename T>
+using Graph = vector<vector<edge<T>>>;
+template<typename T>
+Graph<T> readGraph(int n,int m,int indexed=1,bool directed=false,bool weighted=false){
+  Graph<T> ret(n);
+  for(int es = 0; es < m; es++){
+    int u,v,w=1;
+    cin>>u>>v;u-=indexed,v-=indexed;
+    if(weighted)cin>>w;
+    ret[u].emplace_back(v,w,es);
+    if(!directed)ret[v].emplace_back(u,w,es);
+  }
+  return ret;
+}
+template<typename T>
+Graph<T> readParent(int n,int indexed=1,bool directed=true){
+  Graph<T>ret(n);
+  for(int i=1;i<n;i++){
+    int p;cin>>p;
+    p-=indexed;
+    ret[p].emplace_back(i);
+    if(!directed)ret[i].emplace_back(p);
+  }
+  return ret;
+}
+template<typename T>
+map<T,ll> compress(vector<T> &v){
+  sort(ALL(v));
+  v.erase(unique(ALL(v)),v.end());
+  map<T, ll> ret;
+  for(ll i=0;i<(ll)v.size();i++)
+    ret[v[i]]=i;
+  return ret;
+}
+struct RationalNumber{
+  long long a, b;
+  RationalNumber(){
+    a = 0,b = 1;
+  }
+  RationalNumber(long long p, long long q):a(p), b(q){// a / b
+    reduction();
+  };
+  RationalNumber(long long p):a(p), b(1){// a / b
+  };
+  void reduction(){
+    long long g = gcd(a, b);
+    a /= g, b /= g;
+    if(b < 0)a *= -1,b *= -1;
+  }
+  RationalNumber &operator+=(const RationalNumber &x){
+    long long q = b / __gcd(b, x.b) * x.b;
+    a = a * (q / b) + x.a * (q / x.b);
+    b = q;
+    reduction();
+    return *this;
+  }
+  RationalNumber &operator-=(const RationalNumber &x){
+    long long q = b / __gcd(b, x.b) * x.b;
+    a = a * (q / b) - x.a * (q / x.b);
+    b = q;
+    reduction();
+    return *this;
+  }
+  RationalNumber &operator*=(const RationalNumber &x){
+    long long q1 = __gcd(a, x.b), q2 = __gcd(b, x.a); 
+    a = (a / q1) * (x.a / q2), b = (b / q2) * (x.b / q1); 
+    return *this;
+  }
+  RationalNumber &operator/=(const RationalNumber &x){
+    long long q1 = gcd(a, x.a), q2 = gcd(b, x.b); 
+    a = (a / q1) * (x.b / q2), b = (b / q2) * (x.a / q1); 
+    reduction();
+    return *this;
+  }
+  RationalNumber operator+(const RationalNumber &x)const{
+    return RationalNumber(*this) += x;
+  }
+  RationalNumber operator-(const RationalNumber &x)const{
+    return RationalNumber(*this) -= x;
+  }
+  RationalNumber operator*(const RationalNumber &x)const{
+    return RationalNumber(*this) *= x;
+  }
+  RationalNumber operator/(const RationalNumber &x)const{
+    return RationalNumber(*this) /= x;
+  }
+  RationalNumber operator-()const{
+    return RationalNumber(-a, b);
+  }
+  bool operator==(const RationalNumber &x)const{
+    return a * x.b == b * x.a;
+  };
+  bool operator!=(const RationalNumber &x)const{
+    return !(*this == x);
+  };
+  bool operator<(const RationalNumber &x)const{
+    return __int128_t(a) * x.b < __int128_t(b) * x.a;
+  }
+  bool operator>(const RationalNumber &x)const{
+    return a * x.b > b * x.a;
+  }
+  bool operator<=(const RationalNumber &x)const{
+    return *this < x || *this == x;
+  }
+  bool operator>=(const RationalNumber &x)const{
+    return *this > x || *this == x;
+  }
+  friend ostream &operator<<(ostream &os, RationalNumber x){
+    return os << x.a << "/" << x.b;
+  }
+};
+using rat=RationalNumber;
+template<typename T>
+struct Compress{
+	vector<T>v;
+	Compress(){
+	}
+	Compress(const vector<T>&input){
+		add(input);
+    build();
+	}
+  template<typename... Args>
+  Compress(const vector<T> &head,const Args& ...input){
+    add(head, input...);
+    build();
+  }
+  void build(){
+		sort(v.begin(), v.end());
+    v.erase(unique(v.begin(),v.end()),v.end());
+  }
+	void add(const vector<T>&add){
+		v.insert(v.end(),add.begin(), add.end());
+	}
+  template<typename Head, typename ...Tail>
+  void add(const Head& head,const Tail& ...tail){
+    add(head);
+    add(tail...);
+  }
+  int next(T val){
+    return lower_bound(v.begin(), v.end(), val) - v.begin();
+  }
+  int prev(T val){
+    return upper_bound(v.begin(), v.end(), val) - v.begin() - 1;
+  }
+  bool exist(T val){
+    return binary_search(v.begin(), v.end(), val);
+  }
+  T val(int x){
+    return v[x];
+  }
+  vector<int>compress(const vector<T>&input){
+    vector<int>ret(input.size());
+    for(int i=0;i<input.size();i++)ret[i]=next(input[i]);
+    return ret;
+  }
+  int operator[](T val){
+    return next(val);
+  }
+  int size(){
+    return v.size();
+  }
+};
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n;cin>>n;
+  vector<rat>px(n),py(n);
+  rep(i,0,n){
+    ll a,b,c,d;cin>>a>>b>>c>>d;
+    rat x(a,b),y(c,d);
+    px[i]=(x+1)/y,py[i]=x/(y+1);
+  }
+  //cout<<endl;
+  //for(auto [a,b]:co)cout<<a spa b<<endl;
+  auto mp=Compress(px,py);
+  ll sz=mp.size();
+  Graph<int>g(sz);
+  rep(i,0,n){
+    g[mp[px[i]]].EB(mp[py[i]],0,i);
+    g[mp[py[i]]].EB(mp[px[i]],0,i);
+    //cout<<i spa mp[px[i]] spa mp[py[i]]<<endl;
+  }
+  vector<P>ret;
+  vector<bool>used(sz);
+  vector<bool>check(n);
+  rep(i,0,sz){
+    if(used[i])continue;
+    auto dfs=[&](auto &&f,ll k,ll ep)->bool{
+      used[k]=true;
+      stack<ll>koutai;
+      for(auto e:g[k]){
+        if(check[e.id])continue;
+        check[e.id]=true;
+        if(!used[e.to]){
+          auto tmp=f(f,e.to,e.id);
+          if(tmp)koutai.push(e.id);
+        }
+        else koutai.push(e.id);
+      }
+      while(koutai.size()>=2){
+        ll x=poll(koutai);
+        ll y=poll(koutai);
+        ret.EB(x+1,y+1);
+      }
+      if(koutai.size()==1&&ep!=-1){
+        ret.EB(ep+1,poll(koutai)+1);
+        return false;
+      }
+      else return true;
+    };
+    dfs(dfs,i,-1);
+  }
+  cout<<ret.size()<<endl;
+  for(auto z:ret)cout<<z<<endl;
+  return 0;
+}
