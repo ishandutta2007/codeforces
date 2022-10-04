41028a8b7ca449610d08183ed8151039e0f9f507
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:56:04 +0530

    cookiedoth

---
+++ cookiedoth/normal/1359/A.cpp
@@ -0,0 +1,103 @@
+/*
+
+Code for problem A by cookiedoth
+Generated 28 May 2020 at 05.48 PM
+
+
+_____________
+_____________
+_____________
+_____________ ______
+_____________
+_____________
+___________ ________
+
+z_z
+>_<
+o_O
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#include <random>
+#include <cstring>
+#include <numeric>
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define sz(a) (int)a.size()
+
+using namespace std;
+
+template<class T> int chkmax(T &a, T b) {
+	if (b > a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class T> int chkmin(T &a, T b) {
+	if (b < a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class iterator> void output(iterator begin, iterator end, ostream& out = cerr) {
+	while (begin != end) {
+		out << (*begin) << " ";
+		begin++;
+	}
+	out << endl;
+}
+
+template<class T> void output(T x, ostream& out = cerr) {
+	output(x.begin(), x.end(), out);
+}
+
+void fast_io() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+void solve() {
+	int n, m, k;
+	cin >> n >> m >> k;
+	int maxJ = min(m, n / k);
+	int others = (m - maxJ + k - 2) / (k - 1);
+	cout << maxJ - others << '\n';
+}
+
+signed main() {
+	fast_io();
+	int T;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+}
