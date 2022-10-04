8e769bb5c95582f4426590179aa4315434130bf2
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:19:12 +0530

    tute7627

---
+++ tute7627/normal/1185/D.cpp
@@ -0,0 +1,103 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define lfs <<fixed<<setprecision(10)<<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define rep(i,n,m) for(ll i = n; i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = n - 1; i >= (ll)(m); i--)
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>v,ll n){cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+template<typename T>
+void emp(map<T,ll>&m, T x){m.emplace(x,0).first->second++;}
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  ll res=0,res1=INF,res2=-INF,buf=0;
+  bool judge = true;
+  ll n;cin>>n;
+  vector<ll>a(n);
+  rep(i,0,n)cin>>a[i];
+  auto b=a;
+  sort(ALL(a));
+  res=-1;
+  ll cnt=0;
+  ll ind=-1;
+  ll x=a[1]-a[0];
+  rep(i,0,n-1){
+    if(a[i+1]-a[i]!=x){
+      ind=i;
+      cnt++;
+    }
+  }
+  if(cnt==0){
+    res=n-1;
+  }
+  else if(cnt==1){
+    if(a[ind+1]-a[ind-1]==x)res=ind;
+    else if(ind==n-2){
+      res=n-1;
+    }
+  }
+  else if(cnt==2){
+    if(a[ind+1]-a[ind-1]==x)res=ind;
+  }
+
+  if(n>=3){
+    x=a[2]-a[0];
+    cnt=0;
+    rep(i,2,n-1)if(a[i+1]-a[i]!=x)cnt++;
+    if(cnt==0)res=1;
+
+    x=a[2]-a[1];
+    cnt=0;
+    rep(i,2,n-1)if(a[i+1]-a[i]!=x)cnt++;
+    if(cnt==0)res=0;
+  }
+  if(res==-1)cout<<res<<endl;
+  else{
+    rep(i,0,n){
+      if(b[i]==a[res]){
+        cout<<i+1<<endl;
+        break;
+      }
+    }
+  }
+  return 0;
+}
