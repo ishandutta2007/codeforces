f87c0dde0751175ba7444fbb6e3154316f310ec2
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:01:11 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/977/B.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define dF2(i,a,b) for(int i=a;i>(b);--i)
+#define eF(i,u) for(int i=h[u];i;i=nxt[i])
+#define ll long long
+#define ld double
+using namespace std;
+typedef pair<int,int> pii;
+inline int Gcd(int X,int Y){return Y?Gcd(Y,X%Y):X;}
+inline ll Gcd(ll X,ll Y){return Y?Gcd(Y,X%Y):X;}
+inline int Pow(int base,ll exp,int _mod){if(!(base%=_mod))return 0;int _ans=1;for(;exp;exp>>=1,base=(ll)base*base%_mod)exp&1?_ans=(ll)_ans*base%_mod:0;return _ans;}
+inline ll Pow(ll base,ll exp,ll _mod){if(!(base%=_mod))return 0;ll _ans=1;for(;exp;exp>>=1,base=base*base%_mod)exp&1?_ans=_ans*base%_mod:0;return _ans;}
+const int INF=0x3f3f3f3f;
+
+int n,q;
+char s[10001];
+int c[10001],ans;
+
+int main(){
+	scanf("%d%s",&n,s+1);
+	F(i,1,n-1) ++c[(s[i]-'A')*26+s[i+1]-'A'];
+	F(i,0,26*26-1) if(c[i]>c[ans]) ans=i;
+	printf("%c%c",ans/26+'A',ans%26+'A');
+	return 0;
+}
