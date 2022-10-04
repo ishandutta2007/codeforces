ed05c57918e86e8bf637cf7f2e97a41e7ea7aa7b
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 09:10:09 +0530

    a.k.e.e

---
+++ a.k.e.e/normal/1320/A.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+#define x first
+#define y second
+#define mp make_pair
+#define pb push_back
+template <typename TYPE> inline void chkmax(TYPE &x,TYPE y){x<y?x=y:0;}
+template <typename TYPE> inline void chkmin(TYPE &x,TYPE y){y<x?x=y:0;}
+template <typename TYPE> void readint(TYPE &x)
+{
+    x=0;int f=1;char c;
+    for(c=getchar();!isdigit(c);c=getchar())if(c=='-')f=-1;
+    for(;isdigit(c);c=getchar())x=x*10+c-'0';
+    x*=f;
+}
+const int MAXN=800005;
+
+int n,a[MAXN];
+ll res[MAXN];
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("code.in","r",stdin);
+//	freopen("code.out","w",stdout);
+	#endif
+	readint(n);
+	for(int i=1;i<=n;++i)readint(a[i]),res[a[i]-i+400000]+=a[i];
+	ll ans=0;
+	for(int i=0;i<MAXN;++i)chkmax(ans,res[i]);
+	cout<<ans<<endl;
+	return 0;
+}
