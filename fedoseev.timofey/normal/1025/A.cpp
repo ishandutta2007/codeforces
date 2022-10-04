0abde90366ea2d21c7057ac02c24a3dfc5e22644
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 16:05:45 +0530

    fedoseev.timofey

---
+++ fedoseev.timofey/normal/1025/A.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <ctime>
+#include <random>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <cassert>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+    ios_base::sync_with_stdio(false); cout.setf(ios::fixed); cout.precision(20); cin.tie(0);
+    #ifdef HOME
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (s.size() == 1) {
+        cout << "Yes\n";
+        return 0;
+    }
+    set <char> have;
+    for (auto c : s) have.insert(c);
+    if (have.size() == s.size()) cout << "No\n";
+    else cout << "Yes\n";
+}
