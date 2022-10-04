449a8c93acfc8cef127275da31b640eb243a9b52
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 07:26:04 +0530

    cookiedoth

---
+++ cookiedoth/normal/1109/B.cpp
@@ -0,0 +1,134 @@
+/*
+
+Code for problem B by cookiedoth
+Generated 16 Feb 2019 at 07.32 P
+
+
+
+10%
+
+30%
+
+50%
+
+70%
+
+100%
+
+z_z
+>_<
+\_()_/
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#include <random>
+#include <cstring>
+#include <numeric>
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define sz(a) (int)a.size()
+
+using namespace std;
+
+template<class T> int chkmax(T &a, T b) {
+	if (b > a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class T> int chkmin(T &a, T b) {
+	if (b < a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class iterator> void output(iterator begin, iterator end, ostream& out = cerr) {
+	while (begin != end) {
+		out << (*begin) << " ";
+		begin++;
+	}
+	out << endl;
+}
+
+template<class T> void output(T x, ostream& out = cerr) {
+	output(x.begin(), x.end(), out);
+}
+
+void fast_io() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int n;
+string s;
+string IMP = "Impossible";
+
+signed main() {
+	fast_io();
+	cin >> s;
+	int eq = 1;
+	n = s.size();
+	int m = n;
+	if (n % 2 == 1) {
+		m = n / 2;
+	}
+	for (int i = 1; i < m; ++i) {
+		if (s[i] != s[i - 1]) {
+			eq = 0;
+			break;
+		}
+	}
+
+	if (eq) {
+		cout << IMP << endl;
+		exit(0);
+	}
+
+	for (int i = 1; i < n; ++i) {
+		string t = s.substr(i, n - i) + s.substr(0, i);
+		if (t == s) {
+			continue;
+		}
+		int ok = 1;
+		for (int j = 0; j < n; ++j) {
+			if (t[j] != t[n - j - 1]) {
+				ok = 0;
+				break;
+			}
+		}
+		if (ok) {
+			cout << 1 << endl;
+			exit(0);
+		}
+	}
+
+	cout << 2 << endl;
+}
