ddae79c62bbcc0339dc1cf082fbc337a8e3839ff
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:28:12 +0530

    cookiedoth

---
+++ cookiedoth/normal/1661/B.cpp
@@ -0,0 +1,121 @@
+/*
+
+Code for problem B by cookiedoth
+Generated 09 Apr 2022 at 10.41 AM
+
+
+  !
+
+
+
+
+
+
+
+
+
+
+
+z_z
+>_<
+o_O
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#include <random>
+#include <cstring>
+#include <numeric>
+#include <random>
+#include <utility>
+#include <tuple>
+#include <chrono>
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define length(a) (int)a.size()
+
+using namespace std;
+
+template<class T> int chkmax(T &a, T b) {
+	if (b > a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class T> int chkmin(T &a, T b) {
+	if (b < a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class iterator> void output(iterator begin, iterator end, ostream& out = cerr) {
+	while (begin != end) {
+		out << (*begin) << " ";
+		begin++;
+	}
+	out << endl;
+}
+
+template<class T> void output(T x, ostream& out = cerr) {
+	output(x.begin(), x.end(), out);
+}
+
+void fast_io() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+const int B = 15;
+
+signed main() {
+	fast_io();
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; ++i) {
+		int x;
+		cin >> x;
+		if (x == 0) {
+			cout << 0 << ' ';
+			continue;
+		}
+		int ans = 1e9;
+		for (int it = 0; it < 30; ++it) {
+			int y = x + it, cnt = 0;
+			while (!(y & 1)) {
+				y >>= 1;
+				cnt++;
+			}
+			chkmin(ans, max(0, 15 - cnt) + it);
+		}
+		cout << ans << ' ';
+	}
+	cout << '\n';
+}
