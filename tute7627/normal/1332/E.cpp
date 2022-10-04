2acd628004c378e582935a1cfb5a0a98976f17c6
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 13:13:42 +0530

    tute7627

---
+++ tute7627/normal/1332/E.cpp
@@ -0,0 +1,292 @@
+//#define _GLIBCXX_DEBUG
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define lfs cout<<fixed<<setprecision(10)
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define fi first
+#define se second
+#define MP make_pair
+#define MT make_tuple
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (ll)(m) - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+const ll MOD1 = 1e9+7;
+const ll MOD9 = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+bool chmin(T &a,T b){if(a>b){a=b;return true;}else return false;}
+template<typename T>
+bool chmax(T &a,T b){if(a<b){a=b;return true;}else return false;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,0,-1,0,1,1,-1,-1};
+vector<ll>dy={0,1,0,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+template<typename T1, typename T2>
+ostream &operator<<(ostream &os, pair<T1, T2>&p){
+  return os << p.first << " " << p.second;
+}  
+template< class T >
+struct Matrix {
+  vector< vector< T > > A;
+
+  Matrix() {}
+
+  Matrix(size_t n, size_t m) : A(n, vector< T >(m, 0)) {}
+
+  Matrix(size_t n) : A(n, vector< T >(n, 0)) {};
+
+  Matrix(vector<vector<T>>v):A(v){};
+
+  size_t height() const {
+    return (A.size());
+  }
+
+  size_t width() const {
+    return (A[0].size());
+  }
+
+  inline const vector< T > &operator[](int k) const {
+    return (A.at(k));
+  }
+
+  inline vector< T > &operator[](int k) {
+    return (A.at(k));
+  }
+
+  static Matrix I(size_t n) {
+    Matrix mat(n);
+    for(int i = 0; i < n; i++) mat[i][i] = 1;
+    return (mat);
+  }
+
+  Matrix &operator+=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for(int i = 0; i < n; i++)
+      for(int j = 0; j < m; j++)
+        (*this)[i][j] += B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator-=(const Matrix &B) {
+    size_t n = height(), m = width();
+    assert(n == B.height() && m == B.width());
+    for(int i = 0; i < n; i++)
+      for(int j = 0; j < m; j++)
+        (*this)[i][j] -= B[i][j];
+    return (*this);
+  }
+
+  Matrix &operator*=(const Matrix &B) {
+    size_t n = height(), m = B.width(), p = width();
+    assert(p == B.height());
+    vector< vector< T > > C(n, vector< T >(m, 0));
+    for(int i = 0; i < n; i++)
+      for(int j = 0; j < m; j++)
+        for(int k = 0; k < p; k++)
+          C[i][j] = (C[i][j] + (*this)[i][k] * B[k][j]);
+    A.swap(C);
+    return (*this);
+  }
+
+  Matrix &operator^=(long long k) {
+    Matrix B = Matrix::I(height());
+    while(k > 0) {
+      if(k & 1) B *= *this;
+      *this *= *this;
+      k >>= 1LL;
+    }
+    A.swap(B.A);
+    return (*this);
+  }
+
+  Matrix operator+(const Matrix &B) const {
+    return (Matrix(*this) += B);
+  }
+
+  Matrix operator-(const Matrix &B) const {
+    return (Matrix(*this) -= B);
+  }
+
+  Matrix operator*(const Matrix &B) const {
+    return (Matrix(*this) *= B);
+  }
+
+  Matrix operator^(const long long k) const {
+    return (Matrix(*this) ^= k);
+  }
+
+  friend ostream &operator<<(ostream &os, Matrix p) {
+    size_t n = p.height(), m = p.width();
+    for(int i = 0; i < n; i++) {
+      os << "[";
+      for(int j = 0; j < m; j++) {
+        os << p[i][j] << (j + 1 == m ? "]\n" : ",");
+      }
+    }
+    return (os);
+  }
+
+
+  T determinant() {
+    Matrix B(*this);
+    assert(width() == height());
+    T ret = 1;
+    for(int i = 0; i < width(); i++) {
+      int idx = -1;
+      for(int j = i; j < width(); j++) {
+        if(B[j][i] != 0) idx = j;
+      }
+      if(idx == -1) return (0);
+      if(i != idx) {
+        ret *= -1;
+        swap(B[i], B[idx]);
+      }
+      ret *= B[i][i];
+      T vv = B[i][i];
+      for(int j = 0; j < width(); j++) {
+        B[i][j] /= vv;
+      }
+      for(int j = i + 1; j < width(); j++) {
+        T a = B[j][i];
+        for(int k = 0; k < width(); k++) {
+          B[j][k] -= B[i][k] * a;
+        }
+      }
+    }
+    return (ret);
+  }
+};
+template< int mod >
+struct ModInt {
+  int x;
+
+  ModInt() : x(0) {}
+
+  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+  ModInt &operator+=(const ModInt &p) {
+    if((x += p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator-=(const ModInt &p) {
+    if((x += mod - p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator*=(const ModInt &p) {
+    x = (int) (1LL * x * p.x % mod);
+    return *this;
+  }
+
+  ModInt &operator/=(const ModInt &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+
+  ModInt operator-() const { return ModInt(-x); }
+
+  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+  bool operator==(const ModInt &p) const { return x == p.x; }
+
+  bool operator!=(const ModInt &p) const { return x != p.x; }
+
+  ModInt inverse() const {
+    int a = x, b = mod, u = 1, v = 0, t;
+    while(b > 0) {
+      t = a / b;
+      swap(a -= t * b, b);
+      swap(u -= t * v, v);
+    }
+    return ModInt(u);
+  }
+
+  ModInt pow(int64_t n) const {
+    ModInt ret(1), mul(x);
+    while(n > 0) {
+      if(n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  friend ostream &operator<<(ostream &os, const ModInt &p) {
+    return os << p.x;
+  }
+
+  friend istream &operator>>(istream &is, ModInt &a) {
+    int64_t t;
+    is >> t;
+    a = ModInt< mod >(t);
+    return (is);
+  }
+
+  static int get_mod() { return mod; }
+};
+using modint = ModInt< MOD9 >;modint pow(ll n, ll x){return modint(n).pow(x);}
+//using modint=ld;
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n,m,l,r;cin>>n>>m>>l>>r;
+  ll odd=0,even=0;
+  if(r%2==0)even++;
+  r++;
+  if(l&1)odd++,l++;
+  odd+=(r-l)/2;
+  even+=(r-l)/2;
+  //cout<<odd spa even<<endl;
+  Matrix<modint>mat({
+    {even,odd},
+    {odd,even}
+  });
+  mat^=n*m;
+  //cout<<mat<<endl;
+  if((n*m)&1){
+    cout<<mat[1][0]+mat[1][1]<<endl;
+  }
+  else cout<<mat[1][1]<<endl;
+  return 0;
+}
