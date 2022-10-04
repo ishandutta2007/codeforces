3266e537588da36f95c2a1a53cf28248164ddd68
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 13:24:58 +0530

    tute7627

---
+++ tute7627/normal/1271/C.cpp
@@ -0,0 +1,78 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define lfs cout<<fixed<<setprecision(10)
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define fi first
+#define se second
+#define MP make_pair
+#define MT make_tuple
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (m) - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+bool chmin(T &a,T b){if(a>b){a=b;return true;}else return false;}
+template<typename T>
+bool chmax(T &a,T b){if(a<b){a=b;return true;}else return false;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,0,-1,0,1,1,-1,-1};
+vector<ll>dy={0,1,0,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+ostream &operator<<(ostream &os, pair<ll, ll>&p){
+  return os << p.first << " " << p.second;
+}  
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n,sx,sy;cin>>n>>sx>>sy;
+  vector<ll>x(n),y(n);
+  rep(i,0,n)cin>>x[i]>>y[i];
+  vector<ll>p(4);
+  rep(i,0,n){
+    if(x[i]<sx)p[0]++;
+    if(x[i]>sx)p[1]++;
+    if(y[i]<sy)p[2]++;
+    if(y[i]>sy)p[3]++;
+  }
+  ll it=max_element(ALL(p))-p.begin();
+  cout<<p[it]<<endl;
+  if(it==0)cout<<sx-1 spa sy<<endl;
+  if(it==1)cout<<sx+1 spa sy<<endl;
+  if(it==2)cout<<sx spa sy-1<<endl;
+  if(it==3)cout<<sx spa sy+1<<endl;
+  return 0;
+}
