ec3c2054cc6429ba8d2d723cb576bd5bb4484de8
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 13:12:11 +0530

    tute7627

---
+++ tute7627/normal/992/E.cpp
@@ -0,0 +1,222 @@
+#pragma region Macros
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+#define ll long long
+#define ld long double
+#define rep2(i,a,b) for(ll i=a;i<=b;++i)
+#define rep(i,n) for(ll i=0;i<n;i++)
+#define rep3(i,a,b) for(ll i=a;i>=b;i--)
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define pq priority_queue
+#define pb push_back
+#define eb emplace_back
+#define vec vector<int>
+#define vecll vector<ll>
+#define vecpii vector<pii>
+#define vecpll vector<pll>
+#define vec2(a,b) vector<vec>(a,vec(b))
+#define vec2ll(a,b) vector<vecll>(a,vecll(b))
+#define vec3(a,b,c) vector<vector<vec>>(a,vec2(b,c))
+#define vec3ll(a,b,c) vector<vector<vecll>>(a,vec2ll(b,c))
+#define fi first
+#define se second
+#define endl "\n"
+#define all(c) begin(c),end(c)
+#define ios ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define lb(c,x) distance(c.begin(),lower_bound(all(c),(x)))
+#define ub(c,x) distance(c.begin(),upper_bound(all(c),(x)))
+using namespace std;
+int in() {int x;cin>>x;return x;}
+ll lin() {ll x;cin>>x;return x;}
+string stin() {string s;cin>>s;return s;}
+template<class T> inline bool chmax(T& a,T b){if(a<b){a=b;return 1;}return 0;}
+template<class T> inline bool chmin(T& a,T b){if(a>b){a=b;return 1;}return 0;}
+vec iota(int n){vec a(n);iota(all(a),0);return a;}
+void print(){putchar(' ');}
+void print(bool a){cout<<a;}
+void print(int a){cout<<a;}
+void print(long long a){cout<<a;}
+void print(char a){cout<<a;}
+void print(string &a){cout<<a;}
+void print(double a){cout<<a;}
+template<class T> void print(const vector<T>&);
+template<class T, size_t size> void print(const array<T, size>&);
+template<class T, class L> void print(const pair<T, L>& p);
+template<class T, size_t size> void print(const T (&)[size]);
+template<class T> void print(const vector<T>& a){ if(a.empty()) return; print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } cout<<endl;}
+template<class T> void print(const deque<T>& a){ if(a.empty()) return; print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } }
+template<class T, size_t size> void print(const array<T, size>& a){ print(a[0]); for(auto i = a.begin(); ++i != a.end(); ){ cout<<" "; print(*i); } }
+template<class T, class L> void print(const pair<T, L>& p){ cout<<'(';print(p.first); cout<<","; print(p.second);cout<<')'; }
+template<class T, size_t size> void print(const T (&a)[size]){ print(a[0]); for(auto i = a; ++i != end(a); ){ cout<<" "; print(*i); } }
+template<class T> void print(const T& a){ cout << a; }
+int out(){ putchar('\n'); return 0; }
+template<class T> int out(const T& t){ print(t); putchar('\n'); return 0; }
+template<class Head, class... Tail> int out(const Head& head, const Tail&... tail){ print(head); putchar(' '); out(tail...); return 0; }
+ll gcd(ll a, ll b){ while(b){ ll c = b; b = a % b; a = c; } return a; }
+ll lcm(ll a, ll b){ if(!a || !b) return 0; return a * b / gcd(a, b); }
+#define _GLIBCXX_DEBU
+#ifdef _MY_DEBUG
+    #undef endl
+    #define debug(x) cout<<#x<<": "<<x<<endl
+    void err(){}
+    template<class T> void err(const T& t){ print(t);  cout<<" ";}
+    template<class Head, class... Tail> void err(const Head& head, const Tail&... tail){ print(head); putchar(' '); out(tail...); }
+#else
+    #define debug(x)
+    template<class... T> void err(const T&...){}
+#endif
+#pragma endregion
+
+
+const int T =300;
+
+template< typename Monoid >
+struct SegmentTree {
+  using F = function< Monoid(Monoid, Monoid) >;
+
+  int sz;
+  vector< Monoid > seg;
+
+  const F f;
+  const Monoid M1;
+
+  SegmentTree(int n, const F f, const Monoid &M1) : f(f), M1(M1) {
+    sz = 1;
+    while(sz < n) sz <<= 1;
+    seg.assign(2 * sz, M1);
+  }
+
+  void set(int k, const Monoid &x) {
+    seg[k + sz] = x;
+  }
+
+  void build() {
+    for(int k = sz - 1; k > 0; k--) {
+      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    }
+  }
+
+  void update(int k, const Monoid &x) {
+    k += sz;
+    seg[k] = x;
+    while(k >>= 1) {
+      seg[k] = f(seg[2 * k + 0], seg[2 * k + 1]);
+    }
+  }
+
+  Monoid query(int a, int b) {
+    Monoid L = M1, R = M1;
+    for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if(a & 1) L = f(L, seg[a++]);
+      if(b & 1) R = f(seg[--b], R);
+    }
+    return f(L, R);
+  }
+
+  Monoid operator[](const int &k) const {
+    return seg[k + sz];
+  }
+
+  template< typename C >
+  int find_subtree(int a, const C &check, Monoid &M, bool type) {
+    while(a < sz) {
+      Monoid nxt = type ? f(seg[2 * a + type], M) : f(M, seg[2 * a + type]);
+      if(check(nxt)) a = 2 * a + type;
+      else M = nxt, a = 2 * a + 1 - type;
+    }
+    return a - sz;
+  }
+
+
+  template< typename C >
+  int find_first(int a, const C &check) {
+    Monoid L = M1;
+    if(a <= 0) {
+      if(check(f(L, seg[1]))) return find_subtree(1, check, L, false);
+      return -1;
+    }
+    int b = sz;
+    for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if(a & 1) {
+        Monoid nxt = f(L, seg[a]);
+        if(check(nxt)) return find_subtree(a, check, L, false);
+        L = nxt;
+        ++a;
+      }
+    }
+    return -1;
+  }
+
+  template< typename C >
+  int find_last(int b, const C &check) {
+    Monoid R = M1;
+    if(b >= sz) {
+      if(check(f(seg[1], R))) return find_subtree(1, check, R, true);
+      return -1;
+    }
+    int a = sz;
+    for(b += sz; a < b; a >>= 1, b >>= 1) {
+      if(b & 1) {
+        Monoid nxt = f(seg[--b], R);
+        if(check(nxt)) return find_subtree(b, check, R, true);
+        R = nxt;
+      }
+    }
+    return -1;
+  }
+};
+
+constexpr int N = 200010;
+
+
+signed main(){
+    ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);cout<<fixed<<setprecision(15);
+    int n=in(),q=in();
+    // RSQ
+    SegmentTree<ll> su(N,[](ll x,ll y){return x+y;},0LL);
+
+    // RMQ
+    SegmentTree<ll> mi(N,[](ll x,ll y){return max(x,y);},-LONG_LONG_MAX);
+    rep(i,n){
+        int x=in();
+        su.set(i,x);
+        mi.set(i,x);
+    }
+    su.build();mi.build();
+    rep(_i,q){
+        int p=in()-1;
+        ll x=in();
+        su.update(p,x);
+        mi.update(p,x);
+        if(su[0]==0){
+            cout<<1<<endl;continue;
+        }
+        ll now=su[0];
+        int pos=0;
+        bool flag=true;
+        while(1){
+            int next;
+            if(mi[pos+1]>=now){
+                next=pos+1;
+            }
+            else next = mi.find_first(pos+1,[&](ll x){return x>=now;});
+            if(next==-1){
+                flag=false;
+                break;
+            }
+            if(su.query(0,next)==mi[next]){
+                cout<<next+1<<endl;
+                break;
+            }
+            now=su.query(0,next+1);
+            pos=next;
+            if(pos==n-1){
+                flag=false;
+                break;
+            }
+        }
+        if(!flag)cout<<-1<<endl;
+    }
+
+}
