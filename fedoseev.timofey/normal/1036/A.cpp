57e231592109f10186f3cd980545fb5437aaf7a1
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 16:03:37 +0530

    fedoseev.timofey

---
+++ fedoseev.timofey/normal/1036/A.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <ctime>
+#include <random>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <list>
+#include <unordered_set>
+#include <unordered_map>
+#include <cassert>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.setf(ios::fixed); cout.precision(20);
+    #ifdef LOCAL
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ll n, k;
+    cin >> n >> k;
+    ll one = (k + n - 1) / n;
+    cout << one << '\n';
+}
