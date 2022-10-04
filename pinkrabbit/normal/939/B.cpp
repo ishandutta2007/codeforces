a63021ed0bbdc69eacd85d8ed989106b57c4f739
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:11:11 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/939/B.cpp
@@ -0,0 +1,36 @@
+#include<cstdio>
+#include<cstring>
+#include<algorithm>
+using namespace std;
+#define F(i,a,b) for(int i=(a);i<=(b);++i)
+#define F2(i,a,b) for(int i=(a);i<(b);++i)
+#define dF(i,a,b) for(int i=(a);i>=(b);--i)
+#define dF2(i,a,b) for(int i=(a);i>(b);--i)
+#include<cmath>
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+#include<set>
+#include<map>
+#define ll long long
+#define eF(i,u) for(int i=h[u];i;i=nxt[i])
+const int INF=0x3f3f3f3f;
+inline int Gcd(int X,int Y){return Y?Gcd(Y,X%Y):X;}
+inline ll Gcd(ll X,ll Y){return Y?Gcd(Y,X%Y):X;}
+inline int Max(int X,int Y){return X<Y?Y:X;}
+inline int Min(int X,int Y){return X<Y?X:Y;}
+inline ll Max(ll X,ll Y){return X<Y?Y:X;}
+inline ll Min(ll X,ll Y){return X<Y?X:Y;}
+inline int Pow(int base,ll exp,int _mod){int _ans=1;for(;exp;exp>>=1,base=(ll)base*base%_mod)exp&1?_ans=(ll)_ans*base%_mod:0;return _ans;}
+inline ll Pow(ll base,ll exp,ll _mod){ll _ans=1;for(;exp;exp>>=1,base=base*base%_mod)exp&1?_ans=_ans*base%_mod:0;return _ans;}
+ll n;int k;
+ll a[100001];
+int ans1; ll ans2=7000000000000000000ll;
+int main(){
+	scanf("%lld%d",&n,&k);
+	F(i,1,k) scanf("%lld",a+i);
+	F(i,1,k) if(n-n/a[i]*a[i]<ans2) ans2=n-n/a[i]*a[i], ans1=i;
+	printf("%d %lld",ans1,n/a[ans1]);
+	return 0;
+}
