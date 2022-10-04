04c6511e56d0d5940deae2e701dbec9a0ec2a344
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 12:03:06 +0530

    tute7627

---
+++ tute7627/normal/1179/C.cpp
@@ -0,0 +1,228 @@
+//#define _GLIBCXX_DEBUG
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define lfs cout<<fixed<<setprecision(10)
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define fi first
+#define se second
+#define MP make_pair
+#define MT make_tuple
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (ll)(m) - 1; i >= (ll)(n); i--)
+using ll = int;
+using ld = long double;
+const ll MOD1 = 1e9+7;
+const ll MOD9 = 998244353;
+const ll INF = 1e9;
+using P = pair<ll, ll>;
+template<typename T1, typename T2>bool chmin(T1 &a,T2 b){if(a>b){a=b;return true;}else return false;}
+template<typename T1, typename T2>bool chmax(T1 &a,T2 b){if(a<b){a=b;return true;}else return false;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++){cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++){for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>vector<vector<T>>vec(ll x, ll y, T w){vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,-1,0,0,1,1,-1,-1};vector<ll>dy={0,0,1,-1,1,-1,1,-1};
+template<typename T>vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>auto make_v(size_t a,Ts... ts){return vector<decltype(make_v(ts...))>(a,make_v(ts...));}
+template<typename T1, typename T2>ostream &operator<<(ostream &os, const pair<T1, T2>&p){return os << p.first << " " << p.second;}
+template<typename T>ostream &operator<<(ostream &os, const vector<T> &v){for(auto &z:v)os << z << " ";cout<<"|"; return os;}
+//mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+int popcount(ll x){return __builtin_popcountll(x);};
+int poplow(ll x){return __builtin_ctzll(x);};
+int pophigh(ll x){return 63 - __builtin_clzll(x);};
+template< typename Monoid, typename OperatorMonoid,typename F, typename G, typename H>
+struct LazySegmentTree {
+  ll sz, height, n;
+  vector< Monoid > data;
+  vector< OperatorMonoid > lazy;
+  const F f;
+  const G g;
+  const H h;
+  Monoid M1;
+  OperatorMonoid OM0;
+  LazySegmentTree(ll n, const F &f,const G &g, const H &h, Monoid M1, OperatorMonoid OM0):n(n),f(f),g(g),h(h),M1(M1),OM0(OM0){
+    sz = 1;
+    height = 0;
+    while(sz < n) sz <<= 1, height++;
+    data.assign(2 * sz, M1);
+    lazy.assign(2 * sz, OM0);
+  }
+
+  void set(ll k, const Monoid &x) {
+    data[k + sz] = x;
+  }
+
+  void build() {
+    for(ll k = sz - 1; k > 0; k--) {
+      data[k] = f(data[2 * k + 0], data[2 * k + 1]);
+    }
+  }
+
+  inline void propagate(ll k) {
+    if(lazy[k] != OM0) {
+      lazy[2 * k + 0] = h(lazy[2 * k + 0], lazy[k]);
+      lazy[2 * k + 1] = h(lazy[2 * k + 1], lazy[k]);
+      data[k] = reflect(k);
+      lazy[k] = OM0;
+    }
+  }
+
+  inline Monoid reflect(ll k) {
+    return lazy[k] == OM0 ? data[k] : g(data[k], lazy[k]);
+  }
+
+  inline void recalc(ll k) {
+    while(k >>= 1) data[k] = f(reflect(2 * k + 0), reflect(2 * k + 1));
+  }
+
+  inline void thrust(ll k) {
+    for(ll i = height; i > 0; i--) propagate(k >> i);
+  }
+
+  void update(ll a, ll b, const OperatorMonoid &x) {
+	if(a>=b)return;
+    thrust(a += sz);
+    thrust(b += sz - 1);
+    for(ll l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+      if(l & 1) lazy[l] = h(lazy[l], x), ++l;
+      if(r & 1) --r, lazy[r] = h(lazy[r], x);
+    }
+    recalc(a);
+    recalc(b);
+  }
+
+  Monoid query(ll a, ll b) {
+    thrust(a += sz);
+    thrust(b += sz - 1);
+    Monoid L = M1, R = M1;
+    for(ll l = a, r = b + 1; l < r; l >>= 1, r >>= 1) {
+      if(l & 1) L = f(L, reflect(l++));
+      if(r & 1) R = f(reflect(--r), R);
+    }
+    return f(L, R);
+  }
+
+  Monoid operator[](const ll &k) {
+    return query(k, k + 1);
+  }
+
+  template< typename C >
+  ll find_subtree(ll a, const C &check, Monoid &M, bool type) {
+    while(a < sz) {
+      propagate(a);
+      Monoid nxt = type ? f(reflect(2 * a + type), M) : f(M, reflect(2 * a + type));
+      if(check(nxt)) a = 2 * a + type;
+      else M = nxt, a = 2 * a + 1 - type;
+    }
+    return a - sz;
+  }
+
+  template< typename C >
+  ll find_first(ll a, const C &check) {
+    Monoid L = M1;
+    if(a <= 0) {
+      if(check(f(L, reflect(1)))) return find_subtree(1, check, L, false);
+      return -1;
+    }
+    thrust(a + sz);
+    ll b = sz;
+    for(a += sz, b += sz; a < b; a >>= 1, b >>= 1) {
+      if(a & 1) {
+        Monoid nxt = f(L, reflect(a));
+        if(check(nxt)) return find_subtree(a, check, L, false);
+        L = nxt;
+        ++a;
+      }
+    }
+    return -1;
+  }
+
+
+  template< typename C >
+  ll find_last(ll b, const C &check) {
+    Monoid R = M1;
+    if(b >= sz) {
+      if(check(f(reflect(1), R))) return find_subtree(1, check, R, true);
+      return -1;
+    }
+    thrust(b + sz - 1);
+    ll a = sz;
+    for(b += sz; a < b; a >>= 1, b >>= 1) {
+      if(b & 1) {
+        Monoid nxt = f(reflect(--b), R);
+        if(check(nxt)) return find_subtree(b, check, R, true);
+        R = nxt;
+      }
+    }
+    return -1;
+  }
+  void print(){
+    for(ll i=0;i<n;i++)if((*this)[i]==M1)cout<<"x|";else cout<<(*this)[i]<<"|";
+    cout<<endl;
+  }
+};
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  ll res=0,buf=0;
+  bool judge = true;
+  auto ff=[&](ll x,ll y){
+    return min(x,y);
+  };
+  auto gg=[&](ll x,ll y){
+    return x+y;
+  };
+  auto hh=[&](ll x,ll y){
+    return x+y;
+  };
+  ll sz=1000005;
+  //ll sz=105;cout<<"naosu"<<endl;
+  LazySegmentTree<ll,ll,decltype(ff),decltype(gg),decltype(hh)>seg(sz,ff,gg,hh,INF,0);
+  ll n,m;cin>>n>>m;
+  vector<ll>a(n),b(m);
+  vector<ll>c(sz);
+  rep(i,0,n){
+    cin>>a[i];
+    c[a[i]]--;
+  }
+  rep(i,0,m){
+    cin>>b[i];
+    c[b[i]]++;
+  }
+  rrep(i,0,sz-1){
+    c[i]+=c[i+1];
+  }
+  rep(i,0,sz)seg.set(i,c[i]);
+  seg.build();
+  auto ch=[&](ll k){return k<0;};
+  ll q;cin>>q;
+  while(q--){
+    ll t,i,x;cin>>t>>i>>x;i--;
+    if(t==1){
+      seg.update(0,a[i]+1,1);
+      a[i]=x;
+      seg.update(0,a[i]+1,-1);
+    }
+    else{
+      seg.update(0,b[i]+1,-1);
+      b[i]=x;
+      seg.update(0,b[i]+1,1);
+    }
+    cout<<seg.find_last(sz,ch)<<endl;
+  }
+  //seg.print();
+  return 0;
+}
