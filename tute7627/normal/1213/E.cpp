01c6d22678d5b56cc49c653170078699e6806090
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:17:36 +0530

    tute7627

---
+++ tute7627/normal/1213/E.cpp
@@ -0,0 +1,117 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define rep(i,n,m) for(ll i = n; i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = n - 1; i >= (ll)(m); i--)
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>v,ll n){cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+//m.emplace(x,0).fi->se++;
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  ll res=0,res1=INF,res2=-INF,buf=0;
+  bool judge = true;
+  ll n;cin>>n;
+  string s,t;cin>>s>>t;
+  string r="abc";
+  string ret;
+  if(t[0]==t[1])swap(s,t);
+  if(s[0]==s[1]){
+    do{
+      string x=r.substr(0,2);
+      string y=r.substr(1,2);
+      string z=r.substr(2,1)+r[0];
+      if(t==x||s==x)continue;
+      if(t==y||s==y)continue;
+      if(t==z||s==z)continue;
+      break;
+    }while(next_permutation(ALL(r)));
+    rep(i,0,n)ret+=r;
+  }
+  else if(s[0]==t[0]){
+    do{
+      string x=r.substr(0,2);
+      string y=r.substr(1,2);
+      string z=r.substr(1,1)+r[0];
+      if(t==x||s==x)continue;
+      if(t==z||s==z)continue;
+      break;
+    }while(next_permutation(ALL(r)));
+    rep(i,0,n)ret+=r.substr(0,2);
+    rep(i,0,n)ret+=r[2];
+  }
+  else if(s[1]==t[1]){
+    do{
+      string x=r.substr(0,2);
+      string y=r.substr(1,2);
+      string z=r.substr(1,1)+r[0];
+      if(t==x||s==x)continue;
+      if(t==z||s==z)continue;
+      break;
+    }while(next_permutation(ALL(r)));
+    rep(i,0,n)ret+=r[2];
+    rep(i,0,n)ret+=r.substr(0,2);
+  }
+  else if(s[0]==t[1]&&s[1]==t[0]){
+    do{
+      if(r[1]==s[1]||r[1]==s[0])continue;
+      break;
+    }while(next_permutation(ALL(r)));
+    rep(i,0,n)ret+=r[0];
+    rep(i,0,n)ret+=r[1];
+    rep(i,0,n)ret+=r[2];
+  }
+  else{
+    do{
+      string x=r.substr(0,2);
+      string y=r.substr(1,2);
+      string z=r.substr(2,1)+r[0];
+      if(t==x||s==x)continue;
+      if(t==y||s==y)continue;
+      if(t==z||s==z)continue;
+      break;
+    }while(next_permutation(ALL(r)));
+    rep(i,0,n)ret+=r;
+  }
+  cout<<"YES"<<endl;
+  cout<<ret<<endl;
+  return 0;
+}
