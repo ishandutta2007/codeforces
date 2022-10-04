3138d112fdb51dd12a817f93244a6e6eb95f645d
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 08:49:35 +0530

    a.k.e.e

---
+++ a.k.e.e/normal/1583/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+template <typename T> void chkmin(T &x,T y){y<x?x=y:T();}
+template <typename T> void chkmax(T &x,T y){x<y?x=y:T();}
+template <typename T> void readint(T &x)
+{
+	int f=1;char c;x=0;
+	for(c=getchar();!isdigit(c);c=getchar())if(c=='-')f=-1;
+	for(;isdigit(c);c=getchar())x=x*10+(c-'0');
+	x*=f;
+}
+/*const int MOD=
+inline int dmy(int x){return x>=MOD?x-MOD:x;}
+inline void inc(int &x,int y){x=dmy(x+y);}
+int qmi(int x,int y)
+{
+	int ans=1;
+	for(;y;y>>=1,x=1ll*x*x%MOD)
+		if(y&1)ans=1ll*ans*x%MOD;
+	return ans;
+}*/
+const int MAXN=100005;
+
+int n;
+int a[MAXN],timer;
+void solve()
+{
+	++timer;
+	int m;
+	readint(n),readint(m);
+	while(m--)
+	{
+		int s,t,u;readint(s),readint(t),readint(u);
+		a[t]=timer;
+	}
+	int u=0;
+	for(int i=1;i<=n;++i)if(a[i]!=timer)u=i;
+	for(int i=1;i<=n;++i)if(i!=u)printf("%d %d\n",i,u);
+}
+
+int main()
+{
+	#ifdef LOCAL
+	freopen("code.in","r",stdin);
+//	freopen("code.out","w",stdout);
+	#endif
+	int T;
+	readint(T);
+	while(T--)solve();
+	return 0;
+}
