a557d687140a7758fb9d5f05bcdf0418a3ba5971
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 12:33:19 +0530

    tute7627

---
+++ tute7627/normal/806/C.cpp
@@ -0,0 +1,146 @@
+//#define _GLIBCXX_DEBUG
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define lfs cout<<fixed<<setprecision(10)
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define fi first
+#define se second
+#define MP make_pair
+#define MT make_tuple
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (ll)(m) - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+const ll MOD1 = 1e9+7;
+const ll MOD9 = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T1, typename T2>
+bool chmin(T1 &a,T2 b){if(a>b){a=b;return true;}else return false;}
+template<typename T1, typename T2>
+bool chmax(T1 &a,T2 b){if(a<b){a=b;return true;}else return false;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,-1,0,0,1,1,-1,-1};
+vector<ll>dy={0,0,1,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+template<typename T1, typename T2>
+ostream &operator<<(ostream &os, const pair<T1, T2>&p){
+  return os << p.first << " " << p.second;
+}
+//mt19937 mt(chrono::steady_clock::now().time_since_epoch().count());
+
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n;cin>>n;
+  vector<ll>a(n);
+  rep(i,0,n)cin>>a[i];
+  ll sz=42;
+  map<ll,ll>mp;
+  vector<ll>c(sz);
+  {
+    ll tmp=1;
+    rep(i,0,sz){
+      mp[tmp]=i;
+      c[i]=tmp;
+      tmp*=2;
+    }
+  }
+  priority_queue<ll>que;
+  vector<ll>b(sz);
+  rep(i,0,n){
+    if(mp.count(a[i])){
+      b[mp[a[i]]]++;
+    }
+    else que.push(a[i]);
+  }
+  ll rem=n;
+  ll mx=sz-1;
+  ll cap=0;
+  ll num=0;
+  while(!que.empty()&&judge){
+    while(mx>=0&&b[mx]==0)mx--;
+    if(mx==-1||que.top()>c[mx]*2){
+      judge=false;
+      break;
+    }
+    que.pop();
+    rem--;
+    rrep(i,0,mx+1){
+      if(b[i]<=0)judge=false;
+      b[i]--;
+      rem--;
+    }
+    num++;
+  }
+  ll l=INF;
+  if(rem==0)l=num;
+  while(rem>0&&judge){
+    if(cap>=rem)chmin(l,num);
+    while(mx>=0&&b[mx]==0)mx--;
+    if(mx<0)break;
+    ll pre=rem;
+    bool back=false;
+    rrep(i,0,mx+1){
+      if(b[i]<=0){
+        if(cap==0)judge=false;
+        else{
+          cap--;
+          back=true;
+        }
+        rem=pre-1;
+        rep(j,i+1,mx)b[j]++;
+        break;
+      }
+      b[i]--;
+      rem--;
+    }
+    if(back)continue;
+    cap++;
+    num++;
+    if(cap>=rem)chmin(l,num);
+    //cout<<rem spa mx<<endl;
+  }
+  //debug(b,10);
+  if(cap<rem)judge=false;
+  //cout<<l spa num spa judge spa cap spa rem spa mx<<endl;
+  if(!judge)cout<<-1<<endl;
+  else{
+    //cout<<l spa num<<endl;
+    vector<ll>v;
+    rep(i,l,num+1)v.PB(i);
+    debug(v,v.size());
+  }
+  return 0;
+}
