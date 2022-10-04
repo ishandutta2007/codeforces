a5c5a84818ef6877e4dce84b3fbd571367ce75d7
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 13:50:36 +0530

    tute7627

---
+++ tute7627/normal/1082/G.cpp
@@ -0,0 +1,159 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (m) - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e14;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,0,-1,0,1,1,-1,-1};
+vector<ll>dy={0,1,0,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+ostream &operator<<(ostream &os, pair<ll, ll>&p){
+  return os << p.first << " " << p.second;
+} 
+template< typename flow_t >
+struct Dinic {
+  const flow_t INF;
+
+  struct edge {
+    ll to;
+    flow_t cap;
+    ll rev;
+    bool isrev;
+  };
+
+  vector< vector< edge > > graph;
+  vector< ll > min_cost, iter;
+
+  Dinic(ll V) : INF(numeric_limits< flow_t >::max()), graph(V) {}
+
+  void add_edge(ll from, ll to, flow_t cap) {
+    graph[from].emplace_back((edge) {to, cap, (ll) graph[to].size(), false});
+    graph[to].emplace_back((edge) {from, 0, (ll) graph[from].size() - 1, true});
+  }
+
+  bool bfs(ll s, ll t) {
+    min_cost.assign(graph.size(), -1);
+    queue< ll > que;
+    min_cost[s] = 0;
+    que.push(s);
+    while(!que.empty() && min_cost[t] == -1) {
+      ll p = que.front();
+      que.pop();
+      for(auto &e : graph[p]) {
+        if(e.cap > 0 && min_cost[e.to] == -1) {
+          min_cost[e.to] = min_cost[p] + 1;
+          que.push(e.to);
+        }
+      }
+    }
+    return min_cost[t] != -1;
+  }
+
+  flow_t dfs(ll idx, const ll t, flow_t flow) {
+    if(idx == t) return flow;
+    for(ll &i = iter[idx]; i < graph[idx].size(); i++) {
+      edge &e = graph[idx][i];
+      if(e.cap > 0 && min_cost[idx] < min_cost[e.to]) {
+        flow_t d = dfs(e.to, t, min(flow, e.cap));
+        if(d > 0) {
+          e.cap -= d;
+          graph[e.to][e.rev].cap += d;
+          return d;
+        }
+      }
+    }
+    return 0;
+  }
+
+  flow_t max_flow(ll s, ll t) {
+    flow_t flow = 0;
+    while(bfs(s, t)) {
+      iter.assign(graph.size(), 0);
+      flow_t f = 0;
+      while((f = dfs(s, t, INF)) > 0) flow += f;
+    }
+    return flow;
+  }
+
+  void output() {
+    for(ll i = 0; i < graph.size(); i++) {
+      for(auto &e : graph[i]) {
+        if(e.isrev) continue;
+        auto &rev_e = graph[e.to][e.rev];
+        cout << i << "->" << e.to << " (flow: " << rev_e.cap << "/" << e.cap + rev_e.cap << ")" << endl;
+      }
+    }
+  }
+}; 
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  //cout<<fixed<<setprecision(10);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n,m;cin>>n>>m;
+  Dinic<ll>dinic(n+m+2);
+  ll s=n+m,t=n+m+1;
+  rep(i,0,n){
+    ll x;cin>>x;
+    dinic.add_edge(s,i,0);
+    dinic.add_edge(i,t,x);
+  }
+  rep(i,0,m){
+    ll u,v,w;cin>>u>>v>>w;
+    u--;v--;
+    res+=w;
+    dinic.add_edge(s,n+i,w);
+    dinic.add_edge(n+i,t,0);
+    dinic.add_edge(n+i,u,INF);
+    dinic.add_edge(n+i,v,INF);
+  }
+  cout<<res-dinic.max_flow(s,t)<<endl;
+  return 0;
+}
