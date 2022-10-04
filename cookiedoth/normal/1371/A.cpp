ace16e6d8b9d1295ef5e66e40cb6b57e6754e8a7
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:48:22 +0530

    cookiedoth

---
+++ cookiedoth/normal/1371/A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void solve() {
+	int n;
+	cin >> n;
+	cout << (n + 1) / 2 << '\n';
+}
+
+signed main() {
+	int T;
+	cin >> T;
+	while (T--) {
+		solve();
+	}
+}
