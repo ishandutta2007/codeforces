4635710a8a76c5b002696b6d005edb0884dd9cf8
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 13:54:23 +0530

    tute7627

---
+++ tute7627/normal/1096/G.cpp
@@ -0,0 +1,190 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define int long long
+#define endl '\n'
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = n; i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = m - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+//const ll MOD = 1e9+7;
+const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,0,-1,0,1,1,-1,-1};
+vector<ll>dy={0,1,0,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+ostream &operator<<(ostream &os, pair<ll, ll>&p){
+  return os << p.first << " " << p.second;
+}  
+template< int mod >
+struct NumberTheoreticTransform {
+
+  vector< int > rev, rts;
+  int base, max_base, root;
+
+  NumberTheoreticTransform() : base(1), rev{0, 1}, rts{0, 1} {
+    assert(mod >= 3 && mod % 2 == 1);
+    auto tmp = mod - 1;
+    max_base = 0;
+    while(tmp % 2 == 0) tmp >>= 1, max_base++;
+    root = 2;
+    while(mod_pow(root, (mod - 1) >> 1) == 1) ++root;
+    assert(mod_pow(root, mod - 1) == 1);
+    root = mod_pow(root, (mod - 1) >> max_base);
+  }
+
+  inline int mod_pow(int x, int n) {
+    int ret = 1;
+    while(n > 0) {
+      if(n & 1) ret = mul(ret, x);
+      x = mul(x, x);
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  inline int inverse(int x) {
+    return mod_pow(x, mod - 2);
+  }
+
+  inline unsigned add(unsigned x, unsigned y) {
+    x += y;
+    if(x >= mod) x -= mod;
+    return x;
+  }
+
+  inline unsigned mul(unsigned a, unsigned b) {
+    return 1ull * a * b % (unsigned long long) mod;
+  }
+
+  void ensure_base(int nbase) {
+    if(nbase <= base) return;
+    rev.resize(1 << nbase);
+    rts.resize(1 << nbase);
+    for(int i = 0; i < (1 << nbase); i++) {
+      rev[i] = (rev[i >> 1] >> 1) + ((i & 1) << (nbase - 1));
+    }
+    assert(nbase <= max_base);
+    while(base < nbase) {
+      int z = mod_pow(root, 1 << (max_base - 1 - base));
+      for(int i = 1 << (base - 1); i < (1 << base); i++) {
+        rts[i << 1] = rts[i];
+        rts[(i << 1) + 1] = mul(rts[i], z);
+      }
+      ++base;
+    }
+  }
+
+
+  void ntt(vector< int > &a) {
+    const int n = (int) a.size();
+    assert((n & (n - 1)) == 0);
+    int zeros = __builtin_ctz(n);
+    ensure_base(zeros);
+    int shift = base - zeros;
+    for(int i = 0; i < n; i++) {
+      if(i < (rev[i] >> shift)) {
+        swap(a[i], a[rev[i] >> shift]);
+      }
+    }
+    for(int k = 1; k < n; k <<= 1) {
+      for(int i = 0; i < n; i += 2 * k) {
+        for(int j = 0; j < k; j++) {
+          int z = mul(a[i + j + k], rts[j + k]);
+          a[i + j + k] = add(a[i + j], mod - z);
+          a[i + j] = add(a[i + j], z);
+        }
+      }
+    }
+  }
+
+
+  vector< int > multiply(vector< int > a, vector< int > b) {
+    int need = a.size() + b.size() - 1;
+    int nbase = 1;
+    while((1 << nbase) < need) nbase++;
+    ensure_base(nbase);
+    int sz = 1 << nbase;
+    a.resize(sz, 0);
+    b.resize(sz, 0);
+    ntt(a);
+    ntt(b);
+    int inv_sz = inverse(sz);
+    for(int i = 0; i < sz; i++) {
+      a[i] = mul(a[i], mul(b[i], inv_sz));
+    }
+    reverse(a.begin() + 1, a.end());
+    ntt(a);
+    a.resize(need);
+    return a;
+  }
+};
+signed main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  //cout<<fixed<<setprecision(10);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n,k;cin>>n>>k;
+  vector<int>d(k);
+  rep(i,0,k)cin>>d[i];
+  NumberTheoreticTransform<MOD>ntt;
+  vector<int>tmp(10),dp(1);
+  dp[0]=1;
+  rep(i,0,k)tmp[d[i]]=1;
+  buf=n/2;
+  while(buf){
+    if(buf&1)dp=ntt.multiply(dp,tmp);
+    tmp=ntt.multiply(tmp,tmp);
+    buf/=2;
+  }
+  ll ret=0;
+  rep(i,0,dp.size()){
+    //cout<<ret<<endl;
+    ret+=dp[i]*dp[i]%MOD;
+    ret%=MOD;
+  }
+  cout<<ret<<endl;
+  return 0;
+}
