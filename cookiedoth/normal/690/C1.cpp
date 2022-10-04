68e586c39e82f4c5dcf7d3eb94f4c5dddf9a298c
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:37:16 +0530

    cookiedoth

---
+++ cookiedoth/normal/690/C1.cpp
@@ -0,0 +1,131 @@
+/*
+
+Code for problem C1 by cookiedoth
+Generated 21 Mar 2021 at 08.17 PM
+
+
+
+
+
+
+
+
+
+
+(@)(@)(@)(@)(@)
+
+z_z
+=_=
+\_()_/
+
+*/
+
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <ctime>
+#include <functional>
+#include <unordered_set>
+#include <unordered_map>
+#include <string>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <complex>
+#include <cassert>
+#include <random>
+#include <cstring>
+#include <numeric>
+#include <random>
+#include <utility>
+#include <tuple>
+#define ll long long
+#define ld long double
+#define null NULL
+#define all(a) a.begin(), a.end()
+#define rall(a) a.rbegin(), a.rend()
+#define debug(a) cerr << #a << " = " << a << endl
+#define forn(i, n) for (int i = 0; i < n; ++i)
+#define length(a) (int)a.size()
+
+using namespace std;
+
+template<class T> int chkmax(T &a, T b) {
+	if (b > a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class T> int chkmin(T &a, T b) {
+	if (b < a) {
+		a = b;
+		return 1;
+	}
+	return 0;
+}
+
+template<class iterator> void output(iterator begin, iterator end, ostream& out = cerr) {
+	while (begin != end) {
+		out << (*begin) << " ";
+		begin++;
+	}
+	out << endl;
+}
+
+template<class T> void output(T x, ostream& out = cerr) {
+	output(x.begin(), x.end(), out);
+}
+
+void fast_io() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+}
+
+int n, m;
+vector<vector<int>> g;
+vector<int> used;
+
+void dfs(int v) {
+	used[v] = 1;
+	for (auto v1 : g[v]) {
+		if (used[v1] == 0) {
+			dfs(v1);
+		}
+	}
+}
+
+signed main() {
+	fast_io();
+	cin >> n >> m;
+	g.resize(n);
+	for (int i = 0; i < m; ++i) {
+		int u, v;
+		cin >> u >> v;
+		u--;
+		v--;
+		g[u].push_back(v);
+		g[v].push_back(u);
+	}
+	if (m != n - 1) {
+		cout << "no\n";
+		exit(0);
+	}
+	used.resize(n);
+	dfs(0);
+	for (int i = 0; i < n; ++i) {
+		if (used[i] == 0) {
+			cout << "no\n";
+			exit(0);
+		}
+	}
+	cout << "yes\n";
+}
