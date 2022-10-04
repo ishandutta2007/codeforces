c4a2465d10a6cf66dda5b3eee311af31b4446851
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:16:45 +0530

    tute7627

---
+++ tute7627/normal/1214/D.cpp
@@ -0,0 +1,108 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define lfs <<fixed<<setprecision(10)<<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define rep(i,n,m) for(ll i = n; i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = n - 1; i >= (ll)(m); i--)
+typedef int ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e9;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>v,ll n){cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+template<typename T>
+void emp(map<T,ll>&m, T x){m.emplace(x,0).first->second++;}
+
+
+ll n,m;
+vector<string>a(1000001);
+vector<ll>t1(1000001);
+vector<ll>t2(1000001);
+void dfs1(ll x,ll y){
+  if(x<0||y<0||x>=n||y>=m||a[x][y]=='#')return;
+  //cout<<x spa y<<endl;
+  if(t1[x*m+y]==0){
+    t1[x*m+y]=1;
+    dfs1(x+1,y);
+    dfs1(x,y+1);
+  }
+  return;
+}
+void dfs2(ll x,ll y){
+  if(x<0||y<0||x>=n||y>=m||a[x][y]=='#')return;
+  //cout<<x spa y<<endl;
+  if(t2[x*m+y]==0){
+    t2[x*m+y]=1;
+    dfs2(x-1,y);
+    dfs2(x,y-1);
+  }
+  return;
+}
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  ll res=0,res1=INF,res2=-INF,buf=0;
+  bool judge = true;
+  cin>>n>>m;
+  rep(i,0,n)cin>>a[i];
+  dfs1(0,0);dfs2(n-1,m-1);
+  //test;
+  //debug(t1,n*m);
+  if(t1[n*m-1]==0){
+    cout<<0<<endl;
+  }
+  else{
+    rep(i,1,n+m-2){
+      ll cnt=0;
+      ll p=0;
+      chmax(p,i-m+1);
+      rep(j,p,min(n,i+1)){
+        //if(i-j>=m||j>=n)continue;
+        //cout<<j spa i-j<<endl;
+        ll k=j*m+(i-j);
+        if(t2[k]==1&&t1[k]==1)cnt++;
+      }
+      if(cnt==1){
+        //cout<<i<<endl;
+        judge=false;
+      }
+      //cout<<i spa cnt<<endl;
+    }
+    ans(judge,2,1);
+  }
+  return 0;
+}
