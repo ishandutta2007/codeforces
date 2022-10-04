6177163dae296b9d57693f175c41162da9a14de6
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 13:43:16 +0530

    tute7627

---
+++ tute7627/normal/1042/E.cpp
@@ -0,0 +1,186 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define lfs cout<<fixed<<setprecision(10)
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = (n); i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = (m) - 1; i >= (ll)(n); i--)
+using ll = long long;
+using ld = long double;
+//const ll MOD = 1e9+7;
+const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>&v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>&v,ll n){if(n!=0)cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+vector<ll>dx={1,0,-1,0,1,1,-1,-1};
+vector<ll>dy={0,1,0,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+ostream &operator<<(ostream &os, pair<ll, ll>&p){
+  return os << p.first << " " << p.second;
+}  
+template< int mod >
+struct ModInt {
+  int x;
+
+  ModInt() : x(0) {}
+
+  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+  ModInt &operator+=(const ModInt &p) {
+    if((x += p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator-=(const ModInt &p) {
+    if((x += mod - p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator*=(const ModInt &p) {
+    x = (int) (1LL * x * p.x % mod);
+    return *this;
+  }
+
+  ModInt &operator/=(const ModInt &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+
+  ModInt operator-() const { return ModInt(-x); }
+
+  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+  bool operator==(const ModInt &p) const { return x == p.x; }
+
+  bool operator!=(const ModInt &p) const { return x != p.x; }
+
+  ModInt inverse() const {
+    int a = x, b = mod, u = 1, v = 0, t;
+    while(b > 0) {
+      t = a / b;
+      swap(a -= t * b, b);
+      swap(u -= t * v, v);
+    }
+    return ModInt(u);
+  }
+
+  ModInt pow(int64_t n) const {
+    ModInt ret(1), mul(x);
+    while(n > 0) {
+      if(n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  friend ostream &operator<<(ostream &os, const ModInt &p) {
+    return os << p.x;
+  }
+
+  friend istream &operator>>(istream &is, ModInt &a) {
+    int64_t t;
+    is >> t;
+    a = ModInt< mod >(t);
+    return (is);
+  }
+
+  static int get_mod() { return mod; }
+};
+using modint = ModInt< MOD >;
+modint pow2(int64_t n, int64_t x){
+    return modint(n).pow(x);
+}
+int main(){
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  ll res=0,buf=0;
+  bool judge = true;
+  ll n,m;cin>>n>>m;
+  auto a=vec(n,m,0LL);
+  rep(i,0,n)rep(j,0,m)cin>>a[i][j];
+  ll r,c;cin>>r>>c;r--;c--;
+  vector<P>order;
+  rep(i,0,n)rep(j,0,m)order.EB(i,j);
+  sort(ALL(order),[&](P x,P y){
+    return a[x.fi][x.se]<a[y.fi][y.se];
+  });
+  auto dp=vec(n,m,(modint)0);
+  modint sumx=0,sumx2=0,sumy=0,sumy2=0,cum=0;
+  ll idx=0;
+  while(1){
+    if(idx>=n*m)break;
+    ll preidx=idx,now=a[order[idx].fi][order[idx].se];
+    while(1){
+      if(idx>=n*m)break;
+      ll x=order[idx].fi,y=order[idx].se;
+      //cout<<x spa y spa preidx<<endl;
+      if(a[x][y]>now)break;
+      idx++;
+      if(preidx==0)continue;
+      dp[x][y]+=(modint)x*x*preidx-sumx*2*x+sumx2;
+      dp[x][y]+=(modint)y*y*preidx-sumy*2*y+sumy2;
+      //cout<<x spa y spa (modint)x*x*preidx-sumx*2*x+sumx2
+      // spa (modint)y*y*preidx-sumy*2*y+sumy2 spa cum<<endl;
+      dp[x][y]+=cum;
+      dp[x][y]/=preidx;
+    }
+    //cout<<preidx spa idx<<endl;
+    rep(i,preidx,idx){
+      ll x=order[i].fi,y=order[i].se;
+      sumx+=x;sumx2+=x*x;
+      sumy+=y;sumy2+=y*y;
+      cum+=dp[x][y];
+    }
+  }
+  //debug(dp,n,m);
+  //rep(i,0,n*m)cout<<order[i]<<endl;
+  //debug(a,n,m);
+  cout<<dp[r][c]<<endl;
+  return 0;
+}
