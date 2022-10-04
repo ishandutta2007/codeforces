8a9468e783e6ae186c6ecbd72c33dc4d02b8faa7
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 16:14:52 +0530

    fedoseev.timofey

---
+++ fedoseev.timofey/normal/979/A.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <ctime>
+#include <random>
+#include <cmath>
+#include <bitset>
+#include <iomanip>
+#include <list>
+
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+    ll n;
+    cin >> n;
+    ++n;
+    if (n == 1) {
+        cout << 0 << '\n';
+        return 0;
+    }
+    if (n % 2 == 1) {
+        cout << n << '\n';
+    }
+    else {
+        cout << n / 2 << '\n';
+    }
+}
