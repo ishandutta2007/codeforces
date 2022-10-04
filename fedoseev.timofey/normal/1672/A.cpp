782296413dc658aa26ab94295141a105a8d768d7
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:29:22 +0530

    fedoseev.timofey

---
+++ fedoseev.timofey/normal/1672/A.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <cstring>
+#include <cmath>
+#include <cstdlib>
+#include <algorithm>
+#include <random>
+#include <iomanip>
+#include <functional>
+#include <cassert>
+#include <bitset>
+#include <chrono>
+ 
+using namespace std;
+ 
+typedef long long ll;
+ 
+int main() {
+  ios_base::sync_with_stdio(false); cin.tie(0);
+#ifdef LOCAL
+  freopen("input.txt", "r", stdin);
+#endif
+  int t;
+  cin >> t;
+  while (t--) {
+    int n;
+    cin >> n;
+    int sum = 0;
+    for (int i = 0; i < n; ++i) {
+      int x;
+      cin >> x;
+      sum += x - 1;
+    }
+    cout << (sum % 2 == 1 ? "errorgorn" : "maomao90") << '\n';
+  }
+}
