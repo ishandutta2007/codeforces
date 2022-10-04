0ad73b2db50a6531158f6ebe1d5f44b5d90ca877
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:09:57 +0530

    tute7627

---
+++ tute7627/normal/1221/D.cpp
@@ -0,0 +1,85 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define endl '\n' 
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define lfs <<fixed<<setprecision(10)<<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define PB push_back
+#define EB emplace_back
+#define rep(i,n,m) for(ll i = n; i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = n - 1; i >= (ll)(m); i--)
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>v,ll n){cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+template<typename T>
+void emp(map<T,ll>&m, T x){m.emplace(x,0).first->second++;}
+vector<ll>dx={1,0,-1,0,1,1,-1,-1};
+vector<ll>dy={0,1,0,-1,1,-1,1,-1};
+template<typename T>
+vector<T> make_v(size_t a,T b){return vector<T>(a,b);}
+template<typename... Ts>
+auto make_v(size_t a,Ts... ts){
+  return vector<decltype(make_v(ts...))>(a,make_v(ts...));
+}
+ 
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  ll res=0,res1=INF,res2=-INF,buf=0;
+  bool judge = true;
+  ll q;cin>>q;
+  while(q--){
+    ll n;cin>>n;
+    vector<ll>a(n+1),b(n);
+    a[0]=-100;
+    rep(i,0,n)cin>>a[i+1]>>b[i];
+    auto dp=vec(n+1,3,2*INF);
+    dp[0][0]=0;
+    rep(i,0,n){
+      rep(j,0,3){
+        rep(o,0,3){
+          if(a[i]+j!=a[i+1]+o){
+            chmin(dp[i+1][o],dp[i][j]+b[i]*o);
+          }
+        }
+      }
+    }
+    cout<<*min_element(ALL(dp[n]))<<endl;
+  }
+  return 0;
+}
