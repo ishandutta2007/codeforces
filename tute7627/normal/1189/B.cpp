1c363c7c5c6209d7013b7a1d2ee376e8eeafd51f
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:26:19 +0530

    tute7627

---
+++ tute7627/normal/1189/B.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ld long double
+#define ll long long
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define MP make_pair
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e18;
+const ll zero=0;
+using P = pair<ll, ll>;
+void chmin(ll &a,ll b){if(a>b)a=b;}
+void chmax(ll &a,ll b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+void ans(bool x,ll y,ll z){if(x)cout<<y<<endl;else cout<<z<<endl;}
+void ans(bool x,string y,string z){if(x)cout<<y<<endl;else cout<<z<<endl;}   
+ll gcd(ll x,ll y){ll r;while((r=x%y)!=0){x=y;y=r;}return y;}
+
+int main(){
+  ll i,j,o;
+  ll res=0,res1=INF,res2=-INF,buf=0,buf1=0,buf2=0,buf3=0,buf4=0,sum=0;
+  bool judge = true;
+  ll n;cin>>n;
+  vector<ll>a(n);
+  for(i=0;i<n;i++)cin>>a[i];
+  sort(ALLR(a));
+  if(a[0]>=a[1]+a[2])cout<<"NO"<<endl;
+  else{
+    cout<<"YES"<<endl;
+    reverse(ALL(a));
+    for(i=0;i<n-3;i++){
+      cout<<a[i]<<" ";
+    }
+    cout<<a[n-3] spa a[n-1] spa a[n-2]<<endl;
+  }
+
+  return 0;
+}
