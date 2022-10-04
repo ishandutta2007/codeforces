7f4e141fac9558b1c3a72beda12b80eb0f009a14
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:30:08 +0530

    cookiedoth

---
+++ cookiedoth/normal/1599/C.cpp
@@ -0,0 +1,153 @@
+/*
++++++++++++++///////////////////////////////////////////////////::::::::::::::::::::::::::::::::::::
+++++++++++++//+++//+sydmNNNNNNNNmNNNNNNNNNmNNNNNNNNNmmmmmmNNNNNmhs/:::::::::::::::::::::::::::::::::
+++++++++++++++++shmNMMMMMMMMMMNNMMMMMMMMNNNNNNNNNMMMMMMMNNNNNMNNNNmddhs/::::::::::::::::::::::::::::
+++++oooo+++++shNMMMMMMMMMMMMMNNMMMMMMMNMMMMMMMMMMMMMMMMMMMMNNMMMMNNNMMMm/:::::::::::::::::::::::::::
+sdmNNNNNNmdhmNMMMMMMMMMNNNNNNmNNNNNNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNMMm/+/::::::::::::::::::::::::
+yNNNNNNMMMMMMNNMMMMMMNNMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNNmNmds/:::::::::::::::::::::
+smmmNNNMMMMMMMMmNNNMNMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMMMh-::::::::::::::::::::
+yNMMMMMMMMMMNMNmMMMNNNMMMNNMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNMM/-:::::::::::::::::::
+yMMMMMMMMMMMMNmMMMMMMmmmmNMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNNy::::::::::::::::::::
+sNMMMMMMMMMMMNMMMMMMMMNNNMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMMNNh/:::::::::::::::::
+sdmMMMMMMMMNNMMMMMMMMMMNMMMMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMN/-:::::::::::::::
+smmmMMMMMMMNMMMMMMMMMMMMNMMMNmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNMy.:::::::::::::::
+smmmmMMMMMMNMMMMMMMMMMMMNMMNdMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNd.-::::::::::::::
+ymdmmNMMMNNNNMMMMMMMMMMMNNmdNMMMMMMMMMMMMNMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMNMMMMMMMMMMNy:::::::::::::::
+yMmdmmmNMMMMNMMMMMMMMMMMNdmdMMMMMMMMMMMMNMMMMMMmMMMMMMMMMMMMNMMMMMMMMMMMNMMMMMMMMMMMMd.:::::::::::::
+yNdmmNMMMMMMMMMMMMMMMMNNmddNMMMMMMMMMMMMNMMMMMNNMMMMMMMMMMMMNMMMMMMMMMMMNMMMMMMMNMMMMs.-::::::::::::
+smNMMMMMMMMMMMMMMNNmmmmmhddMMMMMMMMMMMMMNMMMMMNNNMMMMMMMMMMMNMMMMMMMMMMMNMMMMMMMNMMMMs.:::::::::::::
+yMMMMMMMMMMMMMNmmmmmmmmdddmMMMMMMMMMMMMMNMMMMNMNNMMMMMMMMMMMNMMMMMMMMMMMNMMMMMMMNNMMMm--::::::::::::
+yMMMMMMMMMMMNNmmmmmmmmdhdhNMMMMMMMMMMMMMNMMMMMMMNNMMMMMMMNNNmmmmNMMMMMMMNMMMMMMMNMMNMM/.::::::::::::
+yMMMMMMMMMNNNmmmmmmmmhhhhhNMMMMMMMMMMMMMNMMMNMMMNNMMMMMMMMMNNNMMMMMMMMMMNMMMMMMMNMMNMMs.::::::::::::
+sNMMMMNNMMMMNmdmmmmdhhhhhhMMMMMMMMMMMMMMNMMNMMMMNNMMMMMMMMMNMNMMMMMMMMMMmMMMMNNMNMMmMMh.-:::::::::::
+smmNNMMMNNmmmmmmmmmhhhhhhdMMMMMMMMMMMMMMNMMNMMMMMNNMMMMMMMMNMNMMMMMMMMMNNMMMMMmNNMMmMMd.-:::::::::::
+sNNMMNNmmmmmmmmmmmhhhhhhhmMMMMMMMMMMMMMMNNmmNNNNNmNMMMMMMMMNMNMMMMMMMMMNNMMMMMMNmNMmMMd.-:::::::::::
+yMMNNmmmmmmmmmmmmhhhhhhhhmMMMMMMMMMMMMMMNmhyso////+oyymNMMMNMNMMMMMMMMMNNMMMMMMNMNNmMMm.-:::::::::::
+yNNmmmmmmmmdNNNmdhhhhhhhhNMMMMMMMMMMMMMMmNmMNsy///::/:/smNNNNNMMMMMMMMNNNMMMMMMNMMMmMMm.-::::::::::-
+smmmmmmmmmmdNMMMMNNmdhhhyNMMMMMMMMMMNMMMNNmMs/o+/++/omy//omNNMNMMMMMMMNNMMMMMMMNMMNNMMm.-:::--::::::
+ooyddmddddhhdMMNMMMMMMNmyNMMMMMMMMMMNMMMNNdMosyhyyso+MMNh+yNMMNMMMMMMMNMMMMMMMNMMMmMMMd.-:::::::::::
+::+-:::/hhhhhNMNNNNMMMMMNNMMMMMMMMMMMNMMMmNNdddddmmhyMMMMMmMMMMMMMMMMMMMNNNMMNNMMNmMMMy.-:::::-:::::
+:y//--:/+hhhhhNMNNNNNNNMMNMMMMMMMMMMMNMMMNMNMmNMMmdmMMMMMMMMMMMMMMMMMMMmy++osdmNMdNMMM+.::::::::::::
+oN:o-:/y/+hhhhhMMMMMNNNmMNMMMMMMMMMMMNMMMNNMMMNMNNMMMMMMMMMMMMMMMMMMMMy+/:mNy+shdmMMMN-.::::::::::::
+yd:s/:+Nh/:yhhhhNMMMNNNmNNMMMMMMMMMMMNMMMMmMMMMMMMMMMMMMMMMMMMMMMMMMMho+++MMMmhhhNMMMm-.-:::::::::::
+ys+s/:oMMmo:ohhhhNMMNNNNmNMMMMMMMMMMMNNMMMmMMMMMMMMMMMMMMMMMMMMMMMMMMdhhydMMNddhNMMMMmo.::::::::::::
+o/+h/:sMMMNy:+hhhhdNMNNNNNMMMMMMMMMMMMNMMMmMMMMMMMMMMMMMMMMMMMMMMMMMMdmmmMNmhhhdMMMMNmm.-:::::::::::
+//od//yMMMMMd:/yhhhhdNMMMNNMMMMMMMMMMMNMMMNMMMMMMMMMMMMMMMMMMMMMMMMMMNNNMNNhhhhmMMMMmMM+.:::::::::::
+//yh:/+mMMMMMy/:shhhhhdmNNmMMMMMMMMMMMNMMMmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMdhhhhmMMMMmMMh.-::::::::::
+//mh-//oNMMMNhy//yhhhyhhhhdMMMMMMMMMMMMNMMNNMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNhhhhhmMMMNhMMN:-----::::::
+/yMs:h//yMMMdhhs/+hhyhhhhhyNMMMMMMMMMMMNMMNMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMmhhhhhmMMMm-yNMh.----:::::-
+oNN/+My//dMNhhho//yhyhhhhhhNMMMMMMMMMMMMMMmMMMMMMMMMMMMMMMMMMMMMMMMMMNMMMdhhhhydMMMh`-+mN+.---::::--
+yMh/hMNo/+ddhhy//+hyhhhhhhhdMMMMMMMMMMMMMMmMMMMMMMMMMMMMMMMMMMMMMMMMMMMMNhhhhh+yMMMs`--/hm:-::::::--
+yN+/NMMN+//+++//+yyhhhhhhhhhMMMMMMMMMMMMMMmMMMMMMMMMMMNMMMMMMMMMMMMMMMMNhhhhhh-oMMMs`-:::yh---------
+sy/sMMMMms+//++syyyhhhhhhhhyNMMMMMMMMMMMMMNMMMMMMMMMMMMNNNMMMNNMMMMMMMmhhhhhhy.+MMMo.---::ss.-------
+o/+NNMMMMdhhhhhhhyhhhhhhhhhhmMMMMMMMMMMMMMNMMMMMMMMMMMMMMMMMMMMMMMMMNdhhhhhhhs./MMM+.:----:s/-------
+/+mMNMMMmhhhhhhhyhhhmmmmdhymmMMMMMMMMMMMMMmMMMMMMMMMMMMMMMMMMMMMMMNyhhhhhhhhhs.:NMN-.::----:s-------
+/dMMNMMNhhhhhhhyhhho+oosydmNmNMMMMMMMMMMMMmNNMMMMMMMMMMMMMMMMMMNmNdyhhhhhhhhhs.-NMh.-::-----:+.-----
+sMMNNMNdhhhhhhyhhhhmMMMMmho+ydMMMMMMMMMMMMmNNdddmNMMMMMMMMMMNmhhdMhyhhhhhhhhhy.-mMo.-:------:o------
+yMMNMMdhhhhhhyhhhhhdMMMMMMMmo+mMMMMMMMMMMMmNddhhhhhddmmNNNmdhhhhdNhyyhhhhhhhhy.-NN-.-::------++.----
+yMNNMmhhhhhhyhhhhdmdMMMMMMMMNshMMMMMMMMMMMmdmmmdhhhhhhhhhhhhhhhhhNhhyhhhhhhhhh/:My`----------/s.---:
+yMNMmhhhhhhyyhhhmNmmNMMMMMMMMMNMMMMMMMMMMMmmmmmmdhhhhhhhhhhhyhhhhmhhhyhhhhhhhhssN:.----------/y..---
+yNMmhhhhhhhyhhhmMMNmmNMMMMMMMMNMMMMMMMMMMMmmmmmmmhhhhhhhhhhhhhhhhhdhhyyhhhhhhhydy.------::---+h-.---
+sNNhhhhhhhyhhdmNMMMMmmNMMMMMMMNMmMMMMMMMMMmmmmmmmdmddhhhhhhhhhhhhhhyhhyhhhhhhhhms.----::-::--oy..---
+sNhhhhhhhyhNNNNMMMMMMNmNMMMMMMNMNNMMMMMMMMmyyyhhmmmmmhdhhhhhhhhhhhhyyhhyhhhhhhhNm.----------:s+`-:--
+ohhhhhhhhmNMMMNNNMMMMMNmNMMMMMNNMNNMMMMMMMmNMMMmshmmhmNNNNmmmddhhhhhyyhhyhhhhhdNMs.---------:s-.----
+ohhhhdmNMNNNMMMMNNMMMMMNmMMMMMMNNMNNMMMMMMmMMMMMhymymMMNMMMMMMMNmdhhhyyhyyhhhhmNMN:---------/+.-----
+ohdNMMMMMMMMMMMMMMNMMMMMNmMMMMMNNMMNNMMMMMmMMMMNsmydmNMNNMMMMMMMMMNhhhyyyyyhhhNNMMh.--------+-.-----
+omMMMMMMMMMMMMMMMMNNMMMMMNNMMMMMNNMMNNMMMMNNMMMsdyNNdmNMNNMMMMMMMMMNhhhhyyyhhhMNMMMo.------::-------
+yMMMMMMMMMMMMMMMMMMMNMMMMMNMMMMMNNNMNmNMMMMmMNNshhMMNdmNMMNMMMMMMMMMNhhhhyhhhmMNMMMN/-----+y--------
+yMMMMMMMMMMMMMMMMMMMMNMMNNMNMMMMMNNMMMNNMMMNmmmyddNMMMmmMMMNMMMMMMMMMhhhhhyhhmMNMMMMN/---oNs`-------
+yMMMMMMMMMMMMMMMMMMNMMNNMmNNNMMMMMNNMMMNNMMNdmmmmmdMMMMmmMMMNMMMMMMMMmhhhhyhhNMdmNMMMN/-:NM+.-------
+yMMMMMMMMMMMMMMMMMMMNNMNNNmNmMMMMMMNNMMMNMMNNNNmmmdNMMMMmmMMNNMMMMMMMMyhhhyyhMMhhhmNMMNs/NM/.-------
+yMMMMMMMMMMMMMMMMMMMMMNMNNmmmMMMMMMMNNNMMNMNNMMmdhhNMMMMMNNMMNNMMMMMMMhhhhyyhMNhhhhhdNMMmNM+.-------
+yMMMMMMMMMMMMMMMMMMMMMMNMNmmmMMMMMMMMMNNNNMNMMMMMNmMMMMMMMNNNNNMMMMMMMhhhyyhhMdhhhhhhhhdNNMh.-------
+yMMMMMMMMMMMMMMMMMMMMMMMMMhmNMMMMMMMMMMMMNNmMMMMMmmNMMMMMMNMNNNNMMMMMMhhyhhhhNhhhhhhhhhhhhdmy/------
+yMMMMMMMMMMMMMMMMMMMMMMMMNddNMMMMMMMMMMMMMmNMMMMMNhdMMMMMMoyNNmmMMMMMMhhhhhhhdhhhhhhhhhhhhhhhhs/----
+yMMMMMMMMMMMMMMMMMMMMMMMMMmNNNMMMMMMMmhNMmNMMMMMMMsoyNMMMMs/+hddMMMMMNddhhhhhhhhhhhhhhhhhhhhhhhhy:--
+sMMMMMMMMMMMMMMMMMMMMMMMMMmMMmMMMMMMN+/+hNMMMMMMMh///odMMMyoo/osmMMMMNmNdhhyhhhhhhhhhhhhhhhhhhhhhs--
++NMMMMMMMMMMMMMMMMMMMMMMMMNMMmMMMMMNo////odMMMMMh//ss//smMhsmmddmNNNNNmMNhhhhhhhhhhhhhhhhhhhhhhhhs--
+ohNMMMMMMMMMMMMMMMMMMMMMMNMMMdmMMMNo//////+mMMMm+//hdh+/+NNNmNMMNNNNNNmNNdhhhhhhhhhhhhhhhhhhhhhhhs--
+ohhNMMMMMMMMMMMMMMMMMMMMMNMMMdmNMMm+//////hMNNNMmo+dhydsyMMMMmmNMMMNNNNMMNmdhhhhhhhhhhhhhhhhhhhhhs--
+ohhdMMMMMMMMMMMMMMMMMMMMMNMMMNmmNNMNs////yMMMMMMMMddhsyNmMMMMNNNNNNMMMMNNNMMMNmdhhhhhhhhhhhhhhhhhs--
+ohhdmMMMMMMMMMMMMMMMMMMMNNMNMMmmmMMMMdo/oMMMMMMMMMMmdsomNMMMMMNNMMMNNNMMMMNNNMMMNhhhhhhhhhhhhhhhhs--
+ohhdNmMMMMMMMMMMMMMMMMMMNMMmMMNhMMMMMMMmNMMMMMMMMMMMmdhdNMMMMMMMMNNNMMNNMMMMMMMMMMdhhhhhhhhhhhhhhs--
+ohhhMmmMMMMMMMMMMMMMMMMNMMNmMMMmMMMMMMMMMMMMMMMMMMMMMNddmMMMMMMMMMMNmMMMNNMMMMMMMMMmhhhhhhhhhhhhhs--
+ohhhdmdNNMMMMMMMMMMMMMNNMMmmMMMNMMMMMMMMMMMMMMMMMMMMMMMmNMMMMMMMMMMMNmNMMMMMMMMMMMMMmhhhhhhhhhhhhs--
+*/                                                                                                                                                                                                                                                                                              
+#include <iostream>
+#include <complex>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <cstdio>
+#include <numeric>
+#include <cstring>
+#include <ctime>
+#include <cstdlib>
+#include <set>
+#include <map>
+#include <unordered_map>
+#include <unordered_set>
+#include <list>
+#include <cmath>
+#include <bitset>
+#include <cassert>
+#include <queue>
+#include <stack>
+#include <deque>
+#include <random>
+ 
+using namespace std;
+template<typename T1, typename T2> inline void chkmin(T1 &a, T2 b) {if (a > b) a = b;}
+template<typename T1, typename T2> inline void chkmax(T1 &a, T2 b) {if (a < b) a = b;}
+#define files(FILENAME) read(FILENAME); write(FILENAME)
+#define read(FILENAME) freopen((FILENAME + ".in").c_str(), "r", stdin)
+#define write(FILENAME) freopen((FILENAME + ".out").c_str(), "w", stdout)
+#define all(c) (c).begin(), (c).end()
+#define sz(c) (int)(c).size()
+#define left left228
+#define right right228
+#define y1 y1228
+#define mp make_pair
+#define pb push_back
+#define y2 y2228
+#define rank rank228
+using ll = long long;
+using ld = long double; 
+const string FILENAME = "input";
+
+
+
+
+
+int n;
+double p;
+
+
+void solve() {
+    cin >> n >> p;
+    for (int k = 0; k <= n; k++) {
+        double p1 = double(k * (k - 1) * (k - 2) / 6) / double(n * (n - 1) * (n - 2) / 6);
+        double p2 = double(k * (k - 1) / 2 * (n - k)) / double(n * (n - 1) * (n - 2) / 6);
+        double p3 = double(k * (n - k) * (n - k - 1) / 2) / double(n * (n - 1) * (n - 2) / 6);
+        double p4 = double((n - k) * (n - k - 1) * (n - k - 2) / 2) / double(n * (n - 1) * (n - 2) / 6);
+        double q = p1 + p2 + p3 * 0.5;
+        if (q >= p - 1e-9) {
+            cout << k << '\n';
+            return;
+        }
+    }   
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    //read(FILENAME);
+    int t = 1;
+    //cin >> t;
+    while (t--) {
+        solve();
+    }
+    return 0;
+}
