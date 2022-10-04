4af299114c0b3f5064578c6f80c52177d73b57d6
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 09:06:59 +0530

    a.k.e.e

---
+++ a.k.e.e/normal/1375/C.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define x first
+#define y second
+typedef pair<int,int> pii;
+typedef long long ll;
+typedef unsigned long long ull;
+template <typename T> void chkmax(T &x,T y){x<y?x=y:T();}
+template <typename T> void chkmin(T &x,T y){x>y?x=y:T();}
+template <typename T> void readint(T &x)
+{
+	x=0;int f=1;char c;
+	for(c=getchar();!isdigit(c);c=getchar())if(c=='-')f=-1;
+	for(;isdigit(c);c=getchar())x=x*10+c-'0';
+	x*=f;
+}
+const int MAXN=300005;
+
+int n,a[MAXN];
+void solve()
+{
+	readint(n);
+	for(int i=1;i<=n;++i)readint(a[i]);
+	printf(a[1]<a[n]?"YES\n":"NO\n");
+}
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+	freopen("code.in","r",stdin);
+//	freopen("code.out","w",stdout);
+	#endif
+	int T;
+	readint(T);
+	while(T--)solve();
+	return 0;
+}
