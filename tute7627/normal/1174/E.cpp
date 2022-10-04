7cb1e2c06d98269aaf5003a0383a968aba2601a9
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 14:17:13 +0530

    tute7627

---
+++ tute7627/normal/1174/E.cpp
@@ -0,0 +1,221 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ALL(a)  (a).begin(),(a).end()
+#define ALLR(a)  (a).rbegin(),(a).rend()
+#define spa << " " <<
+#define lfs <<fixed<<setprecision(10)<<
+#define test cout<<"test"<<endl;
+#define fi first
+#define se second
+#define MP make_pair
+#define rep(i,n,m) for(ll i = n; i < (ll)(m); i++)
+#define rrep(i,n,m) for(ll i = n - 1; i >= (ll)(m); i--)
+typedef long long ll;
+typedef long double ld;
+const ll MOD = 1e9+7;
+//const ll MOD = 998244353;
+const ll INF = 1e18;
+using P = pair<ll, ll>;
+template<typename T>
+void chmin(T &a,T b){if(a>b)a=b;}
+template<typename T>
+void chmax(T &a,T b){if(a<b)a=b;}
+void pmod(ll &a,ll b){a=(a+b)%MOD;}
+void pmod(ll &a,ll b,ll c){a=(b+c)%MOD;}
+void qmod(ll &a,ll b){a=(a*b)%MOD;}
+void qmod(ll &a,ll b,ll c){a=(b*c)%MOD;}
+ll median(ll a,ll b, ll c){return a+b+c-max({a,b,c})-min({a,b,c});}
+void ans1(bool x){if(x) cout<<"Yes"<<endl;else cout<<"No"<<endl;}
+void ans2(bool x){if(x) cout<<"YES"<<endl;else cout<<"NO"<<endl;}
+void ans3(bool x){if(x) cout<<"Yay!"<<endl;else cout<<":("<<endl;}
+template<typename T1,typename T2>
+void ans(bool x,T1 y,T2 z){if(x)cout<<y<<endl;else cout<<z<<endl;}  
+template<typename T>
+void debug(vector<vector<T>>v,ll h,ll w){for(ll i=0;i<h;i++)
+{cout<<v[i][0];for(ll j=1;j<w;j++)cout spa v[i][j];cout<<endl;}};
+void debug(vector<string>v,ll h,ll w){for(ll i=0;i<h;i++)
+{for(ll j=0;j<w;j++)cout<<v[i][j];cout<<endl;}};
+template<typename T>
+void debug(vector<T>v,ll n){cout<<v[0];
+for(ll i=1;i<n;i++)cout spa v[i];cout<<endl;};
+template<typename T>
+vector<vector<T>>vec(ll x, ll y, T w){
+  vector<vector<T>>v(x,vector<T>(y,w));return v;}
+ll gcd(ll x,ll y){ll r;while(y!=0&&(r=x%y)!=0){x=y;y=r;}return y==0?x:y;}
+template<typename T>
+void emp(map<T,ll>&m, T x){m.emplace(x,0).first->second++;}
+
+template< int mod >
+struct ModInt {
+  int x;
+
+  ModInt() : x(0) {}
+
+  ModInt(int64_t y) : x(y >= 0 ? y % mod : (mod - (-y) % mod) % mod) {}
+
+  ModInt &operator+=(const ModInt &p) {
+    if((x += p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator-=(const ModInt &p) {
+    if((x += mod - p.x) >= mod) x -= mod;
+    return *this;
+  }
+
+  ModInt &operator*=(const ModInt &p) {
+    x = (int) (1LL * x * p.x % mod);
+    return *this;
+  }
+
+  ModInt &operator/=(const ModInt &p) {
+    *this *= p.inverse();
+    return *this;
+  }
+
+  ModInt operator-() const { return ModInt(-x); }
+
+  ModInt operator+(const ModInt &p) const { return ModInt(*this) += p; }
+
+  ModInt operator-(const ModInt &p) const { return ModInt(*this) -= p; }
+
+  ModInt operator*(const ModInt &p) const { return ModInt(*this) *= p; }
+
+  ModInt operator/(const ModInt &p) const { return ModInt(*this) /= p; }
+
+  bool operator==(const ModInt &p) const { return x == p.x; }
+
+  bool operator!=(const ModInt &p) const { return x != p.x; }
+
+  ModInt inverse() const {
+    int a = x, b = mod, u = 1, v = 0, t;
+    while(b > 0) {
+      t = a / b;
+      swap(a -= t * b, b);
+      swap(u -= t * v, v);
+    }
+    return ModInt(u);
+  }
+
+  ModInt pow(int64_t n) const {
+    ModInt ret(1), mul(x);
+    while(n > 0) {
+      if(n & 1) ret *= mul;
+      mul *= mul;
+      n >>= 1;
+    }
+    return ret;
+  }
+
+  friend ostream &operator<<(ostream &os, const ModInt &p) {
+    return os << p.x;
+  }
+
+  friend istream &operator>>(istream &is, ModInt &a) {
+    int64_t t;
+    is >> t;
+    a = ModInt< mod >(t);
+    return (is);
+  }
+
+  static int get_mod() { return mod; }
+};
+using modint = ModInt< MOD >;
+
+template< typename T >
+struct Combination {
+  vector< T > _fact, _rfact, _inv;
+
+  Combination(ll sz) : _fact(sz + 1), _rfact(sz + 1), _inv(sz + 1) {
+    _fact[0] = _rfact[sz] = _inv[0] = 1;
+    for(ll i = 1; i <= sz; i++) _fact[i] = _fact[i - 1] * i;
+    _rfact[sz] /= _fact[sz];
+    for(ll i = sz - 1; i >= 0; i--) _rfact[i] = _rfact[i + 1] * (i + 1);
+    for(ll i = 1; i <= sz; i++) _inv[i] = _rfact[i] * _fact[i - 1];
+  }
+
+  inline T fact(ll k) const { return _fact[k]; }
+
+  inline T rfact(ll k) const { return _rfact[k]; }
+
+  inline T inv(ll k) const { return _inv[k]; }
+
+  T P(ll n, ll r) const {
+    if(r < 0 || n < r) return 0;
+    return fact(n) * rfact(n - r);
+  }
+
+  T C(ll p, ll q) const {
+    if(q < 0 || p < q) return 0;
+    return fact(p) * rfact(q) * rfact(p - q);
+  }
+
+  T H(ll n, ll r) const {
+    if(n < 0 || r < 0) return (0);
+    return r == 0 ? 1 : C(n + r - 1, r);
+  }
+};
+using Comb=Combination<modint>;
+
+
+
+int main(){
+  cin.tie(NULL);
+  ios_base::sync_with_stdio(false);
+  ll res=0,res1=INF,res2=-INF,buf=0;
+  bool judge = true;
+  ll n;cin>>n;
+  vector<ll>a;
+  rep(i,1,n+1){
+    ll cnt=0;
+    buf=i;
+    while(buf%2==0){
+      buf/=2;
+      cnt++;
+    }
+    if(a.size()<=cnt)a.push_back(1);
+    else a[cnt]++;
+  }
+  ll m=a.size();
+  modint ret=0;
+  modint tmp=1;
+  ll sb=0;
+  Comb comb(1000005);
+  rep(i,0,m){
+    modint x=comb.P(sb+a[i],a[i]);
+    x-=comb.P(sb+a[i]-1,a[i]);
+    tmp*=x;
+    sb+=a[i];
+  }
+  ret+=tmp;
+  if(a[m-2]==2){
+    rep(i,1,m){
+      vector<ll>b(m);
+      rep(j,1,n+1){
+        ll cnt=0;
+        buf=j;
+        while(buf%2==0){
+          buf/=2;
+          cnt++;
+        }
+        if(cnt>=i&&buf%3!=0){
+          cnt=i-1;
+        }
+        if(buf%3==0&&cnt>=i-1)cnt++;
+        b[cnt]++;
+      }
+      tmp=1;
+      sb=0;
+      rep(j,0,m){
+        modint x=comb.P(sb+b[j],b[j]);
+        x-=comb.P(sb+b[j]-1,b[j]);
+        tmp*=x;
+        sb+=b[j];
+      }
+      ret+=tmp;
+    }
+  }
+  cout<<ret<<endl;
+  return 0;
+}
