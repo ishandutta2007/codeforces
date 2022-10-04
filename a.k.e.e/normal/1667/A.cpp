82e410f93d8a913488ed716c8246e68bc45f5661
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 08:43:48 +0530

    a.k.e.e

---
+++ a.k.e.e/normal/1667/A.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+template <typename T> bool chkmin(T &x,T y){return y<x?x=y,1:0;}
+template <typename T> bool chkmax(T &x,T y){return x<y?x=y,1:0;}
+template <typename T> void readint(T &x)
+{
+	int f=1;char c;x=0;
+	for(c=getchar();!isdigit(c);c=getchar())if(c=='-')f=-1;
+	for(;isdigit(c);c=getchar())x=x*10+(c-'0');
+	x*=f;
+}
+const int MOD=998244353;
+inline int dmy(int x){return x>=MOD?x-MOD:x;}
+inline void inc(int &x,int y){x=dmy(x+y);}
+int qmi(int x,int y)
+{
+	int ans=1;
+	for(;y;y>>=1,x=1ll*x*x%MOD)
+		if(y&1)ans=1ll*ans*x%MOD;
+	return ans;
+}
+const int MAXN=5005;
+
+int n,a[MAXN];
+
+int main()
+{
+	#ifdef LOCAL
+	freopen("code.in","r",stdin);
+//	freopen("code.out","w",stdout);
+	#endif
+	readint(n);
+	for(int i=1;i<=n;++i)readint(a[i]);
+	ll res=1ll<<60;
+	for(int i=1;i<=n;++i)
+	{
+		ll sum=0,cur=0;
+		for(int j=i-1;j;--j)
+		{
+			cur=(cur/a[j]+1)*a[j];
+			sum+=cur/a[j];
+		}
+		cur=0;
+		for(int j=i+1;j<=n;++j)
+		{
+			cur=(cur/a[j]+1)*a[j];
+			sum+=cur/a[j];
+		}
+		chkmin(res,sum);
+	}
+	printf("%lld\n",res);
+	return 0;
+}
