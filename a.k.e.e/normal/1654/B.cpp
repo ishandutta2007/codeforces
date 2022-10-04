0b010147f6014c11357fec8410bf231b9589ef6b
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 08:45:47 +0530

    a.k.e.e

---
+++ a.k.e.e/normal/1654/B.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+template <typename T> bool chkmin(T &x,T y){return y<x?x=y,1:0;}
+template <typename T> bool chkmax(T &x,T y){return x<y?x=y,1:0;}
+template <typename T> void readint(T &x)
+{
+	int f=1;char c;x=0;
+	for(c=getchar();!isdigit(c);c=getchar())if(c=='-')f=-1;
+	for(;isdigit(c);c=getchar())x=x*10+(c-'0');
+	x*=f;
+}
+const int MOD=998244353;
+inline int dmy(int x){return x>=MOD?x-MOD:x;}
+inline void inc(int &x,int y){x=dmy(x+y);}
+int qmi(int x,int y)
+{
+	int ans=1;
+	for(;y;y>>=1,x=1ll*x*x%MOD)
+		if(y&1)ans=1ll*ans*x%MOD;
+	return ans;
+}
+const int MAXN=200005;
+
+int n;
+char a[MAXN];
+bool vis[26];
+void solve()
+{
+	scanf("%s",a+1);
+	n=strlen(a+1);
+	memset(vis,0,sizeof(vis));
+	int p=n;
+	for(int i=n;i;--i)
+	{
+		if(!vis[a[i]-'a'])chkmin(p,i);
+		vis[a[i]-'a']=1;
+	}
+	for(int i=p;i<=n;++i)putchar(a[i]);
+	putchar('\n');
+}
+
+int main()
+{
+	#ifdef LOCAL
+	freopen("code.in","r",stdin);
+	#endif 
+	int T;
+	readint(T);
+	while(T--)solve();
+	return 0;
+}
