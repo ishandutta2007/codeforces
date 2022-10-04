d8e60e2c9f096f0e9b813ca2f10fd1347c9e2683
ishan dutta <ishandutta2007@gmail.com>
2022-10-04 06:03:44 +0530

    pinkrabbit

---
+++ pinkrabbit/normal/958/E1.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#define F(i,a,b) for(int i=a;i<=(b);++i)
+#define F2(i,a,b) for(int i=a;i<(b);++i)
+#define dF(i,a,b) for(int i=a;i>=(b);--i)
+#define dF2(i,a,b) for(int i=a;i>(b);--i)
+#define eF(i,u) for(int i=h[u];i;i=nxt[i])
+#define ll long long
+#define ld double
+using namespace std;
+typedef pair<int,int> pii;
+inline int Gcd(int X,int Y){return Y?Gcd(Y,X%Y):X;}
+inline ll Gcd(ll X,ll Y){return Y?Gcd(Y,X%Y):X;}
+inline int Pow(int base,ll exp,int _mod){int _ans=1;for(;exp;exp>>=1,base=(ll)base*base%_mod)exp&1?_ans=(ll)_ans*base%_mod:0;return _ans;}
+inline ll Pow(ll base,ll exp,ll _mod){ll _ans=1;for(;exp;exp>>=1,base=base*base%_mod)exp&1?_ans=_ans*base%_mod:0;return _ans;}
+const int INF=0x3f3f3f3f;
+
+int R,B;
+
+int main(){
+	scanf("%d%d",&R,&B);
+	if(R!=B) printf("No");
+	else printf("Yes");
+	return 0;
+}
