07bc7eb147e865f89f547359dd67d8110d384197
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:22:01 +0530

    arvindf232

---
+++ arvindf232/normal/734/E.kt
@@ -0,0 +1,488 @@
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import java.util.*
+import kotlin.math.abs
+import java.util.*
+
+class DisjointUnionSets(n:Int) {
+	var rank:IntArray
+	var parent:IntArray
+	var n:Int
+
+	fun makeSet() {
+		for(i in 0 until n) {
+			parent[i] = i
+		}
+	}
+
+	fun find(x:Int):Int {
+		if(parent[x] != x) {
+			parent[x] = find(parent[x])
+		}
+		return parent[x]
+	}
+
+	fun union(x:Int, y:Int) {
+		val xRoot = find(x)
+		val yRoot = find(y)
+		if(xRoot == yRoot) return
+		if(rank[xRoot] < rank[yRoot])
+			parent[xRoot] = yRoot else if(rank[yRoot] < rank[xRoot])
+			parent[yRoot] = xRoot else {
+			parent[yRoot] = xRoot
+			rank[xRoot] = rank[xRoot] + 1
+		}
+	}
+
+	init {
+		rank = IntArray(n)
+		parent = IntArray(n)
+		this.n = n
+		makeSet()
+	}
+}
+
+class FastScanner {
+	private val BS = 1 shl 16
+	private val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+	private var `in`:BufferedInputStream? = null
+
+	constructor() {
+		`in` = BufferedInputStream(System.`in`, BS)
+	}
+
+	private val char:Char
+		private get() {
+			while(bId == size) {
+				size = try {
+					`in`!!.read(buf)
+				} catch(e:Exception) {
+					return NC
+				}
+				if(size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt():Int {
+		var neg = false
+		if(c == NC) c = char
+		while(c < '0' || c > '9') {
+			if(c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while(c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if(neg) -res else res
+	}
+
+	fun nextLong():Long {
+		var neg = false
+		if(c == NC) c = char
+		while(c < '0' || c > '9') {
+			if(c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while(c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if(neg) -res else res
+	}
+
+	fun nextString():String {
+		if(c == NC) c = char
+		val ret = StringBuilder()
+		while(true) {
+			c = char
+			if(!isWhitespace(c)) {
+				break
+			}
+		}
+		ret.appendCodePoint(c.toInt())
+		while(true) {
+			c = char
+			if(isWhitespace(c)) {
+				break
+			}
+			ret.appendCodePoint(c.toInt());
+		}
+		return ret.toString()
+	}
+
+	fun isWhitespace(c:Char):Boolean {
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+
+class reading {
+	companion object {
+		var jin = FastScanner()
+		var pw = PrintWriter(System.out)
+	}
+}
+
+fun put(aa:Any) {
+	reading.pw.println(aa)
+}
+
+fun done() {
+	reading.pw.close()
+}
+
+fun getint():Int {
+	return reading.jin.nextInt()
+}
+
+fun getlong():Long {
+	return reading.jin.nextLong()
+}
+
+fun getline(n:Int):List<Int> {
+	return (1..n).map { reading.jin.nextInt() }
+}
+
+fun getlineL(n:Int):List<Long> {
+	return (1..n).map { reading.jin.nextLong() }
+}
+
+fun getstr():String {
+	return reading.jin.nextString()
+}
+
+inline fun cases(ask:() -> Unit) {
+	val t = getint()
+	repeat(t) {
+		ask()
+	}
+}
+
+fun just(a:Boolean) {
+	put(if(a) "YES" else "NO")
+}
+
+fun List<Int>.out() {
+	put(this.joinToString(" "))
+}
+
+val List<Char>.ret:String
+	get() = this.joinToString("")
+
+infix fun Long.divCeil(b:Long):Long{
+	//Positives numbers only!
+	if(this == 0L) {
+		return 0
+	}
+	return (this-1)/b + 1
+}
+fun prefixSumL(arr:List<Long>):MutableList<Long>{
+	val ret = mutableListOf<Long>()
+
+	var previous = 0L
+	for(a in arr){
+		previous += a
+		ret.add(previous)
+	}
+	return ret
+}
+
+
+val powersOfTwo = intArrayOf(1,2,4,8,16,32,64,128,256,512,1024,2048,4096,8192,16384,32768,65536,131072,262144)
+
+class Graph(val n:Int,val directed:Boolean){
+	val E = Array<MutableList<Int>>(n+1) { mutableListOf() }
+//    val invE = Array<MutableList<Int>>(n+1) { mutableListOf() }
+
+
+	// Maintaining all the in/out edges
+	fun add(a:Int,b:Int){
+		E[a].add(b)
+//        invE[b].add(a)
+		if(!directed){
+			E[b].add(a)
+//            invE[a].add(b)
+		}
+	}
+	inline fun NS(from:Int,act:(Int)->Unit){
+		for(v in E[from]){
+			act(v)
+		}
+	}
+
+
+	// maintaining by adjaceny matrix
+//    val E = List(n+1){BooleanArray(n+1)}
+//    fun add(a:Int,b:Int){
+//        E[a][b] = true
+//        if(!directed){
+//            E[b][a] = true
+//        }
+//    }
+//    fun remove(a:Int,b:Int){
+//        E[a][b] = false
+//        if(!directed){
+//            E[b][a] = false
+//        }
+//    }
+//    inline fun NS(from:Int,act:(Int)->Unit){
+//        for(j in 0..n){
+//            if(E[from][j]){
+//                act(j)
+//            }
+//        }
+//    }
+
+
+	fun streamEdges(m:Int){
+		repeat(m){
+			val x = getint()
+			val y = getint()
+			add(x,y)
+		}
+	}
+	fun BFS(){
+		val root = 1
+		val toDo: Queue<Int> = LinkedList<Int>()
+		val explored = IntArray(n+1)
+		toDo.add(root)
+		explored[root] = 1
+
+		while(toDo.size > 0){
+			val x = toDo.remove()
+			NS(x){ a->
+				if(explored[a] == 0){
+					explored[a] = 1
+					toDo.add(a)
+				}
+			}
+		}
+	}
+	fun DFS(){
+		val root = 1
+		val toDo = ArrayDeque<Int>()
+		val explored = IntArray(n+1)
+		toDo.add(root)
+		explored[root] = 1
+
+		while(toDo.size > 0){
+			val x = toDo.removeLast()
+			NS(x){ a->
+				if(explored[a] == 0){
+					explored[a] = 1
+					toDo.addLast(a)
+				}
+			}
+		}
+	}
+	fun DFSrecur(){
+		val root = 1
+		val explored = IntArray(n+1)
+
+		fun go(i:Int,from:Int){
+			NS(i){v ->
+				if(v != from){
+					go(v,i)
+				}
+			}
+		}
+		go(1,0)
+	}
+	fun DFSrecurTrvael(){
+		val root = 0
+		val explored = IntArray(n+1)
+		fun travel(a:Int,b:Int){
+
+		}
+		fun go(i:Int,from:Int){
+			NS(i){v ->
+				if(v != from){
+					travel(i,v)
+					go(v,i)
+					travel(v,i)
+				}
+			}
+		}
+		go(root,0)
+	}
+	//With depth: add a depth parameter in recursion, or = from + 1
+	//With Sizes: Return sizes and accumulate them
+	//With order: Update pointer, U
+	// With Euler order: Add a update function
+	//With reached: Add one in
+
+	val parents = IntArray(n+1)
+	val depth = IntArray(n+1)
+	val DFSorder = IntArray(n+1)
+	var DFSPointer = 1
+	val treeSize = IntArray(n+1)
+
+
+	fun DFSTransversalDiTree(){
+		fun go(i:Int,from:Int,d:Int):Int{
+			depth[i] = d
+
+
+			var size:Int = 1
+			NS(i){v ->
+				if(v != from){
+					size += go(v,i,d+1)
+					parents[v] = i
+				}
+			}
+
+			DFSorder[DFSPointer] = i
+			DFSPointer += 1
+//            f(i)
+			treeSize[i] = size
+			return size
+		}
+		DFSPointer = 1
+		go(1,0,0)
+		parents[1] = 1
+	}
+
+
+
+
+	val quickParents = mutableListOf<IntArray>()
+	fun calQuickPA(){
+		var needed = 1
+		var total = this.n
+		while(total > 1){
+			total = total shr 1
+			needed += 1
+		}
+		quickParents.add(parents)
+		for( i in 1..needed){
+			val previous = quickParents.last()
+			val new = IntArray(n+1)
+			for(a in new.indices){
+				new[a] = previous[previous[a]]
+			}
+			quickParents.add(new)
+		}
+	}
+	fun calParent(i:Int,l:Int):Int{
+		var now = i
+		var layer = l
+
+		for( (i,p) in powersOfTwo.withIndex()){
+			if(p > layer){
+				break
+			}
+			if(p and layer > 0){
+				now = quickParents[i][now]
+				layer -= p
+			}
+		}
+		return now
+	}
+	fun isParent(i:Int,of:Int):Boolean{
+		val a = depth[i]
+		val b = depth[of]
+		if(a > b){
+			return false
+		}else{
+			return calParent(of,b-a) == a
+		}
+	}
+
+	fun connectedComponents(VS:List<Int>):List<List<Int>>{
+		val haveNotDone = mutableSetOf<Int>()
+		haveNotDone.addAll(VS)
+		val ret = mutableListOf<List<Int>>()
+
+		val explored = IntArray(n+1)
+//		val from = IntArray(n+1)
+		while(haveNotDone.any()){
+			var component = mutableListOf<Int>()
+			val root = haveNotDone.first()
+			val toDo = ArrayDeque<Int>()
+			toDo.add(root)
+			explored[root] = 1
+
+			while(toDo.size > 0){
+				val x = toDo.removeLast()
+				NS(x){a ->
+					if(explored[a] == 0){
+						explored[a] = 1
+						component.add(a)
+						toDo.addLast(a)
+					}
+				}
+			}
+			ret.add(component)
+
+		}
+		return ret
+	}
+
+}
+fun IntArray.maxInsert(v:Int){
+	val a = this.indexOfFirst { v > it }
+	if(a == -1){
+		return
+	}else{
+		for(i in a until this.size - 1){
+			this[i+1] = this[i]
+		}
+		this[a] = v
+	}
+}
+
+
+fun main() {
+	val n = getint()
+
+	val ci = mutableListOf<Int>(0)
+	repeat(n){
+		ci.add(getint())
+	}
+	val G = Graph(n,false)
+
+	G.streamEdges(n-1)
+
+	G.DFSTransversalDiTree()
+
+
+	val maxColour = IntArray(n+1)
+//	val maxColour2  = IntArray(n+1)
+	var ret = 0
+
+	for(i in 1..n){
+		val v = G.DFSorder[i]
+		var currentColour = intArrayOf(1,1)
+
+		val thisC = ci[v]
+
+		for(w in G.E[v]){
+			if(w == G.parents[v]){
+				continue
+			}
+			val thatC = ci[w]
+			if(thatC == thisC){
+				currentColour.maxInsert(maxColour[w])
+			}else{
+				currentColour.maxInsert(maxColour[w]+1)
+			}
+		}
+		maxColour[v] = currentColour[0]
+		ret = maxOf(ret,currentColour[0] + currentColour[1] - 1)
+//		println("v${v}")
+//		println(currentColour.joinToString(" "))
+	}
+	put(ret/2)
+	done()
+
+}
