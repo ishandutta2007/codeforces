831a33db81c0527527a299ecbfbcea5b98c1da77
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 23:32:47 +0530

    kmjp

---
+++ kmjp/normal/1217/C.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int T;
+int N;
+string S;
+int num[202020];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>T;
+	while(T--) {
+		cin>>S;
+		N=S.size();
+		FOR(i,N) {
+			S[i]-='0';
+			num[i+1]=num[i]+S[i];
+		}
+		int ret=0;
+		FOR(i,N) if(S[i]) {
+			int cur=0;
+			for(int len=1;1<<(len-1)<=N && i+len<=N;len++) {
+				cur=cur*2+S[i+len-1];
+				if(cur>=len && cur<=i+len) {
+					if(num[i+len]-num[i+len-cur]==__builtin_popcount(cur)) ret++;
+				}
+			}
+		}
+		cout<<ret<<endl;
+	}
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
