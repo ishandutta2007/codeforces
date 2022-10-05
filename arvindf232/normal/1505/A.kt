14249367064b676c42050391b81a0a38be69915d
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:16:08 +0530

    arvindf232

---
+++ arvindf232/normal/1505/A.kt
@@ -0,0 +1,133 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import kotlin.math.min
+import kotlin.math.max
+import kotlin.reflect.KProperty0
+
+class FastScanner {
+	private val BS = 1 shl 16
+	private val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+	private var `in`: BufferedInputStream? = null
+
+	constructor() {
+		`in` = BufferedInputStream(System.`in`, BS)
+	}
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = try {
+					`in`!!.read(buf)
+				} catch (e: Exception) {
+					return NC
+				}
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.appendCodePoint(c.toInt())
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.appendCodePoint(c.toInt());
+
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+class reading{
+	companion object{
+		var jin = FastScanner()
+		var pw = PrintWriter(System.out)
+	}
+}
+fun put(aa:Any){ reading.pw.println(aa)}
+fun done(){ reading.pw.close() }
+
+fun getint():Int{ return reading.jin.nextInt() }
+fun getlong():Long{ return reading.jin.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getstr():String{ return reading.jin.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+inline fun cases(ask:()->Unit){ val t = getint();repeat(t){ ask() }}
+
+val List<Char>.ret:String
+	get() = this.joinToString("")
+infix fun Any.dei(a:Any){
+	//does not stand for anything it is just easy to type
+	var str = ""
+	if(this is String){ str = this
+	}else if(this is Int){ str = this.toString()
+	}else if(this is Long){ str = this.toString()
+	}
+	if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+	}else{ println("$str : $a")
+	}
+}
+val just = " " // usage: just dei x , where x is the debug variable
+
+fun main() {
+	while(true){
+		val a = readLine()
+		if(a == null){
+			break
+		}
+		println("NO")
+		System.out.flush()
+	}
+
+    // Write your solution here
+}
---
+++ arvindf232/normal/1505/C.kt
@@ -0,0 +1,148 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import kotlin.math.min
+import kotlin.math.max
+import kotlin.reflect.KProperty0
+
+class FastScanner {
+	private val BS = 1 shl 16
+	private val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+	private var `in`: BufferedInputStream? = null
+
+	constructor() {
+		`in` = BufferedInputStream(System.`in`, BS)
+	}
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = try {
+					`in`!!.read(buf)
+				} catch (e: Exception) {
+					return NC
+				}
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.appendCodePoint(c.toInt())
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.appendCodePoint(c.toInt());
+
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+class reading{
+	companion object{
+		var jin = FastScanner()
+		var pw = PrintWriter(System.out)
+	}
+}
+fun put(aa:Any){ reading.pw.println(aa)}
+fun done(){ reading.pw.close() }
+
+fun getint():Int{ return reading.jin.nextInt() }
+fun getlong():Long{ return reading.jin.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getstr():String{ return reading.jin.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+inline fun cases(ask:()->Unit){ val t = getint();repeat(t){ ask() }}
+
+val List<Char>.ret:String
+	get() = this.joinToString("")
+infix fun Any.dei(a:Any){
+	//does not stand for anything it is just easy to type
+	var str = ""
+	if(this is String){ str = this
+	}else if(this is Int){ str = this.toString()
+	}else if(this is Long){ str = this.toString()
+	}
+	if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+	}else{ println("$str : $a")
+	}
+}
+val just = " " // usage: just dei x , where x is the debug variable
+
+fun main() {
+	val at = readLine()!!
+
+	fun mod(a:Char):Int{
+		return a.toInt() - 'A'.toInt()
+	}
+	fun ask(a:String):Boolean{
+		var good = true
+		for(c in a.windowed(3)){
+			val a1 = mod(c[0])
+			val a2 = mod(c[1])
+			val a3 = mod(c[2])
+//			just dei a1
+//			just dei a2
+//			just dei a3
+			if((a1+a2)%26 != a3){
+				good = false
+			}
+		}
+		return good
+	}
+	put(if(ask(at))"YES" else "NO")
+	done()
+
+//1,1,2,3,5,8,13,21,34,55,89
+    // Write your solution here
+}
