40c3160202b58444d9c64d13a1da2be7d5d6a80c
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 17:02:59 +0530

    akulyat

---
+++ akulyat/normal/1693/B.cpp
@@ -0,0 +1,102 @@
+// #include <bits/stdc++.h>
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#pragma GCC optimize ("O3", "unroll-all-loops")
+#pragma GCC target ("sse4.2")
+
+using namespace std;
+
+#define F first
+#define S second
+typedef long long       ll;
+typedef long double     ld;
+typedef pair<ll, ll>   pll; 
+typedef pair<int, int> pii; 
+
+const long long kk = 1000;
+const long long ml = kk * kk;
+const long long mod = ml * kk + 7;
+const long long inf = ml * ml * ml + 7; 
+#ifdef DEBUG
+	mt19937 rng(1033);
+#else
+	mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());	
+#endif
+int rnd(int mod) { return uniform_int_distribution<int>(0, mod - 1)(rng); }
+
+
+bool viv = false;
+int n;
+vector<vector<int>> e;
+vector<int> p;
+vector<ll> l;
+vector<ll> r;
+
+
+void solve() {
+	cin >> n;	
+	e.assign(n, {});
+	p.clear();
+	l.clear();
+	r.clear();
+	p.push_back(-1);
+	for (int i = 1; i < n; i++) {
+		int pp;
+		cin >> pp;
+		pp--;
+		p.push_back(pp);
+		e[pp].push_back(i);
+	}
+	for (int i = 0; i < n; i++) {
+		ll a, b;
+		cin >> a >> b;
+		l.push_back(a);
+		r.push_back(b);
+	}
+	int ans = 0;
+	for (int i = n - 1; i >= 0; i--) {
+		ll sum = 0;
+		for (auto u : e[i]) {
+			sum += r[u];
+		}
+		if (sum < l[i])
+			ans++;
+		else {
+			r[i] = min(r[i], sum);
+		}
+		// cout << "\t" << i << ": " << r[i] << " ans = " << ans << endl;
+	}
+	cout << ans << '\n';
+}
+
+int main() {
+	// viv = true;
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	cout << fixed << setprecision(20);
+	int t = 1; 
+	cin >> t;
+	while (t--)
+		solve();
+
+	#ifdef DEBUG
+		cerr << "Runtime is: " << clock() * 1.0 / CLOCKS_PER_SEC << endl;
+	#endif
+}
