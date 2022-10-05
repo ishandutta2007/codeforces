190d72d4d9dcfd8f864dc5dd937c58cf0c2e1883
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:25:11 +0530

    arvindf232

---
+++ arvindf232/normal/1480/B.kt
@@ -0,0 +1,147 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+
+class FastScanner {
+    private val BS = 1 shl 16
+    private val NC = 0.toChar()
+    private val buf = ByteArray(BS)
+    private var bId = 0
+    private var size = 0
+    private var c = NC
+    private var `in`: BufferedInputStream? = null
+
+    constructor() {
+        `in` = BufferedInputStream(System.`in`, BS)
+    }
+
+    private val char: Char
+        private get() {
+            while (bId == size) {
+                size = try {
+                    `in`!!.read(buf)
+                } catch (e: Exception) {
+                    return NC
+                }
+                if (size == -1) return NC
+                bId = 0
+            }
+            return buf[bId++].toChar()
+        }
+
+    fun nextInt(): Int {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0
+        while (c >= '0' && c <= '9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextLong(): Long {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0L
+        while (c >= '0' && c <= '9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextString():String{
+        if (c == NC) c = char
+        val ret = StringBuilder()
+        while (true){
+            c = char
+            if(!isWhitespace(c)){ break}
+        }
+        ret.appendCodePoint(c.toInt())
+        while (true){
+            c = char
+            if(isWhitespace(c)){ break}
+            ret.appendCodePoint(c.toInt());
+
+        }
+        return ret.toString()
+    }
+    fun isWhitespace(c:Char):Boolean{
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+    }
+}
+class reading{
+    companion object{
+        var jin = FastScanner()
+        var pw = PrintWriter(System.out)
+    }
+}
+fun put(aa:Any){
+    reading.pw.println(aa)
+}
+fun done(){
+    reading.pw.close()
+}
+
+fun getInt():Int{ return reading.jin.nextInt() }
+fun getLong():Long{ return reading.jin.nextLong() }
+fun getLine(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getLineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getString():String{ return reading.jin.nextString() }
+
+
+inline fun cases(ask:()->Unit){
+    val t = getInt()
+    repeat(t){
+        ask()
+    }
+}
+infix fun Long.divCeil(b:Long):Long{
+    //Positives numbers only!
+    if(this == 0L) {
+        return 0
+    }
+    return (this-1)/b + 1
+}
+
+fun main() {
+    cases case@{
+        val A = getLong()
+        val B = getLong()
+        val n = getInt()
+        val Atk = getLineL(n)
+        val H = getLineL(n)
+
+        var couldExtra =0L
+        var healthRemain = B
+        for(i in Atk.indices){
+            val a = Atk[i]
+            val b = H[i]
+
+            val rounds = b divCeil A
+            val healthCost = rounds * a
+
+            couldExtra = maxOf(couldExtra,a)
+            healthRemain -= healthCost
+
+            if(healthRemain <= (-couldExtra)){
+                put("NO")
+                return@case
+            }
+        }
+        put("YES")
+    }
+    done()
+    // Write your solution here
+}
