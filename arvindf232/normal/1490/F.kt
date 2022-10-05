e44dd14ba79871f86a26dbec4af14ad1b5141ea9
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:17:36 +0530

    arvindf232

---
+++ arvindf232/normal/1490/F.kt
@@ -0,0 +1,172 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import kotlin.reflect.KProperty0
+
+class FastScanner {
+	private val BS = 1 shl 16
+	private val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+	private var `in`: BufferedInputStream? = null
+
+	constructor() {
+		`in` = BufferedInputStream(System.`in`, BS)
+	}
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = try {
+					`in`!!.read(buf)
+				} catch (e: Exception) {
+					return NC
+				}
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.appendCodePoint(c.toInt())
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.appendCodePoint(c.toInt());
+
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+class reading{
+	companion object{
+		var jin = FastScanner()
+		var pw = PrintWriter(System.out)
+	}
+}
+fun put(aa:Any){
+	reading.pw.println(aa)
+}
+fun done(){
+	reading.pw.close()
+}
+
+fun getint():Int{ return reading.jin.nextInt() }
+fun getlong():Long{ return reading.jin.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getstr():String{ return reading.jin.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+
+
+inline fun cases(ask:()->Unit){
+	val t = getint()
+	repeat(t){
+		ask()
+	}
+}
+val List<Char>.ret:String
+	get() = this.joinToString("")
+infix fun Any.dei(a:Any){
+	//does not stand for anything it is just easy to type
+	var str = ""
+	if(this is String){ str = this
+	}else if(this is Int){ str = this.toString()
+	}else if(this is Long){ str = this.toString()
+	}
+	if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+	}else{ println("$str : $a")
+	}
+}
+fun <K> MutableMap<K,Int>.addone(key:K){
+	this[key] = this.getOrDefault(key,0) + 1
+}
+fun countIt(max:Int,arr:List<Int>):IntArray{
+	val ret = IntArray(max)
+	for(a in arr){
+		ret[a] += 1
+	}
+	return ret
+}
+class rsq(val arr:List<Int>) {
+	val ps = arr.runningFold(0L, { a,b->a+b.toLong()} )
+	fun sumQuery(l:Int,r:Int):Long{
+		val ll = maxOf(l,0)
+		val rr = minOf(arr.lastIndex,r) + 1
+		return ps[rr] - ps[ll]
+	}
+}
+val just = " "
+fun main() {
+	cases {
+		val n = getint()
+		val arr = getline(n)
+		val map = mutableMapOf<Int,Int>()
+		for(a in arr){
+			map.addone(a)
+		}
+		val multi = IntArray(n+1)
+		val sums = IntArray(n+1)
+		for((k,v) in map.entries){
+			multi[v] += 1
+			sums[v] += v
+		}
+		val mSum = rsq(multi.toList())
+		val sSum = rsq(sums.toList())
+
+		fun cost(a:Int):Long{
+			return sSum.sumQuery(0,n) - mSum.sumQuery(a,n) * a
+		}
+		val minCost = (0..n).map{cost(it)}.minOrNull()!!
+		put(minCost)
+	}
+	done()
+    // Write your solution here
+}
