42c3f048d4e9653bc83c0fd9310124835c4f571f
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 20:43:17 +0530

    arvindf232

---
+++ arvindf232/normal/1698/G.kt
@@ -0,0 +1,381 @@
+// 2022.06.28 at 23:59:56 HKT
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.PrintWriter
+import kotlin.system.measureTimeMillis
+import java.util.TreeMap
+import java.util.TreeSet
+import kotlin.random.Random
+import kotlin.random.nextInt
+
+// 1. Modded
+const val p = 1000000007L
+const val pI = p.toInt()
+fun Int.adjust():Int{ if(this >= pI){ return this  - pI }else if (this < 0){ return this + pI };return this }
+fun Int.snap():Int{ if(this >= pI){return this - pI} else return this}
+infix fun Int.modM(b:Int):Int{ return ((this.toLong() * b) % pI).toInt() }
+infix fun Int.modPlus(b:Int):Int{ val ans = this + b;return if(ans >= pI) ans - pI else ans }
+fun intPow(x:Int,e:Int,m:Int):Int{
+    var X = x ; var E =e ; var Y = 1
+    while(E > 0){
+        if(E and 1 == 0){
+            X = ((1L * X * X) % m).toInt()
+            E = E shr 1
+        }else{
+            Y = ((1L * X * Y) % m).toInt()
+            E -= 1
+        }
+    }
+    return Y
+}
+// 2. DP initial values
+const val plarge = 1_000_000_727
+const val nlarge = -plarge
+const val phuge = 2_727_000_000_000_000_000L
+const val nhuge = -phuge
+// 3. conveniecen conversions
+val Boolean.chi:Int get() = if(this) 1 else 0 //characteristic function
+val Char.code :Int get() = this.toInt() -  'a'.toInt()
+//3. hard to write stuff
+fun IntArray.put(i:Int,v:Int){ this[i] = (this[i] + v).adjust() }
+val mint:MutableList<Int> get() = mutableListOf<Int>()
+val mong:MutableList<Long> get() = mutableListOf<Long>()
+//4. more outputs
+fun List<Char>.conca():String = this.joinToString("")
+val CharArray.conca :String get() = this.joinToString("")
+val IntArray.conca :String get() = this.joinToString(" ")
+@JvmName("concaInt")
+fun List<Int>.conca():String = this.joinToString(" ")
+val LongArray.conca:String get() = this.joinToString(" ")
+@JvmName("concaLong")
+fun List<Long>.conca():String = this.joinToString(" ")
+//5. Pair of ints
+const val longmask = (1L shl 32) - 1
+fun makepair(a:Int, b:Int):Long = (a.toLong() shl 32) xor (longmask and b.toLong()) // remember positev sonly
+val Long.first get() = (this ushr 32).toInt()
+val Long.second get() = this.toInt()
+//6. strings
+val String.size get() = this.length
+const val randCount = 100
+//7. bits
+fun Int.has(i:Int):Boolean = (this and (1 shl i) != 0)
+fun Long.has(i:Int):Boolean = (this and (1L shl i) != 0L)
+//8 TIME
+inline fun TIME(f:()->Unit){
+    val t = measureTimeMillis(){
+        f()
+    }
+    println("$t ms")
+}
+object Reader{
+    private const val BS = 1 shl 16
+    private const val NC = 0.toChar()
+    private val buf = ByteArray(BS)
+    private var bId = 0
+    private var size = 0
+    private var c = NC
+
+    var warningActive = true
+    var fakein = StringBuilder()
+
+    private var IN: BufferedInputStream = BufferedInputStream(System.`in`, BS)
+    val OUT: PrintWriter = PrintWriter(System.out)
+
+    private val char: Char
+        get() {
+            while (bId == size) {
+                size = IN.read(buf) // no need for checked exceptions
+                if (size == -1) return NC
+                bId = 0
+            }
+            return buf[bId++].toChar()
+        }
+
+    fun nextInt(): Int {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0
+        while (c in '0'..'9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextLong(): Long {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0L
+        while (c in '0'..'9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextString():String{
+        val ret = StringBuilder()
+        while (true){
+            c = char
+            if(!isWhitespace(c)){ break}
+        }
+        ret.append(c)
+        while (true){
+            c = char
+            if(isWhitespace(c)){ break}
+            ret.append(c)
+        }
+        return ret.toString()
+    }
+    fun isWhitespace(c:Char):Boolean{
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+    }
+    fun rerouteInput(){
+        if(warningActive){
+            put("Custom test enabled")
+            println("Custom test enabled")
+            warningActive = false
+        }
+        val S = fakein.toString()
+        println("New Case ")
+        println(S.take(80))
+        println("...")
+        fakein.clear()
+        IN = BufferedInputStream(S.byteInputStream(),BS)
+    }
+    fun takeFile(name:String){
+        IN = BufferedInputStream(File(name).inputStream(),BS)
+    }
+}
+fun put(aa:Any){ Reader.OUT.println(aa)}
+fun done(){ Reader.OUT.close() }
+fun share(aa:Any){
+    if(aa is IntArray){Reader.fakein.append(aa.joinToString(" "))}
+    else if(aa is LongArray){Reader.fakein.append(aa.joinToString(" "))}
+    else if(aa is List<*>){Reader.fakein.append(aa.toString())}
+    else{Reader.fakein.append(aa.toString())}
+    Reader.fakein.append("\n")
+}
+
+val getintfast:Int get() = Reader.nextInt()
+val getint:Int get(){ val ans = getlong ; if(ans > Int.MAX_VALUE) IntArray(1000000000); return ans.toInt() }
+val getlong:Long get() = Reader.nextLong()
+val getstr:String get() = Reader.nextString()
+fun getline(n:Int):IntArray{
+    return IntArray(n){getint}
+}
+fun getlineL(n:Int):LongArray{
+    return LongArray(n){getlong}
+}
+var dmark = -1
+infix fun Any.dei(a:Any){
+    dmark++
+    var str = "<${dmark}>   "
+    debug()
+    if(this is String){ str += this
+    }else if(this is Int){ str += this.toString()
+    }else if(this is Long){ str += this.toString()
+    }else{ str += this.toString()}
+    if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+    }else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+    }else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+    }else if(a is BooleanArray){ println("$str :${a.map{if(it)'1' else '0'}.joinToString(" ")}")
+    }else if(a is Array<*>){
+        println("$str : ")
+        for(c in a){if(c is IntArray){println(c.joinToString(" "))}
+        else if(c is LongArray){println(c.joinToString(" "))}
+        else if(c is BooleanArray){println(c.map { if(it) '1' else '0' }.joinToString(""))
+        }
+        }
+        println()
+    }else{ println("$str : $a")
+    }
+}
+val just = " "
+fun crash(){
+    throw Exception("Bad programme")}
+fun assert(a:Boolean){
+    if(!a){
+        throw Exception("Failed Assertion")
+    }}
+enum class solveMode {
+    real, rand, tc
+}
+object solve{
+    var mode:solveMode = solveMode.real
+    var tcNum:Int = 0
+    var rand:()->Unit = {}
+    var TC:MutableMap<Int,()->Unit> = mutableMapOf()
+    var tn:Long = 0
+    fun cases(onecase:()->Unit){
+        val t = if(mode == solveMode.real){if(singleCase) 1 else getint} else if(mode == solveMode.tc){1 } else randCount
+        if(pI != 998_244_353 && pI != 1_000_000_007){
+            throw Exception("Not usual primes!")
+        }
+        if(t == 1 && mode != solveMode.real){
+            tn = System.currentTimeMillis()
+        }
+        repeat(t){
+            if(mode == solveMode.tc){
+                TC[tcNum]?.let { it() }
+                Reader.rerouteInput()
+            }else if(mode == solveMode.rand){
+                rand()
+                Reader.rerouteInput()
+            }
+            onecase()
+        }
+        if(t == 1 && mode != solveMode.real){
+            val dt = System.currentTimeMillis() - tn
+            println("Time $dt ms ")
+        }
+    }
+    inline fun singleCase(a:solve.()->Unit){
+        val t = if(mode != solveMode.rand){1} else randCount
+        repeat(t) { a() }
+    }
+    fun rand(a:()->Unit){
+        this.rand = a
+    }
+    fun tc(id:Int = 0,a:()->Unit){
+        TC[id] = a
+    }
+    fun usetc(a:Int = 0 ){
+        this.tcNum = a
+        this.mode = solveMode.tc
+    }
+    fun userand(){
+        this.mode = solveMode.rand
+    }
+}
+fun debug(){}
+const val singleCase = true
+fun main(){
+    solve.cases{
+        var s = getstr
+        if(s.all { it == '0' }) {
+            put(-1)
+            return@cases
+        }
+        var start = 0
+        while(s.first() == '0'){
+            start++
+            s= s.drop(1)
+        }
+        while(s.last() == '0'){
+            s = s.dropLast(1)
+        }
+        val n = s.size
+        if(n ==1){
+            put("${1 + start} ${2 + start}")
+            return@cases
+        }
+        var forwardmask = 0L
+        for(i in 1 until n){
+            if(s[i] == '1'){
+                forwardmask = forwardmask xor ( 1L shl (i-1))
+            }
+        }
+        fun next(x:Long):Long {
+            var new = x shr 1
+            if(x and 1L == 1L){
+                new = new xor forwardmask
+            }
+            return new
+        }
+        fun previous(x:Long):Long{
+            var new:Long
+            if(x and (1L shl (n-2)) != 0L){
+                new = (((x xor forwardmask) shl 1) and ((1L shl (n-1)) - 1 )) or 1L
+            }else{
+                new = (x shl 1) and ((1L shl (n-1)) - 1 )
+            }
+            return new
+        }
+        fun Long.binary():String{
+            val ret = CharArray(n-1){if((this and (1L shl it)) != 0L)'1' else '0' }
+            return ret.conca
+        }
+        fun getinverse(k:Int):LongArray{
+            var now = 1L
+            val last = mong
+            last.add(now)
+            repeat(k + (n-2)){
+                last.add(previous(last.last()))
+            }
+            return last.takeLast(n-1).toLongArray()
+        }
+        fun calculate(M:LongArray,start:Long):Long{
+            var ret = 0L
+            for(i in 0 until n-1){
+                if(start.has(i)){
+                    ret = ret xor M[i]
+                }
+            }
+            return ret
+        }
+
+        fun solve():Long{
+            val cut = 1 shl 18
+            val new = LongArray(cut)
+            new[0]=  1L
+            for(i in 1 until cut){
+                new[i] = next(new[i-1])
+                if(new[i] == 1L){
+                    return i.toLong()
+                }
+            }
+            val invert1 = getinverse(cut)
+            val map = mutableMapOf<Long,Int>()
+            for((i,v) in new.withIndex()){
+                map[v] = i
+            }
+            var now = 1L
+            var res = 0L
+            repeat(1 shl 18){
+                now = calculate(invert1,now)
+                res += cut
+                if(map[now] != null){
+                    return map[now]!! + res
+                }
+            }
+            crash()
+            return 0L
+        }
+        val ans = solve()
+        put(start+ 1)
+        put(start + 1 + ans)
+//        just dei now.binary()
+//        repeat(100){it ->
+//            val new = next(now)
+//            val old = previous(new)
+//            assert(old == now)
+//            now = new
+//
+//            val M = getinverse(it + 1 )
+//            assert(calculate(M,new) == 1L )
+//
+//            just dei now.binary()
+//        }
+
+
+
+
+
+
+    }
+    done()
+}
+/*
+1011
+100111
+1000101
+10000001
+
+ */
