d0a2789962e20912cbdc2f9b2c94fe4ddc99c102
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 19:40:11 +0530

    akulyat

---
+++ akulyat/normal/1037/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define F first
+#define S second
+typedef long long   ll;
+typedef long double ld;
+typedef pair <ll, ll> pll;
+
+ifstream in;
+ofstream out;
+
+const long long kk=1000;
+const long long ml=kk*kk;
+const long long mod=ml*kk+7;
+const long long inf=ml*ml*ml+7;
+
+
+ll n,i,j;
+vector <ll> m;
+bool viv=false;
+
+
+int main()
+{
+ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+cin>>n;
+ll s=1;
+ll ans=0;
+while (n>0)
+  {
+  n-=s;
+  s*=2;
+  ans++;
+  }
+
+cout<<ans;
+
+
+    return 0;
+}
