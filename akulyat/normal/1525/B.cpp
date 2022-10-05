705f9c2cfe702aa8b2822f5e74f064026fd4f0ba
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 17:26:04 +0530

    akulyat

---
+++ akulyat/normal/1525/B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3", "unroll-all-loops")
+#pragma GCC target ("sse4.2")
+
+using namespace std;
+
+#define F first
+#define S second
+typedef long long       ll;
+typedef long double     ld;
+typedef pair<ll, ll>   pll; 
+typedef pair<int, int> pii; 
+
+ifstream in;
+ofstream out;
+
+const long long kk = 1000;
+const long long ml = kk * kk;
+const long long mod = ml * kk + 7;
+const long long inf = ml * ml * ml + 7; 
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+ll n;
+vector<ll> v;
+bool viv = false;
+
+
+void solve() {
+	cin >> n;
+	v.resize(n);
+	for (auto &i : v)
+		cin >> i, i--;
+	bool need = false;
+	for (int i = 0; i < n; i++)
+		if (v[i] != i)
+			need = true;
+	if (!need) {
+		cout << "0\n";
+		return;
+	}
+	if (v[0] == 0 || v[n - 1] == n - 1) {
+		cout << "1\n";
+		return;
+	}
+	if (v[0] == n - 1 && v[n - 1] == 0) {
+		cout << "3\n";
+		return;
+	}
+	cout << "2\n";
+}
+
+int main() {
+	// viv = true;
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ll t = 1; 
+	cin >> t;
+	while (t--)
+		solve();
+
+	return 0;
+}
