8bf751092b81f03b34ae9f1fb0535eb20623be49
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 18:05:10 +0530

    akulyat

---
+++ akulyat/normal/1398/A.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3", "unroll-all-loops")
+#pragma GCC target ("sse4.2")
+
+using namespace std;
+
+#define F first
+#define S second
+typedef long long       ll;
+typedef long double     ld;
+typedef pair<ll, ll>   pll; 
+typedef pair<int, int> pii; 
+
+ifstream in;
+ofstream out;
+
+const long long kk = 1000;
+const long long ml = kk * kk;
+const long long mod = ml * kk + 7;
+const long long inf = ml * ml * ml + 7; 
+
+ll n;
+vector<ll> v;
+bool viv = false;
+
+
+void solve() {
+	cin >> n;
+	v.resize(n);
+	for (auto &i : v)
+		cin >> i;
+	if (v[0] + v[1] > v.back())
+		cout << -1 << '\n';
+	else
+		cout << 1 << ' ' << 2 << ' ' << v.size() << '\n';
+
+}
+
+int main() {
+	// viv = true;
+	srand(time(0));
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ll t = 1; 
+	cin >> t;
+	while (t--)
+		solve();
+
+	return 0;
+}
