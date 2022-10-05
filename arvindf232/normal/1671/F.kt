2ca211478381eff3765fa976eb9c188bb17cf888
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 21:09:31 +0530

    arvindf232

---
+++ arvindf232/normal/1671/F.kt
@@ -0,0 +1,615 @@
+// 2022-04-22, Fri, 23:19
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.PrintWriter
+import kotlin.system.measureTimeMillis
+import java.util.TreeMap
+import java.util.TreeSet
+
+inline fun TIME(f:()->Unit){
+    val t = measureTimeMillis(){
+        f()
+    }
+    println("$t ms")
+}
+
+object IO{
+    private const val BS = 1 shl 16
+    private const val NC = 0.toChar()
+    private val buf = ByteArray(BS)
+    private var bId = 0
+    private var size = 0
+    private var c = NC
+
+    var warningActive = true
+    var fakein = StringBuilder()
+
+    private var IN: BufferedInputStream = BufferedInputStream(System.`in`, BS)
+    val OUT: PrintWriter = PrintWriter(System.out)
+
+    private val char: Char
+        get() {
+            while (bId == size) {
+                size = IN.read(buf) // no need for checked exceptions
+                if (size == -1) return NC
+                bId = 0
+            }
+            return buf[bId++].toChar()
+        }
+
+    fun nextInt(): Int {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0
+        while (c in '0'..'9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextLong(): Long {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0L
+        while (c in '0'..'9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextString():String{
+        val ret = StringBuilder()
+        while (true){
+            c = char
+            if(!isWhitespace(c)){ break}
+        }
+        ret.append(c)
+        while (true){
+            c = char
+            if(isWhitespace(c)){ break}
+            ret.append(c)
+        }
+        return ret.toString()
+    }
+    fun isWhitespace(c:Char):Boolean{
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+    }
+    fun rerouteInput(){
+        if(warningActive){
+            put("You forgot to disable tests you digital dummy!")
+            println("You forgot to disable tests you digital dummy!")
+            warningActive = false
+        }
+        val S = fakein.toString()
+        println("New Case ")
+        println(S.take(80))
+        println("...")
+        fakein.clear()
+        IN = BufferedInputStream(S.byteInputStream(),BS)
+    }
+    fun takeFile(name:String){
+        IN = BufferedInputStream(File(name).inputStream(),BS)
+    }
+}
+fun put(aa:Any){ IO.OUT.println(aa)}
+fun done(){ IO.OUT.close() }
+fun share(aa:Any){
+    if(aa is IntArray){IO.fakein.append(aa.joinToString(" "))}
+    else if(aa is LongArray){IO.fakein.append(aa.joinToString(" "))}
+    else if(aa is List<*>){IO.fakein.append(aa.toString())}
+    else{IO.fakein.append(aa.toString())}
+    IO.fakein.append("\n")
+}
+
+val getintfast:Int get() = IO.nextInt()
+val getint:Int get(){ val ans = getlong ; if(ans > Int.MAX_VALUE) IntArray(1000000000); return ans.toInt() }
+val getlong:Long get() = IO.nextLong()
+val getstr:String get() = IO.nextString()
+fun getline(n:Int):IntArray{
+    return IntArray(n){getint}
+}
+fun getlineL(n:Int):LongArray{
+    return LongArray(n){getlong}
+}
+fun getbinary(n:Int, asTrue:Char):BooleanArray{
+    val str = getstr
+    return BooleanArray(n){str[it] == asTrue}
+}
+
+val List<Char>.ret:String
+get() = this.joinToString("")
+var dmark = -1
+infix fun Any.dei(a:Any){
+    //does not stand for anything it is just easy to type, have to be infix because kotlin does not have custom prefix operators
+    dmark++
+    var str = "<${dmark}>   "
+    debug()
+    if(this is String){ str += this
+    }else if(this is Int){ str += this.toString()
+    }else if(this is Long){ str += this.toString()
+    }else{ str += this.toString()}
+    if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+    }else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+    }else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+    }else if(a is BooleanArray){ println("$str :${a.map{if(it)'1' else '0'}.joinToString(" ")}")
+    }else if(a is Array<*>){
+        println("$str : ")
+        for(c in a){if(c is IntArray){println(c.joinToString(" "))}
+        else if(c is LongArray){println(c.joinToString(" "))}
+        else if(c is BooleanArray){println(c.map { if(it) '1' else '0' }.joinToString(""))
+        }
+
+        }
+        println()
+    }else{ println("$str : $a")
+    }
+}
+val just = " " // usage: just dei x , where x is the debug variable
+fun crash(){
+    throw Exception("Bad programme")} // because assertion does not work
+fun assert(a:Boolean){
+    if(!a){
+        throw Exception("Failed Assertion")
+    }}
+enum class solveMode {
+    real, rand, tc
+}
+object solve{
+    var mode:solveMode = solveMode.real
+    var tcNum:Int = 0
+    var rand:()->Unit = {}
+    var TC:MutableMap<Int,()->Unit> = mutableMapOf()
+    var answersChecked = 0
+    var tn:Long = 0
+    fun cases(onecase:()->Unit){
+        val t = if(mode == solveMode.real){if(singleCase) 1 else getint} else if(mode == solveMode.tc){1 } else randCount
+        //safety checks
+        if(pI != 998_244_353 && pI != 1_000_000_007){
+            throw Exception("Modding a wrong prime!")
+        }
+        if(withBruteForce){
+            println("Brute force is active")
+        }
+
+        if(t == 1 && mode != solveMode.real){
+            tn = System.currentTimeMillis()
+        }
+        repeat(t){
+            if(mode == solveMode.tc){
+                TC[tcNum]?.let { it() }
+                IO.rerouteInput()
+            }else if(mode == solveMode.rand){
+                rand()
+                IO.rerouteInput()
+            }
+            onecase()
+        }
+        if(t == 1 && mode != solveMode.real){
+            val dt = System.currentTimeMillis() - tn
+            println("Time $dt ms ")
+        }
+    }
+    inline fun singleCase(a:solve.()->Unit){
+        val t = if(mode != solveMode.rand){1} else randCount
+        repeat(t) { a() }
+    }
+    fun rand(a:()->Unit){
+        this.rand = a
+    }
+    fun tc(id:Int = 0,a:()->Unit){
+        TC[id] = a
+    }
+    fun usetc(a:Int = 0 ){
+        this.tcNum = a
+        this.mode = solveMode.tc
+    }
+    fun userand(){
+        this.mode = solveMode.rand
+    }
+}
+inline fun<T> T.alsoBrute(cal:() -> T){
+    if(!withBruteForce) return
+    val also = cal()
+    if(this != also){
+        println("Checking failed: Got ${this} Brute ${also}")
+        crash()
+    }
+}
+// 1. Modded
+const val p = 998244353L
+const val pI = p.toInt()
+fun Int.adjust():Int{ if(this >= pI){ return this  - pI }else if (this < 0){ return this + pI };return this }
+fun Int.snap():Int{ if(this >= pI){return this - pI} else return this}
+infix fun Int.modM(b:Int):Int{ return ((this.toLong() * b) % pI).toInt() }
+infix fun Int.modPlus(b:Int):Int{ val ans = this + b;return if(ans >= pI) ans - pI else ans }
+fun intPow(x:Int,e:Int,m:Int):Int{
+    var X = x ; var E =e ; var Y = 1
+    while(E > 0){
+        if(E and 1 == 0){
+            X = ((1L * X * X) % m).toInt()
+            E = E shr 1
+        }else{
+            Y = ((1L * X * Y) % m).toInt()
+            E -= 1
+        }
+    }
+    return Y
+}
+// 2. DP initial values
+const val plarge = 1_000_000_727
+const val nlarge = -plarge
+const val phuge = 2_727_000_000_000_000_000L
+const val nhuge = -phuge
+// 3. conveniecen conversions
+val Boolean.chi:Int get() = if(this) 1 else 0 //characteristic function
+val Char.code :Int get() = this.toInt() -  'a'.toInt()
+//3. hard to write stuff
+fun IntArray.put(i:Int,v:Int){ this[i] = (this[i] + v).adjust() }
+val mint:MutableList<Int> get() = mutableListOf<Int>()
+val mong:MutableList<Long> get() = mutableListOf<Long>()
+//4. more outputs
+fun List<Char>.conca():String = this.joinToString("")
+val CharArray.conca :String get() = this.joinToString("")
+val IntArray.conca :String get() = this.joinToString(" ")
+@JvmName("concaInt")
+fun List<Int>.conca():String = this.joinToString(" ")
+val LongArray.conca:String get() = this.joinToString(" ")
+@JvmName("concaLong")
+fun List<Long>.conca():String = this.joinToString(" ")
+//5. Pair of ints
+const val longmask = (1L shl 32) - 1
+fun makepair(a:Int, b:Int):Long = (a.toLong() shl 32) xor (longmask and b.toLong()) // remember positev sonly
+val Long.first get() = (this ushr 32).toInt()
+val Long.second get() = this.toInt()
+//6. strings
+val String.size get() = this.length
+const val randCount = 100
+//7. bits
+fun Int.has(i:Int):Boolean = (this and (1 shl i) != 0)
+
+
+
+
+fun basicDP(n:Int, k:Int, x:Int):Int {
+    //phrase, drops, inversions taken, level down
+    var DP = Array(x+1){Array(k+1){IntArray(k+1)} }
+    DP[0][0][0] = 1
+    for(p in 0 until n){
+        val new = Array(x+1){Array(k+1){IntArray(k+1)} }
+        for(drop in 0..x){
+            for(inversions in 0..k){
+                for(leveldown in 0..k){
+                    for(nextlevel in 0..k){
+                        val newdrop = drop + (nextlevel > leveldown).chi
+                        val newinversions = inversions + nextlevel
+                        if(nextlevel !in 0..p) continue
+                        if(newdrop !in 0..x) continue
+                        if(newinversions !in 0..k) continue
+                        new[newdrop][newinversions][nextlevel] = new[newdrop][newinversions][nextlevel] modPlus DP[drop][inversions][leveldown]
+                    }
+                }
+            }
+        }
+        DP = new
+//        just dei p
+    }
+    var total = 0
+    val want = DP[x][k]
+    for(a in want){
+        total = total modPlus a
+    }
+    return total
+}
+
+infix fun Int.divceil(b:Int):Int{
+    //Positives numbers only!
+    if(this == 0) {
+        return 0
+    }
+    return (this-1)/b + 1
+}
+infix fun Long.divceil(b:Long):Long{
+    //Positives numbers only!
+    if(this == 0L) {
+        return 0
+    }
+    return (this-1)/b + 1
+}
+
+infix fun Long.modM(b:Long):Long{
+    return (this * b) % p
+}
+//infix fun Int.modPlus(b:Int):Int{
+//    val ans = this + b
+//    return if(ans >= pI) ans - pI else ans
+//}
+infix fun Int.modMinus(b:Int):Int{
+    val ans = this - b
+    return if(ans < 0) ans + pI else ans
+}
+infix fun Int.modDivide(b:Int):Int{
+    return this modM (b.inverse())
+}
+fun Int.additiveInverse():Int{
+    return if(this == 0) 0 else pI - this
+}
+
+fun intPowEXP(x:Int,e:Long,m:Int):Int{
+    var X = x
+    var E =e
+    var Y = 1
+    while(E > 0){
+        if(E % 2 == 0L){
+            X = ((1L * X * X) % m).toInt()
+            E = E shr 1
+        }else{
+            Y = ((1L * X * Y) % m).toInt()
+            E -= 1
+        }
+    }
+    return Y
+}
+
+fun pow(x:Long,e:Long,m:Long):Long{
+    var X = x
+    var E =e
+    var Y = 1L
+    while(E > 0){
+        if(E % 2 == 0L){
+            X = (X * X) % m
+            E /= 2
+        }else{
+            Y = (X * Y) % m
+            E -= 1
+        }
+    }
+    return Y
+}
+fun Long.inverse():Long{
+    return pow(this,p-2,p)
+}
+fun Int.inverse():Int{
+    return intPow(this,pI-2,pI)
+}
+fun min_rem(m:Int, r:Int, c:Int):Int {
+    if(c < 1){
+        return Int.MIN_VALUE
+    }else if(r == 0){
+        return 0
+    }else{
+        val step = m % r
+        val mx = ((1L * c * r) /m ).toInt()
+        val t = max_rem(r,step,mx)
+        return r- t
+    }
+}
+fun max_rem(m:Int, r:Int, c:Int):Int {
+    if(r == 0|| c <= m/r){
+        return r * c
+    }else{
+        val step =  m % r
+        val mx = ((1L * (c+1) * r )/m).toInt()
+        val t = min_rem(r,step,mx)
+        return  m - t
+    }
+}
+fun Int.reconstruct():String{
+    val num = min_rem(pI,this, 10000)
+    val denom = (this modDivide num).inverse()
+    return "$num / $denom"
+}
+
+//make this int instead
+class FACT{
+    companion object {
+        var store = IntArray(0)
+        var invStore = IntArray(0)
+
+        var slowStore:IntArray = IntArray(0)
+
+        fun preCal(upto:Int){
+            store = IntArray(upto+1)
+            invStore = IntArray(upto + 1 )
+            store[0] = 1
+            invStore[0] = 1
+
+            for(i in 1..upto) {
+                store[i] = store[i-1] modM i
+                invStore[i] = invStore[i-1] modM (i.inverse())
+            }
+        }
+        fun choose(n:Int,r:Int):Int{
+            if(r < 0 || r > n) return 0
+            val a = store[n]
+            val b = invStore[n-r]
+            val c = invStore[r]
+            return (a modM b) modM c
+        }
+
+        fun bigChoose(n:Int,r:Int):Int{
+            var ret = 1
+            for(i in 0 until r){
+                ret = ret modM (n - i)
+            }
+            ret = ret modM (invStore[r])
+            return ret
+        }
+
+    }
+}
+
+data class polynomial(val terms:IntArray){
+    val degree = (terms.indices.lastOrNull{it != 0 }) ?: -1
+
+    companion object{
+        fun zeroOfDegree(d:Int): polynomial {
+            return polynomial(IntArray(d+1))
+        }
+
+        fun interpolate(X:IntArray, Y:IntArray, newpoint:Int):Int{
+            // N^2 version
+            kotlin.assert(X.size == Y.size)
+//            val find = X.indexOf(newpoint)
+//            if(find != -1){
+//                return Y[find]
+//            }
+            var ans = 0
+            val n = X.size
+
+            for (i in 0 until n) {
+                var num = Y[i]
+                var den = 1
+                for (j in 0 until n) {
+                    if (i != j) {
+                        num = num modM (newpoint modMinus X[j])
+                        den = den modM (X[i] modMinus X[j])
+                    }
+                }
+                ans = (ans modPlus (num modM den.inverse()))
+            }
+            return ans
+        }
+    }
+
+    operator fun get(index:Int): Int {
+        return terms[index]
+    }
+    operator fun set(index:Int, new:Int) {
+        terms[index] = new
+    }
+    operator fun plus(other:polynomial): polynomial {
+        val d = maxOf(this.degree, other.degree)
+        val new = terms.copyOf(d + 1 )
+        for(i in 0 until other.degree){
+            new[i] = new[i] modPlus other.terms[i]
+        }
+        return polynomial(new)
+    }
+    operator fun times(other:polynomial):polynomial{
+        val ret = zeroOfDegree(other.degree + this.degree)
+        for(l in 0..degree){
+            for(r in 0..other.degree){
+                val new = l + r
+                ret[new] = ret[new] modPlus (this[l] modM other[r])
+            }
+        }
+        return ret
+    }
+
+    operator fun unaryMinus():polynomial{
+        val new = terms.copyOf()
+        for(i in new.indices){
+            new[i] = new[i].additiveInverse()
+        }
+        return polynomial(new)
+    }
+//    fun evaluateat(x:Int):Int{
+//        var ret = 0
+//        var power = 1
+//        for(i in 0..degree){
+//
+//        }
+//    }
+
+    fun derivative():polynomial{
+        val new = IntArray(this.degree)
+        for(i in 0 until this.degree){
+            new[i] = terms[i+1] modM i
+        }
+        return polynomial(new)
+    }
+
+
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as polynomial
+
+        if (!terms.contentEquals(other.terms)) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return terms.contentHashCode()
+    }
+}
+fun debug(){}
+const val withBruteForce = false
+const val singleCase = false
+fun main(){
+    //calculate the answers from 23 to 34
+    val res = Array(12) { Array(12) { IntArray(12) } }
+    val lookup = Array(12) { Array(12) { IntArray(35) } }
+//    TIME {
+
+        for(k in 1..11) {
+            for(x in 1..11) {
+                for(other in 1..34) {
+                    if(other >= 23){
+                        res[k][x][other - 23] = basicDP(other, k, x)
+                        lookup[k][x][other] = res[k][x][other - 23]
+                    }else{
+                        lookup[k][x][other] = basicDP(other, k, x)
+                    }
+
+                }
+            }
+        }
+//    }
+    val X = (23..34).toList().toIntArray()
+
+    solve.cases{
+        val n = getint
+        val k = getint
+        val x = getint
+        if(n >= 23){
+            val ans = polynomial.interpolate(X,res[k][x],n)
+            put(ans)
+        }else{
+            put(lookup[k][x][n])
+        }
+//        val k = 5
+//        val x = 7
+//        val ret = mint
+//        for(n in 1..100){
+//            ret.add(basicDP(n,k,x))
+//        }
+//        just dei ret
+//        val n = getint
+//        val k = getint
+//        val x = getint
+//
+//        val k = 3
+//
+//        println(basicDP(n,k,x))
+
+
+
+    }
+    done()
+}
+/*
+2
+10 6 4
+7 3 1
+
+1 4 6 3
+
+1 3 3 2
+1 2 1 1
+
+
+1
+163316 11 7
+ */
