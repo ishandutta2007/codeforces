e5e0c963e1bf521ceb8b1d5dfb3780dbcca2fc17
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 23:46:16 +0530

    kmjp

---
+++ kmjp/normal/889/B.cpp
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N;
+int cand[26];
+set<int> from[26],to[26];
+string S;
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) {
+		cin>>S;
+		FORR(c,S) {
+			c-='a';
+			cand[c]=1;
+		}
+		FOR(j,S.size()-1) {
+			from[S[j+1]].insert(S[j]);
+			to[S[j]].insert(S[j+1]);
+		}
+	}
+	
+	FOR(i,26) {
+		if(from[i].size()>1 || to[i].size()>1) return _P("NO\n");
+	}
+	
+	string R;
+	FOR(i,26) if(cand[i]==1 && from[i].size()==0) {
+		int cur=i;
+		while(1) {
+			R+=(char)('a'+cur);
+			if(to[cur].empty()) break;
+			cur=*to[cur].begin();
+		}
+	}
+	
+	if(R.size()!=count(cand,cand+26,1)) R="NO";
+	cout<<R<<endl;
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
