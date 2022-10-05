864afe8c6e0579391e3f47d9d01918984b4b0c9b
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:27:21 +0530

    arvindf232

---
+++ arvindf232/normal/1475/F.kt
@@ -0,0 +1,148 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+
+class FastScanner {
+    private val BS = 1 shl 16
+    private val NC = 0.toChar()
+    private val buf = ByteArray(BS)
+    private var bId = 0
+    private var size = 0
+    private var c = NC
+    private var `in`: BufferedInputStream? = null
+
+    constructor() {
+        `in` = BufferedInputStream(System.`in`, BS)
+    }
+
+    private val char: Char
+        private get() {
+            while (bId == size) {
+                size = try {
+                    `in`!!.read(buf)
+                } catch (e: Exception) {
+                    return NC
+                }
+                if (size == -1) return NC
+                bId = 0
+            }
+            return buf[bId++].toChar()
+        }
+
+    fun nextInt(): Int {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0
+        while (c >= '0' && c <= '9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextLong(): Long {
+        var neg = false
+        if (c == NC) c = char
+        while (c < '0' || c > '9') {
+            if (c == '-') neg = true
+            c = char
+        }
+        var res = 0L
+        while (c >= '0' && c <= '9') {
+            res = (res shl 3) + (res shl 1) + (c - '0')
+            c = char
+        }
+        return if (neg) -res else res
+    }
+    fun nextString():String{
+        if (c == NC) c = char
+        val ret = StringBuilder()
+        while (true){
+            c = char
+            if(!isWhitespace(c)){ break}
+        }
+        ret.appendCodePoint(c.toInt())
+        while (true){
+            c = char
+            if(isWhitespace(c)){ break}
+            ret.appendCodePoint(c.toInt());
+
+        }
+        return ret.toString()
+    }
+    fun isWhitespace(c:Char):Boolean{
+        return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+    }
+}
+class reading{
+    companion object{
+        var jin = FastScanner()
+        var pw = PrintWriter(System.out)
+    }
+}
+fun put(aa:Any){
+    reading.pw.println(aa)
+}
+fun done(){
+    reading.pw.close()
+}
+
+fun getInt():Int{ return reading.jin.nextInt() }
+fun getLong():Long{ return reading.jin.nextLong() }
+fun getLine(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getLineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getString():String{ return reading.jin.nextString() }
+
+
+inline fun cases(ask:()->Unit){
+    val t = getInt()
+    repeat(t){
+        ask()
+    }
+}
+
+
+fun main() {
+    cases{
+        val n = getInt()
+        val lists = mutableListOf<IntArray>()
+        repeat(n){
+            val st = getString()
+            val str = IntArray(n)
+            for((i,c) in st.withIndex()){
+                str[i] = if(c == '1') 1 else 0
+            }
+            lists.add(str)
+        }
+        repeat(n){
+            val st = getString()
+            for((i,c) in st.withIndex()){
+                val value = if(c == '1') 1 else 0
+                lists[it][i] = lists[it][i] xor value
+            }
+        }
+        for(i in 0 until n){
+            if(lists[0][i] == 1){
+                for(j in 0 until n){
+                    lists[j][i] = 1 - lists[j][i]
+                }
+            }
+        }
+        fun goodRow(i:Int):Boolean{
+            val row = lists[i]
+            val min = row[0]
+            return row.all { it == min }
+        }
+        val ans = (0 until n).all { goodRow(it) }
+        put(if(ans) "YES" else "NO")
+    }
+    done()
+    // Write your solution here
+}
