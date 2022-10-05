a01a42b241aa7a416e67c514f1cef30a37cc061d
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 23:47:22 +0530

    kmjp

---
+++ kmjp/normal/859/C.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N,S[101];
+ll sum[101];
+ll dp[51][2];
+
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	FOR(i,N) cin>>S[i];
+	FOR(i,N) for(j=i;j<N;j++) sum[i]+=S[j];
+	
+	for(i=N-1;i>=0;i--) {
+		dp[i][0]=max(dp[i+1][0],sum[i]-dp[i+1][1]);
+		dp[i][1]=max(dp[i+1][1],sum[i]-dp[i+1][0]);
+	}
+	
+	cout<<sum[0]-dp[0][1]<<" "<<dp[0][1]<<endl;
+	
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
