4500c8b541c9365e6ec5757d35ee3d0d3d29bbc1
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 00:01:21 +0530

    kmjp

---
+++ kmjp/normal/417/A.cpp
@@ -0,0 +1,51 @@
+#include <cstdlib>
+#include <cstring>
+#include <memory>
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <sstream>
+#include <stack>
+#include <queue>
+#include <vector>
+#include <set>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+
+#define FOR(x,to) for(x=0;x<to;x++)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ZERO(a) memset(a,0,sizeof(a))
+void _fill_int(int* p,int val,int rep) {int i;  FOR(i,rep) p[i]=val;}
+#define FILL_INT(a,val) _fill_int((int*)a,val,sizeof(a)/4)
+#define MINUS(a) _fill_int((int*)a,-1,sizeof(a)/4)
+ll GETi() { ll i;scanf("%lld",&i); return i;}
+//-------------------------------------------------------
+
+int C,D,N,M,K;
+
+void solve() {
+    int f,i,j,k,l,x,y;
+    cin>>C>>D>>N>>M>>K;
+    
+    int mi=100000;
+    FOR(x,10001) FOR(y,10001) if(x*N+y+K>=N*M) mi=min(mi,x*C+y*D);
+    cout << mi << endl;
+}
+
+
+int main(int argc,char** argv){
+    string s;
+    if(argc==1) ios::sync_with_stdio(false);
+    for(int i=1;i<argc;i++) s+=argv[i],s+='\n';
+    for(int i=s.size()-1;i>=0;i--) ungetc(s[i],stdin);
+    solve();
+    return 0;
+}
