0296cbb467fcbc6381e6f51904721576920e19a8
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 23:41:57 +0530

    kmjp

---
+++ kmjp/normal/1005/A.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef signed long long ll;
+
+#undef _P
+#define _P(...) (void)printf(__VA_ARGS__)
+#define FOR(x,to) for(x=0;x<(to);x++)
+#define FORR(x,arr) for(auto& x:arr)
+#define ITR(x,c) for(__typeof(c.begin()) x=c.begin();x!=c.end();x++)
+#define ALL(a) (a.begin()),(a.end())
+#define ZERO(a) memset(a,0,sizeof(a))
+#define MINUS(a) memset(a,0xff,sizeof(a))
+//-------------------------------------------------------
+
+int N;
+vector<int> S;
+void solve() {
+	int i,j,k,l,r,x,y; string s;
+	
+	cin>>N;
+	int nex=1;
+	FOR(i,N) {
+		cin>>x;
+		if(x!=nex) {
+			S.push_back(nex-1);
+		}
+		nex=x+1;
+	}
+	S.push_back(nex-1);
+	
+	cout<<S.size()<<endl;
+	FORR(s,S) cout<<s<<" ";
+	
+}
+
+
+int main(int argc,char** argv){
+	string s;int i;
+	if(argc==1) ios::sync_with_stdio(false), cin.tie(0);
+	FOR(i,argc-1) s+=argv[i+1],s+='\n'; FOR(i,s.size()) ungetc(s[s.size()-1-i],stdin);
+	cout.tie(0); solve(); return 0;
+}
