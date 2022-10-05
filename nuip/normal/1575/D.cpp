e8ad5e614ce7e1e7885e80a95ed7e46f3dfb292d
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 05:28:45 +0530

    nuip

---
+++ nuip/normal/1575/D.cpp
@@ -0,0 +1,235 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define inf 1000000007
+#define MP make_pair
+#define MT make_tuple
+#define PB push_back
+#define fi first
+#define se second
+#define rep(i,n) for(int i = 0; i < (int)(n); ++i)
+#define rrep(i,n) for(int i = (int)n-1; i >= 0; --i)
+#define srep(i,a,b) for(int i = (int)a; i < (int)(b); ++i)
+#define all(x) (x).begin(),(x).end()
+#define SUM(v) accumulate(all(v), 0LL)
+#define MIN(v) *min_element(all(v))
+#define MAX(v) *max_element(all(v))
+#define lb(c, x) distance((c).begin(), lower_bound(all(c), (x)))
+#define ub(c, x) distance((c).begin(), upper_bound(all(c), (x)))
+#define UNIQUE(x) sort(all(x)), x.erase(unique(all(x)), x.end())
+#define SZ(c) (int)(c).size()
+template<typename T>
+ostream& operator << (ostream& os, vector<T>& vec) {
+    os << "{";
+    for (int i = 0; i<(int)vec.size(); i++) {
+        os << vec[i] << (i + 1 == (int)vec.size() ? "" : ", ");
+    }
+    os << "}";
+    return os;
+}
+// pair
+template<typename T, typename U>
+ostream& operator << (ostream& os, pair<T, U> pair_var) {
+    os << "(" << pair_var.first << ", " << pair_var.second << ")";
+    return os;
+}
+// map
+template<typename T, typename U>
+ostream& operator << (ostream& os, map<T, U>& map_var) {
+    os << "{";
+    for(auto itr = map_var.begin(); itr != map_var.end(); itr++){
+        os << "(" << itr->first << ", " << itr->second << ")";
+        itr++;
+        if(itr != map_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+// set 
+template<typename T>
+ostream& operator << (ostream& os, set<T>& set_var) {
+    os << "{";
+    for(auto itr = set_var.begin(); itr != set_var.end(); itr++){
+        os << (*itr);
+        ++itr;
+        if(itr != set_var.end()) os << ", ";
+        itr--;
+    }
+    os << "}";
+    return os;
+}
+#define overload2(_1, _2, name, ...) name
+#define vec(type, name, ...) vector<type> name(__VA_ARGS__)
+#define VEC(type, name, size)                                                                                                                                  \
+    vector<type> name(size);                                                                                                                                   \
+    IN(name)
+#define vv(type, name, h, ...) vector<vector<type>> name(h, vector<type>(__VA_ARGS__))
+#define VV(type, name, h, w)                                                                                                                                   \
+    vector<vector<type>> name(h, vector<type>(w));                                                                                                             \
+    IN(name)
+#define vvv(type, name, h, w, ...) vector<vector<vector<type>>> name(h, vector<vector<type>>(w, vector<type>(__VA_ARGS__)))
+#define vvvv(type, name, a, b, c, ...)                                                                                                                         \
+    vector<vector<vector<vector<type>>>> name(a, vector<vector<vector<type>>>(b, vector<vector<type>>(c, vector<type>(__VA_ARGS__))))
+#define INT(...)                                                                                                                                               \
+    int __VA_ARGS__;                                                                                                                                           \
+    IN(__VA_ARGS__)
+#define LL(...)                                                                                                                                                \
+    ll __VA_ARGS__;                                                                                                                                            \
+    IN(__VA_ARGS__)
+#define STR(...)                                                                                                                                               \
+    string __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+#define CHR(...)                                                                                                                                               \
+    char __VA_ARGS__;                                                                                                                                          \
+    IN(__VA_ARGS__)
+#define DBL(...)                                                                                                                                               \
+    double __VA_ARGS__;                                                                                                                                        \
+    IN(__VA_ARGS__)
+int scan() { return getchar(); }
+void scan(int &a) { cin >> a; }
+void scan(long long &a) { cin >> a; }
+void scan(char &a) { cin >> a; }
+void scan(double &a) { cin >> a; }
+void scan(string &a) { cin >> a; }
+template <class T, class S> void scan(pair<T, S> &p) { scan(p.first), scan(p.second); }
+template <class T> void scan(vector<T> &);
+template <class T> void scan(vector<T> &a) {
+    for(auto &i : a) scan(i);
+}
+template <class T> void scan(T &a) { cin >> a; }
+void IN() {}
+template <class Head, class... Tail> void IN(Head &head, Tail &...tail) {
+    scan(head);
+    IN(tail...);
+}
+const string YESNO[2] = {"NO", "YES"};
+const string YesNo[2] = {"No", "Yes"};
+const string yesno[2] = {"no", "yes"};
+void YES(bool t = 1) { cout << YESNO[t] << endl; }
+void NO(bool t = 1) { YES(!t); }
+void Yes(bool t = 1) { cout << YesNo[t] << endl; }
+void No(bool t = 1) { Yes(!t); }
+void yes(bool t = 1) { cout << yesno[t] << endl; }
+void no(bool t = 1) { yes(!t); }
+#ifdef LOCAL
+void debug_out() { cerr << endl; }
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+    cerr << " " << H;
+    debug_out(T...);
+}
+#define dbg(...) \
+    cerr << __LINE__ << " [" << #__VA_ARGS__ << "]:", debug_out(__VA_ARGS__)
+#define dump(x) cerr << __LINE__ << " " << #x << " = " << (x) << endl
+#else
+#define dbg(...) (void(0))
+#define dump(x) (void(0))
+#endif
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){
+    std::fill( (T*)array, (T*)(array+N), val );
+}
+template <class T> T ceil(T x, T y) {assert(y >= 1);return (x > 0 ? (x + y - 1) / y : x / y);}
+template <class T> T floor(T x, T y) {assert(y >= 1);return (x > 0 ? x / y : (x + y - 1) / y);}
+vector<int> iota(int n) {vector<int> a(n);iota(all(a), 0);return a;}
+template <class T> T POW(T x, int n) {T res = 1;for(; n; n >>= 1, x *= x){if(n & 1) res *= x;}return res;}
+ll pow2(int i) { return 1LL << i; }
+int topbit(signed t) { return t == 0 ? -1 : 31 - __builtin_clz(t); }
+int topbit(ll t) { return t == 0 ? -1 : 63 - __builtin_clzll(t); }
+int lowbit(signed a) { return a == 0 ? 32 : __builtin_ctz(a); }
+int lowbit(ll a) { return a == 0 ? 64 : __builtin_ctzll(a); }
+// int allbit(int n) { return (1 << n) - 1; }
+ll allbit(ll n) { return (1LL << n) - 1; }
+int popcount(signed t) { return __builtin_popcount(t); }
+int popcount(ll t) { return __builtin_popcountll(t); }
+bool ispow2(int i) { return i && (i & -i) == i; }
+
+
+template <class S> void fold_in(vector<S> &v) {}
+template <typename Head, typename... Tail, class S> void fold_in(vector<S> &v, Head &&a, Tail &&...tail) {
+    for(auto e : a) v.emplace_back(e);
+    fold_in(v, tail...);
+}
+template <class S> void renumber(vector<S> &v) {}
+template <typename Head, typename... Tail, class S> void renumber(vector<S> &v, Head &&a, Tail &&...tail) {
+    for(auto &&e : a) e = lb(v, e);
+    renumber(v, tail...);
+}
+template <class S, class... Args> void zip(vector<S> &head, Args &&...args) {
+    vector<S> v;
+    fold_in(v, head, args...);
+    sort(all(v)), v.erase(unique(all(v)), v.end());
+    renumber(v, head, args...);
+}
+template<class T> inline bool chmax(T &a, T b){
+    if(a<b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+template<class T> inline bool chmin(T &a, T b){
+    if(a>b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+ll dp[10][26][11];
+int main(){
+    STR(s);
+    int n = s.size();
+    if(n>1&&s[0]=='0'){
+        cout << 0 << endl;
+        return 0;
+    }
+    if(n==1){
+        if(s[0]=='0'||s[0]=='_'||s[0]=='X'){
+            cout << 1 << endl;
+        }else{
+            cout << 0 << endl;
+        }
+        return 0;
+    }
+    dp[0][0][10] = 1;
+    rep(i,n){
+        if(s[i]=='_'){
+            rep(k,10){
+                if(i==0&&k==0)continue;
+                rep(S,25){
+                    rep(x,11){
+                        dp[i+1][(S*10+k)%25][x] += dp[i][S][x];
+                    }
+                }
+            }
+        }else if(s[i]=='X'){
+            rep(S,25){
+                rep(x,11){
+                    if(x!=10){
+                        dp[i+1][(S*10+x)%25][x] += dp[i][S][x];
+                    }else{
+                        rep(k,10){
+                            if(i==0&&k==0)continue;    
+                            dp[i+1][(S*10+k)%25][k] += dp[i][S][x];
+                        }    
+                    }
+                }
+            }
+        }else{
+            int k = s[i]-'0';
+            rep(S,25){
+                rep(x,11){
+                    dp[i+1][(S*10+k)%25][x] += dp[i][S][x];
+                }
+            }
+        }
+    }
+    ll res = 0;
+    rep(i,11){
+        res += dp[n][0][i];
+    }
+    cout << res << endl;
+    return 0;
+}
