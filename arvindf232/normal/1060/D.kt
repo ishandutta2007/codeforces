039b8d74b20c9a390f1657d6fa7a0540b702f4e9
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 21:26:51 +0530

    arvindf232

---
+++ arvindf232/normal/1060/D.kt
@@ -0,0 +1,122 @@
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.PrintWriter
+import kotlin.system.measureTimeMillis
+object IO{
+	private const val BS = 1 shl 16
+	private const val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+
+	var warningActive = true
+	var fakein = StringBuilder()
+
+	private var IN: BufferedInputStream = BufferedInputStream(System.`in`, BS)
+	val OUT: PrintWriter = PrintWriter(System.out)
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = IN.read(buf) // no need for checked exceptions
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.append(c)
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.append(c)
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+fun put(aa:Any){ IO.OUT.println(aa)}
+fun done(){ IO.OUT.close() }
+val getint:Int get() = IO.nextInt()
+val getlong:Long get() = IO.nextLong()
+val getstr:String get() = IO.nextString()
+fun getline(n:Int):IntArray{
+	return IntArray(n){getint}
+}
+fun getlineL(n:Int):LongArray{
+	return LongArray(n){getlong}
+}
+fun main(){
+	repeat(1){
+		val n = getint
+		val left = mutableListOf<Int>()
+		val right = mutableListOf<Int>()
+		var atleast = 0L
+		repeat(n){
+			left.add(getint)
+			right.add(getint)
+			atleast += left.last()
+			atleast += right.last()
+		}
+		left.sort()
+		right.sort()
+		for(i in n-1 downTo 0){
+			atleast -= minOf(left[i],right[i])
+		}
+		put(atleast + n)
+	}
+	done()
+}
+/*
+
+
+4  3
+1 2
+
+
+7 8 9 2
+6 5 4 1
+
+
+DIDN"T SEE MULTIPLES CIRCLES
+
+
+
+ */
