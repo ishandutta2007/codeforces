d8cc2ced7fb38ef74c553a0209db1ddbe7f5e0b9
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 21:11:34 +0530

    arvindf232

---
+++ arvindf232/normal/1324/E.kt
@@ -0,0 +1,111 @@
+import java.io.BufferedInputStream
+import java.io.File
+import java.io.PrintWriter
+import kotlin.system.measureTimeMillis
+object IO{
+	private const val BS = 1 shl 16
+	private const val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+
+	var warningActive = true
+	var fakein = StringBuilder()
+
+	private var IN: BufferedInputStream = BufferedInputStream(System.`in`, BS)
+	val OUT: PrintWriter = PrintWriter(System.out)
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = IN.read(buf) // no need for checked exceptions
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.append(c)
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.append(c)
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+fun put(aa:Any){ IO.OUT.println(aa)}
+fun done(){ IO.OUT.close() }
+val getint:Int get() = IO.nextInt()
+val getlong:Long get() = IO.nextLong()
+val getstr:String get() = IO.nextString()
+fun getline(n:Int):IntArray{
+	return IntArray(n){getint}
+}
+fun getlineL(n:Int):LongArray{
+	return LongArray(n){getlong}
+}
+fun main(){
+	repeat(1){ _ ->
+		val n = getint
+		val h = getint
+		val l = getint
+		val r = getint
+		val A = getline(n)
+
+		val DP = Array(n+1){IntArray(h){-100000727} }
+		DP[0][0] = 0
+		for(p in 0 until n){
+			for(hn in 0 until h){
+				val new1pre = (hn + A[p]) % h
+				val new2pre = (hn + A[p] + h - 1 ) % h
+				DP[p+1][new1pre] = maxOf(DP[p+1][new1pre],DP[p][hn] + if(new1pre in l..r) 1 else 0)
+				DP[p+1][new2pre] = maxOf(DP[p+1][new2pre],DP[p][hn] + if(new2pre in l..r) 1 else 0)
+			}
+		}
+		put(DP[n].maxOrNull()!!)
+
+
+
+
+	}
+	done()
+}
