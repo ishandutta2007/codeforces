4a656c94b13726944a369e71d2ce210104363514
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:08:50 +0530

    arvindf232

---
+++ arvindf232/normal/1571/C.kt
@@ -0,0 +1,259 @@
+import java.io.BufferedInputStream
+import java.io.PrintWriter
+import kotlin.math.max
+import kotlin.system.measureTimeMillis
+
+inline fun TIME(f:()->Unit){
+	val t = measureTimeMillis(){
+		f()
+	}
+	println(t)
+}
+
+object IO{
+	private const val BS = 1 shl 16
+	private const val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+
+	var warningActive = true
+	var fakein = StringBuilder()
+
+	private var IN: BufferedInputStream = BufferedInputStream(System.`in`, BS)
+	val OUT: PrintWriter = PrintWriter(System.out)
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = IN.read(buf) // no need for checked exceptions
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.append(c)
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.append(c)
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+	fun rerouteInput(){
+		if(warningActive){
+			System.err.println("Not accepting inputs!")
+			warningActive = false
+		}
+		IN = BufferedInputStream(fakein.toString().byteInputStream(),BS)
+	}
+
+//    init {
+//        IN = BufferedInputStream(System.`in`, BS)
+//    }
+}
+//class reading{
+//    companion object{
+//        var jin = FastScanner()
+//        var pw = PrintWriter(System.out)
+//    }
+//}
+fun put(aa:Any){ IO.OUT.println(aa)}
+fun done(){ IO.OUT.close() }
+fun share(aa:Any){
+	if(aa is IntArray){IO.fakein.append(aa.joinToString(" "))}
+	else if(aa is LongArray){IO.fakein.append(aa.joinToString(" "))}
+	else if(aa is List<*>){IO.fakein.append(aa.toString())}
+	else{IO.fakein.append(aa.toString())}
+	IO.fakein.append("\n")
+}
+
+fun getint():Int{ return IO.nextInt() }
+fun getlong():Long{ return IO.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{IO.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{IO.nextLong()} }
+fun getstr():String{ return IO.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+inline fun cases(ask:()->Unit){ val t = getint();repeat(t){ ask() }}
+
+val List<Char>.ret:String
+	get() = this.joinToString("")
+infix fun Any.dei(a:Any){
+	//does not stand for anything it is just easy to type, have to be infix because kotlin does not have custom prefix operators
+	var str = ""
+	if(this is String){ str = this
+	}else if(this is Int){ str = this.toString()
+	}else if(this is Long){ str = this.toString()
+	}
+	if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+	}else{ println("$str : $a")
+	}
+}
+val just = " " // usage: just dei x , where x is the debug variable
+fun crash(){throw Exception("Bad programme")} // because assertion does not work
+fun assert(a:Boolean){if(!a){throw Exception("Failed Assertion")}}
+enum class solveMode {
+	real, rand, tc
+}
+object solve{
+	var mode:solveMode = solveMode.real
+	var tcNum:Int = 0
+	var rand:()->Unit = {}
+	var TC:MutableMap<Int,()->Unit> = mutableMapOf()
+	var answersChecked = 0
+	inline fun cases(a:solve.()->Unit){
+		val t = if(mode == solveMode.real){if(singleCase) 1 else getint()} else if(mode == solveMode.tc){1 } else randCount
+		repeat(t){
+			if(mode == solveMode.tc){
+				TC[tcNum]?.let { it() }
+				IO.rerouteInput()
+			}else if(mode == solveMode.rand){
+				rand()
+				IO.rerouteInput()
+			}
+			currentAnswer = null
+			currentBruteAnswer = null
+			a()
+		}
+		if(withBruteForce){
+			put("Checked ${answersChecked}")
+		}
+	}
+	inline fun singleCase(a:solve.()->Unit){
+		val t = if(mode != solveMode.rand){1} else randCount
+		repeat(t) { a() }
+	}
+	fun rand(a:()->Unit){
+		this.rand = a
+	}
+	fun tc(id:Int = 0,a:()->Unit){
+		TC[id] = a
+	}
+	inline fun brute(a:()->Unit){
+		if(withBruteForce){
+			a()
+		}
+	}
+	fun usetc(a:Int = 0 ){
+		this.tcNum = a
+		this.mode = solveMode.tc
+	}
+	fun userand(){
+		this.mode = solveMode.rand
+	}
+
+
+	var currentAnswer:String? = null
+	var currentBruteAnswer:String? = null
+	fun answer(a:Any){
+		currentAnswer = a.toString()
+		if(currentBruteAnswer != null){
+			checkAnswer()
+		}
+	}
+	fun put2(a:Any){answer(a);put(a) }
+
+	fun bruteAnswer(a:Any){
+		currentBruteAnswer = a.toString()
+		if(currentAnswer != null){
+			checkAnswer()
+		}
+	}
+	fun checkAnswer(){
+		if(currentAnswer != currentBruteAnswer){
+			throw Exception("Failed Test: BF $currentBruteAnswer Current $currentAnswer")
+		}
+		answersChecked ++
+	}
+}
+fun Int2(a:Int,b:Int) = List(a){IntArray(b)}
+fun Int3(a:Int,b:Int,c:Int) = List(a){List(b){IntArray(c)}}
+const val p = 1000000007L
+const val pI = p.toInt()
+fun Int.adjust():Int{ if(this >= pI){ return this  - pI }else if (this < 0){ return this + pI };return this }
+fun IntArray.put(i:Int,v:Int){ this[i] = (this[i] + v).adjust() }
+
+const val singleCase = false
+const val withBruteForce = false
+const val randCount = 100
+fun main(){
+	solve.cases{
+		val q = getint()
+		var atleast = 0
+		var atmost = Int.MAX_VALUE
+
+		repeat(q){
+			val s1 = getstr()
+			val s2 = getstr()
+			val mark = getint()
+
+			var maxRhyme = 0
+			for(i in 0 until minOf(s1.length,s2.length)){
+				if(s1[s1.length - 1 - i] == s2[s2.length - 1 - i]) maxRhyme ++
+				else break
+			}
+			if(mark == 1){
+				atmost = minOf(atmost, maxRhyme)
+			}else{
+				atleast = maxOf(maxRhyme + 1, atleast)
+			}
+
+		}
+//		1 dei atmost
+//		2 dei atleast
+		if(atmost < atleast){
+			put(0)
+		}else{
+			put(atmost - atleast + 1 )
+			for(i in atleast.. atmost){
+				put(i)
+			}
+		}
+
+
+	}
+	done()
+}
---
+++ arvindf232/normal/1571/D.kt
@@ -0,0 +1,279 @@
+import java.io.BufferedInputStream
+import java.io.PrintWriter
+import kotlin.system.measureTimeMillis
+
+inline fun TIME(f:()->Unit){
+	val t = measureTimeMillis(){
+		f()
+	}
+	println(t)
+}
+
+object IO{
+	private const val BS = 1 shl 16
+	private const val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+
+	var warningActive = true
+	var fakein = StringBuilder()
+
+	private var IN: BufferedInputStream = BufferedInputStream(System.`in`, BS)
+	val OUT: PrintWriter = PrintWriter(System.out)
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = IN.read(buf) // no need for checked exceptions
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c in '0'..'9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.append(c)
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.append(c)
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+	fun rerouteInput(){
+		if(warningActive){
+			System.err.println("Not accepting inputs!")
+			warningActive = false
+		}
+		IN = BufferedInputStream(fakein.toString().byteInputStream(),BS)
+	}
+
+//    init {
+//        IN = BufferedInputStream(System.`in`, BS)
+//    }
+}
+//class reading{
+//    companion object{
+//        var jin = FastScanner()
+//        var pw = PrintWriter(System.out)
+//    }
+//}
+fun put(aa:Any){ IO.OUT.println(aa)}
+fun done(){ IO.OUT.close() }
+fun share(aa:Any){
+	if(aa is IntArray){IO.fakein.append(aa.joinToString(" "))}
+	else if(aa is LongArray){IO.fakein.append(aa.joinToString(" "))}
+	else if(aa is List<*>){IO.fakein.append(aa.toString())}
+	else{IO.fakein.append(aa.toString())}
+	IO.fakein.append("\n")
+}
+
+fun getint():Int{ return IO.nextInt() }
+fun getlong():Long{ return IO.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{IO.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{IO.nextLong()} }
+fun getstr():String{ return IO.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+inline fun cases(ask:()->Unit){ val t = getint();repeat(t){ ask() }}
+
+val List<Char>.ret:String
+	get() = this.joinToString("")
+infix fun Any.dei(a:Any){
+	//does not stand for anything it is just easy to type, have to be infix because kotlin does not have custom prefix operators
+	var str = ""
+	if(this is String){ str = this
+	}else if(this is Int){ str = this.toString()
+	}else if(this is Long){ str = this.toString()
+	}
+	if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+	}else{ println("$str : $a")
+	}
+}
+val just = " " // usage: just dei x , where x is the debug variable
+fun crash(){throw Exception("Bad programme")} // because assertion does not work
+fun assert(a:Boolean){if(!a){throw Exception("Failed Assertion")}}
+enum class solveMode {
+	real, rand, tc
+}
+object solve{
+	var mode:solveMode = solveMode.real
+	var tcNum:Int = 0
+	var rand:()->Unit = {}
+	var TC:MutableMap<Int,()->Unit> = mutableMapOf()
+	var answersChecked = 0
+	inline fun cases(a:solve.()->Unit){
+		val t = if(mode == solveMode.real){if(singleCase) 1 else getint()} else if(mode == solveMode.tc){1 } else randCount
+		repeat(t){
+			if(mode == solveMode.tc){
+				TC[tcNum]?.let { it() }
+				IO.rerouteInput()
+			}else if(mode == solveMode.rand){
+				rand()
+				IO.rerouteInput()
+			}
+			currentAnswer = null
+			currentBruteAnswer = null
+			a()
+		}
+		if(withBruteForce){
+			put("Checked ${answersChecked}")
+		}
+	}
+	inline fun singleCase(a:solve.()->Unit){
+		val t = if(mode != solveMode.rand){1} else randCount
+		repeat(t) { a() }
+	}
+	fun rand(a:()->Unit){
+		this.rand = a
+	}
+	fun tc(id:Int = 0,a:()->Unit){
+		TC[id] = a
+	}
+	inline fun brute(a:()->Unit){
+		if(withBruteForce){
+			a()
+		}
+	}
+	fun usetc(a:Int = 0 ){
+		this.tcNum = a
+		this.mode = solveMode.tc
+	}
+	fun userand(){
+		this.mode = solveMode.rand
+	}
+
+
+	var currentAnswer:String? = null
+	var currentBruteAnswer:String? = null
+	fun answer(a:Any){
+		currentAnswer = a.toString()
+		if(currentBruteAnswer != null){
+			checkAnswer()
+		}
+	}
+	fun put2(a:Any){answer(a);put(a) }
+
+	fun bruteAnswer(a:Any){
+		currentBruteAnswer = a.toString()
+		if(currentAnswer != null){
+			checkAnswer()
+		}
+	}
+	fun checkAnswer(){
+		if(currentAnswer != currentBruteAnswer){
+			throw Exception("Failed Test: BF $currentBruteAnswer Current $currentAnswer")
+		}
+		answersChecked ++
+	}
+}
+fun Int2(a:Int,b:Int) = List(a){IntArray(b)}
+fun Int3(a:Int,b:Int,c:Int) = List(a){List(b){IntArray(c)}}
+const val p = 1000000007L
+const val pI = p.toInt()
+fun Int.adjust():Int{ if(this >= pI){ return this  - pI }else if (this < 0){ return this + pI };return this }
+fun IntArray.put(i:Int,v:Int){ this[i] = (this[i] + v).adjust() }
+
+const val singleCase = true
+const val withBruteForce = false
+const val randCount = 100
+
+
+class answers(val A:List<IntArray>,val myA:Int,val myB:Int){
+	val n = A.size
+	val rowSum = IntArray(n)
+	val columnSum = IntArray(n)
+
+
+	init{
+		for(i in 0 until n){
+			for(j in 0 until n){
+				rowSum[i] += A[i][j]
+				columnSum[j] += A[i][j]
+			}
+		}
+	}
+	fun resultfor(a:Int,b:Int):Int{
+		val rank = if(a == myA) 0 else 1  + if(myB == b) 0 else 1
+		val correct = A[a][b]
+		val partial = rowSum[a] + columnSum[b] - correct
+		if(rank == 0) return 1
+		else if(rank == 1) return correct + 1
+		else return partial + 1
+	}
+	fun worstCase():Int{
+		var bad = 0
+		for(i in 0 until n){
+			for(j in 0 until n){
+				if( i == j) continue
+				val result = resultfor(i,j)
+				bad = maxOf(result,bad)
+			}
+		}
+		return bad
+	}
+}
+fun main(){
+	solve.cases{
+		val n = getint()
+		val m = getint()
+		val A = List(n){IntArray(n)}
+		var myA = -1
+		var myB = - 1
+		repeat(m){
+			val x = getint() -1
+			val y = getint() - 1
+			if(it == 0){
+				myA = x
+				myB = y
+			}
+			A[x][y] ++
+		}
+		val data = answers(A,myA,myB)
+		put(data.worstCase())
+
+
+	}
+	done()
+}
