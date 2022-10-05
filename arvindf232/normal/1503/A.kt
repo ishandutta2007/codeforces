6213b7e6fca4668cca9a7624affd757845e1df25
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:15:52 +0530

    arvindf232

---
+++ arvindf232/normal/1503/A.kt
@@ -0,0 +1,181 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import kotlin.math.min
+import kotlin.math.max
+import kotlin.reflect.KProperty0
+
+class FastScanner {
+	private val BS = 1 shl 16
+	private val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+	private var `in`: BufferedInputStream? = null
+
+	constructor() {
+		`in` = BufferedInputStream(System.`in`, BS)
+	}
+
+	private val char: Char
+		get() {
+			while (bId == size) {
+				size = try {
+					`in`!!.read(buf)
+				} catch (e: Exception) {
+					return NC
+				}
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.appendCodePoint(c.toInt())
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.appendCodePoint(c.toInt());
+
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+class reading{
+	companion object{
+		var jin = FastScanner()
+		var pw = PrintWriter(System.out)
+	}
+}
+fun put(aa:Any){ reading.pw.println(aa)}
+fun done(){ reading.pw.close() }
+
+fun getint():Int{ return reading.jin.nextInt() }
+fun getlong():Long{ return reading.jin.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getstr():String{ return reading.jin.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+inline fun cases(ask:()->Unit){ val t = getint();repeat(t){ ask() }}
+
+val List<Char>.ret:String
+	get() = this.joinToString("")
+infix fun Any.dei(a:Any){
+	//does not stand for anything it is just easy to type
+	var str = ""
+	if(this is String){ str = this
+	}else if(this is Int){ str = this.toString()
+	}else if(this is Long){ str = this.toString()
+	}
+	if(a is List<*>){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is IntArray){ println("$str : ${a.joinToString(" ")}")
+	}else if(a is LongArray){ println("$str : ${a.joinToString(" ")}")
+	}else{ println("$str : $a")
+	}
+}
+val just = " " // usage: just dei x , where x is the debug variable
+
+fun main() {
+	cases{
+		val n = getint()
+		val st = getstr()
+
+		val count1 = st.count{it == '1'}
+		val count0 = st.count{it == '0'}
+		val goodends = st[0] == '1' && st.last() == '1'
+
+//		just dei st
+//		just dei count1
+//		2 dei count0
+//		3 dei goodends
+		if(count1 % 2 == 0 && count0 % 2 == 0 && goodends){
+			var ret1 = mutableListOf<Char>()
+			var ret2 = mutableListOf<Char>()
+			ret1.add('(')
+			ret2.add('(')
+			var currentState = 1
+			for(i in 1 until st.length - 1){
+				val c = st[i] == '1'
+				if(c){
+					if(currentState == 1 || currentState == 3){
+						ret1.add('(')
+						ret2.add('(')
+						currentState += 1
+					}else{
+						ret1.add(')')
+						ret2.add(')')
+						currentState -= 1
+					}
+				}else{
+					if(currentState == 1 || currentState == 2){
+						ret1.add('(')
+						ret2.add(')')
+						currentState += 2
+					}else{
+						ret1.add(')')
+						ret2.add('(')
+						currentState -= 2
+					}
+				}
+			}
+			ret1.add(')')
+			ret2.add(')')
+
+			put("YES")
+			put(ret1.ret)
+			put(ret2.ret)
+			return@cases
+
+		}else{
+			put("NO")
+			return@cases
+		}
+
+	}
+	done()
+    // Write your solution here
+}
