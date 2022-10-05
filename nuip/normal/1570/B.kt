5c79baea449a8ab1459637f6dc6dcbbe9a76ce58
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 05:27:20 +0530

    nuip

---
+++ nuip/normal/1570/B.kt
@@ -0,0 +1,95 @@
+import java.io.PrintWriter
+import java.lang.Integer.min
+import java.util.*
+
+val pw = PrintWriter(System.out)
+fun main() {
+    _writer.solve(); _writer.flush()
+}
+
+fun PrintWriter.solve() {
+    var a0=readInt()
+    var a1=readInt()
+    var b0=readInt()
+    var b1=readInt()
+    val n=readInt()
+    if(b0>b1) {
+        a0=a1.also{a1=a0}
+        b0=b1.also{b1=b0}
+    }
+    val max=run{
+        var re=0
+        var rem=n
+        val t0=min(rem/b0,a0)
+        re+=t0
+        rem-=b0*t0
+        val t1=min(rem/b1,a1)
+        re+t1
+    }
+    var min= kotlin.math.max(0,n-a0*(b0-1)-a1*(b1-1))
+    println("$min $max")
+}
+
+@JvmField
+val INPUT = System.`in`
+
+@JvmField
+val OUTPUT = System.out
+
+@JvmField
+val _reader = INPUT.bufferedReader()
+fun readLine(): String? = _reader.readLine()
+fun readLn() = _reader.readLine()!!
+
+@JvmField
+var _tokenizer: StringTokenizer = StringTokenizer("")
+fun _read(): String {
+    while (_tokenizer.hasMoreTokens().not()) _tokenizer = StringTokenizer(_reader.readLine() ?: return "", " ")
+    return _tokenizer.nextToken()
+}
+
+fun readInt() = _read().toInt()
+fun readDouble() = _read().toDouble()
+fun readLong() = _read().toLong()
+fun readString() = _read()
+fun readVString(n: Int) = List(n) { _read() }
+fun readLines(n: Int) = List(n) { readLn() }
+fun readVInt(n: Int) = List(n) { _read().toInt() }
+fun readAInt(n: Int) = IntArray(n) { _read().toInt() }
+fun readVDouble(n: Int) = List(n) { _read().toDouble() }
+fun readADouble(n: Int) = DoubleArray(n) { _read().toDouble() }
+fun readVLong(n: Int) = List(n) { _read().toLong() }
+fun readALong(n: Int) = LongArray(n) { _read().toLong() }
+fun read(d: Int) = readInt() - d
+fun read(d: Long) = readInt() - d
+fun readV(n: Int, d: Int) = readVInt(n).map { x -> x - d }
+fun readV(n: Int, d: Long) = readVLong(n).map { x -> x - d }
+fun readA(n: Int, d: Int) = readAInt(n).map { x -> x - d }
+fun readA(n: Int, d: Long) = readALong(n).map { x -> x - d }
+
+@JvmField
+val _writer = PrintWriter(OUTPUT, false)
+inline fun output(block: PrintWriter.() -> Unit) {
+    _writer.apply(block).flush()
+}
+
+// old
+//fun next() = readLine()!!
+//fun nint() = next().toInt()
+//fun nlong() = next().toLong()
+//fun ndouble() = next().toDouble()
+//fun nlstring() = next().split(" ")
+//fun nlint() = nlstring().map { it.toInt() }
+//fun nllong() = nlstring().map { it.toLong() }
+//fun nldouble() = nlstring().map { it.toDouble() }
+//fun reads() = next()
+//fun readi() = nint()
+//fun readl() = nlong()
+//fun readd() = ndouble()
+//fun readVs() = nlstring()
+//fun readVi() = nlint()
+//fun readVl() = nllong()
+//fun readVd() = nldouble()
+//
+//fun read() = readl()
+//fun readV() = readVl()
