b36ec7640778776fadc0b88264629aaebe457916
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:20:03 +0530

    arvindf232

---
+++ arvindf232/normal/1010/B.kt
@@ -0,0 +1,174 @@
+/**
+ * Accomplished using the EduTools plugin by JetBrains https://plugins.jetbrains.com/plugin/10081-edutools
+ */
+import java.io.BufferedInputStream
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.PrintWriter
+import kotlin.coroutines.coroutineContext
+import kotlin.system.exitProcess
+
+class FastScanner {
+	private val BS = 1 shl 16
+	private val NC = 0.toChar()
+	private val buf = ByteArray(BS)
+	private var bId = 0
+	private var size = 0
+	private var c = NC
+	private var `in`: BufferedInputStream? = null
+
+	constructor() {
+		`in` = BufferedInputStream(System.`in`, BS)
+	}
+
+	private val char: Char
+		private get() {
+			while (bId == size) {
+				size = try {
+					`in`!!.read(buf)
+				} catch (e: Exception) {
+					return NC
+				}
+				if (size == -1) return NC
+				bId = 0
+			}
+			return buf[bId++].toChar()
+		}
+
+	fun nextInt(): Int {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextLong(): Long {
+		var neg = false
+		if (c == NC) c = char
+		while (c < '0' || c > '9') {
+			if (c == '-') neg = true
+			c = char
+		}
+		var res = 0L
+		while (c >= '0' && c <= '9') {
+			res = (res shl 3) + (res shl 1) + (c - '0')
+			c = char
+		}
+		return if (neg) -res else res
+	}
+	fun nextString():String{
+		if (c == NC) c = char
+		val ret = StringBuilder()
+		while (true){
+			c = char
+			if(!isWhitespace(c)){ break}
+		}
+		ret.appendCodePoint(c.toInt())
+		while (true){
+			c = char
+			if(isWhitespace(c)){ break}
+			ret.appendCodePoint(c.toInt());
+
+		}
+		return ret.toString()
+	}
+	fun isWhitespace(c:Char):Boolean{
+		return c == ' ' || c == '\n' || c == '\r' || c == '\t'
+	}
+}
+class reading{
+	companion object{
+		var jin = FastScanner()
+		var pw = PrintWriter(System.out)
+	}
+}
+fun put(aa:Any){
+	reading.pw.println(aa)
+}
+fun done(){
+	reading.pw.close()
+}
+
+fun getint():Int{ return reading.jin.nextInt() }
+fun getlong():Long{ return reading.jin.nextLong() }
+fun getline(n:Int):List<Int>{ return (1..n).map{reading.jin.nextInt()} }
+fun getlineL(n:Int):List<Long>{return (1..n).map{reading.jin.nextLong()} }
+fun getstr():String{ return reading.jin.nextString() }
+fun MutableList<Int>.streamint(n:Int){ repeat(n){this.add(getint())}}
+fun MutableList<Long>.streamlong(n:Int){ repeat(n){this.add(getlong())}}
+
+
+inline fun cases(ask:()->Unit){
+	val t = getint()
+	repeat(t){
+		ask()
+	}
+}
+fun just(a:Boolean){
+	put(if(a)"YES" else "NO")
+}
+fun List<Int>.out(){ put(this.joinToString(" ")) }
+val List<Char>.ret:String
+	get() = this.joinToString("")
+
+inline fun BinarySearchFirstTrue(l:Int,r:Int,isTrue:(Int)->Boolean):Int? {
+	//inclusive
+	var L = l
+	var R = r
+	while(L < R) {
+		val m = (L + R) / 2
+		if(isTrue(m)) {
+			R = m
+		} else {
+			L = m + 1
+		}
+	}
+	return if(isTrue(L)) {
+		L
+	} else {
+		if(L == r) {
+			null
+		} else {
+			L + 1
+		}
+	}
+}
+fun main() {
+	val m = getint()
+	val n = getint()
+
+	val ans = BooleanArray(n) // will flip
+	var cycle = 0
+	fun ask(a:Int):Boolean{
+		println(a)
+		System.out.flush()
+		val x = getint()
+		if(x== 0 || x == -2){
+			exitProcess(0)
+		}
+		cycle += 1
+		cycle = cycle % n
+		return x == -1
+	}
+	fun modask(a:Int):Boolean{
+		val currentCycle = cycle
+		val ret = ask(a)
+		return ret xor ans[currentCycle]
+	}
+
+	repeat(n){
+		val sol = ask(1)
+		ans[it] = sol
+	}
+	val ret = BinarySearchFirstTrue(1,m){
+		modask(it)
+	}
+    // Write your solution here
+}
