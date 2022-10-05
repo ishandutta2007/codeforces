b3346ebab627a52b96cb7c3cf6f446c2719658fb
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 18:13:08 +0530

    akulyat

---
+++ akulyat/normal/1352/C.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize ("O3", "unroll-all-loops")
+#pragma GCC target ("avx2")
+
+using namespace std;
+
+#define F first
+#define S second
+typedef long long     ll;
+typedef long double   ld;
+typedef pair<ll, ll> pll; 
+
+ifstream in;
+ofstream out;
+
+const long long kk = 1000;
+const long long ml = kk * kk;
+const long long mod = ml * kk + 7;
+const long long inf = ml * ml * ml + 7; 
+
+ll n;
+bool viv = false;
+string s;
+
+
+void solve() {
+	ll n, k;
+	cin >> n >> k;
+	ll b = k / (n - 1);
+	ll ans = b * n;
+	ans += k - b * (n - 1);
+	if (ans % n == 0)
+		ans--;
+	cout << ans << '\n';
+}
+
+
+int main() {
+	// viv = true;
+	srand(time(0));
+	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+	ll t = 1; 
+	cin >> t;
+	while (t--)
+		solve();
+
+	return 0;
+}
