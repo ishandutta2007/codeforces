4723a387f15352d962f113fe7d9fadfbdee0205e
ishan dutta <ishandutta2007@gmail.com>
2022-10-05 22:39:07 +0530

    vccaiji

---
+++ vccaiji/normal/1684/B.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int a[210000];
+int b[210000];
+int main(){
+	int _;
+	cin>>_;
+	while(_--){
+		int a,b,c;
+		cin>>a>>b>>c;
+		cout<<1ll*b*c+a<<" "<<b<<" "<<c<<"\n";
+	}
+	return 0;
+}
