e4a2da45909191a24d2f12f078f713a0e7cef76c
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 03:02:44 +0530

    batrr

---
+++ batrr/normal/1505/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+#define f first
+#define s second
+#define pb push_back
+#define mp make_pair
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+
+const int N = (int)2e5 + 123, inf = 1e9, mod = 1e9 + 7;
+const ll INF = 1e18;
+
+int main()
+{
+    string s;
+    while(getline(cin, s))
+        cout << "NO\n";
+}
