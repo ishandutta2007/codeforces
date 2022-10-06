99cf4d4ecd7414e5f8dad203eae4d1b9512f87e1
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 15:50:03 +0530

    mtsd

---
+++ mtsd/normal/708/A.cpp
@@ -0,0 +1,73 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <cstdint>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+#define MP make_pair
+#define PB push_back
+#define inf 1000000007
+#define rep(i,n) for(int i = 0; i < (int)(n); ++i)
+#define all(x) (x).begin(),(x).end()
+
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){
+    std::fill( (T*)array, (T*)(array+N), val );
+}
+ 
+template<class T> inline bool chmax(T &a, T b){
+    if(a<b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template<class T> inline bool chmin(T &a, T b){
+    if(a>b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+int main(){
+    string s;
+    cin >> s;
+    int n = s.size();
+    bool f = 0;
+    rep(i,n){
+        if(s[i]!='a'){
+            f = 1;
+            s[i]--;
+        }else{
+            if(f)break;
+        }
+    }
+    if(!f){
+        s[n-1] = 'z';
+    }
+    cout << s << endl;
+    return 0;
+}
