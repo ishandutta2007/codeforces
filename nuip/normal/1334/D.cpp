daad9e5d44bb144accfb9edbda9a02627aecae4d
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 06:24:02 +0530

    nuip

---
+++ nuip/normal/1334/D.cpp
@@ -0,0 +1,92 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#include<random>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (Y);++(X))
+#define reps(X,S,Y) for (int (X) = S;(X) < (Y);++(X))
+#define rrep(X,Y) for (int (X) = (Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (Y)-1;(X) >= (S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "benri.h"
+#else
+#define out(args...)
+#endif
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+const ll MOD=1e9+7; //998244353
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	int T;
+	cin>>T;
+	while(T--){
+		int n;
+		ll l,r;
+		cin>>n>>l>>r; --l;
+		//12 13 14 .. 1n
+		//23 24 .. 2n
+		vector<int> re;
+		ll sum=0;
+		rep(i,n){
+			ll L=sum;
+			int d=2*(n-1-i);
+			ll R=L+d;
+			sum=R;
+			// out(i,L,R,1);
+			if(R<=l || r<=L) continue;
+			rep(j,d)if(l<=L+j && L+j<r){
+				if(j%2){
+					re.pb(i+1+j/2);
+				}else{
+					re.pb(i);
+				}
+			}
+		}
+		if(l<=sum && sum<r) re.pb(0);
+		for(auto x:re) cout<<x+1<<" "; cout NL;
+	}
+  return 0;
+}
