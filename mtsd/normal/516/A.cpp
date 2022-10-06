b9b14b734c5b45046b1ace0322c673eb30840b3f
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 15:48:35 +0530

    mtsd

---
+++ mtsd/normal/516/A.cpp
@@ -0,0 +1,68 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <climits>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <iomanip>
+#include <list>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+#include <cstdint>
+using namespace std;
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+#define MP make_pair
+#define PB push_back
+#define inf 1000000007
+#define rep(i,n) for(int i = 0; i < (int)(n); ++i)
+#define all(x) (x).begin(),(x).end()
+
+template<typename A, size_t N, typename T>
+void Fill(A (&array)[N], const T &val){
+    std::fill( (T*)array, (T*)(array+N), val );
+}
+ 
+template<class T> inline bool chmax(T &a, T b){
+    if(a<b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template<class T> inline bool chmin(T &a, T b){
+    if(a>b){
+        a = b;
+        return true;
+    }
+    return false;
+}
+string p[]={"","2","3","322","5","53","7","7222","7332"};
+int main(){
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+    string res;
+    rep(i,n){
+        if(s[i]=='0')continue;
+        res += p[s[i]-'1'];
+    }
+    sort(res.rbegin(),res.rend());
+    cout << res << endl;
+    return 0;
+}
