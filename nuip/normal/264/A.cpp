7588904cafed626f1163b6183bf5889d2a335094
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 14:21:23 +0530

    nuip

---
+++ nuip/normal/264/A.cpp
@@ -0,0 +1,54 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < int(Y);++(X))
+#define rrep(X,Y) for (int (X) = int(Y-1);(X) >=0;--(X))
+#define repe(X,Y) for ((X) = 0;(X) < int(Y);++(X))
+#define peat(X,Y) for (;(X) < int(Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+template<class T> ostream& operator<<(ostream &os, const vector<T> &t) {
+os<<"{"; rep(i,t.size()) {os<<t[i]<<",";} os<<"}"<<endl; return os;}
+template<class S, class T> ostream& operator<<(ostream &os, const pair<S,T> &t) { return os<<"("<<t.first<<","<<t.second<<")";}
+
+char str[1123456];
+
+int main(){
+  int i,j,k;
+  scanf("%s",str);
+  vector<int> v[2];
+  int n=strlen(str);
+  rep(i,n){
+    v[str[i]=='r'].pb(i+1);
+  }
+  rep(i,v[1].size())
+    printf("%d\n",v[1][i]);
+  rrep(i,v[0].size())
+    printf("%d\n",v[0][i]);
+  return 0;
+}
