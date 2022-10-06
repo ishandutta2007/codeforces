145c9daa0af21dfa104b2c09c237fdcbaee85dea
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 14:19:29 +0530

    nuip

---
+++ nuip/normal/287/A.cpp
@@ -0,0 +1,49 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < int(Y);++(X))
+#define rrep(X,Y) for (int (X) = int(Y-1);(X) >=0;--(X))
+#define repe(X,Y) for ((X) = 0;(X) < int(Y);++(X))
+#define peat(X,Y) for (;(X) < int(Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+
+int main(){
+  int i,j,k;
+  vector<string> a(4);
+  rep(i,4)cin>>a[i];
+  rep(i,3)rep(j,3){
+    int sum=0;
+    rep(k,2)rep(l,2)sum+=(a[i+k][j+l]=='.');
+    if(sum!=2){
+      cout<<"YES"<<endl;
+      return 0;
+    }
+  }
+  cout<<"NO"<<endl;
+  return 0;
+}
