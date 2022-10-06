cdc4ba57f7664988877de8a20dab9e4c6c004581
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 06:41:42 +0530

    nuip

---
+++ nuip/normal/1246/D.cpp
@@ -0,0 +1,115 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (Y);++(X))
+#define reps(X,S,Y) for (int (X) = S;(X) < (Y);++(X))
+#define rrep(X,Y) for (int (X) = (Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (Y)-1;(X) >= (S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "../benri.h"
+#else
+#define out(args...)
+#endif
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+const ll MOD=1e9+7; //998244353
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	int n;
+	cin>>n;
+	vv<int> g(n);
+	reps(i,1,n){
+		int p;
+		cin>>p;
+		g[p].pb(i);
+	}
+	{
+		function<int(int)> dfs=
+			[&](int v){
+				if(g[v].empty()){
+					return 0;
+				}
+				int mx=0;
+				int mxi;
+				rep(i,g[v].size())if(MX(mx,dfs(g[v][i])+1)) mxi=i;
+				if(mxi+1!=g[v].size()) swap(g[v][mxi],g[v].back());
+				return mx;
+			};
+		dfs(0);
+	}
+	out(g,1);
+	int stksz=0;
+	vector<pii> re;
+	int prv=-1;
+	vector<int> nxt(n,-1);
+	function<void(int)> dfs=
+		[&](int v){
+			if(prv>=0) nxt[prv]=v;
+			prv=v;
+			for(int w:g[v]){
+				if(stksz){
+					re.eb(w,stksz);
+					stksz=0;
+				}
+				dfs(w);
+			}
+			// stk.pb(v);
+			++stksz;
+			return;
+		};
+	dfs(0);
+	out(nxt,1);
+	for(int v=0;v>=0;v=nxt[v]) cout<<v<<" "; cout NL;
+	ll sum=0;
+	for(pii p:re) sum+=p.Y;
+	// out(sum,1); return 0;
+	out(re,1);
+	assert(sum<=1000000);
+	cout<<sum NL;
+	for(pii p:re)rep(_,p.Y) cout<<p.X<<" "; cout NL;
+  return 0;
+}
