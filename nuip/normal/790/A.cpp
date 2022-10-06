16e7e5f581fe0d1b8a8363d8c1e7977759601a0c
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 06:13:59 +0530

    nuip

---
+++ nuip/normal/790/A.cpp
@@ -0,0 +1,87 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#include<random>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (int)(Y);++(X))
+#define reps(X,S,Y) for (int (X) = (int)(S);(X) < (int)(Y);++(X))
+#define rrep(X,Y) for (int (X) = (int)(Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (int)(Y)-1;(X) >= (int)(S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "benri.h"
+#else
+#define out(args...)
+#endif
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+const ll MOD=1e9+7; //998244353
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	int n,t;
+	cin>>n>>t;
+	vector<int> dis(n-t+1);
+	for(auto &x:dis){
+		string s;
+		cin>>s;
+		x=(s.size()==3);
+	}
+	vector<int> re(n);
+	rep(i,t-1) re[i]=i;
+	reps(i,t-1,n){
+		if(dis[i-(t-1)]){
+			int s=t*(t-1)/2;
+			reps(j,1,t) s-=re[i-j];
+			re[i]=s;
+		}else{
+			re[i]=re[i-(t-1)];
+		}
+	}
+	out(re,1);
+	vector<string> names;
+	reps(c,'A','Z'+1)reps(d,'a','z'+1) names.eb(string(1,c)+string(1,d));
+	rep(i,n) cout<<names[re[i]]<<" \n"[i+1==n];
+  return 0;
+}
