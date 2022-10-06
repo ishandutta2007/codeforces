ee1db600c268a020278241990a1937be0e9f222a
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 05:47:46 +0530

    nuip

---
+++ nuip/normal/1491/D.cpp
@@ -0,0 +1,118 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#include<random>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (int)(Y);++(X))
+#define reps(X,S,Y) for (int (X) = (int)(S);(X) < (int)(Y);++(X))
+#define rrep(X,Y) for (int (X) = (int)(Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (int)(Y)-1;(X) >= (int)(S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+#define cauto const auto
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "benri.h"
+#else
+#define out(args...)
+#endif
+void ouT(ll x,int d=3){auto re=to_string(x);if((int)re.size()>d) re=x>0?"oo":"-oo";cout<<string(d-re.size(),' ')<<re<<",";}
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+template<class A,class B> pair<A,B> operator+(const pair<A,B> &p,const pair<A,B> &q){ return {p.X+q.X,p.Y+q.Y};}
+template<class A,class B,class C,class D> pair<A,B>& operator+=(pair<A,B> &p,const pair<C,D> &q){ p.X+=q.X; p.Y+=q.Y; return p;}
+template<class A,class B> pair<A,B> operator-(const pair<A,B> &p,const pair<A,B> &q){ return {p.X-q.X,p.Y-q.Y};}
+template<class A,class B,class C,class D> pair<A,B>& operator-=(pair<A,B> &p,const pair<C,D> &q){ p.X-=q.X; p.Y-=q.Y; return p;}
+template<class A,class B> istream& operator>>(istream &is, pair<A,B> &p){ is>>p.X>>p.Y; return is;}
+template<class T=ll> T read(){ T re; cin>>re; return move(re);}
+template<class T=ll> T read(const T &dec){ T re; cin>>re; return re-dec;}
+template<class T=ll> vector<T> readV(const int sz){ vector<T> re(sz); for(auto &x:re) x=read<T>(); return move(re);}
+template<class T=ll> vector<T> readV(const int sz, const T &dec){ vector<T> re(sz); for(auto &x:re) x=read<T>(dec); return move(re);}
+vv<int> readG(const int &n,const int &m){ vv<int> g(n); rep(_,m){ cauto a=read<int>(1),b=read<int>(1); g[a].pb(b); g[b].pb(a);} return move(g);}
+vv<int> readG(const int &n){ return readG(n,n-1);}
+#define TT cauto TTT=read();rep(_,TTT)
+const ll MOD=1e9+7; //998244353;
+
+bool solve(const int a,const int b){
+	if(a>b) return false;
+	int l=0,r=0;
+	rep(d,30){
+		if(a>>d&1){
+			if(b>>d&1){
+			}else{
+				++r;
+				if(l==0) l=1;
+			}		
+		}else{
+			if(b>>d&1){
+				if(!r) return false;
+				--r;
+				l=max(0,l-1);
+			}else{
+			}
+		}
+	}
+	return !l;
+}
+
+const int N=1000;
+int ok[N][N];
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	if(0){
+	rep(i,N)reps(j,i,N)if((i&(j-i))==(j-i)) ok[i][j]=1;
+	rep(k,N)rep(i,N)rep(j,N) ok[i][j]|=ok[i][k]&ok[k][j];
+	rep(i,N)rep(j,N){
+		if(solve(i,j)!=ok[i][j]){
+			out(i,j,solve(i,j),ok[i][j],1);
+			return 0;
+		}
+	}return 0;
+	}
+	TT{
+		cauto a=read(0);
+		cauto b=read(0);
+		cout<<(solve(a,b)?"YES":"NO") NL;
+	}
+  return 0;
+}
