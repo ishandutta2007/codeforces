a650313728c6b948f1a891beb64ffa741a0d2ece
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 06:33:08 +0530

    nuip

---
+++ nuip/normal/1292/B.cpp
@@ -0,0 +1,89 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (Y);++(X))
+#define reps(X,S,Y) for (int (X) = S;(X) < (Y);++(X))
+#define rrep(X,Y) for (int (X) = (Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (Y)-1;(X) >= (S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "benri.h"
+#else
+#define out(args...)
+#endif
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+const ll MOD=1e9+7; //998244353
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	const ll INF=4e16;
+	vector<pll> ps;
+	{
+		ll x0,y0,ax,ay,bx,by;
+		cin>>x0>>y0>>ax>>ay>>bx>>by;
+		ps.eb(x0,y0);
+		while(ps.back().X<=INF && ps.back().Y<=INF){
+			ll x,y;
+			tie(x,y)=ps.back();
+			ps.eb(x*ax+bx,y*ay+by);
+		}
+	}
+	ll xs,ys,t;
+	cin>>xs>>ys>>t;
+	out(ps,1);
+	int n=ps.size();
+	int re=0;
+	rep(r,n)rep(l,r+1){
+		ll x0,x1,y0,y1;
+		tie(x0,y0)=ps[l];
+		tie(x1,y1)=ps[r];
+		ll cost0=abs(xs-x0)+abs(ys-y0)+abs(x1-x0)+abs(y1-y0);
+		ll cost1=abs(xs-x1)+abs(ys-y1)+abs(x1-x0)+abs(y1-y0);
+		if(min(cost0,cost1)<=t) MX(re,r-l+1);
+	}
+	cout<<re NL;
+  return 0;
+}
