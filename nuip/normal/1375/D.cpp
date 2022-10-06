7d0251bafd429722b36867468493fef6a54d462a
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 06:12:37 +0530

    nuip

---
+++ nuip/normal/1375/D.cpp
@@ -0,0 +1,105 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#include<random>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (int)(Y);++(X))
+#define reps(X,S,Y) for (int (X) = (int)(S);(X) < (int)(Y);++(X))
+#define rrep(X,Y) for (int (X) = (int)(Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (int)(Y)-1;(X) >= (int)(S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "benri.h"
+#else
+#define out(args...)
+#endif
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+const ll MOD=1e9+7; //998244353
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	int T;
+	cin>>T;
+	while(T--){
+		int n;
+		cin>>n;
+		vector<int> a(n);
+		for(auto &x:a) cin>>x;
+		auto Mex=
+			[&](){
+				auto b=a;
+				sort(all(b)); UNIQUE(b);
+				rep(i,b.size())if(b[i]!=i) return i;
+				return (int)b.size();
+			};
+		vector<int> re;
+		auto fun=
+			[&](int i){
+				a[i]=Mex();
+				re.pb(i);
+			};
+		while(1){
+			{
+				int ok=1;
+				reps(i,1,n)if(a[i-1]>a[i]){ ok=0; break;}
+				if(ok) break;
+			}
+			int mex=Mex();
+			if(mex==n){
+				rep(i,n)if(a[i]!=i){
+					fun(i);
+					break;
+				}
+			}else{
+				fun(mex);
+			}
+		}
+		out(re,re.size(),n,1);
+		cout<<re.size() NL;
+		for(auto x:re) cout<<x+1<<" ";
+		cout NL;
+	}
+  return 0;
+}
