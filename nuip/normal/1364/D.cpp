6aa8cd0546c57389b5b0b71c73128e325a58afd8
ishan dutta <ishandutta2007@gmail.com>
2022-10-06 06:18:08 +0530

    nuip

---
+++ nuip/normal/1364/D.cpp
@@ -0,0 +1,233 @@
+#include <string>
+#include <vector>
+#include<iostream>
+#include<cstdio>
+#include<cstdlib>
+#include<stack>
+#include<queue>
+#include<cmath>
+#include<algorithm>
+#include<functional>
+#include<list>
+#include<deque>
+#include<bitset>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cstring>
+#include<sstream>
+#include<complex>
+#include<iomanip>
+#include<numeric>
+#include<cassert>
+#include<random>
+#define X first
+#define Y second
+#define pb push_back
+#define rep(X,Y) for (int (X) = 0;(X) < (int)(Y);++(X))
+#define reps(X,S,Y) for (int (X) = (int)(S);(X) < (int)(Y);++(X))
+#define rrep(X,Y) for (int (X) = (int)(Y)-1;(X) >=0;--(X))
+#define rreps(X,S,Y) for (int (X) = (int)(Y)-1;(X) >= (int)(S);--(X))
+#define repe(X,Y) for ((X) = 0;(X) < (Y);++(X))
+#define peat(X,Y) for (;(X) < (Y);++(X))
+#define all(X) (X).begin(),(X).end()
+#define rall(X) (X).rbegin(),(X).rend()
+#define eb emplace_back
+#define UNIQUE(X) (X).erase(unique(all(X)),(X).end())
+#define Endl endl
+#define NL <<"\n"
+
+using namespace std;
+using ll=long long;
+using pii=pair<int,int>;
+using pll=pair<ll,ll>;
+template<class T> using vv=vector<vector<T>>;
+template<class T> inline bool MX(T &l,const T &r){return l<r?l=r,1:0;}
+template<class T> inline bool MN(T &l,const T &r){return l>r?l=r,1:0;}
+//#undef NUIP
+#ifdef NUIP
+#include "benri.h"
+#else
+#define out(args...)
+#endif
+#ifdef __cpp_init_captures
+template<typename T>vector<T> table(int n, T v){ return vector<T>(n, v);}
+template <class... Args> auto table(int n, Args... args){auto val = table(args...); return vector<decltype(val)>(n, move(val));}
+#endif
+const ll MOD=1e9+7; //998244353
+
+struct UF{
+  vector<int> data;
+  UF(int size):data(size,-1){}
+  bool unite(int x,int y){
+    x=root(x); y=root(y);
+    if(x!=y){
+      if(-data[y]>-data[x]) swap(x,y);
+      data[x]+=data[y]; data[y]=x;
+    }
+    return x!=y;
+  }
+  bool findSet(int x,int y){return root(x)==root(y);}
+  int root(int x){return data[x]<0?x:data[x]=root(data[x]);}
+  int size(int x) {return -data[root(x)];}
+	bool operator()(int x,int y){ return findSet(x,y);}
+	int operator[](int x){ return root(x);}
+	bool unti(int x,int y){ return unite(x,y);}
+};
+
+template<class T> class RMQ{
+  vv<T> vals;
+public:
+  inline T get(int l,int r){//[l,r]
+    if(l==r)return vals[0][l];
+    const int d=31-__builtin_clz(l^r); //left-most distinct bit
+    return min(vals[d][l],vals[d][r]);
+  }
+  RMQ(){};
+  RMQ(vector<T> &v,T e=-MOD){ init(v,e); }
+  void init(vector<T> &v,T e){
+    int n=v.size();
+    int d=1,N=2;
+    while(N<n) N*=2, ++d;
+    vals.resize(d,vector<T>(N,e));
+    rep(i,n) vals[0][i]=v[i];
+    reps(i,1,d)rep(j,N){
+      const int b=(j>>i|1)<<i;
+      vals[i][j]=j>>i&1?get(b,j):get(j,b-1);
+    }
+  }
+};
+
+typedef vv<int> Graph;
+class Lca{
+public:
+  vector<int> dep,cld;
+  vector<pii> et;
+  vector<int> l,r;
+  vector<ll> wdep;
+  vector<int> par;
+  int weighted;
+  vv<int> wei;
+  RMQ<pii> rmq;
+  void dfs(const Graph &g,int root){
+    int n=g.size();
+    vector<int> vst(n);
+    stack<int> st; st.push(root);
+    while(!st.empty()){
+      int v=st.top(); st.pop();
+      if(vst[v]==1){
+	vst[v]=2;
+	r[v]=et.size()-1;
+	rep(i,g[v].size())if(g[v][i]!=par[v])
+	  cld[v]+=cld[g[v][i]]+1;
+	if(par[v]>=0) et.eb(dep[par[v]],par[v]);
+      }else if(vst[v]==0){
+	st.push(v);
+	vst[v]=1;
+	l[v]=et.size(); et.eb(dep[v],v);
+	rep(i,g[v].size())if(!vst[g[v][i]]){
+	  dep[g[v][i]]=dep[v]+1;
+	  wdep[g[v][i]]=wdep[v]+(weighted?wei[v][i]:1);
+	  par[g[v][i]]=v;
+	  st.push(g[v][i]);
+	}
+      }
+    }
+  }
+  
+  Lca(const Graph &g):weighted(0){
+    int n=g.size();
+    par.resize(n);
+    dep.resize(n); wdep.resize(n); cld.resize(n); l.resize(n); r.resize(n); dfs(g,0);
+    rmq.init(et,pii(MOD,0));
+  }
+  Lca(const vv<pii> &h):weighted(1){
+    int n=h.size();
+    vv<int> g(n);
+    wei.resize(n);
+    rep(i,n) for(pii x:h[i]){
+      g[i].pb(x.X);
+      wei[i].pb(x.Y);
+    }
+    par.resize(n);
+    dep.resize(n); wdep.resize(n); cld.resize(n); l.resize(n); r.resize(n); dfs(g,0);
+    rmq.init(et,pii(MOD,0));
+  }
+  
+  int LCA(int v,int w){
+    v=l[v]; w=l[w];
+    if(v>w) swap(v,w);
+    return rmq.get(v,w).Y;
+  }
+  ll dist(int v,int w){return wdep[v]+wdep[w]-2*wdep[LCA(v,w)];}
+	vector<int> pathp(int v,int p){
+		vector<int> re;
+		out(v,p,par,1);
+		for(int i=v;i!=p;i=par[i]) re.pb(i);
+		re.pb(p);
+		return re;
+	}
+	vector<int> path(int v,int w){
+		int p=LCA(v,w);
+		vector<int> a=pathp(v,p);
+		vector<int> b=pathp(w,p);
+		b.pop_back();
+		reverse(all(b));
+		a.insert(a.end(),all(b));
+		return a;
+	}
+};
+
+
+int main(){
+  ios_base::sync_with_stdio(false); cin.tie(0);
+  cout<<fixed<<setprecision(0);
+	int n,m,t;
+	cin>>n>>m>>t;
+	vv<int> g(n);
+	vector<pii> es;
+	{
+		UF uf(n);
+		rep(_,m){
+			int a,b;
+			cin>>a>>b; --a; --b;
+			if(uf.unite(a,b)){
+				g[a].pb(b);
+				g[b].pb(a);
+			}else{
+				es.eb(a,b);
+			}
+		}
+	}
+	if(es.empty()){
+		vv<int> re(2);
+		auto dfs=
+			[&](auto &&dfs,int v,int p,int d)->void{
+				re[d%2].pb(v);
+				for(int w:g[v])if(w!=p) dfs(dfs,w,v,d+1);
+			};
+		dfs(dfs,0,-1,0);
+		int N=(t+1)/2;
+		int t=0;
+		if(re[t].size()<N) t=1;
+		cout<<1 NL;
+		rep(i,N) cout<<re[t][i]+1<<" \n"[i+1==N];
+		return 0;
+	}
+	Lca h(g);
+	int mn=MOD,x,y;
+	for(auto [a,b]:es)if(MN(mn,(int)h.dist(a,b)+1)) x=a, y=b;
+	out(mn,x,y,1);
+	auto path=h.path(x,y);
+	if(mn<=t){
+		cout<<2 NL;
+		cout<<mn NL;
+		rep(i,mn) cout<<path[i]+1<<" \n"[i+1==mn];
+	}else{
+		cout<<1 NL;
+		int N=(t+1)/2;
+		rep(i,N) cout<<path[i*2]+1<<" \n"[i+1==N];
+	}
+  return 0;
+}
