2f52a7dca0f85b4a99fb4a1b0e3dd82300259c75
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 09:45:18 +0530

    stevenkplus

---
+++ stevenkplus/normal/197/B.cpp
@@ -0,0 +1,93 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <complex>
+#include <functional>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef stringstream ss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > PQ;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define clr(t,v) memset((t),(v),sizeof(t))
+#define p(a) cout << #a << ": " << a << endl;
+
+#define rep(i,n) for(int i=0,_##i=(n);i<_##i;++i)
+#define dwn(i,n) for(int i=(n);--i>=0;)
+#define repr(i,l,r) for(int i=(l),_##i=(r);i<_##i;++i)
+#define dwnr(i,l,r) for(int i=(r),_##i=(l);--i>=_##i;)
+#define repi(i,a) for(__typeof((a).begin()) i=(a).begin(),_##i=(a).end();i!=_##i;++i)
+#define dwni(i,a) for(__typeof((a).rbegin()) i=(a).rbegin(),_##i=(a).rend();i!=_##i;++i)
+const int inf = 1 << 30;
+char buf[1000];
+string nextLine(int length = 100) {
+	cin.getline(buf, length);
+	string s(buf);
+	return s;
+}
+string next(int length = 100) {
+	string tmp;
+	cin >> tmp;
+	return tmp;
+}
+
+int nextInt() {
+	int tmp;
+	scanf("%d", &tmp);
+	return tmp;
+}
+int ar1[200],ar2[200];
+int main() {
+	int n = nextInt(),m=nextInt();
+	rep(i,n+1) {
+		ar1[i]=nextInt();
+	}
+	rep(i,m+1) {
+		ar2[i]=nextInt();
+	}
+	if(n>m) {
+		if(ar1[0]<0^ar2[0]<0) cout<<"-";
+		cout<<"Infinity"<<endl;
+	}
+	else if(n<m) {
+		cout<<"0/1"<<endl;
+	}
+	else {
+		int a = ar1[0],b=ar2[0];
+		int c = __gcd(abs(a),abs(b));
+		a/=c; b/=c;
+		if(b<0) {
+			b*=-1;
+			a*=-1;
+		}
+		printf("%d/%d\n",a,b);
+	}
+}
