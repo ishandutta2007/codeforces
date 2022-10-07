87221b5a23bcd059a5d67c6450945a90cdbaaa83
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 09:40:48 +0530

    stevenkplus

---
+++ stevenkplus/normal/152/C.cpp
@@ -0,0 +1,89 @@
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <complex>
+#include <functional>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+typedef stringstream ss;
+typedef priority_queue<pii, vector<pii>, greater<pii> > PQ;
+typedef pair<pii, int> circ;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define clr(t,v) memset((t),(v),sizeof(t))
+#define p(a) cout << #a << ": " << a << endl;
+
+#define rep(i,n) for(int i=0,_##i=(n);i<_##i;++i)
+#define dwn(i,n) for(int i=(n);--i>=0;)
+#define repr(i,l,r) for(int i=(l),_##i=(r);i<_##i;++i)
+#define dwnr(i,l,r) for(int i=(r),_##i=(l);--i>=_##i;)
+#define repi(i,a) for(__typeof((a).begin()) i=(a).begin(),_##i=(a).end();i!=_##i;++i)
+#define dwni(i,a) for(__typeof((a).rbegin()) i=(a).rbegin(),_##i=(a).rend();i!=_##i;++i)
+const int inf = 1 << 30;
+char buf[1000];
+string nextLine(int length = 100) {
+	cin.getline(buf, length);
+	string s(buf);
+	return s;
+}
+string next(int length = 100) {
+	string tmp;
+	cin >> tmp;
+	return tmp;
+}
+
+int nextInt() {
+	int tmp;
+	scanf("%d", &tmp);
+	return tmp;
+}
+const int MAXM = 110;
+bool chartaken[MAXM][1000];
+const int mod = 1E9 + 7;
+int counts[MAXM];
+int main() {
+	int n = nextInt(), m = nextInt();
+	rep(i,n) {
+		string s = next();
+		rep(j,m) {
+			if (chartaken[j][int(s[j])])
+				continue;
+			chartaken[j][int(s[j])] = true;
+			counts[j]++;
+		}
+	}
+	ll prod = 1;
+	rep(i,m) {
+		prod *= counts[i];
+		prod %= mod;
+	}
+	cout << prod << endl;
+}
