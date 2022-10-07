7cc7c65c2079d3908ff1e6bf2c5a740f9af1f9cc
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 09:46:46 +0530

    stevenkplus

---
+++ stevenkplus/normal/158/A.cpp
@@ -0,0 +1,72 @@
+//============================================================================
+// Name        : HelloWorld.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cmath>
+#include <ctime>
+#include <cctype>
+#include <iostream>
+#include <sstream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <set>
+#include <map>
+#include <bitset>
+#include <algorithm>
+#include <numeric>
+#include <complex>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef vector<int> vi;
+typedef stringstream ss;
+
+#define mp make_pair
+#define pb push_back
+#define fi first
+#define se second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(),(x).end()
+#define clr(t,v) memset((t),(v),sizeof(t))
+#define p(a) cout << #a << ": " << a << endl;
+
+#define rep(i,n) for(int i=0,_##i=(n);i<_##i;++i)
+#define dwn(i,n) for(int i=(n);--i>=0;)
+#define repr(i,l,r) for(int i=(l),_##i=(r);i<_##i;++i)
+#define dwnr(i,l,r) for(int i=(r),_##i=(l);--i>=_##i;)
+#define repi(i,a) for(__typeof((a).begin()) i=(a).begin(),_##i=(a).end();i!=_##i;++i)
+#define dwni(i,a) for(__typeof((a).rbegin()) i=(a).rbegin(),_##i=(a).rend();i!=_##i;++i)
+
+int main() {
+	int N,K;
+	cin >> N >> K;
+	vector<int> v;
+	int i;
+	for(i=0;i<N;i++) {
+		int next;
+		cin>>next;
+		v.pb(next);
+	}
+
+	int t = v[K-1];
+	if(t<1) t = 1;
+	int count = 0;
+	int x;
+	for(x=0;x<N;x++)
+		if(v[x]>=t) count++;
+	cout<<count;
+}
