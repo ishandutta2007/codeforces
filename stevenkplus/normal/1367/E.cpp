1b0b4bd488c6774193ca79d62335850d614775bc
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 07:44:15 +0530

    stevenkplus

---
+++ stevenkplus/normal/1367/E.cpp
@@ -0,0 +1,74 @@
+// =========== Template ========== //
+#include <bits/stdc++.h>
+using namespace std;
+int fastio() { ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); cin.tie(nullptr); return 0; }
+int __fastio = fastio();
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p);
+template<typename A> ostream& operator<<(ostream &os, const set<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A> ostream& operator<<(ostream &os, const multiset<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e.first << ": " << e.second, sep = ", "; return os << "}"; }
+template<typename T>ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; string sep = ""; for (auto e: v) os << sep << e, sep = ", "; return os << "]"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { os << '(' << p.first << ", " << p.second << ')'; return os; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef STEVEN
+#define pp(...) cerr << "\t[" << #__VA_ARGS__ << "]:\t", dbg_out(__VA_ARGS__)
+#define ppp(x) cerr << x << "\n"
+#else
+#define pp(...)
+#define ppp(...)
+#endif
+template<class Fun> class y_combinator_result { Fun fun_; public: template<class T> explicit y_combinator_result(T &&fun): fun_(std::forward<T>(fun)) {} template<class ...Args> decltype(auto) operator()(Args &&...args) { return fun_(std::ref(*this), std::forward<Args>(args)...); } };
+template<class Fun> decltype(auto) y_combinator(Fun &&fun) { return y_combinator_result<std::decay_t<Fun>>(std::forward<Fun>(fun)); }
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+#define fi first
+#define se second
+#define sz(x) (int((x).size()))
+#define data _data
+// =========== Book ============ //
+
+// ============= Solution ============= //
+
+int _gcd(int a, int b) {
+  if (a == 0) return b;
+  return _gcd(b % a, a);
+}
+
+int main() {
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; ++t) {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    vector<int> cnts(26);
+    for (char c: s) {
+      ++cnts[c - 'a'];
+    }
+    int ans = 0;
+    for (int len = 1; len <= n; ++len) {
+      int need = _gcd(len, k);
+      int thres = len / need;
+      int val = 0;
+      for (int i = 0; i < 26; ++i) {
+        val += cnts[i] / thres;
+      }
+      if (val >= need) {
+        ans = len;
+      }
+    }
+    cout << ans << "\n";
+  }
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN?)
+// - special cases (n=1? graph not connected?)
+// - re-initialize efficiently between test cases
