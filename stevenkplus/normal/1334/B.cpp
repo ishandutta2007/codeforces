06f9e2cfdc3a4c9e500772d0ca5a433c6afb6a95
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 07:57:24 +0530

    stevenkplus

---
+++ stevenkplus/normal/1334/B.cpp
@@ -0,0 +1,56 @@
+// =========== Template ========== //
+#include <bits/stdc++.h>
+using namespace std;
+int fastio() { ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); cin.tie(nullptr); return 0; }
+int __fastio = fastio();
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p);
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e.first << ": " << e.second, sep = ", "; return os << "}"; }
+template<typename T>ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; string sep = ""; for (auto e: v) os << sep << e, sep = ", "; return os << "]"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { os << '(' << p.first << ", " << p.second << ')'; return os; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef STEVEN
+#define pp(...) cerr << "\t[" << #__VA_ARGS__ << "]:\t", dbg_out(__VA_ARGS__)
+#else
+#define pp(...)
+#endif
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+#define fi first
+#define se second
+#define sz(x) (int((x).size()))
+#define data _data
+// =========== Book ============ //
+
+// ============= Solution ============= //
+int main() {
+  int T;
+  cin >> T;
+  for (int t = 1; t <= T; ++t) {
+    int n, x;
+    cin >> n >> x;
+    vector<int> v(n);
+    for (auto &i: v) cin >> i;
+    sort(v.begin(), v.end());
+    reverse(v.begin(), v.end());
+    ll sum = 0;
+    ll need = 0;
+    int ans = 0;
+    for (auto i: v) {
+      sum += i;
+      need += x;
+      if (sum >= need) ++ans;
+    }
+    cout << ans << "\n";
+  }
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN?)
+// - special cases (n=1? graph not connected?)
+// - re-initialize efficiently between test cases
