3265158297339ed74f3b7afbeb6f7b8b2ec88eef
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 07:47:38 +0530

    stevenkplus

---
+++ stevenkplus/normal/1361/B.cpp
@@ -0,0 +1,158 @@
+// AVX-enabled std::bitset Pragmas (before #includes!)
+// - bitset<200000> takes ~ 1ms per 1000 operations on GNU C++14 & GNU C++17
+// #pragma GCC optimize("Ofast","unroll-loops","omit-frame-pointer","inline") //Optimization flags
+// #pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
+// #pragma GCC target("avx2")  //Enable AVX
+
+// =========== Template ========== //
+#include <bits/stdc++.h>
+using namespace std;
+int fastio() { ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); cin.tie(nullptr); return 0; }
+int __fastio = fastio();
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p);
+template<typename A> ostream& operator<<(ostream &os, const set<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A> ostream& operator<<(ostream &os, const multiset<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e.first << ": " << e.second, sep = ", "; return os << "}"; }
+template<typename T>ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; string sep = ""; for (auto e: v) os << sep << e, sep = ", "; return os << "]"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { os << '(' << p.first << ", " << p.second << ')'; return os; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef STEVEN
+#define pp(...) cerr << "\t[" << #__VA_ARGS__ << "]:\t", dbg_out(__VA_ARGS__)
+#define ppp(x) cerr << x << "\n"
+#else
+#define pp(...)
+#define ppp(...)
+#endif
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+#define fi first
+#define se second
+#define sz(x) (int((x).size()))
+#define data _data
+// =========== Book ============ //
+
+template <int MOD_> struct modnum {
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+  private:
+  using ll = long long;
+  int v;
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
+  }
+  public:
+  modnum() : v(0) {}
+  modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+  explicit operator int() const { return v; }
+  friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+  friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+  friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+  friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
+  }
+  friend modnum inv(const modnum& m) { return m.inv(); }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD-v : 0;
+    return res;
+  }
+  friend modnum neg(const modnum& m) { return m.neg(); }
+  modnum operator- () const {
+    return neg();
+  }
+  modnum operator+ () const {
+    return modnum(*this);
+  }
+  modnum& operator ++ () {
+    v ++;
+    if (v == MOD) v = 0;
+    return *this;
+  }
+  modnum& operator -- () {
+    if (v == 0) v = MOD;
+    v --;
+    return *this;
+  }
+  modnum& operator += (const modnum& o) {
+    v += o.v;
+    if (v >= MOD) v -= MOD;
+    return *this;
+  }
+  modnum& operator -= (const modnum& o) {
+    v -= o.v;
+    if (v < 0) v += MOD;
+    return *this;
+  }
+  modnum& operator *= (const modnum& o) {
+    v = int(ll(v) * ll(o.v) % MOD);
+    return *this;
+  }
+  modnum& operator /= (const modnum& o) {
+    return *this *= o.inv();
+  }
+  friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+  friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+  friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+  friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+  friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+  friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+};
+
+const int inf = 1000 << 20;
+// =========== Solution ============= //
+
+typedef modnum<1000000007> mint;
+int main() {
+  int T;
+  cin >> T;
+  auto pw = [&](mint base, int exp) -> mint {
+    mint ret = 1;
+    for (int i = 0; i < 30; ++i) {
+      if ((exp >> i) & 1) {
+        ret *= base;
+      }
+      base *= base;
+    }
+    return ret;
+  };
+  for (int t = 1; t <= T; ++t) {
+    int n, p;
+    cin >> n >> p;
+    vector<int> vk(n);
+    for (int &k: vk) cin >> k;
+    sort(vk.begin(), vk.end());
+    reverse(vk.begin(), vk.end());
+    ll val = 0;
+    mint ans = 0;
+    int prv = -1;
+    for (int k: vk) {
+      if (prv != -1) {
+        for (int i = 0; i < prv - k && i < 30; ++i) {
+          if (val < inf) val *= p;
+        }
+      }
+      prv = k;
+      int add = val > 0 ? -1 : 1;
+      ans += add * pw(p, k);
+      val += add;
+    }
+    cout << ans << "\n";
+  }
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN? different MAXN vs MAXM?)
+// - special cases (n=1? graph not connected?)
+// - sorting (input or ouput must be in particular order?)
+// - initialize & re-initialize efficiently between test cases
