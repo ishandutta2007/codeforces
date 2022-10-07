c2f63a0f3778e4e324764a075a779f2190bd6325
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 07:46:38 +0530

    stevenkplus

---
+++ stevenkplus/normal/1358/E.cpp
@@ -0,0 +1,79 @@
+// AVX-enabled std::bitset Pragmas (before #includes!)
+// - bitset<200000> takes ~ 1ms per 1000 operations on GNU C++14 & GNU C++17
+// #pragma GCC optimize("Ofast","unroll-loops","omit-frame-pointer","inline") //Optimization flags
+// #pragma GCC option("arch=native","tune=native","no-zero-upper") //Enable AVX
+// #pragma GCC target("avx2")  //Enable AVX
+
+// =========== Template ========== //
+#include <bits/stdc++.h>
+using namespace std;
+int fastio() { ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); cin.tie(nullptr); return 0; }
+int __fastio = fastio();
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p);
+template<typename A> ostream& operator<<(ostream &os, const set<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A> ostream& operator<<(ostream &os, const multiset<A> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const map<A, B> &m) { os << "{"; string sep = ""; for (auto e: m) os << sep << e.first << ": " << e.second, sep = ", "; return os << "}"; }
+template<typename T>ostream& operator<<(ostream& os, const vector<T>& v) { os << "["; string sep = ""; for (auto e: v) os << sep << e, sep = ", "; return os << "]"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { os << '(' << p.first << ", " << p.second << ')'; return os; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef STEVEN
+#define pp(...) cerr << "\t[" << #__VA_ARGS__ << "]:\t", dbg_out(__VA_ARGS__)
+#define ppp(x) cerr << x << "\n"
+#else
+#define pp(...)
+#define ppp(...)
+#endif
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+#define fi first
+#define se second
+#define sz(x) (int((x).size()))
+#define data _data
+// =========== Book ============ //
+
+const ll infl = 1LL << 60;
+// =========== Solution ============= //
+const int MAXN = 1001000;
+
+int main() {
+  int n;
+  cin >> n;
+  int a = (n + 1) / 2;
+  int b = n - a;
+  vector<ll> v(a);
+  for (ll &i: v) cin >> i;
+  ll x;
+  cin >> x;
+  for (int i = 0; i < b; ++i) v.push_back(x);
+  vector<ll> p(n + 1);
+  for (int i = 0; i < n; ++i) p[i + 1] = p[i] + v[i];
+  if (p[n] > 0) {
+    cout << n << "\n";
+  } else if (x >= 0) {
+    cout << "-1\n";
+  } else {
+    pp(p);
+    ll mx = 0;
+    for (int i = 0; i < a; ++i) {
+      mx = max(mx, p[i] - i * x);
+      int m = n - i;
+      if (p[m] > mx) {
+        cout << m << "\n";
+        return 0;
+      }
+    }
+    cout << "-1\n";
+  }
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN? different MAXN vs MAXM?)
+// - special cases (n=1? graph not connected?)
+// - sorting (input or ouput must be in particular order?)
+// - initialize & re-initialize efficiently between test cases
