bbcb384be3aac102e68ab3f90b643bf1e93c8395
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 13:08:05 +0530

    autumnkite

---
+++ autumnkite/normal/1670/C.cpp
@@ -0,0 +1,207 @@
+#include <bits/stdc++.h>
+
+template<unsigned P>
+class modint {
+  static_assert(1 <= P, "P must be a positive integer");
+
+  using mint = modint<P>;
+
+protected:
+  unsigned v;
+
+public:
+  constexpr modint() : v() {}
+
+  template<typename T,
+           typename std::enable_if<std::is_integral<T>::value &&
+                                       std::is_signed<T>::value,
+                                   bool>::type = true>
+  constexpr modint(T t_v) : v() {
+    long long tmp = t_v % static_cast<long long>(P);
+    if (tmp < 0) {
+      tmp += P;
+    }
+    v = tmp;
+  }
+
+  template<typename T,
+           typename std::enable_if<std::is_integral<T>::value &&
+                                       std::is_unsigned<T>::value,
+                                   bool>::type = true>
+  constexpr modint(T t_v) : v() {
+    v = t_v % P;
+  }
+
+  constexpr unsigned val() const {
+    return v;
+  }
+
+  static constexpr unsigned mod() {
+    return P;
+  }
+
+  static constexpr mint raw(unsigned v) {
+    mint res;
+    res.v = v;
+    return res;
+  }
+
+  constexpr mint &operator+=(const mint &rhs) {
+    v < P - rhs.v ? v += rhs.v : v -= P - rhs.v;
+    return *this;
+  }
+
+  constexpr mint &operator++() {
+    v + 1 < P ? ++v : v = 0;
+    return *this;
+  }
+
+  constexpr mint operator++(int) {
+    mint tmp = *this;
+    v + 1 < P ? ++v : v = 0;
+    return tmp;
+  }
+
+  constexpr mint &operator-=(const mint &rhs) {
+    v < rhs.v ? v += P - rhs.v : v -= rhs.v;
+    return *this;
+  }
+
+  constexpr mint &operator--() {
+    v ? --v : v = P - 1;
+    return *this;
+  }
+  
+  constexpr mint operator--(int) {
+    mint tmp = *this;
+    v ? --v : v = P - 1;
+    return tmp;
+  }
+
+  constexpr mint operator-() const {
+    mint res;
+    res.v = v ? P - v : 0;
+    return res;
+  }
+
+  constexpr mint &operator*=(const mint &rhs) {
+    v = static_cast<unsigned long long>(v) * rhs.v % P;
+    return *this;
+  }
+
+  constexpr mint pow(unsigned long long b) const {
+    mint a(*this), s(1);
+    for (; b; b >>= 1) {
+      if (b & 1) {
+        s *= a;
+      }
+      a *= a;
+    }
+    return s;
+  }
+
+  constexpr mint inv() const {
+    return pow(P - 2);
+  }
+
+  constexpr friend mint operator+(const mint &lhs, const mint &rhs) {
+    return mint(lhs) += rhs;
+  }
+
+  constexpr friend mint operator-(const mint &lhs, const mint &rhs) {
+    return mint(lhs) -= rhs;
+  }
+
+  constexpr friend mint operator*(const mint &lhs, const mint &rhs) {
+    return mint(lhs) *= rhs;
+  }
+
+  constexpr friend bool operator==(const mint &lhs, const mint &rhs) {
+    return lhs.v == rhs.v;
+  }
+
+  constexpr friend bool operator!=(const mint &lhs, const mint &rhs) {
+    return lhs.v != rhs.v;
+  }
+
+  friend std::istream &operator>>(std::istream &in, mint &x) {
+    return in >> x.v;
+  }
+
+  friend std::ostream &operator<<(std::ostream &out, const mint &x) {
+    return out << x.v;
+  }
+};
+
+class disjoint_set {
+public:
+  typedef std::size_t size_type;
+
+protected:
+  std::vector<size_type> fa;
+
+public:
+  disjoint_set(size_type n = 0) : fa(n) {
+    std::iota(fa.begin(), fa.end(), 0);
+  }
+
+  size_type find(size_type x) {
+    return fa[x] == x ? x : (fa[x] = find(fa[x]));
+  }
+
+  bool merge(size_type x, size_type y) {
+    x = find(x), y = find(y);
+    if (x == y) {
+      return false;
+    }
+    fa[y] = x;
+    return true;
+  }
+};
+
+using mint = modint<1000000007>;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int T;
+  std::cin >> T;
+  while (T--) {
+    int n;
+    std::cin >> n;
+    std::vector<int> a(n), b(n), d(n);
+    for (int i = 0; i < n; ++i) {
+      std::cin >> a[i];
+      --a[i];
+    }
+    for (int i = 0; i < n; ++i) {
+      std::cin >> b[i];
+      --b[i];
+    }
+    for (int i = 0; i < n; ++i) {
+      std::cin >> d[i];
+      --d[i];
+    }
+    disjoint_set D(n);
+    for (int i = 0; i < n; ++i) {
+      D.merge(a[i], b[i]);
+    }
+    std::vector<int> size(n), cnt(n);
+    for (int i = 0; i < n; ++i) {
+      ++size[D.find(i)];
+    }
+    for (int i = 0; i < n; ++i) {
+      if (d[i] != -1) {
+        ++cnt[D.find(d[i])];
+      }
+    }
+    mint ans = 1;
+    for (int i = 0; i < n; ++i) {
+      if ((int)D.find(i) == i && !cnt[i] && size[i] > 1) {
+        ans *= 2;
+      }
+    }
+    std::cout << ans << "\n";
+  }
+}
