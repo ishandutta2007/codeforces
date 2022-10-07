209e7873c40537b9e447c0ebd4535aaf82fb90c6
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 08:51:02 +0530

    stevenkplus

---
+++ stevenkplus/normal/322/A.cpp
@@ -0,0 +1,16 @@
+#include <iostream> //scott
+#include<cstdio> //eugene
+using namespace std; //steven
+
+int main() {//scott
+int n, m; // eugene
+    scanf("%d %d", &n, &m); //steven
+    for (int i = 1; i <= n; i++) //scott gogo
+        ;// scott you need to print the answer first --eugene
+    printf("%d\n", n + m - 1); //steven
+    for (int i = 1; i <= n; i++) // eugene
+        printf ("%d %d\n", i, 1); //scott
+    for (int i = 2; i <= m; ++i) //steven wow scot yo'ure slow
+        cout << 1 << " " << i << endl; //eugene
+    return 0; //scott
+}//steven
