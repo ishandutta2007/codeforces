33661c0e881620dd23b77c45cb97620ac028a2d1
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 14:55:56 +0530

    autumnkite

---
+++ autumnkite/normal/1447/A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+int n;
+
+void solve() {
+	std::cin >> n;
+	std::cout << n << "\n";
+	for (int i = 1; i <= n; ++i) {
+		std::cout << i << " \n"[i == n];
+	}
+}
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int T = 1;
+	std::cin >> T;
+	while (T--) {
+		solve();
+	}
+}
