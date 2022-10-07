346c5c21088f3932a955445afedc3683f3975057
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 14:16:26 +0530

    autumnkite

---
+++ autumnkite/normal/1184/E2.cpp
@@ -0,0 +1,221 @@
+// ...,o[[[[[[[[ooo[[[[\oooOooooo**]]]`,oOO@@@@@OOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OoOOOOOOOOOOOOOOO
+// ....*.........*.........*........**.*oOOOOOOOOOOOOOOOo\oOOOOOOOooooOOOOOOOOOOOOOOOO@@@@o**/oOOOOOOOOOOOO
+// /**[oooo\]*[ooooo]]`**,/o***....,OOO@OOO@@@OOOOOOOO@@@O\`=O\*OOOOO@@O\*[[\\ooooOOOOOO@OO**oOOOOOOOOOOOOO
+// ........**........[`......*,OOO@@@@@OO@O@@OOOOOOOOOOO@@@OO\@@OOOo[oOOOOOo`*......***o@Oo**oo^*****^**...
+// ........*`........**....,]/O@@@@OOOOO@@@@@OO@O@@@@@OOOO@@@OOO\OO@OOOO@OOOOOo^*..***=O@Oo**ooo****oo^****
+// ****oooo/[`*=oo\`,^**]O@@@@@@@OoOOooO@@O[O@@@@@@@@@@@@@OOO@@@OOOOOO@OO@@OOOOoo`*.**=O@O/**ooo***********
+// ..............*..../@@@@O[O@O^]O@OO@@@@OOO@@@@@@@@@@@@@@@@@@@@@OOOOOO@OOOOOOOOOoo]*,O@O^**oo/***********
+// `**=o\*****..**.,O@@@O*,O@@OOO@@@OO@@OOO@@@O@@@@@O@@@@@@@@@@@@@@@OOOOO@OOOOOOOOO\/\*,OO^**oo^........*..
+// ........**....,O@@OO\/O@@@@OO@@@@@@@@OO@@@@@@@@@@@@@@@@@@@@@@@@@@@OO@OOOOOOOOOOOOOOo`*,`**oo^...........
+// .............OOOOOOOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OOOOOOOOOoOOOOo`***oo*...........
+// ****,]**=o\oO@OOOO@@@@@@@@@@@O@@@@@@@@@@@@@@@@@@@@@@@@@@@OO@@@@@@@@@@@@@@@@OO@OOOOOOOOO]**oo*...........
+// ....**..*/O@OOOO@@@@@@@@@@@@OO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OOOo/\OOOOOOOo*...........
+// ****/`*,O@@@OO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@Oo]]oOOOO@OO^....   ....
+// *....,oOOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@O@@@@OOOO`*oOO@OO^...   ....
+// ......=O@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@O@@O^*O@OOO\..    ....
+// `**,oO@@@@OOOOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OOOOOOOOOOOOOOOO@@@@@@@@@@@@@@Oooo\O@@O.     ...
+// ....oO@@@@OO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OO/o[***oo[*****,oOOOOOOOO@@@@@@@@@OO\oO@@O`    ...
+// ...,OO@@OOO@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@OO`*................,[[`**,\oOO@@@@@@@@O^=O@@O.   ...
+// **=OO@OOO@@@@@@@@@@@@@@@@@@@@@@OOOO@OOOOOOOO@@@@@OO/*...........................*,\O@@@@@@@@OO@@^.   ...
+// ../O@@@@@@@@@@@@@@@@@@OO@@@OOO@OOOOOOoooooooOOOOOoo*.....         .......      ....\O@@@@@@@@@O@O^.  ...
+// *=O@@@@@@@@@@@@@@@@@OOOOOOOOo/oo/*[****....**,\/*.....                           ...\O@@@@@@@@@@@o.  ...
+// .=@@@@@@@@@@@@@@@@Ooooo[****.......................                               ...OO@@@@@@@@@@O.  .. 
+// *O@@@@@@@@@@@@@@@Ooo`**.............                                               ..,O@@@@@@@@@@@O.    
+// *\O@@@@@@@@@@@@@OO**..........                                  .......]]]]*,*..    ..=O@@@@@@@@@@/.    
+// .=O@@@@@@@@@@@@@Oo*...............      .....  ....      .......,]/O@@@OO@OOo/[^*.   ..=O@@@@@@@@@O.    
+// `oO@@@@@@@@@@@@@Oo*........,/ooooOOOOOO\]]`*`.............*,/OO@@@@@@O/[[.*......*..   .,O@@@@@@@@O.  . 
+// .\O@@@@@@@@@@@@@O/.......*/oOOOO@@@@@@@@@@@OO\*...........*oOOOOOo`..       .........   .,O@@@@@@@O.    
+// .=O@@@@@@@@@@@@@O^....../Oo=oo[[`**,**[[[\oOO/**...........**[`*..............*=o]]`..    =@@@@@@Oo..   
+// **\@@@@@@@@@@@@@O*.....*`***.......    ......**.....     ....,oOOo*.          ....\@@@@@` .@@@@@@O/.    
+// ..,O@@@@@@@@@@@O^......*.......        ....**]]...        ,OO[`....,]]]]]]]..   .  .@@/\OO`,O@@@@O`     
+// `.,O@@@@@@@@@@@O`.............,]`***..........*[\OOOO@@@@@@   ...oO@@@@@@@@@@@OO*..,OO..[\OO\=@@@O.     
+// ...,O@@@@@@@@@@o*..... ..]/OO[`.   ,]]/OOO\`...   ,@@`  ,\O      ............   ....[`.     ,@@@O`.  .  
+// ....O@@@@@@@@@O^....,]O@@@/....]O@@@@@@@OO[[.     =O^    =@O        ........        ..       =O^       .
+// ***.,O@@@@@@@@@^]OOO@@@@@^../OO@O/[[[......      ,@@O.    \@\                       *        .OO`,/\]   
+// .....,O@@@@@@@OOOOO[.  .OO. .       .....       .O@@`      ,@\                    ..         .\OOOOO@O. 
+// *...../@@@@@O@@@O`..... .O^                     .=@`         \O`              ..=*.           .o..,OOO^ 
+// ....=`[\@@@@@@@^.......  =\.                  ...O`            [[**...........                .[. .[oo. 
+// ....     .\O@@O......     ,^                ..]/O`  .........     ..              ..         ..,*. .\.. 
+// *,.  ,/O\].O@@@^......  .  .o]...........]]\`[[`  .,]/oOOOOOOO\`. .....            ...  .......OO...*.  
+// ..../@@OOOOO@@O^...........  ........  .   ..*...]@@@@OOOOO@@@@@O\.....             ...........OO^. ..  
+// *...O@@O^.=O@OO\...............         .....,oO/O@OOOOOOOO[*,\OO/.               ..............\`.     
+// .*...O@O^.*\OOOO*.............         ........\OOO`.......                  ................  ...      
+// .....,OO^.=OO@@O^...........................      ..... ...                   ...............           
+// *.**].,[^.oOO@@@^........................                                     ...............     .     
+// ..........oOOOOO`.......................             .]]].,]/o]].   ......     .............            
+// ..........,OO^..=^***..............................,OOOOOOOOOO@@@@\]..*****..  .............            
+// ****,****........**...........................***/@@@@@@@@@@@OOO@@@@@@@OO\*..................]]...      
+// ..............   .**......................=oooOO@@@@@/[.....     .,\OOOOOOo`...  ................       
+// *..***.......*. ....*....................=ooO@@@@@O`.**]]*..,/\]]*.....*,ooo*............**.            
+// ........**...      ..*...................*oOOOOo`**=oOOOO@@@@@@@OO\]*...**oo`*..........**..,]**........
+// .............      .=\*.*................*=oo/******\OOO@@@OOOOOOOo`*******[[*****.....***.*=^***,[[oooo
+// .*************....*]Oo`****..............*,\^**...*****........  ........*.********.**/o^..=^***********
+// ..............*........******........*******[***.......   ....      ........*******,ooOo`.,oo/oo\]]]]]]]
+// ...***.......**........*/`]/\*******************.............................*/ooooooOO^. .........*****
+// ..................*......*\ooooo\]**************.......*]o\]]]]]/]]`.......**ooOOOOOOOo*.               
+// ..................*.......*oooooooooooooooo^******..*,oOOOOOOOOOOOOOOoooooooOOOOOOOOOo*..               
+// ...**..***..***..*`]****`***ooOOOOOOOOOOOOOoooooooooOOOOOOO@@@@@@@@OOOOOOOOO@@@OOOOOo*... ,/\`....      
+// ....*.......................*oOOOOOOOOOOOOOOOOOOOOOOOOO@@@@@@@@@@@@@@@@@@@@@@OOOOOOo`.... .O@Ooo*...    
+// ...***.......***.......*.....,ooOOOOOOOOOOOOOOOOO@@@@@@@@@@@@@@@@@@@@@@@@@@OOOOOOOo`*.... .OOOo*...     
+
+#include <bits/stdc++.h>
+
+struct edge {
+	int u, v, w;
+};
+
+class DSU {
+	std::vector<int> fa;
+
+public:
+	DSU(int n) : fa(n) {
+		for (int i = 0; i < n; ++i) {
+			fa[i] = i;
+		}
+	}
+
+	int find(int x) {
+		return fa[x] == x ? x : (fa[x] = find(fa[x]));
+	}
+
+	bool merge(int x, int y) {
+		x = find(x), y = find(y);
+		if (x == y) {
+			return false;
+		}
+		fa[y] = x;
+		return true;
+	}
+};
+
+class tree {
+	static int get_log(int n) {
+		int res = 0;
+		while ((1 << res) < n) {
+			++res;
+		}
+		return res;
+	}
+
+	int n, LG;
+	std::vector<std::vector<std::tuple<int, int, int>>> E;
+	std::vector<int> dep, fid;
+	std::vector<std::vector<int>> fa;
+	std::vector<std::vector<int>> fw;
+	DSU D;
+	std::vector<int> ans;
+
+	void dfs(int u) {
+		for (int i = 1; i < LG && fa[u][i - 1] != -1; ++i) {
+			fa[u][i] = fa[fa[u][i - 1]][i - 1];
+			fw[u][i] = std::max(fw[u][i - 1], fw[fa[u][i - 1]][i - 1]);
+		}
+		for (auto [v, w, id] : E[u]) {
+			if (v != fa[u][0]) {
+				fid[v] = id;
+				dep[v] = dep[u] + 1;
+				fa[v][0] = u;
+				fw[v][0] = w;
+				dfs(v);
+			}
+		}
+	}
+
+public:
+	tree(int _n, int m) : n(_n), LG(get_log(n)), E(n), dep(n), fid(n, -1),
+	                      fa(n, std::vector<int>(LG, -1)), fw(n, std::vector<int>(LG)),
+						  D(n), ans(m, -1) {}
+
+	void add_edge(int u, int v, int w, int id) {
+		E[u].emplace_back(v, w, id);
+		E[v].emplace_back(u, w, id);
+	}
+
+	void build() {
+		dfs(0);
+	}
+
+	int work(int u, int v, int w) {
+		int x = u, y = v;
+		if (dep[u] < dep[v]) {
+			std::swap(u, v);
+		}
+		int mx = 0;
+		for (int i = LG - 1; i >= 0; --i) {
+			if (fa[u][i] != -1 && dep[fa[u][i]] >= dep[v]) {
+				mx = std::max(mx, fw[u][i]);
+				u = fa[u][i];
+			}
+		}
+		if (u != v) {
+			for (int i = LG - 1; i >= 0; --i) {
+				if (fa[u][i] != fa[v][i]) {
+					mx = std::max(mx, std::max(fw[u][i], fw[v][i]));
+					u = fa[u][i];
+					v = fa[v][i];
+				}
+			}
+			mx = std::max(mx, std::max(fw[u][0], fw[v][0]));
+			u = fa[u][0];
+			v = fa[v][0];
+		}
+		auto cover = [&](int x) {
+			while (dep[D.find(x)] > dep[u]) {
+				x = D.find(x);
+				ans[fid[x]] = w;
+				D.merge(fa[x][0], x);
+			}
+		};
+		cover(x);
+		cover(y);
+		return mx;
+	}
+
+	int get_ans(int id) {
+		return ans[id];
+	}
+};
+
+int main() {
+	std::ios_base::sync_with_stdio(false);
+	std::cin.tie(0);
+
+	int n, m;
+	std::cin >> n >> m;
+	std::vector<edge> E(m);
+	for (int i = 0; i < m; ++i) {
+		std::cin >> E[i].u >> E[i].v >> E[i].w;
+		--E[i].u, --E[i].v;
+	}
+	std::vector<int> id(m);
+	for (int i = 0; i < m; ++i) {
+		id[i] = i;
+	}
+	std::sort(id.begin(), id.end(), [&](int i, int j) {
+		return E[i].w < E[j].w;
+	});
+
+	DSU D(n);
+	tree T(n, m);
+	std::vector<bool> on_tree(m);
+	for (int i : id) {
+		if (D.merge(E[i].u, E[i].v)) {
+			on_tree[i] = true;
+			T.add_edge(E[i].u, E[i].v, E[i].w, i);
+		} else {
+			on_tree[i] = false;
+		}
+	}
+	T.build();
+	std::vector<int> ans(m, -1);
+	for (int i : id) {
+		if (!on_tree[i]) {
+			ans[i] = T.work(E[i].u, E[i].v, E[i].w);
+		}
+	}
+	for (int i = 0; i < m; ++i) {
+		if (!on_tree[i]) {
+		    std::cout << ans[i] << "\n";
+		}
+	}
+}
