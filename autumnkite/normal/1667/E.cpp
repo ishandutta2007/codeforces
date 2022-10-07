5a061f2fd6ba933dc37eb8148c54c51d66f04e56
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 13:11:25 +0530

    autumnkite

---
+++ autumnkite/normal/1667/E.cpp
@@ -0,0 +1,167 @@
+#include <bits/stdc++.h>
+
+template<unsigned P>
+class modint {
+  static_assert(1 <= P, "P must be a positive integer");
+
+  using mint = modint<P>;
+
+protected:
+  unsigned v;
+
+public:
+  constexpr modint() : v() {}
+
+  template<typename T,
+           typename std::enable_if<std::is_integral<T>::value &&
+                                       std::is_signed<T>::value,
+                                   bool>::type = true>
+  constexpr modint(T t_v) : v() {
+    long long tmp = t_v % static_cast<long long>(P);
+    if (tmp < 0) {
+      tmp += P;
+    }
+    v = tmp;
+  }
+
+  template<typename T,
+           typename std::enable_if<std::is_integral<T>::value &&
+                                       std::is_unsigned<T>::value,
+                                   bool>::type = true>
+  constexpr modint(T t_v) : v() {
+    v = t_v % P;
+  }
+
+  constexpr unsigned val() const {
+    return v;
+  }
+
+  static constexpr unsigned mod() {
+    return P;
+  }
+
+  static constexpr mint raw(unsigned v) {
+    mint res;
+    res.v = v;
+    return res;
+  }
+
+  constexpr mint &operator+=(const mint &rhs) {
+    v < P - rhs.v ? v += rhs.v : v -= P - rhs.v;
+    return *this;
+  }
+
+  constexpr mint &operator++() {
+    v + 1 < P ? ++v : v = 0;
+    return *this;
+  }
+
+  constexpr mint operator++(int) {
+    mint tmp = *this;
+    v + 1 < P ? ++v : v = 0;
+    return tmp;
+  }
+
+  constexpr mint &operator-=(const mint &rhs) {
+    v < rhs.v ? v += P - rhs.v : v -= rhs.v;
+    return *this;
+  }
+
+  constexpr mint &operator--() {
+    v ? --v : v = P - 1;
+    return *this;
+  }
+  
+  constexpr mint operator--(int) {
+    mint tmp = *this;
+    v ? --v : v = P - 1;
+    return tmp;
+  }
+
+  constexpr mint operator-() const {
+    mint res;
+    res.v = v ? P - v : 0;
+    return res;
+  }
+
+  constexpr mint &operator*=(const mint &rhs) {
+    v = static_cast<unsigned long long>(v) * rhs.v % P;
+    return *this;
+  }
+
+  constexpr mint pow(unsigned long long b) const {
+    mint a(*this), s(1);
+    for (; b; b >>= 1) {
+      if (b & 1) {
+        s *= a;
+      }
+      a *= a;
+    }
+    return s;
+  }
+
+  constexpr mint inv() const {
+    return pow(P - 2);
+  }
+
+  constexpr friend mint operator+(const mint &lhs, const mint &rhs) {
+    return mint(lhs) += rhs;
+  }
+
+  constexpr friend mint operator-(const mint &lhs, const mint &rhs) {
+    return mint(lhs) -= rhs;
+  }
+
+  constexpr friend mint operator*(const mint &lhs, const mint &rhs) {
+    return mint(lhs) *= rhs;
+  }
+
+  constexpr friend bool operator==(const mint &lhs, const mint &rhs) {
+    return lhs.v == rhs.v;
+  }
+
+  constexpr friend bool operator!=(const mint &lhs, const mint &rhs) {
+    return lhs.v != rhs.v;
+  }
+
+  friend std::istream &operator>>(std::istream &in, mint &x) {
+    return in >> x.v;
+  }
+
+  friend std::ostream &operator<<(std::ostream &out, const mint &x) {
+    return out << x.v;
+  }
+};
+
+using mint = modint<998244353>;
+
+int main() {
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  int n;
+  std::cin >> n;
+
+  std::vector<mint> fac(n + 1), ifac(n + 1);
+  fac[0] = 1;
+  for (int i = 1; i <= n; ++i) {
+    fac[i] = fac[i - 1] * i;
+  }
+  ifac[n] = fac[n].inv();
+  for (int i = n; i >= 1; --i) {
+    ifac[i - 1] = ifac[i] * i;
+  }
+
+  std::vector<mint> res(n);
+  res[0] = fac[n - 1];
+  for (int k = 1; k <= n / 2; ++k) {
+    res[k] = fac[k - 1] * fac[n - k - 1] *
+             fac[n / 2] * ifac[k] * ifac[n / 2 - k];
+  }
+  mint sum = std::accumulate(res.begin(), res.end(), mint());
+  for (int i = 0; i < n; ++i) {
+    sum -= res[i];
+    mint ans = res[i] * std::max(i, 1) - sum;
+    std::cout << ans << " \n"[i == n - 1];
+  }
+}
