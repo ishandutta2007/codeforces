6600a3cd116155ffafcb21bd7c7683bf88ae0ce3
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 07:58:43 +0530

    stevenkplus

---
+++ stevenkplus/normal/757/E.cpp
@@ -0,0 +1,177 @@
+// =========== Template ========== //
+#include <bits/stdc++.h>
+using namespace std;
+int fastio() { ios_base::sync_with_stdio(false); cout << fixed << setprecision(10); cin.tie(nullptr); return 0; }
+int __fastio = fastio();
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p);
+template<typename T>ostream& operator<<(ostream& os, const vector<T>& v) { os << "{"; string sep = ""; for (auto e: v) os << sep << e, sep = ", "; return os << "}"; }
+template<typename A, typename B> ostream& operator<<(ostream &os, const pair<A, B> &p) { os << '(' << p.first << ", " << p.second << ')'; return os; }
+void dbg_out() { cerr << endl; }
+template<typename Head, typename... Tail> void dbg_out(Head H, Tail... T) { cerr << ' ' << H; dbg_out(T...); }
+#ifdef STEVEN
+#define pp(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define pp(...)
+#endif
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+#define fi first
+#define se second
+#define sz(x) (int((x).size()))
+#define data _data
+// =========== Book ============ //
+
+const int inf = 1000 << 20;
+
+template <int MOD_> struct modnum {
+  static constexpr int MOD = MOD_;
+  static_assert(MOD_ > 0, "MOD must be positive");
+  private:
+  using ll = long long;
+  int v;
+  static int minv(int a, int m) {
+    a %= m;
+    assert(a);
+    return a == 1 ? 1 : int(m - ll(minv(m, a)) * ll(m) / a);
+  }
+  public:
+  modnum() : v(0) {}
+  modnum(ll v_) : v(int(v_ % MOD)) { if (v < 0) v += MOD; }
+  explicit operator int() const { return v; }
+  friend std::ostream& operator << (std::ostream& out, const modnum& n) { return out << int(n); }
+  friend std::istream& operator >> (std::istream& in, modnum& n) { ll v_; in >> v_; n = modnum(v_); return in; }
+  friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+  friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+  modnum inv() const {
+    modnum res;
+    res.v = minv(v, MOD);
+    return res;
+  }
+  friend modnum inv(const modnum& m) { return m.inv(); }
+  modnum neg() const {
+    modnum res;
+    res.v = v ? MOD-v : 0;
+    return res;
+  }
+  friend modnum neg(const modnum& m) { return m.neg(); }
+  modnum operator- () const {
+    return neg();
+  }
+  modnum operator+ () const {
+    return modnum(*this);
+  }
+  modnum& operator ++ () {
+    v ++;
+    if (v == MOD) v = 0;
+    return *this;
+  }
+  modnum& operator -- () {
+    if (v == 0) v = MOD;
+    v --;
+    return *this;
+  }
+  modnum& operator += (const modnum& o) {
+    v += o.v;
+    if (v >= MOD) v -= MOD;
+    return *this;
+  }
+  modnum& operator -= (const modnum& o) {
+    v -= o.v;
+    if (v < 0) v += MOD;
+    return *this;
+  }
+  modnum& operator *= (const modnum& o) {
+    v = int(ll(v) * ll(o.v) % MOD);
+    return *this;
+  }
+  modnum& operator /= (const modnum& o) {
+    return *this *= o.inv();
+  }
+  friend modnum operator ++ (modnum& a, int) { modnum r = a; ++a; return r; }
+  friend modnum operator -- (modnum& a, int) { modnum r = a; --a; return r; }
+  friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+  friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+  friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+  friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= b; }
+};
+
+// =========== Solution ============= //
+
+const int MAXN = 2001000;
+typedef modnum<int(1E9 + 7)> mint;
+mint fac[MAXN];
+mint ifac[MAXN];
+int p[MAXN];
+
+int main() {
+  int q;
+  cin >> q;
+  for (int i = 2; i * i < MAXN; ++i) {
+    if (p[i]) continue;
+    p[i] = i;
+    for (int j = i * i; j < MAXN; j += i) {
+      if (!p[j]) p[j] = i;
+    }
+  }
+
+  fac[0] = 1;
+  for (int i = 0; i + 1 < MAXN; ++i) {
+    fac[i + 1] = fac[i] * (i + 1);
+  }
+  ifac[MAXN - 1] = 1 / fac[MAXN - 1];
+  for (int i = MAXN - 1; i > 0; --i) {
+    ifac[i - 1] = ifac[i] * i;
+  }
+  for (int i = 0; i < 10; ++i) {
+    pp(fac[i], ifac[i], fac[i] * ifac[i]);
+  }
+
+  for (int _q = 0; _q < q; ++_q) {
+    int r, n;
+    cin >> r >> n;
+    mint ans = 1;
+    int prv = 0;
+    vector<int> v;
+    pp(n);
+    while (n > 1) {
+      if (!p[n]) p[n] = n;
+      if (p[n] != prv) {
+        v.push_back(0);
+      }
+      ++v.back();
+      prv = p[n];
+      n /= p[n];
+    }
+    pp(v);
+    for (int i: v) {
+      mint c1 = fac[i + r - 1] * ifac[r] * ifac[i - 1];
+      mint c2 = fac[i + r] * ifac[r] * ifac[i];
+      ans *= c1 + c2;
+    }
+    cout << ans << "\n";
+  }
+}
+
+// ============== Notes =============== //
+// common bugs
+// - fastio
+// - int overflow (especially intermediate expressionns)
+// - array bounds (indices bigger than MAXN? different MAXN vs MAXM?)
+// - special cases (n=1? graph not connected?)
+// - initialize & re-initialize efficiently between test cases
+//
+// history of bugs
+// - 3/28/20 forgot to initialize set with dummy node (n) [cf 1136e]
+// - 3/28/20 array bounds (map was size MAXN instead of MAXK) [cf 1210b]
+// - 3/29/20 array bounds ([MAXN][MAXN][4] -> [MAXN][MAXN * 2][4]) [cf 1051d]
+// - 3/29/20 special cases (answer cannot be empty string) [cf 848a]
+// - 3/29/20 special case / bad initialization of (ans, ansi) when backtracking [cf 733f]
+// - 3/30/20 special case / "0" has length 0 in binary [cf 748b]
+// - 4/2/20 fakesolve -- setup of dp states missed possible transitions / solutions [cf 771D]
+// - 4/2/20 fakesolve -- cant count inversions in linear time, misunderstand structure of problem [cf 771D]
+// - 4/3/20 (TLE) re-initialization was too slow [cf 1329 D]
+// - 4/3/20 (WA) bottom-up segtree query didn't handle [0, n) correctly
+// - 4/5/20 array bounds -- two different array sizes needed [cf 18d]
+// - 4/5/20:???
