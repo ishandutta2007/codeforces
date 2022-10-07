7aaa4e03c0456f7713bbe07bc9e6ada5db35559c
ishan dutta <ishandutta2007@gmail.com>
2022-10-07 12:48:01 +0530

    cirno_9baka

---
+++ cirno_9baka/normal/1331/A.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+const int N=500005,E=262144;
+const int M=998244353;
+int n;
+int main()
+{
+#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+#endif
+	puts("No");
+}
