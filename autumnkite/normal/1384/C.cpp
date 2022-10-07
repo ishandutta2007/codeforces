a8deb34e0167066e38d3add7a4a65a730b2aa522
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 04:16:55 +0530

    autumnkite

---
+++ autumnkite/normal/1384/C.cpp
@@ -0,0 +1,158 @@
+#include <cstdio>
+#include <cctype>
+#include <cstring>
+#include <algorithm>
+#include <vector>
+
+#define debug(...) fprintf(stderr, __VA_ARGS__)
+
+#ifndef AT_HOME
+#define getchar() IO::myGetchar()
+#define putchar(x) IO::myPutchar(x)
+#endif
+
+namespace IO {
+	static const int IN_BUF = 1 << 23, OUT_BUF = 1 << 23;
+
+	inline char myGetchar() {
+		static char buf[IN_BUF], *ps = buf, *pt = buf;
+		if (ps == pt) {
+			ps = buf, pt = buf + fread(buf, 1, IN_BUF, stdin);
+		}
+		return ps == pt ? EOF : *ps++;
+	}
+
+	template<typename T>
+	inline bool read(T &x) {
+		bool op = 0;
+		char ch = getchar();
+		x = 0;
+		for (; !isdigit(ch) && ch != EOF; ch = getchar()) {
+			op ^= (ch == '-');
+		}
+		if (ch == EOF) {
+			return false;
+		}
+		for (; isdigit(ch); ch = getchar()) {
+			x = x * 10 + (ch ^ '0');
+		}
+		if (op) {
+			x = -x;
+		}
+		return true;
+	}
+
+	inline int readStr(char *s) {
+		int n = 0;
+		char ch = getchar();
+		for (; isspace(ch) && ch != EOF; ch = getchar())
+			;
+		for (; !isspace(ch) && ch != EOF; ch = getchar()) {
+			s[n++] = ch;
+		}
+		s[n] = '\0';
+		return n;
+	}
+
+	inline void myPutchar(char x) {
+		static char pbuf[OUT_BUF], *pp = pbuf;
+		struct _flusher {
+			~_flusher() {
+				fwrite(pbuf, 1, pp - pbuf, stdout);
+			}
+		};
+		static _flusher outputFlusher;
+		if (pp == pbuf + OUT_BUF) {
+			fwrite(pbuf, 1, OUT_BUF, stdout);
+			pp = pbuf;
+		}
+		*pp++ = x;
+	}
+
+	template<typename T>
+	inline void print_(T x) {
+		if (x == 0) {
+			putchar('0');
+			return;
+		}
+		std::vector<int> num;
+		if (x < 0) {
+			putchar('-');
+			x = -x;
+		}
+		for (; x; x /= 10) {
+			num.push_back(x % 10);
+		}
+		while (!num.empty()) {
+			putchar(num.back() ^ '0');
+			num.pop_back();
+		}
+	}
+
+	template<typename T>
+	inline void print(T x, char ch = '\n') {
+		print_(x);
+		putchar(ch);
+	}
+
+	inline void printStr_(const char *s, int n = -1) {
+		if (n == -1) {
+			n = strlen(s);
+		}
+		for (int i = 0; i < n; ++i) {
+			putchar(s[i]);
+		}
+	}
+
+	inline void printStr(const char *s, int n = -1, char ch = '\n') {
+		printStr_(s, n);
+		putchar(ch);
+	}
+}
+using namespace IO;
+
+int n;
+char a[100005], b[100005];
+bool e[20][20];
+
+void solve() {
+	read(n);
+	readStr(a), readStr(b);
+	memset(e, 0, sizeof e);
+	for (int i = 0; i < n; ++i) {
+		int l = a[i] - 'a', r = b[i] - 'a';
+		if (l > r) {
+			print(-1);
+			return;
+		}
+		if (l < r) {
+			e[l][r] = 1;
+		}
+	}
+	int ans = 0;
+	for (int i = 0; i < 20; ++i) {
+		int k = -1;
+		for (int j = i + 1; j < 20; ++j) {
+			if (e[i][j]) {
+				k = j;
+				break;
+			}
+		}
+		if (k == -1) {
+			continue;
+		}
+		++ans;
+		for (int j = k + 1; j < 20; ++j) {
+			e[k][j] |= e[i][j];
+		}
+	}
+	print(ans);
+}
+
+int main() {
+	int T = 1;
+	read(T);
+	while (T--) {
+		solve();
+	}
+}
