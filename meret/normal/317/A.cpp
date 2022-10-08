bc525ca4f242ae2dccc70af009f4e04c0280cf15
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 10:42:41 +0530

    meret

---
+++ meret/normal/317/A.cpp
@@ -0,0 +1,68 @@
+#include <vector>
+#include <list>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <stack>
+#include <bitset>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <utility>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <cstring>
+#include <cassert>
+
+using namespace std;
+
+typedef long long LL;
+typedef pair<int, int> PII;
+#define MP make_pair
+#define FOR(v,p,k) for(int v=p;v<=k;++v)
+#define FORD(v,p,k) for(int v=p;v>=k;--v)
+#define REP(i,n) for(int i=0;i<(n);++i)
+#define VAR(v,i) __typeof(i) v=(i)
+#define FORE(i,c) for(__typeof(c.begin()) i=(c.begin());i!=(c).end();++i)
+#define PB push_back
+#define ST first
+#define ND second
+#define SZ(x) (int)(x).size()
+#define ALL(c) c.begin(),c.end()
+#define ZERO(x) memset(x,0,sizeof(x))
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    LL x, y, m;
+    cin >> x >> y >> m;
+    if (max(x, y) >= m) {
+        cout << 0 << endl;
+        return 0;
+    }
+    if (x > y) {
+        swap(x, y);
+    }
+    if (y <= 0) {
+        cout << -1 << endl;
+        return 0;
+    }
+    LL result = 0;
+    if (x < 0) {
+        result = (-x) / y;
+        x += result * y;
+    }
+    while (max(x, y) < m) {
+        x += y;
+        ++result;
+        if (x > y) {
+            swap(x, y);
+        }
+    }
+    cout << result << endl;
+}
