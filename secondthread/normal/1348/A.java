a66ff56cc68fadb801c2f7ecabc4f93040b87fb5
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 20:26:29 +0530

    secondthread

---
+++ secondthread/normal/1348/A.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+	
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int n=fs.nextInt();
+			long ans=0;
+			for (int i=1; i<=n; i++) {
+				if (i<n/2) ans+=1<<i;
+				else if (i==n) ans+=1<<i;
+				else ans-=1<<i;
+			}
+			System.out.println(ans);
+		}
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+
+		public String next() {
+			while (!st.hasMoreElements())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		public int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) {
+				a[i]=nextInt();
+			}
+			return a;
+		}
+
+		public double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+	}
+	
+}
