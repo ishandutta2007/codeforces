ae299c0f0cbadf2980003f73c213a3dc8234556c
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 20:06:30 +0530

    secondthread

---
+++ secondthread/normal/1400/D.java
@@ -0,0 +1,68 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+public class D {
+
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int n=fs.nextInt();
+			int[] a=fs.readArray(n);
+			int[] lCounts=new int[n+100];
+			long ans=0;
+			for (int l=0; l<n; l++) {
+				int[] rCounts=new int[n+100];
+				for (int r=n-1; r>l; r--) {
+					ans+=lCounts[a[r]]*rCounts[a[l]];
+					rCounts[a[r]]++;
+				}
+				lCounts[a[l]]++;
+			}
+			System.out.println(ans);
+		}
+	}
+
+	static final Random random=new Random();
+	
+	static void ruffleSort(int[] a) {
+		int n=a.length;//shuffle, then sort 
+		for (int i=0; i<n; i++) {
+			int oi=random.nextInt(n), temp=a[oi];
+			a[oi]=a[i]; a[i]=temp;
+		}
+		Arrays.sort(a);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
