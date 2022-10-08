b3ff08348f629127da045892ec21001975b72e56
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 10:26:08 +0530

    ivan.metelsky

---
+++ winger/normal/1/A.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	private BufferedReader in;
+	private PrintWriter out;
+	private StringTokenizer st;
+
+	void solve() throws IOException {
+		long n = nextInt();
+		long m = nextInt();
+		long a = nextInt();
+		out.println(((n + a - 1) / a) * ((m + a - 1) / a));
+	}
+
+	Solution() throws IOException {
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		
+		eat("");
+		
+		solve();
+		
+		in.close();
+		out.close();
+	}
+	
+	private void eat(String str) {
+		st = new StringTokenizer(str);
+	}
+	
+	String next() throws IOException {
+		while (!st.hasMoreTokens()) {
+			String line = in.readLine();
+			if (line == null) {
+				return null;
+			}
+			eat(line);
+		}
+		return st.nextToken();
+	}
+	
+	int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	long nextLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	double nextDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution();
+	}
+}
