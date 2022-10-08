805bdcab4f02f09489c28ef93095a8c95ba61785
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 19:56:16 +0530

    secondthread

---
+++ secondthread/normal/1462/C.java
@@ -0,0 +1,81 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/* 
+ * Subscribe to galen_colin!
+ * https://tinyurl.com/galencolin
+*/
+
+public class C {
+
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int n=fs.nextInt();
+			if (n>45) {
+				System.out.println(-1);
+				continue;
+			}
+			ArrayList<Integer> list=new ArrayList<>();
+			int max=9;
+			while (n>0) {
+				list.add(Math.min(max, n));
+				n-= max;
+				max--;
+			}
+			for (int i=list.size()-1; i>=0; i--)
+				System.out.print(list.get(i));
+			System.out.println();
+		}
+	}
+
+	static void ruffle_sort(int[] a) {
+		//shandom_ruffle
+		Random r=new Random();
+		int n=a.length;
+		for (int i=0; i<n; i++) {
+			int oi=r.nextInt(i);
+			int temp=a[i];
+			a[i]=a[oi];
+			a[oi]=temp;
+		}
+		
+		//sort
+		Arrays.sort(a);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
