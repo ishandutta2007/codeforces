520dcb542154fc8bab7c717127154da1cee19371
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 13:48:43 +0530

    dotorya

---
+++ dotorya/normal/731/B.cpp
@@ -0,0 +1,56 @@
+#include <algorithm>
+#include <assert.h>
+#include <complex>
+#include <ctype.h>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <locale.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <cmath>
+#include <vector>
+//#include <unordered_set>
+//#include <unordered_map>
+
+#pragma warning(disable:4996)
+using namespace std;
+
+#define mp make_pair
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <ldb, ldb> pdd;
+
+int IT_MAX = 524288;
+const int MOD = 100000000;
+const int INF = 1034567891;
+const ll LL_INF = 1234567890123456789ll;
+const db PI = 3.141592653589793238;
+const ldb ERR = 1E-12;
+
+int main() {
+	int s = 0, t, i, N;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++) {
+		scanf("%d", &t);
+		if (t == 0) {
+			if (s % 2 == 1) return !printf("NO\n");
+			s = 0;
+		}
+		s += t;
+	}
+	if (s % 2 == 1) return !printf("NO\n");
+	return !printf("YES\n");
+}
