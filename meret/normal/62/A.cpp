a1cb5043022350107a5a2b818de1ece65a50d900
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 10:55:52 +0530

    meret

---
+++ meret/normal/62/A.cpp
@@ -0,0 +1,61 @@
+//BEGIN_CUT
+#include <cstdio>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <numeric>
+#include <bitset>
+#include <cstring>
+#include <sstream>
+#include <utility>
+#include <queue>
+#include <cassert>
+using namespace std;
+
+#define X first
+#define Y second
+#define FI first
+#define SE second
+#define ST first
+#define ND second
+#define MP make_pair
+#define PB push_back
+typedef long long LL;
+typedef double D;
+typedef long double LD;
+typedef vector<int> VI;
+typedef pair<int,int> PII;
+#define REP(i,n) for(int i=0;i<(n);i++)
+#define FOR(i,a,b) for(VAR(i,a);i<=(b);++i)
+#define FORD(i,a,b) for(VAR(i,a);i>=(b);--i)
+#define FORE(a,b) for(VAR(a,(b).begin());a!=(b).end();++a)
+#define VAR(a,b) __typeof(b) a=(b)
+#define SIZE(a) ((int)((a).size()))
+#define ALL(x) (x).begin(),(x).end()
+#define CLR(x,a) memset(x,a,sizeof(x))
+int cond = 1;
+#define db(x) {if(cond){cerr << __LINE__ << " " << #x << " " << x << endl; } }
+#define dbv(x) {if(cond){cerr << __LINE__ << " " << #x << ": "; FORE(__i,x) cerr << *__i << " "; cerr << endl;} }
+//END_CUT
+
+bool check(int v, int m) {
+	return m >= v - 1 && v >= (m - 1) / 2;
+}
+
+int vl, vr;
+int ml, mr;
+
+int main()
+{
+	scanf("%d %d", &vl, &vr);
+	scanf("%d %d", &ml, &mr);
+
+	printf((check(vl, mr) || check(vr, ml)) ? "YES\n" : "NO\n");
+
+    return 0;
+}
