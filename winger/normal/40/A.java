8580721de07ca259dfc800b065298ad084e91028
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 10:22:05 +0530

    winger

---
+++ winger/normal/40/A.java
@@ -0,0 +1,63 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	private BufferedReader in;
+	private PrintWriter out;
+	private StringTokenizer st;
+
+	void solve() throws IOException {
+		int x = nextInt();
+		int y = nextInt();
+		int d = (int)Math.sqrt(x * x + y * y);
+		if (d * d == x * x + y * y || x == 0 || y == 0) {
+			out.println("black");
+			return;
+		}
+		out.println((x > 0) ^ (y > 0) ^ (d % 2 == 0) ? "black" : "white");
+	}
+
+	Solution() throws IOException {
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		
+		eat("");
+		
+		solve();
+		
+		in.close();
+		out.close();
+	}
+	
+	private void eat(String str) {
+		st = new StringTokenizer(str);
+	}
+	
+	String next() throws IOException {
+		while (!st.hasMoreTokens()) {
+			String line = in.readLine();
+			if (line == null) {
+				return null;
+			}
+			eat(line);
+		}
+		return st.nextToken();
+	}
+	
+	int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	long nextLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	double nextDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution();
+	}
+}
