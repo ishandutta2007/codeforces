b8064053858cc100c9dd99b8311428f805ab7cb9
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 20:42:01 +0530

    secondthread

---
+++ secondthread/normal/1283/B.java
@@ -0,0 +1,55 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/*
+ABC
+AAC
+Z
+ */
+public class B {
+	
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int t=fs.nextInt();
+		for (int tt=0; tt<t; tt++) {
+			int n=fs.nextInt(), k=fs.nextInt();
+			System.out.println(Math.min(n, n-n%k+k/2));
+		}
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		
+		public String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+			
+		}
+		
+		public int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		public long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		public int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++)
+				a[i]=nextInt();
+			return a;
+		}
+	}
+	
+}
