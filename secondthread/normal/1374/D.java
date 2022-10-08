fbcdd8848cf760f07e90edae6daa9c91473170d5
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 20:15:20 +0530

    secondthread

---
+++ secondthread/normal/1374/D.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class D {
+
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int n=fs.nextInt(), k=fs.nextInt();
+			int[] a=fs.readArray(n);
+			HashMap<Long, Integer> timesUsed=new HashMap<>();
+			long ans=0;
+			for (int i:a) {
+				long toAdd=((k-i%k)+k)%k;
+				if (toAdd==0) continue;
+				int times=timesUsed.getOrDefault(toAdd, 0);
+				timesUsed.put(toAdd, times+1);
+				toAdd+=k*(long)times;
+				ans=Math.max(ans, toAdd+1);
+			}
+			System.out.println(ans);
+		}
+	}
+
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
