22e35e02f8d1a4674b89f077398a8b217528b354
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 15:52:29 +0530

    romanandreev

---
+++ romanandreev/normal/172/A.cpp
@@ -0,0 +1,67 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <cstdarg>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <complex>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <ctime>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+
+
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c).size()
+
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define x first
+#define y second
+#define y1 y1_gdssdfjsdgf
+#define y0 y0_fsdjfsdogfs
+#define ws ws_fdfsdfsdgfs
+#define image(a) {sort(all(a)),a.resize(unique(all(a))-a.begin());}
+#define eprintf(...) {fprintf(stderr,__VA_ARGS__),fflush(stderr);}
+
+#define forn(i,n) for( int i = 0 ; i < (n) ; i++ )
+#define forit(it,c) for( __typeof((c).begin()) it = (c).begin() ; it != (c).end() ; it++ )
+#define problem_name "a"
+int n;
+char s[1000];
+char t[1000];
+
+int main(){
+	#ifdef home
+	assert(freopen(problem_name".out","wt",stdout));
+	assert(freopen(problem_name".in","rt",stdin));
+	#endif
+	scanf("%d\n", &n);
+	gets(s);
+	int m = strlen(s);
+	for (int i = 1; i < n; i++) {
+		gets(t);
+		for (int j = 0; j < m; j++) {
+			if (t[j] != s[j]) {
+				m = j;
+				break;
+			}
+		}
+	}
+	printf("%d\n", m);
+	return 0;
+}
