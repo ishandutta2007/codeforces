9b7b15cff13b426a60cf783b6844ebd969455c44
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 19:46:10 +0530

    secondthread

---
+++ secondthread/normal/1516/C.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class C {
+
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int n=fs.nextInt();
+		int[] a=fs.readArray(n);
+		boolean[] canReach=new boolean[2001*100];
+		canReach[0]=true;
+		for (int cost:a) {
+			for (int to=canReach.length-cost-1; to>=0; to--) {
+				if (!canReach[to]) continue;
+				canReach[to+cost]=true;
+			}
+		}
+		int sum=0;
+		for (int i:a) sum+=i;
+		if (!canReach[sum/2] || sum%2!=0) {
+			System.out.println(0);
+			return;
+		}
+		int trailingZeros=31;
+		for (int i:a) trailingZeros=Math.min(trailingZeros, Integer.numberOfTrailingZeros(i));
+		int ans=-2;
+		for (int i=0; i<n; i++) {
+			if (a[i]%(1<<trailingZeros+1)!=0) ans=i;
+		}
+		System.out.println(1+"\n"+(ans+1));
+	}
+
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
