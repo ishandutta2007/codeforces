3ea832d5f56b1ed23767425a5f4ae9372555f7f4
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 16:28:08 +0530

    enchom

---
+++ enchom/normal/1537/B.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdio.h>
+using namespace std;
+
+int t;
+int n, m, x, y;
+
+int main()
+{
+    int test;
+    int i, j;
+
+    scanf("%d", &t);
+
+    for (test=1;test<=t;test++)
+    {
+        scanf("%d %d %d %d", &n, &m, &x, &y);
+
+        printf("%d %d %d %d\n", 1, 1, n, m);
+    }
+}
