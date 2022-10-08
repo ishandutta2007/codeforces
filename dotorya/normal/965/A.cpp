b51cab475f5cdebd4da1ea797128829d7f49ad1d
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 12:44:12 +0530

    dotorya

---
+++ dotorya/normal/965/A.cpp
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+
+using namespace std;
+
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+
+typedef tuple<int, int, int> t3;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <db, db> pdd;
+
+int IT_MAX = 1 << 18; // segment tree size
+const ll MOD = 1000000007;
+const int INF = 0x3f3f3f3f; // memset(dp, 0x3f, sizeof(dp)), 2*INF < INT_MAX
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const db PI = acos(-1);
+const db ERR = 1e-10; // double EPS
+
+int main() {
+	ll K, N, S, P;
+	scanf("%lld %lld %lld %lld", &K, &N, &S, &P);
+
+	ll t = (N + S - 1) / S;
+	ll ans = (K*t + P - 1) / P;
+	return !printf("%lld\n", ans);
+}
