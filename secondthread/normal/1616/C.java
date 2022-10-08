cde82316f0d1123ff5968abbdb05943ff0dee6a3
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 19:28:16 +0530

    secondthread

---
+++ secondthread/normal/1616/C.java
@@ -0,0 +1,137 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/*
+
+1
+7
+1 2 3 4 5 6 7
+
+1
+5
+1 2 4 8 16
+
+1
+6
+0 10 100 3 1000 5
+ */
+public class C {
+
+	
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		PrintWriter out=new PrintWriter(System.out);
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int n=fs.nextInt();
+			int[] a=fs.readArray(n);
+			int maxKeep=1;
+			for (int l=0; l<n; l++) {
+				for (int r=l+1; r<n; r++) {
+					int lVal=a[l], rVal=a[r];
+					int dx=(r-l);
+					int dy=rVal-lVal;
+					int gcd=Math.abs(gcd(dx, dy));
+					dx/=gcd;
+					dy/=gcd;
+					int count=0;
+					for (int i=0; i<n; i++) {
+						int myDx=i-l;
+						if (myDx%dx!=0) continue;
+						int targetY=lVal+dy*(myDx/dx);
+						if (targetY==a[i]) count++;
+					}
+					maxKeep=Math.max(maxKeep, count);
+				}
+			}
+			System.out.println(n-maxKeep);
+		}
+	}
+	
+	static int gcd(int a, int b) {
+		return b==0?a:gcd(b, a%b);
+	}
+	
+	static final Random random=new Random();
+	static final int mod=1_000_000_007;
+	
+	static void ruffleSort(int[] a) {
+		int n=a.length;//shuffle, then sort 
+		for (int i=0; i<n; i++) {
+			int oi=random.nextInt(n), temp=a[oi];
+			a[oi]=a[i]; a[i]=temp;
+		}
+		Arrays.sort(a);
+	}
+	static long add(long a, long b) {
+		return (a+b)%mod;
+	}
+	static long sub(long a, long b) {
+		return ((a-b)%mod+mod)%mod;
+	}
+	static long mul(long a, long b) {
+		return (a*b)%mod;
+	}
+	static long exp(long base, long exp) {
+		if (exp==0) return 1;
+		long half=exp(base, exp/2);
+		if (exp%2==0) return mul(half, half);
+		return mul(half, mul(half, base));
+	}
+	static long[] factorials=new long[2_000_001];
+	static long[] invFactorials=new long[2_000_001];
+	static void precompFacts() {
+		factorials[0]=invFactorials[0]=1;
+		for (int i=1; i<factorials.length; i++) factorials[i]=mul(factorials[i-1], i);
+		invFactorials[factorials.length-1]=exp(factorials[factorials.length-1], mod-2);
+		for (int i=invFactorials.length-2; i>=0; i--)
+			invFactorials[i]=mul(invFactorials[i+1], i+1);
+	}
+	
+	static long nCk(int n, int k) {
+		return mul(factorials[n], mul(invFactorials[k], invFactorials[n-k]));
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
