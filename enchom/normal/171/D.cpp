8bb71a5ebf1fa58f29a820d67eae805252125f98
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 18:58:02 +0530

    enchom

---
+++ enchom/normal/171/D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int k;
+    cin>>k;
+    if (k==3 || k==5)
+    cout<<1<<endl;
+    if (k==1 || k==4)
+    cout<<2<<endl;
+    if (k==2)
+    cout<<3<<endl;
+    return 0;
+}
