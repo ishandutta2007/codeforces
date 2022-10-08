94ca3443fa639039e724dfcd17f6f9816b719e8e
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 17:34:50 +0530

    dotorya

---
+++ dotorya/normal/837/A.cpp
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:1048576")
+using namespace std;
+
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define ldb ldouble
+#define rf(x) (x)=0;while(*p<48)p++;while(47<*p)(x)=((x)<<3)+((x)<<1)+(*p++&15);
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <db, db> pdd;
+typedef tuple <int, int, int> t3;
+
+int IT_MAX = 1 << 18;
+const ll MOD = 1000000009;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 1234567890123456789ll;
+const db PI = acos(-1);
+const db EPS = 1e-8;
+#define szz(x) (int)(x).size()
+#define rep(i, n) for(int i=0;i<(n);i++)
+
+char u[10005];
+int main() {
+	int N;
+	scanf("%d", &N);
+
+	int ans = 0;
+	while (scanf("%s", u) != EOF) {
+		int c = 0, i;
+		for (i = 0; u[i] != 0; i++) if (u[i] >= 'A' && u[i] <= 'Z') c++;
+		ans = max(ans, c);
+	}
+	return !printf("%d\n", ans);
+}
