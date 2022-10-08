87c24ac937eb186eb034c08108c356ef32903e5f
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 16:20:04 +0530

    ktuan

---
+++ ktuan/normal/20/A.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <math.h>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <map>
+#include <string>
+#include <set>
+#include <sstream>
+#include <queue>
+#include <stack>
+
+#define FOR(i,a,b) for(int i = a; i <= b; i++) 
+#define DOWN(i,a,b) for(int i = a; i >= b; i--)
+#define FORV(i,a) for(typeof(a.begin()) i = a.begin(); i != a.end(); i++)
+#define FR(i,a) for(int i = 0; i < a; i++)
+#define DR(i,a) for(int i = a-1; i >= 0; i--)
+#define REP(i,a) for(int i = 0; i < a; i++)
+#define Rep(i,a) for(int i = 0; i < a; i++)
+#define For(i,a,b) for(int i = a; i <= b; i++)
+
+#define sqr(x) (x)*(x)
+#define dout debug && cout 
+#define ll long long
+#define sz size()
+#define ull unsigned long long
+#define pb push_back
+#define oo 1000000002
+/* DEBUGGING */
+bool debug = false;
+/* MAIN PROGRAM */
+
+using namespace std;
+/*************************TEMPLATE**********************************/
+long long convertToNum(string s)
+{
+    long long val = 0; FR(i,s.size()) val = val * 10 + s[i] - '0';
+    return val;
+}
+string convertToString(long long a) {
+    string res = ""; if (!a) return "0";
+    while (a) { res = (char)(a % 10 + 48) + res;  a /= 10; }
+    return res;
+}
+long long GCD(long long x,long long y)  {
+    if (!x) return y; if (!y) return x;
+    if (x == y) return x; if (x < y) return GCD(x,y%x); else return GCD(x%y,y);
+}
+long long POW(long long x,long long y,long long Base){
+    if (!y) return 1; long long u = POW(x,y/2,Base);
+    u = (u * u) % Base;
+    if (y & 1) return (u * x) % Base; else return u;
+}
+/**************************CODE HERE*****************************/
+
+int main()
+{
+//    freopen("test.in","r",stdin);
+  //  freopen("test.out","w",stdout);
+    string s;
+    cin >> s;
+    string res = "@";
+    FR(i,s.size())
+    if (s[i] != '/') res += s[i];
+    else if (res[res.size()-1] != '/') res += '/';
+    int cnt = 0;
+    FOR(i,1,res.size()-1)
+    if (s[i] == '/') cnt++;
+    if (res.size() >= 3 && res[res.size()-1] == '/')
+    FOR(i,1,res.size()-2) cout << res[i];
+    else
+    FOR(i,1,res.size()-1) cout << res[i];
+//    system("pause");
+    return 0;
+}
