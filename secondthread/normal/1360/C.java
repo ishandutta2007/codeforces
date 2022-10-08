8074d9cd1eea0110911b8bc334abf8b017ba03f1
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 20:23:12 +0530

    secondthread

---
+++ secondthread/normal/1360/C.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class C {
+
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int n=fs.nextInt();
+			int[] a=fs.readArray(n);
+			Arrays.sort(a);
+			int nEven=0;
+			int last=-2;
+			boolean foundPair=false;
+			for (int i:a) {
+				if (i==last+1) foundPair=true;
+				if (i%2==0) nEven++;
+				last=i;
+			}
+			if (nEven%2==0 || foundPair) {
+				System.out.println("YES");
+			}
+			else System.out.println("NO");
+		}
+	}
+
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
