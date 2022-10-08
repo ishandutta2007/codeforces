e1e58b98515485a3e1a052e1d906f7b7f020677f
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 10:22:42 +0530

    winger

---
+++ winger/normal/23/B.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	private BufferedReader in;
+	private PrintWriter out;
+	private StringTokenizer st;
+
+	void solve() throws IOException {
+		int tests = nextInt();
+		for (int test = 0; test < tests; ++test) {
+			int n = nextInt();
+			out.println(Math.max(0, n - 2));
+		}
+	}
+
+	Solution() throws IOException {
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		
+		eat("");
+		
+		solve();
+		
+		in.close();
+		out.close();
+	}
+	
+	private void eat(String str) {
+		st = new StringTokenizer(str);
+	}
+	
+	String next() throws IOException {
+		while (!st.hasMoreTokens()) {
+			String line = in.readLine();
+			if (line == null) {
+				return null;
+			}
+			eat(line);
+		}
+		return st.nextToken();
+	}
+	
+	int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	long nextLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	double nextDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution();
+	}
+}
