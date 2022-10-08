b5aae622ecf74c4020d6b0f2b055c500bba1c1c5
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 15:48:38 +0530

    romanandreev

---
+++ romanandreev/normal/306/A.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <string>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <ctime>
+#include <cassert>
+
+#define x first
+#define y second
+#define pb push_back
+#define mp make_pair
+#define forn(i, n) for(int i = 0 ; (i) < (n) ; ++i)
+#define forit(it,v) for(typeof((v).begin()) it = v.begin() ; it != (v).end() ; ++it)
+#define eprintf(...) fprintf(stderr, __VA_ARGS__),fflush(stderr)
+#define sz(a) ((int)(a).size())
+#define all(a) (a).begin(),a.end()
+
+using namespace std;
+
+typedef long long ll;
+typedef double ld;
+typedef vector<int> vi;
+typedef pair<int, int> pi;
+
+const int inf = (int)1e9;
+const ld eps = 1e-9;
+
+
+#define TASK "a"
+int main()
+{
+    #ifdef home
+        assert(freopen(TASK".in", "r", stdin));
+        assert(freopen(TASK".out", "w", stdout));
+    #endif
+    int n, m;
+    scanf("%d %d", &n, &m);
+    for (int i = 0; i < n % m; i++) {
+        printf("%d ", n / m + 1);
+    }
+    for (int i = 0; i < m - n % m; i++) {
+        printf("%d ", n / m);
+    }
+
+    #ifdef home
+        eprintf("Time: %d ms\n", (int)(clock() * 1000. / CLOCKS_PER_SEC));
+    #endif
+    return 0;
+}
