426e5c18970dfce452f6c2c04aedeac3491bee4f
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 10:25:39 +0530

    winger

---
+++ winger/normal/4/C.java
@@ -0,0 +1,67 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+	
+	private BufferedReader in;
+	private PrintWriter out;
+	private StringTokenizer st;
+
+	void solve() throws IOException {
+		int n = nextInt();
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		for (int i = 0; i < n; ++i) {
+			String s = next();
+			if (map.containsKey(s)) {
+				out.println(s + map.get(s));
+				map.put(s, map.get(s) + 1);
+			} else {
+				out.println("OK");
+				map.put(s, 1);
+			}
+		}
+	}
+
+	Solution() throws IOException {
+		in = new BufferedReader(new InputStreamReader(System.in));
+		out = new PrintWriter(System.out);
+		
+		eat("");
+		
+		solve();
+		
+		in.close();
+		out.close();
+	}
+	
+	private void eat(String str) {
+		st = new StringTokenizer(str);
+	}
+	
+	String next() throws IOException {
+		while (!st.hasMoreTokens()) {
+			String line = in.readLine();
+			if (line == null) {
+				return null;
+			}
+			eat(line);
+		}
+		return st.nextToken();
+	}
+	
+	int nextInt() throws IOException {
+		return Integer.parseInt(next());
+	}
+	
+	long nextLong() throws IOException {
+		return Long.parseLong(next());
+	}
+	
+	double nextDouble() throws IOException {
+		return Double.parseDouble(next());
+	}
+
+	public static void main(String[] args) throws IOException {
+		new Solution();
+	}
+}
