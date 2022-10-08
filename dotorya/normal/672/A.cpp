a2a6ba623e6b68cc54353a2633e3d2af66ca95ed
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 14:02:23 +0530

    dotorya

---
+++ dotorya/normal/672/A.cpp
@@ -0,0 +1,59 @@
+#include <algorithm>
+#include <assert.h>
+#include <complex>
+#include <ctype.h>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <locale.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+#pragma warning(disable:4996)
+using namespace std;
+
+#define mp make_pair
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <ldb, ldb> pdd;
+
+int IT_MAX = 1 << 17;
+const ll MOD = 1000000007;
+const int INF = 1034567890;
+const ll LL_INF = 1234567890123456789ll;
+const db PI = acos(-1);
+const ldb ERR = 1E-8;
+
+vector <int> V;
+vector <int> V2;
+int main() {
+	int i;
+	for (i = 1; i <= 1000; i++) {
+		int t = i;
+		while (t) {
+			V2.push_back(t % 10);
+			t /= 10;
+		}
+		while (!V2.empty()) {
+			V.push_back(V2.back());
+			V2.pop_back();
+		}
+	}
+	scanf("%d", &i);
+	printf("%d\n", V[i - 1]);
+	return 0;
+}
