7a417f472cc0c1152c49baa11e9b4bc82cdfc74a
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 14:00:25 +0530

    dotorya

---
+++ dotorya/normal/681/B.cpp
@@ -0,0 +1,53 @@
+//*
+#include <algorithm>
+#include <assert.h>
+#include <complex>
+#include <ctype.h>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <locale.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+#pragma warning(disable:4996)
+using namespace std;
+
+#define mp make_pair
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <ldb, ldb> pdd;
+
+int IT_MAX = 4096;
+const ll MOD = 1000000007;
+const int INF = 1034567890;
+const ll LL_INF = 1234567890123456789ll;
+const db PI = acos(-1);
+const ldb ERR = 1E-10;
+
+int main() {
+	ll N, i, j;
+	scanf("%lld", &N);
+	for (i = 0; i <= 1000; i++) {
+		for (j = 0; j <= 10000; j++) {
+			ll t = N - i * 1234567 - j * 123456;
+			if (t < 0) continue;
+			if (t % 1234 == 0) return !printf("YES\n");
+		}
+	}
+	return !printf("NO\n");
+}
