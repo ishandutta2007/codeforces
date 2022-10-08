629e24b3523eebaa89a2e4feb3e2c61e0e929632
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 11:27:50 +0530

    burunduk1

---
+++ burunduk1/normal/89/A.cpp
@@ -0,0 +1,85 @@
+/**
+ * Author: Sergey Kopeliovich (Burunduk30@gmail.com)
+ */
+
+#include <ctime>
+#include <cmath>
+#include <cctype>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cassert>
+
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <functional>
+#include <map>
+#include <set>
+#include <fstream>
+#include <queue>
+
+using namespace std;
+
+#ifdef _WIN32
+#  define I64 "%I64d"
+#else
+#  define I64 "%Ld"
+#endif
+
+#define forn(i, n) for (int i = 0; i < (int)(n); i++)
+#define fornd(i, n) for (int i = (int)(n) - 1; i >= 0; i--)
+#define forab(i, a, b) for (int i = (int)(a); i <= (int)(b); i++)
+#define forabd(i, a, b) for (int i = (int)(b); i >= (int)(a); i--)
+#define forit(i, a) for (__typeof((a).begin()) i = (a).begin(); i != (a).end(); i++)
+#define sz(a) (int)(a).size()
+#define all(a) (a).begin(), (a).end()
+#define zero(a) memset(a, 0, sizeof(a))
+#define pb push_back
+#define mp make_pair
+
+#define EOL(i, n) " \n"[i == (n) - 1]
+#define LEN(a) (int)(sizeof(a) / sizeof(a[0]))
+#define IS(a, i) (((a) >> (i)) & 1)
+
+typedef double dbl;
+typedef long long ll;
+typedef vector <int> vi;
+typedef pair <int, int> pii;
+typedef unsigned char byte;
+
+template <class T> T inline sqr(T x) { return x * x; }
+template <class T> inline void relaxMin( T &a, T b ) { a = min(a, b); }
+template <class T> inline void relaxMax( T &a, T b ) { a = max(a, b); }
+
+string str( int i ) { char s[100]; sprintf(s, "%d", i); return string(s); }
+
+template <class T> inline T sign( T x ) { return x > 0 ? 1 : (x < 0 ? -1 : 0); }
+template <class T> inline T myAbs( T a ) { return a > 0 ? a : -a; }
+
+const int maxn = (int)1e5 + 10;
+int n, m, k, a[maxn];
+
+int main()
+{
+  scanf("%d%d%d", &n, &m, &k);
+  forn(i, n)
+    scanf("%d", &a[i]);
+
+  int need = (n + 1) / 2;
+  if (n % 2 == 0 || m < need)
+  {
+    puts("0");
+    return 0;
+  }
+
+  ll sum = (ll)(m / need) * k;
+  //fprintf(stderr, "sum = %I64d\n", sum);
+  int Min = a[0];
+  for (int i = 0; i < n; i += 2)
+    Min = min(Min, a[i]);
+  printf("%d\n", (int)min((ll)Min, sum)); 
+  return 0;
+}
