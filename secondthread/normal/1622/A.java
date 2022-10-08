ab29d6e79e484cf57db0788d9d5a2362db6ff74e
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 19:29:16 +0530

    secondthread

---
+++ secondthread/normal/1622/A.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+/*
+
+ */
+public class A {
+
+	
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		PrintWriter out=new PrintWriter(System.out);
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int[] a=fs.readArray(3);
+			Arrays.sort(a);
+			if (a[0]%2==0 && a[1]==a[2]) {
+				System.out.println("YES");
+			}
+			else if (a[2]%2==0 && a[0]==a[1]) {
+				System.out.println("YES");
+			}
+			else if (a[0]+a[1]==a[2]) {
+				System.out.println("YES");
+			}
+			else {
+				System.out.println("NO");
+			}
+		}
+	}
+	
+	static final Random random=new Random();
+	static final int mod=1_000_000_007;
+	
+	static void ruffleSort(int[] a) {
+		int n=a.length;//shuffle, then sort 
+		for (int i=0; i<n; i++) {
+			int oi=random.nextInt(n), temp=a[oi];
+			a[oi]=a[i]; a[i]=temp;
+		}
+		Arrays.sort(a);
+	}
+	static long add(long a, long b) {
+		return (a+b)%mod;
+	}
+	static long sub(long a, long b) {
+		return ((a-b)%mod+mod)%mod;
+	}
+	static long mul(long a, long b) {
+		return (a*b)%mod;
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> l=new ArrayList<>();
+		for (int i:a) l.add(i);
+		Collections.sort(l);
+		for (int i=0; i<a.length; i++) a[i]=l.get(i);
+	}
+	
+	
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
