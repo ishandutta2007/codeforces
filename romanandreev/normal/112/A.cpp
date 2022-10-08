ab5106b45ab0b81835557d1d6fadeb27886c66b5
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 15:55:03 +0530

    romanandreev

---
+++ romanandreev/normal/112/A.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <cstdarg>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <complex>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <ctime>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+
+
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c).size()
+
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define y1 y1_gdssdfjsdgf
+#define y0 y0_fsdjfsdogfs
+#define ws ws_fdfsdfsdgfs
+#define image(a) {sort(all(a)),a.resize(unique(all(a))-a.begin());}
+#define eprintf(...) {fprintf(stderr,__VA_ARGS__),fflush(stderr);}
+
+#define forn(i,n) for( int i = 0 ; i < (n) ; i++ )
+#define forit(it,c) for( __typeof((c).begin()) it = (c).begin() ; it != (c).end() ; it++ )
+
+string lower(string s) {
+	for (int i = 0; i < sz(s); i++) {
+		if (s[i] >= 'A' && s[i] <= 'Z') {
+			s[i] = s[i] - 'A' + 'a';
+		}
+	}
+	return s;
+}
+int main(){
+	#ifdef home
+	assert(freopen("a.out","wt",stdout));
+	assert(freopen("a.in","rt",stdin));
+	#endif
+	string s1, s2;      
+	cin>>s1>>s2;
+	s1 = lower(s1);
+	s2 = lower(s2);
+	if (s1 < s2) cout<<"-1"<<endl;
+	if (s1 > s2) cout<<"1"<<endl;
+	if (s1 == s2) cout<<"0"<<endl;
+	return 0;
+}
