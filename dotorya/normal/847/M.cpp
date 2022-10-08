74c41f20a0f1a8b43259d7365d3fd0d51988c402
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 13:16:29 +0530

    dotorya

---
+++ dotorya/normal/847/M.cpp
@@ -0,0 +1,62 @@
+#include <stdio.h>  
+#include <algorithm>  
+#include <assert.h>
+#include <bitset>
+#include <cmath>  
+#include <complex>  
+#include <deque>  
+#include <functional>  
+#include <iostream>  
+#include <limits.h>  
+#include <map>  
+#include <math.h>  
+#include <queue>  
+#include <set>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <string>  
+#include <time.h>  
+#include <unordered_map>  
+#include <unordered_set>  
+#include <vector>
+
+#pragma warning(disable:4996)  
+#pragma comment(linker, "/STACK:336777216")  
+using namespace std;
+
+#define mp make_pair  
+#define Fi first  
+#define Se second  
+#define pb(x) push_back(x)  
+#define szz(x) ((int)(x).size()) 
+#define all(x) (x).begin(), (x).end()  
+#define ldb ldouble  
+
+typedef tuple<int, int, int> t3;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <db, db> pdd;
+
+int IT_MAX = 1 << 19;
+const ll MOD = 1000000007;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const db PI = acos(-1);
+const db ERR = 1e-10;
+
+int in[1050];
+int main() {
+	int N, i;
+	scanf("%d", &N);
+	for (i = 1; i <= N; i++) scanf("%d", &in[i]);
+
+	for (i = 2; i < N; i++) if (in[i + 1] - in[i] != in[2] - in[1]) break;
+	if (i < N) return !printf("%d\n", in[N]);
+	else return !printf("%d\n", in[N] * 2 - in[N - 1]);
+	return 0;
+}
