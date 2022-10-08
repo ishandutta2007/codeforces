0599482bfe3c0e85db2654f3cc6da5b38f53ea70
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 19:52:37 +0530

    secondthread

---
+++ secondthread/normal/1487/A.java
@@ -0,0 +1,70 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Random;
+import java.util.StringTokenizer;
+
+//Good morning!
+public class A {
+
+	public static void main(String[] args) {
+		FastScanner fs=new FastScanner();
+		int T=fs.nextInt();
+		for (int tt=0; tt<T; tt++) {
+			int[] a=fs.readArray(fs.nextInt());
+			int n=a.length;
+			ruffleSort(a);
+			int ans=0;
+			for (int i:a) if (i!=a[0]) ans++;
+			System.out.println(ans);
+		}
+	}
+
+	// Use this instead of Arrays.sort() on an array of ints. Arrays.sort() is n^2
+	// worst case since it uses a version of quicksort. Although this would never
+	// actually show up in the real world, in codeforces, people can hack, so
+	// this is needed.
+	static void ruffleSort(int[] a) {
+		//ruffle
+		int n=a.length;
+		Random r=new Random();
+		for (int i=0; i<a.length; i++) {
+			int oi=r.nextInt(n), temp=a[i];
+			a[i]=a[oi];
+			a[oi]=temp;
+		}
+		
+		//then sort
+		Arrays.sort(a);
+	}
+	
+	// Use this to input code since it is faster than a Scanner
+	static class FastScanner {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st=new StringTokenizer("");
+		String next() {
+			while (!st.hasMoreTokens())
+				try {
+					st=new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		int[] readArray(int n) {
+			int[] a=new int[n];
+			for (int i=0; i<n; i++) a[i]=nextInt();
+			return a;
+		}
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+	}
+
+	
+}
