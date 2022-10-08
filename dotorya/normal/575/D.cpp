0cae682fc8bc91139102e90a1ff3dbd44532af3a
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 14:19:04 +0530

    dotorya

---
+++ dotorya/normal/575/D.cpp
@@ -0,0 +1,35 @@
+//*
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <string>
+#include <functional>
+#define MOD 1000000007
+#define MAX ((1<<30)-1)
+#define MAX2 ((1ll<<62)-1)
+#define mp make_pair
+#pragma warning(disable:4996)
+using namespace std;
+
+typedef long long ll;
+typedef unsigned int ui;
+typedef long double ldb;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<double, double> pdd;
+
+int main()
+{
+	int i, j, k;
+	printf("%d\n", 2000);
+	for(i=0;i<1000;i++) printf("%d %d %d %d\n", i+1, 1, i+1, 2);
+	for(i=999;i>=0;i--) printf("%d %d %d %d\n", i+1, 1, i+1, 2);
+	return 0;
+}
+//*/
