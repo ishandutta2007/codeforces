1ddbe3e8d711c4efffb7bc66a66a6f60c924d1b7
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 13:28:26 +0530

    dotorya

---
+++ dotorya/normal/725/B.cpp
@@ -0,0 +1,66 @@
+#include <stdio.h>  
+#include <algorithm>  
+#include <assert.h>
+#include <bitset>
+#include <cmath>  
+#include <complex>  
+#include <deque>  
+#include <functional>  
+#include <iostream>  
+#include <limits.h>  
+#include <map>  
+#include <math.h>  
+#include <queue>  
+#include <set>  
+#include <stdlib.h>  
+#include <string.h>  
+#include <string>  
+#include <time.h>  
+#include <unordered_map>  
+#include <unordered_set>  
+#include <vector>  
+
+#pragma warning(disable:4996)  
+#pragma comment(linker, "/STACK:336777216")  
+using namespace std;
+
+#define mp make_pair  
+#define all(x) (x).begin(), (x).end()  
+#define ldb ldouble
+
+typedef tuple<int, int, int> t3;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <db, db> pdd;
+
+int IT_MAX = 1 << 17;
+const ll MOD = 1000000009;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const db PI = acos(-1);
+const db ERR = 1e-10;
+#define szz(x) (int)(x).size()
+#define rep(i, n) for(int i=0;i<n;i++)
+
+char in[105];
+int main() {
+	scanf("%s", in);
+	int L = strlen(in);
+
+	ll v = 0, i;
+	for (i = 0; i <= L - 2; i++) v = v * 10 + in[i] - '0';
+
+	v--;
+	
+	ll ans = (v / 4) * 16;
+	if (v % 4 == 1 || v % 4 == 3) ans += 7;
+	
+	if (in[L - 1] >= 'd') ans += 'g' - in[L - 1];
+	else ans += in[L - 1] - 'a' + 4;
+	return !printf("%lld\n", ans);
+}
