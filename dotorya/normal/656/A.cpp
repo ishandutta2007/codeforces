e145bb1420e07f3882a312404a40e77d80083e2a
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 14:05:59 +0530

    dotorya

---
+++ dotorya/normal/656/A.cpp
@@ -0,0 +1,48 @@
+#include <algorithm>
+#include <assert.h>
+#include <complex>
+#include <ctype.h>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <locale.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <vector>
+#include <unordered_set>
+#include <unordered_map>
+
+#pragma warning(disable:4996)
+using namespace std;
+
+#define mp make_pair
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <ldb, ldb> pdd;
+
+int IT_MAX = 1 << 17;
+const ll MOD = 987654321;
+const int INF = 1034567890;
+const ll LL_INF = 3234567890123456789ll;
+const db PI = acos(-1);
+const ldb ERR = 1E-10;
+
+ll in[100] = { 1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8092, 16184, 32368, 64736, 129472, 258944, 517888, 1035776, 2071552, 4143104, 8286208, 16572416, 33144832, 66289664, 132579328, 265158656, 530317312, 1060634624, 2121269248, 4242538496, 8485076992, 16970153984, 33940307968 };
+int main() {
+	int N;
+	scanf("%d", &N);
+	printf("%lld\n", in[N]);
+	return 0;
+}
+//*/
