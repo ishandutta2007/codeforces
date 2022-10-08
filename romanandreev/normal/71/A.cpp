54b8799a6f0c597a656714fd49181a16f364ac69
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 15:55:30 +0530

    romanandreev

---
+++ romanandreev/normal/71/A.cpp
@@ -0,0 +1,66 @@
+#include <algorithm>
+#include <iostream>
+#include <cstdlib>
+#include <cstdarg>
+#include <cassert>
+#include <climits>
+#include <cstring>
+#include <complex>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <queue>
+#include <cmath>
+#include <ctime>
+#include <set>
+#include <map>
+
+using namespace std;
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+
+
+#define all(c) (c).begin(),(c).end()
+#define sz(c) (int)(c).size()
+
+#define pb push_back
+#define mp make_pair
+#define fs first
+#define sc second
+#define y1 y1_gdssdfjsdgf
+#define y0 y0_fsdjfsdogfs
+#define ws ws_fdfsdfsdgfs
+#define image(a) {sort(all(a)),a.resize(unique(all(a))-a.begin());}
+#define eprintf(...) {fprintf(stderr,__VA_ARGS__),fflush(stderr);}
+
+#define forn(i,n) for( int i = 0 ; i < (n) ; i++ )
+#define forit(it,c) for( __typeof((c).begin()) it = (c).begin() ; it != (c).end() ; it++ )
+
+#ifdef WIN32
+#define INT64 "%I64""d"
+#else
+#define INT64 "%ll""d"
+#endif
+
+
+int main(){
+	#ifdef home
+	assert(freopen("a.out","wt",stdout));
+	assert(freopen("a.in","rt",stdin));
+	#endif 
+	string s;
+	int n;
+	cin>>n;
+	for (int i = 0; i < n; i++) {
+		cin>>s;
+		if (sz(s) <= 10) {
+			cout<<s<<endl;
+		} else {
+			cout<<s[0]<<sz(s) - 2<<s[sz(s) - 1]<<endl;
+		}
+	}
+	return 0;
+}
