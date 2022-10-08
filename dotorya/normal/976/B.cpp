60a506bbd647649f84e6198f42437698ada40e7a
ishan dutta <ishandutta2007@gmail.com>
2022-10-08 12:45:08 +0530

    dotorya

---
+++ dotorya/normal/976/B.cpp
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+#include <cmath>
+#include <complex>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/STACK:336777216")
+using namespace std;
+
+#define mp make_pair
+#define all(x) (x).begin(), (x).end()
+#define ldb ldouble
+
+typedef tuple<int, int, int> t3;
+typedef long long ll;
+typedef unsigned long long ull;
+typedef double db;
+typedef long double ldb;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef pair <ll, int> pli;
+typedef pair <db, db> pdd;
+
+int IT_MAX = 1 << 17;
+const ll MOD = 998244353;
+const int INF = 0x3f3f3f3f;
+const ll LL_INF = 0x3f3f3f3f3f3f3f3f;
+const db PI = acos(-1);
+const db ERR = 1e-10;
+#define szz(x) (int)(x).size()
+#define rep(i, n) for(int i=0;i<n;i++)
+#define Se second
+#define Fi first
+
+int main() {
+	ll N, M, K;
+	scanf("%lld %lld %lld", &N, &M, &K);
+	if (K < N) return !printf("%lld 1\n", K + 1);
+
+	K -= N;
+
+	ll a1 = N - (K / (M - 1));
+	ll a2 = 0;
+	if (a1 % 2 == N % 2) a2 = 2 + K % (M - 1);
+	else a2 = M - K % (M - 1);
+	return !printf("%lld %lld\n", a1, a2);
+}
