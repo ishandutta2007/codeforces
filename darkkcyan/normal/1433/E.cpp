d206a8a449333665d63735bc3fb83f354b63e4a0
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 03:33:29 +0530

    darkkcyan

---
+++ darkkcyan/normal/1433/E.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define llong long long 
+#define len(x) ((int)x.size())
+#define rep(i,n) for (int i = -1; ++ i < n; )
+#define rep1(i,n) for (int i = 0; i ++ < n; )
+#ifdef testing
+mt19937 rng(177013);
+#else
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());
+#endif
+#define rand() (int)(rng() >> 1)
+#define CONCAT_(x, y) x##y/*{{{*/
+#define CONCAT(x, y) CONCAT_(x, y)
+#define SPEC(name) CONCAT(name, __LINE__)
+#ifdef LOCAL_DEBUG   
+int __db_level = 0;
+#define clog cerr << string(__db_level * 2, ' ')
+struct debug_block {
+    string msg;
+    debug_block(const string& s): msg(s) { clog << "{ " << msg << endl; ++__db_level; }
+    ~debug_block() { --__db_level; clog << "} " << msg << endl; }
+};
+#define DB(args...) stringstream SPEC(ss); SPEC(ss)<< args; debug_block SPEC(dbbl)(SPEC(ss).str())
+#else
+#define clog if (0) cerr
+#define DB(...)
+#endif
+#define db(val) "[" #val " = " << val << "]; "
+template<class U, class V> ostream& operator<<(ostream& out, const pair<U, V>& p) {
+    return out << "(" << p.first << ", " << p.second << ")";
+}
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")";
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup);
+}
+template<class ...U>
+ostream& operator<<(ostream& out, const tuple<U...>& tup) { return print_tuple_utils<0, tuple<U...>>(out, tup); }
+template<class, typename = void> struct has_const_iterator : false_type {};
+template<class T> struct has_const_iterator<T, void_t<typename T::const_iterator>> : true_type {};
+template<class T>
+typename enable_if<has_const_iterator<T>::value && !is_same<T, string>::value, ostream&>::type
+operator<<(ostream& out, const T& container) { 
+    auto beg = container.begin(), end = container.end();
+    out << "(" << container.size() << ") {";
+    if (beg != end) out << *(beg++);
+    while (beg != end) out << ", " << *(beg++);
+    return out << "}";
+}
+#define ptrtype(x) typename iterator_traits<x>::value_type
+template<class u> vector<ptrtype(u)> $v(u a, u b) { return vector<ptrtype(u)>(a, b); }/*}}}*/
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////
+
+using ul = unsigned long long;
+ul fac[21];
+
+ul choose(int n, int k) {
+    if (n < 0 or k > n) return 0;
+    return fac[n] / fac[k] / fac[n - k];
+}
+
+int main(void) {
+#ifdef LOCAL
+    freopen("main.inp", "r", stdin);
+    freopen("main.out", "w", stdout);
+    freopen(".log", "w", stderr);
+#endif
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    fac[0] = 1;
+    rep1(i, 20) fac[i] = fac[i - 1] * i;
+    
+    int n; cin >> n;
+    ul pick = choose(n - 1, n / 2);
+    ul cycle = fac[n / 2 - 1];
+    ul ans = pick * cycle * cycle;
+    cout << ans;
+    
+    return 0;
+}
+
+// vim: foldmethod=marker
