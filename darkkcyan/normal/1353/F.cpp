34742e5db4e4355a2104338d47356bb1d9ed2efd
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 04:01:06 +0530

    darkkcyan

---
+++ darkkcyan/normal/1353/F.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define llong long long 
+#define len(x) ((int)x.size())
+#define rep(i,n) for (int i = -1; ++ i < n; )
+#define rep1(i,n) for (int i = 0; i ++ < n; )
+#define rand __rand
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());  // or mt19937_64
+template<class T = int> T rand(T range = numeric_limits<T>::max()) { return (T)(rng() % range); }
+
+#define CONCAT_(x, y) x##y/*{{{*/
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+int __db_level = 0;
+bool __db_same_line = false;
+#define clog cerr << string(!__db_same_line ? __db_level * 2 : 0, ' ')
+struct debug_block {
+    function<void()> fn;
+    void print_name() { __db_same_line = true; fn(); clog << endl; __db_same_line = false;  }
+    debug_block(function<void()> fn_): fn(fn_) { clog << "{ "; print_name(); ++__db_level; }
+    ~debug_block() { --__db_level; clog << "} "; print_name(); }
+};
+#define DB(args...) debug_block CONCAT(dbbl, __LINE__)([=]{ clog << args; })
+#define deb(...)  if (1) { (clog << "[" #__VA_ARGS__ "] = [" << __VA_ARGS__) << "]"; if (!__db_same_line) clog << endl; }
+#else
+#define clog if (0) cerr
+#define DB(...)
+#define deb(...)
+#endif
+template<class T>
+ostream& operator,(ostream& out, const T& thing) { return out << ", " << thing; }
+template<class U, class V>
+ostream& operator<<(ostream& out, const pair<U, V>& p) { return (out << "(" << p.first, p.second) << ")"; }
+template<class A, class B>
+ostream& operator<<(ostream& out, const tuple<A, B>& t) { return (out << "(" << get<0>(t), get<1>(t)) << ")"; }
+template<class A, class B, class C>
+ostream& operator<<(ostream& out, const tuple<A, B, C>& t) { return (out << "(" << get<0>(t), get<1>(t), get<2>(t)) << ")"; }
+template<class T> ostream& operator<<(ostream& out, const vector<T>& container) { 
+    out << "{";
+    if (len(container)) out << container[0];
+    rep1(i, len(container) - 1) out, container[i];
+    return out << "}";
+}
+template<class x> vector<typename x::value_type> $v(const x& a) { return vector<typename x::value_type>(a.begin(), a.end()); }
+#define ptrtype(x) typename iterator_traits<x>::value_type
+template<class u> vector<ptrtype(u)> $v(u a, u b) { return vector<ptrtype(u)>(a, b); }/*}}}*/
+// ACTUAL SOLUTION BELOW ////////////////////////////////////////////////////////////
+
+const llong inf = LLONG_MAX;
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int ntest; cin >> ntest;
+    while (ntest--) {
+        int n, m; cin >> n >> m;
+        vector<vector<llong>> a(n, vector<llong>(m));
+        rep(i, n) {
+            rep(f, m) {
+                cin >> a[i][f];
+                a[i][f] -= i + f;
+            }
+        }
+
+        llong ans = inf ;
+        rep(i, n) rep(f, m) {
+            llong base = a[i][f];
+            if (base > a[0][0]) continue;
+            vector<vector<llong>> dp(n, vector<llong>(m, inf));
+            
+            dp[0][0] = 0;
+            rep(r, n) {
+                rep(c, m) {
+                    llong diff = a[r][c] - base;
+                    if (diff < 0) continue;
+                    if (r and dp[r - 1][c] != inf) dp[r][c] = dp[r - 1][c];
+                    if (c and dp[r][c - 1] != inf) dp[r][c] = min(dp[r][c], dp[r][c - 1]);
+                    if (dp[r][c] != inf) dp[r][c] += diff;
+                }
+            }
+            ans = min(ans, dp.back().back());
+        }
+        cout << ans << '\n';;
+    }
+
+    return 0;
+}
+
+// vim: foldmethod=marker
