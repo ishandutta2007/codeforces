578ff4c8d56aa8c05b27b993140472ef072a938f
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 22:17:07 +0530

    ivan100sic

---
+++ ivan100sic/normal/822/D.cpp
@@ -0,0 +1,216 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+template<int m>
+struct modint {
+
+	unsigned x;
+
+	modint() : x(0) {}
+
+	modint(long long arg) {
+		arg %= m;
+		if (arg < 0) {
+			x = arg + m;
+		} else {
+			x = arg;
+		}
+	}	
+
+	modint& operator+= (const modint& other) {
+		x += other.x;
+		if (x >= m) {
+			x -= m;
+		}
+		return *this;
+	}
+
+	modint& operator*= (const modint& other) {
+		x = (x * 1ll * other.x) % m;
+		return *this;
+	}
+
+	modint& operator-= (const modint& other) {
+		x += m - other.x;
+		if (x >= m) {
+			x -= m;
+		}
+		return *this;
+	}
+
+	modint operator+ (const modint& other) const {
+		modint tmp = *this;
+		tmp += other;
+		return tmp;
+	}
+
+	modint operator- (const modint& other) const {
+		modint tmp = *this;
+		tmp -= other;
+		return tmp;
+	}
+
+	modint operator* (const modint& other) const {
+		modint tmp = *this;
+		tmp *= other;
+		return tmp;
+	}
+
+	explicit operator int () const {
+		return x;
+	}
+
+	modint& operator++ () {
+		++x;
+		if (x == m) {
+			x = 0;
+		}
+		return *this;
+	}
+
+	modint& operator-- () {
+		if (x == 0) {
+			x = m-1;
+		} else {
+			--x;
+		}
+		return *this;
+	}
+
+	modint operator++ (int) {
+		modint tmp = *this;
+		++*this;
+		return tmp;
+	}
+
+	modint operator-- (int) {
+		modint tmp = *this;
+		--*this;
+		return tmp;
+	}
+
+	bool operator== (const modint& other) const {
+		return x == other.x;
+	}
+
+	bool operator!= (const modint& other) const {
+		return x != other.x;
+	}
+
+	template<class T>
+	modint operator^ (T arg) const {
+		if (arg == 0) {
+			return 1;
+		}
+		if (arg == 1) {
+			return x;
+		}
+		auto t = *this ^ (arg >> 1);
+		t *= t;
+		if (arg & 1) {
+			t *= *this;
+		}
+		return t;
+	}
+
+	modint inv() const {
+		return *this ^ (m-2);
+	}
+};
+
+const int MOD = 1'000'000'007;
+typedef modint<MOD> mint;
+
+
+const int SITO_MAX = 5000001;
+
+int f[SITO_MAX+1];
+vector<int> prosti;
+
+struct sito {
+	sito() {
+		for (int i=2; i<=SITO_MAX; i++) {
+			if (f[i] == 0) {
+				f[i] = i;
+				prosti.push_back(i);
+			}
+			int j = 0;
+			while (j < (int)prosti.size()) {
+				if (prosti[j] > f[i]) {
+					break;
+				}
+				int x = i * prosti[j];
+				if (x > SITO_MAX) {
+					break;
+				}
+				f[x] = prosti[j];
+				j++;
+			}
+		}
+	}
+} sito_obj_983431;
+
+vector<pair<int, int>> factor_small(int x) {
+	vector<pair<int, int>> v;
+	while (x > 1) {
+		int p = f[x];
+		int c = 0;
+		while (x % p == 0) {
+			x /= p;
+			c++;
+		}
+		v.push_back({p, c});
+	}
+	return v;
+}
+
+vector<pair<int, int>> factor(int x) {
+	vector<pair<int, int>> v;
+	for (int p : prosti) {
+		if (x % p == 0) {
+			int c = 0;
+			while (x % p == 0) {
+				x /= p;
+				c++;
+			}
+			v.push_back({p, c});
+		}			
+	}
+
+	if (x > 1) {
+		v.push_back({x, 1});
+	}
+	return v;
+}
+
+mint d[5000005], z;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cerr.tie(nullptr);
+
+	int t, l, r;
+	cin >> t >> l >> r;
+	mint tp = 1;
+
+	for (int i=2; i<l; i++) {
+		int g = f[i];
+		int b = i / f[i];
+		d[i] = mint(b) * (g * (g-1ll) / 2) + d[b];
+	}
+
+	for (int i=l; i<=r; i++) {
+		int g = f[i];
+		int b = i / f[i];
+		d[i] = mint(b) * (g * (g-1ll) / 2) + d[b];
+		z += tp * d[i];
+		tp *= t;
+	}
+	cout << (int)z << '\n';
+}
