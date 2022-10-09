898468009c552d4024da77dea9af80303803e357
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 02:53:28 +0530

    darkkcyan

---
+++ darkkcyan/normal/1557/B.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define constexpr(...) (__VA_ARGS__)  
+using ll = long long;
+using ld = long double;
+const ld eps = 1e-8;
+// for matching the kactl notes
+#define sz(x) ((int)x.size())
+#define rep(i,a,b) for (int i = (int)(a); i < (int)(b); ++i) 
+#define all(a) (a).begin(), (a).end()
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+// DEBUGING TEMPLETE ////////////////////////////////////////////////////////////////////////{{{
+#define db(val) "["#val" = "<<(val)<<"] "
+#define CONCAT_(x, y) x##y
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+#   define clog cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#   define DB() debug_block CONCAT(dbbl, __LINE__)
+    int __db_level = 0;
+    struct debug_block {
+        debug_block() { clog << "{" << endl; ++__db_level; }
+        ~debug_block() { --__db_level; clog << "}" << endl; }
+    };
+#else
+#   define clog if (0) cerr
+#   define DB(...)
+#endif
+
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")"; 
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup); 
+}
+template<class ...U> print_op(tuple<U...>) {
+    return print_tuple_utils<0, tuple<U...>>(out, u);
+}
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////}}}
+
+int main() {
+#ifdef LOCAL
+    freopen("main.inp", "r", stdin); 
+    freopen("main.out", "w", stdout);  
+    freopen(".log", "w", stderr);
+#endif
+    cin.tie(0)->sync_with_stdio(0);
+    int ntest; cin >> ntest;
+    while (ntest--) {
+        int n, k; cin >> n >> k;
+        vector<ll> a(n);
+        rep(i, 0, n) cin >> a[i];
+        
+        vector<int> pos(n);
+        rep(i, 0, n) pos[i] = i;
+        sort(all(pos), [&](int u, int v) { return a[u] < a[v]; });
+        
+        int cnt_split = 0;
+        rep(i, 1, n) {
+            cnt_split += pos[i] != pos[i - 1] + 1;
+        }
+        if (cnt_split <= k - 1) {
+            cout << "Yes\n";
+        } else {
+            cout << "No\n";
+        }
+    }
+    
+    return 0;
+}
+
+// vim: foldmethod=marker
