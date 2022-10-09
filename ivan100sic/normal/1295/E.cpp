83d0d174d0df9a9d208dfbc296c5a4f52d9b795a
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 18:33:57 +0530

    ivan100sic

---
+++ ivan100sic/normal/1295/E.cpp
@@ -0,0 +1,103 @@
+// Retired?
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef long unsigned long ull;
+typedef double long ld;
+
+template<class T, class U, class F, class G, class H>
+struct segtreelazy {
+	struct node { T v; U u; };
+	int maxn;
+	vector<node> a;
+	T e; U eu;
+	F f; G g; H h;
+	// Add two elements
+	// Add two updaters
+	// Add an element and an update
+	// Neutral element
+	// Neutral update
+	// Bottom level element
+	segtreelazy(int n, F f, G g, H h, T e = T(), U eu = U(), T v = T()) :
+		e(e), eu(eu), f(f), g(g), h(h)
+	{
+		maxn = 1;
+		while (maxn < n)
+			maxn <<= 1;
+		a.assign(2*maxn, node{v, eu});
+		for (int i=maxn-1; i; i--)
+			a[i].v = f(a[2*i].v, a[2*i+1].v);
+	}
+
+	void push(int x) {
+		a[x].v = h(a[x].v, a[x].u);
+		if (x < maxn) {
+			a[2*x  ].u = g(a[2*x  ].u, a[x].u);
+			a[2*x+1].u = g(a[2*x+1].u, a[x].u);
+		}
+		a[x].u = eu;
+	}
+
+	void update(int l, int r, const U& u, int x, int xl, int xr) {
+		if (r <= xl || xr <= l)
+			return push(x);
+		if (l <= xl && xr <= r)
+			return a[x].u = g(a[x].u, u), push(x);
+		push(x);
+		int xm = (xl+xr) >> 1;
+		update(l, r, u, 2*x  , xl, xm);
+		update(l, r, u, 2*x+1, xm, xr);
+		a[x].v = f(a[2*x].v, a[2*x+1].v);
+	}
+
+	T get(int l, int r, int x, int xl, int xr) {
+		push(x);
+		if (r <= xl || xr <= l)
+			return e;
+		if (l <= xl && xr <= r)
+			return a[x].v;
+		int xm = (xl+xr) >> 1;
+		return f(get(l, r, 2*x, xl, xm), get(l, r, 2*x+1, xm, xr));
+	}
+
+	void update(int l, int r, const U& u) { update(l, r, u, 1, 0, maxn); }
+	T operator() (int l, int r) { return get(l, r, 1, 0, maxn); }
+};
+
+int n;
+int a[200005], p[200005];
+
+int main() {
+	ios::sync_with_stdio(!cin.tie(0));
+
+	cin >> n;
+	for (int i=1; i<=n; i++)
+		cin >> p[i];
+	for (int i=1; i<=n; i++)
+		cin >> a[i];
+
+	segtreelazy d(
+		n+2,
+		[](ll x, ll y) { return min(x, y); },
+		plus<ll>(),
+		plus<ll>(),
+		ll(1e18),
+		ll(0),
+		ll(0)
+	);
+
+	for (int i=1; i<=n; i++) {
+		d.update(p[i], n+2, a[i]);
+	}
+
+	ll sol = 1e18;
+
+	for (int i=1; i<n; i++) {
+		d.update(p[i], n+2, -a[i]);
+		d.update(0, p[i], a[i]);
+		sol = min(sol, d(0, n+2));
+	}
+
+	cout << sol << '\n';
+}
