54ba806717fb7734e30e8913eef1e87411c3b976
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 04:49:36 +0530

    darkkcyan

---
+++ darkkcyan/normal/1061/C.cpp
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define llong long long /*{{{*/
+#define ii pair<int, int>
+#define xx first
+#define yy second
+#define len(x) ((int)x.size())
+#define rep(i,n) for (int i = -1; ++ i < n; )
+#define rep1(i,n) for (int i = 0; i ++ < n; )
+
+template<class x> vector<typename x::value_type> tovec(const x& a) { return vector<typename x::value_type>(a.begin(), a.end()); }
+#define ptrtype(x) typename iterator_traits<x>::value_type
+template<class u> vector<ptrtype(u)> tovec(u a, u b) { return vector<ptrtype(u)>(a, b); }
+
+template<typename x> ostream& operator<<(ostream& cout, const vector<x>& a) {
+  cout << "[";
+  if (len(a)) cout << a[0];
+  rep1(i, len(a) - 1) cout << ", " << a[i];
+  return cout << "]";
+}
+
+template<typename u, typename v> ostream& operator<<(ostream& cout, const pair<u, v>& a) {
+  return cout << "(" << a.xx << ", " << a.yy << ")";
+}/*}}}*/
+
+#define maxa 1010100
+#define maxn 100010
+#define rem ((llong)1e9+7)
+
+vector<int> divs[maxa];
+void gendiv(int n) {
+  for (int i = 1; i <= n; ++i) {
+    for (int f = i; f < maxa; f += i)
+      divs[f].push_back(i);
+  }
+  rep(i, maxa) reverse(divs[i].begin(), divs[i].end());
+}
+
+int n;
+int a[maxn];
+llong dp[maxa];
+
+int main(void) {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+  cin >> n;
+  rep1(i, n) cin >> a[i];
+  gendiv(n);
+
+  dp[0] = 1;
+  rep1(i, n) {
+    for (auto f: divs[a[i]]) {
+      if (f > i) continue;
+      (dp[f] += dp[f - 1]) %= rem;
+    }
+  }
+
+  llong ans = 0;
+  rep(i, maxa) (ans += dp[i]) %= rem;
+  cout << (ans - 1 + rem) % rem;
+
+
+  return 0;
+}
