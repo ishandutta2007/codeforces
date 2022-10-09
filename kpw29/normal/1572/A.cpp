cf2c9649689a7b01c007b32d6fc4401a3dfd3c93
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 05:33:08 +0530

    kpw29

---
+++ kpw29/normal/1572/A.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+#define e1 first
+#define e2 second
+#define pb push_back
+#define OUT(x) {cout << x << "\n"; exit(0); }
+#define TCOUT(x) {cout << x << "\n"; return; }
+#define FOR(i, l, r) for(int i = (l); i <= (r); ++i)
+#define rep(i, l, r) for(int i = (l); i < (r); ++i)
+#define boost {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define sz(x) int(x.size())
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+/*#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
+vector <mint> fac, inv;
+mint binom(int n, int k) {
+	if (n < k || n < 0) return 0;
+	return fac[n] * inv[k] * inv[n-k];
+}
+
+void prep(int N) {
+	fac.resize(N+1, 1); inv.resize(N+1, 1);
+	for (int i=1; i<=N; ++i) fac[i] = fac[i-1] * i;
+	inv[N] = fac[N].inv();
+	for (int i=N-1; i>0; --i) inv[i] = inv[i+1] * (i + 1);
+}*/
+
+mt19937_64 rng(time(0));
+int random(int l, int r) {
+	return uniform_int_distribution<int>(l, r)(rng);
+}
+#ifdef DEBUG
+template<class T> int size(T &&x) {
+	return int(x.size());
+}
+template<class A, class B> ostream& operator<<(ostream &out, const pair<A, B> &p) {
+	return out << '(' << p.first << ", " << p.second << ')';
+}
+template<class T> auto operator<<(ostream &out, T &&x) -> decltype(x.begin(), out) {
+	out << '{';
+	for(auto it = x.begin(); it != x.end(); ++it)
+		out << *it << (it == prev(x.end()) ? "" : ", ");
+	return out << '}';
+}
+void dump() {}
+template<class T, class... Args> void dump(T &&x, Args... args) {
+	cerr << x << ";  ";
+	dump(args...);
+}
+#endif
+#ifdef DEBUG
+  struct Nl{~Nl(){cerr << '\n';}};
+# define debug(x...) cerr << (strcmp(#x, "") ? #x ":  " : ""), dump(x), Nl(), cerr << ""
+#else
+# define debug(...) 0 && cerr
+#endif
+const int maxn = 1000100;
+
+vi topoSort(const vector<vi>& gr) {
+	vi indeg(sz(gr)), ret;
+	for (auto& li : gr) for (int x : li) indeg[x]++;
+	queue<int> q; // use priority_queue for lexic. largest ans.
+	rep(i,0,sz(gr)) if (indeg[i] == 0) q.push(i);
+	while (!q.empty()) {
+		int i = q.front(); // top() for priority queue
+		ret.push_back(i);
+		q.pop();
+		for (int x : gr[i])
+			if (--indeg[x] == 0) q.push(x);
+	}
+	return ret;
+}
+
+//Did you REAALLY consider sample tests?
+void solve(int tc) {
+	int n;
+	cin >> n;
+	vector <vi> graf(n);
+	rep(i, 0, n) {
+		int ki;
+		cin >> ki;
+		rep(j, 0, ki) {
+			int a;
+			cin >> a;
+			--a;
+			graf[i].pb(a);
+		}
+	}
+	
+	vector <int> dp(n, 0);
+	auto vec = topoSort(graf);
+	if (sz(vec) < n) {
+		TCOUT(-1);
+	}
+	
+	debug(vec);
+	int r = 0;
+	reverse(all(vec));
+	trav(node, vec) {
+		trav(sas, graf[node]) {
+			if (node >= sas) dp[node] = max(dp[node], dp[sas]);
+			else dp[node] = max(dp[node], 1 + dp[sas]);
+		}
+	}
+	
+	trav(node, vec) r = max(r, dp[node]);
+	cout << r + 1 << "\n";
+}
+
+int main() {
+	boost;
+	int tests;
+	cin >> tests;
+	//tests = 1;
+	FOR(test, 1, tests) {
+		solve(test);
+	}
+}
