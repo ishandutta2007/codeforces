0511a350bd45ec611d8ea7abadeecc9e6e356e78
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 07:10:06 +0530

    kpw29

---
+++ kpw29/normal/630/B.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define e1 first
+#define e2 second
+#define OUT(x) {cout << x; exit(0); }
+#define scanf(...) scanf(__VA_ARGS__)?:0
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef unsigned int ui;
+typedef long double ld;
+typedef pair <int, int> PII;
+typedef pair <ll, ll> PLL;
+typedef pair <PII, PII> PP;
+const int mod = 100;
+const int inf = 1e9+9;
+const int MOD = 1e9+696969;
+const ll INF = 4e18 + 3;
+#define maxn 1500100
+int n, m, a, b, c, d;
+const long double T = 1.000000011;
+int main()
+{
+	double P, help;
+	cin >> P >> help;
+	printf("%.15lf", P * (double)pow(T, help));
+}
