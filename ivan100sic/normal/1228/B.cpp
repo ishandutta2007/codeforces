d68be1e4dea693d54e9e1500a3c77b88e8abf165
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 19:16:56 +0530

    ivan100sic

---
+++ ivan100sic/normal/1228/B.cpp
@@ -0,0 +1,175 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+
+template<int m>
+struct modint {
+
+	unsigned x;
+
+	modint() : x(0) {}
+
+	modint(long long arg) {
+		arg %= m;
+		if (arg < 0) {
+			x = arg + m;
+		} else {
+			x = arg;
+		}
+	}	
+
+	modint& operator+= (const modint& other) {
+		x += other.x;
+		if (x >= m) {
+			x -= m;
+		}
+		return *this;
+	}
+
+	modint& operator*= (const modint& other) {
+		x = (x * 1ull * other.x) % m;
+		return *this;
+	}
+
+	modint& operator-= (const modint& other) {
+		x += m - other.x;
+		if (x >= m) {
+			x -= m;
+		}
+		return *this;
+	}
+
+	modint operator+ (const modint& other) const {
+		modint tmp = *this;
+		tmp += other;
+		return tmp;
+	}
+
+	modint operator- (const modint& other) const {
+		modint tmp = *this;
+		tmp -= other;
+		return tmp;
+	}
+
+	modint operator* (const modint& other) const {
+		modint tmp = *this;
+		tmp *= other;
+		return tmp;
+	}
+
+	explicit operator int () const {
+		return x;
+	}
+
+	modint& operator++ () {
+		++x;
+		if (x == m) {
+			x = 0;
+		}
+		return *this;
+	}
+
+	modint& operator-- () {
+		if (x == 0) {
+			x = m-1;
+		} else {
+			--x;
+		}
+		return *this;
+	}
+
+	modint operator++ (int) {
+		modint tmp = *this;
+		++*this;
+		return tmp;
+	}
+
+	modint operator-- (int) {
+		modint tmp = *this;
+		--*this;
+		return tmp;
+	}
+
+	bool operator== (const modint& other) const {
+		return x == other.x;
+	}
+
+	bool operator!= (const modint& other) const {
+		return x != other.x;
+	}
+
+	template<class T>
+	modint operator^ (T arg) const {
+		if (arg == 0) {
+			return 1;
+		}
+		if (arg == 1) {
+			return x;
+		}
+		auto t = *this ^ (arg >> 1);
+		t *= t;
+		if (arg & 1) {
+			t *= *this;
+		}
+		return t;
+	}
+
+	template<class T>
+	modint operator^= (T arg) {
+		return *this = *this ^ arg;
+	}
+
+	modint inv() const {
+		return *this ^ (m-2);
+	}
+};
+
+const int MOD = 1'000'000'007;
+typedef modint<MOD> mint;
+
+int n, m;
+int a[1005][1005];
+
+mint z = 1;
+
+void postavi(int x, int y, int z) {
+	if (a[x][y] != -1 && a[x][y] != z)
+		::z = 0;
+	a[x][y] = z;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	cerr.tie(nullptr);
+
+	cin >> n >> m;
+	memset(a, 0xff, sizeof a);
+	for (int i=0; i<n; i++) {
+		int y;
+		cin >> y;
+		for (int j=0; j<y; j++)
+			postavi(i, j, 1);
+		if (y < m)
+			postavi(i, y, 0);
+	}
+	for (int j=0; j<m; j++) {
+		int x;
+		cin >> x;
+		for (int i=0; i<x; i++)
+			postavi(i, j, 1);
+		if (x < n)
+			postavi(x, j, 0);
+	}
+
+	for (int i=0; i<n; i++)
+		for (int j=0; j<m; j++)
+			if (a[i][j] == -1)
+				z += z;
+
+	cout << (int)z << '\n';
+}
