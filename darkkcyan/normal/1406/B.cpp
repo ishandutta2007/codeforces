11bfd98c10cda6642087f21f8ed4e5e690af09f1
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 03:41:01 +0530

    darkkcyan

---
+++ darkkcyan/normal/1406/B.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define llong long long 
+#define len(x) ((int)x.size())
+#define rep(i,n) for (int i = -1; ++ i < n; )
+#define rep1(i,n) for (int i = 0; i ++ < n; )
+#define rand __rand
+mt19937 rng(chrono::system_clock::now().time_since_epoch().count());  // or mt19937_64
+template<class T = int> T rand(T range = numeric_limits<T>::max()) { return (T)(rng() % range); }
+
+#define CONCAT_(x, y) x##y/*{{{*/
+#define CONCAT(x, y) CONCAT_(x, y)
+#define SPEC(name) CONCAT(name, __LINE__)
+#ifdef LOCAL_DEBUG   
+int __db_level = 0;
+#define clog cerr << string(__db_level * 2, ' ')
+struct debug_block {
+    string msg;
+    debug_block(const string& s): msg(s) { clog << "{ " << msg << endl; ++__db_level; }
+    ~debug_block() { --__db_level; clog << "} " << msg << endl; }
+};
+#define DB(args...) stringstream SPEC(ss); SPEC(ss)<< args; debug_block SPEC(dbbl)(SPEC(ss).str())
+#else
+#define clog if (0) cerr
+#define DB(...)
+#endif
+#define db(val) "[" #val " = " << val << "]; "
+template<class U, class V> ostream& operator<<(ostream& out, const pair<U, V>& p) {
+    return out << "(" << p.first << ", " << p.second << ")";
+}
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")";
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup);
+}
+template<class ...U>
+ostream& operator<<(ostream& out, const tuple<U...>& tup) { return print_tuple_utils<0, tuple<U...>>(out, tup); }
+template<class, typename = void> struct has_const_iterator : false_type {};
+template<class T> struct has_const_iterator<T, void_t<typename T::const_iterator>> : true_type {};
+template<class T>
+typename enable_if<has_const_iterator<T>::value && !is_same<T, string>::value, ostream&>::type
+operator<<(ostream& out, const T& container) { 
+    auto beg = container.begin(), end = container.end();
+    out << "(" << container.size() << ") {";
+    if (beg != end) out << *(beg++);
+    while (beg != end) out << ", " << *(beg++);
+    return out << "}";
+}
+#define ptrtype(x) typename iterator_traits<x>::value_type
+template<class u> vector<ptrtype(u)> $v(u a, u b) { return vector<ptrtype(u)>(a, b); }/*}}}*/
+// ACTUAL SOLUTION BELOW ////////////////////////////////////////////////////////////
+
+using ans_type = llong; 
+constexpr bool print_case = false;
+
+void print_ans(const ans_type& ans) {
+    cout << ans << '\n';
+}
+
+ans_type solve() {
+    int n; cin >> n;
+    vector<int> neg, pos;
+    int cnt0 = 0;
+    rep(i, n) {
+        int u; cin >> u;
+        if (u > 0) pos.push_back(u);
+        else if (u < 0) neg.push_back(u);
+        else ++cnt0;
+    }
+    
+    sort(neg.begin(), neg.end());
+    sort(pos.begin(), pos.end());
+    
+    llong ans = LLONG_MIN;
+    rep(cnt_pos, 6) rep(cnt_neg, 6) {
+        int cur_cnt0 = 5 - cnt_pos - cnt_neg;
+        if (cur_cnt0 < 0) continue;
+        if (cnt_pos > len(pos)) continue;
+        if (cnt_neg > len(neg)) continue;
+        if (cur_cnt0 > cnt0) continue;
+        if (cur_cnt0) {
+            ans = max(ans, 0ll);
+            continue;
+        }
+        
+        llong cur_ans = 1;
+        rep(i, cnt_pos) {
+            if (cnt_neg & 1) cur_ans *= pos[i];
+            else cur_ans *= pos[len(pos) - 1 - i];
+        }
+        rep(i, cnt_neg) {
+            if (!(cnt_neg & 1)) cur_ans *= neg[i];
+            else cur_ans *= neg[len(neg) - i - 1];
+        }
+        ans = max(ans, cur_ans);
+    }
+    return ans;
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int ntest; cin >> ntest;
+    rep1(testcase, ntest) {
+        DB(""; deb(testcase));
+        if (print_case) {
+            cout << "Case #" << testcase << ": ";
+            cerr << testcase << endl;
+        }
+        print_ans(solve());
+    }
+
+    return 0;
+}
+
+// Remember:
+// - Multitest? REFRESHING the data!!!
+// - Constrains for each set of data may differs. Should NOT USE the same max constant (maxn)
+//   for all of them.
+// vim: foldmethod=marker
