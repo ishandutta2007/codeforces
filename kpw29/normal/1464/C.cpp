30963d086044eb6e5bd0562469c21dafdaa0ce32
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 06:05:07 +0530

    kpw29

---
+++ kpw29/normal/1464/C.cpp
@@ -0,0 +1,108 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define e1 first
+#define e2 second
+#define pb push_back
+#define mp make_pair
+#define boost {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define eb emplace_back
+#define OUT(x) {cout << x; exit(0); }
+#define FOR(i,a,b) for(int i=(a);i<=(b);++i)
+#define scanf(...) scanf(__VA_ARGS__)?:0
+typedef long long int ll;
+typedef unsigned long long ull;
+typedef pair <int, int> PII;
+typedef pair <ll, ll> PLL;
+typedef pair <PLL, int> PLLI;
+typedef pair <PII, PII> PP;
+typedef pair <PII, int> PPI;
+typedef pair <int, PII> PIP;
+typedef pair <ll, int> PLI;
+typedef unsigned int ui;
+const int mod = 998244353;
+const int inf = 1e9+9;
+const ll MOD = 1e9+696969;
+const long long INF = 4LL * 1000000LL * 1000000LL * 1000000LL;
+
+template<class T> int size(T &&x) {
+	return int(x.size());
+}
+template<class A, class B> ostream& operator<<(ostream &out, const pair<A, B> &p) {
+	return out << '(' << p.first << ", " << p.second << ')';
+}
+template<class T> auto operator<<(ostream &out, T &&x) -> decltype(x.begin(), out) {
+	out << '{';
+	for(auto it = x.begin(); it != x.end(); ++it)
+		out << *it << (it == prev(x.end()) ? "" : ", ");
+	return out << '}';
+}
+void dump() {}
+template<class T, class... Args> void dump(T &&x, Args... args) {
+	cerr << x << ";  ";
+	dump(args...);
+}
+#ifdef DEBUG
+  struct Nl{~Nl(){cerr << '\n';}};
+# define debug(x...) cerr << (strcmp(#x, "") ? #x ":  " : ""), dump(x), Nl(), cerr << ""
+#else
+# define debug(...) 0 && cerr
+#endif
+mt19937_64 rng(0);
+int random(int l, int r) {
+	return uniform_int_distribution<int>(l, r)(rng);
+}
+
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+inline void addmod(int &x, int val) {
+	x += val;
+	while (x >= mod) x -= mod;
+}
+
+const int maxn = 100100;
+
+inline ll pot(char c) {
+	int val = c - 'a';
+	return (1LL << val);
+}
+
+void solveone() {
+	ll goal, n;
+	cin >> n >> goal;
+	string s;
+	cin >> s;
+	
+	goal -= pot(s[n-1]);
+	goal += pot(s[n-2]);
+	
+	vector <ll> elems = {};
+	goal = abs(goal);
+	
+	for (int i=0; i<n-2; ++i) {
+		goal += pot(s[i]);
+		elems.push_back(2LL * pot(s[i]));
+	}
+	
+	sort(elems.begin(), elems.end());
+	//chcialbym miec goal = 0
+	for (int i=elems.size()-1; i>=0; --i) {
+		ll value = elems[i];
+		if (goal - value >= 0) goal -= value;
+	}
+	
+	if (goal == 0) cout << "Yes\n";
+	else cout << "No\n";
+}
+
+int main() {
+	boost;
+	int tests;
+	tests = 1;
+	//cin >> tests;
+	FOR(_, 1, tests) solveone();
+}
