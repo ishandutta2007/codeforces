72ecf455b9a950090891e95d34371022a5d05ac1
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 07:55:47 +0530

    thescrasse

---
+++ thescrasse/normal/1530/A.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define nl "\n"
+#define nf endl
+#define ll long long
+#define pb push_back
+#define _ << ' ' <<
+
+#define INF (ll)1e18
+#define mod 998244353
+#define maxn 110
+
+ll i, i1, j, k, k1, t, n, m, res, flag[10], a, b;
+
+ll solve(ll x) {
+    if (x == 0) return 0;
+    return max(solve(x / 10), x % 10);
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    #if !ONLINE_JUDGE && !EVAL
+        ifstream cin("input.txt");
+        ofstream cout("output.txt");
+    #endif
+
+    cin >> t;
+    while (t--) {
+        cin >> n;
+
+        res = solve(n);
+        cout << res << nl;
+    }
+
+    return 0;
+}
