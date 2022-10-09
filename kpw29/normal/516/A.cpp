5cb56554f7356661b2074540b30019874dbdcb99
ishan dutta <ishandutta2007@gmail.com>
2022-10-09 05:34:18 +0530

    kpw29

---
+++ kpw29/normal/516/A.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+using namespace std;
+using LL = long long;
+#define e1 first
+#define e2 second
+#define pb push_back
+#define OUT(x) {cout << x << "\n"; exit(0); }
+#define TCOUT(x) {cout << x << "\n"; return; }
+#define FOR(i, l, r) for(int i = (l); i <= (r); ++i)
+#define rep(i, l, r) for(int i = (l); i < (r); ++i)
+#define boost {ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0); }
+#define sz(x) int(x.size())
+#define trav(a, x) for(auto& a : x)
+#define all(x) begin(x), end(x)
+typedef long long ll;
+typedef pair <int, int> pii;
+typedef pair <ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+/*#include <atcoder/modint>
+using namespace atcoder;
+using mint = modint998244353;
+vector <mint> fac, inv;
+mint binom(int n, int k) {
+	if (n < k || n < 0) return 0;
+	return fac[n] * inv[k] * inv[n-k];
+}
+
+void prep(int N) {
+	fac.resize(N+1, 1); inv.resize(N+1, 1);
+	for (int i=1; i<=N; ++i) fac[i] = fac[i-1] * i;
+	inv[N] = fac[N].inv();
+	for (int i=N-1; i>0; --i) inv[i] = inv[i+1] * (i + 1);
+}*/
+
+mt19937_64 rng(time(0));
+int random(int l, int r) {
+	return uniform_int_distribution<int>(l, r)(rng);
+}
+#ifdef DEBUG
+template<class T> int size(T &&x) {
+	return int(x.size());
+}
+template<class A, class B> ostream& operator<<(ostream &out, const pair<A, B> &p) {
+	return out << '(' << p.first << ", " << p.second << ')';
+}
+template<class T> auto operator<<(ostream &out, T &&x) -> decltype(x.begin(), out) {
+	out << '{';
+	for(auto it = x.begin(); it != x.end(); ++it)
+		out << *it << (it == prev(x.end()) ? "" : ", ");
+	return out << '}';
+}
+void dump() {}
+template<class T, class... Args> void dump(T &&x, Args... args) {
+	cerr << x << ";  ";
+	dump(args...);
+}
+#endif
+#ifdef DEBUG
+  struct Nl{~Nl(){cerr << '\n';}};
+# define debug(x...) cerr << (strcmp(#x, "") ? #x ":  " : ""), dump(x), Nl(), cerr << ""
+#else
+# define debug(...) 0 && cerr
+#endif
+const int maxn = 1000100;
+
+bool pr[10];
+//Did you REAALLY consider sample tests?
+int ile[10];
+
+void solve(int tc) {
+	int n;
+	string s;
+	cin >> n >> s;
+	vector <int> digs;
+	rep(i, 0, n) {
+		int val = s[i] - '0';
+		int init = val;
+		if (val < 2) continue;
+		auto ins = [&](vector <int> v) {
+			trav(c, v) digs.pb(c);
+		};
+		
+		if (pr[val]) digs.pb(val);
+		else {
+			if (val == 4) {
+				ins({2, 2, 3});
+			}
+			if (val == 6) {
+				ins({3, 5});
+			}
+			if (val == 8) {
+				ins({2, 2, 2, 7});
+			}
+			if (val == 9) {
+				ins({3, 3, 2, 7});
+			}
+		}
+	}
+	
+	sort(all(digs), greater<int>());
+	trav(d, digs) cout << d;
+}
+
+
+int main() {
+	rep(i, 0, 10) pr[i] = 0;
+	pr[2] = pr[3] = pr[5] = pr[7] = 1;
+	boost;
+	int tests;
+	//cin >> tests;
+	tests = 1;
+	FOR(test, 1, tests) {
+		solve(test);
+	}
+}
