debfb61b9a7660c12b79e5d8a4b4bdf3d7ef6c9f
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 04:43:36 +0530

    darkkcyan

---
+++ darkkcyan/normal/1117/A.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define cli clog << "\"" << __PRETTY_FUNCTION__ << "\"@" << __LINE__ << ": "
+#define llong long long 
+#define ii pair<int, int>
+#define xx first
+#define yy second
+#define len(x) ((int)x.size())
+#define rep(i,n) for (int i = -1; ++ i < n; )
+#define rep1(i,n) for (int i = 0; i ++ < n; )
+template<class x> vector<typename x::value_type> tovec(const x& a) { return vector<typename x::value_type>(a.begin(), a.end()); }
+#define ptrtype(x) typename iterator_traits<x>::value_type
+template<class u> vector<ptrtype(u)> tovec(u a, u b) { return vector<ptrtype(u)>(a, b); }
+template<typename x> ostream& operator<<(ostream& cout, const vector<x>& a) {
+  cout << "[";
+  if (len(a)) cout << a[0];
+  rep1(i, len(a) - 1) cout << ", " << a[i];
+  return cout << "]";
+}
+template<typename u, typename v> ostream& operator<<(ostream& cout, const pair<u, v>& a) {
+  return cout << "(" << a.xx << ", " << a.yy << ")";
+}
+
+#define maxn 101010
+int n;
+int a[maxn];
+
+int main(void) {
+  ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  cin >> n;
+  rep(i, n) cin >> a[i];
+  int mx = *max_element(a, a + n);
+  
+  int ans = 0;
+  int last = -1;
+  rep(i, n) {
+    if (a[i] == mx) {
+      if (last == -1) last = i;
+    } else {
+      if (last == -1) continue;
+      ans = max(ans, i - last);
+      last = -1;
+    }
+  }
+
+  if (last != -1) ans = max(ans, n - last);
+  cout << ans;
+
+  return 0;
+}
