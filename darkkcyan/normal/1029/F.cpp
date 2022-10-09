7c2b542baf8f0a1abc92dc351e833676408cf277
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 02:31:50 +0530

    darkkcyan

---
+++ darkkcyan/normal/1029/F.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define constexpr(...) (__VA_ARGS__)  
+using ll = long long;
+using ld = long double;
+const ld eps = 1e-8;
+// for matching the kactl notes
+#define sz(x) ((int)x.size())
+#define rep(i,a,b) for (int i = (int)(a); i < (int)(b); ++i) 
+#define all(a) (a).begin(), (a).end()
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+// DEBUGING TEMPLETE ////////////////////////////////////////////////////////////////////////{{{
+// What? You wanted to see how this template works?  Check this out:
+// https://quangloc99.github.io/2021/07/30/my-CP-debugging-template.html
+#define db(val) "["#val" = "<<(val)<<"] "
+#define CONCAT_(x, y) x##y
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+#   define clog cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#   define DB() debug_block CONCAT(dbbl, __LINE__)
+    int __db_level = 0;
+    struct debug_block {
+        debug_block() { clog << "{" << endl; ++__db_level; }
+        ~debug_block() { --__db_level; clog << "}" << endl; }
+    };
+#else
+#   define clog if (0) cerr
+#   define DB(...)
+#endif
+
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")"; 
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup); 
+}
+template<class ...U> print_op(tuple<U...>) {
+    return print_tuple_utils<0, tuple<U...>>(out, u);
+}
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////}}}
+
+vector<ll> small_div(ll x) {
+    vector<ll> ans;
+    for (ll i = 1; i * i <= x; ++i) {
+        if (x % i == 0) ans.push_back(i);
+    }
+    return ans;
+}
+
+int main() {
+#ifdef LOCAL
+    freopen("main.inp", "r", stdin); 
+    freopen("main.out", "w", stdout);  
+    freopen(".log", "w", stderr);
+#endif
+    cin.tie(0)->sync_with_stdio(0);
+    
+    ll a, b;
+    cin >> a >> b;
+    auto da = small_div(a);
+    auto db = small_div(b);
+    auto ds = small_div(a + b);
+    
+    ll ans = 0;
+    int ida = 0, idb = 0;
+    for (auto x: ds) {
+        ll y = (a + b) / x;
+        while (ida < sz(da) - 1 and da[ida + 1] <= x) ++ ida;
+        while (idb < sz(db) - 1 and db[idb + 1] <= x) ++ idb;
+        ll xa = da[ida], ya = a / xa;
+        ll xb = db[idb], yb = b / xb;
+        if ((xa <= x and ya <= y) or (xb <= x and yb <= y)) {
+            ans = (x + y) * 2;
+        }
+    }
+    cout << ans << '\n';
+    
+    return 0;
+}
+
+// vim: foldmethod=marker
---
+++ darkkcyan/normal/1287/B.cpp
@@ -0,0 +1,96 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #define constexpr(...) (__VA_ARGS__)  
+using ll = long long;
+using ld = long double;
+const ld eps = 1e-8;
+// for matching the kactl notes
+#define sz(x) ((int)x.size())
+#define rep(i,a,b) for (int i = (int)(a); i < (int)(b); ++i) 
+#define all(a) (a).begin(), (a).end()
+#define print_op(...) ostream& operator<<(ostream& out, const __VA_ARGS__& u)
+// DEBUGING TEMPLETE ////////////////////////////////////////////////////////////////////////{{{
+// What? You wanted to see how this template works?  Check this out:
+// https://quangloc99.github.io/2021/07/30/my-CP-debugging-template.html
+#define db(val) "["#val" = "<<(val)<<"] "
+#define CONCAT_(x, y) x##y
+#define CONCAT(x, y) CONCAT_(x, y)
+#ifdef LOCAL_DEBUG   
+#   define clog cerr << setw(__db_level * 2) << setfill(' ') << "" << setw(0)
+#   define DB() debug_block CONCAT(dbbl, __LINE__)
+    int __db_level = 0;
+    struct debug_block {
+        debug_block() { clog << "{" << endl; ++__db_level; }
+        ~debug_block() { --__db_level; clog << "}" << endl; }
+    };
+#else
+#   define clog if (0) cerr
+#   define DB(...)
+#endif
+
+template<class U, class V> print_op(pair<U, V>) {
+    return out << "(" << u.first << ", " << u.second << ")";
+}
+template<class Con, class = decltype(begin(declval<Con>()))>
+typename enable_if<!is_same<Con, string>::value, ostream&>::type
+operator<<(ostream& out, const Con& con) { 
+    out << "{";
+    for (auto beg = con.begin(), it = beg; it != con.end(); ++it)
+        out << (it == beg ? "" : ", ") << *it;
+    return out << "}";
+}
+template<size_t i, class T> ostream& print_tuple_utils(ostream& out, const T& tup) {
+    if constexpr(i == tuple_size<T>::value) return out << ")"; 
+    else return print_tuple_utils<i + 1, T>(out << (i ? ", " : "(") << get<i>(tup), tup); 
+}
+template<class ...U> print_op(tuple<U...>) {
+    return print_tuple_utils<0, tuple<U...>>(out, u);
+}
+// ACTUAL SOLUTION START HERE ////////////////////////////////////////////////////////////////}}}
+
+const int maxn = 1505;
+int n, k;
+string a[maxn];
+map<string, int> cnt;
+
+int main() {
+#ifdef LOCAL
+    freopen("main.inp", "r", stdin); 
+    freopen("main.out", "w", stdout);  
+    freopen(".log", "w", stderr);
+#endif
+    cin.tie(0)->sync_with_stdio(0);
+    
+    cin >> n >> k;
+    rep(i, 0, n) {
+        cin >> a[i];
+        cnt[a[i]]++;
+    }
+    
+    ll ans = 0;
+    rep(i, 0, n) {
+        rep(f, i + 1, n) {
+            if (a[i] == a[f]) continue;
+            string other = "";
+            rep(pos, 0, k) {
+                if (a[i][pos] == a[f][pos]) other += a[i][pos];
+                else other += char('S' ^ 'T' ^ 'E' ^ a[i][pos] ^ a[f][pos]);
+            }
+            auto it = cnt.find(other);
+            if (it != cnt.end()) {
+                ans += it->second;
+            }
+        }
+    }
+    
+    for (auto& [u, c]: cnt) {
+        ans += 1ll * c * (c - 1) * (c - 2) / 6;
+    }
+    cout << ans / 3;
+    
+    
+    return 0;
+}
+
+// vim: foldmethod=marker
