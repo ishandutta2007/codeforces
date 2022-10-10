c2efe5d2aa572532987305aaca7b68e2b3f40fe4
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 22:34:17 +0530

    aryanc403

---
+++ aryanc403/normal/1003/D.cpp
@@ -0,0 +1,127 @@
+/************************************************************************************************************************************************************************************
+  *                                                                                                                                                                                  *
+   *                                                                                                                                                                                  *
+    *  ****************    **************    *               *    ****************    *           *    ****************    *              *    ****************    ****************    *
+     *  *              *    *            *    *               *    *              *    **          *    *                   *              *    *              *                   *    *
+      *  *              *    *            *    *               *    *              *    * *         *    *                   *              *    *              *                   *    *
+       *  *              *    *            *    *               *    *              *    *  *        *    *                   *              *    *              *                   *    *
+        *  *              *    *            *    *               *    *              *    *   *       *    *                   *              *    *              *                   *    *
+         *  *              *    *            *    *               *    *              *    *    *      *    *                   *              *    *              *                   *    *
+          *  ****************    **************    *****************    ****************    *     *     *    *                   ****************    *              *    ****************    *
+           *  *              *    *  *                      *            *              *    *      *    *    *                                  *    *              *                   *    *
+            *  *              *    *    *                    *            *              *    *       *   *    *                                  *    *              *                   *    *
+             *  *              *    *      *                  *            *              *    *        *  *    *                                  *    *              *                   *    *
+              *  *              *    *        *                *            *              *    *         * *    *                                  *    *              *                   *    *
+               *  *              *    *          *              *            *              *    *          **    *                                  *    *              *                   *    *
+                *  *              *    *            *            *            *              *    *           *    ****************                   *    ****************    ****************    *
+                 *                                                                                                                                                                                  *
+                  *                                                                                                                                                                                  *
+                   ************************************************************************************************************************************************************************************/
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("-ffloat-store")
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define MAX 1000002
+#define PI 3.1415926535897932384626433832795
+typedef long long int lli;
+typedef pair<lli,lli> ii;
+typedef vector<ii> vii;
+typedef vector<lli> vi;
+
+clock_t time_p=clock();
+void aryanc403()
+{
+    time_p=clock()-time_p;
+    cerr<<"Time Taken : "<<(float)(time_p)/CLOCKS_PER_SEC<<"\n";
+}
+
+class cmp
+{
+public:
+    bool operator()(lli a , lli b) //For min priority_queue .
+    {
+        return a > b;
+    }
+};
+
+void add( map<lli,lli> &m, lli x)
+{
+    map<lli,lli> ::iterator jt;
+    jt=m.find(x);
+    if(jt==m.end())         m.insert(mp(x,1));
+    else                    jt->Y++;
+}
+
+void del( map<lli,lli> &m, lli x)
+{
+    map<lli,lli> ::iterator jt;
+    jt=m.find(x);
+    if(jt->Y==1)            m.erase(jt);
+    else                    jt->Y--;
+}
+
+bool cmp(const ii &a,const ii &b)
+{
+    return a.X<b.X||(a.X==b.X&&a.Y<b.Y);
+}
+const lli INF = 0xFFFFFFFFFFFFFFFL;
+const lli mod = 1000000007L;
+
+    lli T,n,i,m,j,k,in,cnt,l;
+    lli a[50];
+    vi :: iterator it;
+    //priority_queue < lli , vector < lli > , cmp > pq;// min priority_queue .
+
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    lli q;
+    scanf(" %lld %lld",&n,&q);
+    fo(i,n)
+    {
+        scanf(" %lld",&in);
+        for(j=0;j<30;++j)
+        {
+            if(in&(1<<j))
+            {
+                a[j]++;
+                break;
+            }
+        }
+    }
+    
+    //cout<<( (1<<30) > 1000000000 )<<endl<<endl;
+    
+    lli x;
+    while(q--)
+    {
+        scanf(" %lld",&x);
+        lli cnt=0;
+        for(j=30;j>=0;j--)
+        {
+            i=min((x/(1<<j)),a[j]);
+            cnt+=i;
+            x-=i*((1<<j));
+            //cout<<(1<<j)<<endl;
+        }
+        //cout<<x<<" ";
+        if(x>0)
+            cnt=-1;
+            cout<<cnt<<endl;
+    }
+
+    aryanc403();
+    return 0;
+}
