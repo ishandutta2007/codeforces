7b04b78a521c760d2a0f57ff5daeede7c432aa8b
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 09:34:55 +0530

    smax

---
+++ smax/normal/1354/B.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__);
+#else
+#define DEBUG(...)
+#endif
+
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << s << " = " << x << "\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {cerr << s.substr(0, s.find(',')) << " = " << x << " | "; debug(s.substr(s.find(',') + 2), args...);}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int n = s.length();
+
+        int ret = INT_MAX;
+        vector<int> cnt(3);
+        for (int i=0, j=0; j<n; j++) {
+            cnt[s[j]-'1']++;
+            while (i < n && cnt[s[i]-'1'] > 1)
+                cnt[s[i++]-'1']--;
+            if (cnt[0] && cnt[1] && cnt[2])
+                ret = min(ret, j - i + 1);
+        }
+
+        cout << (ret == INT_MAX ? 0 : ret) << "\n";
+    }
+
+    return 0;
+}
