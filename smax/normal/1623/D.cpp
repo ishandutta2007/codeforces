269c13651d43cb5858e3e84268439cf4598cb708
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 07:32:53 +0530

    smax

---
+++ smax/normal/1623/D.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DEBUG(...) 6
+#endif
+
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+
+template<int MOD>
+struct ModInt {
+    long long v;
+    ModInt(long long _v = 0) {v = (-MOD < _v && _v < MOD) ? _v : _v % MOD; if (v < 0) v += MOD;}
+    ModInt& operator += (const ModInt &other) {v += other.v; if (v >= MOD) v -= MOD; return *this;}
+    ModInt& operator -= (const ModInt &other) {v -= other.v; if (v < 0) v += MOD; return *this;}
+    ModInt& operator *= (const ModInt &other) {v = v * other.v % MOD; return *this;}
+    ModInt& operator /= (const ModInt &other) {return *this *= inverse(other);}
+    bool operator == (const ModInt &other) const {return v == other.v;}
+    bool operator != (const ModInt &other) const {return v != other.v;}
+    friend ModInt operator + (ModInt a, const ModInt &b) {return a += b;}
+    friend ModInt operator - (ModInt a, const ModInt &b) {return a -= b;}
+    friend ModInt operator * (ModInt a, const ModInt &b) {return a *= b;}
+    friend ModInt operator / (ModInt a, const ModInt &b) {return a /= b;}
+    friend ModInt operator - (const ModInt &a) {return 0 - a;}
+    friend ModInt power(ModInt a, long long b) {ModInt ret(1); while (b > 0) {if (b & 1) ret *= a; a *= a; b >>= 1;} return ret;}
+    friend ModInt inverse(ModInt a) {return power(a, MOD - 2);}
+    friend istream& operator >> (istream &is, ModInt &m) {is >> m.v; m.v = (-MOD < m.v && m.v < MOD) ? m.v : m.v % MOD; if (m.v < 0) m.v += MOD; return is;}
+    friend ostream& operator << (ostream &os, const ModInt &m) {return os << m.v;}
+};
+using M = ModInt<1000000007>;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m, rb, cb, rd, cd;
+        M p;
+        cin >> n >> m >> rb >> cb >> rd >> cd >> p;
+        rb--, cb--, rd--, cd--;
+        p /= 100;
+
+        int r = rb, c = cb, dr = 1, dc = 1, d = 0;
+        if (r + dr < 0 || r + dr >= n)
+            dr = -dr;
+        if (c + dc < 0 || c + dc >= m)
+            dc = -dc;
+        int sr = dr, sc = dc;
+        vector<int> cycle;
+        do {
+            if (r == rd || c == cd)
+                cycle.push_back(d);
+            r += dr;
+            c += dc;
+            d++;
+            if (r + dr < 0 || r + dr >= n)
+                dr = -dr;
+            if (c + dc < 0 || c + dc >= m)
+                dc = -dc;
+        } while (r != rb || c != cb || dr != sr || dc != sc);
+
+        M a = d - cycle.back() + cycle[0], b = 1;
+        for (int i=(int)cycle.size()-2; i>=0; i--) {
+            a = cycle[i+1] - cycle[i] + (1 - p) * a;
+            b *= 1 - p;
+        }
+        a *= 1 - p;
+        b *= 1 - p;
+        cout << a / (1 - b) + cycle[0] << "\n";
+    }
+
+    return 0;
+}
