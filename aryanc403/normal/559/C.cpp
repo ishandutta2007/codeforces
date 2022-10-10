e649d3221e22fdb36e21bf901ef74bad64537cfc
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 21:52:10 +0530

    aryanc403

---
+++ aryanc403/normal/559/C.cpp
@@ -0,0 +1,179 @@
+/*
+  Warn - Don't change next line else you will get WA verdict. Online Judge is configured to give WA if next line is not present.
+  "An ideal problem has no test data."
+  Author - Aryan Choudhary (@aryanc403)
+*/
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("-ffloat-store")
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define repA(i,j,n)   for(i=(j);i<=(n);++i)
+#define repD(i,j,n)   for(i=(j);i>=(n);--i)
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define endl "\n"
+typedef long long int lli;
+typedef long double mytype;
+typedef pair<lli,lli> ii;
+typedef vector<ii> vii;
+typedef vector<lli> vi;
+
+clock_t time_p=clock();
+void aryanc403()
+{
+    time_p=clock()-time_p;
+    cerr<<"Time Taken : "<<(float)(time_p)/CLOCKS_PER_SEC<<"\n";
+}
+
+#ifdef ARYANC403
+#define dbg(...) { cerr<<"[ "; __aryanc403__(#__VA_ARGS__, __VA_ARGS__);}
+#undef endl
+template <typename Arg1,typename Arg2>
+ostream& operator << (ostream& out, const pair<Arg1,Arg2> &x) {
+    return out<<"("<<x.X<<","<<x.Y<<")";
+}
+
+template <typename Arg1>
+ostream& operator << (ostream& out, const vector<Arg1> &a) {
+    out<<"[";for(const auto &x:a)out<<x<<",";return out<<"]";
+}
+
+template <typename Arg1>
+ostream& operator << (ostream& out, const set<Arg1> &a) {
+    out<<"[";for(const auto &x:a)out<<x<<",";return out<<"]";
+}
+
+template <typename Arg1,typename Arg2>
+ostream& operator << (ostream& out, const map<Arg1,Arg2> &a) {
+    out<<"[";for(const auto &x:a)out<<x<<",";return out<<"]";
+}
+
+template <typename Arg1>
+void __aryanc403__(const string name, Arg1&& arg1){
+	cerr << name << " : " << arg1 << " ] " << endl;
+}
+
+template <typename Arg1, typename... Args>
+void __aryanc403__(const string names, Arg1&& arg1, Args&&... args){
+	const string name = names.substr(0,names.find(','));
+	cerr<<name<<" : "<<arg1<<" | ";
+	__aryanc403__(names.substr(1+(int)name.size()), args...);
+}
+
+#else
+    #define dbg(args...)
+#endif
+
+const lli INF = 0xFFFFFFFFFFFFFFFL;
+
+lli seed;
+mt19937 rng(seed=chrono::steady_clock::now().time_since_epoch().count());
+inline lli rnd(lli l=0,lli r=INF)
+{return uniform_int_distribution<lli>(l,r)(rng);}
+
+class CMP
+{public:
+bool operator()(ii a , ii b) //For min priority_queue .
+{    return ! ( a.X < b.X || a.X==b.X && a.Y <= b.Y);   }};
+
+void add( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    auto jt=m.find(x);
+    if(jt==m.end())         m.insert({x,cnt});
+    else                    jt->Y+=cnt;
+}
+
+void del( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    auto jt=m.find(x);
+    if(jt->Y<=cnt)            m.erase(jt);
+    else                      jt->Y-=cnt;
+}
+
+bool cmp(const ii &a,const ii &b)
+{
+    return a.X<b.X||(a.X==b.X&&a.Y<b.Y);
+}
+
+const lli mod = 1000000007L;
+const lli maxN = 2000LL;
+const lli N = 200000LL;
+
+    lli T,n,i,j,k,in,cnt,l,r,u,v,x,y;
+    lli m;
+    string s;
+    vii a;
+    lli dp[maxN+5];
+    lli fac[N+5],inv[N+5];
+    //priority_queue < ii , vector < ii > , CMP > pq;// min priority_queue .
+
+lli ways(lli x,lli y)
+{
+    if(x<0||y<0)
+        return 0;
+    lli ans=1;
+    ans=(ans*fac[x+y])%mod;
+    ans=(ans*inv[x])%mod;
+    ans=(ans*inv[y])%mod;
+    return ans;    
+}
+
+lli po(lli a,lli n)
+{
+    if(n==0)
+        return 1;
+    if(n%2)
+        return (a*po(a,n-1))%mod;
+    return po(a*a%mod,n/2);
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    // freopen("txt.in", "r", stdin);
+    // freopen("txt.out", "w", stdout);
+// cout<<std::fixed<<std::setprecision(35);
+// cin>>T;while(T--)
+{
+
+    fac[0]=1;
+    repA(i,1,N)
+        fac[i]=(i*fac[i-1])%mod;
+    inv[N]=po(fac[N],mod-2);
+    repD(i,N,1)
+        inv[i-1]=(i*inv[i])%mod;
+    assert(inv[0]==1);
+
+    cin>>n>>m;
+    a.pb({n,m});
+    cin>>n;
+    fo(i,n)
+    {
+        cin>>x>>y;
+        a.pb({x,y});
+    }
+    
+    sort(a.begin(),a.end());
+    repA(i,0,n)
+    {
+        dp[i]=ways(a[i].X-1,a[i].Y-1);
+        fo(j,i)
+            dp[i]-=dp[j]*ways(a[i].X-a[j].X,a[i].Y-a[j].Y)%mod;
+        dp[i]%=mod;
+        dp[i]+=mod;
+        dp[i]%=mod;
+    }
+    
+    cout<<dp[n]<<endl;
+}   aryanc403();
+    return 0;
+}
