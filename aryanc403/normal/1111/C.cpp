6589a4de45d2c44b8569614a6590f6a9668fe127
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 02:10:20 +0530

    aryanc403

---
+++ aryanc403/normal/1111/C.cpp
@@ -0,0 +1,122 @@
+/*
+  Warn - Don't change next line else you will get WA verdict. Online Judge is configured to give WA if next line is not present.
+  Author - Aryan Choudhary (@aryanc403)
+ 
+  const short DEBUG { 0 };
+  #define debug(x) if (DEBUG) cout << #x << " = " << x << '\n'
+*/
+
+#pragma warning(disable:4996)
+#pragma comment(linker, "/stack:200000000")
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("-ffloat-store")
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define repA(i,j,n)   for(i=(j);i<=(n);++i)
+#define repD(i,j,n)   for(i=(j);i>=(n);--i)
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define endl "\n"
+#define PI 3.1415926535897932384626433832795
+typedef long long int lli;
+typedef long double mytype;
+typedef pair<lli,lli> ii;
+typedef vector<ii> vii;
+typedef vector<lli> vi;
+
+//const lli [3] ={ 999119999L,1000000007L,1000992299L};
+//const lli [3] ={ 97L,101L,103L};
+//const lli  = chrono::high_resolution_clock::now().time_since_epoch().count();
+clock_t time_p=clock();
+void aryanc403()
+{
+    time_p=clock()-time_p;
+    cerr<<"Time Taken : "<<(float)(time_p)/CLOCKS_PER_SEC<<"\n";
+}
+
+class CMP
+{
+public:
+    bool operator()(lli a , lli b) //For min priority_queue .
+    {
+        return ! ( a <= b );
+    }
+};
+
+void add( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    map<lli,lli> ::iterator jt;
+    jt=m.find(x);
+    if(jt==m.end())         m.insert(mp(x,cnt));
+    else                    jt->Y+=cnt;
+}
+
+void del( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    map<lli,lli> ::iterator jt;
+    jt=m.find(x);
+    if(jt->Y<=cnt)            m.erase(jt);
+    else                      jt->Y-=cnt;
+}
+
+bool cmp(const ii &a,const ii &b)
+{
+    return a.X<b.X||(a.X==b.X&&a.Y<b.Y);
+}
+const lli INF = 0xFFFFFFFFFFFFFFFL;
+const lli mod = 1000000007L;
+
+    lli T,n,i,j,k,in,cnt,l,r,a,b;
+    lli m;
+    string s;
+    vi c;
+    vi :: iterator it;
+    lli dp[35][100005];
+    //priority_queue < lli , vector < lli > , CMP > pq;// min priority_queue .
+
+lli sol(lli l,lli r,lli L,lli R,lli lvl)//[L,R)
+{
+    //cerr<<l<<" "<<r<<" "<<L<<" "<<R<<endl;
+    if(R==L)//empty
+        return a;
+    
+    if(r==l)//contains one.
+        return b*(R-L);
+    
+    lli ans=b*(R-L)*(r-l+1);
+    lli k=L;
+    while(k<R&&c[k]<=(l+r)/2)
+        k++;
+    
+    lli c=sol(l,(l+r)/2,L,k,lvl-1);
+    c+=sol((l+r)/2+1,r,k,R,lvl-1);
+    return min(ans,c);
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    // freopen("txt.in", "r", stdin);
+    // freopen("txt.out", "w", stdout);
+// cout<<std::fixed<<std::setprecision(35);
+// cin>>T;while(T--)
+{
+    memset(dp,-1,35*100005*sizeof(lli));
+    cin>>n>>k>>a>>b;
+    c.clear();c.reserve(k);
+    fo(i,k)
+    {
+        cin>>in;
+        c.pb(in);
+    }
+    sort(c.begin(),c.end());
+    cout<<sol(1,1LL<<n,0,k,n+1)<<endl;
+}   aryanc403();
+    return 0;
+}
