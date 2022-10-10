4984fe2e41da766361e19a99e5a2877ab7a2a11b
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 07:07:31 +0530

    smax

---
+++ smax/normal/1700/F.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DEBUG(...) 6
+#endif
+
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector a(2, vector<int>(n)), b(2, vector<int>(n));
+    for (auto &v : a)
+        for (int &x : v)
+            cin >> x;
+    for (auto &v : b)
+        for (int &x : v)
+            cin >> x;
+
+    long long ret = 0;
+    vector<int> delta(2);
+    for (int j=0; j<n; j++) {
+        delta[0] += a[0][j] - b[0][j];
+        delta[1] += a[1][j] - b[1][j];
+        if (delta[0] < 0 && delta[1] > 0) {
+            ret++;
+            delta[0]++;
+            delta[1]--;
+        } else if (delta[0] > 0 && delta[1] < 0) {
+            ret++;
+            delta[0]--;
+            delta[1]++;
+        }
+        ret += abs(delta[0]) + abs(delta[1]);
+    }
+    cout << (delta[0] == 0 && delta[1] == 0 ? ret : -1) << "\n";
+
+    return 0;
+}
