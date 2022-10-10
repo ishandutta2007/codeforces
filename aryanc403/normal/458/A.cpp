570c3f57e8610c7d3cdff429a37ea0f3929ee483
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 01:54:14 +0530

    aryanc403

---
+++ aryanc403/normal/458/A.cpp
@@ -0,0 +1,144 @@
+/*
+  Compete against Yourself.
+  Author - Aryan Choudhary (@aryanc403)
+*/
+
+#pragma GCC optimize ("Ofast")
+#pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#pragma GCC optimize ("-ffloat-store")
+
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define repA(i,j,n)   for(i=(j);i<=(n);++i)
+#define repD(i,j,n)   for(i=(j);i>=(n);--i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define endl "\n"
+
+typedef long long int lli;
+typedef long double mytype;
+typedef pair<lli,lli> ii;
+typedef vector<ii> vii;
+typedef vector<lli> vi;
+
+clock_t time_p=clock();
+void aryanc403()
+{
+#ifdef ARYANC403
+    time_p=clock()-time_p;
+    cerr<<"Time Taken : "<<(float)(time_p)/CLOCKS_PER_SEC<<"\n";
+#endif
+}
+
+#ifdef ARYANC403
+#include "/home/aryan/codes/PastCodes/header.h"
+#else
+    #define dbg(args...)
+#endif
+
+const lli INF = 0xFFFFFFFFFFFFFFFL;
+
+lli seed;
+mt19937 rng(seed=chrono::steady_clock::now().time_since_epoch().count());
+inline lli rnd(lli l=0,lli r=INF)
+{return uniform_int_distribution<lli>(l,r)(rng);}
+
+class CMP
+{public:
+bool operator()(ii a , ii b) //For min priority_queue .
+{    return ! ( a.X < b.X || ( a.X==b.X && a.Y <= b.Y ));   }};
+
+void add( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    auto jt=m.find(x);
+    if(jt==m.end())         m.insert({x,cnt});
+    else                    jt->Y+=cnt;
+}
+
+void del( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    auto jt=m.find(x);
+    if(jt->Y<=cnt)            m.erase(jt);
+    else                      jt->Y-=cnt;
+}
+
+bool cmp(const ii &a,const ii &b)
+{
+    return a.X<b.X||(a.X==b.X&&a.Y<b.Y);
+}
+
+const lli mod = 1000000007L;
+// const lli maxN = 1000000007L;
+
+    lli T,n,i,j,k,in,cnt,l,r,u,v,x,y;
+    lli m;
+    string s;
+    vi a,b;
+    //priority_queue < ii , vector < ii > , CMP > pq;// min priority_queue .
+
+void getA(vi &a)
+{
+    a.clear();
+    cin>>s;
+    n=sz(s);
+    a.resize(n,0);
+    fo(i,n)
+        a[n-i-1]=s[i]-'0';
+    dbg(a);
+}
+
+void prt(lli x,lli y)
+{
+    if(x>y)
+    cout<<">";
+    else if(x<y)
+        cout<<"<";
+    else
+        cout<<"=";
+    exit(0);
+}
+
+int main(void) {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    // freopen("txt.in", "r", stdin);
+    // freopen("txt.out", "w", stdout);
+// cout<<std::fixed<<std::setprecision(35);
+// cin>>T;while(T--)
+{
+
+    getA(a);
+    getA(b);
+
+    while(sz(a)<sz(b))
+        a.pb(0);
+    while(sz(b)<sz(a))
+        b.pb(0);
+    n=sz(a)-1;
+    while(sz(a)>2&&!a.empty()&&!b.empty())
+    {
+        lli x=a.back(),y=b.back();
+        if(abs(x-y)>10)
+            prt(x,y);
+        x-=y;
+        a.pop_back();
+        b.pop_back();
+        n--;
+        a[n]+=x;
+        a[n-1]+=x;
+    }
+
+    dbg(a,b);
+    if(sz(a)==2&&a[1]!=b[1])
+        prt(a[1],b[1]);
+    prt(a[0],b[0]);
+
+
+}   aryanc403();
+    return 0;
+}
