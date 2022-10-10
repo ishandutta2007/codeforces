ed1a649e7150e148cf8404a3e1fead3d186f205f
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 22:50:17 +0530

    xellos

---
+++ xellos/normal/1687/A.cpp
@@ -0,0 +1,101 @@
+// iostream is too mainstream
+#include <cstdio>
+// bitch please
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+#define dibs reserve
+#define OVER9000 1234567890123456789LL
+#define tisic 47
+#define soclose 1e-8
+#define patkan 9
+#define ff first
+#define ss second
+using uint = unsigned int;
+using cat = long long;
+using dbl = long double;
+constexpr dbl pi = 3.14159265358979323846;
+using namespace std;
+
+#ifdef DONLINE_JUDGE
+	// palindromic tree is better than splay tree!
+	#define lld I64d
+#endif
+
+template <typename T>
+T abs(T x) { return (x < 0) ? (-x) : x; }
+
+cat pw(cat a, cat e, cat mod) {
+	if(e <= 0) return 1;
+	cat x = pw(a, e/2, mod);
+	x = x * x % mod;
+	return (e&1) ? x * a % mod : x;
+}
+
+template <typename T>
+class fin {
+	vector<T> node_val;
+
+	int lastone(int x) { return x & (x ^ (x-1)); }
+
+public:
+	fin(int N, T init_val) {
+		node_val.resize(N+10, init_val);
+	}
+
+	void put(int pos, T val) {
+		for(int i = pos+1; i < (int)node_val.size(); i += lastone(i))
+			node_val[i] += val;
+	}
+
+	T get(int pos) {
+		T ret = 0;
+		for(int i = pos+1; i > 0; i -= lastone(i))
+			ret += node_val[i];
+		return ret;
+	}
+};
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cout << fixed << setprecision(12);
+	int T;
+	cin >> T;
+	while(T--) {
+		int N;
+		cat K;
+		cin >> N >> K;
+		vector<cat> A(N);
+		for(int i = 0; i < N; i++) cin >> A[i];
+		vector<cat> S(N+1, 0);
+		for(int i = 0; i < N; i++) S[i+1] = S[i]+A[i];
+		if(K <= N) {
+			cat ans = 0;
+			for(int i = 0; i <= N-K; i++) ans = max(ans, S[i+K]-S[i]);
+			ans += K*(K-1)/2;
+			cout << ans << "\n";
+		}
+		else {
+			cat ans = S[N];
+			for(int i = 0; i < N; i++) ans += K-1-i;
+			cout << ans << "\n";
+		}
+	}
+}
+
+// look at my code
+// my code is amazing
