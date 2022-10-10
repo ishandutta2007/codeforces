c58c07c425024634d707dc687cc379f4039ba86c
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 22:47:53 +0530

    xellos

---
+++ xellos/normal/1738/B.cpp
@@ -0,0 +1,115 @@
+// iostream is too mainstream
+#include <cstdio>
+// bitch please
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <chrono>
+#include <random>
+#include <cstdlib>
+#include <cmath>
+#include <ctime>
+#include <cstring>
+#include <iomanip>
+#define dibs reserve
+#define OVER9000 1234567890123456789LL
+#define tisic 47
+#define soclose 1e-8
+#define patkan 9
+#define ff first
+#define ss second
+using uint = unsigned int;
+using cat = long long;
+using dbl = long double;
+constexpr dbl pi = 3.14159265358979323846;
+using namespace std;
+
+#ifdef DONLINE_JUDGE
+	// palindromic tree is better than splay tree!
+	#define lld I64d
+#endif
+
+template <typename T>
+T abs(T x) { return (x < 0) ? (-x) : x; }
+
+cat gcd(cat a, cat b) {
+	if(a > b) swap(a, b);
+	while(a) {
+		cat c = b%a;
+		b = a;
+		a = c;
+	}
+	return b;
+}
+
+cat pw(cat a, cat e, cat mod) {
+	if(e <= 0) return 1;
+	cat x = pw(a, e/2, mod);
+	x = x * x % mod;
+	return (e&1) ? x * a % mod : x;
+}
+
+template <typename T>
+class fin {
+	vector<T> node_val;
+
+	int lastone(int x) { return x & (x ^ (x-1)); }
+
+public:
+	fin(int N, T init_val) {
+		node_val.resize(N+10, init_val);
+	}
+
+	void put(int pos, T val) {
+		for(int i = pos+1; i < (int)node_val.size(); i += lastone(i))
+			node_val[i] += val;
+	}
+
+	T get(int pos) {
+		T ret = 0;
+		for(int i = pos+1; i > 0; i -= lastone(i))
+			ret += node_val[i];
+		return ret;
+	}
+};
+
+constexpr cat MOD = 998244353;
+
+int main() {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	cout << fixed << setprecision(12);
+	int T;
+	cin >> T;
+	while(T--) {
+		int N, K;
+		cin >> N >> K;
+		vector<cat> S(N+1, 0);
+		for(int i = N-K+1; i <= N; i++) cin >> S[i];
+		if(K == 1) {
+			cout << "YES\n";
+			continue;
+		}
+		vector<cat> A(N);
+		for(int i = N-K+2; i <= N; i++) A[i-1] = S[i]-S[i-1];
+		bool ok = true;
+		for(int i = N-K+2; i < N; i++) if(A[i-1] > A[i]) ok = false;
+		if(K == N) {
+			A[0] = S[1];
+			if(A[0] > A[1]) ok = false;
+			cout << (ok ? "YES\n" : "NO\n");
+			continue;
+		}
+		if(S[N-K+1] > (N-K+1) * A[N-K+1]) ok = false;
+		cout << (ok ? "YES\n" : "NO\n");
+	}
+}
+
+// look at my code
+// my code is amazing
