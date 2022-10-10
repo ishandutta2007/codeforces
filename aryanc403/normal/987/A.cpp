9f740b95b13d78def23957f3ee528b682f3d1e69
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 02:19:27 +0530

    aryanc403

---
+++ aryanc403/normal/987/A.cpp
@@ -0,0 +1,72 @@
+/************************************************************************************************************************************************************************************
+ *                                                                                                                                                                                  *
+ *                                                                                                                                                                                  *
+ *  ****************    **************    *               *    ****************    *           *    ****************    *              *    ****************    ****************    *
+ *  *              *    *            *    *               *    *              *    **          *    *                   *              *    *              *                   *    *
+ *  *              *    *            *    *               *    *              *    * *         *    *                   *              *    *              *                   *    *
+ *  *              *    *            *    *               *    *              *    *  *        *    *                   *              *    *              *                   *    *
+ *  *              *    *            *    *               *    *              *    *   *       *    *                   *              *    *              *                   *    *
+ *  *              *    *            *    *               *    *              *    *    *      *    *                   *              *    *              *                   *    *
+ *  ****************    **************    *****************    ****************    *     *     *    *                   ****************    *              *    ****************    *
+ *  *              *    *  *                      *            *              *    *      *    *    *                                  *    *              *                   *    *
+ *  *              *    *    *                    *            *              *    *       *   *    *                                  *    *              *                   *    *
+ *  *              *    *      *                  *            *              *    *        *  *    *                                  *    *              *                   *    *
+ *  *              *    *        *                *            *              *    *         * *    *                                  *    *              *                   *    *
+ *  *              *    *          *              *            *              *    *          **    *                                  *    *              *                   *    *
+ *  *              *    *            *            *            *              *    *           *    ****************                   *    ****************    ****************    *
+ *                                                                                                                                                                                  *
+ *                                                                                                                                                                                  *
+ ************************************************************************************************************************************************************************************/
+
+#include<iostream>
+#include<bits/stdc++.h>
+#include<stdio.h>
+using namespace std;
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define LLI long long int
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define MAX 1000002
+typedef long long int lli;
+typedef pair<lli,lli> ii;
+typedef vector<ii> vii;
+typedef vector<lli> vi;
+const lli INF = 0xFFFFFFFFFFFFFFFL;
+const lli MOD = 1000000007L;
+    lli T,n,in,i,m,j,k;
+    vi a;
+    vi :: iterator it;
+    bool vis[6];
+    string s;
+    string stn[6] = {"purple","green","blue","orange","red","yellow"};
+    string clr[6] = {"Power","Time","Space","Soul","Reality","Mind"};
+    
+int main(void) {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cin>>T;//scanf(" %lld",&T);
+    lli cnt=6;
+    while(T--)
+    {
+        cin>>s;//scanf(" %s",s);
+        for(i=0;i<6;++i)
+        {
+            if(stn[i]==s&&vis[i]==false)
+            {
+                vis[i]=true;
+                cnt--;
+            }
+        }
+    }
+    cout<<cnt<<endl;
+    for(i=0;i<6;++i)
+        {
+            if(vis[i]==false)
+            {
+                cout<<clr[i]<<endl;
+            }
+        }
+    return 0;
+}
