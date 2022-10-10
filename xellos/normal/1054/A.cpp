721a0d89489f742849422d929d20655aa72c11f1
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 02:50:50 +0530

    xellos

---
+++ xellos/normal/1054/A.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+// iostream is too mainstream
+#include <cstdio>
+// bitch please
+#include <iostream>
+#include <algorithm>
+#include <cstdlib>
+#include <vector>
+#include <set>
+#include <map>
+#include <queue>
+#include <stack>
+#include <list>
+#include <cmath>
+#include <iomanip>
+#include <time.h>
+#define dibs reserve
+#define OVER9000 1234567890
+#define ALL_THE(CAKE,LIE) for(auto LIE =CAKE.begin(); LIE != CAKE.end(); LIE++)
+#define tisic 47
+#define soclose 1e-8
+#define chocolate win
+// so much chocolate
+#define patkan 9
+#define ff first
+#define ss second
+#define abs(x) (((x) < 0)?-(x):(x))
+#define uint unsigned int
+#define dbl long double
+#define pi 3.14159265358979323846
+using namespace std;
+// mylittledoge
+
+using cat = long long;
+
+#ifdef DONLINE_JUDGE
+	// palindromic tree is better than splay tree!
+	#define lld I64d
+#endif
+
+int main() {
+	cin.sync_with_stdio(0);
+	cin.tie(0);
+	cout << fixed << setprecision(10);
+	int x, y, z, t1, t2, t3;
+	cin >> x >> y >> z >> t1 >> t2 >> t3;
+	int t_st = abs(y-x) * t1;
+	int t_el = abs(y-x) * t2 + abs(z-x) * t2 + 3 * t3;
+	if(t_st < t_el) cout << "NO\n";
+	else cout << "YES\n";
+	return 0;
+}
+
+// look at my code
+// my code is amazing
