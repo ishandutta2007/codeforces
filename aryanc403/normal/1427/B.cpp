1e87f049b2ab14bc83044f9d06febf06514ea027
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 21:27:20 +0530

    aryanc403

---
+++ aryanc403/normal/1427/B.cpp
@@ -0,0 +1,152 @@
+/* in the name of Anton */
+
+/*
+  Compete against Yourself.
+  Author - Aryan Choudhary (@aryanc403)
+*/
+
+#ifdef ARYANC403
+    #include "/home/aryan/codes/PastCodes/template/header.h"
+#else
+    #pragma GCC optimize ("Ofast")
+    #pragma GCC target ("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx")
+    #pragma GCC optimize ("-ffloat-store")
+    #include<iostream>
+    #include<bits/stdc++.h>
+    #define dbg(args...)
+#endif
+
+using namespace std;
+#define fo(i,n)   for(i=0;i<(n);++i)
+#define repA(i,j,n)   for(i=(j);i<=(n);++i)
+#define repD(i,j,n)   for(i=(j);i>=(n);--i)
+#define all(x) begin(x), end(x)
+#define sz(x) ((lli)(x).size())
+#define pb push_back
+#define mp make_pair
+#define X first
+#define Y second
+#define endl "\n"
+
+typedef long long int lli;
+typedef long double mytype;
+typedef pair<lli,lli> ii;
+typedef vector<ii> vii;
+typedef vector<lli> vi;
+
+
+const auto start_time = std::chrono::high_resolution_clock::now();
+void aryanc403()
+{
+#ifdef ARYANC403
+auto end_time = std::chrono::high_resolution_clock::now();
+std::chrono::duration<double> diff = end_time-start_time;
+    cerr<<"Time Taken : "<<diff.count()<<"\n";
+#endif
+}
+
+const lli INF = 0xFFFFFFFFFFFFFFFL;
+
+lli seed;
+mt19937 rng(seed=chrono::steady_clock::now().time_since_epoch().count());
+inline lli rnd(lli l=0,lli r=INF)
+{return uniform_int_distribution<lli>(l,r)(rng);}
+
+class CMP
+{public:
+bool operator()(ii a , ii b) //For min priority_queue .
+{    return ! ( a.X < b.X || ( a.X==b.X && a.Y <= b.Y ));   }};
+
+void add( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    auto jt=m.find(x);
+    if(jt==m.end())         m.insert({x,cnt});
+    else                    jt->Y+=cnt;
+}
+
+void del( map<lli,lli> &m, lli x,lli cnt=1)
+{
+    auto jt=m.find(x);
+    if(jt->Y<=cnt)            m.erase(jt);
+    else                      jt->Y-=cnt;
+}
+
+bool cmp(const ii &a,const ii &b)
+{
+    return a.X<b.X||(a.X==b.X&&a.Y<b.Y);
+}
+
+const lli mod = 1000000007L;
+// const lli maxN = 1000000007L;
+
+    lli T,n,i,j,k,in,cnt,l,r,u,v,x,y;
+    lli m;
+    string s;
+    vi a;
+    //priority_queue < ii , vector < ii > , CMP > pq;// min priority_queue .
+
+int main(void) {
+    ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+    // freopen("txt.in", "r", stdin);
+    // freopen("txt.out", "w", stdout);
+// cout<<std::fixed<<std::setprecision(35);
+cin>>T;while(T--)
+{
+
+    cin>>n>>k;
+    cin>>s;
+    i=0;
+    while(i<n&&s[i]=='L')
+        i++;
+    if(i==n)
+    {
+        cout<<max(0LL,2*k-1)<<endl;
+        continue;
+    }
+
+    cnt=0;
+    char pvr='L';
+    lli mx=n;
+    for(auto x:s)
+    {
+        if(x=='L')
+        {
+            pvr=x;
+            continue;
+        }
+
+        cnt++;
+        cnt+=(pvr=='W');
+        pvr=x;
+        mx--;
+    }
+
+    k=min(mx,k);
+    cnt+=2*k;
+    a.clear();
+    while(i<n)
+    {
+        j=i+1;
+        while(j<n&&s[j]=='L')
+            j++;
+        if(j==n)
+            break;
+        // dbg(i,j,j-i-1,s.substr(i,j-i));
+        if(i+1!=j)
+            a.pb(j-i-1);
+        i=j;
+    }
+
+    dbg(a,s,k,cnt);
+    sort(all(a));
+    for(auto x:a)
+    {
+        if(x>k)
+            break;
+        k-=x;
+        cnt++;
+    }
+    cout<<cnt<<endl;
+}   aryanc403();
+    return 0;
+}
