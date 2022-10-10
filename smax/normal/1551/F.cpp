0f6a4b3e207e040acc2fa6138ad724f0e3258f7f
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 07:47:30 +0530

    smax

---
+++ smax/normal/1551/F.cpp
@@ -0,0 +1,103 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#ifdef LOCAL
+#define DEBUG(...) debug(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define DEBUG(...) 6
+#endif
+
+template<typename T, typename S> ostream& operator << (ostream &os, const pair<T, S> &p) {return os << "(" << p.first << ", " << p.second << ")";}
+template<typename C, typename T = decay<decltype(*begin(declval<C>()))>, typename enable_if<!is_same<C, string>::value>::type* = nullptr>
+ostream& operator << (ostream &os, const C &c) {bool f = true; os << "["; for (const auto &x : c) {if (!f) os << ", "; f = false; os << x;} return os << "]";}
+template<typename T> void debug(string s, T x) {cerr << "\033[1;35m" << s << "\033[0;32m = \033[33m" << x << "\033[0m\n";}
+template<typename T, typename... Args> void debug(string s, T x, Args... args) {for (int i=0, b=0; i<(int)s.size(); i++) if (s[i] == '(' || s[i] == '{') b++; else
+if (s[i] == ')' || s[i] == '}') b--; else if (s[i] == ',' && b == 0) {cerr << "\033[1;35m" << s.substr(0, i) << "\033[0;32m = \033[33m" << x << "\033[31m | "; debug(s.substr(s.find_first_not_of(' ', i + 1)), args...); break;}}
+
+template<int MOD>
+struct ModInt {
+    long long v;
+    ModInt(long long _v = 0) {v = (-MOD < _v && _v < MOD) ? _v : _v % MOD; if (v < 0) v += MOD;}
+    ModInt& operator += (const ModInt &other) {v += other.v; if (v >= MOD) v -= MOD; return *this;}
+    ModInt& operator -= (const ModInt &other) {v -= other.v; if (v < 0) v += MOD; return *this;}
+    ModInt& operator *= (const ModInt &other) {v = v * other.v % MOD; return *this;}
+    ModInt& operator /= (const ModInt &other) {return *this *= inverse(other);}
+    bool operator == (const ModInt &other) const {return v == other.v;}
+    bool operator != (const ModInt &other) const {return v != other.v;}
+    friend ModInt operator + (ModInt a, const ModInt &b) {return a += b;}
+    friend ModInt operator - (ModInt a, const ModInt &b) {return a -= b;}
+    friend ModInt operator * (ModInt a, const ModInt &b) {return a *= b;}
+    friend ModInt operator / (ModInt a, const ModInt &b) {return a /= b;}
+    friend ModInt operator - (const ModInt &a) {return 0 - a;}
+    friend ModInt power(ModInt a, long long b) {ModInt ret(1); while (b > 0) {if (b & 1) ret *= a; a *= a; b >>= 1;} return ret;}
+    friend ModInt inverse(ModInt a) {return power(a, MOD - 2);}
+    friend istream& operator >> (istream &is, ModInt &m) {is >> m.v; m.v = (-MOD < m.v && m.v < MOD) ? m.v : m.v % MOD; if (m.v < 0) m.v += MOD; return is;}
+    friend ostream& operator << (ostream &os, const ModInt &m) {return os << m.v;}
+};
+typedef ModInt<1000000007> M;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, k;
+        cin >> n >> k;
+        vector<vector<int>> adj(n);
+        for (int i=0; i<n-1; i++) {
+            int u, v;
+            cin >> u >> v;
+            u--, v--;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        if (k == 2) {
+            cout << M(n) * (n - 1) / 2 << "\n";
+            continue;
+        }
+
+        M ret = 0;
+        for (int center=0; center<n; center++) {
+            vector<int> dist(n, -1), branch(n);
+            queue<int> q;
+            dist[center] = 0;
+            q.push(center);
+            while (!q.empty()) {
+                int u = q.front();
+                q.pop();
+                for (int v : adj[u])
+                    if (dist[v] == -1) {
+                        dist[v] = dist[u] + 1;
+                        if (u == center)
+                            branch[v] = v;
+                        else
+                            branch[v] = branch[u];
+                        q.push(v);
+                    }
+            }
+
+            vector<vector<int>> cnt(n, vector<int>(n));
+            for (int u=0; u<n; u++)
+                cnt[dist[u]][branch[u]]++;
+            for (int d=0; d<n; d++) {
+                vector<int> all;
+                for (int j=0; j<n; j++)
+                    if (cnt[d][j] > 0)
+                        all.push_back(cnt[d][j]);
+                DEBUG(center, d, all);
+                vector<M> dp(k + 1);
+                dp[0] = 1;
+                for (int x : all)
+                    for (int j=k; j>=1; j--)
+                        dp[j] += dp[j-1] * x;
+                ret += dp[k];
+            }
+        }
+        cout << ret << "\n";
+    }
+
+    return 0;
+}
