761983908a99590a674adc4d752ade4fad16b679
ishan dutta <ishandutta2007@gmail.com>
2022-10-10 21:51:31 +0530

    aryanc403

---
+++ aryanc403/normal/1197/A.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <queue>
+#include <bitset>
+using namespace std;
+
+#ifdef LOCAL
+	#define eprintf(...) fprintf(stderr, __VA_ARGS__)
+#else
+	#define eprintf(...) 42
+#endif
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, int> pli;
+typedef pair<ll, ll> pll;
+typedef long double ld;
+#define mp make_pair
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+const int N = 100100;
+int n;
+int a[N];
+
+void solve() {
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+		scanf("%d", &a[i]);
+	sort(a, a + n);
+	int k = a[n - 2] - 1;
+	k = min(k, n - 2);
+	printf("%d\n", k);
+}
+
+int main()
+{
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt", "w", stdout);
+
+	int t;
+	scanf("%d", &t);
+	while(t--) solve();
+
+	return 0;
+}
