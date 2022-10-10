a3d6a6d438168fda27066c3f19a190572c7dbafe
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 01:02:53 +0530

    intothenight

---
+++ intothenight/normal/1078/B.cpp
@@ -0,0 +1,164 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+using namespace chrono;
+#define all(a) a.begin(), a.end()
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<string> vs;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<int, ll> pil;
+typedef pair<ll, int> pli;
+typedef tuple<int, int, int> tpl;
+template<class T>
+using Tree = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+template<class T1, class T2>
+istream &operator>>(istream &in, pair<T1, T2> &P){
+	in >> P.first >> P.second;
+	return in;
+}
+template<class T>
+istream &operator>>(istream &in, vector<T> &arr){
+	for(auto &x: arr) in >> x;
+	return in;
+}
+template<class T>
+istream &operator>>(istream &in, deque<T> &arr){
+	for(auto &x: arr) in >> x;
+	return in;
+}
+template<class T1, class T2>
+ostream &operator<<(ostream &out, const pair<T1, T2> &P){
+	out << P.first << " " << P.second << "\n";
+	return out;
+}
+template<class T>
+ostream &operator<<(ostream &out, const vector<T> &arr){
+	for(auto &x: arr) out << x << ' '; cout << "\n";
+	return out;
+}
+template<class T>
+ostream &operator<<(ostream &out, const deque<T> &arr){
+	for(auto &x: arr) out << x << ' '; cout << "\n";
+	return out;
+}
+template<class T>
+ostream &operator<<(ostream &out, const set<T> &arr){
+	for(auto &x: arr) out << x << ' '; cout << "\n";
+	return out;
+}
+template<class T1, class T2>
+ostream &operator<<(ostream &out, const map<T1, T2> &arr){
+	for(auto &x: arr) out << x << ' '; cout << "\n";
+	return out;
+}
+mt19937 rnd(time(0));
+
+
+
+int main(){
+	cin.tie(0)->sync_with_stdio(0);
+	int n;
+	cin >> n;
+	vi cnt(101);
+	for(int i = 0; i < n; i ++){
+		int x;
+		cin >> x;
+		cnt[x] ++;
+	}
+	vector<vi> d(101, vi(10001));
+	d[0][0] = 1;
+	int mx = 0, tcnt = 0;
+	for(int w = 1; w <= 100; w ++){
+		vector<vi> p(d);
+		mx += w * cnt[w];
+		for(int t = mx; t >= w; t --){ // target weight
+			for(int i = 1; i <= cnt[w] && t >= i * w; i ++){// number of w being used
+				for(int j = 0; j <= tcnt; j ++){
+					if(p[j][t - i * w] == 1){
+						d[i + j][t] ++;
+					}
+					else if(p[j][t - i * w] >= 2){
+						d[i + j][t] = 2;
+					}
+				}
+			}
+		}
+		tcnt += cnt[w];
+	}
+	int res = 0;
+	for(int w = 1; w <= 100; w ++){
+		for(int i = 1; i <= cnt[w]; i ++){
+			if(d[i][i * w] == 1){
+				cnt[w] -= i;
+				int t = 0;
+				for(int i = 1; i <= 100; i ++){
+					if(cnt[i]){
+						t ++;
+					}
+				}
+				if(t == 1){
+					cout << n;
+					return 0;
+				}
+				else{
+					res = max(res, i);
+				}
+				cnt[w] += i;
+			}
+		}
+	}
+	cout << res;
+	return 0;
+}
+/*
+100
+
+*/
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  //
+//                                                                                                  //
+//                                              _____________                                       //
+//             ++++++++++                ___------------------\                                    //
+//           +++`+``+`+`++++        ///`````````````````````````````\                              //
+//           ++`+`+``+++`++++  /////```````````````````````````````````\                            //
+//           +++`++`+`+``+++/////`````````````````````````````````````````\                         //
+//             +++`++`+``+///```````````|```````````````````````````````````\                       //
+//          ____++++/++++/`````````````/````````|````````|```````````````````\                      //
+//        /  /  / |    //``````````````|````````|```````|````````|````````````\                     //
+//       /  /  /  | ///````````/```````|```````||```````|````````|``````\```````\                   //
+//       | /  /   |///`````````|``````/````````|````````|````````|```````|```````\                  //
+//       |/   |   |//``|```````|``````|````````|`````````|```````|```````|````````\                 //
+//       /\___|__//`|``|```````|`    |      ``:|````````|:```````|```````|```|`````|                //
+//      /     /  /``|``|``````|/     |        :|     ```:|```````|```````|``++````++                //
+//     /     / //```|``|``````|      |        |:        :|    ```|```````|```++``++`\               //
+//     |    /  /````|``|``````/    _.::::.    |          |      |    ````|```|`++`\`|               //
+//     |   /   |````|``|`````|            `                    |       ``|```++``++`|               //
+//     |  /    |````|``|`````|                                 :         |``++````++|               //
+//     | /     /````|``|`````|                              _.-:::.      |..`|``.`|.|               //
+//     |/     /`````|``|`````|                                     `     |```|````|`|               //
+//    /|      |`````|``|`````|                    :'                    .|```|````|.|               //
+//   / |      |`````|``|`````|                                         /|-|``|````|`|               //
+//  /  |      |`````|```\````|                                        / ||```|````|``\              //
+// /   |      |`````|````|```|::                                    /_| ||```|````|``|              //
+//            |`````|````|```|:|:.        `.._                    .\___/:|```|````|``|              //
+//            |`````\````|```|:|::-          ``:::....           -:|:|:::|```|````|``|              //
+//            |``````|```|```|:|::`|.                          .:::|:|:::|```|````|``|              //
+//             \`````|```|```|:|::/|--.                     .`:|:::|:|:::/```|````|``|              //
+//              |````|```|```\:|:|:|-----             _..-:|:|:|:::|:|::|````|````|`/               //
+//              |````|```|````\|:|:|-------.____.....------|/::|:::|:|::|````|````|`|               //
+//              |````|```|\````\:|/\___________    ________/\--\:::|:|::|````/````|`|               //
+//              |````\```| \```|:/-------------\  /----------\``\::|:|::|```/`````|`|               //
+//              |`````|``|  \``|/---------------\/------------\_________|```|`````|`|               //
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
+//                                                                                                  //
+//                                        Created by Aeren                                          //
+//                                                                                                  //
+//////////////////////////////////////////////////////////////////////////////////////////////////////
