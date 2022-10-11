eea789a5f2444e94e3114ff33f5ac8d94c897caf
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 00:23:55 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/996/A.cpp
@@ -0,0 +1,43 @@
+#include <algorithm>
+#include <bitset>
+#include <cassert>
+#include <chrono>
+#include <cstring>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <random>
+#include <set>
+#include <stack>
+#include <vector>
+
+using namespace std;
+
+// BEGIN NO SAD
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define trav(a, x) for(auto& a : x)
+#define all(x) x.begin(), x.end()
+#define sz(x) (int)(x).size()
+typedef vector<int> vi;
+// END NO SAD
+
+typedef long long ll;
+typedef pair<int, int> pii;
+
+void solve() {
+  ll n;
+  cin >> n;
+  ll ret = 0;
+  for(ll out: {100, 20, 10, 5, 1}) {
+    ret += n / out;
+    n %= out;
+  }
+  cout << ret << "\n";
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  solve();
+}
