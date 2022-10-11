1fabd0dfb48a70b7aa56c105cfb45e267452b95e
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 07:32:39 +0530

    galen_colin

---
+++ galen_colin/normal/1536/F.cpp
@@ -0,0 +1,162 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+/* 
+find my code templates at https://github.com/galencolin/cp-templates
+also maybe subscribe please thanks 
+*/
+ 
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL);}
+#define f first
+#define s second
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+ 
+typedef long long ll;
+// typedef int ll;
+// #pragma warning("int")
+ 
+typedef long double lld;
+typedef unsigned long long ull;
+ 
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+	cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+	cin >> p.first;
+	return cin >> p.second;
+}
+ 
+mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 rng(61378913);
+/* usage - just do rng() */
+ 
+void usaco(string filename) {
+  // #pragma message("be careful, freopen may be wrong")
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+ 
+// #include <atcoder/all>
+// using namespace atcoder;
+ 
+const lld pi = 3.14159265358979323846;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+ 
+namespace modop {
+	ll madd(ll a, ll b) {
+	  return (a + b) % mod;
+	}
+	ll msub(ll a, ll b) {
+	  return (((a - b) % mod) + mod) % mod;
+	}
+	ll mmul(ll a, ll b) {
+	  return ((a % mod) * (b % mod)) % mod;
+	}
+	ll mpow(ll base, ll exp) {
+	  ll res = 1;
+	  while (exp) {
+		if (exp % 2 == 1){
+			res = (res * base) % mod;
+		}
+		exp >>= 1;
+		base = (base * base) % mod;
+	  }
+	  return res;
+	}
+	ll minv(ll base) {
+	  return mpow(base, mod - 2);
+	}
+	ll mdiv(ll a, ll b) {
+	  return mmul(a, minv(b));
+	}
+	
+	const ll FACTORIAL_SIZE = 1.1e6;
+	ll fact[FACTORIAL_SIZE], ifact[FACTORIAL_SIZE];
+	bool __factorials_generated__ = 0;
+	void gen_factorial(ll n) {
+		__factorials_generated__ = 1;
+		fact[0] = fact[1] = ifact[0] = ifact[1] = 1;
+		
+		for (ll i = 2; i <= n; i++) {
+			fact[i] = (i * fact[i - 1]) % mod;
+		}
+		ifact[n] = minv(fact[n]);
+		for (ll i = n - 1; i >= 2; i--) {
+			ifact[i] = ((i + 1) * ifact[i + 1]) % mod;
+		}
+	}
+	ll nck(ll n, ll k) {
+		if (!__factorials_generated__) {
+			cerr << "Call gen_factorial you dope" << endl;
+			exit(1);
+		}
+		ll den = (ifact[k] * ifact[n - k]) % mod;
+		return (den * fact[n]) % mod;
+	}
+}
+
+using namespace modop;
+ 
+ll n, m, k, l, r, x, y, z;
+const ll template_array_size = 1e6 + 15258;
+ll a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+string s, t;
+
+ll dp[1000005]; // (size, bipartite)
+ll f[1000005];
+// ANY MOVE WORKS jesus christ
+
+void solve(int tc = 0) {
+	cin >> n;
+	
+	ll ans = 2 * n;
+	
+	gen_factorial(1.05e6);
+	
+	ll v = 0;
+	for (ll i = n % 2; i < n; i += 2) {
+		if (n - i < i) break;
+		ll x = (fact[n - i - 1]) % mod;
+		x = (x * nck(n - i, i)) % mod;
+		v = (v + x) % mod;
+	}
+	cout << (ans * v) % mod << '\n';
+}
+ 
+int main() {
+	#ifdef galen_colin_local
+		auto begin = std::chrono::high_resolution_clock::now();
+	#endif
+	
+	send help
+ 
+	#ifndef galen_colin_local
+		// usaco("code");
+	#endif
+	
+	// usaco("cowland");
+	
+	// freopen("tc.cpp", "r", stdin);
+	// freopen("tc2.cpp", "w", stdout);
+		
+	cout << setprecision(15) << fixed;
+	
+
+		
+	int tc = 1;
+	// cin >> tc;
+	for (int t = 0; t < tc; t++) solve(t);
+	
+	#ifdef galen_colin_local
+		auto end = std::chrono::high_resolution_clock::now();
+		cerr << setprecision(4) << fixed;
+		cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+	#endif
+}
---
+++ galen_colin/normal/1538/D.cpp
@@ -0,0 +1,180 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+/* 
+find my code templates at https://github.com/galencolin/cp-templates
+also maybe subscribe please thanks 
+*/
+ 
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL);}
+#define f first
+#define s second
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+ 
+typedef long long ll;
+// typedef int ll;
+// #pragma warning("int")
+ 
+typedef long double lld;
+typedef unsigned long long ull;
+ 
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+	cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+	cin >> p.first;
+	return cin >> p.second;
+}
+ 
+mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 rng(61378913);
+/* usage - just do rng() */
+ 
+void usaco(string filename) {
+  // #pragma message("be careful, freopen may be wrong")
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+ 
+// #include <atcoder/all>
+// using namespace atcoder;
+ 
+const lld pi = 3.14159265358979323846;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+ 
+namespace number_theory {
+	ll gcd(ll x, ll y) {
+	  if (x == 0) return y;
+	  if (y == 0) return x;
+	  return gcd(y, x % y);
+	}
+	bool isprime(ll n) { 
+	  if (n <= 1) return false; 
+	  if (n <= 3) return true; 
+	  
+	  if (n % 2 == 0 || n % 3 == 0) return false; 
+	  
+	  for (ll i = 5; i * i <= n; i += 6) 
+		if (n % i == 0 || n % (i+2) == 0) 
+		  return false; 
+	  
+	  return true; 
+	} 
+	 
+	bool prime[15000105]; 
+	void sieve(int n) { 
+	  for (ll i = 0; i <= n; i++) prime[i] = 1;
+	  for (ll p = 2; p * p <= n; p++) { 
+		if (prime[p] == true) { 
+		  for (ll i = p * p; i <= n; i += p) 
+			prime[i] = false; 
+		} 
+	  } 
+	  prime[1] = prime[0] = 0;
+	} 
+	 
+	vector<ll> primelist;
+	bool __primes_generated__ = 0;
+	 
+	void genprimes(int n) {
+	  __primes_generated__ = 1;
+	  sieve(n + 1);
+	  for (ll i = 2; i <= n; i++) if (prime[i]) primelist.push_back(i);
+	}
+	 
+	vector<ll> factors(ll n) {
+	  if (!__primes_generated__) {
+		cerr << "Call genprimes you dope" << endl;
+		exit(1);
+	  }
+	  vector<ll> facs;
+	 
+	  for (ll i = 0; primelist[i] * primelist[i] <= n && i < primelist.size(); i++) {
+		if (n % primelist[i] == 0) {
+		  while (n % primelist[i] == 0) {
+			n /= primelist[i];
+			facs.push_back(primelist[i]);
+		  }
+		}
+	  }
+	  if (n > 1) {
+		facs.push_back(n);
+	  }
+	  sort(facs.begin(), facs.end());
+	  return facs;
+	}
+	
+	vector<ll> getdivs(ll n) {
+    vector<ll> divs;
+    for (ll i = 1; i * i <= n; i++) {
+      if (n % i == 0) {
+        divs.push_back(i);
+        divs.push_back(n / i);
+      }
+    }
+
+    getunique(divs);
+    return divs;
+  }
+}
+
+using namespace number_theory;
+
+ 
+ll n, m, k, l, r, x, y, z;
+const ll template_array_size = 1e6 + 15258;
+ll a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+string s, t;
+
+
+
+void solve(int tc = 0) {
+	cin >> x >> y >> k;
+	
+	if (k == 1) {
+		cout << ((x % y == 0 || y % x == 0) && x != y ? "YES\n" : "NO\n");
+		return;
+	}
+	
+	vector<ll> a = factors(x), b = factors(y);
+	
+	cout << (a.size() + b.size() >= k ? "YES\n" : "NO\n");
+}
+ 
+int main() {
+	#ifdef galen_colin_local
+		auto begin = std::chrono::high_resolution_clock::now();
+	#endif
+	
+	send help
+ 
+	#ifndef galen_colin_local
+		// usaco("code");
+	#endif
+	
+	// usaco("cowland");
+	
+	// freopen("tc.cpp", "r", stdin);
+	// freopen("tc2.cpp", "w", stdout);
+		
+	cout << setprecision(15) << fixed;
+	
+	genprimes(1e6);
+		
+	int tc = 1;
+	cin >> tc;
+	for (int t = 0; t < tc; t++) solve(t);
+	
+	#ifdef galen_colin_local
+		auto end = std::chrono::high_resolution_clock::now();
+		cerr << setprecision(4) << fixed;
+		cerr << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+	#endif
+}
