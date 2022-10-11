f2f2e6d5ad638644223806a761c7f61445f08c19
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 07:59:14 +0530

    galen_colin

---
+++ galen_colin/normal/603/A.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+#define send {ios_base::sync_with_stdio(false);}
+#define help {cin.tie(NULL);}
+#define f first
+#define s second
+#define getunique(v) {sort(v.begin(), v.end()); v.erase(unique(v.begin(), v.end()), v.end());}
+typedef long long ll;
+typedef long double lld;
+typedef unsigned long long ull;
+ 
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+	cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+template<typename A, typename B> istream& operator>>(istream& cin, pair<A, B> &p) {
+	cin >> p.first;
+	return cin >> p.second;
+}
+ 
+mt19937_64 rng(std::chrono::steady_clock::now().time_since_epoch().count());
+// mt19937_64 rng(61378913);
+/* usage - just do rng() */
+ 
+void usaco(string filename) {
+  // #pragma message("be careful, freopen may be wrong")
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+ 
+// #include <atcoder/all>
+// using namespace atcoder;
+ 
+const lld pi = 3.14159265358979323846;
+const ll mod = 1000000007;
+// const ll mod = 998244353;
+// ll mod;
+ 
+
+ 
+ll n, m, k, q, l, r, x, y, z;
+const ll template_array_size = 1e6 + 5827;
+ll a[template_array_size];
+ll b[template_array_size];
+ll c[template_array_size];
+string s, t;
+ll ans = 0;
+
+// 1200 1500 1600 1700 2000 2100
+
+/*
+1
+2 C
+3
+4 C
+5
+6 S
+*/
+
+const lld eps = 1e-10;
+
+// 300
+void solve(int tc = 0) {
+	ll dp[3][2];
+	memset(dp, 0, sizeof(dp));
+	
+	ll ans = 0;
+	
+	cin >> n >> s;
+	for (ll i = 0; i < n; i++) {
+		ll v = s[i] - '0';
+		
+		ll ndp[3][2];
+		memset(ndp, 0, sizeof(dp));
+		
+		ndp[0][v] = 1 + dp[0][!v];
+		ndp[1][v] = dp[1][v];
+		ndp[2][v] = 1 + max({dp[0][!v], dp[1][!v], dp[2][!v]});
+		
+		ndp[0][!v] = dp[0][!v];
+		ndp[1][!v] = 1 + max({dp[0][v], dp[1][v]});
+		ndp[2][!v] = dp[2][!v];
+		
+		for (ll i = 0; i < 3; i++) {
+			for (ll j = 0; j < 2; j++) {
+				ans = max(ans, ndp[i][j]);
+				dp[i][j] = ndp[i][j];
+			}
+		}
+	}
+	cout << ans << '\n';
+}
+
+int main() {
+	#ifdef galen_colin_local
+		auto begin = std::chrono::high_resolution_clock::now();
+	#endif
+	
+	send help
+ 
+	#ifndef galen_colin_local
+		// usaco("cowpatibility");
+	#endif
+	
+	// usaco("cowland");
+	
+	// freopen("tc.cpp", "r", stdin);
+ 		
+	cout << setprecision(12) << fixed;
+	
+	int tc = 1;
+	// cin >> tc;
+	for (int t = 0; t < tc; t++) solve(t);
+	
+	#ifdef galen_colin_local
+		auto end = std::chrono::high_resolution_clock::now();
+		cout << setprecision(4) << fixed;
+		// cout << "Execution time: " << std::chrono::duration_cast<std::chrono::duration<double>>(end - begin).count() << " seconds" << endl;
+	#endif
+}
