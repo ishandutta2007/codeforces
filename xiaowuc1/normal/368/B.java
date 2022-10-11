a82239db4cd82be0c476b429674b21c942a21f66
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 03:59:31 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/368/B.java
@@ -0,0 +1,72 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+  private static BufferedReader br;
+  private static StringTokenizer st;
+  private static PrintWriter pw;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    //int qq = 1;
+    int qq = Integer.MAX_VALUE;
+    //int qq = readInt();
+    for(int casenum = 1; casenum <= qq; casenum++)  {
+      int n = readInt();
+      int m = readInt();
+      int[] ret = new int[n];
+      int[] list = new int[n];
+      for(int i = 0; i < n; i++) 
+        list[i] = readInt();
+      Set<Integer> set = new HashSet<Integer>();
+      for(int i = n-1; i >= 0; i--) {
+        set.add(list[i]);
+        ret[i] = set.size();
+      }
+      while(m-- > 0) {
+        pw.println(ret[readInt()-1]);
+      }
+    }
+    pw.close();
+  }
+
+  private static void exitImmediately() {
+    pw.close();
+    System.exit(0);
+  }
+
+  private static long readLong() throws IOException {
+    return Long.parseLong(nextToken());
+  }
+
+  private static double readDouble() throws IOException {
+    return Double.parseDouble(nextToken());
+  }
+
+  private static int readInt() throws IOException {
+    return Integer.parseInt(nextToken());
+  }
+
+  private static String nextToken() throws IOException  {
+    while(st == null || !st.hasMoreTokens())  {
+      if(!br.ready()) {
+        exitImmediately();
+      }
+      st = new StringTokenizer(br.readLine().trim());
+    }
+    return st.nextToken();
+  }
+}
