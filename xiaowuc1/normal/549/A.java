d7745b2965dec14528cbf5a836c9d48196831fb5
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 03:17:07 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/549/A.java
@@ -0,0 +1,91 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*; 
+import java.util.*;
+import java.util.regex.*;
+
+/*
+	  br = new BufferedReader(new FileReader("input.txt"));
+	  pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+	  br = new BufferedReader(new InputStreamReader(System.in));
+	  pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	public static void main(String[] args) throws IOException {
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		//int qq = 1;
+		int qq = Integer.MAX_VALUE;
+		//int qq = readInt();
+		for(int casenum = 1; casenum <= qq; casenum++) {
+			int n = readInt();
+			int m = readInt();
+			char[][] grid = new char[n][m];
+			for(int i = 0; i < n; i++) {
+				String s = nextToken();
+				for(int j = 0; j < m; j++) {
+					grid[i][j] = s.charAt(j);
+				}
+			}
+			int ret = 0;
+			for(int i = 0; i < n-1; i++) {
+				for(int j = 0; j < m-1; j++) {
+					char[] list = new char[]{
+							grid[i][j],
+							grid[i+1][j],
+							grid[i][j+1],
+							grid[i+1][j+1],
+					};
+					Arrays.sort(list);
+					if(list[0] == 'a' && list[1] == 'c' && list[2] == 'e' && list[3] == 'f') ret++;
+				}
+			}
+			pw.println(ret);
+		}
+		exitImmediately();
+	}
+
+	private static void exitImmediately() {
+		pw.close();
+		System.exit(0);
+	}
+
+	private static long readLong() throws IOException {
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException {
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException {
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextLine() throws IOException  {
+		if(!br.ready()) {
+			exitImmediately();
+		}
+		st = null;
+		return br.readLine();
+	}
+
+	private static String nextToken() throws IOException  {
+		while(st == null || !st.hasMoreTokens())  {
+			if(!br.ready()) {
+				exitImmediately();
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
