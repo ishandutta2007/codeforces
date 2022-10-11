a94575ea73d90644d8ee62cf4e348789084916cd
ishan dutta <ishandutta2007@gmail.com>
2022-10-11 08:50:11 +0530

    galen_colin

---
+++ galen_colin/normal/1108/E2.cpp
@@ -0,0 +1,229 @@
+#include <bits/stdc++.h>
+#include <chrono> 
+ 
+using namespace std;
+using namespace std::chrono; 
+ 
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+// #pragma optimization_level 3
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+ 
+#define f0r(a, b) for (long long a = 0; a < b; a++)
+#define f1r(a, b, c) for (long long a = b; a < c; a++)
+#define f0rd(a, b) for (long long a = b; a >= 0; a--)
+#define f1rd(a, b, c) for (long long a = b; a >= c; a--)
+#define ms(arr, v) memset(arr, v, sizeof(arr))
+#define pb push_back
+#define io {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define mp make_pair
+#define f first
+#define s second
+#define presum(p, a, n) {p[0] = a[0]; for (int i = 1; i < n; i++) p[i] = a[i] + p[i-1];}
+#define all(v) v.begin(), v.end()
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+#define readgraph(list, edges) for (int i = 0; i < edges; i++) {int a, b; cin >> a >> b; a--; b--; list[a].pb(b); list[b].pb(a);}
+#define ai(a, n) for (int ele = 0; ele < n; ele++) cin >> a[ele];
+#define ain(a, lb, rb) for (int ele = lb; ele <= rb; ele++) cin >> a[ele];
+#define ao(a, n) {for (int ele = 0; ele < n; ele++) { if (ele) cout << " "; cout << a[ele]; } cout << '\n';}
+#define aout(a, lb, rb) {for (int ele = lb; ele <= rb; ele++) { if (ele > lb) cout << " "; cout << a[ele]; } cout << '\n';}
+typedef long long ll;
+typedef double ld;
+typedef long double lld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+ 
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+  cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+ 
+// template<typename A, typename B> ll max(A x, B y) {
+//   return x > y ? x : y;
+// }
+// template<typename A, typename B> ll min(A x, B y) {
+//   return x < y ? x : y;
+// }
+ 
+mt19937 rng(steady_clock::now().time_since_epoch().count());
+/* usage - just do rng() */
+ 
+void usaco(string filename) {
+  #pragma message("be careful, freopen may be wrong")
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+ 
+const ll mod = 1000000007;
+
+template <typename num_t> 
+struct segtree {
+  int n, depth;
+  vector<num_t> tree, lazy;
+
+  void init(int s, long long* arr) {
+    n = s;
+    tree = vector<num_t>(4 * s, 0);
+    lazy = vector<num_t>(4 * s, 0);
+    init(0, 0, n - 1, arr);
+  }
+
+  num_t init(int i, int l, int r, long long* arr) {
+    if (l == r) return tree[i] = arr[l];
+
+    int mid = (l + r) / 2;
+    num_t a = init(2 * i + 1, l, mid, arr),
+          b = init(2 * i + 2, mid + 1, r, arr);
+    return tree[i] = a.op(b);
+  }
+
+  void update(int l, int r, num_t v) {
+	if (l > r) return;
+    update(0, 0, n - 1, l, r, v);
+  }
+
+  num_t update(int i, int tl, int tr, int ql, int qr, num_t v) {
+    eval_lazy(i, tl, tr);
+	
+	if (tl > tr || tr < ql || qr < tl) return tree[i];
+    if (ql <= tl && tr <= qr) {
+      lazy[i] = lazy[i].val + v.val;
+      eval_lazy(i, tl, tr);
+      return tree[i];
+    }
+    
+    if (tl == tr) return tree[i];
+
+    int mid = (tl + tr) / 2;
+    num_t a = update(2 * i + 1, tl, mid, ql, qr, v),
+          b = update(2 * i + 2, mid + 1, tr, ql, qr, v);
+    return tree[i] = a.op(b);
+  }
+
+  num_t query(int l, int r) {
+	if (l > r) return num_t::null_v;
+    return query(0, 0, n-1, l, r);
+  }
+
+  num_t query(int i, int tl, int tr, int ql, int qr) {
+    eval_lazy(i, tl, tr);
+    
+    if (ql <= tl && tr <= qr) return tree[i];
+    if (tl > tr || tr < ql || qr < tl) return num_t::null_v;
+
+    int mid = (tl + tr) / 2;
+    num_t a = query(2 * i + 1, tl, mid, ql, qr),
+          b = query(2 * i + 2, mid + 1, tr, ql, qr);
+    return a.op(b);
+  }
+
+  void eval_lazy(int i, int l, int r) {
+    tree[i] = tree[i].lazy_op(lazy[i], (r - l + 1));
+    if (l != r) {
+      lazy[i * 2 + 1] = lazy[i].val + lazy[i * 2 + 1].val;
+      lazy[i * 2 + 2] = lazy[i].val + lazy[i * 2 + 2].val;
+    }
+
+    lazy[i] = num_t();
+  }
+};
+
+struct min_t {
+  long long val;
+  static const long long null_v = 9223372036854775807LL;
+
+  min_t(): val(0) {}
+  min_t(long long v): val(v) {}
+
+  min_t op(min_t& other) {
+    return min_t(min(val, other.val));
+  }
+  
+  min_t lazy_op(min_t v, int size) {
+    return min_t(val + v.val);
+  }
+};
+
+ll n, m, k, q, Q, T, l, r, x, y, z;
+ll a[1000005];
+ll b[1000005];
+ll c[1000005];
+string s, t;
+ll ans = 0;
+
+segtree<min_t> st;
+
+vpi slist, sleft, sright;
+
+int main() {
+  io;
+  // freopen("case", "r", stdin);
+  // freopen("test.txt", "r", stdin);
+  // freopen("case", "w", stdout);
+  // freopen("file.in", "r", stdin);
+ 
+  // usaco("file");
+  
+  cin >> n >> m;
+  ai(a, n);
+
+  st.init(n, a);
+
+  f0r(i, m) {
+    cin >> l >> r;
+    --l; --r;
+
+    slist.pb(mp(l, r));
+    sleft.pb(mp(l, r));
+    sright.pb(mp(r, l));
+  }
+
+  sort(all(sleft));
+  sort(all(sright));
+
+  int lpt = 0, rpt = 0;
+  for (pii x: slist) {
+    st.update(x.f, x.s, -1);
+  }
+
+  f0r(i, n) {
+    while (lpt < m && sleft[lpt].f == i) {
+      st.update(sleft[lpt].f, sleft[lpt].s, 1);
+      ++lpt;
+    }
+
+    c[i] = a[i] - st.query(0, n - 1).val;
+    ans = max(ans, c[i]);
+
+    while (rpt < m && sright[rpt].f == i) {
+      st.update(sright[rpt].s, sright[rpt].f, -1);
+      ++rpt;
+    }
+  }
+
+  cout << ans << endl;
+
+  f0r(i, n) {
+    if (c[i] == ans) {
+      vi seq;
+      f0r(j, m) {
+        if (!(slist[j].f <= i && i <= slist[j].s)) {
+          seq.pb(j + 1);
+        }
+      }
+
+      cout << seq.size() << endl;
+      ao(seq, seq.size());
+
+      break;
+    }
+  }
+}
---
+++ galen_colin/normal/1263/E.cpp
@@ -0,0 +1,240 @@
+#include <bits/stdc++.h>
+#include <chrono> 
+ 
+using namespace std;
+using namespace std::chrono; 
+ 
+// #pragma GCC target ("avx2")
+// #pragma GCC optimization ("O3")
+// #pragma GCC optimization ("unroll-loops")
+// #pragma optimization_level 3
+// #pragma GCC optimize("Ofast,no-stack-protector,unroll-loops,fast-math,O3")
+// #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+ 
+#define f0r(a, b) for (long long a = 0; a < b; a++)
+#define f1r(a, b, c) for (long long a = b; a < c; a++)
+#define f0rd(a, b) for (long long a = b; a >= 0; a--)
+#define f1rd(a, b, c) for (long long a = b; a >= c; a--)
+#define ms(arr, v) memset(arr, v, sizeof(arr))
+#define pb push_back
+#define io {ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);}
+#define mp make_pair
+#define f first
+#define s second
+#define presum(p, a, n) {p[0] = a[0]; for (int i = 1; i < n; i++) p[i] = a[i] + p[i-1];}
+#define all(v) v.begin(), v.end()
+#define getunique(v) {sort(all(v)); v.erase(unique(all(v)), v.end());}
+#define readgraph(list, edges) for (int i = 0; i < edges; i++) {int a, b; cin >> a >> b; a--; b--; list[a].pb(b); list[b].pb(a);}
+#define ai(a, n) for (int ele = 0; ele < n; ele++) cin >> a[ele];
+#define ain(a, lb, rb) for (int ele = lb; ele <= rb; ele++) cin >> a[ele];
+#define ao(a, n) {for (int ele = 0; ele < n; ele++) { if (ele) cout << " "; cout << a[ele]; } cout << '\n';}
+#define aout(a, lb, rb) {for (int ele = lb; ele <= rb; ele++) { if (ele > lb) cout << " "; cout << a[ele]; } cout << '\n';}
+typedef long long ll;
+typedef double ld;
+typedef long double lld;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpi;
+typedef vector<pll> vpl;
+ 
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v);
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+  cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+ 
+// template<typename A, typename B> ll max(A x, B y) {
+//   return x > y ? x : y;
+// }
+// template<typename A, typename B> ll min(A x, B y) {
+//   return x < y ? x : y;
+// }
+ 
+mt19937 rng(steady_clock::now().time_since_epoch().count());
+/* usage - just do rng() */
+ 
+void usaco(string filename) {
+  #pragma message("be careful, freopen may be wrong")
+	freopen((filename + ".in").c_str(), "r", stdin);
+	freopen((filename + ".out").c_str(), "w", stdout);
+}
+ 
+const ll mod = 1000000007;
+
+template <typename num_t> 
+struct segtree {
+  int n, depth;
+  vector<num_t> tree, lazy;
+
+  void init(int s, long long* arr) {
+    n = s;
+    tree = vector<num_t>(4 * s, 0);
+    lazy = vector<num_t>(4 * s, 0);
+    init(0, 0, n - 1, arr);
+  }
+
+  num_t init(int i, int l, int r, long long* arr) {
+    if (l == r) return tree[i] = arr[l];
+
+    int mid = (l + r) / 2;
+    num_t a = init(2 * i + 1, l, mid, arr),
+          b = init(2 * i + 2, mid + 1, r, arr);
+    return tree[i] = a.op(b);
+  }
+
+  void update(int l, int r, num_t v) {
+	if (l > r) return;
+    update(0, 0, n - 1, l, r, v);
+  }
+
+  num_t update(int i, int tl, int tr, int ql, int qr, num_t v) {
+    eval_lazy(i, tl, tr);
+	
+	if (tl > tr || tr < ql || qr < tl) return tree[i];
+    if (ql <= tl && tr <= qr) {
+      lazy[i] = lazy[i].val + v.val;
+      eval_lazy(i, tl, tr);
+      return tree[i];
+    }
+    
+    if (tl == tr) return tree[i];
+
+    int mid = (tl + tr) / 2;
+    num_t a = update(2 * i + 1, tl, mid, ql, qr, v),
+          b = update(2 * i + 2, mid + 1, tr, ql, qr, v);
+    return tree[i] = a.op(b);
+  }
+
+  num_t query(int l, int r) {
+	if (l > r) return num_t::null_v;
+    return query(0, 0, n-1, l, r);
+  }
+
+  num_t query(int i, int tl, int tr, int ql, int qr) {
+    eval_lazy(i, tl, tr);
+    
+    if (ql <= tl && tr <= qr) return tree[i];
+    if (tl > tr || tr < ql || qr < tl) return num_t::null_v;
+
+    int mid = (tl + tr) / 2;
+    num_t a = query(2 * i + 1, tl, mid, ql, qr),
+          b = query(2 * i + 2, mid + 1, tr, ql, qr);
+    return a.op(b);
+  }
+
+  void eval_lazy(int i, int l, int r) {
+    tree[i] = tree[i].lazy_op(lazy[i], (r - l + 1));
+    if (l != r) {
+      lazy[i * 2 + 1] = lazy[i].val + lazy[i * 2 + 1].val;
+      lazy[i * 2 + 2] = lazy[i].val + lazy[i * 2 + 2].val;
+    }
+
+    lazy[i] = num_t();
+  }
+};
+
+struct max_t {
+  int val;
+  static const int null_v = -100000000;
+
+  max_t(): val(0) {}
+  max_t(int v): val(v) {}
+
+  max_t op(max_t& other) {
+    return max_t(max(val, other.val));
+  }
+  
+  max_t lazy_op(max_t v, int size) {
+    return max_t(val + v.val);
+  }
+};
+
+struct min_t {
+  long long val;
+  static const long long null_v = 9223372036854775807LL;
+
+  min_t(): val(0) {}
+  min_t(long long v): val(v) {}
+
+  min_t op(min_t& other) {
+    return min_t(min(val, other.val));
+  }
+  
+  min_t lazy_op(min_t v, int size) {
+    return min_t(val + v.val);
+  }
+};
+
+
+ll n, m, k, q, Q, T, l, r, x, y, z;
+ll a[1000005];
+ll b[1000005];
+ll c[1000005];
+string s, t;
+ll ans = 0;
+
+int get(char c) {
+  if (c == '(') return 1;
+  if (c == ')') return -1;
+  return 0;
+}
+
+segtree<max_t> st;
+segtree<min_t> mt;
+
+int main() {
+  io;
+  // freopen("case", "r", stdin);
+  // freopen("test.txt", "r", stdin);
+  // freopen("case", "w", stdout);
+  // freopen("file.in", "r", stdin);
+ 
+  // usaco("file");
+  
+  cin >> n >> s;
+  
+  ms(c, 0);
+  st.init(1000000, c);
+  mt.init(1000000, c);
+
+  f0r(i, 1000000) t.pb('a');
+
+  int pos = 0, cnt = 0, batch = 0;
+
+  f0r(i, n) {
+    if (s[i] == 'L') {
+      --pos;
+      if (pos < 0) pos = 0;
+    } else if (s[i] == 'R') {
+      ++pos;
+    } else {
+      int old = get(t[pos]);
+      int ne = get(s[i]);
+
+      t[pos] = s[i];
+
+      st.update(pos, 999999, ne - old);
+      mt.update(pos, 999999, ne - old);
+
+      cnt += ne - old;
+
+      if (cnt != 0) {
+        batch = -1;
+      } else {
+        ll v = mt.query(0, 999999).val;
+
+        if (v < -1) v = -1;
+        if (v > -1) {
+          v = st.query(0, 999999).val;
+        }
+        batch = v;
+      }
+    }
+
+    b[i] = batch;
+  }
+  ao(b, n);
+}
