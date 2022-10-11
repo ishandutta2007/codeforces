70d9647f5981a394fa1f2ba9b49c7d7315b2fdb3
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 04:15:51 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/76/D.java
@@ -0,0 +1,67 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+
+/*
+	br = new BufferedReader(new FileReader("input.txt"));
+	pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+	br = new BufferedReader(new InputStreamReader(System.in));
+	pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+
+ */
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	public static void main(String[] args) throws IOException	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		//int qq = 1;
+		int qq = Integer.MAX_VALUE;
+		//int qq = readInt();
+		BigInteger two = BigInteger.valueOf(2);
+		for(int casenum = 1; casenum <= qq; casenum++)	{
+			BigInteger x = new BigInteger(nextToken());
+			BigInteger y = new BigInteger(nextToken());
+			if(x.compareTo(y) < 0)	{
+				pw.println(-1);
+			}
+			else if(!x.mod(two).equals(y.mod(two)))	{
+				pw.println(-1);
+			}
+			else	{
+				pw.println(x.subtract(y).divide(two) + " " + x.add(y).divide(two));
+			}
+		}
+		pw.close();
+	}
+
+	private static long readLong() throws IOException	{
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException	{
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException	{
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextToken() throws IOException	{
+		while(st == null || !st.hasMoreTokens())	{
+			if(!br.ready())	{
+				pw.close();
+				System.exit(0);
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
