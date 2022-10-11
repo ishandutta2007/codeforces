1fad12902205026b3ccb209d22f864402ce00df7
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 04:02:31 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/390/B.java
@@ -0,0 +1,78 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+  private static BufferedReader br;
+  private static StringTokenizer st;
+  private static PrintWriter pw;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    //int qq = 1;
+    int qq = Integer.MAX_VALUE;
+    //int qq = readInt();
+    for(int casenum = 1; casenum <= qq; casenum++)  {
+      int n = readInt();
+      int[] max = new int[n];
+      for(int i = 0; i < n; i++) {
+        max[i] = readInt();
+      }
+      int[] actual = new int[n];
+      for(int i = 0; i < n; i++) {
+        actual[i] = readInt();
+      }
+      long ret = 0;
+      for(int i = 0; i < n; i++) {
+        if(2*max[i] < actual[i] || actual[i] == 1) {
+          ret--;
+        }
+        else {
+          long curr = actual[i];
+          ret += (curr/2) * (curr - (curr/2));
+        }
+      }
+      pw.println(ret);
+    }
+    pw.close();
+  }
+
+  private static void exitImmediately() {
+    pw.close();
+    System.exit(0);
+  }
+
+  private static long readLong() throws IOException {
+    return Long.parseLong(nextToken());
+  }
+
+  private static double readDouble() throws IOException {
+    return Double.parseDouble(nextToken());
+  }
+
+  private static int readInt() throws IOException {
+    return Integer.parseInt(nextToken());
+  }
+
+  private static String nextToken() throws IOException  {
+    while(st == null || !st.hasMoreTokens())  {
+      if(!br.ready()) {
+        exitImmediately();
+      }
+      st = new StringTokenizer(br.readLine().trim());
+    }
+    return st.nextToken();
+  }
+}
