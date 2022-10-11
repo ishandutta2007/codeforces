713391dc0e7300ffe048e0db9611937bb4b1f326
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 04:20:04 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/265/B.java
@@ -0,0 +1,76 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+/*
+	br = new BufferedReader(new FileReader("input.txt"));
+	pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+	br = new BufferedReader(new InputStreamReader(System.in));
+	pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	static int[] dx = new int[]{-1,0,0,0,1};
+	static int[] dy = new int[]{0,-1,0,1,0};
+
+	public static void main(String[] args) throws IOException	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		int qq = 1;
+		//int qq = Integer.MAX_VALUE;
+		//int qq = readInt();
+		for(int casenum = 1; casenum <= qq; casenum++)	{
+			int n = readInt();
+			long ret = 0;
+			int[] list = new int[n];
+			for(int i = 0; i < n; i++)	{
+				list[i] = readInt();
+			}
+			ret += list[0];
+			for(int i = 1; i < n; i++)	{
+				ret += Math.abs(list[i-1] - list[i]);
+			}
+			pw.println(ret + 2*n-1);
+		}
+		pw.close();
+	}
+
+	public static boolean good(int curr)	{
+		String ret = String.valueOf(curr);
+		Set<Character> set = new HashSet<Character>();
+		for(int i = 0; i < ret.length(); i++)	{
+			set.add(ret.charAt(i));
+		}
+		return set.size() == ret.length();
+	}
+	
+	private static long readLong() throws IOException	{
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException	{
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException	{
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextToken() throws IOException	{
+		while(st == null || !st.hasMoreTokens())	{
+			if(!br.ready())	{
+				pw.close();
+				System.exit(0);
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
---
+++ xiaowuc1/normal/266/A.java
@@ -0,0 +1,59 @@
+import java.awt.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	public static void main(String[] args) throws IOException	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		readInt();
+		String str = nextToken();
+		int ret = 0;
+		for(int i = 1; i < str.length(); i++)	{
+			if(str.charAt(i) == str.charAt(i-1))	{
+				ret++;
+			}
+		}
+		pw.println(ret);
+		pw.close();
+	}
+	
+	/* NOTEBOOK CODE */
+
+	private static long readLong() throws IOException	{
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException	{
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException	{
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextToken() throws IOException	{
+		while(st == null || !st.hasMoreTokens())	{
+			if(!br.ready())	{
+				pw.close();
+				System.exit(0);
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
