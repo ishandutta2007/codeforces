2669eb397df8803ffc776aa0980bb5cbd99d3895
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 03:40:25 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/474/B.java
@@ -0,0 +1,83 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	public static void main(String[] args) throws IOException	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		int qq = 1;
+		//int qq = Integer.MAX_VALUE;
+		//int qq = readInt();
+		for(int casenum = 1; casenum <= qq; casenum++)	{
+			int n = readInt();
+			int curr = 0;
+			Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+			for(int i = 1; i <= n; i++) {
+				int k = readInt();
+				while(k-- > 0) {
+					curr++;
+					map.put(curr, i);
+				}
+			}
+			int qqq = readInt();
+			while(qqq-- > 0) {
+				pw.println(map.get(readInt()));
+			}
+		}
+		pw.close();
+	}
+
+	static class State {
+		public int num, appear;
+		public State(int a, int b) {
+			num = a;
+			appear = b;
+		}
+	}
+	
+	private static void exitImmediately()	{
+		pw.close();
+		System.exit(0);
+	}
+
+	private static long readLong() throws IOException	{
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException	{
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException	{
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextToken() throws IOException	{
+		while(st == null || !st.hasMoreTokens())	{
+			if(!br.ready())	{
+				exitImmediately();
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
