d125929175e53cbdfe36a3238ebfbb5109f1fef9
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 03:39:16 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/424/C.java
@@ -0,0 +1,79 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+import static java.lang.Math.min;
+import static java.lang.Math.max;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	public static void main(String[] args) throws IOException	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		int qq = 1;
+		//int qq = Integer.MAX_VALUE;
+		//int qq = readInt();
+		int[] prefix = new int[1000005];
+		for(int i = 1; i < prefix.length; i++) {
+			prefix[i] = prefix[i-1] ^ i;
+		}
+		for(int casenum = 1; casenum <= qq; casenum++)	{
+			int n = readInt();
+			int ret = 0;
+			for(int i = 0; i < n; i++) {
+				ret ^= readInt();
+			}
+			for(int mod = 1; mod <= n; mod++) {
+				int div = n / mod;
+				int rem = n % mod;
+				if(div % 2 == 1) {
+					ret ^= prefix[mod-1];
+				}
+				ret ^= prefix[rem];
+			}
+			pw.println(ret);
+		}
+		pw.close();
+	}
+
+	private static void exitImmediately()	{
+		pw.close();
+		System.exit(0);
+	}
+
+	private static long readLong() throws IOException	{
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException	{
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException	{
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextToken() throws IOException	{
+		while(st == null || !st.hasMoreTokens())	{
+			if(!br.ready())	{
+				exitImmediately();
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
