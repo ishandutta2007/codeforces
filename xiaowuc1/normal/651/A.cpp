f9f3ec3bd17c28f4e225be2fd044e7f50c1b8851
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 02:39:18 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/651/A.cpp
@@ -0,0 +1,65 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// DID YOU FIX GLOBAL STATE
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<pii, int> ppiii;
+const double PI = 2 * acos(0);
+const int MOD = 1000000007;
+const int HASH = 3137;
+
+void solve() {
+  int a, b;
+  int ret = 0;
+  scanf("%d%d", &a, &b);
+  while(a >= 2 || b >= 2) {
+    if(a==0 || b==0) break;
+    if(a < b) {
+      a++;
+      b-=2;
+    }
+    else {
+      b++;
+      a-=2;
+    }
+    ret++;
+  }
+  printf("%d\n", ret);
+}
+
+int main() {
+  /*
+  freopen("integral.in","r",stdin);
+  freopen("integral.out","w",stdout);
+  */
+  int qq = 1;
+  //int qq = 1e9;
+  //int qq; scanf("%d\n", &qq);
+  for(int casenum = 1; casenum <= qq; casenum++) {
+    solve();
+  }
+  return 0;
+}
---
+++ xiaowuc1/normal/681/B.cpp
@@ -0,0 +1,61 @@
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <iostream>
+#include <map>
+#include <memory>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+// DID YOU FIX GLOBAL STATE
+
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<pii, int> ppiii;
+const double PI = 2 * acos(0);
+const int MOD = 1000000007;
+const int HASH = 3137;
+
+void solve() {
+  int n;
+  scanf("%d", &n);
+  for(int i = 0; i * 1234567 <= n; i++) {
+    for(int j = 0; i*1234567 + j*123456 <= n; j++) {
+      int k = n - i*1234567 - j*123456;
+      if(k%1234 == 0) {
+        printf("YES\n");
+        return;
+      }
+    }
+  }
+  printf("NO\n");
+}
+
+int main() {
+  /*
+  freopen("integral.in","r",stdin);
+  freopen("integral.out","w",stdout);
+  */
+  int qq = 1;
+  //int qq = 1e9;
+  //int qq; scanf("%d\n", &qq);
+  for(int casenum = 1; casenum <= qq; casenum++) {
+    solve();
+  }
+  return 0;
+}
