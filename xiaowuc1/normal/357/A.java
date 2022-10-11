27f017c8b39e41bffcce341c6a1b51287114fc77
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 04:01:57 +0530

    xiaowuc1

---
+++ xiaowuc1/normal/357/A.java
@@ -0,0 +1,73 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+	private static BufferedReader br;
+	private static StringTokenizer st;
+	private static PrintWriter pw;
+
+	public static void main(String[] args) throws IOException	{
+		br = new BufferedReader(new InputStreamReader(System.in));
+		pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+		//int qq = 1;
+		int qq = Integer.MAX_VALUE;
+		//int qq = readInt();
+		for(int casenum = 1; casenum <= qq; casenum++)	{
+			int n = readInt();
+			int[] list = new int[n+1];
+			for(int i = 1; i <= n; i++) {
+				list[i] = readInt() + list[i-1];
+			}
+			int x = readInt();
+			int y = readInt();
+			for(int i = 1; i <= n; i++) {
+				if(list[i] >= x && list[i] <= y && (list[n] - list[i]) >= x && (list[n] - list[i]) <= y) {
+					pw.println(i+1);
+					pw.close();
+					return;
+				}
+			}
+			pw.println(0);
+		}
+		pw.close();
+	}
+
+	private static void exitImmediately()	{
+		pw.close();
+		System.exit(0);
+	}
+
+	private static long readLong() throws IOException	{
+		return Long.parseLong(nextToken());
+	}
+
+	private static double readDouble() throws IOException	{
+		return Double.parseDouble(nextToken());
+	}
+
+	private static int readInt() throws IOException	{
+		return Integer.parseInt(nextToken());
+	}
+
+	private static String nextToken() throws IOException	{
+		while(st == null || !st.hasMoreTokens())	{
+			if(!br.ready())	{
+				exitImmediately();
+			}
+			st = new StringTokenizer(br.readLine().trim());
+		}
+		return st.nextToken();
+	}
+}
---
+++ xiaowuc1/normal/387/A.java
@@ -0,0 +1,71 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.geom.*;
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+
+/*
+br = new BufferedReader(new FileReader("input.txt"));
+pw = new PrintWriter(new BufferedWriter(new FileWriter("output.txt")));
+br = new BufferedReader(new InputStreamReader(System.in));
+pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+ */
+
+public class Main {
+  private static BufferedReader br;
+  private static StringTokenizer st;
+  private static PrintWriter pw;
+
+  public static void main(String[] args) throws IOException {
+    br = new BufferedReader(new InputStreamReader(System.in));
+    pw = new PrintWriter(new BufferedWriter(new OutputStreamWriter(System.out)));
+    //int qq = 1;
+    int qq = Integer.MAX_VALUE;
+    //int qq = readInt();
+    for(int casenum = 1; casenum <= qq; casenum++)  {
+      int end = parse(nextToken());
+      int start = parse(nextToken());
+      int ret = 0;
+      while(start != end) {
+        ret++;
+        start++;
+        start %= 1440;
+      }
+      pw.printf("%02d:%02d\n", ret/60, ret%60);
+    }
+    pw.close();
+  }
+
+  public static int parse(String str) {
+    return 60 * Integer.parseInt(str.substring(0,2)) + Integer.parseInt(str.substring(3));
+  }
+  
+  private static void exitImmediately() {
+    pw.close();
+    System.exit(0);
+  }
+
+  private static long readLong() throws IOException {
+    return Long.parseLong(nextToken());
+  }
+
+  private static double readDouble() throws IOException {
+    return Double.parseDouble(nextToken());
+  }
+
+  private static int readInt() throws IOException {
+    return Integer.parseInt(nextToken());
+  }
+
+  private static String nextToken() throws IOException  {
+    while(st == null || !st.hasMoreTokens())  {
+      if(!br.ready()) {
+        exitImmediately();
+      }
+      st = new StringTokenizer(br.readLine().trim());
+    }
+    return st.nextToken();
+  }
+}
