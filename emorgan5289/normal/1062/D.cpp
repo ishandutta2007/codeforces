71a76a65f1bd5b0ee4cda389633657ae4519691e
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 10:26:43 +0530

    emorgan5289

---
+++ emorgan5289/normal/1062/D.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+const int inf = 1e9+10;
+const ll inf_ll = 1e18+10;
+#define all(x) (x).begin(), (x).end()
+#define pb push_back
+#define cmax(x, y) (x = max(x, y))
+#define cmin(x, y) (x = min(x, y))
+
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "../../debug.cpp"
+#endif
+
+struct dsu {
+
+    vector<int> p, sz;
+
+    dsu(int n) : p(n), sz(n, 1) {
+        iota(p.begin(), p.end(), 0);
+    }
+
+    int rep(int x) {
+        while (x != p[x]) x = p[x] = p[p[x]];
+        return x;
+    }
+
+    // returns true if a and b are in the same set, and then unites them.
+    bool unite(int a, int b) {
+        a = rep(a), b = rep(b);
+        if (sz[a] < sz[b]) swap(a, b);
+        if (a != b) p[b] = a, sz[a] += sz[b];
+        return a == b;
+    }
+
+    // returns true if a and b are in the same set.
+    bool query(int a, int b) {
+        return rep(a) == rep(b);
+    }
+
+    // returns the size of the set containing x
+    int size(int x) {
+        return sz[rep(x)];
+    }
+};
+
+int main() {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    ll n; cin >> n;
+    dsu u(n+1);
+    vector<ll> v(n+1);
+    for (int i = 2; i <= n; i++)
+        for (int j = 2*i; j <= n; j += i)
+            u.unite(i, j);
+    for (int i = 2; i <= n; i++)
+        for (int j = 2*i; j <= n; j += i)
+            v[u.rep(i)] += j/i;
+    cout << 4**max_element(all(v)) << "\n";
+}
