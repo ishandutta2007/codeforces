4023c880c2bfdd97add8589ff289600b6fa77b5a
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 08:28:00 +0530

    igori

---
+++ igori/normal/4/D.cpp
@@ -0,0 +1,219 @@
+// Code it, hate me?
+// .................
+// WA 138 or TL 109?
+// Hope, Accepted...
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <map>
+#include <set>
+#include <cstring>
+#include <cmath>
+#include <stdio.h>
+using namespace std;
+#define sqr(z) (z) * (z)
+#define ch_NO(x) cout << "NO\n"; x = 1 - x;
+#define b_NO cout << "NO\n"; break;
+#define r_NO cout << "NO\n"; return 0;
+#define c_NO cout << "NO\n"; continue;
+#define ch_YES(x) cout << "YES\n"; x = 1 - x;
+#define b_YES cout << "YES\n"; break;
+#define r_YES cout << "YES\n"; return 0;
+#define c_YES cout << "YES\n"; continue;
+#define sz size()
+#define pb push_back
+#define int_ "%d",
+#define fi first
+#define se second
+#define mm(k) memset((k), -1, sizeof(k));
+#define all(k) (k).begin(), (k).end()
+#define forn(i, n) for (int (i) = 0; (i) != (n); i++)
+#define nfor(i, n) for (int (i) = (n) - 1; (i) != -1; i--)
+#define do_nothing cout << " Work_Error ";
+#define erace erase
+#define incert insert
+#define reverce reverse
+#define re return
+#define prev prev1234
+typedef vector<int> vi;
+typedef pair<int, int> pii;
+typedef long long ll;
+typedef double ld;
+typedef pair<ll, ll> lpii;
+const int INF = 1e9;
+const ll INFLL = 1e18 + 1;
+const int MAX = 200001;
+const ll MOD = 1000000007;
+ll inq(ll k, ll q)
+{
+    if (q == 0)
+        return 1;
+    ll l = inq(k, q / 2);
+    if (q % 2 == 0)
+        return l * l % MOD;
+    else
+        return (l * l) % MOD * k % MOD;
+}
+ll gcd(ll a, ll b)
+{
+    if (a < b)
+        swap(a, b);
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+ll cubr(ll a)
+{
+    ll l = -1, r = 1e6 + 2;
+    while (l < r - 1)
+    {
+        ll mid = (l + r) / 2;
+        if (mid * mid * mid > a)
+            r = mid;
+        else
+            l = mid;
+    }
+    return l;
+}
+ll max(ll a, ll b)
+{
+    if (a > b)
+        return a;
+    return b;
+}
+ll min(ll a, ll b)
+{
+    return -1 * max(-a, -b);
+}
+ll possible(ll q)
+{
+    if (q == INF)
+        return -1;
+    return q;
+}
+bool correct(int x, int xx)
+{
+    if (x < 0)
+        return 0;
+    if (x >= xx)
+        return 0;
+    return 1;
+}
+ll dsumm(ll x, ll k)
+{
+    ll y = 1;
+    ll z = 0;
+    for (int i = 0; y < 1e18; i++)
+    {
+        z += x / y % k;
+        y *= k;
+    }
+    return z;
+}
+ll dcount(ll x)
+{
+    ll y = 1;
+    ll z = 0;
+    int c[100];
+    for (int i = 0; i < 10; i++)
+    c[i] = 0;
+    for (int i = 0; x > 0; i++)
+    {
+        if (c[x / y % 10] == 0)
+        z++;
+        c[x / y % 10] = 1;
+        x /= 10;
+    }
+    return z;
+}
+ll lg10(ll x)
+{
+    if (x == 0)
+        return 0;
+    return lg10(x / 10) + 1;
+}
+
+#define bitcount __builtin_popcount
+#define bitcountl __builtin_popcountll
+
+#define RA(n, arr) cin >> n; forn(i, n) cin >> (arr)[i];
+#define WA(n, arr) cout << n << "\n"; forn(i, n) cout << (arr)[i] << " ";
+#define WWA(n, arr) forn(i, n) cout << (arr)[i] << " ";
+#define RSA(n, arr) cin >> n; forn(i, n) cin >> (arr)[i]; sort(arr, arr + n);
+#define WSA(n, arr) cout << n << "\n"; sort(arr, arr + n); forn(i, n) cout << (arr)[i] << " ";
+#define WWSA(n, arr) sort(arr, arr + n); forn(i, n) cout << (arr)[i] << " ";
+#define WSV(vec) cout << vec.size() << "\n"; sort(all(vec)); forn(i, vec.sz) cout << (vec)[i] << " ";
+#define WWSV(vec) sort(all(vec)); forn(i, vec.sz) cout << (vec)[i] << " ";
+
+#define cnks(l) for (int i = 0; i < l; i++) cnk[i][0] = 1; for (int i = 0; i < l; i++) cnk[0][i] = 1; for (int i = 0; i < l; i++) for (int j = 1; j < i; j++) cnk[i][j] = cnk[i - 1][j] + cnk[i - 1][j - 1];
+#define facts(l) fact[0] = 1; for (int i = 1; i < l; i++) fact[i] = fact[i - 1] * i;
+#define stirling_2s(l) stirling_2[0][0] = 1; for (int i = 1; i < l; i++) stirling_2[i][0] = 0; for (int i = 0; i < l; i++) for (int j = 1; j <= i; j++) stirling_2[i][j] = stirling_2[i - 1][j - 1] + j * stirling_2[i - 1][j];
+//______________________
+ll n, x0, y0;
+pii xy[5000];
+ll dp[5000];
+ll prev[5000];
+map<pii, int> id;
+
+int main()
+{
+    mm(dp);
+    cin >> n >> x0 >> y0;
+    forn(i, n)
+    {
+        cin >> xy[i].fi >> xy[i].se;
+        id[{xy[i].fi, xy[i].se}] = i;
+    }
+    sort(xy, xy + n);
+    ll ans = 0, x = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (xy[i].fi > x0 && xy[i].se > y0)
+        {
+            //cout << i << endl;
+            dp[i] = 1;
+            prev[i] = -1;
+            if (dp[i] > ans)
+            {
+                ans = dp[i];
+                x = i;
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (xy[j].fi > xy[i].fi && xy[j].se > xy[i].se)
+            {
+                if (dp[i] + 1 > dp[j] && dp[i] != -1)
+                {
+                    dp[j] = dp[i] + 1;
+                    prev[j] = i;
+                    if (dp[j] > ans)
+                    {
+                        ans = dp[j];
+                        x = j;
+                    }
+                }
+            }
+        }
+    }
+    //cout << ans << endl;
+    vi answ;
+    while (x != -1)
+    {
+        answ.pb(x);
+        x = prev[x];
+    }
+    cout << answ.sz << endl;
+    reverse(all(answ));
+    forn(i, answ.sz)
+    {
+        int x = xy[answ[i]].fi;
+        int y = xy[answ[i]].se;
+        cout << id[{x, y}] + 1 << " ";
+    }
+}
