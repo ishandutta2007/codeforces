2b3e3eebaa9f4845dce27beaf25d444bb9befc7f
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 06:10:14 +0530

    anandoza

---
+++ anandoza/normal/1182/E.cpp
@@ -0,0 +1,421 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+// template {{{
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define lb lower_bound
+#define ub upper_bound
+#define f first
+#define s second
+#define resz resize
+
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+#define L1(u, ...) [&](auto &&u) { return __VA_ARGS__; }
+#define L2(u, v, ...) [&](auto &&u, auto &&v) { return __VA_ARGS__; }
+
+#define sort_by(x, y) sort(all(x), [&](const auto &l, const auto &r) { return y; })
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vpdd = vector<pdd>;
+
+template <typename T> void ckmin(T &a, const T &b) { a = min(a, b); }
+template <typename T> void ckmax(T &a, const T &b) { a = max(a, b); }
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+namespace __input {
+    template <class T1, class T2> void re(pair<T1, T2> &p);
+    template <class T> void re(vector<T> &a);
+    template <class T, size_t SZ> void re(array<T, SZ> &a);
+
+    template <class T> void re(T &x) { cin >> x; }
+    void re(double &x) { string t; re(t); x = stod(t); }
+    template <class Arg, class... Args> void re(Arg &first, Args &...rest) { re(first); re(rest...); }
+
+    template <class T1, class T2> void re(pair<T1, T2> &p) { re(p.f, p.s); }
+    template <class T> void re(vector<T> &a) { F0R (i, sz(a)) re(a[i]); }
+    template <class T, size_t SZ> void re(array<T, SZ> &a) { F0R (i, SZ) re(a[i]); }
+}
+using namespace __input;
+
+namespace __output {
+    template <typename T> struct is_outputtable { template <typename C> static constexpr decltype(declval<ostream &>() << declval<const C &>(), bool()) test(int) { return true; } template <typename C> static constexpr bool test(...) { return false; } static constexpr bool value = test<T>(int()); };
+    template <class T, typename V = decltype(declval<const T &>().begin()), typename S = typename enable_if<!is_outputtable<T>::value, bool>::type> void pr(const T &x);
+
+    template <class T, typename V = decltype(declval<ostream &>() << declval<const T &>())> void pr(const T &x) { cout << x; }
+    template <class T1, class T2> void pr(const pair<T1, T2> &x);
+    template <class Arg, class... Args> void pr(const Arg &first, const Args &...rest) { pr(first); pr(rest...); }
+
+    template <class T, bool pretty = true> void prContain(const T &x) { if (pretty) pr("{"); bool fst = 1; for (const auto &a : x) pr(!fst ? pretty ? ", " : " " : "", a), fst = 0; if (pretty) pr("}"); }
+
+    template <class T> void pc(const T &x) { prContain<T, false>(x); pr("\n"); }
+    template <class T1, class T2> void pr(const pair<T1, T2> &x) { pr("{", x.f, ", ", x.s, "}"); }
+    template <class T, typename V, typename S> void pr(const T &x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template <class Arg> void ps(const Arg &first) { pr(first); ps(); }
+    template <class Arg, class... Args> void ps(const Arg &first, const Args &...rest) { pr(first, " "); ps(rest...); }
+}
+using namespace __output;
+
+#define TRACE(x) x
+#define __pn(x) pr(#x, " = ")
+#ifdef ANAND_LOCAL
+#define pd(...) pr("\033[1;31m"), __pn((__VA_ARGS__)), ps(__VA_ARGS__), pr("\033[0m"), cout << flush
+#else
+#define pd(...)
+#endif
+
+namespace __algorithm {
+    template <typename T> void dedup(vector<T> &v) { sort(all(v)); v.erase(unique(all(v)), v.end()); }
+    template <typename T> typename vector<T>::const_iterator find(const vector<T> &v, const T &x) { auto it = lower_bound(all(v), x); return it != v.end() && *it == x ? it : v.end(); }
+    template <typename T> size_t index(const vector<T> &v, const T &x) { auto it = find(v, x); assert(it != v.end() && *it == x); return it - v.begin(); }
+    template <typename T1, typename T2> typename vector<pair<T1, T2>>::iterator lower_bound(const vector<pair<T1, T2>> &v, const T1 &x) { return lower_bound(all(v), x, [](pair<T1, T2> a, pair<T1, T2> b) { return a.f < b.f; }); }
+    template <typename T1, typename T2> typename vector<pair<T1, T2>>::iterator upper_bound(const vector<pair<T1, T2>> &v, const T1 &x) { return upper_bound(all(v), x, [](pair<T1, T2> a, pair<T1, T2> b) { return a.f < b.f; }); }
+    template <typename I, typename B, typename E> struct reversed { I &v_; explicit reversed(I &v) : v_{v} {} B begin() const { return rbegin(v_); } E end() const { return rend(v_); } };
+}
+using namespace __algorithm;
+
+struct __monostate {
+    friend istream &operator>>(istream &is, const __attribute__((unused)) __monostate &ms) { return is; }
+    friend ostream &operator<<(ostream &os, const __attribute__((unused)) __monostate &ms) { return os; }
+} ms;
+
+namespace __io {
+    void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+    void setOut(string s) { freopen(s.c_str(), "w", stdout); }
+    void setIO() { ios_base::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); }
+}
+using namespace __io;
+// }}}
+// matrix {{{
+template<typename T> struct matrix {
+    int N, M;
+    T* data;
+
+    matrix<T> () : N(0), M(0), data(nullptr) {}
+
+    matrix<T> (int N_, int M_, T f = 0, T d = 0) : N(N_), M(M_) {
+        data = (T*) malloc(N * M * sizeof(T));
+        fill(data, data + N * M, f);
+        for (int i = 0; i < min(N, M); i++)
+            (*this)[i][i] = d;
+    }
+
+    matrix<T> (int N_, int M_, const initializer_list<T>& init) : N(N_), M(M_) {
+        assert(sz(init) == N * M);
+        data = (T*) malloc(N * M * sizeof(T));
+        copy(all(init), data);
+    }
+
+    matrix<T> (const matrix<T>& m) : N(m.N), M(m.M) {
+        data = (T*) malloc(N * M * sizeof(T));
+        copy(m.data, m.data + N * M, data);
+    }
+
+    matrix<T>& operator=(const matrix<T>& o) {
+        if (this != &o) {
+            N = o.N, M = o.M;
+            free(data);
+            data = (T*) malloc(N * M * sizeof(T));
+            copy(o.data, o.data + N * M, data);
+        }
+        return *this;
+    }
+
+    ~matrix<T> () { free(data); }
+
+    explicit operator T() const { assert(N == 1 && M == 1); return data[0]; }
+    T* operator[](int i) { return data + i * M; }
+    const T* operator[](int i) const { return data + i * M; }
+
+    friend matrix<T> operator *(const matrix<T>& a, const matrix<T>& b) {
+        assert(a.M == b.N);
+        matrix<T> res(a.N, b.M);
+        for (int i = 0; i < a.N; i++)
+            for (int j = 0; j < b.M; j++)
+                for (int k = 0; k < a.M; k++)
+                    res[i][j] += a[i][k] * b[k][j];
+        return res;
+    }
+
+    friend vector<T> operator *(const vector<T>& v, const matrix<T>& m) {
+        assert(sz(v) == m.N);
+        vector<T> res(m.M);
+        for (int j = 0; j < m.M; j++)
+            for (int i = 0; i < m.N; i++)
+                res[j] += v[i] * m[i][j];
+        return res;
+    }
+
+    friend vector<T> operator *(const matrix<T>& m, const vector<T>& v) {
+        assert(m.M == sz(v));
+        vector<T> res(m.N);
+        for (int i = 0; i < m.N; i++)
+            for (int j = 0; j < m.M; j++)
+                res[i] += m[i][j] * v[j];
+        return res;
+    }
+
+    matrix pow(ll e) const {
+        assert(N == M);
+        if (e == 0) return matrix<T>(N, M, 0, 1);
+        if (e&1) return *this * pow(e - 1);
+        return (*this * *this).pow(e / 2);
+    }
+
+    int rank() const {
+        matrix<T> m = *this;
+        int r = 0;
+        for (int j = 0; j < M; j++) {
+            for (int i = r; i < N; i++) if (m[i][j] != 0) {
+                swap_ranges(m[r], m[r] + M, m[i]);
+                break;
+            }
+            if (r == N || m[r][j] == 0) continue;
+
+            for (int i = 0; i < N; i++) if (i != r) {
+                T c = m[i][j] / m[r][j];
+                for (int k = 0; k < M; k++)
+                    m[i][k] -= c * m[r][k];
+            }
+            r++;
+        }
+        return r;
+    }
+
+    friend ostream& operator <<(ostream& os, const matrix<T>& m) {
+        for (int i = 0; i < m.N; i++) {
+            os << (i ? i < m.N - 1 ? "\u2503" : "\u2517" : "\n\u250F");
+            for (int j = 0; j < m.M; j++)
+                os << setw(12) << m[i][j];
+            os << "  " << (i ? i < m.N - 1 ? "\u2503" : "\u251B" : "\u2512") << "\n";
+        }
+        return os;
+    }
+};
+// }}}
+// modnum {{{
+using v_t = int;
+using vv_t = ll;
+template<v_t MOD> struct modnum {
+    static_assert(numeric_limits<v_t>::max() / 2 >= MOD, "Addition overflows v_t");
+    static_assert(numeric_limits<vv_t>::max() / MOD >= MOD, "Multiplication overflows vv_t");
+
+    v_t v;
+    modnum() : v(0) {}
+    modnum(vv_t _v) : v(v_t(_v % MOD)) { if (v < 0) v += MOD; }
+    explicit operator v_t() const { return v; }
+    friend istream& operator >> (istream& i, modnum& n) { vv_t w; i >> w; n = modnum(w); return i; }
+    friend ostream& operator << (ostream& o, const modnum& n) { return o << n.v; }
+
+    friend bool operator == (const modnum& a, const modnum& b) { return a.v == b.v; }
+    friend bool operator != (const modnum& a, const modnum& b) { return a.v != b.v; }
+
+    static unsigned fast_mod(uint64_t x, unsigned m = MOD) {
+#if !defined(_WIN32) || defined(_WIN64)
+        return unsigned(x % m);
+#endif
+        // x must be less than 2^32 * m so that x / m fits in a 32-bit integer.
+        unsigned x_high = unsigned(x >> 32), x_low = unsigned(x), quot, rem;
+        asm("divl %4\n"
+                : "=a" (quot), "=d" (rem)
+                : "d" (x_high), "a" (x_low), "r" (m));
+        return rem;
+    }
+
+    modnum& operator += (const modnum& o) { v += o.v; if (v >= MOD) v -= MOD; return *this; }
+    modnum& operator -= (const modnum& o) { v -= o.v; if (v < 0) v += MOD; return *this; }
+    modnum& operator *= (const modnum& o) { v = fast_mod(vv_t(v) * o.v); return *this; }
+    modnum operator - () { modnum res; if (v) res.v = MOD - v; return res; }
+    friend modnum operator + (const modnum& a, const modnum& b) { return modnum(a) += b; }
+    friend modnum operator - (const modnum& a, const modnum& b) { return modnum(a) -= b; }
+    friend modnum operator * (const modnum& a, const modnum& b) { return modnum(a) *= b; }
+
+    modnum pow(vv_t e) const {
+        if (e < 0) return 1 / this->pow(-e);
+        if (e == 0) return 1;
+        if (e & 1) return *this * this->pow(e-1);
+        return (*this * *this).pow(e/2);
+    }
+
+    modnum inv() const {
+        v_t g = MOD, x = 0, y = 1;
+        for (v_t r = v; r != 0; ) {
+            v_t q = g / r;
+            g %= r; swap(g, r);
+            x -= q * y; swap(x, y);
+        }
+
+        assert(g == 1);
+        assert(y == MOD || y == -MOD);
+        return x < 0 ? x + MOD : x;
+    }
+    modnum& operator /= (const modnum& o) { return (*this) *= o.inv(); }
+    friend modnum operator / (const modnum& a, const modnum& b) { return modnum(a) /= modnum(b); }
+
+    static constexpr v_t totient() {
+        v_t tot = MOD, tmp = MOD;
+        for (v_t p = 2; p * p <= tmp; p++) if (tmp % p == 0) {
+            tot = tot / p * (p - 1);
+            while (tmp % p == 0) tmp /= p;
+        }
+        if (tmp > 1) tot = tot / tmp * (tmp - 1);
+        return tot;
+    }
+
+    static v_t primitive_root() {
+        if (MOD == 1) return 0;
+        if (MOD == 2) return 1;
+
+        v_t tot = totient(), tmp = tot;
+        vi tot_pr;
+        for (v_t p = 2; p * p <= tmp; p++) if (tot % p == 0) {
+            tot_pr.push_back(p);
+            while (tmp % p == 0) tmp /= p;
+        }
+        if (tmp > 1) tot_pr.push_back(tmp);
+
+        for (v_t r = 2; r < MOD; r++) if (gcd(r, MOD) == 1) {
+            bool root = true;
+            for (v_t p : tot_pr) root &= modnum(r).pow(tot / p) != 1;
+            if (root) return r;
+        }
+        assert(false);
+    }
+
+    static modnum generator() { static modnum g = primitive_root(); return g; }
+    static v_t discrete_log(modnum v) {
+        static const v_t M = ceil(sqrt(MOD));
+        static unordered_map<v_t, v_t> table;
+        if (table.empty()) {
+            modnum e = 1;
+            for (v_t i = 0; i < M; i++) { table[e.v] = i; e *= generator(); }
+        }
+        static modnum f = generator().pow(totient() - M);
+
+        for (v_t i = 0; i < M; i++) {
+            if (table.count(v.v)) return table[v.v] + i * M;
+            v *= f;
+        }
+        assert(false);
+    }
+
+    static modnum unity_root(int deg) {
+        assert(totient() % deg == 0);
+        return generator().pow(totient() / deg);
+    }
+
+    static modnum unity_root(int deg, int pow) {
+        static vector<modnum> table{ 0, 1 };
+        while (sz(table) <= deg) {
+            modnum w = unity_root(sz(table));
+            for (int i = sz(table)/2, s = sz(table); i < s; i++) {
+                table.push_back(table[i]);
+                table.push_back(table[i] * w);
+            }
+        }
+        return table[deg + (pow < 0 ? deg + pow : pow)];
+    }
+
+    static modnum factorial(int n) {
+        static vector<modnum> fact = {1};
+        assert(n >= 0);
+        if (sz(fact) <= n) {
+            int had = sz(fact);
+            fact.resize(n + 1);
+            for (int i = had; i <= n; i++) fact[i] = fact[i-1] * i;
+        }
+        return fact[n];
+    }
+    static modnum inverse_factorial(int n) {
+        static vector<modnum> finv = {1};
+        assert(n >= 0);
+        if (sz(finv) <= n) {
+            int had = sz(finv);
+            finv.resz(n + 1), finv[n] = factorial(n).inv();
+            for (int i = n - 1; i >= had; i--) finv[i] = finv[i+1] * (i+1);
+        }
+        return finv[n];
+    }
+
+    static modnum small_inv(int n) {
+        assert(n > 0); return factorial(n - 1) * inverse_factorial(n);
+    }
+
+    static modnum ncr(int n, int r) {
+        if (r < 0 || n < r) return 0;
+        return factorial(n) * inverse_factorial(r) * inverse_factorial(n - r);
+    }
+};
+// }}}
+using mn = modnum<int(1e9 + 7)>;
+using vmn = vector<mn>;
+using vvmn = vector<vmn>;
+
+// clang-format on
+
+using M = matrix<modnum<int(1e9 + 7) - 1>>;
+using V = vector<modnum<int(1e9 + 7) - 1>>;
+
+int main() {
+    setIO();
+
+    M t(3, 3);
+    t[0][0] = t[0][1] = t[0][2] = t[1][0] = t[2][1] = 1;
+
+    ll n;
+    re(n);
+    mn f1, f2, f3, c;
+    re(f1, f2, f3, c);
+
+    f1 *= c;
+    f2 *= c * c;
+    f3 *= c * c * c;
+
+    auto p1 = t.pow(n - 3) * V{0, 0, 1};
+    auto p2 = t.pow(n - 3) * V{0, 1, 0};
+    auto p3 = t.pow(n - 3) * V{1, 0, 0};
+
+    pd(p1);
+    pd(p2);
+    pd(p3);
+
+    mn ans = 1;
+    ans *= f1.pow(p1[0].v);
+    ans *= f2.pow(p2[0].v);
+    ans *= f3.pow(p3[0].v);
+
+    ans /= c.pow(n);
+
+    ps(ans);
+
+    return 0;
+}
