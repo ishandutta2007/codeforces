25237206d306aeb23af08b6a82f3f214322cfaf0
ishan dutta <ishandutta2007@gmail.com>
2022-10-12 06:11:42 +0530

    anandoza

---
+++ anandoza/normal/1454/E.cpp
@@ -0,0 +1,232 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// clang-format off
+// template {{{
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define lb lower_bound
+#define ub upper_bound
+#define f first
+#define s second
+#define resz resize
+
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+#define L1(u, ...) [&](auto &&u) { return __VA_ARGS__; }
+#define L2(u, v, ...) [&](auto &&u, auto &&v) { return __VA_ARGS__; }
+
+#define sort_by(x, y) sort(all(x), [&](const auto &l, const auto &r) { return y; })
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+
+using vpii = vector<pii>;
+using vpll = vector<pll>;
+using vpdd = vector<pdd>;
+
+template <typename T> void ckmin(T &a, const T &b) { a = min(a, b); }
+template <typename T> void ckmax(T &a, const T &b) { a = max(a, b); }
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+namespace __input {
+    template <class T1, class T2> void re(pair<T1, T2> &p);
+    template <class T> void re(vector<T> &a);
+    template <class T, size_t SZ> void re(array<T, SZ> &a);
+
+    template <class T> void re(T &x) { cin >> x; }
+    void re(double &x) { string t; re(t); x = stod(t); }
+    template <class Arg, class... Args> void re(Arg &first, Args &...rest) { re(first); re(rest...); }
+
+    template <class T1, class T2> void re(pair<T1, T2> &p) { re(p.f, p.s); }
+    template <class T> void re(vector<T> &a) { F0R (i, sz(a)) re(a[i]); }
+    template <class T, size_t SZ> void re(array<T, SZ> &a) { F0R (i, SZ) re(a[i]); }
+}
+using namespace __input;
+
+namespace __output {
+    template <typename T> struct is_outputtable { template <typename C> static constexpr decltype(declval<ostream &>() << declval<const C &>(), bool()) test(int) { return true; } template <typename C> static constexpr bool test(...) { return false; } static constexpr bool value = test<T>(int()); };
+    template <class T, typename V = decltype(declval<const T &>().begin()), typename S = typename enable_if<!is_outputtable<T>::value, bool>::type> void pr(const T &x);
+
+    template <class T, typename V = decltype(declval<ostream &>() << declval<const T &>())> void pr(const T &x) { cout << x; }
+    template <class T1, class T2> void pr(const pair<T1, T2> &x);
+    template <class Arg, class... Args> void pr(const Arg &first, const Args &...rest) { pr(first); pr(rest...); }
+
+    template <class T, bool pretty = true> void prContain(const T &x) { if (pretty) pr("{"); bool fst = 1; for (const auto &a : x) pr(!fst ? pretty ? ", " : " " : "", a), fst = 0; if (pretty) pr("}"); }
+
+    template <class T> void pc(const T &x) { prContain<T, false>(x); pr("\n"); }
+    template <class T1, class T2> void pr(const pair<T1, T2> &x) { pr("{", x.f, ", ", x.s, "}"); }
+    template <class T, typename V, typename S> void pr(const T &x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template <class Arg> void ps(const Arg &first) { pr(first); ps(); }
+    template <class Arg, class... Args> void ps(const Arg &first, const Args &...rest) { pr(first, " "); ps(rest...); }
+}
+using namespace __output;
+
+#define TRACE(x) x
+#define __pn(x) pr(#x, " = ")
+#ifdef ANAND_LOCAL
+#define pd(...) pr("\033[1;31m"), __pn((__VA_ARGS__)), ps(__VA_ARGS__), pr("\033[0m"), cout << flush
+#else
+#define pd(...)
+#endif
+
+namespace __algorithm {
+    template <typename T> void dedup(vector<T> &v) { sort(all(v)); v.erase(unique(all(v)), v.end()); }
+    template <typename T> typename vector<T>::const_iterator find(const vector<T> &v, const T &x) { auto it = lower_bound(all(v), x); return it != v.end() && *it == x ? it : v.end(); }
+    template <typename T> size_t index(const vector<T> &v, const T &x) { auto it = find(v, x); assert(it != v.end() && *it == x); return it - v.begin(); }
+    template <typename T1, typename T2> typename vector<pair<T1, T2>>::iterator lower_bound(const vector<pair<T1, T2>> &v, const T1 &x) { return lower_bound(all(v), x, [](pair<T1, T2> a, pair<T1, T2> b) { return a.f < b.f; }); }
+    template <typename T1, typename T2> typename vector<pair<T1, T2>>::iterator upper_bound(const vector<pair<T1, T2>> &v, const T1 &x) { return upper_bound(all(v), x, [](pair<T1, T2> a, pair<T1, T2> b) { return a.f < b.f; }); }
+    template <typename I, typename B, typename E> struct reversed { I &v_; explicit reversed(I &v) : v_{v} {} B begin() const { return rbegin(v_); } E end() const { return rend(v_); } };
+}
+using namespace __algorithm;
+
+struct __monostate {
+    friend istream &operator>>(istream &is, const __attribute__((unused)) __monostate &ms) { return is; }
+    friend ostream &operator<<(ostream &os, const __attribute__((unused)) __monostate &ms) { return os; }
+} ms;
+
+namespace __io {
+    void setIn(string s) { freopen(s.c_str(), "r", stdin); }
+    void setOut(string s) { freopen(s.c_str(), "w", stdout); }
+    void setIO(string s = "") { ios_base::sync_with_stdio(0); cin.tie(0); cout << setprecision(15); if (sz(s)) { setIn(s + ".in"), setOut(s + ".out"); } }
+}
+using namespace __io;
+// }}}
+// union_find {{{
+struct union_find {
+    struct node {
+        int par, rnk, size;
+        node(int id = 0) : par(id), rnk(0), size(1) {}
+    };
+
+    vector<node> uf;
+    union_find(int N = 0) : uf(N) {
+        for (int i = 0; i < N; i++)
+            uf[i] = node(i);
+    }
+
+    int rep(int i) {
+        if (i != uf[i].par)
+            uf[i].par = rep(uf[i].par);
+        return uf[i].par;
+    }
+
+    bool unio(int a, int b) {
+        a = rep(a), b = rep(b);
+        if (a == b) return false;
+        if (uf[a].rnk < uf[b].rnk) swap(a, b);
+        uf[b].par = a;
+        uf[a].size += uf[b].size;
+        if (uf[a].rnk == uf[b].rnk) uf[a].rnk++;
+        return true;
+    }
+
+    node& operator[](int i) { return uf[rep(i)]; }
+
+    friend void pr(const union_find& u) {
+        pr("{"); bool f = 1;
+        for (int i = 0; i < sz(u.uf); i++) if (u.uf[i].par == i) {
+            if (!f) pr(", "); else f = 0;
+            pr("[ ", i, " | rank=", u.uf[i].rnk, " size=", u.uf[i].size, " ]");
+        }
+        pr("}");
+    }
+};
+// }}}
+ll nc2(ll v) {
+    if (v < 2) return 0;
+    assert(v < 3.0e9);
+    return v * (v - 1) / 2;
+}
+
+// clang-format on
+
+void solve() {
+    int n;
+    re(n);
+    vvi adj(n);
+    F0R (i, n) {
+        int u, v;
+        re(u, v);
+        u--, v--;
+        adj[u].pb(v);
+        adj[v].pb(u);
+    }
+
+    vi parent(n, -1);
+    set<int> cycle_vertices;
+
+    auto find_cycle = [&](auto self, int cur) {
+        trav (next, adj[cur]) {
+            if (next == parent[cur])
+                continue;
+            if (parent[next] != -1) {
+                cycle_vertices.insert(next);
+                for (int i = cur; i != next; i = parent[i])
+                    cycle_vertices.insert(i);
+                return true;
+            }
+
+            parent[next] = cur;
+            if (self(self, next))
+                return true;
+        }
+        return false;
+    };
+
+    parent[0] = 0;
+    find_cycle(find_cycle, 0);
+
+    union_find uf(n);
+    for (int i = 0; i < n; i++) {
+        trav (j, adj[i]) {
+            if (cycle_vertices.count(i) && cycle_vertices.count(j))
+                continue;
+            uf.unio(i, j);
+        }
+    }
+
+    ll ans = 0;
+    ans += 2 * nc2(n);
+    for (int i = 0; i < n; i++) {
+        if (uf.rep(i) != i)
+            continue;
+        ans -= nc2(uf[i].size);
+    }
+
+    ps(ans);
+}
+
+int main() {
+    setIO();
+
+    int T;
+    re(T);
+    F0R (t, T)
+        solve();
+
+    return 0;
+}
