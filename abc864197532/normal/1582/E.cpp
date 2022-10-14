110330a3a95181b9589da91f1de8e40691b83fda
ishan dutta <ishandutta2007@gmail.com>
2022-10-14 14:53:05 +0530

    abc864197532

---
+++ abc864197532/normal/1582/E.cpp
@@ -0,0 +1,90 @@
+/*
+ *   ##   #####   ####      ####  #    #  ####
+ *  #  #  #    # #    #    #    # #    # #    #
+ * #    # #####  #         #    # #    # #    #
+ * ###### #    # #         #    # # ## # #    #
+ * #    # #    # #    #    #    # ##  ## #    #
+ * #    # #####   ####      ####  #    #  ####
+ */
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define pii pair <int, int>
+#define X first
+#define Y second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+#define info() cout << __PRETTY_FUNCTION__ << ": " << __LINE__ << endl
+void abc() {cout << endl;}
+template <typename T, typename ...U> void abc(T a, U ...b) {
+    cout << a << ' ', abc(b...);
+}
+template <typename T> void printv(T l, T r) {
+    for (; l != r; ++l) cout << *l << " \n"[l + 1 == r];
+}
+template <typename A, typename B> istream& operator >> (istream& o, pair<A, B> &a) {
+    return o >> a.X >> a.Y;
+}
+template <typename A, typename B> ostream& operator << (ostream& o, pair<A, B> a) {
+    return o << '(' << a.X << ", " << a.Y << ')';
+}
+template <typename T> ostream& operator << (ostream& o, vector<T> a) {
+    bool is = false;
+    if (a.empty()) return o << "{}";
+    for (T i : a) {o << (is ? ' ' : '{'), is = true, o << i;}
+    return o << '}';
+}
+template <typename T> struct vv : vector <vector <T>> {
+    vv(int n, int m, T v) : vector <vector <T>> (n, vector <T>(m, v)) {}
+    vv() {}
+};
+template <typename T> struct vvv : vector <vv <T>> {
+    vvv(int n, int m, int k, T v) : vector <vv <T>> (n, vv <T>(m, k, v)) {}
+    vvv() {}
+};
+#ifdef Doludu
+#define test(args...) abc("[" + string(#args) + "]", args)
+#define owo freopen("input.txt", "r", stdin), freopen("output.txt", "w", stdout); 
+#else
+#define test(args...) void(0)
+#define owo ios::sync_with_stdio(false); cin.tie(0)
+#endif
+const int mod = 998244353, N = 1 << 20, logN = 19, K = 1 << 9;
+ 
+int main () {
+    owo;
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector <int> a(n);
+        vector <lli> pre(n + 1);
+        for (int i = 0; i < n; ++i) {
+            cin >> a[i];
+            pre[i + 1] = pre[i] + a[i];
+        }
+        vv <lli> dp(n + 1, 450, 0);
+        dp[n][0] = 1ll << 60;
+        int ans = 0, st = n;
+        for (int j = 1; j < 450; ++j) {
+            lli mx = 0;
+            for (int i = st - j; i >= 0; --i) {
+                dp[i][j] = dp[i + 1][j];
+                mx = max(mx, dp[i + j][j - 1]);
+                if (pre[i + j] - pre[i] < mx) {
+                    dp[i][j] = max(dp[i][j], pre[i + j] - pre[i]);
+                }
+                if (dp[i][j])
+                    ans = j;
+            }
+            if (ans != j)
+                break;
+            n -= j;
+        }
+        cout << ans << endl;
+    }
+}
