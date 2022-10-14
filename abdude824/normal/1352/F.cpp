2a38f3a7e16ab1b85cbf84465fe956d3d85342cd
ishan dutta <ishandutta2007@gmail.com>
2022-10-15 00:44:14 +0530

    abdude824

---
+++ abdude824/normal/1352/F.cpp
@@ -0,0 +1,90 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define fi(n) for(int i=0;i<n;i++)
+#define fj(n) for(int j=0;j<n;j++)
+#define fr(n) for(int i=n-1;i>=0;i--)
+#define fo(i,a,n) for(int i=a;i<n;i++)
+#define vi vector<int>
+#define vs vector<string>
+#define vpi vector<pair<int,int> >
+#define int long long int
+#define all(a) (a).begin(),(a).end()
+#define sortall(x) sort(all(x))
+#define itr(a) for(auto it=a.begin();it!=a.end();it++)
+#define pb push_back
+#define setbits(x) __builtin_popcountll(x)
+#define zerbits(x) __builtinctzll(x)
+#define mod 1000000007
+#define sz(x) (int)x.size()
+#define inf 1e18
+#define W(x) int x;cin>>x;while(x--)
+#define ps(x,y) fixed<<setprecision(y)<<x
+#define mp make_pair
+#define INT_SIZE 32
+#define pr pair<int,int>
+#define deb(x) cout<<#x<<"="<<x<<endl;
+#define F first
+#define S second
+#define pii pair<int,int>
+#define mk(arr,n,type)  type *arr=new type[n];
+
+int mpow(int exp, int base);
+void aeh()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(0);
+#ifndef ONLINE_JUDGE
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif
+}
+int arr[8001], is[8001]; //is will tell if ith number is special or not
+int32_t main()
+{
+	aeh();
+	W(t)
+	{
+		int n0, n1, n2;
+		//n0=> Number of pairs with both as 0//0 0
+		//n1=>nu mber of pairs with one as 1//1 0//0 1
+		//n2=>number of pairs with one as 2//1 1
+		cin >> n0 >> n1 >> n2;
+		if (n0 == 0)
+			n0--;
+		if (n2 == 0)
+			n2--;
+		if (n1 == 0)
+			n1--;
+		fi(n0 + 1)
+		cout << "0";
+		fi(n2 + 1)
+		cout << "1";
+		if (n0 != -1 && n2 != -1)
+			n1--;
+		else if (n0 == -1 && n2 == -1)
+			n1++;
+		int o = 0;
+		if (n2 == -1)
+			o = 1;
+		fi(n1)
+		{
+			cout << o;
+			o = !o;
+		}
+		cout << endl;
+	}
+}
+int mpow(int base, int exp)
+{
+	base %= mod;
+	int result = 1;
+	while (exp > 0)
+	{
+		if (exp & 1)result = (result * base) % mod;
+		base = (base * base) % mod;
+		exp >>= 1;
+	}
+	return result;
+}
