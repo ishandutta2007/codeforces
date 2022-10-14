98a038962a2ee4959f6edcf03e931367235b983e
ishan dutta <ishandutta2007@gmail.com>
2022-10-14 16:15:39 +0530

    matrixcascade_qwq

---
+++ matrixcascade_qwq/normal/1365/G.cpp
@@ -0,0 +1,134 @@
+//#pragma GCC optimize("Ofast", "unroll-loops")
+//#pragma GCC target("sse", "sse2", "sse3", "ssse3", "sse4", "avx")
+#include <bits/stdc++.h>
+#define ll long long
+#define ull unsigned long long
+#define int long long
+#define YES puts("YES")
+#define NO puts("NO")
+#define Yes puts("Yes")
+#define No puts("No")
+#define edl puts("")
+#define mc cerr<<"qwq\n"
+#define error goto gg
+#define def gg:
+#define rd(x) x=read()
+#define opl(x) printf("%lld",x)
+#define opls(x) printf("%lld ",x)
+#define opd(x) printf("%d",x)
+#define opds(x) printf("%d ",x)
+#define ver(x) for(int i=head[x];i;i=nxt[i])
+#define up(i,x,y) for(int i=x,i##end=y;i<=i##end;++i)
+#define pu(i,x,y) for(int i=x,i##end=y;i>=i##end;--i)
+#define ft(x) for(int i=head[x];i;i=nxt[i])
+#define upn up(i,1,n)
+#define upm up(j,1,m)
+#define pun pu(i,n,1)
+#define pum pu(j,m,1)
+#define up_(x) up(i,1,x)
+#define pu_(x) pu(j,x,1)
+#define ep emplace_back
+#define fp for(auto to:
+#define pf )
+#define pii pair<int,int>
+#define pis pair<int,string>
+#define psi pair<string,int>
+#define mkp make_pair
+#define fi first
+#define se second
+#define mii map<int,int>
+#define mis map<int,string>
+#define msi map<string,int>
+#define mvi map<vector<int>,int>
+#define miv map<int,vector<int>>
+#define rdn rd(n)
+#define rdm rd(m)
+#define rdk rd(k)
+#define pb push_back
+using namespace std;
+int n, m, k;
+int read()
+{
+    int s = 0, f = 1;
+    char ch = getchar();
+    while (ch < '0' || ch > '9')
+    {
+        if (ch == '-')
+            f = -1;
+        ch = getchar();
+    }
+    while (ch >= '0' && ch <= '9')
+    {
+        s = s * 10 + ch - '0';
+        ch = getchar();
+    }
+    return s * f;
+}
+
+#define inf 1000000000000000000ll
+ll Max(ll a=-inf,ll b=-inf,ll c=-inf,ll d=-inf,ll e=-inf,ll f=-inf,ll g=-inf,ll h=-inf)
+{
+	return max(max(max(a,b),max(c,d)),max(max(e,f),max(g,h)));
+}
+ll Min(ll a=inf,ll b=inf,ll c=inf,ll d=inf,ll e=inf,ll f=inf,ll g=inf,ll h=inf)
+{
+	return min(min(min(a,b),min(c,d)),min(min(e,f),min(g,h)));
+}
+#undef inf
+void chkmin(int &x,int y)
+{
+	if(x>y)x=y;
+}
+void chkmax(int &x,int y)
+{
+	if(x<y)x=y;
+}
+int id[2020],tot;
+int val[2020],g[2020];
+signed main()
+{
+	int T=1;
+	while(T--)
+	{
+		rdn;
+		up(i,1,(1<<13)-1)
+		{
+			if(__builtin_popcount(i)==6)id[++tot]=i;
+		}
+		
+		up(i,0,12)
+		{
+			vector<int>v;
+			up(j,1,n)
+			{
+				if(id[j]&(1<<i))
+				{
+					v.ep(j);
+				}
+			}
+			if(v.size()==0)continue;
+			cout<<"? "<<v.size()<<" ";
+			fp v pf cout<<to<<" ";
+			cout<<endl;
+			cin>>val[i];
+		}
+		upn
+		{
+			up(j,0,12)
+			{
+				if(!(id[i]&(1<<j)))g[i]|=val[j];
+			}
+		}
+		cout<<"! ";
+		upn cout<<g[i]<<" ";
+		edl;
+		
+	}
+}
+/*
+  When you are coding,remember to:
+	   - clear the arrays if a problem has many tasks.
+	   - pay attention to some special cases(n=0,1).
+	   - Don't code before think completely.
+	   - ...
+*/
