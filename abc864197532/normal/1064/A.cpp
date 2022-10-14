5d71d883d23e89f627f4028e8f11d7fa1573e9ad
ishan dutta <ishandutta2007@gmail.com>
2022-10-14 13:43:52 +0530

    abc864197532

---
+++ abc864197532/normal/1064/A.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define lli long long int
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define test(x) cout << "Line(" << __LINE__ << ") " #x << ' ' << x << endl
+#define printv(x) {\
+	for (auto i : x) cout << i << ' ';\
+	cout << endl;\
+}
+#define pii pair <int, int>
+#define pll pair <lli, lli>
+#define X first
+#define Y second
+#define all(x) x.begin(), x.end()
+#define rall(x) x.rbegin(), x.rend()
+template<typename A, typename B>
+ostream& operator << (ostream& o, pair<A, B> a){
+    return o << a.X << ' ' << a.Y;
+}
+template<typename A, typename B>
+istream& operator >> (istream& o, pair<A, B> &a){
+    return o >> a.X >> a.Y;
+}
+const int mod = 1e9 + 7, abc = 864197532, N = 504, K = 111;
+
+int main () {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    int a[3];
+    cin >> a[0] >> a[1] >> a[2];
+    sort(a, a + 3);
+    cout << max(a[2] - a[0] - a[1] + 1, 0);
+}
---
+++ abc864197532/normal/1104/A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int mod = 1e9 + 7, N = 2000000;
+
+int main () {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  int n;
+  cin >> n;
+  cout << n << endl;
+  while (n--) cout << "1 ";
+}
