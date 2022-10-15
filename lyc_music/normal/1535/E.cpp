14763936060b779bfe2afd84b98fda4feef1069b
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 00:06:01 +0530

    lyc_music

---
+++ lyc_music/normal/1535/E.cpp
@@ -0,0 +1,167 @@
+//QwQcOrZ yyds!!!
+#ifndef _GLIBCXX_NO_ASSERT
+#include <cassert>
+#endif
+#include <cctype>
+#include <cerrno>
+#include <cfloat>
+#include <ciso646>
+#include <climits>
+#include <clocale>
+#include <cmath>
+#include <csetjmp>
+#include <csignal>
+#include <cstdarg>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+
+#if __cplusplus >= 201103L
+#include <ccomplex>
+#include <cfenv>
+#include <cinttypes>
+#include <cstdalign>
+#include <cstdbool>
+#include <cstdint>
+#include <ctgmath>
+#include <cwchar>
+#include <cwctype>
+#endif
+
+// C++
+#include <algorithm>
+#include <bitset>
+#include <complex>
+#include <deque>
+#include <exception>
+#include <fstream>
+#include <functional>
+#include <iomanip>
+#include <ios>
+#include <iosfwd>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <list>
+#include <locale>
+#include <map>
+#include <memory>
+#include <new>
+#include <numeric>
+#include <ostream>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <stdexcept>
+#include <streambuf>
+#include <string>
+#include <typeinfo>
+#include <utility>
+#include <valarray>
+#include <vector>
+
+#if __cplusplus >= 201103L
+#include <array>
+#include <atomic>
+#include <chrono>
+#include <condition_variable>
+#include <forward_list>
+#include <future>
+#include <initializer_list>
+#include <mutex>
+#include <random>
+#include <ratio>
+#include <regex>
+#include <scoped_allocator>
+#include <system_error>
+#include <thread>
+#include <tuple>
+#include <typeindex>
+#include <type_traits>
+#include <unordered_map>
+#include <unordered_set>
+#endif
+#define ll long long
+#define F(i,a,b) for (int i=(a);i<=(b);i++)
+#define R(i,a,b) for (int i=(a);i<(b);i++)
+#define D(i,a,b) for (int i=(a);i>=(b);i--)
+#define go(i,x) for (int i=head[x];i;i=e[i].nx)
+#define mp make_pair
+#define pb push_back
+#define pa pair < int,int >
+#define fi first
+#define se second
+#define re register
+#define be begin()
+#define en end()
+#define sqr(x) ((x)*(x))
+#define ret return puts("-1"),0;
+#define put putchar('\n')
+#define inf 1000000005
+#define mod 998244353
+#define int ll
+#define N 400005
+using namespace std;
+inline char gc(){static char buf[100000],*p1=buf,*p2=buf;return p1==p2&&(p2=(p1=buf)+fread(buf,1,100000,stdin),p1==p2)?EOF:*p1++;}
+#define gc getchar
+inline ll read(){char c=gc();ll su=0,f=1;for (;c<'0'||c>'9';c=gc()) if (c=='-') f=-1;for (;c>='0'&&c<='9';c=gc()) su=su*10+c-'0';return su*f;}
+inline void write(ll x){if (x<0){putchar('-');write(-x);return;}if (x>=10) write(x/10);putchar(x%10+'0');}
+inline void writesp(ll x){write(x),putchar(' ');}
+inline void writeln(ll x){write(x);putchar('\n');}
+int q,a[N],c[N],sn[N],sc[N],opt,x,y,fa[N][21],dep[N],l,r,mid,now,ans,z,anss;
+int get(int x)
+{
+	for (int i=20;i>=0;i--)
+	{
+		if (a[fa[x][i]]) x=fa[x][i];
+	}
+	return x;
+}
+signed main()
+{
+	ios::sync_with_stdio(false);
+	cin>>q>>a[1]>>c[1];
+	for (int i=2;i<=q+1;i++)
+	{
+		cin>>opt>>x>>y;
+		x++;
+		if (opt==1)
+		{
+			cin>>z;
+			fa[i][0]=x;
+			for (int j=1;j<=20;j++)
+				 fa[i][j]=fa[fa[i][j-1]][j-1];
+			a[i]=y,c[i]=z;
+		}
+		else
+		{
+			anss=y,ans=0;
+			while (a[x])
+			{
+				l=get(x);
+				if (a[l]<=y)
+				{
+					y-=a[l];ans+=a[l]*c[l];a[l]=0;
+				} else
+				{
+					ans+=y*c[l];
+					a[l]-=y;
+					y=0;
+					break;
+				}
+			}
+			cout<<anss-y<<" "<<ans<<endl;
+			fflush(stdout);
+		}
+	}
+			
+			
+			
+}
+/*
+
+*/
