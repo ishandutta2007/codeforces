13d848e59fea032bf713746887dfe3c8c66480c1
ishan dutta <ishandutta2007@gmail.com>
2022-10-15 22:15:44 +0530

    ilya_msu

---
+++ ilya_msu/normal/285/B.cpp
@@ -0,0 +1,61 @@
+#define _CRT_SECURE_NO_WARNINGS
+#pragma comment(linker, "/STACK:128000000")
+#define _USE_MATH_DEFINES
+#include<iostream>
+#include<vector>
+#include<string>
+#include<stack>
+#include<algorithm>
+#include<cmath>
+#include<set>
+#include<queue>
+#include<sstream>
+#include<utility>
+#include<map>
+#include<ctime>
+#include<cstdio>
+
+ 
+using namespace std; 
+ 
+typedef long long ll; 
+typedef pair<ll, ll> pll;
+typedef pair<int, int> pii;
+const long double PI = 3.14159265358979323846;  
+const long double gammama = 0.57721566490153286060;
+const long double eps = 1e-5;
+const int INF = 1000 * 1000 * 1000 + 10;
+const int N = 1000 * 1000 * 1000 + 10;
+
+
+int main() {
+	//ios_base::sync_with_stdio(0);
+	//freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+	int n, s, t;
+	cin >> n >> s >> t;
+	vector<int> p(n);
+	for (int i = 0; i < n; ++i)
+		cin >> p[i];
+	if (s == t) {
+		cout << 0 << endl;
+		return 0;
+	}
+	--s;
+	--t;
+	int res = 0;
+	int x = s;
+	while (true) {
+		x = p[x] - 1;
+		++res;
+		if (x == s) {
+			cout << -1 << endl;
+			return 0;
+		}
+		if (x == t) {
+			cout << res << endl;
+			return 0;
+		}
+	}
+	return 0;
+}
