9f9c866d66412485bc404660303ad05d2c21adda
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 22:09:08 +0530

    nok0

---
+++ nok0/normal/1458/E.cpp
@@ -0,0 +1,626 @@
+/**
+ *	author: nok0
+ *	created: 2021.06.09 21:48:22
+**/
+#ifdef LOCAL
+#define _GLIBCXX_DEBUG
+#endif
+
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+
+#pragma region Macros
+// rep macro
+#define foa(v, a) for(auto &v : a)
+#define REPname(a, b, c, d, e, ...) e
+#define REP(...) REPname(__VA_ARGS__, REP3, REP2, REP1, REP0)(__VA_ARGS__)
+#define REP0(x) for(int i = 0; i < (x); ++i)
+#define REP1(i, x) for(int i = 0; i < (x); ++i)
+#define REP2(i, l, r) for(int i = (l); i < (r); ++i)
+#define REP3(i, l, r, c) for(int i = (l); i < (r); i += (c))
+#define REPSname(a, b, c, ...) c
+#define REPS(...) REPSname(__VA_ARGS__, REPS1, REPS0)(__VA_ARGS__)
+#define REPS0(x) for(int i = 1; i <= (x); ++i)
+#define REPS1(i, x) for(int i = 1; i <= (x); ++i)
+#define RREPname(a, b, c, d, e, ...) e
+#define RREP(...) RREPname(__VA_ARGS__, RREP3, RREP2, RREP1, RREP0)(__VA_ARGS__)
+#define RREP0(x) for(int i = (x)-1; i >= 0; --i)
+#define RREP1(i, x) for(int i = (x)-1; i >= 0; --i)
+#define RREP2(i, r, l) for(int i = (r)-1; i >= (l); --i)
+#define RREP3(i, r, l, c) for(int i = (r)-1; i >= (l); i -= (c))
+#define RREPSname(a, b, c, ...) c
+#define RREPS(...) RREPSname(__VA_ARGS__, RREPS1, RREPS0)(__VA_ARGS__)
+#define RREPS0(x) for(int i = (x); i >= 1; --i)
+#define RREPS1(i, x) for(int i = (x); i >= 1; --i)
+
+// name macro
+#define pb push_back
+#define eb emplace_back
+#define SZ(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+#define popcnt(x) __builtin_popcountll(x)
+template <class T = int>
+using V = std::vector<T>;
+template <class T = int>
+using VV = std::vector<std::vector<T>>;
+template <class T>
+using pqup = std::priority_queue<T, std::vector<T>, std::greater<T>>;
+using ll = long long;
+using ld = long double;
+using int128 = __int128_t;
+using pii = std::pair<int, int>;
+using pll = std::pair<long long, long long>;
+
+// input macro
+template <class T, class U>
+std::istream &operator>>(std::istream &is, std::pair<T, U> &p) {
+	is >> p.first >> p.second;
+	return is;
+}
+template <class T>
+std::istream &operator>>(std::istream &is, std::vector<T> &v) {
+	for(T &i : v) is >> i;
+	return is;
+}
+std::istream &operator>>(std::istream &is, __int128_t &a) {
+	std::string s;
+	is >> s;
+	__int128_t ret = 0;
+	for(int i = 0; i < s.length(); i++)
+		if('0' <= s[i] and s[i] <= '9')
+			ret = 10 * ret + s[i] - '0';
+	a = ret * (s[0] == '-' ? -1 : 1);
+	return is;
+}
+#if __has_include(<atcoder/all>)
+std::istream &operator>>(std::istream &is, atcoder::modint998244353 &a) {
+	long long v;
+	is >> v;
+	a = v;
+	return is;
+}
+std::istream &operator>>(std::istream &is, atcoder::modint1000000007 &a) {
+	long long v;
+	is >> v;
+	a = v;
+	return is;
+}
+template <int m>
+std::istream &operator>>(std::istream &is, atcoder::static_modint<m> &a) {
+	long long v;
+	is >> v;
+	a = v;
+	return is;
+}
+template <int m>
+std::istream &operator>>(std::istream &is, atcoder::dynamic_modint<m> &a) {
+	long long v;
+	is >> v;
+	a = v;
+	return is;
+}
+#endif
+namespace scanner {
+void scan(int &a) { std::cin >> a; }
+void scan(long long &a) { std::cin >> a; }
+void scan(std::string &a) { std::cin >> a; }
+void scan(char &a) { std::cin >> a; }
+void scan(char a[]) { std::scanf("%s", a); }
+void scan(double &a) { std::cin >> a; }
+void scan(long double &a) { std::cin >> a; }
+template <class T, class U>
+void scan(std::pair<T, U> &p) { std::cin >> p; }
+template <class T>
+void scan(std::vector<T> &a) { std::cin >> a; }
+void INPUT() {}
+template <class Head, class... Tail>
+void INPUT(Head &head, Tail &... tail) {
+	scan(head);
+	INPUT(tail...);
+}
+}  // namespace scanner
+#define VEC(type, name, size)     \
+	std::vector<type> name(size); \
+	scanner::INPUT(name)
+#define VVEC(type, name, h, w)                                    \
+	std::vector<std::vector<type>> name(h, std::vector<type>(w)); \
+	scanner::INPUT(name)
+#define INT(...)     \
+	int __VA_ARGS__; \
+	scanner::INPUT(__VA_ARGS__)
+#define LL(...)            \
+	long long __VA_ARGS__; \
+	scanner::INPUT(__VA_ARGS__)
+#define STR(...)             \
+	std::string __VA_ARGS__; \
+	scanner::INPUT(__VA_ARGS__)
+#define CHAR(...)     \
+	char __VA_ARGS__; \
+	scanner::INPUT(__VA_ARGS__)
+#define DOUBLE(...)     \
+	double __VA_ARGS__; \
+	scanner::INPUT(__VA_ARGS__)
+#define LD(...)              \
+	long double __VA_ARGS__; \
+	scanner::INPUT(__VA_ARGS__)
+
+// output-macro
+template <class T, class U>
+std::ostream &operator<<(std::ostream &os, const std::pair<T, U> &p) {
+	os << p.first << " " << p.second;
+	return os;
+}
+template <class T>
+std::ostream &operator<<(std::ostream &os, const std::vector<T> &a) {
+	for(int i = 0; i < int(a.size()); ++i) {
+		if(i) os << " ";
+		os << a[i];
+	}
+	return os;
+}
+std::ostream &operator<<(std::ostream &dest, __int128_t &value) {
+	std::ostream::sentry s(dest);
+	if(s) {
+		__uint128_t tmp = value < 0 ? -value : value;
+		char buffer[128];
+		char *d = std::end(buffer);
+		do {
+			--d;
+			*d = "0123456789"[tmp % 10];
+			tmp /= 10;
+		} while(tmp != 0);
+		if(value < 0) {
+			--d;
+			*d = '-';
+		}
+		int len = std::end(buffer) - d;
+		if(dest.rdbuf()->sputn(d, len) != len) {
+			dest.setstate(std::ios_base::badbit);
+		}
+	}
+	return dest;
+}
+#if __has_include(<atcoder/all>)
+std::ostream &operator<<(std::ostream &os, const atcoder::modint998244353 &a) { return os << a.val(); }
+std::ostream &operator<<(std::ostream &os, const atcoder::modint1000000007 &a) { return os << a.val(); }
+template <int m>
+std::ostream &operator<<(std::ostream &os, const atcoder::static_modint<m> &a) { return os << a.val(); }
+template <int m>
+std::ostream &operator<<(std::ostream &os, const atcoder::dynamic_modint<m> &a) { return os << a.val(); }
+#endif
+template <class T>
+void print(const T a) { std::cout << a << '\n'; }
+template <class Head, class... Tail>
+void print(Head H, Tail... T) {
+	std::cout << H << ' ';
+	print(T...);
+}
+template <class T>
+void printel(const T a) { std::cout << a << '\n'; }
+template <class T>
+void printel(const std::vector<T> &a) {
+	for(const auto &v : a)
+		std::cout << v << '\n';
+}
+template <class Head, class... Tail>
+void printel(Head H, Tail... T) {
+	std::cout << H << '\n';
+	printel(T...);
+}
+void Yes(const bool b = true) { std::cout << (b ? "Yes\n" : "No\n"); }
+void No() { std::cout << "No\n"; }
+void YES(const bool b = true) { std::cout << (b ? "YES\n" : "NO\n"); }
+void NO() { std::cout << "NO\n"; }
+void err(const bool b = true) {
+	if(b) {
+		std::cout << "-1\n", exit(0);
+	}
+}
+
+//debug macro
+namespace debugger {
+template <class T>
+void view(const std::vector<T> &a) {
+	std::cerr << "{ ";
+	for(const auto &v : a) {
+		std::cerr << v << ", ";
+	}
+	std::cerr << "\b\b }";
+}
+template <class T>
+void view(const std::vector<std::vector<T>> &a) {
+	std::cerr << "{\n";
+	for(const auto &v : a) {
+		std::cerr << "\t";
+		view(v);
+		std::cerr << "\n";
+	}
+	std::cerr << "}";
+}
+template <class T, class U>
+void view(const std::vector<std::pair<T, U>> &a) {
+	std::cerr << "{\n";
+	for(const auto &p : a) std::cerr << "\t(" << p.first << ", " << p.second << ")\n";
+	std::cerr << "}";
+}
+template <class T, class U>
+void view(const std::map<T, U> &m) {
+	std::cerr << "{\n";
+	for(const auto &p : m) std::cerr << "\t[" << p.first << "] : " << p.second << "\n";
+	std::cerr << "}";
+}
+template <class T, class U>
+void view(const std::pair<T, U> &p) { std::cerr << "(" << p.first << ", " << p.second << ")"; }
+template <class T>
+void view(const std::set<T> &s) {
+	std::cerr << "{ ";
+	for(auto &v : s) {
+		view(v);
+		std::cerr << ", ";
+	}
+	std::cerr << "\b\b }";
+}
+
+template <class T>
+void view(const T &e) { std::cerr << e; }
+}  // namespace debugger
+#ifdef LOCAL
+void debug_out() {}
+template <typename Head, typename... Tail>
+void debug_out(Head H, Tail... T) {
+	debugger::view(H);
+	std::cerr << ", ";
+	debug_out(T...);
+}
+#define debug(...)                                                \
+	do {                                                          \
+		std::cerr << __LINE__ << " [" << #__VA_ARGS__ << "] : ["; \
+		debug_out(__VA_ARGS__);                                   \
+		std::cerr << "\b\b]\n";                                   \
+	} while(false)
+#else
+#define debug(...) (void(0))
+#endif
+
+// vector macro
+template <class T>
+int lb(const std::vector<T> &a, const T x) { return std::distance((a).begin(), std::lower_bound((a).begin(), (a).end(), (x))); }
+template <class T>
+int ub(const std::vector<T> &a, const T x) { return std::distance((a).begin(), std::upper_bound((a).begin(), (a).end(), (x))); }
+template <class T>
+void UNIQUE(std::vector<T> &a) {
+	std::sort(a.begin(), a.end());
+	a.erase(std::unique(a.begin(), a.end()), a.end());
+}
+template <class T>
+std::vector<T> press(std::vector<T> &a) {
+	auto res = a;
+	UNIQUE(res);
+	for(auto &v : a)
+		v = lb(res, v);
+	return res;
+}
+#define SORTname(a, b, c, ...) c
+#define SORT(...) SORTname(__VA_ARGS__, SORT1, SORT0, ...)(__VA_ARGS__)
+#define SORT0(a) std::sort((a).begin(), (a).end())
+#define SORT1(a, c) std::sort((a).begin(), (a).end(), [](const auto x, const auto y) { return x c y; })
+template <class T>
+void ADD(std::vector<T> &a, const T x) {
+	for(auto &v : a) v += x;
+}
+template <class T>
+void SUB(std::vector<T> &a, const T x = 1) {
+	for(auto &v : a) v -= x;
+}
+template <class T>
+void MUL(std::vector<T> &a, const T x) {
+	for(auto &v : a) v *= x;
+}
+template <class T>
+void DIV(std::vector<T> &a, const T x) {
+	for(auto &v : a) v /= x;
+}
+
+// math macro
+template <class T, class U>
+inline bool chmin(T &a, const U &b) { return a > b ? a = b, true : false; }
+template <class T, class U>
+inline bool chmax(T &a, const U &b) { return a < b ? a = b, true : false; }
+template <class T>
+T divup(T x, T y) { return (x + y - 1) / y; }
+template <class T>
+T POW(T a, long long n) {
+	T ret = 1;
+	while(n) {
+		if(n & 1) ret *= a;
+		a *= a;
+		n >>= 1;
+	}
+	return ret;
+}
+// modpow
+long long POW(long long a, long long n, const int mod) {
+	long long ret = 1;
+	while(n) {
+		if(n & 1) (ret *= a) %= mod;
+		(a *= a) %= mod;
+		n >>= 1;
+	}
+	return ret;
+}
+
+// others
+struct fast_io {
+	fast_io() {
+		ios::sync_with_stdio(false);
+		cin.tie(nullptr);
+		cout << fixed << setprecision(15);
+	}
+} fast_io_;
+const int inf = 1e9;
+const ll INF = 1e18;
+#pragma endregion
+
+#pragma region datastructure Range Set
+#include <cassert>
+#include <iostream>
+#include <limits>
+#include <set>
+#include <vector>
+
+template <class T>
+struct RangeSet {
+private:
+	const T TINF = std::numeric_limits<T>::max() / 2;
+	T sum;
+	std::set<std::pair<T, T>> st;
+
+public:
+	RangeSet() : sum(0) {
+		st.emplace(-TINF, -TINF);
+		st.emplace(TINF, TINF);
+	}
+
+	//[l, r) is covered?
+	bool covered(const T l, const T r) {
+		assert(l <= r);
+		if(l == r) return true;
+		auto itr = prev(st.upper_bound({l, TINF}));
+		return itr->first <= l and r <= itr->second;
+	}
+
+	//[x, x + 1) is covered?
+	bool covered(const T x) { return covered(x, x + 1); }
+
+	//return section which covers[l, r)
+	//if not exists, return[-TINF, -TINF)
+	std::pair<T, T> covered_by(const T l, const T r) {
+		assert(l <= r);
+		if(l == r) return {-TINF, -TINF};
+		auto itr = prev(st.upper_bound({l, TINF}));
+		if(itr->first <= l and r <= itr->second) return *itr;
+		return {-TINF, -TINF};
+	}
+
+	//return section which covers[x, x + 1)
+	//if not exists, return[-TINF, -TINF)
+	std::pair<T, T> covered_by(const T x) { return covered_by(x, x + 1); }
+
+	//insert[l, r), and return increment
+	T insert(T l, T r) {
+		assert(l <= r);
+		if(l == r) return T(0);
+		auto itr = prev(st.upper_bound({l, TINF}));
+		if(itr->first <= l and r <= itr->second) return T(0);
+		T sum_erased = T(0);
+		if(itr->first <= l and l <= itr->second) {
+			l = itr->first;
+			sum_erased += itr->second - itr->first;
+			itr = st.erase(itr);
+		} else
+			itr = next(itr);
+		while(r > itr->second) {
+			sum_erased += itr->second - itr->first;
+			itr = st.erase(itr);
+		}
+		if(itr->first <= r) {
+			sum_erased += itr->second - itr->first;
+			r = itr->second;
+			st.erase(itr);
+		}
+		st.emplace(l, r);
+		sum += r - l - sum_erased;
+		return r - l - sum_erased;
+	}
+
+	//insert[x, x + 1), and return increment
+	T insert(const T x) { return insert(x, x + 1); }
+
+	// erase [l, r), and return decrement
+	T erase(const T l, const T r) {
+		assert(l <= r);
+		if(l == r) return T(0);
+		auto itr = prev(st.upper_bound({l, TINF}));
+		if(itr->first <= l and r <= itr->second) {
+			if(itr->first < l) st.emplace(itr->first, l);
+			if(r < itr->second) st.emplace(r, itr->second);
+			st.erase(itr);
+			sum -= r - l;
+			return r - l;
+		}
+
+		T ret = T(0);
+		if(itr->first <= l and l < itr->second) {
+			ret += itr->second - l;
+			if(itr->first < l) st.emplace(itr->first, l);
+			itr = st.erase(itr);
+		} else
+			itr = next(itr);
+		while(itr->second <= r) {
+			ret += itr->second - itr->first;
+			itr = st.erase(itr);
+		}
+		if(itr->first < r) {
+			ret += r - itr->first;
+			st.emplace(r, itr->second);
+			st.erase(itr);
+		}
+		sum -= ret;
+		return ret;
+	}
+
+	// erase [x, x + 1), and return decrement
+	T erase(const T x) { return erase(x, x + 1); }
+
+	int size() const { return (int)st.size() - 2; }
+
+	T mex(const T x = 0) const {
+		auto itr = prev(st.upper_bound({x, TINF}));
+		if(itr->first <= x and x < itr->second)
+			return itr->second;
+		else
+			return x;
+	}
+
+	T sum_all() const { return sum; }
+
+	std::set<std::pair<T, T>> get() const {
+		std::set<std::pair<T, T>> res;
+		for(auto &p : st) {
+			if(std::abs(p.first) == TINF) continue;
+			res.emplace(p.first, p.second);
+		}
+		return res;
+	}
+
+	void dump() const {
+		std::cout << "Rangeset:";
+		for(auto &p : st) {
+			if(std::abs(p.first) == TINF) continue;
+			std::cout << "[" << p.first << "," << p.second << "),";
+		}
+		std::cout << '\n';
+	}
+};
+#pragma endregion
+
+struct line {
+	//y=x+a
+	int sx, gx, a;
+	bool operator<(const line &oth) const { return sx < oth.sx; }
+};
+
+const int n = 11;
+VV<> check(11, V<>(11));
+void test(V<pii> vec) {
+	debug(vec);
+	VV<> board(n, V<>(n, -1));
+	for(auto [x, y] : vec) {
+		board[x][y] = 0;
+	}
+	board[0][0] = 0;
+	REP(i, n) {
+		REP(j, n) {
+			if(board[i][j] == -1) {
+				RangeSet<int> st;
+				REP(k, i) {
+					st.insert(board[k][j]);
+				}
+				REP(k, j) {
+					st.insert(board[i][k]);
+				}
+				board[i][j] = st.mex();
+			}
+		}
+	}
+	REP(i, n) {
+		string s = "";
+		REP(j, n) {
+			//if(board[i][j] == 0) debug(i, j);
+			s.pb(board[i][j] ? 'o' : 'x');
+			check[i][j] = !!board[i][j];
+		}
+		//print(s);
+	}
+}
+
+void main_() {
+	INT(n, m);
+
+	VEC(pii, b, n);
+	b.eb(0, 0);
+
+	set<pii> out;
+	foa(p, b) out.insert(p);
+
+	set<int> xp, yp;
+	for(auto [x, y] : b) {
+		xp.insert(x);
+		yp.insert(y);
+	}
+	xp.insert(inf * 2);
+	yp.insert(inf * 2);
+
+	map<int, int> xminy, yminx;
+	for(auto [x, y] : b) {
+		if(!xminy.count(x)) xminy[x] = y;
+		chmin(xminy[x], y);
+		if(!yminx.count(y)) yminx[y] = x;
+		chmin(yminx[y], x);
+	}
+
+	int nx = 0, ny = 0;
+
+	V<line> lines;
+	while(nx != inf * 2 and ny != inf * 2) {
+		debug(nx, ny);
+		if(xminy.count(nx) or yminx.count(ny)) {
+			int dx = (xminy.count(nx) ? xminy[nx] <= ny : 0);
+			int dy = (yminx.count(ny) ? yminx[ny] <= nx : 0);
+			nx += dx;
+			ny += dy;
+			if(dx + dy == 0) {
+				lines.pb(line{nx, nx + 1, ny - nx});
+				nx++, ny++;
+			}
+		} else {
+			auto new_x = *xp.upper_bound(nx);
+			auto new_y = *yp.upper_bound(ny);
+			if(new_x - nx <= new_y - ny) {
+				lines.pb(line{nx, new_x, ny - nx});
+				ny += new_x - nx, nx = new_x;
+			} else {
+				debug(nx, nx + new_y - ny, ny - nx);
+				lines.pb(line{nx, nx + new_y - ny, ny - nx});
+				nx += new_y - ny, ny = new_y;
+			}
+		}
+	}
+
+	while(m--) {
+		INT(x, y);
+		bool win = 1;
+		if(out.count({x, y}))
+			win = 0;
+		else {
+			auto itr = lower_bound(all(lines), line{x + 1, -inf, -inf});
+			if(itr != lines.begin()) {
+				auto [l, r, a] = *(prev(itr));
+				if(l <= x and x < r and x + a == y) win = 0;
+			}
+		}
+		print(win ? "WIN" : "LOSE");
+	}
+}
+
+int main() {
+	int t = 1;
+	//cin >> t;
+	while(t--) main_();
+	return 0;
+}
