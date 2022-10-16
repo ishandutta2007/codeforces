8d18b1a937cd6a2322ec30c39bbf9895d33fc180
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 07:31:51 +0530

    tmwilliamlin168

---
+++ tmwilliamlin168/normal/848/A.java
@@ -0,0 +1,188 @@
+import java.io.*;
+import java.util.*;
+
+public class MainA {
+	static int[] cnt = new int[26];
+	
+	public static void main(String[] args) {
+		StdIn in = new StdIn();
+		int k=in.nextInt();
+		if(k==0)
+			System.out.print('a');
+		else
+			recur(k, 500, 0);
+	}
+	
+	static void recur(int k, int i, int j) {
+		if(k==0) {
+			for(int l=0; l<26; ++l)
+				for(int m=0; m<cnt[l]; ++m)
+					System.out.print((char)('a'+l));
+			System.exit(0);
+		}
+		if(j==26)
+			return;
+		for(; i>1; --i) {
+			if(i*(i-1)/2>k)
+				continue;
+			cnt[j]=i;
+			recur(k-i*(i-1)/2, i, j+1);
+		}
+	}
+	
+	interface Input {
+		public String next();
+		public String nextLine();
+		public int nextInt();
+		public long nextLong();
+		public double nextDouble();
+	}
+	static class StdIn implements Input {
+		final private int BUFFER_SIZE = 1 << 16;
+		private DataInputStream din;
+		private byte[] buffer;
+		private int bufferPointer, bytesRead;
+
+		public StdIn() {
+			din = new DataInputStream(System.in);
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+		
+		public StdIn(String filename) {
+			try{
+				din = new DataInputStream(new FileInputStream(filename));
+			} catch(Exception e) {
+				throw new RuntimeException();
+			}
+			buffer = new byte[BUFFER_SIZE];
+			bufferPointer = bytesRead = 0;
+		}
+		
+		public String next() {
+			int c;
+			while((c=read())!=-1&&(c==' '||c=='\n'||c=='\r'));
+			StringBuilder s = new StringBuilder();
+			while (c != -1)
+			{
+				if (c == ' ' || c == '\n'||c=='\r')
+					break;
+				s.append((char)c);
+				c=read();
+			}
+			return s.toString();
+		}
+
+		public String nextLine() {
+			int c;
+			while((c=read())!=-1&&(c==' '||c=='\n'||c=='\r'));
+			StringBuilder s = new StringBuilder();
+			while (c != -1)
+			{
+				if (c == '\n'||c=='\r')
+					break;
+				s.append((char)c);
+				c = read();
+			}
+			return s.toString();
+		}
+
+		public int nextInt() {
+			int ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do
+			{
+				ret = ret * 10 + c - '0';
+			}  while ((c = read()) >= '0' && c <= '9');
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+		
+		public int[] readIntArray(int n) {
+			int[] ar = new int[n];
+			for(int i=0; i<n; ++i)
+				ar[i]=nextInt();
+			return ar;
+		}
+
+		public long nextLong() {
+			long ret = 0;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+			do {
+				ret = ret * 10 + c - '0';
+			}
+			while ((c = read()) >= '0' && c <= '9');
+			if (neg)
+				return -ret;
+			return ret;
+		}
+		
+		public long[] readLongArray(int n) {
+			long[] ar = new long[n];
+			for(int i=0; i<n; ++i)
+				ar[i]=nextLong();
+			return ar;
+		}
+
+		public double nextDouble() {
+			double ret = 0, div = 1;
+			byte c = read();
+			while (c <= ' ')
+				c = read();
+			boolean neg = (c == '-');
+			if (neg)
+				c = read();
+
+			do {
+				ret = ret * 10 + c - '0';
+			}
+			while ((c = read()) >= '0' && c <= '9');
+
+			if (c == '.')
+			{
+				while ((c = read()) >= '0' && c <= '9')
+				{
+					ret += (c - '0') / (div *= 10);
+				}
+			}
+
+			if (neg)
+				return -ret;
+			return ret;
+		}
+
+		private void fillBuffer() throws IOException {
+			bytesRead = din.read(buffer, bufferPointer = 0, BUFFER_SIZE);
+			if (bytesRead == -1)
+				buffer[0] = -1;
+		}
+
+		private byte read() {
+			try{
+				if (bufferPointer == bytesRead)
+					fillBuffer();
+				return buffer[bufferPointer++];
+			} catch(IOException e) {
+				throw new RuntimeException();
+			}
+		}
+
+		public void close() throws IOException {
+			if (din == null)
+				return;
+			din.close();
+		}
+	}
+}
