5bea253d6decdebee834cecf4d330529cd135efc
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 11:24:04 +0530

    rfpermen

---
+++ rfpermen/normal/1076/E.cpp
@@ -0,0 +1,142 @@
+#include<bits/stdc++.h>
+ 
+#pragma GCC optimize("O3")
+#pragma GCC target("tune=native")
+#pragma GCC optimize("unroll-loops")
+ 
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(int i=(n);i<=(N);++i)
+#define For(i,n,N) for(int i=(n);i< (N);++i)
+#define rap(i,n,N) for(int i=(n);i>=(N);--i)
+#define rip(i,n,N,V) for(int i=(n);i<=(N);i+=V)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+//#define le left
+//#define ri right
+#define fi first
+#define se second
+#define ff fi.fi
+#define fs fi.se
+#define sf se.fi
+#define ss se.se
+#define lc (id<<1)
+#define rc ((id<<1)|1)
+#define db(x) cout << ">>>> " << #x << " -> " << (x) << endl
+#define out(x,y) cout << ">> " << (x) << " " << (y) << endl
+#define ou3(x,y,z) cout << ">> " << (x) << " " << (y) << " " << (z) << endl
+#define mems(x,y) memset(&x,y,sizeof x)
+#define memsv(x,y) memset(&x[0], y, sizeof(x[0]) * x.size())
+#define popcount __builtin_popcountll
+#define all(x) x.begin(),x.end()
+#define rsort(x) sort(x), reverse(x)
+#define PQ(T) priority_queue<T>
+#define PQr(T) priority_queue<T, vector<T>, greater<T>>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vll vector<pll>
+#define ari(x) array<int,x>
+#define arll(x) array<ll,x>
+#define ard(x) array<double,x>
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define pllll pair<pll,pll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl '\n'
+const int MAX = 1e6+5;
+const ll MOD = 2010408371;
+const ll MOD2 = 2010405347;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+const int block = 100;
+inline ll pw(ll x,ll y,ll md=MOD){ll ret=1;x%=md;while(y){if(y&1)ret=ret*x%md;x=x*x%md,y>>=1;}return ret;}
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int n,m,a,b,k,o,c,d,le,ri,mid,nw,id,hs[MAX],cv[MAX],dep[MAX],sp[19][MAX];
+ll x[MAX],ans[MAX];
+vi v[MAX],z[MAX];
+queue<int> q;
+
+void dfs(int nw,int par){
+	for(int i:v[nw])if(i!=par){
+		ans[i]+= ans[nw];
+		dfs(i,nw);
+	}
+	return;
+}
+
+int main(){
+//	cout<<fixed<<setprecision(10);
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt","w",stdout);
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin>>n;
+	rep(i,2,n){
+		cin>>a>>b;
+		v[a].pb(b);
+		v[b].pb(a);
+	}
+	hs[1] = cv[1] = dep[1] = id = 1;
+	q.push(1);
+	while(!q.empty()){
+		nw = q.front(); q.pop();
+		for(int i:v[nw])if(!hs[i]){
+			hs[i] = ++id;
+			cv[id] = i;
+			sp[0][id] = hs[nw];
+			dep[id] = dep[hs[nw]]+1;
+			z[dep[id]].pb(id);
+			q.push(i);
+		}
+	}
+	
+	rep(i,1,18)rep(j,1,n)sp[i][j] = sp[i-1][sp[i-1][j]];
+	cin>>m;
+	while(m--){
+		cin>>a>>b>>c;
+		nw = hs[a];
+		k = b+1;
+		o = dep[nw]+k;
+		
+		x[nw]+= c, x[nw+1]-= c;
+		if(o<=n && z[o].size()){
+			a = -1;
+			le = z[o][0], ri = z[o].back();
+			while(le<=ri){
+				d = mid = le+ri>>1;
+				rap(i,18,0)if(k&(1<<i))d = sp[i][d];
+				if(d<nw)le = mid+1;
+				else if(d>nw)ri = mid-1;
+				else a = mid, ri = mid-1;
+			}
+			if(a==-1)continue;
+			le = z[o][0], ri = z[o].back();
+			while(le<=ri){
+				d = mid = le+ri>>1;
+				rap(i,18,0)if(k&(1<<i))d = sp[i][d];
+				if(d<nw)le = mid+1;
+				else if(d>nw)ri = mid-1;
+				else b = mid, le = mid+1;
+			}
+			x[a]-= c, x[b+1]+= c;
+		}
+	}
+	rep(i,1,n)x[i]+= x[i-1], ans[cv[i]] = x[i];
+	dfs(1,0);
+	rep(i,1,n){
+		cout<<ans[i]<<" ";
+	}
+	cout<<endl;
+	return 0;
+}
