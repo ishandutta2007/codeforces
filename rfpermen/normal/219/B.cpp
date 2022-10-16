d0a2934aac09ed18aed5ccf17ee0b21e63767257
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 12:19:21 +0530

    rfpermen

---
+++ rfpermen/normal/219/B.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3")
+using namespace std;
+#define ll long long
+#define rep(i,n,N) for(ll i=n;i<=N;i++)
+#define rap(i,n,N) for(int i=(int)n;i>=(int)N;i--)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define fi first
+#define se second
+#define sf se.fi
+#define ss se.se
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define piii pair<int,pii>
+#define piiii pair<piii,pii>
+#define endl "\n"
+const ll MAX=1e5+5;
+const ll MAX2 = 11;
+const int MOD=1000000000 + 7;
+const ll INF=20000;
+const int nr[]={1,0,-1,0,1,1,-1,-1};
+const int nc[]={0,1,0,-1,1,-1,1,-1};
+
+ll n,d,mx,ans,dig;
+
+int main(){
+	cout<<fixed<<setprecision(0);
+    //freopen("input.txt", "r", stdin);
+	//freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>n>>d;
+    d = n-d;
+    dig = 1;
+    while(d<=n){
+    	ans = d;
+    	if((d/dig)%10==9)dig*=10;
+    	else d+=dig;
+	}
+	cout<<ans<<endl;
+    return 0;
+}
