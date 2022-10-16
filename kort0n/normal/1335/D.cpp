9ace51260a7dafa132f7bb0ff4d66d9ad60ae311
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 19:09:15 +0530

    kort0n

---
+++ kort0n/normal/1335/D.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+#define endl "\n"
+using namespace std;
+typedef long long ll;
+typedef pair<ll, ll> l_l;
+typedef pair<int, int> i_i;
+template<class T>
+inline bool chmax(T &a, T b) {
+    if(a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template<class T>
+inline bool chmin(T &a, T b) {
+    if(a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+const long long INF = 1e18;
+//const ll mod = 1000000007;
+string S[9];
+void solve() {
+    for(int i = 0; i < 9; i++) cin >> S[i];
+    for(int i = 0; i < 9; i++) {
+        int j = (i % 3) * 3 + (i / 3);
+        S[i][j] = S[i][(j+1) % 9];
+    }
+    for(int i = 0; i < 9; i++) cout << S[i] << endl;
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int t;
+    cin >> t;
+    while(t--) solve();
+    return 0;
+}
