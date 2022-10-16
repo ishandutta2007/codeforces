e2dc59ff21a7a1652ad6a4d5546fd6f91aef5095
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 11:57:43 +0530

    rfpermen

---
+++ rfpermen/normal/1132/A.cpp
@@ -0,0 +1,48 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3")
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(ll i=n;i<=N;i++)
+#define rap(i,n,N) for(int i=(int)n;i>=(int)N;i--)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define fi first
+#define se second
+#define sf se.fi
+#define ss se.se
+#define lc (id<<1)
+#define rc ((id<<1)|1)
+#define db(x) cout << ">>>>>> " << #x << " -> " << x << endl;
+#define all(x) x.begin(),x.end()
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl "\n"
+const int MAX = 5e3+5;
+const ll MAX2 = 11;
+const int MOD = 998244353;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+
+int a,b,c;
+
+int main(){
+//	cout<<fixed<<setprecision(10);
+//    freopen("input.txt", "r", stdin);
+//	freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>a>>b>>b>>c;
+    if(a!=c||b&&!a)cout<<0;
+    else cout<<1;
+	return 0;
+}
