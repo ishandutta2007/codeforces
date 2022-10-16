245359b79aa30e567162fa3ab4f2b28a4f5a5f51
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 16:33:56 +0530

    rfpermen

---
+++ rfpermen/normal/934/A.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+ 
+#pragma GCC optimize("O3")
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(ll i=n;i<=N;++i)
+#define rap(i,n,N) for(int i=n;i>=N;--i)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define fi first
+#define se second
+#define ff fi.fi
+#define fs fi.se
+#define sf se.fi
+#define ss se.se
+#define lc (id<<1)
+#define rc ((id<<1)|1)
+#define db(x) cout << ">>>>>> " << #x << " -> " << x << endl
+#define all(x) x.begin(),x.end()
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define pllll pair<pll,pll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl '\n'
+const int MAX = 5e4+5;
+const ll MAX2 = 11;
+const ll MOD = 1000000007;
+const ll MOD2 = 1000000006;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+const int block = 316;
+
+ll n,m,x[MAX],y[MAX],ans,nw;
+
+int main(){
+//	cout<<fixed<<setprecision(2);
+//    freopen("input.txt", "r", stdin);
+//	freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>n>>m;
+    rep(i,1,n)cin>>x[i];
+    rep(i,1,m)cin>>y[i];
+    ans = 1e18;
+    rep(i,1,n){
+    	nw = -1e18;
+    	rep(j,1,n){
+    		if(i==j)continue;
+    		rep(k,1,m)nw = max(nw,x[j]*y[k]);
+		}
+		ans = min(ans,nw);
+	}
+	cout<<ans<<endl;
+    return 0;
+}
---
+++ rfpermen/normal/935/D.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+ 
+#pragma GCC optimize("O3")
+#pragma GCC target("tune=native")
+//#pragma GCC optimize("unroll-loops")
+ 
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(int i=(n);i<=(N);++i)
+#define For(i,n,N) for(int i=(n);i< (N);++i)
+#define rap(i,n,N) for(int i=(n);i>=(N);--i)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define fi first
+#define se second
+#define ff fi.fi
+#define fs fi.se
+#define sf se.fi
+#define ss se.se
+#define lc (id<<1)
+#define rc ((id<<1)|1)
+#define db(x) cout << ">>>> " << #x << " -> " << (x) << endl
+#define out(x,y) cout << ">> " << (x) << " " << (y) << endl
+#define ou3(x,y,z) cout << ">> " << (x) << " " << (y) << " " << (z) << endl
+#define mems(x,y) memset(x,y,sizeof x)
+#define all(x) x.begin(),x.end()
+#define PQ(T) priority_queue<T>
+#define PQr(T) priority_queue<T, vector<T>, greater<T>>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vll vector<pll>
+#define ari(x) array<int,x>
+#define arll(x) array<ll,x>
+#define ard(x) array<double,x>
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define pllll pair<pll,pll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl '\n'
+const int MAX = 1e6+5;
+const ll MOD = 1000000007;
+const ll MOD2 = 2010405347;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+const int block = 315;
+inline ll pw(ll x,ll y,ll md=MOD){ll ret=1;while(y){if(y&1)ret=ret*x%md;x=x*x%md,y>>=1;}return ret;}
+inline void add(int &a,int b,int md=MOD){a=a+b>=md?a+b-md:a+b;}
+
+ll n,m,ans,mul,inv,x[MAX],y[MAX],cnt,dv;
+
+int main(){
+//	cout<<fixed<<setprecision(10);
+// freopen("input.txt", "r", stdin);
+// freopen("output.txt","w",stdout);
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin>>n>>m;
+	rep(i,1,n)cin>>x[i];
+	rep(i,1,n){
+		cin>>y[i];
+		cnt+= (!x[i]) + (!y[i]);
+	}
+	inv = pw(m,MOD-2);
+	dv = pw(inv,cnt);
+	mul = pw(m,cnt);
+	rep(i,1,n){
+		if(x[i] && y[i]){
+			if(x[i]<y[i])break;
+			if(x[i]>y[i]){
+				ans+= mul;
+				break;
+			}
+		}
+		else if(x[i]){
+			mul = mul*inv%MOD;
+			ans+= mul*(x[i]-1)%MOD;
+		}
+		else if(y[i]){
+			mul = mul*inv%MOD;
+			ans+= mul*(m-y[i])%MOD;
+		}
+		else {
+			mul = mul*inv%MOD;
+			mul = mul*inv%MOD;
+			ans+= mul*(m*(m-1)/2%MOD)%MOD;
+			mul = mul*m%MOD;
+		}
+	}
+	cout<<ans%MOD*dv%MOD<<endl;
+	return 0;
+}
