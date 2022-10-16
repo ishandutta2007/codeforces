d750874d86dd511987b4ac163b982c1d6192ab97
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 18:55:06 +0530

    amnesiac_dusk

---
+++ amnesiac_dusk/normal/348/B.java
@@ -0,0 +1,353 @@
+import java.io.*;
+import java.util.*;
+/*
+ * 	Heart beats fast
+ * 		Colors and promises
+ * 			How to be brave
+ * 				How can I love when I am afraid to fall...
+ */
+//read the question correctly (is y a vowel? what are the exact constraints?)
+//look out for SPECIAL CASES (n=1?) and overflow (ll vs int?)
+public class Main
+{
+	static long lcm(long a,long b)
+	{
+		return (a*b)/gcd(a,b);
+	}
+	static long gcd(long a,long b)
+	{
+		if(b==0)
+			return a;
+		return gcd(b,a%b);
+	}
+	static class node
+	{
+		long count;
+		vecti adj=new vecti();
+		void dfs(int fr, int at)
+		{
+			if(adj.size==1&&fr!=-1)
+			{
+				if(a[at]==0)
+				{
+					long sum=0;
+					for(int i=1; i<a.length; i++)
+					{
+						sum+=a[i];
+					}
+					sop(sum);
+					exit();
+				}
+				count=1;
+			}
+			else
+			{
+				long lcm=1;
+				for(int i=0; i<adj.size; i++)
+					if(adj.a[i]!=fr)
+					{
+						gra[adj.a[i]].dfs(at,adj.a[i]);
+						lcm=lcm(lcm,gra[adj.a[i]].count);
+					}
+				if(fr!=-1)
+					count=lcm*(adj.size-1);
+				else
+					count=lcm*(adj.size);
+				if(count>1e13)
+				{
+					long sum=0;
+					for(int i=1; i<a.length; i++)
+						sum+=a[i];
+					sop(sum);
+					exit();
+				}
+			}
+		}
+		void dfs1(int fr, int at)
+		{
+			for(int i=0; i<adj.size; i++)
+			{
+				if(adj.a[i]!=fr)
+				{
+					if(fr==-1)
+						gra[adj.a[i]].count=count/(adj.size);
+					else
+						gra[adj.a[i]].count=count/(adj.size-1);
+				if(gra[adj.a[i]].adj.size!=1)
+				{
+					gra[adj.a[i]].dfs1(at, adj.a[i]);
+				}
+				}
+			}
+		}
+	}
+	static node[]gra;
+	static int[]a;
+	public static void main(String[] args) throws IOException
+	{
+		int n=ni();
+		a=pnia(n);
+		gra=new node[n+1];
+		for(int i=1; i<=n; i++)
+			gra[i]=new node();
+		for(int i=0; i<n-1; i++)
+		{
+			int x=ni(),y=ni();
+			gra[x].adj.add(y);
+			gra[y].adj.add(x);
+		}
+		gra[1].dfs(-1,1);
+		gra[1].dfs1(-1, 1);
+		long min=inf;
+		long sum=0;
+		for(int i=2; i<=n; i++)
+		{
+			if(gra[i].adj.size==1)
+			{
+				sum+=a[i];
+				min=Math.min(min, (a[i]/gra[i].count));
+			}
+		}
+		pr(sum-min*gra[1].count);
+		System.out.print(output);
+	}
+	///////////////////////////////////////////
+	///////////////////////////////////////////
+	///template from here
+	static interface combiner
+	{
+		public int combine(int a, int b);
+	}
+	static final int mod=1000000007;
+	static final double eps=1e-8;
+	static final long inf=100000000000000000L;
+	static Reader in=new Reader();
+	static StringBuilder output=new StringBuilder();
+	static Random rn=new Random();
+	public static void sort(int[]a)
+	{
+		int te;
+		for(int i=0; i<a.length; i+=2)
+		{
+			te=rn.nextInt(a.length);
+			if(i!=te)
+			{
+				a[i]^=a[te];
+				a[te]^=a[i];
+				a[i]^=a[te];
+			}
+		}
+		Arrays.sort(a);
+	}
+	public static void sort(long[]a)
+	{
+		int te;
+		for(int i=0; i<a.length; i+=2)
+		{
+			te=rn.nextInt(a.length);
+			if(i!=te)
+			{
+			a[i]^=a[te];
+			a[te]^=a[i];
+			a[i]^=a[te];
+			}
+		}
+		Arrays.sort(a);
+	}
+	public static void sort(double[]a)
+	{
+		int te;
+		double te1;
+		for(int i=0; i<a.length; i+=2)
+		{
+			te=rn.nextInt(a.length);
+			if(i!=te)
+			{
+			te1=a[te];
+			a[te]=a[i];
+			a[i]=te1;
+			}
+		}
+		Arrays.sort(a);
+	}
+	public static void sort(int[][]a)
+	{
+		Arrays.sort(a, new Comparator<int[]>()
+		{
+			public int compare(int[]a,int[]b)
+			{
+				if(a[0]>b[0])
+					return 1;
+				if(b[0]>a[0])
+					return -1;
+				return 0;
+			}
+		});
+	}
+	static int lowerbound(int[]a, int key)
+	{
+		/*
+		 * returns index of smallest element larger than equal to given element between indices l and r inclusive
+		 */
+		int low=0,high=a.length-1,mid=(low+high)/2;
+		while(low<high)
+		{
+			if(a[mid] >= key)
+				high=mid;
+			else
+				low=mid+1;
+			mid=(low+high)/2;
+		}
+		return mid;
+	}
+	static int upperbound(int[]a, int key)
+	{
+		int low=0,high=a.length-1,mid=(low+high+1)/2;
+		while(low<high)
+		{
+			if(a[mid] <= key)
+				low=mid;
+			else
+				high=mid-1;
+			mid=(low+high+1)/2;
+		}
+		return mid;
+	}
+	static int lowerbound(int[]a, int l, int r, int key)
+	{
+		int low=l,high=r,mid=(low+high)/2;
+		while(low<high)
+		{
+			if(a[mid] >= key)
+				high=mid;
+			else
+				low=mid+1;
+			mid=(low+high)/2;
+		}
+		return mid;
+	}
+	static int upperbound(int[]a, int l, int r, int key)
+	{
+		/*
+		 * returns index of largest element smaller than equal to given element
+		 */
+		int low=l,high=r,mid=(low+high+1)/2;
+		while(low<high)
+		{
+			if(a[mid] <= key)
+				low=mid;
+			else
+				high=mid-1;
+			mid=(low+high+1)/2;
+		}
+		return mid;
+	}
+	static class pair
+	{
+		int a,b;
+		pair(){}
+		pair(int c,int d){a=c;b=d;}
+	}
+	void sort(pair[]a)
+	{
+		Arrays.sort(a,new Comparator<pair>()
+				{
+			@Override
+			public int compare(pair a,pair b)
+			{
+				if(a.a>b.a)
+					return 1;
+				if(b.a>a.a)
+				return -1;
+				return 0;
+			}
+				});
+	}
+	static int log2n(long a)
+	{
+		int te=0;
+		while(a>0)
+		{
+			a>>=1;
+			++te;
+		}
+		return te;
+	}
+	static class vecti
+	{
+		int a[],size;
+		vecti(){a=new int[10];size=0;}
+		vecti(int n){a=new int[n];size=0;}
+		public void add(int b){if(++size==a.length)a=Arrays.copyOf(a, 2*size);a[size-1]=b;}
+		public void sort(){Arrays.sort(a, 0, size);}
+		public void sort(int l, int r){Arrays.sort(a, l, r);}
+	}
+	static class vectl
+	{
+		long a[];
+		int size;
+		vectl(){a=new long[10];size=0;}
+		vectl(int n){a=new long[n];size=0;}
+		public void add(long b){if(++size==a.length)a=Arrays.copyOf(a, 2*size);a[size-1]=b;}
+		public void sort(){Arrays.sort(a, 0, size);}
+		public void sort(int l, int r){Arrays.sort(a, l, r);}
+	}
+	static class vectd
+	{
+		double a[];
+		int size;
+		vectd(){a=new double[10];size=0;}
+		vectd(int n){a=new double[n];size=0;}
+		public void add(double b){if(++size==a.length)a=Arrays.copyOf(a, 2*size);a[size-1]=b;}
+		public void sort(){Arrays.sort(a, 0, size);}
+		public void sort(int l, int r){Arrays.sort(a, l, r);}
+	}
+	//output functions////////////////
+	static void pr(Object a){output.append(a+"\n");}
+	static void pr(){output.append("\n");}
+	static void p(Object a){output.append(a);}
+	static void pra(int[]a){for(int i:a)output.append(i+" ");output.append("\n");}
+	static void pra(long[]a){for(long i:a)output.append(i+" ");output.append("\n");}
+	static void pra(String[]a){for(String i:a)output.append(i+" ");output.append("\n");}
+	static void pra(double[]a){for(double i:a)output.append(i+" ");output.append("\n");}
+	static void sop(Object a){System.out.println(a);}
+	static void flush(){System.out.println(output);output=new StringBuilder();}
+	//////////////////////////////////
+	//input functions/////////////////
+	static int ni(){return Integer.parseInt(in.next());}
+	static long nl(){return Long.parseLong(in.next());}
+	static String ns(){return in.next();}
+	static double nd(){return Double.parseDouble(in.next());}
+	static int[] nia(int n){int a[]=new int[n];for(int i=0; i<n; i++)a[i]=ni();return a;}
+	static int[] pnia(int n){int a[]=new int[n+1];for(int i=1; i<=n; i++)a[i]=ni();return a;}
+	static long[] nla(int n){long a[]=new long[n];for(int i=0; i<n; i++)a[i]=nl();return a;}
+	static String[] nsa(int n){String a[]=new String[n];for(int i=0; i<n; i++)a[i]=ns();return a;}
+	static double[] nda(int n){double a[]=new double[n];for(int i=0; i<n; i++)a[i]=nd();return a;}
+	//////////////////////////////////
+	//some utility functions
+	static void exit(){System.exit(0);}
+	static int min(int... a){int min=a[0];for(int i:a)min=Math.min(min, i);return min;}
+	static int max(int... a){int max=a[0];for(int i:a)max=Math.max(max, i);return max;}	
+	static int gcd(int... a){int gcd=a[0];for(int i:a)gcd=gcd(gcd, i);return gcd;}	
+	static String pr(String a, long b){String c="";while(b>0){if(b%2==1)c=c.concat(a);a=a.concat(a);b>>=1;}return c;}
+	static long powm(long a, long b, long m){long an=1;long c=a;while(b>0){if(b%2==1)an=(an*c)%m;c=(c*c)%m;b>>=1;}return an;}
+	static int gcd(int a, int b){if(b==0)return a;return gcd(b, a%b);}
+	static class Reader {
+        public BufferedReader reader;
+        public StringTokenizer tokenizer;
+        public Reader() {
+            reader = new BufferedReader(new InputStreamReader(System.in), 32768);
+            tokenizer = null;
+        }
+        public String next() {
+            while (tokenizer == null || !tokenizer.hasMoreTokens()) {
+                try {
+                    tokenizer = new StringTokenizer(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            return tokenizer.nextToken();
+        }
+    }
+}
