95789611d5e8f8303cc0e0f65f75c80f0fb86f15
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 15:42:14 +0530

    alan233

---
+++ alan233/normal/1578/B.cpp
@@ -0,0 +1,60 @@
+// Author: wlzhouzhuan
+#pragma GCC optimize(2, 3, "Ofast")
+#ifdef xay
+#define D(...) fprintf(stderr, "[D]" __VA_ARGS__)
+#else
+#define D(...) ((void)0)
+#endif
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int, int>
+#define pb push_back
+#define fir first
+#define sec second
+#define rep(i, l, r) for (int i = l; i <= r; i++)
+#define per(i, l, r) for (int i = l; i >= r; i--)
+#define mset(s, t) memset(s, t, sizeof(s))
+#define mcpy(s, t) memcpy(s, t, sizeof(t))
+#define poly vector<int>
+#define SZ(x) (int(x.size()))
+template<typename T1, typename T2> void ckmin(T1 &a, T2 b) { if (a > b) a = b; }
+template<typename T1, typename T2> void ckmax(T1 &a, T2 b) { if (a < b) a = b; }
+int read() {
+  int x = 0, f = 0; char ch = getchar();
+  while (!isdigit(ch)) f |= ch == '-', ch = getchar();
+  while (isdigit(ch)) x = 10 * x + ch - '0', ch = getchar();
+  return f ? -x : x;
+}
+template<typename T> void print(T x) {
+  if (x < 0) putchar('-'), x = -x;
+  if (x >= 10) print(x / 10);
+  putchar(x % 10 + '0');
+}
+template<typename T> void print(T x, char let) {
+  print(x), putchar(let);
+}
+
+const int N = 1000005;
+
+string a[N];
+int n, m;
+int id[N];
+
+int main() {
+  cin >> n >> m;
+  rep(i, 1, n) {
+    cin >> a[i];
+    for (int j = 2; j <= m; j += 2)
+      a[i][j - 1] = 'Z' - (a[i][j - 1] - 'A'); 
+    id[i] = i;
+  }
+  sort(id + 1, id + n + 1, [&](int x, int y) {
+    return a[x] < a[y];
+  });
+  rep(i, 1, n) {
+    print(id[i], " \n"[i == n]);
+  }
+  return 0;
+}
