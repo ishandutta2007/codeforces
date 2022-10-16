479683a2d810f90491e4df5e954cfb00cd83c8c6
ishan dutta <ishandutta2007@gmail.com>
2022-10-17 04:44:34 +0530

    gediminas

---
+++ gediminas/normal/914/C.cpp
@@ -0,0 +1,434 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace __gnu_pbds;
+using namespace std;
+
+template<typename T>
+struct seti : public tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> {
+	seti(): tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>() {}
+	explicit seti(initializer_list<T> o): tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>() {
+		for (const T &x : o)
+			this->insert(x);
+	}
+	inline T operator[](int i) {
+		return *this->find_by_order(i);
+	}
+};
+
+int VERY_RANDOM_NUMBER = (uint64_t)(std::make_unique<char>().get()) ^ (uint64_t)(std::chrono::high_resolution_clock::now().time_since_epoch().count());
+
+struct chash {
+	inline int operator()(int x) const {
+		return x ^ VERY_RANDOM_NUMBER;
+	}
+};
+
+template <class A, class B>
+using hash_table = gp_hash_table<A, B, chash>;
+
+#ifdef LOCAL
+template<typename T>
+ostream &operator<<(ostream &out, const vector<T> &arr) {
+	out << "[";
+	bool buv = false;
+
+	for (auto && x : arr) {
+		if (buv)
+			out << ", ";
+
+		out << x;
+		buv = true;
+	}
+
+	out << "]";
+	return out;
+}
+
+template<typename T, typename P>
+ostream &operator<<(ostream &out, const map<T, P> &arr) {
+	out << "{";
+	bool buv = false;
+
+	for (auto && x : arr) {
+		if (buv)
+			out << ", ";
+
+		out << x.first << " => " << x.second;
+		buv = true;
+	}
+
+	out << "}";
+	return out;
+}
+
+template<typename T, typename P>
+ostream &operator<<(ostream &out, const multimap<T, P> &arr) {
+	out << "{";
+	bool buv = false;
+
+	for (auto && x : arr) {
+		if (buv)
+			out << ", ";
+
+		out << x.first << " => " << x.second;
+		buv = true;
+	}
+
+	out << "}";
+	return out;
+}
+
+template<typename T, typename P>
+ostream &operator<<(ostream &out, const set<T, P> &arr) {
+	out << "{";
+	bool buv = false;
+
+	for (auto && x : arr) {
+		if (buv)
+			out << ", ";
+
+		out << x;
+		buv = true;
+	}
+
+	out << "}";
+	return out;
+}
+
+template<typename T, typename P>
+ostream &operator<<(ostream &out, const multiset<T, P> &arr) {
+	out << "{";
+	bool buv = false;
+
+	for (auto && x : arr) {
+		if (buv)
+			out << ", ";
+
+		out << x;
+		buv = true;
+	}
+
+	out << "}";
+	return out;
+}
+
+template<typename T>
+ostream &operator<<(ostream &out, const seti<T> &arr) {
+	out << "{";
+	bool buv = false;
+
+	for (auto && x : arr) {
+		if (buv)
+			out << ", ";
+
+		out << x;
+		buv = true;
+	}
+
+	out << "}";
+	return out;
+}
+
+#define debug(...) __debugg(#__VA_ARGS__, __VA_ARGS__)
+template <typename Arg1>
+void __debugg(const char *name, Arg1 &&arg1) {
+	cout << name << " : " << arg1 << std::endl;
+}
+
+template <typename Arg1, typename... Args>
+void __debugg(const char *names, Arg1 &&arg1, Args &&... args) {
+	const char *comma = names;
+	int gyl = 0;
+
+	while (*comma != ',' or gyl > 0) {
+		if (*comma == '(' or * comma == '{' or * comma == '[')
+			gyl++;
+
+		if (*comma == ')' or * comma == '}' or * comma == ']')
+			gyl--;
+
+		comma++;
+	}
+
+	cout.write(names, comma - names) << " : " << arg1 << " | ";
+
+	if (*(comma + 1) == ' ')
+		comma++;
+
+	__debugg(comma + 1, args...);
+}
+#else
+#define debug(...)
+#endif
+
+template<typename X, typename Y>
+struct Pair {
+	X x;
+	Y y;
+	template<typename X1, typename Y1>
+	Pair(const X1 &x, const Y1 &y): x(x), y(y) {}
+	Pair() {}
+	template<typename X1>
+	explicit Pair(const X1 &x): x(x), y(Y()) {}
+	template<typename X1, typename Y1>
+	Pair<X, Y> operator+(const Pair<X1, Y1> &o)const {
+		return Pair<X1, Y1>(x + o.x, y + o.y);
+	}
+	template<typename X1, typename Y1>
+	Pair<X, Y> operator-(const Pair<X1, Y1> &o)const {
+		return Pair<X1, Y1>(x - o.x, y - o.y);
+	}
+	template<typename X1, typename Y1>
+	Pair<X, Y> operator*(const Pair<X1, Y1> &o)const {
+		return Pair<X1, Y1>(x * o.x, y * o.y);
+	}
+	template<typename X1, typename Y1>
+	Pair<X, Y> operator/(const Pair<X1, Y1> &o)const {
+		return Pair<X1, Y1>(x / o.x, y / o.y);
+	}
+	template<typename X1, typename Y1>
+	void operator+=(const Pair<X1, Y1> &o) {
+		x += o.x;
+		y += o.y;
+	}
+	template<typename X1, typename Y1>
+	void operator-=(const Pair<X1, Y1> &o) {
+		x -= o.x;
+		y -= o.y;
+	}
+	template<typename X1, typename Y1>
+	void operator*=(const Pair<X1, Y1> &o) {
+		x *= o.x;
+		y *= o.y;
+	}
+	template<typename X1, typename Y1>
+	void operator/=(const Pair<X1, Y1> &o) {
+		x /= o.x;
+		y /= o.y;
+	}
+	template<typename X1, typename Y1>
+	bool operator<(const Pair<X1, Y1> &o)const {
+		if (x == o.x)
+			return y < o.y;
+
+		return x < o.x;
+	}
+	template<typename X1, typename Y1>
+	bool operator<=(const Pair<X1, Y1> &o)const {
+		if (x == o.x)
+			return y <= o.y;
+
+		return x <= o.x;
+	}
+	template<typename X1, typename Y1>
+	bool operator>(const Pair<X1, Y1> &o)const {
+		if (x == o.x)
+			return y > o.y;
+
+		return x > o.x;
+	}
+	template<typename X1, typename Y1>
+	bool operator>=(const Pair<X1, Y1> &o)const {
+		if (x == o.x)
+			return y >= o.y;
+
+		return x >= o.x;
+	}
+	template<typename X1, typename Y1>
+	bool operator==(const Pair<X1, Y1> &o)const {
+		return x == o.x and y == o.y;
+	}
+	template<typename X1, typename Y1>
+	bool operator!=(const Pair<X1, Y1> &o)const {
+		return x != o.x or y != o.y;
+	}
+	template<typename X1, typename Y1>
+	void swap(Pair<X1, Y1> &o) {
+		swap(x, o.x);
+		swap(y, o.y);
+	}
+};
+
+template<typename X, typename Y>
+Pair<X, Y> makep(const X &x, const Y &y) {
+	return Pair<X, Y>(x, y);
+}
+
+template<typename X, typename Y>
+ostream &operator<<(ostream &out, const Pair<X, Y> &p) {
+	out << "(" << p.x << ", " << p.y << ")";
+	return out;
+}
+
+template<typename X, typename Y>
+istream &operator>>(istream &in, Pair<X, Y> &p) {
+	in >> p.x >> p.y;
+	return in;
+}
+
+#define pair Pair
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double ld;
+typedef vector<int> vi;
+typedef vector<ll> vll;
+typedef Pair<int, int> pii;
+typedef Pair<ll, ll> pll;
+typedef vector<pll> vpll;
+
+template<typename T>
+istream &operator>>(istream &in, vector<T> &arr) {
+	for (auto && x : arr) {
+		in >> x;
+	}
+
+	return in;
+}
+
+template<typename X, typename Y>
+inline X mini(X &a, const Y &b) __attribute__((always_inline));
+template<typename X, typename Y>
+inline X mini(X &a, const Y &b) {
+	if (a > b)
+		a = b;
+
+	return a;
+}
+template<typename X, typename Y>
+inline X maxi(X &a, const Y &b) __attribute__((always_inline));
+template<typename X, typename Y>
+inline X maxi(X &a, const Y &b) {
+	if (a < b)
+		a = b;
+
+	return a;
+}
+
+const long long mod = 1000000007;
+long long power(long long x, long long n) {
+	int i = 0;
+	long long d = x;
+	long long ats = 1;
+
+	while (n > 0) {
+		if (n & (1ll << i)) {
+			n ^= (1ll << i);
+			ats *= d;
+			ats %= mod;
+		}
+
+		i++;
+		d = (d * d) % mod;
+	}
+
+	return ats;
+}
+
+/*link https://codeforces.com/problemset/problem/914/C */
+/*input
+110
+2
+
+*/
+/*output
+3
+*/
+/*input
+111111011
+2
+*/
+/*output
+169
+*/
+/*input
+1
+0
+*/
+/*output
+1
+*/
+/*input
+1
+1
+*/
+/*output
+0
+*/
+/*input
+10000000000000000000000000000
+1
+*/
+/*output
+28
+*/
+long long modInverse(long long a) {
+	return power(a, mod - 2);
+}
+
+ll fakt[1001] = {};
+ll faktinv[1001] = {};
+
+int main () {
+	ios_base::sync_with_stdio(false), cin.tie(0), cout.tie(0);
+	vi kiek(1001, 0);
+	kiek[1] = 0;
+	fakt[0] = fakt[1] = 1;
+	faktinv[0] = faktinv[1] = 1;
+
+	for (int i = 2; i < 1001; i++) {
+		kiek[i] = kiek[bitset<32>(i).count()] + 1;
+		fakt[i] = (fakt[i - 1] * i) % mod;
+		faktinv[i] = modInverse(fakt[i]);
+	}
+
+	string a;
+	cin >> a;
+	int k;
+	cin >> k;
+
+	if (k == 0) {
+		cout << 1;
+		return 0;
+	}
+
+	ll ats = 0;
+	int vie = 0;
+
+	for (int i = 0; i < (int)a.size(); i++) {
+		if (a[i] == '1') {
+			ll dyd = (ll)a.size() - i - 1;
+
+			for (int j = 1; j < 1001; j++) {
+				if (kiek[j] == k - 1 and dyd - (j - vie) >= 0 and j - vie >= 0) {
+					if (j - vie == 0 and j == 1)
+						continue;
+
+					ll t = ((faktinv[dyd - (j - vie)] * faktinv[j - vie]) % mod * fakt[dyd]) % mod;
+					debug(dyd, j, j - vie, t);
+					ats += t;
+					ats %= mod;
+				}
+			}
+
+			vie++;
+		}
+	}
+
+	ll dyd = 0;
+
+	for (int j = 1; j < 1001; j++) {
+		if (kiek[j] == k - 1 and dyd - (j - vie) >= 0 and j - vie >= 0) {
+			if (j - vie == 0 and j == 1)
+				continue;
+
+			ll t = ((faktinv[dyd - (j - vie)] * faktinv[j - vie]) % mod * fakt[dyd]) % mod;
+			debug(dyd, j, j - vie, t);
+			ats += t;
+			ats %= mod;
+		}
+	}
+
+	cout << ats;
+}
