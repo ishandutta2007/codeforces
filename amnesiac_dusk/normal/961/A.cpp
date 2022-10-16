a9e96a24dfcd904b02c226379c885dbc4850e4ac
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 19:05:44 +0530

    amnesiac_dusk

---
+++ amnesiac_dusk/normal/961/A.cpp
@@ -0,0 +1,77 @@
+/*
+ * 	i could never find the right way to tell you
+ * 		have you noticed i've been gone?
+ * 			cause i left behind the home that you made me
+ * 				but i will carry it along
+ * 	mm it's a long way forward
+ * 		so trust in me
+ * 			i'll give them shelter like you've done for me
+ * 				and i know i'm not alone
+ * 					you'll be watching over us
+ * 						until you're gone
+ * 	when i'm older i'll be silent beside you
+ * 		I know that words are not enough
+ * 			and they won't need to know our names or our faces
+ * 				but they will carry on for us
+ */
+
+#pragma GCC optimize("Ofast,unroll-loops")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+#include <bits/stdc++.h>
+using namespace std;
+#ifndef rd
+#define endl '\n'
+#endif
+#define deb cout<<"Line: "<<__LINE__<<" plz "
+#define pb push_back
+#define fi first
+#define se second
+//#define int long long
+typedef long long ll;
+#define double long double
+//typedef long double f80;
+#define pii pair<int,int>
+#define pll pair<ll,ll>
+#define sz(x) ((int)x.size())
+#define fr(a,b,c) for(int a=b; a<=c; a++)
+#define all(con) con.begin(),con.end()
+const ll infl=2e18;
+const int infi=2e9;
+const int mod=1e9+7;
+#define dpr(x) cout<<#x<<": "<<x<<endl
+typedef vector<int> vi;
+mt19937 rang(chrono::high_resolution_clock::now().time_since_epoch().count());
+auto clk=clock();
+int rng(int lim)
+{
+	uniform_int_distribution<int> uid(0,lim-1);
+	return uid(rang);
+}
+
+int a[1005];
+void solve() {
+	int n,m,te;
+	cin>>n>>m;
+	for(int i=1; i<=m; i++) {
+		cin>>te;
+		a[te]++;
+	}
+	int ans=a[1];
+	fr(i,1,n)
+		ans=min(ans,a[i]);
+	cout<<ans<<endl;
+}
+signed main() {
+	ios_base::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+	srand(chrono::high_resolution_clock::now().time_since_epoch().count());
+	cout<<fixed<<setprecision(10);
+	int t=1;
+//	cin>>t;
+	while(t--)
+		solve();
+#ifdef rd
+	cout<<endl<<endl<<(double)(clock()-clk)/CLOCKS_PER_SEC<<endl;
+#endif
+	return 0;
+}
+/**/
