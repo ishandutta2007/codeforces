e143112424c75cff90983da32cfeaab7c78ba171
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 15:31:27 +0530

    alan233

---
+++ alan233/normal/1660/E.cpp
@@ -0,0 +1,61 @@
+// Author: wlzhouzhuan
+#include<bits/stdc++.h>
+using namespace std;
+
+#define rep(i,l,r) for(int i=(l);i<=(r);i++)
+#define per(i,l,r) for(int i=(l);i>=(r);i--)
+#define ll long long
+#define ull unsigned long long
+#define pii pair<int,int>
+#define mset(s,t) memset(s,t,sizeof(s))
+#define mcpy(s,t) memcpy(s,t,sizeof(t))
+#define SZ(x) ((int)x.size())
+#define pb push_back
+#define eb emplace_back
+#define fir first
+#define sec second
+
+template<class T1,class T2>bool ckmax(T1 &a,T2 b){if(a<b)return a=b,1;else return 0;}
+template<class T1,class T2>bool ckmin(T1 &a,T2 b){if(a>b)return a=b,1;else return 0;}
+
+inline int read(){
+	int x=0,f=0;char ch=getchar();
+	while(!isdigit(ch))f|=ch=='-',ch=getchar();
+	while(isdigit(ch))x=10*x+ch-'0',ch=getchar();
+	return f?-x:x;
+}
+template<typename T>void print(T x){
+	if(x<0)putchar('-'),x=-x;
+	if(x>=10)print(x/10);
+	putchar(x%10+'0');
+}
+template<typename T>void print(T x,char ch){
+	print(x),putchar(ch);
+}
+
+const int N=2005;
+
+char a[N][N];
+int z[N],n;
+
+void solve(){
+    n=read();
+    rep(i,0,n-1)z[i]=0;
+    int cnt=0;
+    rep(i,1,n){
+        scanf("%s",a[i]+1);
+        rep(j,1,n){
+            if(a[i][j]=='1'){
+                cnt++;
+                z[(i-j+n)%n]++;
+            }
+        }
+    }
+    print(cnt+n-2**max_element(z,z+n),'\n');
+}
+
+int main(){
+    int T=read();
+    while(T--)solve();
+    return 0;
+}
