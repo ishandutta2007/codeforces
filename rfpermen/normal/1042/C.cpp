b5e95d51688433811823c15a736fdbf50131f929
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 11:23:03 +0530

    rfpermen

---
+++ rfpermen/normal/1042/C.cpp
@@ -0,0 +1,104 @@
+#include<bits/stdc++.h>
+ 
+#pragma GCC optimize("O3")
+#pragma GCC target("tune=native")
+#pragma GCC optimize("unroll-loops")
+ 
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(int i=(n);i<=(N);++i)
+#define For(i,n,N) for(int i=(n);i< (N);++i)
+#define rap(i,n,N) for(int i=(n);i>=(N);--i)
+#define rip(i,n,N,V) for(int i=(n);i<=(N);i+=V)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+//#define le left
+//#define ri right
+#define fi first
+#define se second
+#define ff fi.fi
+#define fs fi.se
+#define sf se.fi
+#define ss se.se
+//#define lc (id<<1)
+//#define rc ((id<<1)|1)
+#define db(x) cout << ">>>> " << #x << " -> " << (x) << endl
+#define out(x,y) cout << ">> " << (x) << " " << (y) << endl
+#define ou3(x,y,z) cout << ">> " << (x) << " " << (y) << " " << (z) << endl
+#define mems(x,y) memset(&x,y,sizeof x)
+#define memsv(x,y) memset(&x[0], y, sizeof(x[0]) * x.size())
+#define popcount __builtin_popcountll
+#define clz(x) (1<<(31-__builtin_clz(x)))
+#define clzll(x) (1LL<<(63-__builtin_clzll(x)))
+#define all(x) x.begin(),x.end()
+#define rsort(x) sort(x), reverse(x)
+#define PQ(T) priority_queue<T>
+#define PQr(T) priority_queue<T, vector<T>, greater<T>>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vll vector<pll>
+#define ari(x) array<int,x>
+#define arll(x) array<ll,x>
+#define ard(x) array<double,x>
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define pllll pair<pll,pll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl '\n'
+const int MAX = 1e6+5;
+const ll MOD = 998244353;
+const ll MOD2 = 2010405347;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+const int block = 1000;
+inline ll pw(ll x,ll y,ll md=MOD){ll ret=1;x%=md;while(y){if(y&1)ret=ret*x%md;x=x*x%md,y>>=1;}return ret;}
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int n,x,pos;
+vii ng;
+vi z,p;
+
+int main(){
+//	cout<<fixed<<setprecision(10);
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt","w",stdout);
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	cin>>n;
+	rep(i,1,n){
+		cin>>x;
+		if(x==0){
+			if(pos)z.pb(i);
+			else pos = i;
+		}
+		else if(x>0)p.pb(i);
+		else ng.pb({x,i});
+	}
+	sort(all(ng));
+	reverse(all(ng));
+	while(ng.size()>1){
+		x = ng.back().se;
+		ng.pob();
+		cout<<"1 "<<ng.back().se<<" "<<x<<endl;
+		ng.pob();
+		p.pb(x);
+	}
+	For(i,1,p.size())cout<<"1 "<<p[i]<<" "<<p[0]<<endl;
+	if(pos){
+		if(ng.size())cout<<"1 "<<ng.back().se<<" "<<pos<<endl;
+		for(auto i:z)cout<<"1 "<<i<<" "<<pos<<endl;
+		if(p.size())cout<<"2 "<<pos<<endl;
+	}
+	else if(ng.size())cout<<"2 "<<ng.back().se<<endl;
+	return 0;
+}
---
+++ rfpermen/normal/1101/D.cpp
@@ -0,0 +1,124 @@
+#include<bits/stdc++.h>
+ 
+#pragma GCC optimize("O3")
+#pragma GCC target("tune=native")
+#pragma GCC optimize("unroll-loops")
+ 
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(int i=(n);i<=(N);++i)
+#define For(i,n,N) for(int i=(n);i< (N);++i)
+#define rap(i,n,N) for(int i=(n);i>=(N);--i)
+#define rip(i,n,N,V) for(int i=(n);i<=(N);i+=V)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+//#define le left
+//#define ri right
+#define fi first
+#define se second
+#define ff fi.fi
+#define fs fi.se
+#define sf se.fi
+#define ss se.se
+//#define lc (id<<1)
+//#define rc ((id<<1)|1)
+#define db(x) cout << ">>>> " << #x << " -> " << (x) << endl
+#define out(x,y) cout << ">> " << (x) << " " << (y) << endl
+#define ou3(x,y,z) cout << ">> " << (x) << " " << (y) << " " << (z) << endl
+#define mems(x,y) memset(&x,y,sizeof x)
+#define memsv(x,y) memset(&x[0], y, sizeof(x[0]) * x.size())
+#define popcount __builtin_popcountll
+#define clz(x) (1<<(31-__builtin_clz(x)))
+#define clzll(x) (1LL<<(63-__builtin_clzll(x)))
+#define all(x) x.begin(),x.end()
+#define rsort(x) sort(x), reverse(x)
+#define PQ(T) priority_queue<T>
+#define PQr(T) priority_queue<T, vector<T>, greater<T>>
+#define vi vector<int>
+#define vii vector<pii>
+#define vl vector<ll>
+#define vll vector<pll>
+#define ari(x) array<int,x>
+#define arll(x) array<ll,x>
+#define ard(x) array<double,x>
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define pllll pair<pll,pll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl '\n'
+const int MAX = 2e5+5;
+const ll MOD = 998244353;
+const ll MOD2 = 2010405347;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+const int block = 1000;
+inline ll pw(ll x,ll y,ll md=MOD){ll ret=1;x%=md;while(y){if(y&1)ret=ret*x%md;x=x*x%md,y>>=1;}return ret;}
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+int n,x,a,b,vis[MAX],ans,mx,pos;
+vector<int> v[MAX],y[MAX],z[MAX],o;
+bool st[MAX];
+
+void dfs(int nw,int par){
+	if(vis[nw]>mx)mx = vis[nw], pos = nw;
+	for(int i:v[nw])if(!st[i] && i!=par){
+		vis[i] = vis[nw]+1;
+		dfs(i,nw);
+	}
+	return;
+}
+
+
+int main(){
+//	cout<<fixed<<setprecision(10);
+//	freopen("input.txt", "r", stdin);
+//	freopen("output.txt","w",stdout);
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+	For(i,2,MAX)if(z[i].empty()){
+		rip(j,i,MAX-1,i)z[j].pb(i);
+	}
+	cin>>n;
+	rep(i,1,n){
+		cin>>x;
+		for(auto j:z[x])y[j].pb(i);
+		st[i] = 1;
+	}
+	rep(i,2,n){
+		cin>>a>>b;
+		v[a].pb(b);
+		v[b].pb(a);
+	}
+	For(i,2,MAX)if(y[i].size()){
+		for(auto j:y[i]){
+			st[j] = vis[j] = 0;
+		}
+		o.clear();
+		for(auto j:y[i])if(!vis[j]){
+			mx = -1;
+			vis[j] = 1;
+			dfs(j,0);
+			o.pb(pos);
+		}
+		for(auto j:o){
+			mx = -1;
+			vis[j] = 1;
+			dfs(j,0);
+			ans = max(ans, mx);
+		}
+		for(auto j:y[i]){
+			st[j] = 1;
+		}
+	}
+	cout<<ans<<endl;
+	return 0;
+}
