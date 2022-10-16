0f0d7ca7d484a6b496da7f8087624250dd23552c
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 10:19:48 +0530

    175iq

---
+++ 175iq/normal/1336/A.cpp
@@ -0,0 +1,132 @@
+#include "bits/stdc++.h"
+using namespace std;
+ 
+//Utility functions
+#define pb push_back
+#define sz size()
+#define mp make_pair
+#define fi first
+#define se second
+#define foreach(i,t) for (typeof(t.begin()) i=t.begbin(); i!=t.end(); i++) //traverse an STL data structure
+#define rep(i, begin, end) for (__typeof(end) i = (begin) - ((begin) > (end)); i != (end) - ((begin) > (end)); i += 1 - 2 * ((begin) > (end)))
+#define all(c) (c).begin(),(c).end() //handy for function like "sort()" 
+#define by(x) [](const auto& a, const auto& b) { return a.x < b.x; } //sort array of objects(structs)[Lambda function]
+//sort(begin(v), end(v), [] (int a, int b) { return a > b; });
+ 
+//Constants
+#define EPS 1e-8
+#define INF 0x3f3f3f3f
+#define MOD 1000000007//998244353 
+ 
+//Printing
+#define coutRV(a,L,R) FE(i,L,R) cout<<a[i]<<" \n"[i==R] ;
+#define coutV(a) coutRV(a,0,a.size()-1)
+ 
+//For loops
+#define F(i,a,b)    for(int i=a; i<b; i++)
+#define FE(i,a,b)   for(int i=a; i<=b; i++)
+#define FRE(i,b,a)  for(int i=b; i>=a; i--)
+#define FA(n,container)   for(auto n : container)
+
+//For debug 
+void __print(int x) {cerr << x;}
+void __print(long x) {cerr << x;}
+void __print(long long x) {cerr << x;}
+void __print(unsigned x) {cerr << x;}
+void __print(unsigned long x) {cerr << x;}
+void __print(unsigned long long x) {cerr << x;}
+void __print(float x) {cerr << x;}
+void __print(double x) {cerr << x;}
+void __print(long double x) {cerr << x;}
+void __print(char x) {cerr << '\'' << x << '\'';}
+void __print(const char *x) {cerr << '\"' << x << '\"';}
+void __print(const string &x) {cerr << '\"' << x << '\"';}
+void __print(bool x) {cerr << (x ? "true" : "false");}
+
+template<typename T, typename V>
+void __print(const pair<T, V> &x) {cerr << '{'; __print(x.first); cerr << ','; __print(x.second); cerr << '}';}
+template<typename T>
+void __print(const T &x) {int f = 0; cerr << '{'; for (auto &i: x) cerr << (f++ ? "," : ""), __print(i); cerr << "}";}
+void _print() {cerr << "]\n";}
+template <typename T, typename... V>
+void _print(T t, V... v) {__print(t); if (sizeof...(v)) cerr << ", "; _print(v...);}
+#ifndef ONLINE_JUDGE
+#define debug(x...) cerr << "[" << #x << "] = ["; _print(x)
+#else
+#define debug(x...)
+#endif
+
+//Definitions
+#define vi vector<int>
+#define vll vector<ll>
+#define vd vector<double> 
+#define vvi vector<vector<int> > //For UnGraph
+#define vvpi vector<vector<pii> > //For DirGraph
+#define vviwv(vecname, N, M, value) vector<vector<int> > vecname(N, vector<int> (M, value)) //For DP
+#define vvlwv(vecname, N, M, value) vector<vector<ll> > vecname(N, vector<ll> (M, value)) //For DP  
+#define pii pair<int,int>   
+#define ll long long 
+#define fastIO ios_base::sync_with_stdio(false);cin.tie(0);cout.tie(0)
+
+//Directions
+const int fx[4][2]  = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+const int fxx[8][2] = {{0,1}, {0,-1}, {1,0}, {-1,0}, {1,1}, {1,-1}, {-1,1}, {-1,-1}};
+
+//Input 
+#define cini(i) int i; cin>>i;
+#define cini2(i,j) int i,j; cin>>i>>j;
+#define cini3(i,j,k) int i,j,k; cin>>i>>j>>k;
+#define cini4(i,j,k,l) int i,j,k,l; cin>>i>>j>>k>>l;
+#define cinll(l) ll l; cin>>l;
+#define cind(d) double d; cin>>d;
+#define cins(s) string s; cin>>s;
+#define cinv(a, n) vi a(n); F(i,0,n) { cin>>a[i]; }
+#define cinvll(a, n) vll a(n); F(i,0,n) { cin>>a[i]; }
+#define cinvs(s, n) vs s(n); F(i,0,n) { cin>>s[i]; }
+#define cinvd(a, n) vd a(n); F(i,0,n) { cin>>a[i]; }
+
+int N = 2e5+10;
+vvi gr(N);
+vi subsz(N);
+vi level(N);
+vi aux(N,0);
+ll ans=0;
+
+int dfs(int u, int p, int pl)   
+{
+    level[u] = pl+1;
+    subsz[u] = 1;
+    FA(v, gr[u])
+    {
+        if(v==p)
+            continue;
+        subsz[u]+=dfs(v,u,pl+1);
+    }
+    return subsz[u];
+}
+
+int main()
+{
+    fastIO; 
+    cini2(n,k);
+    F(i,0,n-1)
+    {
+        cini2(u,v);
+        gr[u].pb(v);
+        gr[v].pb(u);
+    }
+
+    dfs(1,-1,0);
+    FE(i,1,n)
+        aux[i] = level[i]-subsz[i];
+
+    sort(aux.begin()+1, aux.begin()+1+n, [](int a, int b){return a>b;});
+
+    ll ans = 0;
+    FE(i,1,k){
+        ans+=(ll)aux[i];
+    }
+
+    cout<<ans<<endl;
+    return 0;
+}
