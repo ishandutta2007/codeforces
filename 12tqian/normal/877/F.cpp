227f386ee4114143cd6e42bcbc4aae89a9430c8b
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 18:29:00 +0530

    12tqian

---
+++ 12tqian/normal/877/F.cpp
@@ -0,0 +1,303 @@
+#pragma comment(linker, "/stack:200000000")
+//#pragma GCC optimize("Ofast")
+//#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,tune=native")
+//#pragma GCC target ("avx2")
+//#pragma GCC optimization ("unroll-loops")
+#pragma GCC optimize ("O3")
+#pragma GCC target ("sse4")
+
+#include <bits/stdc++.h>
+#include <ext/pb_ds/tree_policy.hpp>
+#include <ext/pb_ds/assoc_container.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+template <class T> using Tree = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
+
+const long double PI = 4 * atan((long double) 1);
+
+typedef long long ll;
+typedef long double ld;
+
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<ld, ld> pd;
+typedef complex<ld> cd;
+
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<ld> vd;
+typedef vector<pi> vpi;
+typedef vector<pl> vpl;
+typedef vector<cd> vcd;
+
+#define f0r(i,a) for(int i=0;i<a;i++)
+#define f1r(i,a,b) for(int i=a;i<b;i++)
+#define trav(a, x) for (auto& a : x)
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+
+#define mp make_pair
+#define pb push_back
+#define eb emplace_back
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define sz(x) (int)(x).size()
+#define all(x) x.begin(), x.end()
+#define rall(x) rbegin(x), rend(x)
+#define resz resize
+#define ins insert
+
+template<typename A, typename B> ostream& operator<<(ostream &cout, pair<A, B> const &p) { return cout << "(" << p.f << ", " << p.s << ")"; }
+template<typename A> ostream& operator<<(ostream &cout, vector<A> const &v) {
+  cout << "["; for(int i = 0; i < v.size(); i++) {if (i) cout << ", "; cout << v[i];} return cout << "]";
+}
+
+template<class T> bool ckmin(T& a, const T& b) {
+	return a > b ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) {
+	return a < b ? a = b, 1 : 0; }
+
+namespace input {
+	template<class T> void re(complex<T>& x);
+	template<class T1, class T2> void re(pair<T1,T2>& p);
+	template<class T> void re(vector<T>& a);
+	template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+	template<class T> void re(T& x) { cin >> x; }
+	void re(double& x) { string t; re(t); x = stod(t); }
+	void re(ld& x) { string t; re(t); x = stold(t); }
+	template<class T, class... Ts> void re(T& t, Ts&... ts) {
+		re(t); re(ts...);
+	}
+
+	template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+	template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+	template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+	template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+
+using namespace input;
+
+namespace output {
+	void pr(int x) { cout << x; }
+	void pr(long x) { cout << x; }
+	void pr(ll x) { cout << x; }
+	void pr(unsigned x) { cout << x; }
+	void pr(unsigned long x) { cout << x; }
+	void pr(unsigned long long x) { cout << x; }
+	void pr(float x) { cout << x; }
+	void pr(double x) { cout << x; }
+	void pr(ld x) { cout << x; }
+	void pr(char x) { cout << x; }
+	void pr(const char* x) { cout << x; }
+	void pr(const string& x) { cout << x; }
+	void pr(bool x) { pr(x ? "true" : "false"); }
+	template<class T> void pr(const complex<T>& x) { cout << x; }
+
+	template<class T1, class T2> void pr(const pair<T1,T2>& x);
+	template<class T> void pr(const T& x);
+
+	template<class T, class... Ts> void pr(const T& t, const Ts&... ts) {
+		pr(t); pr(ts...);
+	}
+	template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+		pr("{",x.f,", ",x.s,"}");
+	}
+	template<class T> void pr(const T& x) {
+		pr("{"); // const iterator needed for vector<bool>
+		bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0;
+		pr("}");
+	}
+
+	void ps() { pr("\n"); } // print w/ spaces
+	template<class T, class... Ts> void ps(const T& t, const Ts&... ts) {
+		pr(t); if (sizeof...(ts)) pr(" "); ps(ts...);
+	}
+
+	void pc() { pr("]\n"); } // debug w/ commas
+	template<class T, class... Ts> void pc(const T& t, const Ts&... ts) {
+		pr(t); if (sizeof...(ts)) pr(", "); pc(ts...);
+	}
+	#define dbg(x...) pr("[",#x,"] = ["), pc(x);
+}
+
+using namespace output;
+
+namespace io {
+	void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+	void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+	void setIO(string s = "") {
+		cin.sync_with_stdio(0); cin.tie(0); // fast I/O
+		//cin.exceptions(cin.failbit); // ex. throws exception when you try to read letter into int
+		if (sz(s)) { setIn(s+".in"), setOut(s+".out"); } // for USACO
+	}
+}
+
+using namespace io;
+mt19937 rng((uint32_t)chrono::steady_clock::now().time_since_epoch().count());
+
+const ll MOD = 1e9+7;
+
+typedef decay<decltype(MOD)>::type T;
+struct mi {
+	T val;
+	explicit operator T() const { return val; }
+	mi() { val = 0; }
+	mi(const ll& v) {
+		val = (-MOD <= v && v <= MOD) ? v : v % MOD;
+		if (val < 0) val += MOD;
+	}
+	// friend ostream& operator<<(ostream& os, const mi& a) {
+		// return os << a.val; }
+	friend void pr(const mi& a) { pr(a.val); }
+	friend void re(mi& a) { ll x; re(x); a = mi(x); }
+
+	friend bool operator==(const mi& a, const mi& b) {
+		return a.val == b.val; }
+	friend bool operator!=(const mi& a, const mi& b) {
+		return !(a == b); }
+	friend bool operator<(const mi& a, const mi& b) {
+		return a.val < b.val; }
+
+	mi operator-() const { return mi(-val); }
+	mi& operator+=(const mi& m) {
+		if ((val += m.val) >= MOD) val -= MOD;
+		return *this; }
+	mi& operator-=(const mi& m) {
+		if ((val -= m.val) < 0) val += MOD;
+		return *this; }
+	mi& operator*=(const mi& m) {
+		val = (ll)val*m.val%MOD; return *this; }
+	friend mi pow(mi a, ll p) {
+		mi ans = 1; assert(p >= 0);
+		for (; p; p /= 2, a *= a) if (p&1) ans *= a;
+		return ans;
+	}
+	friend mi inv(const mi& a) {
+		assert(a != 0); return pow(a,MOD-2); }
+	mi& operator/=(const mi& m) { return (*this) *= inv(m); }
+
+	friend mi operator+(mi a, const mi& b) { return a += b; }
+	friend mi operator-(mi a, const mi& b) { return a -= b; }
+	friend mi operator*(mi a, const mi& b) { return a *= b; }
+	friend mi operator/(mi a, const mi& b) { return a /= b; }
+};
+
+typedef pair<mi,mi> pmi;
+typedef vector<mi> vmi;
+typedef vector<pmi> vpmi;
+const int MX = 1e5 + 5;
+int N;;
+ll ans[MX];
+int oc[6*MX], BLOCK;
+vector<array<int,3>> todo; // store left, right, index of ans
+vi t;
+vl a;
+ll pre[MX];
+ll num[MX][3];
+map<ll, int> conv;
+
+bool cmp(array<int,3> a, array<int,3> b) { // sort queries
+	if (a[0]/BLOCK != b[0]/BLOCK) return a[0] < b[0];
+	return a[1] < b[1];
+}
+
+int l = 0, r = -1;
+ll cans = 0;
+void modify(int x, int side, int y = 1) {
+    if(side == 0){
+
+        if(y == 1){
+            cans +=  y*oc[num[x][2]];
+            oc[num[x][0]] += y;
+        }
+        else{
+            oc[num[x][0]] += y;
+            cans +=  y*oc[num[x][2]];
+        }
+
+    }
+    else{
+        if(y == 1){
+            cans += y*oc[num[x][1]];
+            oc[num[x][0]] += y;
+          //  ps("I ADDED", num[x][0], "I QUERIED", num[x][1], "I GOT", oc[num[x][1]]);
+        }
+        else{
+            oc[num[x][0]] += y;
+            cans += y*oc[num[x][1]];
+        }
+
+
+    }
+}
+
+int answer(int L, int R) { // modifyjust interval
+	while (l > L) modify(--l, 0);
+	while (r < R) modify(++r, 1);
+	while (l < L) modify(l++,0,-1);
+	while (r > R) modify(r--,1,-1);
+	return cans;
+}
+
+void solve() {
+	BLOCK = sqrt(N); sort(all(todo),cmp);
+	trav(x,todo) {
+		answer(x[0],x[1]);
+		//ps(x[0], x[1], "NOW I GO");
+		ans[x[2]] = cans;
+	}
+}
+
+int main(){
+    int n;
+    ll k;
+    re(n, k);
+    N = n+1;
+    f0r(i, n){
+        int ti; re(ti);
+        t.eb(2*(ti-1)-1);
+    }
+    f0r(i, n){
+        int ai; re(ai);
+        a.eb(-t[i]*ai);
+    }
+    f1r(i, 1, n+1){
+        if(i == 1) pre[i] = a[i-1];
+        else pre[i] = pre[i-1] + a[i-1];
+    }
+    set<ll> s;
+    f0r(i, n+1){
+        s.insert(pre[i]);
+        s.insert(pre[i] - k);
+        s.insert(pre[i] + k);
+    }
+    //f0r(i, n+1) ps(pre[i], "HUH");
+    int cnt = 0;
+    for(auto x: s){
+        conv[x] = cnt;
+        cnt++;
+    }
+    f0r(i, n+1){
+        num[i][0] = conv[pre[i]];
+        num[i][1] = conv[pre[i] -k];
+        num[i][2] = conv[pre[i] + k];
+    }
+    int q;
+    re(q);
+    f0r(i, q){
+        int li, ri; re(li, ri);
+        array<int, 3> tmp = {li-1, ri, i};
+        todo.eb(tmp);
+    }
+    solve();
+    f0r(i, q) ps(ans[i]);
+
+    return 0;
+}
