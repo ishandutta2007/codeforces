436d7fd37b0f86f982059bd3a05f0b4806c88c3a
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 12:16:23 +0530

    rfpermen

---
+++ rfpermen/normal/112/A.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("O3")
+using namespace std;
+#define ll long long
+#define rep(i,n,N) for(int i=n;i<=N;i++)
+#define rap(i,n,N) for(int i=(int)n;i>=(int)N;i--)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define fi first
+#define se second
+#define sf se.fi
+#define ss se.se
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define piii pair<int,pii>
+#define piiii pair<piii,pii>
+#define endl "\n"
+const ll MAX=1e5+5;
+const ll MAX2 = 11;
+const int MOD=1000000000 + 7;
+const ll INF=20000;
+const int nr[]={1,0,-1,0,1,1,-1,-1};
+const int nc[]={0,1,0,-1,1,-1,1,-1};
+
+ll n,a,b,c,x,y,z,sz,T;
+string s,t;
+
+int main(){
+	//cout<<fixed<<setprecision(10);
+    //freopen("input.txt", "r", stdin);
+	//freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>s>>t;
+    sz = s.size()-1;
+    rep(i,0,sz)if(s[i]>='a')s[i] = s[i]-'a'+'A';
+    rep(i,0,sz)if(t[i]>='a')t[i] = t[i]-'a'+'A';
+    if(s==t)cout<<0;
+    else if(s>t)cout<<1;
+    else cout<<-1;
+    return 0;
+}
