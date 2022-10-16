f9829b6ce66fbcd0d9d50a33defe3ec2e6cce7de
ishan dutta <ishandutta2007@gmail.com>
2022-10-16 11:50:23 +0530

    rfpermen

---
+++ rfpermen/normal/1176/B.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+ 
+#pragma GCC optimize("O3")
+using namespace std;
+#define ll long long
+#define ull unsigned long long
+#define rep(i,n,N) for(ll i=n;i<=N;++i)
+#define rap(i,n,N) for(int i=n;i>=N;--i)
+#define mp make_pair
+#define pb push_back
+#define pob pop_back
+#define pf push_front
+#define pof pop_front
+#define fi first
+#define se second
+//#define ff fi.fi
+//#define fs fi.se
+#define sf se.fi
+#define ss se.se
+#define lc (id<<1)
+#define rc ((id<<1)|1)
+#define db(x) cerr << ">>>>>> " << #x << " -> " << x << endl
+#define nani(x) cerr <<  "Line " << __LINE__ << ": " << #x << " is " << x << endl
+#define all(x) x.begin(),x.end()
+#define pii pair<int,int> 
+#define pll pair<ll,ll>
+#define plll pair<ll,pll>
+#define piii pair<int,pii>
+#define piiii pair<pii,pii>
+#define psi pair<string,int>
+#define endl '\n'
+const int MAX = 4e5+5;
+const ll MAX2 = 11;
+const ll MOD = 1000000007;
+const ll MOD2 = 1000005329;
+const ll INF = 2e18;
+const int dr[]={1,0,-1,0,1,1,-1,-1,0};
+const int dc[]={0,1,0,-1,1,-1,1,-1,0};
+const double pi = acos(-1);
+const double EPS = 1e-9;
+const int block = 2000;
+
+int tc,n,x,cnt[3],z;
+ 
+int main(){
+//	cout<<fixed<<setprecision(4);
+//    freopen("input.txt", "r", stdin);
+//	freopen("output.txt","w",stdout);
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    cin>>tc;
+    while(tc--){
+    	memset(cnt,0,sizeof cnt);
+	    cin>>n;
+	    rep(i,1,n)cin>>x, cnt[x%3]++;
+	    z = min(cnt[1],cnt[2]), cnt[1]-=z, cnt[2]-=z;
+	    cout<<cnt[0]+z+cnt[1]/3+cnt[2]/3<<endl;
+	}
+    return 0;
+}
