0c173039b811a317efd21c850113a8256113a5e9
ishan dutta <ishandutta2007@gmail.com>
2022-10-17 05:57:52 +0530

    saketh

---
+++ saketh/normal/1242/C.cpp
@@ -0,0 +1,251 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// template {{{
+#define pb push_back
+#define eb emplace_back
+#define mp make_pair
+#define mt make_tuple
+#define lb lower_bound
+#define ub upper_bound
+#define f first
+#define s second
+#define resz resize
+
+#define sz(x) int((x).size())
+#define all(x) (x).begin(), (x).end()
+
+#define FOR(i, a, b) for (int i = (a); i < (b); i++)
+#define F0R(i, a) for (int i = 0; i < (a); i++)
+#define FORd(i, a, b) for (int i = (b)-1; i >= (a); i--)
+#define F0Rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto& a : x)
+
+#define sort_by(x, y) sort(all(x), [&](const auto& a, const auto& b) { return y; })
+
+using ll = long long;
+using vi = vector<int>;
+using vvi = vector<vi>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vb = vector<bool>;
+using vd = vector<double>;
+using vs = vector<string>;
+
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using pdd = pair<double, double>;
+
+using vpii = vector<pii>;
+using vvpii = vector<vpii>;
+using vpll = vector<pll>;
+using vvpll = vector<vpll>;
+using vpdd = vector<pdd>;
+using vvpdd = vector<vpdd>;
+
+template<typename T> void ckmin(T& a, const T& b) { a = min(a, b); }
+template<typename T> void ckmax(T& a, const T& b) { a = max(a, b); }
+
+mt19937_64 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+namespace __input {
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) {
+        re(first); re(rest...);
+    }
+
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+using namespace __input;
+
+namespace __output {
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const deque<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) {
+        pr(first); pr(rest...);
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) {
+        pr("{",x.f,", ",x.s,"}");
+    }
+    template<class T, bool pretty = true> void prContain(const T& x) {
+        if (pretty) pr("{");
+        bool fst = 1; for (const auto& a: x) pr(!fst?pretty?", ":" ":"",a), fst = 0;
+        if (pretty) pr("}");
+    }
+    template<class T> void pc(const T& x) { prContain<T, false>(x); pr("\n"); }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const deque<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) {
+        pr(first); ps();
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) {
+        pr(first," "); ps(rest...);
+    }
+}
+using namespace __output;
+
+#define TRACE(x) x
+#define __pn(x) pr(#x, " = ")
+#define pd(...) __pn((__VA_ARGS__)), ps(__VA_ARGS__), cout << flush
+
+namespace __algorithm {
+    template<typename T> void dedup(vector<T>& v) {
+        sort(all(v)); v.erase(unique(all(v)), v.end());
+    }
+    template<typename T> typename vector<T>::iterator find(vector<T>& v, const T& x) {
+        auto it = lower_bound(all(v), x); return it != v.end() && *it == x ? it : v.end();
+    }
+    template<typename T> size_t index(vector<T>& v, const T& x) {
+        auto it = find(v, x); assert(it != v.end() && *it == x); return it - v.begin();
+    }
+    template<typename C, typename T, typename OP> vector<T> prefixes(const C& v, T id, OP op) {
+        vector<T> r(sz(v)+1, id); F0R (i, sz(v)) r[i+1] = op(r[i], v[i]); return r;
+    }
+    template<typename C, typename T, typename OP> vector<T> suffixes(const C& v, T id, OP op) {
+        vector<T> r(sz(v)+1, id); F0Rd (i, sz(v)) r[i] = op(v[i], r[i+1]); return r;
+    }
+}
+using namespace __algorithm;
+
+struct monostate {
+    friend istream& operator>>(istream& is, const __attribute__((unused))monostate& ms) { return is; }
+    friend ostream& operator<<(ostream& os, const __attribute__((unused))monostate& ms) { return os; }
+} ms;
+
+template<typename W=monostate> struct wedge {
+    int u, v, i; W w;
+    wedge<W>(int _u=-1, int _v=-1, int _i=-1) : u(_u), v(_v), i(_i) {}
+    int operator[](int loc) const { return u ^ v ^ loc; }
+    friend void re(wedge& e) { re(e.u, e.v, e.w); --e.u, --e.v; }
+    friend void pr(const wedge& e) { pr(e.u, "<-", e.w, "->", e.v); }
+};
+
+namespace __io {
+    void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+    void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+    void setIO(string s = "") {
+        ios_base::sync_with_stdio(0); cin.tie(0);
+        cout << fixed << setprecision(15);
+        if (sz(s)) { setIn(s+".in"), setOut(s+".out"); }
+    }
+}
+using namespace __io;
+// }}}
+
+// umap {{{
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace __gnu_pbds;
+
+struct sp64_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = splitmix64(((uint64_t) new char | 1)
+                * chrono::steady_clock::now().time_since_epoch().count());
+        return splitmix64(x ^ FIXED_RANDOM);
+    }
+};
+template<class K> struct sp64_pair_hash {
+    size_t operator()(const pair<K, K>& x) const {
+        static_assert(sizeof(K) <= 4);
+        static sp64_hash sp64;
+        return sp64(((uint64_t) x.first << 32) ^ x.second);
+    }
+};
+
+template<class K, class V> using umap = gp_hash_table<K, V, sp64_hash>;
+// }}}
+
+int main() {
+    setIO();
+
+    int K; re(K);
+
+    vvi elts(K);
+    vll sum(K);
+    umap<ll, int> loc;
+
+    F0R (k, K) {
+        int N; re(N); elts[k].resz(N); re(elts[k]);
+        trav (e, elts[k]) loc[e] = k, sum[k] += e;
+    }
+
+    ll targ = accumulate(all(sum), 0ll);
+    if (targ % K != 0) { ps("No"); return 0; }
+    targ /= K;
+
+    vvpii how(1 << K); // can we satisfy subset
+    F0R (k, K) if (sum[k] != targ) F0R (i, sz(elts[k])) {
+        bool ok = 1;
+        vpii moves;
+        int cloc = k, rem = elts[k][i], bmask = 0;
+
+        while (true) {
+            bmask ^= 1 << cloc;
+            ll inc = targ - (sum[cloc] - rem);
+
+            auto it = loc.find(inc);
+            if (it == loc.end()) { ok = 0; break; }
+
+            moves.eb(inc, cloc);
+
+            int nloc = it->s;
+            if (bmask & (1 << nloc)) {
+                if (nloc != k || inc != elts[k][i]) ok = 0; // needs to cycle
+                break;
+            }
+
+            cloc = nloc;
+            rem = inc;
+        }
+
+        if (__builtin_popcount(bmask) == 1) ok = 0;
+        if (ok) how[bmask] = moves;
+    } else {
+        how[1 << k] = { pii{elts[k][0], k} };
+    }
+
+    vi dp(1 << K, -1); dp[0] = 0;
+    F0R (m, sz(dp)) {
+        for (int t = m; t; t = (t-1)&m) if (!how[t].empty() && dp[m^t] != -1) {
+            dp[m] = t; break;
+        }
+    }
+
+    if (dp.back() == -1) ps("No");
+    else {
+        vpii moves;
+        for (int m = sz(dp) - 1; m; m ^= dp[m]) {
+            moves.insert(moves.end(), all(how[dp[m]]));
+        }
+        sort_by(moves, loc[a.f] < loc[b.f]);
+        ps("Yes");
+        trav (m, moves) ps(m.f, m.s+1);
+    }
+
+    // did you check N=1? did you mix up N,M?
+    return 0;
+}
