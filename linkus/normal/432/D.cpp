089735d6b8bedb3ab13bacb33696250e84793d53
ishan dutta <ishandutta2007@gmail.com>
2022-10-18 17:10:35 +0530

    linkus

---
+++ linkus/normal/432/D.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+#define pb push_back
+#define mp make_pair
+#define all(a) begin(a),end(a)
+#define FOR(x,val,to) for(int x=(val);x<int((to));++x)
+#define FORE(x,val,to) for(auto x=(val);x<=(to);++x)
+#define FORR(x,arr) for(auto &x: arr)
+#define FORS(x,plus,arr) for(auto x = begin(arr)+(plus); x != end(arr); ++x)
+#define FORREV(x,plus,arr) for(auto x = (arr).rbegin()+(plus); x !=(arr).rend(); ++x)
+#define REE(s_) {cout<<s_<<'\n';exit(0);}
+#define GET(arr) for(auto &i: (arr)) sc(i)
+#define whatis(x) cerr << #x << " is " << x << endl;
+#define e1 first
+#define e2 second
+#define INF 0x7f7f7f7f
+typedef std::pair<int,int> pi;
+typedef std::vector<int> vi;
+typedef std::vector<std::string> vs;
+typedef int64_t ll;
+typedef uint64_t ull;
+#define umap unordered_map
+#define uset unordered_set
+using namespace std;
+using namespace __gnu_pbds;
+
+#ifdef _WIN32
+#define getchar_unlocked() _getchar_nolock()
+#define _CRT_DISABLE_PERFCRIT_LOCKS
+#endif
+template<class T> ostream& operator<<(ostream &os, vector<T> V) { os<<"[";for(auto const &vv:V)os<<vv<<","; os<<"]"; return os; }
+template<class L, class R> ostream& operator<<(ostream &os, pair<L, R> P) { os<<"("<<P.first<<","<<P.second<<")"; return os; }
+inline int fstoi(const string &str){auto it=str.begin();bool neg=0;int num=0;if(*it=='-')neg=1;else num=*it-'0';++it;while(it<str.end()) num=num*10+(*it++-'0');if(neg)num*=-1;return num;}
+inline void getch(char &x){while(x = getchar_unlocked(), x < 33){;}}
+inline void getstr(string &str){str.clear(); char cur;while(cur=getchar_unlocked(),cur<33){;}while(cur>32){str+=cur;cur=getchar_unlocked();}}
+template<typename T> inline bool sc(T &num){ bool neg=0; int c; num=0; while(c=getchar_unlocked(),c<33){if(c == EOF) return false;} if(c=='-'){ neg=1; c=getchar_unlocked(); } for(;c>47;c=getchar_unlocked()) num=num*10+c-48; if(neg) num*=-1; return true;}template<typename T, typename ...Args> inline void sc(T &num, Args &...args){ bool neg=0; int c; num=0; while(c=getchar_unlocked(),c<33){;} if(c=='-'){ neg=1; c=getchar_unlocked(); } for(;c>47;c=getchar_unlocked()) num=num*10+c-48; if(neg) num*=-1; sc(args...); }
+template<typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>; //s.find_by_order(), s.order_of_key() <- works like lower_bound
+template<typename T> using ordered_map = tree<T, int, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+int main(){
+    ios_base::sync_with_stdio(0);cin.tie(0);
+    string s;
+    getstr(s);
+    int n = s.size();
+    int z[n];
+    z[0] = 0;
+    int l = 0, r = 0;
+    for(int i = 1; i < n; ++i){
+        if(i > r){
+            l = r = i;
+            while(r < n && s[r] == s[r-l]) ++r;
+            z[i] = r-l; --r;
+        }
+        else{
+            int k = i-l;
+            if(z[k] < r-i+1) z[i] = z[k];
+            else{
+                l = i;
+                while(r < n && s[r] == s[r-l]) ++r;
+                z[i] = r-l; --r;
+            }
+        }
+    }
+    /* vi vec; */
+    int vec[n];
+    int ind = 0;
+    FORR(i, z){
+        if(i)
+            vec[ind++] = i;
+        /* vec.pb(i), */
+        cerr << i << ' ';
+    }
+    sort(vec,vec+ind,greater<int>());
+    --ind;
+    /* sort(all(vec),greater<int>()); */
+    vector<pair<int,int>> ans;
+    for(int i = 1; i < n; ++i){
+        if(z[n-i] == i){
+            while(vec[ind] < i) --ind;
+            ans.pb({i,ind+2});
+            /* while(vec.back() < i) vec.pop_back(); */
+            /* ans.pb({i,vec.size()+1}); */
+        }
+    }
+    cout << ans.size()+1 << '\n';
+    FORR(i,ans)
+        cout << i.e1 << ' ' << i.e2 << '\n';
+    cout << n << ' ' << 1 << '\n';
+}
