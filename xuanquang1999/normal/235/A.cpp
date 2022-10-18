71f8ca523cdc02900058caf99ee616876455fe06
ishan dutta <ishandutta2007@gmail.com>
2022-10-18 07:12:23 +0530

    xuanquang1999

---
+++ xuanquang1999/normal/235/A.cpp
@@ -0,0 +1,47 @@
+//84104971101048411497 - Can you guess what does this mean?
+using namespace std;
+#include <bits/stdc++.h>
+#define mapii map<int, int>
+#define debug(a) cout << #a << ": " << a << endl
+#define debuga1(a, l, r) fto(i, l, r) cout << a[i] << " "; cout << endl
+#define fdto(i, r, l) for(int i = (r); i >= (l); --i)
+#define fto(i, l, r) for(int i = (l); i <= (r); ++i)
+#define forit(it, var) for(__typeof(var.begin()) it = var.begin(); it != var.end(); it++)
+#define forrit(rit, var) for(__typeof(var.rbegin()) rit = var.rbegin(); rit != var.rend(); rit++)
+#define ii pair<int, int>
+#define iii pair<int, ii>
+#define ff first
+#define ss second
+#define mp make_pair
+#define pb push_back
+#define ll long long
+#define maxN 200005
+#define EPS 1e-9
+
+template <class T>
+T min(T a, T b, T c) {
+    return min(a, min(b, c));
+}
+
+template <class T>
+T max(T a, T b, T c) {
+    return max(a, max(b, c));
+}
+
+ll n;
+
+int main () {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        //freopen("output.txt", "w", stdout);
+    #endif // ONLINE_JUDGE
+
+    cin >> n;
+    if (n == 1) cout << 1;
+    else if (n == 2) cout << 2;
+    else if (n%2 == 1) cout << n*(n-1)*(n-2);
+    else if (n%3 == 0) cout << (n-1)*(n-2)*(n-3);
+    else cout << n*(n-1)*(n-3);
+
+    return 0;
+}
