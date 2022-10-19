f8bac3da4f42a882df28488755a0c31f9cc332da
ishan dutta <ishandutta2007@gmail.com>
2022-10-19 14:07:46 +0530

    alireza_kaviani

---
+++ alireza_kaviani/normal/1005/A.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long                   ll;
+typedef long double                 ld;
+typedef pair<int,int>               pii;
+typedef pair<ll,ll>                 pll;
+typedef pair<double,double>         pdd;
+typedef pair<ld,ld>                 pld;
+
+#define all(x)                      (x).begin(),(x).end()
+#define Sort(x)                     sort(all((x)))
+#define X                           first
+#define Y                           second
+#define Mp                          make_pair
+#define endt                        '\t'
+#define ends                        ' '
+#define error(x)                    cerr << #x << " = " << x << endl
+#define fast_io                     ios::sync_with_stdio(false);cin.tie(0);cout.tie(0);
+inline int input(){int n; cin >> n ; return n;}
+ll pw(ll a , ll b)  { return (!b ? 1 : (b & 1 ? a * pw(a * a , b / 2) : pw(a * a , b / 2))); }
+
+int main()
+{
+    fast_io;
+
+    int n , p = -1 , t = 0;
+    cin >> n;
+
+    vector<int> ans;
+    int a[n];
+    for(int i = 0 ; i < n ; i++)
+    {
+        cin >> a[i];
+        if(a[i] > p)
+        {
+            p = a[i];
+            t++;
+        }
+        else
+        {
+            p = a[i];
+            ans.push_back(t);
+            t = 1 ;
+        }
+    }
+    ans.push_back(t);
+    cout << ans.size() << endl;
+    for(int i : ans)    cout << i << ends;
+
+    return 0;
+}
