3088255c41aac4e613bc50b9ebb59306f54c42ba
Ishan Dutta <ishandutta2007@gmail.com>
2022-10-21 15:33:34 +0530

    rniya

---
+++ rniya/normal/1616/E.cpp
@@ -0,0 +1,355 @@
+#define LOCAL
+#include <bits/stdc++.h>
+using namespace std;
+#pragma region Macros
+typedef long long ll;
+typedef __int128_t i128;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+#define ALL(x) (x).begin(), (x).end()
+
+template <typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for (T& x : v) is >> x;
+    return is;
+}
+template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        os << v[i] << (i + 1 == v.size() ? "" : " ");
+    }
+    return os;
+}
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& p) {
+    os << '(' << p.first << ',' << p.second << ')';
+    return os;
+}
+template <typename T, typename U> ostream& operator<<(ostream& os, const map<T, U>& m) {
+    os << '{';
+    for (auto itr = m.begin(); itr != m.end();) {
+        os << '(' << itr->first << ',' << itr->second << ')';
+        if (++itr != m.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T, typename U> ostream& operator<<(ostream& os, const unordered_map<T, U>& m) {
+    os << '{';
+    for (auto itr = m.begin(); itr != m.end();) {
+        os << '(' << itr->first << ',' << itr->second << ')';
+        if (++itr != m.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const set<T>& s) {
+    os << '{';
+    for (auto itr = s.begin(); itr != s.end();) {
+        os << *itr;
+        if (++itr != s.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const multiset<T>& s) {
+    os << '{';
+    for (auto itr = s.begin(); itr != s.end();) {
+        os << *itr;
+        if (++itr != s.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const unordered_set<T>& s) {
+    os << '{';
+    for (auto itr = s.begin(); itr != s.end();) {
+        os << *itr;
+        if (++itr != s.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const deque<T>& v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        os << v[i] << (i + 1 == v.size() ? "" : " ");
+    }
+    return os;
+}
+template <typename T, size_t N> ostream& operator<<(ostream& os, const array<T, N>& v) {
+    for (size_t i = 0; i < N; i++) {
+        os << v[i] << (i + 1 == N ? "" : " ");
+    }
+    return os;
+}
+
+template <int i, typename T> void print_tuple(ostream&, const T&) {}
+template <int i, typename T, typename H, class... Args> void print_tuple(ostream& os, const T& t) {
+    if (i) os << ',';
+    os << get<i>(t);
+    print_tuple<i + 1, T, Args...>(os, t);
+}
+template <typename... Args> ostream& operator<<(ostream& os, const tuple<Args...>& t) {
+    os << '{';
+    print_tuple<0, tuple<Args...>, Args...>(os, t);
+    return os << '}';
+}
+
+void debug_out() { cerr << '\n'; }
+template <class Head, class... Tail> void debug_out(Head&& head, Tail&&... tail) {
+    cerr << head;
+    if (sizeof...(Tail) > 0) cerr << ", ";
+    debug_out(move(tail)...);
+}
+#ifdef LOCAL
+#define debug(...)                                                                   \
+    cerr << " ";                                                                     \
+    cerr << #__VA_ARGS__ << " :[" << __LINE__ << ":" << __FUNCTION__ << "]" << '\n'; \
+    cerr << " ";                                                                     \
+    debug_out(__VA_ARGS__)
+#else
+#define debug(...) void(0)
+#endif
+
+template <typename T> T gcd(T x, T y) { return y != 0 ? gcd(y, x % y) : x; }
+template <typename T> T lcm(T x, T y) { return x / gcd(x, y) * y; }
+
+int topbit(signed t) { return t == 0 ? -1 : 31 - __builtin_clz(t); }
+int topbit(long long t) { return t == 0 ? -1 : 63 - __builtin_clzll(t); }
+int botbit(signed a) { return a == 0 ? 32 : __builtin_ctz(a); }
+int botbit(long long a) { return a == 0 ? 64 : __builtin_ctzll(a); }
+int popcount(signed t) { return __builtin_popcount(t); }
+int popcount(long long t) { return __builtin_popcountll(t); }
+bool ispow2(int i) { return i && (i & -i) == i; }
+long long MSK(int n) { return (1LL << n) - 1; }
+
+template <class T> T ceil(T x, T y) {
+    assert(y >= 1);
+    return (x > 0 ? (x + y - 1) / y : x / y);
+}
+template <class T> T floor(T x, T y) {
+    assert(y >= 1);
+    return (x > 0 ? x / y : (x - y + 1) / y);
+}
+
+template <class T1, class T2> inline bool chmin(T1& a, T2 b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T1, class T2> inline bool chmax(T1& a, T2 b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T> void mkuni(vector<T>& v) {
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+}
+template <typename T> int lwb(const vector<T>& v, const T& x) { return lower_bound(v.begin(), v.end(), x) - v.begin(); }
+#pragma endregion
+
+
+#include <cassert>
+#include <vector>
+
+
+#include <cassert>
+#include <numeric>
+#include <type_traits>
+
+namespace atcoder {
+
+namespace internal {
+
+#ifndef _MSC_VER
+template <class T>
+using is_signed_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value ||
+                                  std::is_same<T, __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using is_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __uint128_t>::value ||
+                                  std::is_same<T, unsigned __int128>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using make_unsigned_int128 =
+    typename std::conditional<std::is_same<T, __int128_t>::value,
+                              __uint128_t,
+                              unsigned __int128>;
+
+template <class T>
+using is_integral = typename std::conditional<std::is_integral<T>::value ||
+                                                  is_signed_int128<T>::value ||
+                                                  is_unsigned_int128<T>::value,
+                                              std::true_type,
+                                              std::false_type>::type;
+
+template <class T>
+using is_signed_int = typename std::conditional<(is_integral<T>::value &&
+                                                 std::is_signed<T>::value) ||
+                                                    is_signed_int128<T>::value,
+                                                std::true_type,
+                                                std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<(is_integral<T>::value &&
+                               std::is_unsigned<T>::value) ||
+                                  is_unsigned_int128<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<
+    is_signed_int128<T>::value,
+    make_unsigned_int128<T>,
+    typename std::conditional<std::is_signed<T>::value,
+                              std::make_unsigned<T>,
+                              std::common_type<T>>::type>::type;
+
+#else
+
+template <class T> using is_integral = typename std::is_integral<T>;
+
+template <class T>
+using is_signed_int =
+    typename std::conditional<is_integral<T>::value && std::is_signed<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using is_unsigned_int =
+    typename std::conditional<is_integral<T>::value &&
+                                  std::is_unsigned<T>::value,
+                              std::true_type,
+                              std::false_type>::type;
+
+template <class T>
+using to_unsigned = typename std::conditional<is_signed_int<T>::value,
+                                              std::make_unsigned<T>,
+                                              std::common_type<T>>::type;
+
+#endif
+
+template <class T>
+using is_signed_int_t = std::enable_if_t<is_signed_int<T>::value>;
+
+template <class T>
+using is_unsigned_int_t = std::enable_if_t<is_unsigned_int<T>::value>;
+
+template <class T> using to_unsigned_t = typename to_unsigned<T>::type;
+
+}  // namespace internal
+
+}  // namespace atcoder
+
+
+namespace atcoder {
+
+template <class T> struct fenwick_tree {
+    using U = internal::to_unsigned_t<T>;
+
+  public:
+    fenwick_tree() : _n(0) {}
+    explicit fenwick_tree(int n) : _n(n), data(n) {}
+
+    void add(int p, T x) {
+        assert(0 <= p && p < _n);
+        p++;
+        while (p <= _n) {
+            data[p - 1] += U(x);
+            p += p & -p;
+        }
+    }
+
+    T sum(int l, int r) {
+        assert(0 <= l && l <= r && r <= _n);
+        return sum(r) - sum(l);
+    }
+
+  private:
+    int _n;
+    std::vector<U> data;
+
+    U sum(int r) {
+        U s = 0;
+        while (r > 0) {
+            s += data[r - 1];
+            r -= r & -r;
+        }
+        return s;
+    }
+};
+
+}  // namespace atcoder
+
+
+const int INF = 1e9;
+const long long IINF = 1e18;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const char dir[4] = {'D', 'R', 'U', 'L'};
+const long long MOD = 1000000007;
+// const long long MOD = 998244353;
+
+void solve() {
+    int n;
+    cin >> n;
+    string S, T;
+    cin >> S >> T;
+
+    {
+        if (S < T) {
+            cout << 0 << '\n';
+            return;
+        }
+    }
+
+    {
+        string U = S;
+        sort(ALL(U));
+        if (T <= U) {
+            cout << -1 << '\n';
+            return;
+        }
+    }
+
+    vector<queue<int>> que(26);
+    for (int i = 0; i < n; i++) que[S[i] - 'a'].emplace(i);
+    ll ans = IINF, cur = 0;
+    atcoder::fenwick_tree<int> FT(n);
+
+    for (int i = 0; i < n; i++) {
+        int x = T[i] - 'a';
+        for (int j = 0; j < x; j++) {
+            if (que[j].empty()) continue;
+            int nxt = que[j].front();
+            chmin(ans, cur + (nxt + FT.sum(nxt + 1, n) - i));
+        }
+        {
+            if (que[x].empty()) break;
+            int nxt = que[x].front();
+            que[x].pop();
+            FT.add(nxt, 1);
+            cur += nxt + FT.sum(nxt + 1, n) - i;
+        }
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    int q;
+    cin >> q;
+    for (; q--;) solve();
+    return 0;
+}
