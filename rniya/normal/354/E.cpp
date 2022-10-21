7f7c742296e87932a656ae801e216a27fe1192df
Ishan Dutta <ishandutta2007@gmail.com>
2022-10-21 15:16:18 +0530

    rniya

---
+++ rniya/normal/354/E.cpp
@@ -0,0 +1,221 @@
+#define LOCAL
+#include <bits/stdc++.h>
+using namespace std;
+#pragma region Macros
+typedef long long ll;
+typedef __int128_t i128;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+#define ALL(x) (x).begin(), (x).end()
+
+template <typename T> istream& operator>>(istream& is, vector<T>& v) {
+    for (T& x : v) is >> x;
+    return is;
+}
+template <typename T> ostream& operator<<(ostream& os, const vector<T>& v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        os << v[i] << (i + 1 == v.size() ? "" : " ");
+    }
+    return os;
+}
+template <typename T, typename U> ostream& operator<<(ostream& os, const pair<T, U>& p) {
+    os << '(' << p.first << ',' << p.second << ')';
+    return os;
+}
+template <typename T, typename U> ostream& operator<<(ostream& os, const map<T, U>& m) {
+    os << '{';
+    for (auto itr = m.begin(); itr != m.end();) {
+        os << '(' << itr->first << ',' << itr->second << ')';
+        if (++itr != m.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T, typename U> ostream& operator<<(ostream& os, const unordered_map<T, U>& m) {
+    os << '{';
+    for (auto itr = m.begin(); itr != m.end();) {
+        os << '(' << itr->first << ',' << itr->second << ')';
+        if (++itr != m.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const set<T>& s) {
+    os << '{';
+    for (auto itr = s.begin(); itr != s.end();) {
+        os << *itr;
+        if (++itr != s.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const multiset<T>& s) {
+    os << '{';
+    for (auto itr = s.begin(); itr != s.end();) {
+        os << *itr;
+        if (++itr != s.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const unordered_set<T>& s) {
+    os << '{';
+    for (auto itr = s.begin(); itr != s.end();) {
+        os << *itr;
+        if (++itr != s.end()) os << ',';
+    }
+    os << '}';
+    return os;
+}
+template <typename T> ostream& operator<<(ostream& os, const deque<T>& v) {
+    for (size_t i = 0; i < v.size(); i++) {
+        os << v[i] << (i + 1 == v.size() ? "" : " ");
+    }
+    return os;
+}
+template <typename T, size_t N> ostream& operator<<(ostream& os, const array<T, N>& v) {
+    for (size_t i = 0; i < N; i++) {
+        os << v[i] << (i + 1 == N ? "" : " ");
+    }
+    return os;
+}
+
+template <int i, typename T> void print_tuple(ostream&, const T&) {}
+template <int i, typename T, typename H, class... Args> void print_tuple(ostream& os, const T& t) {
+    if (i) os << ',';
+    os << get<i>(t);
+    print_tuple<i + 1, T, Args...>(os, t);
+}
+template <typename... Args> ostream& operator<<(ostream& os, const tuple<Args...>& t) {
+    os << '{';
+    print_tuple<0, tuple<Args...>, Args...>(os, t);
+    return os << '}';
+}
+
+void debug_out() { cerr << '\n'; }
+template <class Head, class... Tail> void debug_out(Head&& head, Tail&&... tail) {
+    cerr << head;
+    if (sizeof...(Tail) > 0) cerr << ", ";
+    debug_out(move(tail)...);
+}
+#ifdef LOCAL
+#define debug(...)                                                                   \
+    cerr << " ";                                                                     \
+    cerr << #__VA_ARGS__ << " :[" << __LINE__ << ":" << __FUNCTION__ << "]" << '\n'; \
+    cerr << " ";                                                                     \
+    debug_out(__VA_ARGS__)
+#else
+#define debug(...) void(0)
+#endif
+
+template <typename T> T gcd(T x, T y) { return y != 0 ? gcd(y, x % y) : x; }
+template <typename T> T lcm(T x, T y) { return x / gcd(x, y) * y; }
+
+int topbit(signed t) { return t == 0 ? -1 : 31 - __builtin_clz(t); }
+int topbit(long long t) { return t == 0 ? -1 : 63 - __builtin_clzll(t); }
+int botbit(signed a) { return a == 0 ? 32 : __builtin_ctz(a); }
+int botbit(long long a) { return a == 0 ? 64 : __builtin_ctzll(a); }
+int popcount(signed t) { return __builtin_popcount(t); }
+int popcount(long long t) { return __builtin_popcountll(t); }
+bool ispow2(int i) { return i && (i & -i) == i; }
+long long MSK(int n) { return (1LL << n) - 1; }
+
+template <class T> T ceil(T x, T y) {
+    assert(y >= 1);
+    return (x > 0 ? (x + y - 1) / y : x / y);
+}
+template <class T> T floor(T x, T y) {
+    assert(y >= 1);
+    return (x > 0 ? x / y : (x - y + 1) / y);
+}
+
+template <class T1, class T2> inline bool chmin(T1& a, T2 b) {
+    if (a > b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+template <class T1, class T2> inline bool chmax(T1& a, T2 b) {
+    if (a < b) {
+        a = b;
+        return true;
+    }
+    return false;
+}
+
+template <typename T> void mkuni(vector<T>& v) {
+    sort(v.begin(), v.end());
+    v.erase(unique(v.begin(), v.end()), v.end());
+}
+template <typename T> int lwb(const vector<T>& v, const T& x) { return lower_bound(v.begin(), v.end(), x) - v.begin(); }
+#pragma endregion
+
+const int INF = 1e9;
+const long long IINF = 1e18;
+const int dx[4] = {1, 0, -1, 0}, dy[4] = {0, 1, 0, -1};
+const char dir[4] = {'D', 'R', 'U', 'L'};
+const long long MOD = 1000000007;
+// const long long MOD = 998244353;
+
+vector<vector<int>> cand(10);
+int idx[50];
+vector<vector<int>> digits;
+const int MAX_LOG = 19;
+
+void solve() {
+    ll n;
+    cin >> n;
+
+    vector<vector<pair<int, int>>> dp(MAX_LOG + 1, vector<pair<int, int>>(5, {-1, -1}));
+    dp[0][0] = {0, 0};
+    ll m = n;
+    for (int i = 0; i < MAX_LOG; i++, m /= 10) {
+        int d = m % 10;
+        for (int j = 0; j < 5; j++) {
+            if (dp[i][j] == make_pair(-1, -1)) continue;
+            int target = (d + 10 - j) % 10;
+            for (int& x : cand[target]) {
+                dp[i + 1][(j + x) / 10] = {j, x};
+            }
+        }
+    }
+    if (dp[MAX_LOG][0] == make_pair(-1, -1)) {
+        cout << -1 << '\n';
+        return;
+    }
+    vector<ll> ans(6, 0);
+    for (int i = MAX_LOG - 1, j = 0; i >= 0; i--) {
+        auto p = dp[i + 1][j];
+        int x = idx[p.second];
+        // debug(x, digits[x]);
+        for (int k = 0; k < 6; k++) ans[k] = ans[k] * 10 + digits[x][k];
+        j = p.first;
+        // debug(p);
+    }
+
+    cout << ans << '\n';
+}
+
+int main() {
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    for (int i = 0; i <= 6; i++) {
+        for (int j = 0; i + j <= 6; j++) {
+            int k = 6 - i - j;
+            vector<int> v;
+            for (int p = 0; p < i; p++) v.emplace_back(0);
+            for (int p = 0; p < j; p++) v.emplace_back(4);
+            for (int p = 0; p < k; p++) v.emplace_back(7);
+            int sum = accumulate(v.begin(), v.end(), 0);
+            idx[sum] = digits.size();
+            cand[sum % 10].emplace_back(sum);
+            digits.emplace_back(v);
+        }
+    }
+
+    int t;
+    cin >> t;
+    for (; t--;) solve();
+    return 0;
+}
