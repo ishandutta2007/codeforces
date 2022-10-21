aa21be0205f233608509696a8e389682ea9c4376
Ishan Dutta <ishandutta2007@gmail.com>
2022-10-21 16:54:06 +0530

    modified:   rniya

--- rniya/normal/1404/E.cpp
+++ rniya/normal/1404/E.cpp
@@ -1,82 +1,5 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define LOCAL
-#pragma region Macros
-typedef long long ll;
-#define ALL(x) (x).begin(),(x).end()
-const long long MOD=1e9+7;
-// const long long MOD=998244353;
-const int INF=1e9;
-const long long IINF=1e18;
-const int dx[4]={1,0,-1,0},dy[4]={0,1,0,-1};
-const char dir[4]={'D','R','U','L'};
-
-template<typename T>
-istream &operator>>(istream &is,vector<T> &v){
-    for (T &x:v) is >> x;
-    return is;
-}
-template<typename T>
-ostream &operator<<(ostream &os,const vector<T> &v){
-    for (int i=0;i<v.size();++i){
-        os << v[i] << (i+1==v.size()?"": " ");
-    }
-    return os;
-}
-template<typename T,typename U>
-ostream &operator<<(ostream &os,const pair<T,U> &p){
-    os << '(' << p.first << ',' << p.second << ')';
-    return os;
-}
-template<typename T,typename U>
-ostream &operator<<(ostream &os,const map<T,U> &m){
-    os << '{';
-    for (auto itr=m.begin();itr!=m.end();++itr){
-        os << '(' << itr->first << ',' << itr->second << ')';
-        if (++itr!=m.end()) os << ',';
-        --itr;
-    }
-    os << '}';
-    return os;
-}
-template<typename T>
-ostream &operator<<(ostream &os,const set<T> &s){
-    os << '{';
-    for (auto itr=s.begin();itr!=s.end();++itr){
-        os << *itr;
-        if (++itr!=s.end()) os << ',';
-        --itr;
-    }
-    os << '}';
-    return os;
-}
-
-void debug_out(){cerr << '\n';}
-template<class Head,class... Tail>
-void debug_out(Head&& head,Tail&&... tail){
-    cerr << head;
-    if (sizeof...(Tail)>0) cerr << ", ";
-    debug_out(move(tail)...);
-}
-#ifdef LOCAL
-#define debug(...) cerr << " ";\
-cerr << #__VA_ARGS__ << " :[" << __LINE__ << ":" << __FUNCTION__ << "]" << '\n';\
-cerr << " ";\
-debug_out(__VA_ARGS__)
-#else
-#define debug(...) 42
-#endif
-
-template<typename T> T gcd(T x,T y){return y!=0?gcd(y,x%y):x;}
-template<typename T> T lcm(T x,T y){return x/gcd(x,y)*y;}
-
-template<class T1,class T2> inline bool chmin(T1 &a,T2 b){
-    if (a>b){a=b; return true;} return false;
-}
-template<class T1,class T2> inline bool chmax(T1 &a,T2 b){
-    if (a<b){a=b; return true;} return false;
-}
-#pragma endregion
 
 template<typename T,bool directed>
 struct Dinic{
@@ -193,9 +116,7 @@ struct ProjectSelectionProblem{
     }
 };
 
-int main(){
-    cin.tie(0);
-    ios::sync_with_stdio(false);
+void CFR668_E(){
     int n,m; cin >> n >> m;
     vector<string> S(n);
     for (int i=0;i<n;++i) cin >> S[i];
@@ -208,7 +129,7 @@ int main(){
             if (S[i][j]!='#') continue;
             ++ans;
             for (int k=0;k<2;++k){
-                int nx=i+dx[k],ny=j+dy[k];
+                int nx=i+(k^1),ny=j+k;
                 if (n<=nx||m<=ny) continue;
                 if (S[nx][ny]!='#') continue;
                 if (!k) PSP.x_true_y_true_profit(id(i,j),id(nx,ny),1);
@@ -218,4 +139,10 @@ int main(){
     }
 
     cout << ans+PSP.build() << '\n';
+}
+
+int main(){
+    cin.tie(0);
+    ios::sync_with_stdio(false);
+    CFR668_E();
 }
