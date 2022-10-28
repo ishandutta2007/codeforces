b27517132d6428ad9466dfc03aead3b7c74428ab
ishan dutta <ishandutta2007@gmail.com>
2022-10-29 02:44:31 +0530

    ashishgup

---
+++ ashishgup/normal/505/C.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fr(i,s,e) for(i=s;i<e;i++)
+#define rf(i,s,e) for(i=s-1;i>=e;i--)
+#define pb push_back
+#define eb emblace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+typedef int ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<vvvll> vvvvll;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef vector<pair<long long,long long> > vpll;
+typedef vector<vector<pair<ll,ll> > > vvpll;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+#define PI 3.141592653589793
+#define MOD 1000000007
+
+template<typename T> T gcd(T a,T b) { if(a==0) return b; return gcd(b%a,a); }
+template<typename T> T pow(T a,T b, ll m){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a)%m; b/=2; a=(a*a)%m; } return ans%m; }
+
+ll n,d;
+vi v;
+vvi cache(30001,vi(1000,-1));
+vi m(30001,0);
+ll last=0;
+ll nor;
+ll dp(ll i,ll prev)
+{
+	if(i>30000)
+	{
+		return 0;
+	}
+	if(i>last)
+	{
+		return 0;
+	}
+	if(cache[i][prev-nor]!=-1)
+	{
+		return cache[i][prev-nor];
+	}
+//	trace2(i,prev);
+	cache[i][prev-nor]=0;
+	ll st=m[i];
+	for(ll j=max(1,prev-1);j<=prev+1;j++)
+	{
+		cache[i][prev-nor]=max(st+dp(i+j,j),cache[i][prev-nor]);
+	}
+	return cache[i][prev-nor];
+}
+int main()
+{
+	IOS;
+	cin>>n>>d;
+	if(d<=500)
+	{
+		nor=0;
+	}
+	else
+	{
+		nor=d-501;
+	}
+	for(ll i=0;i<n;i++)
+	{
+		ll loc;
+		cin>>loc;
+		last=max(loc,last);
+		m[loc]++;
+	}
+	cout<<dp(d,d);
+	return 0;
+}
