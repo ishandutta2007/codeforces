4594149fbbe21e4800e6809c7eb7ad4dcd85bd1c
ishan dutta <ishandutta2007@gmail.com>
2022-10-29 02:50:07 +0530

    ashishgup

---
+++ ashishgup/normal/812/C.cpp
@@ -0,0 +1,115 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fr(i,s,e) for(i=s;i<e;i++)
+#define rf(i,s,e) for(i=s-1;i>=e;i--)
+#define pb push_back
+#define eb emblace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+typedef long long ll;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<vvvll> vvvvll;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef vector<pair<long long,long long> > vpll;
+typedef vector<vector<pair<ll,ll> > > vvpll;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+#define PI 3.141592653589793
+#define MOD 1000000007
+
+template<typename T> T gcd(T a,T b) { if(a==0) return b; return gcd(b%a,a); }
+template<typename T> T pow(T a,T b, ll m){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a)%m; b/=2; a=(a*a)%m; } return ans%m; }
+
+ll n,s;
+vll v;
+
+bool check(ll k)
+{
+	vll v2(n);
+	for(ll i=0;i<n;i++)
+	{
+		v2[i]=v[i]+(i+1)*k;
+	}
+	sort(v2.begin(),v2.end());
+	ll tot=0;
+	for(ll i=0;i<k;i++)
+	{
+		tot+=v2[i];
+	}
+	if(tot<=s)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+ll binsearch(ll lo,ll hi)
+{
+	while(lo<hi)
+	{
+	//	trace2(lo,hi);
+		ll mid=(lo+hi+1)/2;
+		if(check(mid))
+		{
+			lo=mid;
+		}
+		else
+		{
+			hi=mid-1;
+		}
+	}
+	return lo;
+}
+int main()
+{
+	IOS;
+	cin>>n>>s;
+	v=vll(n);
+	for(ll i=0;i<n;i++)
+	{
+		cin>>v[i];
+	}
+	ll ans=binsearch(0,n);
+	ll k=ans;
+	vll v2(n);
+	for(ll i=0;i<n;i++)
+	{
+		v2[i]=v[i]+(i+1)*k;
+	}
+	sort(v2.begin(),v2.end());
+	ll tot=0;
+	for(ll i=0;i<k;i++)
+	{
+		tot+=v2[i];
+	}
+	cout<<ans<<" "<<tot;
+	return 0;
+}
---
+++ ashishgup/normal/813/B.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fr(i,s,e) for(i=s;i<e;i++)
+#define rf(i,s,e) for(i=s-1;i>=e;i--)
+#define pb push_back
+#define eb emblace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+typedef long double ld;
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<vvvll> vvvvll;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef vector<pair<long long,long long> > vpll;
+typedef vector<vector<pair<ll,ll> > > vvpll;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+#define PI 3.141592653589793
+#define MOD 1000000007
+
+template<typename T> T gcd(T a,T b) { if(a==0) return b; return gcd(b%a,a); }
+template<typename T> T pow(T a,T b, ll m){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a)%m; b/=2; a=(a*a)%m; } return ans%m; }
+
+int main()
+{
+	IOS;
+	ll x,y,l,r;
+	cin>>x>>y>>l>>r;
+	ll amax=(ld)log(1e18)/(ld)(log(x));
+	ll bmax=(ld)log(1e18)/(ld)(log(y));
+	set<ll> s;
+//	trace2(amax,bmax);
+	for(ll i=0;i<=amax;i++)
+	{
+		for(ll j=0;j<=bmax;j++)
+		{
+			ll num=pow(x,i,2*1e18)+pow(y,j,2*1e18);
+			if(num>=l&&num<=r)
+				s.insert(num);
+		}
+	}
+	vll v;
+	if(s.size()==0)
+	{
+		cout<<r-l+1;
+		return 0;
+	}
+	for(auto it:s)
+	{
+		v.pb(it);
+	//	cout<<it<<" ";
+	}
+	
+//	cout<<endl;
+	ll ans=v[0]-l;
+	ll sz=v.size();
+	for(ll i=1;i<sz;i++)
+	{
+		ans=max(ans,v[i]-v[i-1]-1);	
+	}	
+	ans=max(ans,r-v[sz-1]);
+	cout<<max(ans,0LL);
+	return 0;
+}
