baa5974c849cbbb6404d0cb7b07d8470503c7480
ishan dutta <ishandutta2007@gmail.com>
2022-10-29 02:37:28 +0530

    ashishgup

---
+++ ashishgup/normal/16/C.cpp
@@ -0,0 +1,94 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define IOS ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+#define endl "\n"
+#define Max(x,y,z) max(x,max(y,z))
+#define Min(x,y,z) min(x,min(y,z))
+#define fr(i,s,e) for(i=s;i<e;i++)
+#define rf(i,s,e) for(i=s-1;i>=e;i--)
+#define pb push_back
+#define eb emblace_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define trace1(x)                cerr<<#x<<": "<<x<<endl
+#define trace2(x, y)             cerr<<#x<<": "<<x<<" | "<<#y<<": "<<y<<endl
+#define trace3(x, y, z)          cerr<<#x<<":" <<x<<" | "<<#y<<": "<<y<<" | "<<#z<<": "<<z<<endl
+#define trace4(a, b, c, d)       cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<endl
+#define trace5(a, b, c, d, e)    cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<endl
+#define trace6(a, b, c, d, e, f) cerr<<#a<<": "<<a<<" | "<<#b<<": "<<b<<" | "<<#c<<": "<<c<<" | "<<#d<<": "<<d<<" | "<<#e<< ": "<<e<<" | "<<#f<<": "<<f<<endl
+
+typedef long long ll;
+typedef long double ld;
+typedef unsigned int uint;
+typedef unsigned long long ull;
+typedef pair<int, int> pii;
+typedef pair<long long, long long> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<vvi> vvvi;
+typedef vector<vvvi> vvvvi;
+typedef vector<long long> vll;
+typedef vector<vll> vvll;
+typedef vector<vvll> vvvll;
+typedef vector<vvvll> vvvvll;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef vector<vvc> vvvc;
+typedef vector<pair<long long,long long> > vpll;
+typedef vector<vector<pair<ll,ll> > > vvpll;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+
+#define PI 3.141592653589793
+#define MOD 1000000007
+
+template<typename T> T gcd(T a,T b) { if(a==0) return b; return gcd(b%a,a); }
+template<typename T> T pow(T a,T b, ll m){T ans=1; while(b>0){ if(b%2==1) ans=(ans*a)%m; b/=2; a=(a*a)%m; } return ans%m; }
+
+ll a,b,x,y;
+
+bool check(ll k)
+{
+	ll c=a-k*x;
+	ll d=b-k*y;
+	if(c>=0&&d>=0)
+		return true;
+	else
+		return false;
+}
+ll binsearch(ll lo, ll hi)
+{
+	while(lo<hi)
+	{
+		ll mid=(lo+hi+1)/2;
+		if(check(mid))
+		{
+			lo=mid;
+		}
+		else
+		{
+			hi=mid-1;
+		}
+	}
+	if(check(lo))
+	{
+		return lo;
+	}
+	else
+	{
+		return 0;
+	}
+}
+int main()
+{
+	IOS;
+	cin>>a>>b>>x>>y;
+	ll gcd1=gcd(x,y);
+	x/=gcd1;
+	y/=gcd1;
+	ll ans=binsearch(1,a/x);
+	cout<<ans*x<<" "<<ans*y;
+	return 0;
+}
