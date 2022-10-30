904444cfa4fe23ce818376b666e7b9dcff3d5a95
ishan dutta <ishandutta2007@gmail.com>
2022-10-31 04:06:50 +0530

    hloya_ygrt

---
+++ hloya_ygrt/normal/699/B.cpp
@@ -0,0 +1,144 @@
+#include <algorithm>
+#include <assert.h>
+#include <bitset>
+// #include <bits/stdc++.h>
+#include <cmath>
+#include <complex>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <ctime>
+#include <deque>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <iterator>
+#include <list>
+#include <map>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <utility>
+#include <vector>
+ 
+#define f first
+#define s second
+#define ll long long
+#define ld long double
+#define pb push_back
+#define files1 freopen("input.txt","r",stdin)
+#define files2 freopen("output.txt","w",stdout)
+#define files files1;files2
+#define mp make_pair
+#define fast_io ios_base::sync_with_stdio(0);cin.tie(0)
+#define vi vector<int>
+#define pii pair<int,int>
+#define endl '\n'
+
+using namespace std;
+ 
+void bad(string mes = "Impossible"){cout << mes;exit(0);}
+template<typename ... T> void shit(T& ... x){}
+template<typename ... T> void dec(T& ... x){shit(--x...);}
+template<typename ... T> void inc(T& ... x){shit(++x...);}
+#include <unordered_map>
+#include <unordered_set>
+
+template<typename T>
+string bin(T x, int st = 2){
+    string ans = "";
+    while (x > 0){
+        ans += char('0' + x % st);
+        x /= st;
+    }
+    reverse(ans.begin(), ans.end());
+    return ans.empty() ? "0" : ans;
+}
+
+template<typename T>
+T input(){
+    T ans = 0, m = 1;
+    char c = ' ';
+    while (!(c >= '0' && c <= '9') || c == '-') {
+        c = getchar();
+    }
+    if (c == '-')
+        m = -1, c = getchar();
+    while (c >= '0' && c <= '9'){
+        ans = ans * 10 + (c - '0'), c = getchar();
+    }
+    return ans * m;
+}
+
+template<typename T>
+void read(T& a) {
+    a = input<T>();
+}
+
+template<typename T>
+void read(T& a, T& b) {
+    read(a), read(b);
+}
+
+template<typename T>
+void read(T& a, T& b, T& c) {
+    read(a, b), read(c);
+}
+
+template<typename T>
+void read(T& a, T& b, T& c, T& d) {
+    read(a, b), read(c, d);
+}
+ 
+const int inf = 1e9;
+const double eps = 1e-9;
+const int maxn = 1e3 + 3, base = 1e9;
+const ll llinf = 1e18 + 1;
+
+template<typename T>
+T binpow(T n, T s)
+{
+    if (s <= 0)
+        return 1LL;
+    if (s % 2 == 0){
+        T b = binpow(n, s / 2);
+        return ( 1LL * b * b ) % base;
+    } else {
+        return (1LL* binpow(n, s - 1) * n) % base;
+    }
+}
+
+int row[maxn], column[maxn];
+
+char a[maxn][maxn];
+
+int main() {
+    int n, m;
+    read(n, m);
+
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++) {
+            char c = endl;
+            while (c == endl)
+                c = getchar();
+            if (c == '*')
+                row[i] += 1, column[j] += 1, cnt++;
+            a[i][j] = c;
+        }
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++){
+            int cur = row[i] + column[j];
+            if (a[i][j] == '*')
+                cur--;
+            if (cur == cnt) {
+                cout << "YES\n" << i + 1 << ' ' << j + 1;
+                return 0;
+            }
+        }
+        bad("NO");
+    return 0;
+}
