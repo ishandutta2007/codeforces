be38c6f3e8f8830685c8fd107616b3a1b89929e6
ishan dutta <ishandutta2007@gmail.com>
2022-10-31 20:53:09 +0530

    cinnamoroll

---
+++ cinnamoroll/normal/1194/B.cpp
@@ -0,0 +1,74 @@
+// warm heart, wagging tail,and a smile just for you!
+//                                                                        
+//                                                                   
+//                                                                
+//                                                   
+//                                      
+//                               
+//                             
+//                           
+//                         
+//                       
+//                     
+//                 
+//     
+//   
+// 
+// 
+// 
+// 
+//   
+//         
+//                                                        
+#include "bits/stdc++.h"
+using namespace std;
+#define MOD 1000000007
+const double EPS = 1e-9;
+#define INF (1LL<<60)
+#define fs first
+#define sc second
+#define D double
+#define int long long
+#define FOR(i,a,b) for(int i=(a);i<(b);i++)
+#define RFOR(i,a,b) for(int i = (b-1);i>=a;i--)
+#define REP(i,n)  FOR(i,0,n)
+#define RREP(i,n) RFOR(i,0,n)
+#define ITR(itr,mp) for(auto itr = (mp).begin(); itr != (mp).end(); ++itr)
+#define RITR(itr,mp) for(auto itr = (mp).rbegin(); itr != (mp).rend(); ++itr)
+#define range(i,a,b) ((a)<=(i) && (i)<(b))
+#define debug(x)  cout << #x << " = " << (x) << endl;
+#define SP << " " << 
+typedef pair<int,int> P;
+typedef vector<vector<P> > Graph;
+typedef vector<int> vec;
+typedef vector<vector<int>> mat;
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int q;
+  cin >> q;
+
+  REP(_,q){
+    int n,m;
+    cin >> n >>m;
+    
+    vector<string> s(n);
+    REP(i,n) cin >> s[i];
+    
+    vec tate(m,0),yoko(n,0);
+    REP(i,n) REP(j,m){
+      if(s[i][j] == '*') tate[j]++, yoko[i]++;
+    }
+
+    int ans = INF;
+    REP(i,n) REP(j,m){
+      ans = min(ans,n+m-tate[j]-yoko[i]-(s[i][j]=='.'));
+    }
+
+    cout << ans << '\n';
+  }
+
+  return 0;
+}
---
+++ cinnamoroll/normal/1195/D2.cpp
@@ -0,0 +1,142 @@
+// warm heart, wagging tail,and a smile just for you!
+//                                                                     
+//                                                                   
+//                                                                
+//                                                   
+//                                      
+//                               
+//                             
+//                           
+//                         
+//                       
+//                     
+//                 
+//     
+//   
+// 
+// 
+// 
+// 
+//   
+//         
+//                                                      
+#include "bits/stdc++.h"
+using namespace std;
+//#define MOD 1000000007
+#define MOD 998244353
+const double EPS = 1e-9;
+#define INF (1LL<<60)
+#define D double
+#define fs first
+#define sc second
+#define int long long
+#define FOR(i,a,b) for(int i=(a);i<(b);++i)
+#define RFOR(i,a,b) for(int i = (b-1);i>=(a);--i)
+#define REP(i,n)  FOR(i,0,(n))
+#define RREP(i,n) RFOR(i,0,(n))
+#define ITR(itr,mp) for(auto itr = (mp).begin(); itr != (mp).end(); ++itr)
+#define RITR(itr,mp) for(auto itr = (mp).rbegin(); itr != (mp).rend(); ++itr)
+#define range(i,a,b) ((a)<=(i) && (i)<(b))
+#define debug(x)  cout << #x << " = " << (x) << endl;
+#define SP << " " << 
+typedef pair<int,int> P;
+
+#include <cstdint>
+
+template <std::uint_fast64_t Modulus> class modint {
+  using u64 = std::uint_fast64_t;
+
+  u64 a;
+
+public:
+  constexpr modint(const u64 x = 0) noexcept : a(x % Modulus) {}
+  constexpr u64 val() const noexcept { return a; }
+  constexpr modint operator+(const modint rhs) const noexcept {
+    return modint(*this) += rhs;
+  }
+  constexpr modint operator-(const modint rhs) const noexcept {
+    return modint(*this) -= rhs;
+  }
+  constexpr modint operator*(const modint rhs) const noexcept {
+    return modint(*this) *= rhs;
+  }
+  constexpr modint operator/(const modint rhs) const noexcept {
+    return modint(*this) /= rhs;
+  }
+  constexpr bool operator==(const modint rhs) const noexcept {
+    return modint(*this).val() == rhs.val();
+  }
+  modint &operator+=(const modint rhs) noexcept {
+    a += rhs.a;
+    if (a >= Modulus) {
+      a -= Modulus;
+    }
+    return *this;
+  }
+  modint &operator-=(const modint rhs) noexcept {
+    if (a < rhs.a) {
+      a += Modulus;
+    }
+    a -= rhs.a;
+    return *this;
+  }
+  modint &operator*=(const modint rhs) noexcept {
+    a = a * rhs.a % Modulus;
+    return *this;
+  }
+  modint &operator/=(modint rhs) noexcept {
+    u64 exp = Modulus - 2;
+    while (exp) {
+      if (exp % 2) {
+        *this *= rhs;
+      }
+      rhs *= rhs;
+      exp /= 2;
+    }
+    return *this;
+  }
+};
+using mint = modint<MOD>;
+
+typedef vector<mint> vec;
+typedef vector<vector<mint>> mat;
+
+signed main(){
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+
+  int n;
+  cin >>n;
+
+  vector<string> a(n);
+  vector<int> len(11,0);
+  REP(i,n) cin >> a[i], len[a[i].size()]++;
+  
+  mint ans = 0;
+  REP(i,n){
+    FOR(j,1,11){
+    mint tmp = 0, tmp2 = 0;
+    //sita
+      REP(k,a[i].size()){
+        if(a[i].size()-k <= j) tmp *= 100;
+        else tmp *= 10;
+        tmp += (a[i][k]-'0');
+      }
+    //ue
+      REP(k,a[i].size()){
+        if(a[i].size()-k < j) tmp2 *= 100;
+        else tmp2 *= 10;
+        tmp2 += (a[i][k]-'0');
+      }
+      tmp2 *= 10;
+      ans += (tmp+tmp2)*len[j];
+      //cout << len[j] << endl;
+      //cout << tmp.val() SP tmp2.val() << endl;
+      //debug(ans.val());
+    }
+  }
+
+  cout << ans.val() << endl;
+
+  return 0;
+}
