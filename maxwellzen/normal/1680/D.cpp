b058dff4f5c112c53d04a4ad685d8a4f50e06191
ishan dutta <ishandutta2007@gmail.com>
2022-10-31 05:35:20 +0530

    maxwellzen

---
+++ maxwellzen/normal/1680/D.cpp
@@ -0,0 +1,127 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// #include <ext/pb_ds/assoc_container.hpp>
+// using namespace __gnu_pbds;
+// typedef tree<int,null_type,less<int>,rb_tree_tag,tree_order_statistics_node_update> iset;
+
+typedef long long ll;
+typedef long double db;
+typedef string str;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+typedef vector<bool> vb;
+typedef vector<vb> vvb;
+typedef vector<str> vs;
+typedef vector<char> vc;
+typedef vector<vc> vvc;
+typedef pair<int, int> pi;
+typedef pair<ll, ll> pl;
+typedef pair<db, db> pd;
+typedef vector<pi> vpi;
+typedef vector<vpi> vvpi;
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define F0R(i,a) FOR(i,0,a)
+#define ROF(i,a,b) for (int i = (b)-1; i >= (a); --i)
+#define R0F(i,a) ROF(i,0,a)
+#define trav(a,x) for (auto& a: x)
+#define uid(a, b) uniform_int_distribution<int>(a, b)(rng)
+
+#define pb push_back
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define mp make_pair
+#define F first
+#define S second
+#define lb lower_bound
+#define ub upper_bound
+#define arr array
+#define nl '\n'
+#define YES cout<<"YES\n"
+#define NO cout<<"NO\n"
+
+#ifndef LOCAL
+#define debug(...) 0
+#else
+#include "/Users/maxwellzen/CP/debug.cpp"
+#endif
+
+template<class T> using pq = priority_queue<T>;
+template<class T> using pqg = priority_queue<T, vector<T>, greater<T>>;
+template<int N> using ai = array<int, N>;
+template<class T> bool ckmin(T& a, const T& b) { return b < a ? a = b, 1 : 0; }
+template<class T> bool ckmax(T& a, const T& b) { return a < b ? a = b, 1 : 0; }
+template<class T> void read(vector<T>& a, int n) {a.rsz(n); F0R(i, n) cin >> a[i];}
+template<class T> void read(vector<vector<T>>& a, int n, int m) {a.assign(n, vector<T>(m)); F0R(i, n) F0R(j, m) cin >> a[i][j];}
+template<class T> void print(vector<T>& a, int n) {F0R(i, n) cout << a[i] << ' '; cout << nl;}
+template<class T> void print(vector<vector<T>>& a, int n, int m) {F0R(i, n) {F0R(j, m) cout << a[i][j] << ' '; cout << nl;}}
+template<class T> bool btw(T x, T a, T b) {return a <= x && x <= b;}
+
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+const ll MOD = 1e9+7;// 998244353;
+ll gcd(ll a, ll b){return b == 0 ? a: gcd(b, a%b);}
+const int dx[4]={1,0,-1,0}, dy[4]={0,1,0,-1}; // D R U L
+
+void setIO(string name="") {
+    ios_base::sync_with_stdio(0); cin.tie(0);
+    if (name != "") {
+        freopen((name+".in").c_str(),"r",stdin);
+        freopen((name+".out").c_str(),"w",stdout);
+    }
+}
+
+int n, m;
+ll k;
+vll a, p;
+vi cnt;
+
+void solve() {
+    cin >> n >> k;
+    read(a, n);
+    p.rsz(n+1, 0);
+    F0R(i, n) p[i+1] = p[i]+a[i];
+    cnt.rsz(n+1, 0);
+    F0R(i, n) cnt[i+1] = cnt[i] + (a[i]==0);
+    m = cnt[n];
+    if (m*k < abs(p[n])) {
+        cout << -1 << nl;
+        return;
+    }
+    int upnum, downnum;
+    ll upval, downval;
+    F0R(i, m+1) {
+        ll val = p[n] + i*k - (m-i)*k;
+        if (abs(val) <= k) {
+            upnum = i;
+            downnum = m-i;
+            upval = i*k - max(0ll, val);
+            downval = (i-m)*k - min(0ll, val);
+            break;
+        }
+    }
+    ll ans = 0;
+    FOR(j, 1, n) {
+        F0R(i, j+1) {
+            ll val = p[j+1] - p[i];
+            int num = cnt[j+1] - cnt[i];
+            if (num >= upnum) ckmax(ans, abs(val + upval + min(0ll, downval + (m-num)*k)));
+            else ckmax(ans, abs(val + k*num));
+            if (num >= downnum) ckmax(ans, abs(val + downval + max(0ll, upval - (m-num)*k)));
+            else ckmax(ans, abs(val - k*num));
+        }
+    }
+    cout << ans+1 << nl;
+}
+
+int main() {
+    setIO();
+    cout << fixed << setprecision(15);
+    int t = 1;
+    
+    while (t--) {
+        solve();
+    }
+}
