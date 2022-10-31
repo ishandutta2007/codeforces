50c57b387a54cf10be3ed66f9a4f9eb1c6fcea5d
ishan dutta <ishandutta2007@gmail.com>
2022-10-31 16:50:16 +0530

    compute

---
+++ compute/normal/1267/H.cpp
@@ -0,0 +1,229 @@
+/* Aa^~ kokoro ga pyonpyon suru n jaa^~
+// ZZZXXkXkkkZ!``` ``` ``` ``` ``` ``` ``` ``` ``` ``` ``` ```?Wfpppbpbbpbbpbbbkbkk
+// ppbbbpbbpVr`` `` ` ` ` ` ```` `` ` ` ``  ` ` ` ` ` ` ` ` `  dppbbkkkkkkkkkkqkqkk
+// HkqqqqqkkWr`` ` ` ``` ``` `?G, ` ` ``.JC!```` ` ` `` `` ````(Wpbkkkkkkkkqkkkkkqk
+// mmmmmqqqqpr` `` `` ```````.+zT=`` `` 7TO-.```````` `` `` ```(yppbkkkkkkkkkkkkkkk
+// ggmgmmqqqH$ ``````````....````` `  ````````.`````` `` ``````.yfpppbbbbkkkkqqqqqH
+// gmmmmmqqqkW<```` `````...`````  .,.`  ````....````` ``````` (Wbqqmgmmgmggggggggg
+// qmmmqqqqkkWk.``````````````````` ;:<`` `````.`````````````-_<-?WHHqmmmmmmgmmgggg
+// @@@@@@@gggHH6- ``````````````` `` _ `` ```````````````` ._~~_.`-?Wkqmmmmmmmggg@g
+// @@@@g@gggHY~.-<_- `````````````````````````````````` ._~~(<-``.`.(WHqqqmmggggmmm
+// @@g@gggHH=.`..._<-___..```````````````````````. .-_~~~_(!``-.``.`` OHHWUWHmqHWXW
+// gggggmqK1.``..~.. _<<+-(____..  ````````  ..__~~_((<<!.`.``` .``.`` j0C1XUHmHIdW
+// ggmmqH0!,_``.>`````` _<<;<v<<<++((((((((((<<<<<<~_. (-.``~``.>..```  jOuWHHqHIdH
+// gmmqkW!`(_ J>`    `` ` _~<`_~~~~<<<<<<<~~__````````` ?1. ._`(__```    zXWHg@HkXH
+// gHHWS{``(lJ<!``.``.```(:+>`._`````.` <..`` - ``. `   _ ?&._.I`_`````` .XyVfppppW
+// HHHSv``.(X:_..... _..(;+<!.(<..-.....-.-_..+_`..<.`.`..`_IJd}  .`..````jqg@@@@@@
+// XHWZ{..<Jk~!.`.. (<.-(+>(_.(1.(_..`.`.`.<_.+<_..<<-..._..-zy>.`_`...```.WH@HHHHH
+// bkWt~.-jCz(_..`.(+<.(;< ._-<=_(<_..-....(_.<1<..(<<.`._..-JUS-._.`...```dHmH9VUH
+// WUUO..(f.(c...__+z<-(+~` _-+<_(><..__.`.(<._.z_.(1;_..__.(C(zT-(..`...``(WHR<+Xk
+// kkkk._(_.->..._(z;:_><.._>_+_<(1>_._<...(v<<.(<.(+z<..-_(Z~_<_j+_..`...`(WHKz1ZW
+// @@gR._+_..~..-<+z<<?<>```_.<_.(+1><_;_..(1_:`.<<??1z--(+Z!..<_.j<....`..(bgHAAQX
+// @@mR.(j:..~.._<z!`.(>~``` ~(_.(+<1><><_.(((_`.<__`.<_.(X>...<_.(<.....`.JUWWWyWW
+// @gmH_(zl..(.._+>```<+_````.~>``(+.<?>>_._(<```(<<``(__<>....<.._<.......dXkkkHHH
+// mmqHl(dk_.(_.-=~`.`.1-..._~-1.``_:`(??<_~(`.--.&_.`.<(;<...._.._<..`..._Xg@@@@@@
+// qHkpk(dX<.(;..j_```.(((JJ&a&-~``````.1<_```-(((e+.-(/`(>...._..(<......(Wmggg@@g
+// HVHbWcz><__+_.(_.(dWWHHH@HHc~````````.+~`` (jHMMMHHHm&.?..._<..(<_..._.(WqqHHmHg
+// 0>vWWkzZwl~<o.__`__~X@@HM@Hb ```.`.``. ```` d@@HHH@@K?76...(<..(<_...(_(ppWWWWHq
+// X0XWHKXXw$<(z<.( `` WHHMHHHH_``````````````.WHHMNMHHH_`(...(<_.(z_..._<(fWVC174W
+// XuXWHHWWz>__+z+.!`..??CZYCOX_`````````````.`~.OvTUZUS_`~.._+?_.(_~_.._zjO=1+~+jy
+// kkkkkkkkX:._<z=1(_`` << ``->``.``.``.``.```` ?<`` (v!`._..(??_.(1._.._=dUOOzzzwX
+// @@@@@@@@H<...1O=v<_...__ -_````````````````.`` `` ~.`` :.~+=?~.(;_(...jdQQQQQkkk
+// H@@@@@@@H~...(==>.~~~~~....`.`````````.`````.`........->.(===~~<<.(...(dg@@@@@@@
+// @@@H@@HHH_.__(=l>~.~~~~~....``.``.``.```..`......~~~~~(<_+=l=~_<.->..~_dqggggg@g
+// @H@@@@MHH_._<(=l>...........```````````````.`...~~~~~~+<(=lz=~((j=z_..~jWqmmgggm
+// @@H@@HHWH_._<(lll-.......```.````.``.`..`````........_z<+llZz~(lOO=<...(VYUUUW9Y
+// @@HMMHWZf>~_=:=llw+.`````````.`.```__~~_``.`````.....(z+llOOz_zllOlz~..~<<1+dW>_
+// MMM#MHHWXl~_=>1ltwOl&.`.``.`````.``````````````.````.(llttwtz(OltwOz<..__zwOwwOz
+// HM#HMHUUI<._1z+ttOZttlt&....``.``.`.````.``...``...(zZtttOktzjttttwlz_._<(Xkkkkk
+// HHHmHSZu:(_~+OztttXtttOZZttO+-..............-(+ztOttwttttd0tOZttttwOl<~.(_dMMHHH
+// rvuuXuuI~~<~(uttttwvOwwwkQQHMMHHHHHHHHHMMMNmgey?OwwwrtttwXOtwttttttXtO-~.((wZyyy
+// HHHHHHK>(~(-(dOrtrrl(QgMHMMMHHHHHHHHHHHHHHHH##HMNkX0rrrrXXrd%`` (Ctwwtz_~.<(Wg@H
+// NNNNNHD(~(zo~zXrrrQdHHMMNMHHHHHHHHHHHHHHHHHHHHHH##HNmyrdKkwZ ` _``-zwrt1~~_<(MNM
+// MMMMM#<<_jwr:(Z4QHHMMHMHHHHHHHHHHHHHHHHHHHHHHHHHHHH###NHSXZ>` ~````.OXtt>~._<?MM
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define dbg(x...) do { cout << "\033[32;1m " << #x << " -> "; err(x); } while (0)
+void err() { cout << "\033[39;0m" << endl; }
+template<template<typename...> class T, typename t, typename... A>
+void err(T<t> a, A... x) { for (auto v: a) cout << v << ' '; err(x...); }
+template<typename T, typename... A>
+void err(T a, A... x) { cout << a << ' '; err(x...); }
+#else
+#define dbg(...)
+#endif
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+template<class T> using vc=vector<T>;
+template<class T> using vvc=vc<vc<T>>;
+template<class T> void mkuni(vector<T>&v)
+{
+    sort(v.begin(),v.end());
+    v.erase(unique(v.begin(),v.end()),v.end());
+}
+ll rand_int(ll l, ll r) //[l, r]
+{
+    static mt19937_64 gen(chrono::steady_clock::now().time_since_epoch().count());
+    return uniform_int_distribution<ll>(l, r)(gen);
+}
+template<class T>
+void print(T x,int suc=1)
+{
+    cout<<x;
+    if(suc==1) cout<<'\n';
+    else cout<<' ';
+}
+template<class T>
+void print(const vector<T>&v,int suc=1)
+{
+    for(int i=0;i<v.size();i++)
+    print(v[i],i==(int)(v.size())-1?suc:2);
+}
+vi p;
+vi t;
+vi ans;
+void fillans(vi &a,int c)
+{
+    assert(a.size()<=4);
+    vi id(a.size());
+    iota(id.begin(),id.end(),0);
+    sort(id.begin(),id.end(),[&](int x,int y){
+        return t[a[x]]<t[a[y]];
+    });
+    for(auto u:id)
+    {
+        ans[a[u]]=c;
+        c++;
+    }
+    c--;
+    if(a.size()==4)
+    {
+        for(int i=c-3;i<c;i++)
+        {
+            bool suc=1;
+            if(id[3]!=0&&ans[a[id[3]-1]]==i) suc=0;
+            if(id[3]!=3&&ans[a[id[3]+1]]==i) suc=0;
+            if(suc)
+                ans[a[id[3]]]=i;
+        }
+        assert(ans[a[id[3]]]!=c);
+    }
+}
+const int maxn=1e4+7;
+void solve(vi a,int c=1)
+{
+    if(a.size()<=4)
+    {
+        fillans(a,c);
+        return;
+    }
+    vi vis(maxn);
+    vi nxt;
+    set<int> pos;
+    for(int i=0;i<a.size();i+=3)
+    {
+        vi tmp;
+        vi idd;
+        for(int j=i;j<min((int)a.size(),i+3);j++)
+        {
+            tmp.push_back(a[j]);
+            idd.push_back(j);
+        }
+        vi id(tmp.size());
+        iota(id.begin(),id.end(),0);
+        sort(id.begin(),id.end(),[&](int x,int y){
+            return t[tmp[x]]<t[tmp[y]];
+        });
+        nxt.push_back(tmp[id[0]]);
+        vis[tmp[id[0]]]=1;
+    }
+    sort(a.begin(),a.end(),[&](int x,int y){
+        return t[x]<t[y];
+    });
+    dbg(a);
+    for(auto u:a)
+    {
+        if(vis[u])
+        {
+            pos.insert(u);
+            continue;
+        }
+        int l=-1,r=-1;
+        auto it=pos.lower_bound(u);
+        map<int,int> cnt;
+        if(it!=pos.end())
+        {
+            if(!vis[*it])
+            {
+                dbg(*it);
+                r=ans[*it];
+                auto itr=it;
+                while(itr!=pos.end()&&!vis[*itr])
+                {
+                    cnt[ans[*itr]]++;
+                    itr++;
+                }
+            }
+        }
+        if(it!=pos.begin())
+        {
+            it--;
+            if(!vis[*it])
+            {
+                l=ans[*it];
+                do{
+                    if(vis[*it]) break;
+                    cnt[ans[*it]]++;
+                    if(it!=pos.begin())
+                        it--;
+                }while(it!=pos.begin());
+            }
+        }
+        dbg(u,l,r);
+        if(cnt[c]==0) ans[u]=c;
+        else if(cnt[c+1]==0) ans[u]=c+1;
+        else if(cnt[c+2]==0) ans[u]=c+2;
+        else{
+            for(int j=c;j<c+3;j++)
+            {
+                if(j!=l&&j!=r)
+                {
+                    ans[u]=j;
+                    break;
+                }
+            }
+        }
+        assert(ans[u]);
+        pos.insert(u);
+    }
+    dbg(nxt);
+    solve(nxt,c+3);
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int T;
+    cin>>T;
+    while(T--)
+    {
+        int n;
+        cin>>n;
+        p=vi(n);
+        t=vi(n);
+        ans=vi(n);
+        for(int i=0;i<n;i++)
+        {
+            cin>>p[i];
+            p[i]--;
+            t[p[i]]=i;
+        }
+        vi all(n);
+        iota(all.begin(),all.end(),0);
+        solve(all);
+        print(ans);
+    }
+}
---
+++ compute/normal/1371/F.cpp
@@ -0,0 +1,202 @@
+/* Aa^~ kokoro ga pyonpyon suru n jaa^~
+// ZZZXXkXkkkZ!``` ``` ``` ``` ``` ``` ``` ``` ``` ``` ``` ```?Wfpppbpbbpbbpbbbkbkk
+// ppbbbpbbpVr`` `` ` ` ` ` ```` `` ` ` ``  ` ` ` ` ` ` ` ` `  dppbbkkkkkkkkkkqkqkk
+// HkqqqqqkkWr`` ` ` ``` ``` `?G, ` ` ``.JC!```` ` ` `` `` ````(Wpbkkkkkkkkqkkkkkqk
+// mmmmmqqqqpr` `` `` ```````.+zT=`` `` 7TO-.```````` `` `` ```(yppbkkkkkkkkkkkkkkk
+// ggmgmmqqqH$ ``````````....````` `  ````````.`````` `` ``````.yfpppbbbbkkkkqqqqqH
+// gmmmmmqqqkW<```` `````...`````  .,.`  ````....````` ``````` (Wbqqmgmmgmggggggggg
+// qmmmqqqqkkWk.``````````````````` ;:<`` `````.`````````````-_<-?WHHqmmmmmmgmmgggg
+// @@@@@@@gggHH6- ``````````````` `` _ `` ```````````````` ._~~_.`-?Wkqmmmmmmmggg@g
+// @@@@g@gggHY~.-<_- `````````````````````````````````` ._~~(<-``.`.(WHqqqmmggggmmm
+// @@g@gggHH=.`..._<-___..```````````````````````. .-_~~~_(!``-.``.`` OHHWUWHmqHWXW
+// gggggmqK1.``..~.. _<<+-(____..  ````````  ..__~~_((<<!.`.``` .``.`` j0C1XUHmHIdW
+// ggmmqH0!,_``.>`````` _<<;<v<<<++((((((((((<<<<<<~_. (-.``~``.>..```  jOuWHHqHIdH
+// gmmqkW!`(_ J>`    `` ` _~<`_~~~~<<<<<<<~~__````````` ?1. ._`(__```    zXWHg@HkXH
+// gHHWS{``(lJ<!``.``.```(:+>`._`````.` <..`` - ``. `   _ ?&._.I`_`````` .XyVfppppW
+// HHHSv``.(X:_..... _..(;+<!.(<..-.....-.-_..+_`..<.`.`..`_IJd}  .`..````jqg@@@@@@
+// XHWZ{..<Jk~!.`.. (<.-(+>(_.(1.(_..`.`.`.<_.+<_..<<-..._..-zy>.`_`...```.WH@HHHHH
+// bkWt~.-jCz(_..`.(+<.(;< ._-<=_(<_..-....(_.<1<..(<<.`._..-JUS-._.`...```dHmH9VUH
+// WUUO..(f.(c...__+z<-(+~` _-+<_(><..__.`.(<._.z_.(1;_..__.(C(zT-(..`...``(WHR<+Xk
+// kkkk._(_.->..._(z;:_><.._>_+_<(1>_._<...(v<<.(<.(+z<..-_(Z~_<_j+_..`...`(WHKz1ZW
+// @@gR._+_..~..-<+z<<?<>```_.<_.(+1><_;_..(1_:`.<<??1z--(+Z!..<_.j<....`..(bgHAAQX
+// @@mR.(j:..~.._<z!`.(>~``` ~(_.(+<1><><_.(((_`.<__`.<_.(X>...<_.(<.....`.JUWWWyWW
+// @gmH_(zl..(.._+>```<+_````.~>``(+.<?>>_._(<```(<<``(__<>....<.._<.......dXkkkHHH
+// mmqHl(dk_.(_.-=~`.`.1-..._~-1.``_:`(??<_~(`.--.&_.`.<(;<...._.._<..`..._Xg@@@@@@
+// qHkpk(dX<.(;..j_```.(((JJ&a&-~``````.1<_```-(((e+.-(/`(>...._..(<......(Wmggg@@g
+// HVHbWcz><__+_.(_.(dWWHHH@HHc~````````.+~`` (jHMMMHHHm&.?..._<..(<_..._.(WqqHHmHg
+// 0>vWWkzZwl~<o.__`__~X@@HM@Hb ```.`.``. ```` d@@HHH@@K?76...(<..(<_...(_(ppWWWWHq
+// X0XWHKXXw$<(z<.( `` WHHMHHHH_``````````````.WHHMNMHHH_`(...(<_.(z_..._<(fWVC174W
+// XuXWHHWWz>__+z+.!`..??CZYCOX_`````````````.`~.OvTUZUS_`~.._+?_.(_~_.._zjO=1+~+jy
+// kkkkkkkkX:._<z=1(_`` << ``->``.``.``.``.```` ?<`` (v!`._..(??_.(1._.._=dUOOzzzwX
+// @@@@@@@@H<...1O=v<_...__ -_````````````````.`` `` ~.`` :.~+=?~.(;_(...jdQQQQQkkk
+// H@@@@@@@H~...(==>.~~~~~....`.`````````.`````.`........->.(===~~<<.(...(dg@@@@@@@
+// @@@H@@HHH_.__(=l>~.~~~~~....``.``.``.```..`......~~~~~(<_+=l=~_<.->..~_dqggggg@g
+// @H@@@@MHH_._<(=l>...........```````````````.`...~~~~~~+<(=lz=~((j=z_..~jWqmmgggm
+// @@H@@HHWH_._<(lll-.......```.````.``.`..`````........_z<+llZz~(lOO=<...(VYUUUW9Y
+// @@HMMHWZf>~_=:=llw+.`````````.`.```__~~_``.`````.....(z+llOOz_zllOlz~..~<<1+dW>_
+// MMM#MHHWXl~_=>1ltwOl&.`.``.`````.``````````````.````.(llttwtz(OltwOz<..__zwOwwOz
+// HM#HMHUUI<._1z+ttOZttlt&....``.``.`.````.``...``...(zZtttOktzjttttwlz_._<(Xkkkkk
+// HHHmHSZu:(_~+OztttXtttOZZttO+-..............-(+ztOttwttttd0tOZttttwOl<~.(_dMMHHH
+// rvuuXuuI~~<~(uttttwvOwwwkQQHMMHHHHHHHHHMMMNmgey?OwwwrtttwXOtwttttttXtO-~.((wZyyy
+// HHHHHHK>(~(-(dOrtrrl(QgMHMMMHHHHHHHHHHHHHHHH##HMNkX0rrrrXXrd%`` (Ctwwtz_~.<(Wg@H
+// NNNNNHD(~(zo~zXrrrQdHHMMNMHHHHHHHHHHHHHHHHHHHHHH##HNmyrdKkwZ ` _``-zwrt1~~_<(MNM
+// MMMMM#<<_jwr:(Z4QHHMMHMHHHHHHHHHHHHHHHHHHHHHHHHHHHH###NHSXZ>` ~````.OXtt>~._<?MM
+*/
+#include<bits/stdc++.h>
+using namespace std;
+#ifndef ONLINE_JUDGE
+#define dbg(x...) do { cout << "\033[32;1m " << #x << " -> "; err(x); } while (0)
+void err() { cout << "\033[39;0m" << endl; }
+template<template<typename...> class T, typename t, typename... A>
+void err(T<t> a, A... x) { for (auto v: a) cout << v << ' '; err(x...); }
+template<typename T, typename... A>
+void err(T a, A... x) { cout << a << ' '; err(x...); }
+#else
+#define dbg(...)
+#endif
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef vector<int> vi;
+template<class T> using vc=vector<T>;
+template<class T> using vvc=vc<vc<T>>;
+template<class T> void mkuni(vector<T>&v)
+{
+    sort(v.begin(),v.end());
+    v.erase(unique(v.begin(),v.end()),v.end());
+}
+ll rand_int(ll l, ll r) //[l, r]
+{
+    static mt19937_64 gen(chrono::steady_clock::now().time_since_epoch().count());
+    return uniform_int_distribution<ll>(l, r)(gen);
+}
+template<class T>
+void print(T x,int suc=1)
+{
+    cout<<x;
+    if(suc==1) cout<<'\n';
+    else cout<<' ';
+}
+template<class T>
+void print(const vector<T>&v,int suc=1)
+{
+    for(int i=0;i<v.size();i++)
+    print(v[i],i==(int)(v.size())-1?suc:2);
+}
+struct D{
+    int lc,rc,cnt,rans,lans,len;
+    D(int _lc=0,int _rc=0,int _cnt=0,int _rans=0,int _lans=0,int _len=1):lc(_lc),rc(_rc),cnt(_cnt),rans(_rans),lans(_lans),len(_len){}
+};
+inline void upd(D &o,D l,D r)
+{
+    o.lc=l.lc;
+    if(l.lc==l.len) o.lc+=r.lc;
+    o.rc=r.rc;
+    if(r.rc==r.len) o.rc+=l.rc;
+    if(r.rans==r.len)
+    {
+        if(r.lc==r.len)
+            o.rans=max(r.rans+l.rc,r.rans+l.rans);
+        else o.rans=r.rans+l.rc;
+    }
+    else o.rans=r.rans;
+    if(l.lans==l.len)
+    {
+        if(l.rc==l.len)
+            o.lans=max(l.lans+r.lc,l.lans+r.lans);
+        else o.lans=l.lans+r.lc;
+    }
+    else o.lans=l.lans;
+    o.cnt=max({l.cnt,r.cnt,o.lans,o.rans,l.rc+r.lc,l.rans+r.lc,r.lans+l.rc,o.lc,o.rc});
+    o.len=l.len+r.len;
+}
+struct N{
+    D p,r;
+    int lz;
+    N(){}
+};
+const int maxn=5e5+7;
+N T[maxn<<2];
+int n,q;
+string s;
+#define lson o<<1
+#define rson o<<1|1
+#define Lson L,mid,lson
+#define Rson mid+1,R,rson
+inline void pushup(int o)
+{
+    upd(T[o].p,T[lson].p,T[rson].p);
+    upd(T[o].r,T[lson].r,T[rson].r);
+}
+void build(int L=0,int R=n-1,int o=1)
+{
+    if(L==R)
+    {
+        T[o].p.lc=T[o].r.rc=1,T[o].p.rc=T[o].r.lc=0;
+        T[o].p.rans=T[o].r.lans=1;
+        T[o].p.cnt=T[o].r.cnt=1;
+        if(s[L]!='<') swap(T[o].p,T[o].r);
+        return;
+    }
+    int mid=L+R>>1;
+    build(Lson);
+    build(Rson);
+    pushup(o);
+}
+inline void pushdown(int o)
+{
+    if(T[o].lz)
+    {
+        swap(T[lson].p,T[lson].r);
+        swap(T[rson].p,T[rson].r);
+        T[o].lz=0;
+        T[lson].lz^=1;
+        T[rson].lz^=1;
+    }
+}
+void rev(int l,int r,int L=0,int R=n-1,int o=1)
+{
+    if(l<=L&&r>=R)
+    {
+        swap(T[o].p,T[o].r);
+        T[o].lz^=1;
+        return;
+    }
+    pushdown(o);
+    int mid=L+R>>1;
+    if(l<=mid) rev(l,r,Lson);
+    if(r>mid) rev(l,r,Rson);
+    pushup(o);
+}
+D query(int l,int r,int L=0,int R=n-1,int o=1)
+{
+    if(l<=L&&r>=R)
+        return T[o].p;
+    pushdown(o);
+    int mid=L+R>>1;
+    D ret;
+    ret.len=0;
+    // dbg(L,R);
+    if(l<=mid) upd(ret,ret,query(l,r,Lson));
+    if(r>mid) upd(ret,ret,query(l,r,Rson));
+    return ret;
+}
+void solve(int l,int r)
+{
+    rev(l,r);
+    cout<<query(l,r).cnt<<'\n';
+}
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cin>>n>>q;
+    cin>>s;
+    build();
+    while(q--)
+    {
+        int l,r;
+        cin>>l>>r;
+        l--,r--;
+        solve(l,r);
+    }
+}
