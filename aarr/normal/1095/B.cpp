282438fda108ba89817b0cd27b2140dbab40b88f
ishan dutta <ishandutta2007@gmail.com>
2022-11-01 17:55:05 +0530

    aarr

---
+++ aarr/normal/1095/B.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <algorithm>
+#include <set>
+#include <iomanip>
+#include <vector>
+#include <cmath>
+#include <map>
+
+using namespace std;
+
+
+const int N = 100 * 1000 + 5;
+const int inf = 1000 * 1000 * 1000 + 7;
+
+int a[N];
+int b[N];
+bool mark[N];
+map <int, int> mp;
+
+
+int get(int x, int y) {
+	return (x >> y) & 1;
+}
+bool pal(string s) {
+	for (int i = 0; i < s.size(); i++) {
+		if (s[i] != s[s.size() - i - 1]) {
+			return false;
+		}
+	}
+	return true;
+}
+bool lucky(int x) {
+	while (x) {
+		if (x % 10 != 7 && x % 10 != 4) {
+			return false;
+		}
+		x /= 10;
+	}
+	return true;
+}
+int bmm(int x, int y) {
+	if (y == 0)
+		return x;
+	return bmm(y, x % y);
+}
+int main() {
+	int n;
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> a[i];
+	}
+	sort(a + 1, a + n + 1);
+	cout << min(a[n - 1] - a[1], a[n] - a[2]);
+}
