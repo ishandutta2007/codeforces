15f78773b2b267a4deac8f61c09af1e48fc7dd01
ishan dutta <ishandutta2007@gmail.com>
2022-11-02 21:01:24 +0530

    shenmadongdong.qwq

---
+++ shenmadongdong.qwq/normal/1553/B.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,abm,mmx,avx,avx2")
+#pragma GCC optimize("unroll-loops")
+#define rep(i,a,b) for(int i=(a);i<=(b);++i)
+#define req(i,a,b) for(int i=(a);i>=(b);--i)
+#define rep_(i,a,b) for(int i=(a);i<(b).size();++i)
+#define F(a) rep(a,1,n)
+#define M(a,b) memset(a,b,sizeof a)
+#define DC int T;cin>>T;while(T--)
+#define ll long long
+#define Z(a,b) sort(a+1,a+b+1)
+using namespace std;
+const unsigned _mod=998244353;
+const unsigned mod=1e9+7;
+const ll infi=0x3f3f3f3f3f3f3fll;
+const int inf=0x3f3f3f3f;
+void rd(auto &x){x=0;int f=1;char ch=getchar();while(ch<48||ch>57){if(ch==45)f=-1;ch=getchar();}while(ch>=48&&ch<=57)x=x*10+ch-48,ch=getchar();x*=f;}
+ll ksm(ll x,ll y=mod-2,ll m=mod){ll ret=1;while(y){if(y&1)ret=ret*x%m;y>>=1ll;x=x*x%m;}return ret;}
+ll qpow(ll x,ll y=2){ll ret=1;while(y>0){if(y&1ll)ret=ret*x;y>>=1ll;x=x*x;}return ret;}
+/*
+    [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+    [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+    [[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[[]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]]
+*/
+int n,m;
+char s[510],t[1010];
+int main()
+{
+	DC
+    {
+        bool flag=0;
+        cin>>s+1>>t+1;
+        n=strlen(s+1),m=strlen(t+1);
+        F(i)
+        {
+            rep(j,i,min(m+i-1,n))
+            {
+                bool fl=0;
+                int p=0;
+                rep(_,i,j) 
+                {
+                    p++;
+                    if (s[_]!=t[p]) fl=1;
+                }
+                req(_,j-1,1) 
+                {
+                    p++;
+                    if (p>m) break;
+                    if (s[_]!=t[p]) fl=1;
+                }
+                if (p<m) continue;
+                if (!fl) flag=1;
+            }
+        }
+        puts(flag?"YES":"NO");
+    }
+	return 0;
+}
