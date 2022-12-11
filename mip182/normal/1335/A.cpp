ab327385ffa6f54718b8a992f2df6517783c7622
ishan dutta <ishandutta2007@gmail.com>
2022-12-11 15:49:02 +0530

    mip182

---
+++ mip182/normal/1335/A.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <algorithm>
+#include <cmath>
+#include <vector>
+#include <set>
+#include <map>
+#include <unordered_set>
+#include <unordered_map>
+#include <queue>
+#include <ctime>
+#include <cassert>
+#include <complex>
+#include <string>
+#include <cstring>
+#include <chrono>
+#include <random>
+#include <bitset>
+typedef long long ll;
+#define pb push_back
+#define mii map <int, int>
+#define mll map <ll, ll>
+#define pii pair <int, int>
+#define pll pair <ll, ll>
+#define vi vector <int>
+#define vd vector <double>
+#define vll vector <ll>
+#define fi first
+#define se second
+#define si set <int>
+#define sll set <ll>
+#define spii set <pii>
+#define vs vector <string> 
+#define vpii vector <pair <int,int> >
+#define vpll vector <pair <long long,long long> >
+#define vvi vector <vector <int> >
+#define vvpii vector <vector <pii > >
+#define vvll vector <vll>
+#define vsi vector <si>
+#define forn(i, n) for (int i = 0; i < (n); i++)
+#define rep(i, n) for (int i = 0; i < (n); i++)
+#define for1(i, n) for (int i = 1; i < (n); i++)
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(),(x).rend()
+
+
+using namespace std;
+const ll INFLL = 1e18;
+const int MAXN = 1e6+100;
+const ll INF = 1e9;
+const ll mod1=1e9+7;
+const ll mod2=2e9+11;
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+    	ll n;
+    	cin>>n;
+    	cout<<(n-1)/2<<'\n';
+    }
+
+
+}
