e5eca0d18c2c35af255f55a20e45990761f62835
ishan dutta <ishandutta2007@gmail.com>
2022-12-11 20:20:02 +0530

    sert

---
+++ sert/normal/918/A.cpp
@@ -0,0 +1,42 @@
+#pragma GCC diagnostic ignored "-Wunused-result"
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+const int N = (int)1e6 + 34;
+//const ll INF = (ll)1e18 + 41;
+const int INF = (int)1e9 + 41;
+const ll MD = (ll)1e9 + 7;
+bool D = false;
+
+void solve() {
+	string s(N, 'o');
+	int a = 1;
+	int b = 1;
+	s[0] = 'O';
+	while (a + b < N) {
+		b = a + b;
+		a = b - a;
+		s[b - 1] = 'O';
+	}
+	int n;
+	cin >> n;
+	cout << s.substr(0, n) << "\n";
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int tst = 1;
+	//scanf("%d", &tst);
+	//cin >> tst;
+#ifdef MADE_BY_SERT
+	D = true;	
+	tst = 3;
+	while (tst--) {
+		solve();
+	}
+#else
+	while (tst--) solve();
+#endif
+}
